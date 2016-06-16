df4ffdb221f28eab6245ff5280b5c0f3acb4d96d
Henning Baldersheim <balder@yahoo-inc.com>
2016-06-16 10:48:08 +0000

    Ensure that fastos.h is included first

--- searchcore/src/vespa/searchcore/proton/server/feedstate.cpp
+++ searchcore/src/vespa/searchcore/proton/server/feedstate.cpp
@@ -1,11 +1,9 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
+#include <vespa/fastos/fastos.h>
+#include <vespa/document/bucket/bucketid.h>
+#include "feedstate.h"
 #include <vespa/log/log.h>
 LOG_SETUP(".feedstate");
-#include <vespa/fastos/fastos.h>
-
-#include "feedstate.h"
-#include <vespa/document/bucket/bucketid.h>
 
 using document::BucketId;
 using vespalib::IllegalStateException;
