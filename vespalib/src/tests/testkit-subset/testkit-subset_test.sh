c9423c786b49ac7c75f0b3a7c1ada0ba5cec4878
Geir Storli <geirst@yahoo-inc.com>
2016-06-20 14:13:41 +0200

    Fix unit test by allowing test functions to be executed in different order on CentOs.

--- vespalib/src/tests/testkit-subset/testkit-subset_test.sh
+++ vespalib/src/tests/testkit-subset/testkit-subset_test.sh
@@ -3,4 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 TEST_SUBSET=pass $VALGRIND ./vespalib_testkit-subset_test_app 2> out.txt
 TEST_SUBSET="extra\.cpp:.*pass.*" $VALGRIND ./vespalib_testkit-subset_test_app 2>> out.txt
 cat out.txt | grep "\.cpp: " | sed "s~$DIR~~g" > out.relpath.txt
+cmp -s out.relpath.txt out.ref.2.txt && exit 0
 diff -u out.relpath.txt out.ref.txt
