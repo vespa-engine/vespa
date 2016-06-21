1d786a57d5e1b1a782458a928258281ab9c05943
Arnstein Ressem <aressem@gmail.com>
2016-06-21 22:01:53 +0200

    Need to map a temporary directory for rpmbuild. 10G container on Linux limit is not enough and kludgey to change.

--- docker/build-vespa.sh
+++ docker/build-vespa.sh
@@ -8,5 +8,9 @@ fi
 VESPA_VERSION=$1
 
 docker build -t vespabuild -f Dockerfile.build .
-docker run --rm -v $(pwd)/..:/vespa --entrypoint /vespa/docker/build-vespa-internal.sh vespabuild "$VESPA_VERSION"
+
+TMP=$(mktemp -d)
+docker run --rm -v $(pwd)/..:/vespa -v $TMP:/root/rpmbuild --entrypoint /vespa/docker/build-vespa-internal.sh vespabuild "$VESPA_VERSION"
+
+rm -rf $TMP
 
