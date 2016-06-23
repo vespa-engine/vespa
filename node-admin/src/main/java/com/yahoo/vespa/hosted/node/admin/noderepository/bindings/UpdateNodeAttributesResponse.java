67e8d980c0ca2f7d0fb63c51a0f730a2ace29dc8
Harald Musum <musum@yahoo-inc.com>
2016-06-23 11:04:51 +0200

    Log and trow when updating node attributes fails * Catch exception, log with error message and rethrow instead   of just a stack trace with response code and nothing else

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/noderepository/bindings/UpdateNodeAttributesResponse.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/noderepository/bindings/UpdateNodeAttributesResponse.java
@@ -1,6 +1,8 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.noderepository.bindings;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Automagically handles (de)serialization based on 1:1 message fields and identifier names.
  * Deserializes JSON strings on the form:
@@ -12,6 +14,7 @@ package com.yahoo.vespa.hosted.node.admin.noderepository.bindings;
  *
  * @author bakksjo
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateNodeAttributesResponse {
     public String message;
 }
