3e868cfa22a05c1d9acc975c5142e8dae166e099
Henning Baldersheim <balder@yahoo-inc.com>
2016-06-23 19:28:00 +0000

    Use set -e to get the test to fail.

--- searchlib/src/tests/features/prod_features_test.sh
+++ searchlib/src/tests/features/prod_features_test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-VESPA_LOG_TARGET=file:vlog2.txt $VALGRIND ./searchlib_prod_features_test_app && rm -rf *.dat
+set -e
+VESPA_LOG_TARGET=file:vlog2.txt $VALGRIND ./searchlib_prod_features_test_app
+rm -rf *.dat
