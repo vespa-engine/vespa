7ff33c8950725cb20342d1fb88839ba125b8b126
Arne Juul <arnej@yahoo-inc.com>
2016-06-28 14:33:40 +0200

    run tests in correct order

--- fsa/src/alltest/alltest.sh
+++ fsa/src/alltest/alltest.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
-./detector_test.sh
-./fsa_test.sh
+
+# first create the FSA
 ./fsa_fsa_create_test_app
-./fsa_fsa_perf_test_app
-./fsa_fsamanager_test_app . __testfsa__.__fsa__ 
+
+# simple tests
 ./lookup_test.sh
+./fsa_test.sh
+./detector_test.sh
+
+# advanced tests
 ./ngram_test.sh
 ./segmenter_test.sh
 ./vectorizer_test.sh
+
+# manager test
+./fsa_fsamanager_test_app . __testfsa__.__fsa__
+
+# perf tests
 ./fsa_vectorizer_perf_test_app
+./fsa_fsa_perf_test_app
