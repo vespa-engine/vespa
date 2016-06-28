dc1ebf57100a2f89cbdc8358b35d660ffe12b400
Arne Juul <arnej@yahoo-inc.com>
2016-06-28 14:34:07 +0200

    fix usage of progctl.sh

--- messagebus/test/src/tests/trace/ctl.sh
+++ messagebus/test/src/tests/trace/ctl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-set -e
-bash -e ../../binref/progctl.sh progdefs.sh "$@"
+
+exec ../../binref/progctl.sh progdefs.sh "$@"
