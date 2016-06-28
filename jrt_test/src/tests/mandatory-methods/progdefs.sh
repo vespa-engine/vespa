5de783fd8e22c9f0b2e84fb679e8d33afd7a3a1e
Arne Juul <arnej@yahoo-inc.com>
2016-06-28 14:37:25 +0200

    should not use "set -e" in progdefs.sh

--- jrt_test/src/tests/mandatory-methods/progdefs.sh
+++ jrt_test/src/tests/mandatory-methods/progdefs.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
 # Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-set -e
 prog javaserver 1 "tcp/$PORT_1" "$BINREF/runjava RPCServer"
