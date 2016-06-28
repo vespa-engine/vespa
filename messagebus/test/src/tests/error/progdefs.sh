0c6a110f35f206394b6532e2ac0f86a837837581
Arne Juul <arnej@yahoo-inc.com>
2016-06-28 14:37:25 +0200

    should not use "set -e" in progdefs.sh

--- messagebus/test/src/tests/error/progdefs.sh
+++ messagebus/test/src/tests/error/progdefs.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
 # Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-set -e
 prog server cpp "" "./messagebus_test_cpp-server-error_app"
 prog server java "" "../../binref/runjava JavaServer"
