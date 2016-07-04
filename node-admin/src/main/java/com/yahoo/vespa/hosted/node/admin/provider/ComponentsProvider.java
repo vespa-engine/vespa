cc79689d5ace2fa1c1849592faeef5f7476083e5
Harald Musum <musum@yahoo-inc.com>
2016-07-04 21:33:55 +0200

    Add missing copyrights header

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/provider/ComponentsProvider.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/provider/ComponentsProvider.java
@@ -1,3 +1,4 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.provider;
 
 import com.yahoo.vespa.hosted.node.admin.nodeadmin.NodeAdminStateUpdater;
@@ -8,6 +9,5 @@ import com.yahoo.vespa.hosted.node.admin.nodeadmin.NodeAdminStateUpdater;
  * @author dybis
  */
 public interface ComponentsProvider {
-     NodeAdminStateUpdater getNodeAdminStateUpdater();
-
+    NodeAdminStateUpdater getNodeAdminStateUpdater();
 }
