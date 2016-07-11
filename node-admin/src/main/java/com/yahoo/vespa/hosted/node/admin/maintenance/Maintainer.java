2a0077888a07b6095d3a4a2f8e49835137a1b8e3
valerijf <valerijf@yahoo-inc.com>
2016-07-11 15:39:13 +0200

    Made maintenance job name an enum

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/Maintainer.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/Maintainer.java
@@ -7,25 +7,28 @@ import java.util.Arrays;
  */
 public class Maintainer {
     public static void main(String[] args) {
-        String[] options = {"delete-old-app-data"};
-
         if (args.length == 0) {
             System.err.println("Please specify job name. Valid job names:");
-            System.err.println(String.join(", ", options));
+            System.err.println(Arrays.asList(JobName.values()));
             return;
         }
 
-        String jobName = args[0];
-        args = Arrays.copyOfRange(args, 1, args.length);
-        switch (jobName) {
-            case "delete-old-app-data":
-                DeleteOldAppData.main(args);
-                break;
+        try {
+            JobName jobName = JobName.valueOf(args[0]);
+            args = Arrays.copyOfRange(args, 1, args.length);
+            switch (jobName) {
+                case delete_old_app_data:
+                    DeleteOldAppData.main(args);
+                    break;
+            }
 
-            default:
-                System.err.println("Invalid job name. Valid job names:");
-                System.err.println(String.join(", ", options));
-                break;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid job name. Valid job names:");
+            System.err.println(Arrays.asList(JobName.values()));
         }
+    }
+
+    private enum JobName {
+        delete_old_app_data
     }
 }
