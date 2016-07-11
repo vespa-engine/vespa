f37c1ef250e0ef8f5b46dc793fed73f639318fd8
valerijf <valerijf@yahoo-inc.com>
2016-07-11 16:12:48 +0200

    Updated tests to w.r.t. max_age default value and using JUnits own TemporaryFolder

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/maintenance/DeleteOldAppDataTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/maintenance/DeleteOldAppDataTest.java
@@ -1,7 +1,9 @@
 package com.yahoo.vespa.hosted.node.admin.maintenance;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,79 +16,83 @@ import static org.hamcrest.core.Is.is;
  * @author valerijf
  */
 public class DeleteOldAppDataTest {
-    private File folder;
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void initFiles() throws IOException {
-        folder = File.createTempFile("temp_folder", String.valueOf(System.currentTimeMillis()));
-        folder.delete(); // Delete the file so that we can create directory with the same name
-        folder.mkdir(); // Create create the directory
-        folder.deleteOnExit(); // Delete the directory after we are finished
-
         for (int i=0; i<10; i++) {
-            File temp = File.createTempFile("test_" + i, ".json", folder);
+            File temp = folder.newFile("test_" + i + ".json");
             temp.setLastModified(System.currentTimeMillis() - i*Duration.ofSeconds(130).toMillis());
-            temp.deleteOnExit();
         }
 
         for (int i=0; i<7; i++) {
-            File temp = File.createTempFile("test_" + i + "_file", ".test", folder);
+            File temp = folder.newFile("test_" + i + "_file.test");
             temp.setLastModified(System.currentTimeMillis() - i*Duration.ofSeconds(250).toMillis());
-            temp.deleteOnExit();
         }
 
         for (int i=0; i<5; i++) {
-            File temp = File.createTempFile(i + "-abc", ".json", folder);
+            File temp = folder.newFile(i + "-abc" + ".json");
             temp.setLastModified(System.currentTimeMillis() - i*Duration.ofSeconds(80).toMillis());
-            temp.deleteOnExit();
         }
+
+        File temp = folder.newFile("test_week_old_file.json");
+        temp.setLastModified(System.currentTimeMillis() - Duration.ofDays(8).toMillis());
     }
 
     @Test
     public void testDeleteAll() {
-        String[] args = {"--path", folder.getAbsolutePath()};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--max_age=0"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(0));
+        assertThat(folder.getRoot().listFiles().length, is(0));
+    }
+
+    @Test
+    public void testDeleteAllDefaultMaxAge() {
+        String[] args = {"--path", folder.getRoot().getAbsolutePath()};
+        DeleteOldAppData.main(args);
+
+        assertThat(folder.getRoot().listFiles().length, is(22));
     }
 
     @Test
     public void testDeletePrefix() {
-        String[] args = {"--path", folder.getAbsolutePath(), "--prefix", "test_"};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--max_age=0"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(5));
+        assertThat(folder.getRoot().listFiles().length, is(5));
     }
 
     @Test
     public void testDeleteSuffix() {
-        String[] args = {"--path", folder.getAbsolutePath(), "--suffix", ".json"};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--suffix", ".json", "--max_age=0"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(7));
+        assertThat(folder.getRoot().listFiles().length, is(7));
     }
 
     @Test
     public void testDeletePrefixAndSuffix() {
-        String[] args = {"--path", folder.getAbsolutePath(), "--prefix", "test_", "--suffix", ".json"};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--suffix", ".json", "--max_age=0"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(12));
+        assertThat(folder.getRoot().listFiles().length, is(12));
     }
 
     @Test
     public void testDeleteOld() {
-        String[] args = {"--path", folder.getAbsolutePath(), "--max_age", "600"};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--max_age", "600"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(13)); // All 22 - 6 (from test_*_.json) - 3 (from test_*_file.test)
+        assertThat(folder.getRoot().listFiles().length, is(13)); // All 23 - 6 (from test_*_.json) - 3 (from test_*_file.test) - 1 week old file
     }
 
     @Test
     public void testDeleteWithAllParameters() {
-        String[] args = {"--path", folder.getAbsolutePath(), "--prefix", "test_", "--suffix", ".json", "--max_age", "200"};
+        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--suffix", ".json", "--max_age", "200"};
         DeleteOldAppData.main(args);
 
-        assertThat(folder.listFiles().length, is(14)); // All 22 - 8 (from test_*_.json)
+        assertThat(folder.getRoot().listFiles().length, is(14)); // All 23 - 8 (from test_*_.json) - 1 week old file
     }
 }
