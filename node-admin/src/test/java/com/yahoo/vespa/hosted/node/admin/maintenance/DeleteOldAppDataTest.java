4adefbcd4c2aac3856222fdb2cea57fb6e6c77ae
valerijf <valerijf@yahoo-inc.com>
2016-07-12 10:40:21 +0200

    Moved command line logic out of DeleteOldAppData and added option to delete recursively, updated the corresponding tests

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/maintenance/DeleteOldAppDataTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/maintenance/DeleteOldAppDataTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -36,63 +37,115 @@ public class DeleteOldAppDataTest {
             temp.setLastModified(System.currentTimeMillis() - i*Duration.ofSeconds(80).toMillis());
         }
 
-        File temp = folder.newFile("test_week_old_file.json");
+        File temp = folder.newFile("week_old_file.json");
         temp.setLastModified(System.currentTimeMillis() - Duration.ofDays(8).toMillis());
     }
 
     @Test
     public void testDeleteAll() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--max_age=0"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, null, null, false);
 
         assertThat(folder.getRoot().listFiles().length, is(0));
     }
 
     @Test
     public void testDeleteAllDefaultMaxAge() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath()};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(),
+                DeleteOldAppData.DEFAULT_MAX_AGE_IN_SECONDS, null, null, false);
 
         assertThat(folder.getRoot().listFiles().length, is(22));
     }
 
     @Test
     public void testDeletePrefix() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--max_age=0"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, "test_", null, false);
 
-        assertThat(folder.getRoot().listFiles().length, is(5));
+        assertThat(folder.getRoot().listFiles().length, is(6)); // 5 abc files + 1 week_old_file
     }
 
     @Test
     public void testDeleteSuffix() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--suffix", ".json", "--max_age=0"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, null, ".json", false);
 
         assertThat(folder.getRoot().listFiles().length, is(7));
     }
 
     @Test
     public void testDeletePrefixAndSuffix() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--suffix", ".json", "--max_age=0"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, "test_", ".json", false);
 
-        assertThat(folder.getRoot().listFiles().length, is(12));
+        assertThat(folder.getRoot().listFiles().length, is(13)); // 5 abc files + 7 test_*_file.test files + week_old_file
     }
 
     @Test
     public void testDeleteOld() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--max_age", "600"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 600, null, null, false);
 
         assertThat(folder.getRoot().listFiles().length, is(13)); // All 23 - 6 (from test_*_.json) - 3 (from test_*_file.test) - 1 week old file
     }
 
     @Test
     public void testDeleteWithAllParameters() {
-        String[] args = {"--path", folder.getRoot().getAbsolutePath(), "--prefix", "test_", "--suffix", ".json", "--max_age", "200"};
-        DeleteOldAppData.main(args);
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 200, "test_", ".json", false);
 
-        assertThat(folder.getRoot().listFiles().length, is(14)); // All 23 - 8 (from test_*_.json) - 1 week old file
+        assertThat(folder.getRoot().listFiles().length, is(15)); // All 23 - 8 (from test_*_.json)
+    }
+
+    @Test
+    public void testDeleteWithSubDirectoriesNoRecursive() throws IOException {
+        initSubDirectories();
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, "test_", ".json", false);
+
+        // 6 test_*.json from subFolder1/
+        // + 9 test_*.json and 4 abc_*.json from subFolder2/
+        // + 13 test_*.json from subFolder2/subSubFolder2/
+        // + 7 test_*_file.test and 5 *-abc.json and 1 week_old_file from root
+        // + subFolder1/ and subFolder2/ and subFolder2/subSubFolder2/ themselves
+        assertThat(getNumberOfFilesAndDirectoriesIn(folder.getRoot()), is(48));
+    }
+
+    @Test
+    public void testDeleteWithSubDirectoriesRecursive() throws IOException {
+        initSubDirectories();
+        DeleteOldAppData.deleteOldAppData(folder.getRoot().getAbsolutePath(), 0, "test_", ".json", true);
+
+        // 4 abc_*.json from subFolder2/
+        // + 7 test_*_file.test and 5 *-abc.json and 1 week_old_file from root
+        // + subFolder2/ itself
+        assertThat(getNumberOfFilesAndDirectoriesIn(folder.getRoot()), is(18));
+    }
+
+    private void initSubDirectories() throws IOException {
+        File subFolder1 = folder.newFolder("subFolder1");
+        File subFolder2 = folder.newFolder("subFolder2");
+        File subSubFolder2 = folder.newFolder("subFolder2/subSubFolder2");
+
+        for (int j=0; j<6; j++) {
+            File.createTempFile("test_", ".json", subFolder1);
+        }
+
+        for (int j=0; j<9; j++) {
+            File.createTempFile("test_", ".json", subFolder2);
+        }
+
+        for (int j=0; j<4; j++) {
+            File.createTempFile("abc_", ".txt", subFolder2);
+        }
+
+        for (int j=0; j<13; j++) {
+            File.createTempFile("test_", ".json", subSubFolder2);
+        }
+    }
+
+    private static int getNumberOfFilesAndDirectoriesIn(File folder) {
+        int total = 0;
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                total += getNumberOfFilesAndDirectoriesIn(file);
+            }
+            total++;
+        }
+
+        return total;
     }
 }
