3a30d816ccf978a1334087eac121c98727734a86
Arnstein Ressem <aressem@yahoo-inc.com>
2016-07-21 09:14:45 +0200

    Separated post_install script from vespa.spec.

--- dist/post_install.sh
+++ dist/post_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <install prefix>"
   exit 1
@@ -9,7 +8,6 @@ fi
 declare -r PREFIX="$1"
 declare -r INSTALLPATH="$DESTDIR/$PREFIX"
 
-# BEGIN - Put this in post install script called by make install
 # Rewrite config def file names
 for path in $INSTALLPATH/var/db/vespa/config_server/serverdb/classes/*.def; do
     dir=$(dirname $path)
@@ -25,50 +23,50 @@ for path in $INSTALLPATH/var/db/vespa/config_server/serverdb/classes/*.def; do
     fi
 done
 
-mkdir -p $DESTDIR/$PREFIX/conf/configserver/
-mkdir -p $DESTDIR/$PREFIX/conf/configserver-app/
-mkdir -p $DESTDIR/$PREFIX/conf/configserver-app/config-models/
-mkdir -p $DESTDIR/$PREFIX/conf/configserver-app/components/
-mkdir -p $DESTDIR/$PREFIX/conf/filedistributor/
-mkdir -p $DESTDIR/$PREFIX/conf/node-admin-app/
-mkdir -p $DESTDIR/$PREFIX/conf/node-admin-app/components/
-mkdir -p $DESTDIR/$PREFIX/conf/zookeeper/
-mkdir -p $DESTDIR/$PREFIX/libexec/jdisc_core/
-mkdir -p $DESTDIR/$PREFIX/libexec/vespa/modelplugins/
-mkdir -p $DESTDIR/$PREFIX/libexec/vespa/plugins/qrs/
-mkdir -p $DESTDIR/$PREFIX/libexec/yjava_daemon/bin/
-mkdir -p $DESTDIR/$PREFIX/logs/jdisc_core/
-mkdir -p $DESTDIR/$PREFIX/logs/vespa/
-mkdir -p $DESTDIR/$PREFIX/logs/vespa/
-mkdir -p $DESTDIR/$PREFIX/logs/vespa/configserver/
-mkdir -p $DESTDIR/$PREFIX/logs/vespa/search/
-mkdir -p $DESTDIR/$PREFIX/logs/vespa/qrs/
-mkdir -p $DESTDIR/$PREFIX/share/vespa/
-mkdir -p $DESTDIR/$PREFIX/share/vespa/schema/version/6.x/schema/
-mkdir -p $DESTDIR/$PREFIX/tmp/vespa/
-mkdir -p $DESTDIR/$PREFIX/var/db/jdisc/logcontrol/
-mkdir -p $DESTDIR/$PREFIX/var/db/vespa/
-mkdir -p $DESTDIR/$PREFIX/var/db/vespa/config_server/serverdb/configs/
-mkdir -p $DESTDIR/$PREFIX/var/db/vespa/config_server/serverdb/configs/application/
-mkdir -p $DESTDIR/$PREFIX/var/db/vespa/config_server/serverdb/applications/
-mkdir -p $DESTDIR/$PREFIX/var/db/vespa/logcontrol/
-mkdir -p $DESTDIR/$PREFIX/var/jdisc_container/
-mkdir -p $DESTDIR/$PREFIX/var/jdisc_core/
-mkdir -p $DESTDIR/$PREFIX/var/run/
-mkdir -p $DESTDIR/$PREFIX/var/spool/vespa/
-mkdir -p $DESTDIR/$PREFIX/var/spool/master/inbox/
-mkdir -p $DESTDIR/$PREFIX/var/vespa/bundlecache/
-mkdir -p $DESTDIR/$PREFIX/var/vespa/cache/config/
-mkdir -p $DESTDIR/$PREFIX/var/vespa/cmdlines/
-mkdir -p $DESTDIR/$PREFIX/var/zookeeper/version-2/
+mkdir -p $INSTALLDIR/conf/configserver/
+mkdir -p $INSTALLDIR/conf/configserver-app/
+mkdir -p $INSTALLDIR/conf/configserver-app/config-models/
+mkdir -p $INSTALLDIR/conf/configserver-app/components/
+mkdir -p $INSTALLDIR/conf/filedistributor/
+mkdir -p $INSTALLDIR/conf/node-admin-app/
+mkdir -p $INSTALLDIR/conf/node-admin-app/components/
+mkdir -p $INSTALLDIR/conf/zookeeper/
+mkdir -p $INSTALLDIR/libexec/jdisc_core/
+mkdir -p $INSTALLDIR/libexec/vespa/modelplugins/
+mkdir -p $INSTALLDIR/libexec/vespa/plugins/qrs/
+mkdir -p $INSTALLDIR/libexec/yjava_daemon/bin/
+mkdir -p $INSTALLDIR/logs/jdisc_core/
+mkdir -p $INSTALLDIR/logs/vespa/
+mkdir -p $INSTALLDIR/logs/vespa/
+mkdir -p $INSTALLDIR/logs/vespa/configserver/
+mkdir -p $INSTALLDIR/logs/vespa/search/
+mkdir -p $INSTALLDIR/logs/vespa/qrs/
+mkdir -p $INSTALLDIR/share/vespa/
+mkdir -p $INSTALLDIR/share/vespa/schema/version/6.x/schema/
+mkdir -p $INSTALLDIR/tmp/vespa/
+mkdir -p $INSTALLDIR/var/db/jdisc/logcontrol/
+mkdir -p $INSTALLDIR/var/db/vespa/
+mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/configs/
+mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/configs/application/
+mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/applications/
+mkdir -p $INSTALLDIR/var/db/vespa/logcontrol/
+mkdir -p $INSTALLDIR/var/jdisc_container/
+mkdir -p $INSTALLDIR/var/jdisc_core/
+mkdir -p $INSTALLDIR/var/run/
+mkdir -p $INSTALLDIR/var/spool/vespa/
+mkdir -p $INSTALLDIR/var/spool/master/inbox/
+mkdir -p $INSTALLDIR/var/vespa/bundlecache/
+mkdir -p $INSTALLDIR/var/vespa/cache/config/
+mkdir -p $INSTALLDIR/var/vespa/cmdlines/
+mkdir -p $INSTALLDIR/var/zookeeper/version-2/
 
-ln -s $PREFIX/lib/jars/config-model-fat.jar $DESTDIR/$PREFIX/conf/configserver-app/components/config-model-fat.jar
-ln -s $PREFIX/lib/jars/configserver-jar-with-dependencies.jar $DESTDIR/$PREFIX/conf/configserver-app/components/configserver.jar
-ln -s $PREFIX/lib/jars/orchestrator-jar-with-dependencies.jar $DESTDIR/$PREFIX/conf/configserver-app/components/orchestrator.jar
-ln -s $PREFIX/lib/jars/node-repository-jar-with-dependencies.jar $DESTDIR/$PREFIX/conf/configserver-app/components/node-repository.jar
-ln -s $PREFIX/lib/jars/zkfacade-jar-with-dependencies.jar $DESTDIR/$PREFIX/conf/configserver-app/components/zkfacade.jar
-ln -s $PREFIX/conf/configserver-app/components $DESTDIR/$PREFIX/lib/jars/config-models
-ln -s storaged-bin $DESTDIR/$PREFIX/sbin/distributord-bin
+ln -s $PREFIX/lib/jars/config-model-fat.jar $INSTALLDIR/conf/configserver-app/components/config-model-fat.jar
+ln -s $PREFIX/lib/jars/configserver-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/configserver.jar
+ln -s $PREFIX/lib/jars/orchestrator-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/orchestrator.jar
+ln -s $PREFIX/lib/jars/node-repository-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/node-repository.jar
+ln -s $PREFIX/lib/jars/zkfacade-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/zkfacade.jar
+ln -s $PREFIX/conf/configserver-app/components $INSTALLDIR/lib/jars/config-models
+ln -s storaged-bin $INSTALLDIR/sbin/distributord-bin
 
 
 
