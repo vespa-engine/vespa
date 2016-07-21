dd606d58a368a97d75127e3a3901d59e0a8541fd
Arnstein Ressem <aressem@yahoo-inc.com>
2016-07-21 10:51:47 +0200

    Wrong variable name after refactoring.

--- dist/post_install.sh
+++ dist/post_install.sh
@@ -23,50 +23,50 @@ for path in $INSTALLPATH/var/db/vespa/config_server/serverdb/classes/*.def; do
     fi
 done
 
-mkdir -p $INSTALLDIR/conf/configserver/
-mkdir -p $INSTALLDIR/conf/configserver-app/
-mkdir -p $INSTALLDIR/conf/configserver-app/config-models/
-mkdir -p $INSTALLDIR/conf/configserver-app/components/
-mkdir -p $INSTALLDIR/conf/filedistributor/
-mkdir -p $INSTALLDIR/conf/node-admin-app/
-mkdir -p $INSTALLDIR/conf/node-admin-app/components/
-mkdir -p $INSTALLDIR/conf/zookeeper/
-mkdir -p $INSTALLDIR/libexec/jdisc_core/
-mkdir -p $INSTALLDIR/libexec/vespa/modelplugins/
-mkdir -p $INSTALLDIR/libexec/vespa/plugins/qrs/
-mkdir -p $INSTALLDIR/libexec/yjava_daemon/bin/
-mkdir -p $INSTALLDIR/logs/jdisc_core/
-mkdir -p $INSTALLDIR/logs/vespa/
-mkdir -p $INSTALLDIR/logs/vespa/
-mkdir -p $INSTALLDIR/logs/vespa/configserver/
-mkdir -p $INSTALLDIR/logs/vespa/search/
-mkdir -p $INSTALLDIR/logs/vespa/qrs/
-mkdir -p $INSTALLDIR/share/vespa/
-mkdir -p $INSTALLDIR/share/vespa/schema/version/6.x/schema/
-mkdir -p $INSTALLDIR/tmp/vespa/
-mkdir -p $INSTALLDIR/var/db/jdisc/logcontrol/
-mkdir -p $INSTALLDIR/var/db/vespa/
-mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/configs/
-mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/configs/application/
-mkdir -p $INSTALLDIR/var/db/vespa/config_server/serverdb/applications/
-mkdir -p $INSTALLDIR/var/db/vespa/logcontrol/
-mkdir -p $INSTALLDIR/var/jdisc_container/
-mkdir -p $INSTALLDIR/var/jdisc_core/
-mkdir -p $INSTALLDIR/var/run/
-mkdir -p $INSTALLDIR/var/spool/vespa/
-mkdir -p $INSTALLDIR/var/spool/master/inbox/
-mkdir -p $INSTALLDIR/var/vespa/bundlecache/
-mkdir -p $INSTALLDIR/var/vespa/cache/config/
-mkdir -p $INSTALLDIR/var/vespa/cmdlines/
-mkdir -p $INSTALLDIR/var/zookeeper/version-2/
+mkdir -p $INSTALLPATH/conf/configserver/
+mkdir -p $INSTALLPATH/conf/configserver-app/
+mkdir -p $INSTALLPATH/conf/configserver-app/config-models/
+mkdir -p $INSTALLPATH/conf/configserver-app/components/
+mkdir -p $INSTALLPATH/conf/filedistributor/
+mkdir -p $INSTALLPATH/conf/node-admin-app/
+mkdir -p $INSTALLPATH/conf/node-admin-app/components/
+mkdir -p $INSTALLPATH/conf/zookeeper/
+mkdir -p $INSTALLPATH/libexec/jdisc_core/
+mkdir -p $INSTALLPATH/libexec/vespa/modelplugins/
+mkdir -p $INSTALLPATH/libexec/vespa/plugins/qrs/
+mkdir -p $INSTALLPATH/libexec/yjava_daemon/bin/
+mkdir -p $INSTALLPATH/logs/jdisc_core/
+mkdir -p $INSTALLPATH/logs/vespa/
+mkdir -p $INSTALLPATH/logs/vespa/
+mkdir -p $INSTALLPATH/logs/vespa/configserver/
+mkdir -p $INSTALLPATH/logs/vespa/search/
+mkdir -p $INSTALLPATH/logs/vespa/qrs/
+mkdir -p $INSTALLPATH/share/vespa/
+mkdir -p $INSTALLPATH/share/vespa/schema/version/6.x/schema/
+mkdir -p $INSTALLPATH/tmp/vespa/
+mkdir -p $INSTALLPATH/var/db/jdisc/logcontrol/
+mkdir -p $INSTALLPATH/var/db/vespa/
+mkdir -p $INSTALLPATH/var/db/vespa/config_server/serverdb/configs/
+mkdir -p $INSTALLPATH/var/db/vespa/config_server/serverdb/configs/application/
+mkdir -p $INSTALLPATH/var/db/vespa/config_server/serverdb/applications/
+mkdir -p $INSTALLPATH/var/db/vespa/logcontrol/
+mkdir -p $INSTALLPATH/var/jdisc_container/
+mkdir -p $INSTALLPATH/var/jdisc_core/
+mkdir -p $INSTALLPATH/var/run/
+mkdir -p $INSTALLPATH/var/spool/vespa/
+mkdir -p $INSTALLPATH/var/spool/master/inbox/
+mkdir -p $INSTALLPATH/var/vespa/bundlecache/
+mkdir -p $INSTALLPATH/var/vespa/cache/config/
+mkdir -p $INSTALLPATH/var/vespa/cmdlines/
+mkdir -p $INSTALLPATH/var/zookeeper/version-2/
 
-ln -s $PREFIX/lib/jars/config-model-fat.jar $INSTALLDIR/conf/configserver-app/components/config-model-fat.jar
-ln -s $PREFIX/lib/jars/configserver-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/configserver.jar
-ln -s $PREFIX/lib/jars/orchestrator-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/orchestrator.jar
-ln -s $PREFIX/lib/jars/node-repository-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/node-repository.jar
-ln -s $PREFIX/lib/jars/zkfacade-jar-with-dependencies.jar $INSTALLDIR/conf/configserver-app/components/zkfacade.jar
-ln -s $PREFIX/conf/configserver-app/components $INSTALLDIR/lib/jars/config-models
-ln -s storaged-bin $INSTALLDIR/sbin/distributord-bin
+ln -s $PREFIX/lib/jars/config-model-fat.jar $INSTALLPATH/conf/configserver-app/components/config-model-fat.jar
+ln -s $PREFIX/lib/jars/configserver-jar-with-dependencies.jar $INSTALLPATH/conf/configserver-app/components/configserver.jar
+ln -s $PREFIX/lib/jars/orchestrator-jar-with-dependencies.jar $INSTALLPATH/conf/configserver-app/components/orchestrator.jar
+ln -s $PREFIX/lib/jars/node-repository-jar-with-dependencies.jar $INSTALLPATH/conf/configserver-app/components/node-repository.jar
+ln -s $PREFIX/lib/jars/zkfacade-jar-with-dependencies.jar $INSTALLPATH/conf/configserver-app/components/zkfacade.jar
+ln -s $PREFIX/conf/configserver-app/components $INSTALLPATH/lib/jars/config-models
+ln -s storaged-bin $INSTALLPATH/sbin/distributord-bin
 
 
 
