84f4f3b30ee666b04633eee811b9c0b5855fa1d1
Henning Baldersheim <balder@yahoo-inc.com>
2016-08-15 15:44:27 +0200

    Unify code.

--- searchlib/src/vespa/searchlib/docstore/logdatastore.cpp
+++ searchlib/src/vespa/searchlib/docstore/logdatastore.cpp
@@ -1082,14 +1082,9 @@ class LogDataStore::WrapVisitorProgress : public IFileChunkVisitorProgress
     const uint32_t _totalChunks;
     uint32_t _processedChunks;
 
-public:    
-    virtual
-    ~WrapVisitorProgress()
-    {
-    }
-    
-    virtual void
-    updateProgress()
+public:
+    void
+    updateProgress() override
     {
         ++_processedChunks;
         if (_totalChunks != 0) {
