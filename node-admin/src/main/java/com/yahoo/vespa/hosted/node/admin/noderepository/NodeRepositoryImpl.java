2ac920e4aa11b0040d57e01ed57ab139b9f4189b
Haakon Dybdahl <dybdahl@yahoo-inc.com>
2016-08-24 15:53:53 +0200

    Revert "Polishing node repo impl a bit."
    This reverts commit 1cf0a575b5e2dbd2a64b861b694c914b7fe6bde4.

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/noderepository/NodeRepositoryImpl.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/noderepository/NodeRepositoryImpl.java
@@ -1,7 +1,6 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.noderepository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yahoo.vespa.applicationmodel.HostName;
 import com.yahoo.vespa.hosted.node.admin.ContainerNodeSpec;
@@ -16,12 +15,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -37,7 +35,7 @@ public class NodeRepositoryImpl implements NodeRepository {
     private final Set<HostName> configservers;
     private final int port;
     private final ObjectMapper mapper = new ObjectMapper();
-    private final HttpClient client = HttpClientBuilder.create().build();
+
 
     public NodeRepositoryImpl(Set<HostName> configServerHosts, int configPort, String baseHostName) {
         this.baseHostName = baseHostName;
@@ -45,69 +43,77 @@ public class NodeRepositoryImpl implements NodeRepository {
         this.port = configPort;
     }
 
-    interface CreateRequest {
-        HttpUriRequest createRequest(HostName configserver);
-    }
-
-    private <T extends  Object> T tryAllConfigServers(CreateRequest requestFactory, Class<T> wantedReturnType) throws IOException {
+    @Override
+    public List<ContainerNodeSpec> getContainersToRun() throws IOException {
         for (HostName configServer : configservers) {
             final HttpResponse response;
             try {
-
-                response = client.execute(requestFactory.createRequest(configServer));
+                String url = "http://" + configServer + ":" + port
+                        + "/nodes/v2/node/?parentHost=" + baseHostName + "&recursive=true";
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpGet request = new HttpGet(url);
+                response = client.execute(request);
             } catch (Exception e) {
-                NODE_ADMIN_LOGGER.info("Exception while talking to " + configServer + "(will try all config servers)", e);
                 continue;
             }
-            try {
-                return mapper.readValue(response.getEntity().getContent(), wantedReturnType);
-            } catch (IOException e) {
-                throw new RuntimeException("Response didn't contain nodes element, failed parsing?", e);
-            }
-        }
-        throw new RuntimeException("Did not get any positive answer");
-    }
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
 
-    @Override
-    public List<ContainerNodeSpec> getContainersToRun() throws IOException {
-
-        final GetNodesResponse nodesForHost = tryAllConfigServers(configserver -> {
-            String url = "http://" + configserver + ":" + port + "/nodes/v2/node/?parentHost=" + baseHostName + "&recursive=true";
-            return new HttpGet(url);
-        }, GetNodesResponse.class);
-
-        if (nodesForHost.nodes == null) {
-            throw new IOException("Response didn't contain nodes element");
-        }
-        List<ContainerNodeSpec> nodes = new ArrayList<>(nodesForHost.nodes.size());
-        for (GetNodesResponse.Node node : nodesForHost.nodes) {
-            ContainerNodeSpec nodeSpec;
-            try {
-                nodeSpec = createContainerNodeSpec(node);
-            } catch (IllegalArgumentException | NullPointerException e) {
-                NODE_ADMIN_LOGGER.warning("Bad node received from node repo when requesting children of the "
-                        + baseHostName + " host: " + node, e);
+            if (response.getStatusLine().getStatusCode() != 200) {
                 continue;
             }
-            nodes.add(nodeSpec);
+            final GetNodesResponse nodesForHost = mapper.readValue(response.getEntity().getContent(), GetNodesResponse.class);
+            if (nodesForHost.nodes == null) {
+                throw new IOException("Response didn't contain nodes element");
+            }
+            List<ContainerNodeSpec> nodes = new ArrayList<>(nodesForHost.nodes.size());
+            for (GetNodesResponse.Node node : nodesForHost.nodes) {
+                ContainerNodeSpec nodeSpec;
+                try {
+                    nodeSpec = createContainerNodeSpec(node);
+                } catch (IllegalArgumentException | NullPointerException e) {
+                    NODE_ADMIN_LOGGER.warning("Bad node received from node repo when requesting children of the "
+                            + baseHostName + " host: " + node, e);
+                    continue;
+                }
+                nodes.add(nodeSpec);
+            }
+            return nodes;
         }
-        return nodes;
-    }
+        throw new IOException("No answers from config servers");
 
+    }
     @Override
     public Optional<ContainerNodeSpec> getContainerNodeSpec(HostName hostName) throws IOException {
-        final GetNodesResponse nodeResponse = tryAllConfigServers(configserver -> {
-            String url = "http://" + configserver + ":" + port + "/nodes/v2/node/?hostname=" + hostName + "&recursive=true";
-            return new HttpGet(url);
-        }, GetNodesResponse.class);
+        for (HostName configServer : configservers) {
+            final HttpResponse response;
+            try {
+                String url = "http://" + configServer + ":" + port
+                        + "/nodes/v2/node/?hostname=" + hostName + "&recursive=true";
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpGet request = new HttpGet(url);
+                response = client.execute(request);
+            } catch (Exception e) {
+                continue;
+            }
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
 
-        if (nodeResponse.nodes.size() == 0) {
-            return Optional.empty();
+            if (response.getStatusLine().getStatusCode() != 200) {
+                continue;
+            }
+            final GetNodesResponse nodeResponse = mapper.readValue(response.getEntity().getContent(), GetNodesResponse.class);
+
+
+            if (nodeResponse.nodes.size() == 0) {
+                return Optional.empty();
+            }
+            if (nodeResponse.nodes.size() != 1) {
+                throw new RuntimeException("Did not get data for one node using hostname=" + hostName.toString() + "\n" + response.toString());
+            }
+            return Optional.of(createContainerNodeSpec(nodeResponse.nodes.get(0)));
         }
-        if (nodeResponse.nodes.size() != 1) {
-            throw new RuntimeException("Did not get data for one node using hostname=" + hostName.toString() + "\n" + nodeResponse.toString());
-        }
-        return Optional.of(createContainerNodeSpec(nodeResponse.nodes.get(0)));
+        throw new RuntimeException("Did not get any results.");
     }
 
     private static ContainerNodeSpec createContainerNodeSpec(GetNodesResponse.Node node)
@@ -146,36 +152,52 @@ public class NodeRepositoryImpl implements NodeRepository {
             final String currentVespaVersion)
             throws IOException {
 
-        UpdateNodeAttributesResponse response = tryAllConfigServers(configserver -> {
-            String url = "http://" + configserver + ":" + port  + "/nodes/v2/node/" + hostName;
-            HttpPatch request = new HttpPatch(url);
-            UpdateNodeAttributesRequestBody body = new UpdateNodeAttributesRequestBody(
-                    restartGeneration,
-                    dockerImage.asString(),
-                    currentVespaVersion);
+        for (HostName configServer : configservers) {
+            final HttpResponse response;
             try {
+                String url = "http://" + configServer + ":" + port
+                        + "/nodes/v2/node/" + hostName;
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpPatch request = new HttpPatch(url);
+                UpdateNodeAttributesRequestBody body = new UpdateNodeAttributesRequestBody(
+                        restartGeneration,
+                        dockerImage.asString(),
+                        currentVespaVersion);
                 request.setEntity(new StringEntity(mapper.writeValueAsString(body)));
-            } catch (UnsupportedEncodingException|JsonProcessingException e) {
-                throw new RuntimeException(e);
+                response = client.execute(request);
+            } catch (Exception e) {
+                continue;
             }
-            return request;
-        }, UpdateNodeAttributesResponse.class);
-        if (response.errorCode == null || response.errorCode.isEmpty()) {
+            if (response.getStatusLine().getStatusCode() != 200) {
+                System.out.println("Response Code : "
+                        + response.getStatusLine().getStatusCode());
+                continue;
+            }
             return;
         }
-        throw new RuntimeException("Unexcpected message " + response.message + " " + response.errorCode);
+
+        throw new RuntimeException("Failed to update node attributes for " + hostName.s() + ":" + /*updateResponse.message*/"");
     }
 
 
     @Override
     public void markAsReady(final HostName hostName) throws IOException {
-        final HttpResponse nodeResponse = tryAllConfigServers(configserver -> {
-            String url = "http://" + configserver + ":" + port + "/nodes/v2/ready/" + hostName;
-            return new HttpPut(url);
-        }, HttpResponse.class);
-
-
-        if (nodeResponse.getStatusLine().getStatusCode() == 200) {
+        for (HostName configServer : configservers) {
+            final HttpResponse response;
+            try {
+                String url = "http://" + configServer + ":" + port
+                        + "/nodes/v2/ready/" + hostName;
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpPut request = new HttpPut(url);
+                response = client.execute(request);
+            } catch (Exception e) {
+                continue;
+            }
+            if (response.getStatusLine().getStatusCode() != 200) {
+                System.out.println("Response Code : "
+                        + response.getStatusLine().getStatusCode());
+                continue;
+            }
             return;
         }
         throw new RuntimeException("Could not mark as ready" + hostName);
