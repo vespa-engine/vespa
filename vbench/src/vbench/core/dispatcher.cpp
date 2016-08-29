a74e1b7a5e6b4bd91c845f2085498b9c0020d924
Henning Baldersheim <balder@yahoo-inc.com>
2016-08-29 11:06:52 +0000

    Tell gcc to avoid warning on unused-functio here. It is expected.

--- vbench/src/vbench/core/dispatcher.cpp
+++ vbench/src/vbench/core/dispatcher.cpp
@@ -1,6 +1,7 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/fastos/fastos.h>
+#pragma GCC diagnostic ignored "-Wunused-function" // This dirty one is due a suspected bug in gcc 6.2
 #include "dispatcher.h"
 
 namespace vbench {
@@ -14,3 +15,4 @@ struct DummyItem {};
 template class Dispatcher<DummyItem>;
 
 } // namespace vbench
+#pragma GCC diagnostic pop
