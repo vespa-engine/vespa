26543db811bd8747a073cae38becc58c479daa7d
Henning Baldersheim <balder@yahoo-inc.com>
2016-08-29 11:40:01 +0000

    It is better to just remove the code that was intended to help development.

--- vbench/src/vbench/core/time_queue.cpp
+++ vbench/src/vbench/core/time_queue.cpp
@@ -1,18 +1,8 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/fastos/fastos.h>
-#pragma GCC diagnostic ignored "-Wunused-function" // This dirty one is due a suspected bug in gcc 6.2
 #include "time_queue.h"
 
 namespace vbench {
 
-namespace {
-
-struct DummyItem {};
-
-} // namespace vbench::<unnamed>
-
-template class TimeQueue<DummyItem>;
-
 } // namespace vbench
-#pragma GCC diagnostic pop
