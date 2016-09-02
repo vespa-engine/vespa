054c929358fbbffa1e6ca06bd13dbfac873d26eb
Henning Baldersheim <balder@yahoo-inc.com>
2016-09-02 20:13:07 +0000

    Proper shell exit values.

--- searchlib/src/tests/datastore/logdatastore_test.sh
+++ searchlib/src/tests/datastore/logdatastore_test.sh
@@ -15,4 +15,4 @@ truncate --size 3830 bug-7257706-truncated/1422358701368384000.idx
 fail=0
 VESPA_LOG_TARGET=file:vlog2.txt $VALGRIND ./searchlib_logdatastore_test_app || fail=1
 rm -rf bug-7257706-truncated dangling-test
-return fail
+exit $fail
