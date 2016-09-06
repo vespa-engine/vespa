9c4c0b8d5d71e74f82ac8437fc951df03d222642
Henning Baldersheim <balder@yahoo-inc.com>
2016-09-06 13:37:45 +0000

    Update test specification.

--- vespalib/src/tests/exception_classes/silenceuncaught_test.cpp
+++ vespalib/src/tests/exception_classes/silenceuncaught_test.cpp
@@ -17,7 +17,7 @@ TEST("that uncaught silenced exception causes exitcode 66") {
     EXPECT_EQUAL(proc.getExitCode(), 66);
 }
 
-TEST("that caught silenced exception followed by an uncaught causes exitcode 0") {
+TEST("that caught silenced exception followed by an uncaught causes negative exitcode.") {
     SlaveProc proc("./vespalib_caught_uncaught_app uncaught_after_silenced_and_caught");
     proc.wait();
     EXPECT_LESS(proc.getExitCode(), 0);
