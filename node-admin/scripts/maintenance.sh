91f719ec1c07fbcd478d67657d26650dc547239e
Harald Musum <musum@yahoo-inc.com>
2016-09-07 13:20:54 +0200

    Add vespalog to class path (needed for using LogLevel)

--- node-admin/scripts/maintenance.sh
+++ node-admin/scripts/maintenance.sh
@@ -2,7 +2,7 @@
 
 
 if [ -z "$CLASSPATH" ]; then
-    CLASSPATH=/home/y/lib/jars/node-admin-maintenance-jar-with-dependencies.jar:/home/y/lib/jars/docker-api-jar-with-dependencies.jar
+    CLASSPATH=/home/y/lib/jars/node-admin-maintenance-jar-with-dependencies.jar:/home/y/lib/jars/docker-api-jar-with-dependencies.jar:/home/y/lib/jars/vespalog.jar
 fi
 
 java \
