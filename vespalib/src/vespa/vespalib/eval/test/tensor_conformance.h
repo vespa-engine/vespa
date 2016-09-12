e73369b1a6c100acef088d6bc7b4a7757f958a39
Haavard <havardpe@yahoo-inc.com>
2016-09-12 10:00:22 +0000

    extend conformance test WIP

--- vespalib/src/vespa/vespalib/eval/test/tensor_conformance.h
+++ vespalib/src/vespa/vespalib/eval/test/tensor_conformance.h
@@ -12,13 +12,8 @@ namespace test {
  * A collection of tensor-related tests that can be run for various
  * implementations of the TensorEngine interface.
  **/
-class TensorConformance
-{
-private:
-    const TensorEngine &_engine;
-public:
-    TensorConformance(const TensorEngine &engine) : _engine(engine) {}
-    void run_all_tests() const;
+struct TensorConformance {
+    static void run_tests(const TensorEngine &engine);
 };
 
 } // namespace vespalib::eval::test
