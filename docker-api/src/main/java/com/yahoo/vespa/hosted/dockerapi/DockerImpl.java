03b78c8896fe91869fd6a6f548321552e7065807
valerijf <valerijf@yahoo-inc.com>
2016-09-14 16:24:30 +0200

    Code review fixes

--- docker-api/src/main/java/com/yahoo/vespa/hosted/dockerapi/DockerImpl.java
+++ docker-api/src/main/java/com/yahoo/vespa/hosted/dockerapi/DockerImpl.java
@@ -124,6 +124,7 @@ public class DockerImpl implements Docker {
     private void setupDockerNetworkIfNeeded() throws IOException, InterruptedException {
         if (! dockerClient.listNetworksCmd().withNameFilter(DOCKER_CUSTOM_MACVLAN_NETWORK_NAME).exec().isEmpty()) return;
 
+        // Use IPv6 address if there is a mix of IP4 and IPv6 by taking the longest address.
         List<InetAddress> hostAddresses = Arrays.asList(InetAddress.getAllByName(com.yahoo.net.HostName.getLocalhost()));
         InetAddress hostAddress = Collections.max(hostAddresses,
                 (o1, o2) -> o1.getAddress().length - o2.getAddress().length);
@@ -144,7 +145,6 @@ public class DockerImpl implements Docker {
                 .withDriver("macvlan")
                 .withEnableIpv6(true)
                 .withIpam(ipam)
-                .withCheckDuplicate(true)
                 .withOptions(dockerNetworkOptions)
                 .exec();
     }
