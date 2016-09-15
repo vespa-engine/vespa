b98805c6f63b428d62e715ac64630dfe12d126ac
Harald Musum <musum@yahoo-inc.com>
2016-09-15 14:36:18 +0200

    Refactoring to separate some business logic into ApplicationRepository * Start refactoring, no functional changes, more to be done

--- configserver/src/main/java/com/yahoo/vespa/config/server/http/v2/SessionActiveHandler.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/http/v2/SessionActiveHandler.java
@@ -1,24 +1,22 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.config.server.http.v2;
 
-import java.util.Optional;
 import java.util.concurrent.Executor;
 
 import com.google.inject.Inject;
-import com.yahoo.config.provision.Provisioner;
 import com.yahoo.config.provision.TenantName;
 import com.yahoo.config.provision.Zone;
 import com.yahoo.container.jdisc.HttpRequest;
 import com.yahoo.container.jdisc.HttpResponse;
 import com.yahoo.container.logging.AccessLog;
 import com.yahoo.log.LogLevel;
+import com.yahoo.vespa.config.server.ApplicationRepository;
 import com.yahoo.vespa.config.server.tenant.Tenant;
 import com.yahoo.vespa.config.server.tenant.Tenants;
 import com.yahoo.vespa.config.server.TimeoutBudget;
 import com.yahoo.vespa.config.server.http.SessionActiveHandlerBase;
 import com.yahoo.vespa.config.server.http.SessionHandler;
 import com.yahoo.vespa.config.server.http.Utils;
-import com.yahoo.vespa.config.server.provision.HostProvisionerProvider;
 import com.yahoo.vespa.config.server.session.LocalSession;
 
 /**
@@ -30,18 +28,16 @@ import com.yahoo.vespa.config.server.session.LocalSession;
 public class SessionActiveHandler extends SessionActiveHandlerBase {
 
     private final Tenants tenants;
-    private final Optional<Provisioner> hostProvisioner;
     private final Zone zone;
 
     @Inject
     public SessionActiveHandler(Executor executor,
                                 AccessLog accessLog,
                                 Tenants tenants,
-                                HostProvisionerProvider hostProvisionerProvider,
-                                Zone zone) {
-        super(executor, accessLog);
+                                Zone zone,
+                                ApplicationRepository applicationRepository) {
+        super(executor, accessLog, applicationRepository);
         this.tenants = tenants;
-        this.hostProvisioner = hostProvisionerProvider.getHostProvisioner();
         this.zone = zone;
     }
 
@@ -52,7 +48,7 @@ public class SessionActiveHandler extends SessionActiveHandlerBase {
         log.log(LogLevel.DEBUG, "Found tenant '" + tenantName + "' in request");
         Tenant tenant = Utils.checkThatTenantExists(tenants, tenantName);
         LocalSession localSession = getSessionFromRequestV2(tenant.getLocalSessionRepo(), request);
-        activate(request, tenant.getLocalSessionRepo(), tenant.getActivateLock(), timeoutBudget, hostProvisioner, localSession);
+        activate(request, tenant.getLocalSessionRepo(), tenant.getActivateLock(), timeoutBudget, localSession);
         return new SessionActiveResponse(localSession.getMetaData().getSlime(), tenantName, request, localSession, zone);
     }
 
