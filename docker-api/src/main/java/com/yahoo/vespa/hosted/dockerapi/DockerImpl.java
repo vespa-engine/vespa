cedb26037aff2602c81aadc7e298da5ece48e330
valerijf <valerijf@yahoo-inc.com>
2016-09-15 08:54:45 +0200

    Include docker exception with RuntimeException

--- docker-api/src/main/java/com/yahoo/vespa/hosted/dockerapi/DockerImpl.java
+++ docker-api/src/main/java/com/yahoo/vespa/hosted/dockerapi/DockerImpl.java
@@ -102,7 +102,7 @@ public class DockerImpl implements Docker {
         try {
             setupDockerNetworkIfNeeded();
         } catch (Exception e) {
-            throw new RuntimeException("Could not setup docker network");
+            throw new RuntimeException("Could not setup docker network", e);
         }
     }
 
@@ -143,7 +143,7 @@ public class DockerImpl implements Docker {
         dockerClient.createNetworkCmd()
                 .withName(DOCKER_CUSTOM_MACVLAN_NETWORK_NAME)
                 .withDriver("macvlan")
-                .withEnableIpv6(true)
+                .withEnableIpv6(isIPv6)
                 .withIpam(ipam)
                 .withOptions(dockerNetworkOptions)
                 .exec();
