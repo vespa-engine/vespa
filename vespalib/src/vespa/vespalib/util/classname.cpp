a5cd80a8c4768bfdd7d5de1a2e5c937196ffa7ee
Henning Baldersheim <balder@yahoo-inc.com>
2016-09-21 16:55:12 +0000

    Factor out the code to get demangled classnames to a common place

---
+++ vespalib/src/vespa/vespalib/util/classname.cpp
@@ -0,0 +1,17 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include <vespa/vespalib/util/classname.h>
+#include <cxxabi.h>
+
+namespace vespalib {
+
+string demangle(const char * native) {
+    int status = 0;
+    size_t size = 0;
+    char *unmangled = abi::__cxa_demangle(native, 0, &size, &status);
+    string result(unmangled);
+    free(unmangled);
+    return result;
+}
+
+}
