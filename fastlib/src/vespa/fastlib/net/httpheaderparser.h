ea9879e7d84c28bb670ef29bf3bdffa70e5109f2
Henning Baldersheim <balder@yahoo-inc.com>
2016-09-27 07:33:15 +0000

    - Bracing and style - Buffer on heap, not stack. - Do not read past buffer.

--- fastlib/src/vespa/fastlib/net/httpheaderparser.h
+++ fastlib/src/vespa/fastlib/net/httpheaderparser.h
@@ -1,53 +1,26 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/**
-*******************************************************************************
-*
-* @author          Markus Bjartveit Kr�ger
-* @date            Creation date: 2000-11-22
-* @version         $Id$
-*
-* @file
-*
-* HTTP header parser.
-*
-* Copyright (c)  : 2001 Fast Search & Transfer ASA
-*                  ALL RIGHTS RESERVED
-*
-******************************************************************************/
-
 
 #pragma once
 
-
+#include <vector>
 
 class Fast_BufferedInputStream;
 
-
-#define HTTPHEADERPARSER_LINE_BUFFER_SIZE  4096
-
-
 class Fast_HTTPHeaderParser
 {
-  private:
-    // Prevent use of:
-    Fast_HTTPHeaderParser(const Fast_HTTPHeaderParser &);
-    Fast_HTTPHeaderParser & operator=(const Fast_HTTPHeaderParser &);
-  protected:
-    char _pushBack;
-    bool _isPushBacked;
-    char _lineBuffer[HTTPHEADERPARSER_LINE_BUFFER_SIZE];
-    Fast_BufferedInputStream *_input;
-
   public:
+    Fast_HTTPHeaderParser(const Fast_HTTPHeaderParser &) = delete;
+    Fast_HTTPHeaderParser & operator = (const Fast_HTTPHeaderParser &) = delete;
     Fast_HTTPHeaderParser(Fast_BufferedInputStream &in);
-    virtual ~Fast_HTTPHeaderParser(void);
-
+    ~Fast_HTTPHeaderParser();
 
     // Methods
-    bool ReadRequestLine(const char *&method, const char *&url,
-                         int &versionMajor, int &versionMinor);
+    bool ReadRequestLine(const char *&method, const char *&url, int &versionMajor, int &versionMinor);
     bool ReadHeader(const char *&name, const char *&value);
+  private:
+    char                      _pushBack;
+    bool                      _isPushBacked;
+    const size_t              _bufferSize;
+    char                     *_lineBuffer;
+    Fast_BufferedInputStream *_input;
 };
-
-
-
