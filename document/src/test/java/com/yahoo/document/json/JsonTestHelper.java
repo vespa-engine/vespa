7c43a1e913c5c2b6e643812180cfa319743f67fe
Jon Bratseth <bratseth@yahoo-inc.com>
2016-09-29 11:34:11 +0200

    Remove unnecessary dependency

--- document/src/test/java/com/yahoo/document/json/JsonTestHelper.java
+++ document/src/test/java/com/yahoo/document/json/JsonTestHelper.java
@@ -1,9 +1,10 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.document.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Joiner;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
+import static org.junit.Assert.assertEquals;
+import java.io.IOException;
 
 /**
  * @author Vegard Sjonfjell
@@ -23,7 +24,13 @@ public class JsonTestHelper {
      * Structurally compare two JSON encoded strings
      */
     public static void assertJsonEquals(String inputJson, String expectedJson) {
-        assertThat(inputJson, sameJSONAs(expectedJson));
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            assertEquals(mapper.readTree(inputJson), mapper.readTree(expectedJson));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
