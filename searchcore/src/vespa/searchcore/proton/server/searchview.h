392e79817f6bf2e36779e43b70e561811a5fd6bb
Henning Baldersheim <balder@yahoo-inc.com>
2016-11-01 17:04:24 +0000

    Refactor the summary fetching out so that it can be easily retried if documents have moved lid.

--- searchcore/src/vespa/searchcore/proton/server/searchview.h
+++ searchcore/src/vespa/searchcore/proton/server/searchview.h
@@ -14,49 +14,32 @@ private:
     ISummaryManager::ISummarySetup::SP _summarySetup;
     MatchView::SP                      _matchView;
 
+    DocsumReply::UP getDocsumsInternal(const DocsumRequest & req, const search::IDocumentMetaStore & metaStore);
+    using IndexSearchable = searchcorespi::IndexSearchable;
 public:
     typedef std::shared_ptr<SearchView> SP;
 
-    SearchView(const ISummaryManager::ISummarySetup::SP &summarySetup,
-               const MatchView::SP &matchView);
+    SearchView(const ISummaryManager::ISummarySetup::SP &summarySetup, const MatchView::SP &matchView);
 
-    const ISummaryManager::ISummarySetup::SP & getSummarySetup() const
-    { return _summarySetup; }
-
-    const MatchView::SP &getMatchView() const { return _matchView; }
-
-    const Matchers::SP &getMatchers() const
-    { return _matchView->getMatchers(); }
-
-    const searchcorespi::IndexSearchable::SP &
-    getIndexSearchable() const { return _matchView->getIndexSearchable(); }
-
-    const IAttributeManager::SP &getAttributeManager() const
-    { return _matchView->getAttributeManager(); }
-
-    const matching::SessionManager::SP &
-    getSessionManager() const { return _matchView->getSessionManager(); }
-
-    const IDocumentMetaStoreContext::SP &
-    getDocumentMetaStore() const { return _matchView->getDocumentMetaStore(); }
-
+    const ISummaryManager::ISummarySetup::SP & getSummarySetup() const { return _summarySetup; }
+    const MatchView::SP & getMatchView() const { return _matchView; }
+    const Matchers::SP  & getMatchers()  const { return _matchView->getMatchers(); }
+    const IndexSearchable::SP   & getIndexSearchable()  const { return _matchView->getIndexSearchable(); }
+    const IAttributeManager::SP & getAttributeManager() const { return _matchView->getAttributeManager(); }
+    const matching::SessionManager::SP  & getSessionManager()    const { return _matchView->getSessionManager(); }
+    const IDocumentMetaStoreContext::SP & getDocumentMetaStore() const { return _matchView->getDocumentMetaStore(); }
     DocIdLimit &getDocIdLimit() const { return _matchView->getDocIdLimit(); }
 
-    matching::MatchingStats
-    getMatcherStats(const vespalib::string &rankProfile) const
-    { return _matchView->getMatcherStats(rankProfile); }
-
+    matching::MatchingStats getMatcherStats(const vespalib::string &rankProfile) const { return _matchView->getMatcherStats(rankProfile); }
 
     /**
      * Implements ISearchHandler
      */
-    virtual search::engine::DocsumReply::UP
-    getDocsums(const search::engine::DocsumRequest & req);
+    DocsumReply::UP getDocsums(const DocsumRequest & req) override;
 
-    virtual search::engine::SearchReply::UP match(
-            const ISearchHandler::SP &self,
-            const search::engine::SearchRequest &req,
-            vespalib::ThreadBundle &threadBundle) const;
+    SearchReply::UP match(const ISearchHandler::SP &self,
+                          const SearchRequest &req,
+                          vespalib::ThreadBundle &threadBundle) const override;
 };
 
 } // namespace proton
