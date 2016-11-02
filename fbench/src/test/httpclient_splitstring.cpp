1d2309db271d8c2668fbd9c91b5464c0671886ba
Arnstein Ressem <aressem@yahoo-inc.com>
2016-11-02 11:27:21 +0000

    Only name source once. Fix compile errors.

--- fbench/src/test/httpclient_splitstring.cpp
+++ fbench/src/test/httpclient_splitstring.cpp
@@ -1,6 +1,6 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <util/httpclient.h>
+#include <httpclient/httpclient.h>
 
 class DebugHTTPClient : public HTTPClient
 {
