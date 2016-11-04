6aecffc6e96b3141a4051ee6b8ec4b90fa7ce6c6
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2016-11-04 11:29:49 +0100

    Revert "Call AsyncContext.complete() when response is completed"
    This reverts commit c585b50ed6ae14dd4f551547c9ff0179be70d316.

--- jdisc_http_service/src/main/java/com/yahoo/jdisc/http/server/jetty/HttpRequestDispatch.java
+++ jdisc_http_service/src/main/java/com/yahoo/jdisc/http/server/jetty/HttpRequestDispatch.java
@@ -89,7 +89,7 @@ class HttpRequestDispatch {
             onError(servletResponseController.finishedFuture(),
                     servletRequestReader::onError);
 
-            servletResponseController.finishedFuture()
+            CompletableFuture.allOf(servletRequestReader.finishedFuture, servletResponseController.finishedFuture())
                     .whenComplete(completeRequestCallback);
         } catch (Throwable throwable) {
             log.log(Level.WARNING, "Failed registering finished listeners.", throwable);
