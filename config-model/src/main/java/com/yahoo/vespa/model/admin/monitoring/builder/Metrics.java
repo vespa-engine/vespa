b5077c48c523e3ba938b04b8a66c1b63b4340a78
gjoranv <gv@yahoo-inc.com>
2016-11-06 23:33:26 +0100

    Add builder for new 'metrics' element in services.xml

---
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/builder/Metrics.java
@@ -0,0 +1,27 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.model.admin.monitoring.builder;
+
+import com.yahoo.vespa.model.admin.monitoring.MetricsConsumer;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * A mutable helper class representing the user defined metrics and consumers.
+ *
+ * @author gjoranv
+ */
+public class Metrics {
+
+    private final Map<String, MetricsConsumer> consumers = new LinkedHashMap<>();
+
+    public void addConsumer(MetricsConsumer consumer) {
+        consumers.put(consumer.getId(), consumer);
+    }
+
+    public  Map<String, MetricsConsumer> getConsumers() {
+        return Collections.unmodifiableMap(consumers);
+    }
+
+}
