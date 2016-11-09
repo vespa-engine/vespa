11da638b4961c93ae83aa35654b50b886fa9c5a1
gjoranv <gv@yahoo-inc.com>
2016-11-09 15:07:43 +0100

    There is only one default metrics consumer.

--- config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultMetricsConsumer.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultMetricsConsumer.java
@@ -1,9 +1,6 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.admin.monitoring;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * This class sets up the default 'vespa' metrics consumer.
  *
@@ -16,17 +13,9 @@ public class DefaultMetricsConsumer {
 
     private static final MetricSet vespaMetricSet = new VespaMetricSet();
 
-    /**
-     * Populates a map of with consumer as key and metrics for that consumer as value. The metrics
-     * are to be forwarded to consumers.
-     *
-     * @return A map of default metric consumers and default metrics for that consumer.
-     */
     @SuppressWarnings("UnusedDeclaration")
-    public static Map<String, MetricsConsumer> getDefaultMetricsConsumers() {
-        Map<String, MetricsConsumer> metricsConsumers = new LinkedHashMap<>();
-        metricsConsumers.put(VESPA_CONSUMER_ID, new MetricsConsumer(VESPA_CONSUMER_ID, vespaMetricSet));
-        return metricsConsumers;
+    public static MetricsConsumer getDefaultMetricsConsumer() {
+        return new MetricsConsumer(VESPA_CONSUMER_ID, vespaMetricSet);
     }
 
 }
