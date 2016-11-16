2a58636f064125d653ab0182ad1a8e68f6c57ce5
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2016-11-16 13:49:33 +0100

    Replace Scala case classes with Java POJOs

---
+++ application-model/src/main/java/com/yahoo/vespa/applicationmodel/ApplicationInstance.java
@@ -0,0 +1,65 @@
+package com.yahoo.vespa.applicationmodel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author bjorncs
+ */
+public class ApplicationInstance<STATUS> {
+    private final TenantId tenantId;
+    private final ApplicationInstanceId applicationInstanceId;
+    private final Set<ServiceCluster<STATUS>> serviceClusters;
+
+    public ApplicationInstance(TenantId tenantId, ApplicationInstanceId applicationInstanceId, Set<ServiceCluster<STATUS>> serviceClusters) {
+        this.tenantId = tenantId;
+        this.applicationInstanceId = applicationInstanceId;
+        this.serviceClusters = serviceClusters;
+    }
+
+    @JsonProperty("tenantId")
+    public TenantId tenantId() {
+        return tenantId;
+    }
+
+    @JsonProperty("applicationInstanceId")
+    public ApplicationInstanceId applicationInstanceId() {
+        return applicationInstanceId;
+    }
+
+    @JsonProperty("serviceClusters")
+    public Set<ServiceCluster<STATUS>> serviceClusters() {
+        return serviceClusters;
+    }
+
+    @JsonProperty("reference")
+    public ApplicationInstanceReference reference() {
+        return new ApplicationInstanceReference(tenantId, applicationInstanceId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApplicationInstance<?> that = (ApplicationInstance<?>) o;
+        return Objects.equals(tenantId, that.tenantId) &&
+                Objects.equals(applicationInstanceId, that.applicationInstanceId) &&
+                Objects.equals(serviceClusters, that.serviceClusters);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tenantId, applicationInstanceId, serviceClusters);
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationInstance{" +
+                "tenantId=" + tenantId +
+                ", applicationInstanceId=" + applicationInstanceId +
+                ", serviceClusters=" + serviceClusters +
+                '}';
+    }
+}
---
+++ application-model/src/main/java/com/yahoo/vespa/applicationmodel/ServiceCluster.java
@@ -0,0 +1,62 @@
+package com.yahoo.vespa.applicationmodel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Represents a collection of service instances that together make up a service with a single cluster id.
+ *
+ * @author bjorncs
+ */
+public class ServiceCluster<STATUS> {
+    private final ClusterId clusterId;
+    private final ServiceType serviceType;
+    private final Set<ServiceInstance<STATUS>> serviceInstances;
+
+    public ServiceCluster(ClusterId clusterId, ServiceType serviceType, Set<ServiceInstance<STATUS>> serviceInstances) {
+        this.clusterId = clusterId;
+        this.serviceType = serviceType;
+        this.serviceInstances = serviceInstances;
+    }
+
+    @JsonProperty("clusterId")
+    public ClusterId clusterId() {
+        return clusterId;
+    }
+
+    @JsonProperty("serviceType")
+    public ServiceType serviceType() {
+        return serviceType;
+    }
+
+    @JsonProperty("serviceInstances")
+    public Set<ServiceInstance<STATUS>> serviceInstances() {
+        return serviceInstances;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceCluster{" +
+                "clusterId=" + clusterId +
+                ", serviceType=" + serviceType +
+                ", serviceInstances=" + serviceInstances +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceCluster<?> that = (ServiceCluster<?>) o;
+        return Objects.equals(clusterId, that.clusterId) &&
+                Objects.equals(serviceType, that.serviceType) &&
+                Objects.equals(serviceInstances, that.serviceInstances);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clusterId, serviceType, serviceInstances);
+    }
+}
