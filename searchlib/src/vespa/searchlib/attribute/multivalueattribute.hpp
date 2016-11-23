e61905d66fd11efdd71fee1c24d84972d2073adb
Tor Egge <Tor.Egge@yahoo-inc.com>
2016-11-23 17:20:39 +0000

    Increase parameter for small arrays in multi value attribute from 14 to 1023.

--- searchlib/src/vespa/searchlib/attribute/multivalueattribute.hpp
+++ searchlib/src/vespa/searchlib/attribute/multivalueattribute.hpp
@@ -11,7 +11,7 @@ MultiValueAttribute<B, M>::
 MultiValueAttribute(const vespalib::string &baseFileName,
                     const AttributeVector::Config &cfg)
     : B(baseFileName, cfg),
-      _mvMapping(14, cfg.getGrowStrategy())
+      _mvMapping(1023, cfg.getGrowStrategy())
 {
 }
 
