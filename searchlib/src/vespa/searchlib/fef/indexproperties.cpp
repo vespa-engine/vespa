dd98862b1e13654bb4005b88c2d748e5fc47b3c7
Henning Baldersheim <balder@yahoo-inc.com>
2016-11-25 00:57:31 +0100

    Allow for control of soft timeout.

--- searchlib/src/vespa/searchlib/fef/indexproperties.cpp
+++ searchlib/src/vespa/searchlib/fef/indexproperties.cpp
@@ -182,11 +182,25 @@ bool Enabled::lookup(const Properties &props) {
     return lookupBool(props, NAME, DEFAULT_VALUE);
 }
 
+bool Enabled::lookup(const Properties &props, bool defaultValue) {
+    return lookupBool(props, NAME, defaultValue);
+}
+
 const vespalib::string TailCost::NAME("vespa.softtimeout.tailcost");
-const double TailCost::DEFAULT_VALUE(false);
+const double TailCost::DEFAULT_VALUE(0.1);
 
 double TailCost::lookup(const Properties &props) {
     return lookupDouble(props, NAME, DEFAULT_VALUE);
+}
+
+const vespalib::string Factor::NAME("vespa.softtimeout.factor");
+const double Factor::DEFAULT_VALUE(0.5);
+
+double Factor::lookup(const Properties &props) {
+    return lookupDouble(props, NAME, DEFAULT_VALUE);
+}
+double Factor::lookup(const Properties &props, double defaultValue) {
+    return lookupDouble(props, NAME, defaultValue);
 }
 
 }
