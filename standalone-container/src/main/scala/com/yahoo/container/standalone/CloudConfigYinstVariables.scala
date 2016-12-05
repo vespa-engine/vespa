d6050d78ac5bca35b1f857bd3fd34fc0a46858cf
Harald Musum <musum@yahoo-inc.com>
2016-12-05 13:07:17 +0100

    Allow configuring docker registry and Docker Vespa base image

--- standalone-container/src/main/scala/com/yahoo/container/standalone/CloudConfigYinstVariables.scala
+++ standalone-container/src/main/scala/com/yahoo/container/standalone/CloudConfigYinstVariables.scala
@@ -36,6 +36,8 @@ class CloudConfigYinstVariables extends CloudConfigOptions {
   override val useVespaVersionInRequest = optionalYinstVar[java.lang.Boolean]("use_vespa_version_in_request")
   override val hostedVespa = optionalYinstVar[java.lang.Boolean]("hosted_vespa")
   override val numParallelTenantLoaders = optionalYinstVar[java.lang.Integer]("num_parallel_tenant_loaders")
+  override val dockerRegistry = optionalYinstVar[java.lang.String]("docker_registry")
+  override val dockerVespaBaseImage = optionalYinstVar[java.lang.String]("docker_vespa_base_image")
 }
 
 object CloudConfigYinstVariables {
