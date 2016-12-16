11b57c31a6031c4f71f8e650879d0b0b9eb5a6e4
Henning Baldersheim <balder@yahoo-inc.com>
2016-12-16 11:49:17 +0100

    Put map construction in .cpp file

--- document/src/vespa/document/select/context.cpp
+++ document/src/vespa/document/select/context.cpp
@@ -1,10 +1,37 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/fastos/fastos.h>
+
 #include "context.h"
-#include "value.h"
 
 namespace document {
 namespace select {
+
+Context::Context(void)
+    : _doc(NULL),
+      _docId(NULL),
+      _docUpdate(NULL),
+      _variables()
+{ }
+
+Context::Context(const Document& doc)
+    : _doc(&doc),
+      _docId(NULL),
+      _docUpdate(NULL),
+      _variables()
+{ }
+
+Context::Context(const DocumentId& docId)
+    : _doc(NULL),
+      _docId(&docId),
+      _docUpdate(NULL),
+      _variables()
+{ }
+
+Context::Context(const DocumentUpdate& docUpdate)
+    : _doc(NULL),
+      _docId(NULL),
+      _docUpdate(&docUpdate),
+      _variables()
+{ }
 
 Context::~Context() { }
 
