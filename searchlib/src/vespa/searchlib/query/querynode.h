a8ba5da57e5a0a54050376b91233691041c2d0ad
Henning Baldersheim <balder@yahoo-inc.com>
2016-12-21 12:03:21 +0000

    Only include what you need

--- searchlib/src/vespa/searchlib/query/querynode.h
+++ searchlib/src/vespa/searchlib/query/querynode.h
@@ -1,18 +1,17 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
 
-#include <vespa/searchlib/parsequery/stackdumpiterator.h>
-#include <vespa/searchlib/query/base.h>
-#include <vespa/searchlib/query/querynoderesultbase.h>
+#include "posocc.h"
 #include <vespa/vespalib/util/linkedptr.h>
 #include <vespa/searchlib/common/identifiable.h>
-#include "posocc.h"
 
-namespace search
-{
+namespace search {
 
+class SimpleQueryStackDumpIterator;
 class QueryTerm;
 class QueryNode;
+class QueryNodeResultBase;
+
 /// Typedef a simple list that contains references to QueryNodes.
 typedef std::vector<QueryNode *> QueryNodeRefList;
 /// Typedef a simple list that contains const references to QueryNodes.
@@ -56,7 +55,7 @@ class QueryNode : public vespalib::Identifiable
   virtual size_t depth() const { return 1; }
   /// Return the width of this tree.
   virtual size_t width() const { return 1; }
-  static UP Build(const QueryNode * parent, const QueryNodeResultBase & org, search::SimpleQueryStackDumpIterator & queryRep, bool allowRewrite);
+  static UP Build(const QueryNode * parent, const QueryNodeResultBase & org, SimpleQueryStackDumpIterator & queryRep, bool allowRewrite);
 };
 
 /// A list conating the QuerNode objects. With copy/assignment.
