5542b94c413694c82002229b2446dda54a686e1d
Henning Baldersheim <balder@yahoo-inc.com>
2016-12-22 09:29:42 +0000

    Include correct set of files.

--- vespamalloc/src/vespamalloc/malloc/threadproxy.cpp
+++ vespamalloc/src/vespamalloc/malloc/threadproxy.cpp
@@ -1,6 +1,9 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #include "threadproxy.h"
 #include <dlfcn.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
 
 namespace vespamalloc {
 
