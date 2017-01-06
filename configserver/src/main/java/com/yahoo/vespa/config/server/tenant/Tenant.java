7e136f00aefc5cbce014ed9b45e39e0ea9fde68c
Harald Musum <musum@yahoo-inc.com>
2017-01-06 14:24:40 +0100

    Add missing copyright header

--- configserver/src/main/java/com/yahoo/vespa/config/server/tenant/Tenant.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/tenant/Tenant.java
@@ -1,3 +1,4 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.config.server.tenant;
 
 import com.yahoo.config.provision.ApplicationId;
@@ -164,8 +165,7 @@ public class Tenant implements TenantHandlerProvider {
 
     public void redeployApplications(Deployer deployer) {
         // TODO: Configurable timeout
-        applicationRepo.listApplications().stream()
-                .forEach(applicationId -> redeployApplication(applicationId, deployer));
+        applicationRepo.listApplications().forEach(applicationId -> redeployApplication(applicationId, deployer));
     }
 
     private void redeployApplication(ApplicationId applicationId, Deployer deployer) {
