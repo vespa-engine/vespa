70eb84c84a1da2ed166119686972b5714c0b1975
Henning Baldersheim <balder@yahoo-inc.com>
2017-01-07 00:07:02 +0100

    Remove unused code.

--- vespamalloc/src/vespamalloc/util/index.h
+++ vespamalloc/src/vespamalloc/util/index.h
@@ -1,8 +1,7 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
 
-#include <atomic>
-#include <stdio.h>
+#include <cstddef>
 
 namespace vespamalloc {
 
@@ -18,20 +17,6 @@ public:
     index_t operator -= (index_t v) { return _index -= v; }
 private:
     index_t _index;
-};
-
-class AtomicIndex
-{
-public:
-    typedef size_t index_t;
-    AtomicIndex(index_t index = 0) : _index(index) { }
-    operator index_t ()       const { return _index; }
-    index_t operator ++ (int)       { return _index.fetch_add(1); }
-    index_t operator -- (int)       { return _index.fetch_sub(1); }
-    index_t operator += (index_t v) { return _index.fetch_add(v); }
-    index_t operator -= (index_t v) { return _index.fetch_sub(v); }
-private:
-    std::atomic<index_t> _index;
 };
 
 }
