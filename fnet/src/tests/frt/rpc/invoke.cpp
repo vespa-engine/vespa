5a8f6744eb3233c5fa3e243e060d2804f22bce84
Henning Baldersheim <balder@yahoo-inc.com>
2017-01-13 00:06:41 +0100

    Remove unused include

--- fnet/src/tests/frt/rpc/invoke.cpp
+++ fnet/src/tests/frt/rpc/invoke.cpp
@@ -1,7 +1,6 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #include <vespa/vespalib/testkit/test_kit.h>
 #include <vespa/fnet/frt/frt.h>
-#include <vespa/fnet/fnet.h>
 
 //-------------------------------------------------------------
 
