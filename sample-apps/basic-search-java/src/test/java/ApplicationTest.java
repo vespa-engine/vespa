8ec23b82b9ce70ce60c8fac059ce671f73a2f413
Kristian Aune <kraune@yahoo-inc.com>
2017-01-13 08:48:02 +0100

    Add basic-search-java
    - needs to be separate from basic-search (removed java code there)
    - does not work yet, testing fails - I am not sure whether to rewrite of fix yet
    - add now so I can fix documentation at least - fix code later

---
+++ sample-apps/basic-search-java/src/test/java/ApplicationTest.java
@@ -0,0 +1,29 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+//package ${package};
+
+import com.yahoo.application.Application;
+import com.yahoo.application.Networking;
+import com.yahoo.application.container.Processing;
+import com.yahoo.component.ComponentSpecification;
+import com.yahoo.processing.Request;
+import com.yahoo.processing.Response;
+import org.junit.Test;
+
+import java.nio.file.FileSystems;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+public class ApplicationTest {
+
+    @Test
+    public void requireThatResultContainsHelloWorld() throws Exception {
+        try (Application app = Application.fromApplicationPackage(
+                FileSystems.getDefault().getPath("src/main/application"),
+                Networking.disable)) {
+            Processing processing = app.getJDisc("jdisc").processing();
+            Response response = processing.process(ComponentSpecification.fromString("default"), new Request());
+            assertThat(response.data().get(0).toString(), containsString("Hello, services!"));
+        }
+    }
+}
