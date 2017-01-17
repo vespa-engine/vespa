587eb2de3c1139dd681b444e4ae3df03ad4a9b34
Jon Bratseth <bratseth@yahoo-inc.com>
2017-01-17 14:43:50 +0100

    Relax summary field tensor type compatibility check

--- config-model/src/main/java/com/yahoo/searchdefinition/processing/IndexingValidation.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/processing/IndexingValidation.java
@@ -145,4 +145,5 @@ public class IndexingValidation extends Processor {
             }
         }
     }
+
 }
--- config-model/src/main/java/com/yahoo/searchdefinition/processing/SummaryConsistency.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/processing/SummaryConsistency.java
@@ -2,6 +2,8 @@
 package com.yahoo.searchdefinition.processing;
 
 import com.yahoo.config.application.api.DeployLogger;
+import com.yahoo.document.DataType;
+import com.yahoo.document.TensorDataType;
 import com.yahoo.searchdefinition.RankProfileRegistry;
 import com.yahoo.searchdefinition.document.Attribute;
 import com.yahoo.document.WeightedSetDataType;
@@ -28,46 +30,54 @@ public class SummaryConsistency extends Processor {
         for (DocumentSummary summary : search.getSummaries().values()) {
             if (summary.getName().equals("default")) continue;
             for (SummaryField summaryField : summary.getSummaryFields() ) {
-                assertConsistency(summaryField,search);
-                makeAttributeTransformIfAppropriate(summaryField,search);
+                assertConsistency(summaryField, search);
+                makeAttributeTransformIfAppropriate(summaryField, search);
             }
         }
     }
 
     /** If the source is an attribute, make this use the attribute transform */
     private void makeAttributeTransformIfAppropriate(SummaryField summaryField,Search search) {
-        if (summaryField.getTransform()!=SummaryTransform.NONE) return;
-        Attribute attribute=search.getAttribute(summaryField.getSingleSource());
-        if (attribute==null) return;
+        if (summaryField.getTransform() != SummaryTransform.NONE) return;
+        Attribute attribute = search.getAttribute(summaryField.getSingleSource());
+        if (attribute == null) return;
         summaryField.setTransform(SummaryTransform.ATTRIBUTE);
     }
 
-    private void assertConsistency(SummaryField summaryField,Search search) {
-        SummaryField existingDefault=search.getSummary("default").getSummaryField(summaryField.getName()); // Compare to default
-        if (existingDefault!=null) {
-            assertConsistentTypes(existingDefault,summaryField);
-            makeConsistentWithDefaultOrThrow(existingDefault,summaryField);
+    private void assertConsistency(SummaryField summaryField, Search search) {
+        SummaryField existingDefault = search.getSummary("default").getSummaryField(summaryField.getName()); // Compare to default
+        if (existingDefault != null) {
+            assertConsistentTypes(existingDefault, summaryField);
+            makeConsistentWithDefaultOrThrow(existingDefault, summaryField);
         }
         else {
             // If no default, compare to whichever definition of the field
-            SummaryField existing=search.getExplicitSummaryField(summaryField.getName());
-            if (existing==null) return;
-            assertConsistentTypes(existing,summaryField);
-            makeConsistentOrThrow(existing,summaryField,search);
+            SummaryField existing = search.getExplicitSummaryField(summaryField.getName());
+            if (existing == null) return;
+            assertConsistentTypes(existing, summaryField);
+            makeConsistentOrThrow(existing, summaryField, search);
         }
     }
 
-    private void assertConsistentTypes(SummaryField field1,SummaryField field2) {
-        if (field1.getDataType() instanceof WeightedSetDataType && field2.getDataType() instanceof WeightedSetDataType &&
-            ((WeightedSetDataType)field1.getDataType()).getNestedType().equals(((WeightedSetDataType)field2.getDataType()).getNestedType()))
+    private void assertConsistentTypes(SummaryField existing, SummaryField seen) {
+        if (existing.getDataType() instanceof WeightedSetDataType && seen.getDataType() instanceof WeightedSetDataType &&
+            ((WeightedSetDataType)existing.getDataType()).getNestedType().equals(((WeightedSetDataType)seen.getDataType()).getNestedType()))
             return; // Disregard create-if-nonexistent and create-if-zero distinction
-        if ( ! field1.getDataType().equals(field2.getDataType()))
-            throw new IllegalArgumentException(field1.toLocateString() + " is inconsistent with " + field2.toLocateString() + ": All declarations of the same summary field must have the same type");
+        if ( ! compatibleTypes(seen.getDataType(), existing.getDataType()))
+            throw new IllegalArgumentException(existing.toLocateString() + " is inconsistent with " + 
+                                               seen.toLocateString() + ": All declarations of the same summary field must have the same type");
     }
 
-    private void makeConsistentOrThrow(SummaryField field1, SummaryField field2,Search search) {
-        if (field2.getTransform()==SummaryTransform.ATTRIBUTE && field1.getTransform()==SummaryTransform.NONE) {
-            Attribute attribute=search.getAttribute(field1.getName());
+    private boolean compatibleTypes(DataType summaryType, DataType existingType) {
+        if (summaryType instanceof TensorDataType && existingType instanceof TensorDataType) {
+            return summaryType.isAssignableFrom(existingType); // TODO: Just do this for all types
+        }
+        return summaryType.equals(existingType);
+    }
+
+    private void makeConsistentOrThrow(SummaryField field1, SummaryField field2, Search search) {
+        if (field2.getTransform() == SummaryTransform.ATTRIBUTE && field1.getTransform() == SummaryTransform.NONE) {
+            Attribute attribute = search.getAttribute(field1.getName());
             if (attribute != null) {
                 field1.setTransform(SummaryTransform.ATTRIBUTE);
             }
@@ -80,7 +90,7 @@ public class SummaryConsistency extends Processor {
             assertEqualTransform(field1,field2);
         }
     }
-    private void makeConsistentWithDefaultOrThrow(SummaryField defaultField,SummaryField newField) {
+    private void makeConsistentWithDefaultOrThrow(SummaryField defaultField, SummaryField newField) {
         if (newField.getTransform().equals(SummaryTransform.NONE)) {
             newField.setTransform(defaultField.getTransform());
         }
@@ -90,8 +100,8 @@ public class SummaryConsistency extends Processor {
     }
 
 
-    private void assertEqualTransform(SummaryField field1,SummaryField field2) {
-        if (!field2.getTransform().equals(field1.getTransform())) {
+    private void assertEqualTransform(SummaryField field1, SummaryField field2) {
+        if ( ! field2.getTransform().equals(field1.getTransform())) {
             throw new IllegalArgumentException("Conflicting summary transforms. " + field2 +" is already defined as " +
                                        field1 + ". A field with the same name " +
                                        "can not have different transforms in different summary classes");
