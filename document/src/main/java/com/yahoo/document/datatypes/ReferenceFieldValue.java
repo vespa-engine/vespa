f84ca997422575d130c44f14ba763a9b93ab58b0
Tor Brede Vekterli <vekterli@yahoo-inc.com>
2017-01-23 14:19:02 +0100

    Update style on autogenerated equals code

--- document/src/main/java/com/yahoo/document/datatypes/ReferenceFieldValue.java
+++ document/src/main/java/com/yahoo/document/datatypes/ReferenceFieldValue.java
@@ -79,9 +79,15 @@ public class ReferenceFieldValue extends FieldValue {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
         ReferenceFieldValue that = (ReferenceFieldValue) o;
         return Objects.equals(referenceType, that.referenceType) &&
                 Objects.equals(documentId, that.documentId);
