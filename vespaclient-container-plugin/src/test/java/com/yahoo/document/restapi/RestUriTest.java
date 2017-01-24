5b01e13c4654946496c6f0bb2b4834e47d2b18d1
Henning Baldersheim <balder@yahoo-inc.com>
2017-01-24 13:14:26 +0100

    Finger trouble

--- vespaclient-container-plugin/src/test/java/com/yahoo/document/restapi/RestUriTest.java
+++ vespaclient-container-plugin/src/test/java/com/yahoo/document/restapi/RestUriTest.java
@@ -111,7 +111,7 @@ public class RestUriTest {
         RestUri restUri = new RestUri(new URI("/document/v1/test/newsarticle/docid/http://vn.news.yahoo.com/gi-th-ng-t-n-ng-khoa-h-205000458.html"));
         assertThat(restUri.getNamespace(), is("test"));
         assertThat(restUri.getDocumentType(), is("newsarticle"));
-        assertThat(restUri.getDocId(), is("http://vn.news.yahoo.com//gi-th-ng-t-n-ng-khoa-h-205000458.html"));
+        assertThat(restUri.getDocId(), is("http://vn.news.yahoo.com/gi-th-ng-t-n-ng-khoa-h-205000458.html"));
         assertThat(restUri.generateFullId(), is("id:test:newsarticle::http://vn.news.yahoo.com/gi-th-ng-t-n-ng-khoa-h-205000458.html"));
     }
 
