177359fcdbda79284db43da24d7491a9f8b51b43
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-01-25 13:49:57 +0100

    Change DocumentReference to use Field instead of SDField

--- config-model/src/main/java/com/yahoo/searchdefinition/DocumentReference.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/DocumentReference.java
@@ -1,7 +1,7 @@
 // Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.searchdefinition;
 
-import com.yahoo.searchdefinition.document.SDField;
+import com.yahoo.document.Field;
 
 /**
  * Represents a document reference. Contains the document reference field and the search instance of the referred document.
@@ -10,15 +10,15 @@ import com.yahoo.searchdefinition.document.SDField;
  */
 public class DocumentReference {
 
-    private final SDField documentReferenceField;
+    private final Field documentReferenceField;
     private final Search search;
 
-    public DocumentReference(SDField documentReferenceField, Search search) {
+    public DocumentReference(Field documentReferenceField, Search search) {
         this.documentReferenceField = documentReferenceField;
         this.search = search;
     }
 
-    public SDField documentReferenceField() {
+    public Field documentReferenceField() {
         return documentReferenceField;
     }
 
