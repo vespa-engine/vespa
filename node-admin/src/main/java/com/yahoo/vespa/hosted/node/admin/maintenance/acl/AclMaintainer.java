eb1946a76ac9b5cea2590adf8a717e47e4d71b76
Martin Polden <martin.polden@gmail.com>
2017-01-25 16:06:03 +0100

    Only apply ACLs if they change

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/acl/AclMaintainer.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/acl/AclMaintainer.java
@@ -6,9 +6,15 @@ import com.yahoo.vespa.hosted.dockerapi.Container;
 import com.yahoo.vespa.hosted.dockerapi.ContainerName;
 import com.yahoo.vespa.hosted.node.admin.ContainerAclSpec;
 import com.yahoo.vespa.hosted.node.admin.docker.DockerOperations;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.Action;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.Chain;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.FilterCommand;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.FlushCommand;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.ListCommand;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.PolicyCommand;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.Command;
+import com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables.CommandList;
 import com.yahoo.vespa.hosted.node.admin.noderepository.NodeRepository;
-import com.yahoo.vespa.hosted.node.admin.util.IpTables;
-import com.yahoo.vespa.hosted.node.admin.util.IpTables.Action;
 import com.yahoo.vespa.hosted.node.admin.util.PrefixLogger;
 
 import java.net.Inet6Address;
@@ -23,7 +29,7 @@ import java.util.stream.Collectors;
  * repository. Based on those ACLs, iptables commands are created and then executed in each of the containers network
  * namespace.
  *
- * If a rule cannot be configured (e.g. iptables process execution fails), a rollback is attempted by setting the
+ * If an ACL cannot be configured (e.g. iptables process execution fails), a rollback is attempted by setting the
  * default policy to ACCEPT which will allow any traffic. The configuration will be retried the next time the
  * maintainer runs.
  *
@@ -35,6 +41,8 @@ import java.util.stream.Collectors;
 public class AclMaintainer implements Runnable {
 
     private static final PrefixLogger log = PrefixLogger.getNodeAdminLogger(AclMaintainer.class);
+
+    private static final String IPTABLES_COMMAND = "ip6tables";
 
     private final DockerOperations dockerOperations;
     private final NodeRepository nodeRepository;
@@ -51,22 +59,27 @@ public class AclMaintainer implements Runnable {
         this.nodeAdminHostnameSupplier = nodeAdminHostnameSupplier;
     }
 
+    private boolean shouldApplyAcl(ContainerName containerName, CommandList commandList) {
+        final String currentRules = dockerOperations.executeCommandInNetworkNamespace(containerName,
+                new ListCommand().asArray(IPTABLES_COMMAND));
+        return !commandList.asString().equals(currentRules.trim());
+    }
+
     private void applyAcl(ContainerName containerName, List<ContainerAclSpec> aclSpecs) {
+        final CommandList commandList = commandListFrom(aclSpecs);
+        final Command flush = new FlushCommand(Chain.INPUT);
+        final Command rollback = new PolicyCommand(Chain.INPUT, Action.ACCEPT);
+        if (!shouldApplyAcl(containerName, commandList)) {
+            return;
+        }
         try {
-            dockerOperations.executeCommandInNetworkNamespace(containerName, IpTables.flushChain());
-            dockerOperations.executeCommandInNetworkNamespace(containerName, IpTables.allowAssociatedConnections());
-            // ICMPv6 packets are always accepted as they are required for PMTU discovery to work properly.
-            dockerOperations.executeCommandInNetworkNamespace(containerName, IpTables.allowIcmp());
-            aclSpecs.stream()
-                    .map(ContainerAclSpec::ipAddress)
-                    .filter(AclMaintainer::isIpv6)
-                    .forEach(ipAddress -> dockerOperations.executeCommandInNetworkNamespace(containerName,
-                            IpTables.allowFromAddress(ipAddress)));
-            dockerOperations.executeCommandInNetworkNamespace(containerName, IpTables.chainPolicy(Action.DROP));
+            dockerOperations.executeCommandInNetworkNamespace(containerName, flush.asArray(IPTABLES_COMMAND));
+            commandList.commands().forEach(command -> dockerOperations.executeCommandInNetworkNamespace(containerName,
+                    command.asArray(IPTABLES_COMMAND)));
         } catch (Exception e) {
             log.error("Exception occurred while configuring ACLs, attempting rollback", e);
             try {
-                dockerOperations.executeCommandInNetworkNamespace(containerName, IpTables.chainPolicy(Action.ACCEPT));
+                dockerOperations.executeCommandInNetworkNamespace(containerName, rollback.asArray(IPTABLES_COMMAND));
             } catch (Exception ne) {
                 log.error("Rollback failed, giving up", ne);
             }
@@ -100,6 +113,25 @@ public class AclMaintainer implements Runnable {
         } catch (Throwable t) {
             log.error("Failed to configure ACLs", t);
         }
+    }
+
+    private static CommandList commandListFrom(List<ContainerAclSpec> aclSpecs) {
+        final List<Command> allowedIps = aclSpecs.stream()
+                .map(ContainerAclSpec::ipAddress)
+                .filter(AclMaintainer::isIpv6)
+                .map(ipAddress -> new FilterCommand(Chain.INPUT, Action.ACCEPT)
+                        .withOption("-s", String.format("%s/128", ipAddress)))
+                .collect(Collectors.toList());
+        return new CommandList()
+                .add(new PolicyCommand(Chain.INPUT, Action.DROP))
+                .add(new PolicyCommand(Chain.FORWARD, Action.ACCEPT))
+                .add(new PolicyCommand(Chain.OUTPUT, Action.ACCEPT))
+                .add(new FilterCommand(Chain.INPUT, Action.ACCEPT)
+                        .withOption("-m", "state")
+                        .withOption("--state", "RELATED,ESTABLISHED"))
+                .add(new FilterCommand(Chain.INPUT, Action.ACCEPT)
+                        .withOption("-p", "ipv6-icmp"))
+                .addAll(allowedIps);
     }
 
     private static boolean isIpv6(String ipAddress) {
