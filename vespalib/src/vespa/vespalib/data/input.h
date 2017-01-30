b5d0dd417e1b47d493410eca3d0db47a32a8d19c
Haavard <havardpe@yahoo-inc.com>
2017-01-30 15:16:17 +0000

    low-level data flow building blocks

---
+++ vespalib/src/vespa/vespalib/data/input.h
@@ -0,0 +1,35 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+#include "memory.h"
+
+namespace vespalib {
+
+/**
+ * Interface used to abstract a source of input data. Note that the
+ * input data itself is owned by the object implementing this
+ * interface.
+ **/
+struct Input
+{
+    /**
+     * Obtain more input data.
+     *
+     * @return the obtained input data
+     **/
+    virtual Memory obtain() = 0;
+
+    /**
+     * Evict processed input data. Never evict more data than you have
+     * obtained.
+     *
+     * @return this object, for chaining
+     * @param bytes the number of bytes to evict
+     **/
+    virtual Input &evict(size_t bytes) = 0;
+
+    virtual ~Input();
+};
+
+} // namespace vespalib
