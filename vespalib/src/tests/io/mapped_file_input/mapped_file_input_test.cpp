b233017523112c4c0f7fac80ea8799ec55aa49c9
Haavard <havardpe@yahoo-inc.com>
2017-02-06 14:15:48 +0000

    mapped file input in vespalib

---
+++ vespalib/src/tests/io/mapped_file_input/mapped_file_input_test.cpp
@@ -0,0 +1,23 @@
+// Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/vespalib/io/mapped_file_input.h>
+
+using namespace vespalib;
+
+TEST("require that missing file is invalid") {
+    MappedFileInput file(TEST_PATH("not_found.txt"));
+    EXPECT_TRUE(!file.valid());
+}
+
+TEST("require that file can be accessed as in input") {
+    MappedFileInput file(TEST_PATH("file.txt"));
+    EXPECT_TRUE(file.valid());
+    EXPECT_EQUAL(file.get(), Memory("file content\n"));
+    EXPECT_EQUAL(file.obtain(), Memory("file content\n"));
+    file.evict(5);
+    EXPECT_EQUAL(file.obtain(), Memory("content\n"));
+    file.evict(8);
+    EXPECT_EQUAL(file.obtain(), Memory());
+}
+
+TEST_MAIN() { TEST_RUN_ALL(); }
