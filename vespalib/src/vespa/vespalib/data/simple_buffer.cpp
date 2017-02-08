559a76e491e28ca1ec4874d403866fd508064a54
Haavard <havardpe@yahoo-inc.com>
2017-02-08 13:41:59 +0000

    added assert to protect against wrap-around

--- vespalib/src/vespa/vespalib/data/simple_buffer.cpp
+++ vespalib/src/vespa/vespalib/data/simple_buffer.cpp
@@ -29,6 +29,7 @@ SimpleBuffer::evict(size_t bytes)
 WritableMemory
 SimpleBuffer::reserve(size_t bytes)
 {
+    assert((_used + bytes) >= _used);
     _data.resize(_used + bytes, char(0x55));
     return WritableMemory(&_data[_used], bytes);
 }
