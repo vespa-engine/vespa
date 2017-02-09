ae4d956e1eae13b2acc1212e15e0a63fa0f8df59
Geir Storli <geirst@yahoo-inc.com>
2017-02-09 13:28:22 +0100

    Add deriving of imported fields config for the search backend.

---
+++ config-model/src/main/java/com/yahoo/searchdefinition/derived/ImportedFields.java
@@ -0,0 +1,59 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.searchdefinition.derived;
+
+import com.yahoo.searchdefinition.Search;
+import com.yahoo.searchdefinition.document.ImportedField;
+import com.yahoo.vespa.config.search.ImportedFieldsConfig;
+
+import java.util.Optional;
+
+/**
+ * This class derives imported fields from search definition and produces imported-fields.cfg as needed by the search backend.
+ *
+ * @author geirst
+ */
+public class ImportedFields extends Derived implements ImportedFieldsConfig.Producer {
+
+    private Optional<com.yahoo.searchdefinition.document.ImportedFields> importedFields = Optional.empty();
+
+    public ImportedFields(Search search) {
+        derive(search);
+    }
+
+    @Override
+    protected void derive(Search search) {
+        importedFields = search.importedFields();
+    }
+
+    @Override
+    protected String getDerivedName() {
+        return "imported-fields";
+    }
+
+    @Override
+    public void getConfig(ImportedFieldsConfig.Builder builder) {
+        if (importedFields.isPresent()) {
+            importedFields.get().fields().forEach( (name, field) -> considerField(builder, field));
+        }
+    }
+
+    private static void considerField(ImportedFieldsConfig.Builder builder, ImportedField field) {
+        if (field.referencedField().doesAttributing()) {
+            builder.attribute.add(buildAttribute(field));
+        }
+    }
+
+    private static ImportedFieldsConfig.Attribute.Builder buildAttribute(ImportedField field) {
+        ImportedFieldsConfig.Attribute.Builder result = new ImportedFieldsConfig.Attribute.Builder();
+        result.name(field.aliasFieldName());
+        result.reference(buildReference(field));
+        return result;
+    }
+
+    private static ImportedFieldsConfig.Attribute.Reference.Builder buildReference(ImportedField field) {
+        ImportedFieldsConfig.Attribute.Reference.Builder result = new ImportedFieldsConfig.Attribute.Reference.Builder();
+        result.field(field.documentReference().documentReferenceField().getName());
+        result.targetfield(field.referencedField().getName());
+        return result;
+    }
+}
