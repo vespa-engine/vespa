ff12f27c37e3de29ff6dfd207948269573ae0d81
Arne H Juul <arnej@yahoo-inc.com>
2017-02-09 11:12:19 +0100

    avoid scientific notation in degrees formatting

--- document/src/test/java/com/yahoo/document/PositionTypeTestCase.java
+++ document/src/test/java/com/yahoo/document/PositionTypeTestCase.java
@@ -41,4 +41,29 @@ public class PositionTypeTestCase {
         assertEquals("foo.position", PositionDataType.getPositionSummaryFieldName("foo"));
         assertEquals("foo.distance", PositionDataType.getDistanceSummaryFieldName("foo"));
     }
+
+    @Test
+    public void requireFixedPointFormat() {
+        assertEquals("0.0", PositionDataType.fmtD(0));
+        assertEquals("123.0", PositionDataType.fmtD(123));
+        assertEquals("0.000123", PositionDataType.fmtD(0.000123));
+        assertEquals("0.000001", PositionDataType.fmtD(0.000001));
+        assertEquals("0.0", PositionDataType.fmtD(0.0000004));
+        assertEquals("999.123456", PositionDataType.fmtD(999.1234564));
+
+        Struct val1 = PositionDataType.valueOf(0, 0);
+        assertEquals("N0.0;E0.0", PositionDataType.renderAsString(val1));
+
+        Struct val2 = PositionDataType.valueOf(-1, -1);
+        assertEquals("S0.000001;W0.000001", PositionDataType.renderAsString(val2));
+
+        Struct val3 = PositionDataType.valueOf(123456789, 87654321);
+        assertEquals("N87.654321;E123.456789", PositionDataType.renderAsString(val3));
+
+        Struct val4 = PositionDataType.valueOf(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        assertEquals("S2147.483648;W2147.483648", PositionDataType.renderAsString(val4));
+
+        Struct val5 = PositionDataType.valueOf(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        assertEquals("N2147.483647;E2147.483647", PositionDataType.renderAsString(val5));
+    }
 }
