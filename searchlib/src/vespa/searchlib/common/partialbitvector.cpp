3a0075c933ca1fb3ab81687117f68ca245ba74ef
Henning Baldersheim <balder@yahoo-inc.com>
2017-02-21 15:32:51 +0000

    Protect against copying undefined memory.

--- searchlib/src/vespa/searchlib/common/partialbitvector.cpp
+++ searchlib/src/vespa/searchlib/common/partialbitvector.cpp
@@ -19,7 +19,12 @@ PartialBitVector::PartialBitVector(const BitVector & org, Index start, Index end
     _alloc(allocatePaddedAndAligned(start, end))
 {
     init(_alloc.get(), start, end);
-    memcpy(_alloc.get(), org.getWordIndex(start), numActiveBytes(start, end));
+    const Word * startWord = org.getWordIndex(start);
+    size_t numBytes2Copy = numActiveBytes(start, std::min(org.size(), end));
+    memcpy(_alloc.get(), startWord, numBytes2Copy);
+    if (org.size() < end) {
+        clearInterval(org.size(), end);
+    }
     setBit(size());
 }
 
