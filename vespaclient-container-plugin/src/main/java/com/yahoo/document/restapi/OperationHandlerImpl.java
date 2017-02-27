ab60df07045985178876702955330b1978695812
Haakon Dybdahl <dybdahl@yahoo-inc.com>
2017-02-27 10:51:37 +0100

    Only return defined fieldset.

--- vespaclient-container-plugin/src/main/java/com/yahoo/document/restapi/OperationHandlerImpl.java
+++ vespaclient-container-plugin/src/main/java/com/yahoo/document/restapi/OperationHandlerImpl.java
@@ -267,7 +267,8 @@ public class OperationHandlerImpl implements OperationHandler {
         }
 
         VisitorParameters params = new VisitorParameters(selection.toString());
-
+        // Only return fieldset that is part of the document.
+        params.fieldSet("<" + restUri.getDocumentType() + ">:[document]");
         params.setMaxBucketsPerVisitor(1);
         params.setMaxPending(32);
         params.setMaxFirstPassHits(1);
