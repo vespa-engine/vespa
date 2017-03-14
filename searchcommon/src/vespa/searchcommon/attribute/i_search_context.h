bd5402c9d12eb2a78c7ab8539d32d31d64885f83
Geir Storli <geirst@yahoo-inc.com>
2017-03-14 14:14:23 +0000

    Re-indent.

--- searchcommon/src/vespa/searchcommon/attribute/i_search_context.h
+++ searchcommon/src/vespa/searchcommon/attribute/i_search_context.h
@@ -17,52 +17,51 @@ class QueryTermBase;
 
 namespace attribute {
 
+class ISearchContext {
+public:
+    virtual ~ISearchContext() {}
 
-    class ISearchContext {
-    public:
-        virtual ~ISearchContext() {}
+    virtual unsigned int approximateHits() const = 0;
 
-        virtual unsigned int approximateHits() const = 0;
+    /**
+     * Creates an attribute search iterator associated with this
+     * search context.
+     *
+     * @return attribute search iterator
+     *
+     * @param matchData the attribute match data used when
+     * unpacking data for a hit
+     *
+     * @param strict whether the iterator should be strict or not
+     **/
+    virtual std::unique_ptr<queryeval::SearchIterator>
+    createIterator(fef::TermFieldMatchData *matchData, bool strict) = 0;
 
-        /**
-         * Creates an attribute search iterator associated with this
-         * search context.
-         *
-         * @return attribute search iterator
-         *
-         * @param matchData the attribute match data used when
-         * unpacking data for a hit
-         *
-         * @param strict whether the iterator should be strict or not
-         **/
-        virtual std::unique_ptr<queryeval::SearchIterator>
-        createIterator(fef::TermFieldMatchData *matchData, bool strict) = 0;
+    /**
+     * Creates an attribute search iterator associated with this
+     * search context.  Postings lists are not used.
+     *
+     * @return attribute search iterator
+     *
+     * @param matchData the attribute match data used when
+     * unpacking data for a hit
+     *
+     * @param strict whether the iterator should be strict or not
+     **/
+    virtual std::unique_ptr<queryeval::SearchIterator>
+    createFilterIterator(fef::TermFieldMatchData *matchData, bool strict) = 0;
 
-        /**
-         * Creates an attribute search iterator associated with this
-         * search context.  Postings lists are not used.
-         *
-         * @return attribute search iterator
-         *
-         * @param matchData the attribute match data used when
-         * unpacking data for a hit
-         *
-         * @param strict whether the iterator should be strict or not
-         **/
-        virtual std::unique_ptr<queryeval::SearchIterator>
-        createFilterIterator(fef::TermFieldMatchData *matchData, bool strict) = 0;
+    /*
+     * Create temporary posting lists.
+     * Should be called before createIterator() is called.
+     */
+    virtual void fetchPostings(bool strict) = 0;
 
-        /*
-         * Create temporary posting lists.  Should be called before
-         * createIterator is called.
-         */
-        virtual void fetchPostings(bool strict) = 0;
-        virtual bool valid() const = 0;
-        virtual Int64Range getAsIntegerTerm() const = 0;
+    virtual bool valid() const = 0;
+    virtual Int64Range getAsIntegerTerm() const = 0;
+    virtual const QueryTermBase & queryTerm() const = 0;
 
-        virtual const QueryTermBase & queryTerm() const = 0;
-
-    };
+};
 
 }
 }
