d039def41cabed5989672788776033a81b2873e8
Henning Baldersheim <balder@yahoo-inc.com>
2017-03-17 14:34:29 +0100

    Avoid expensive pooling of json fieldnames. Works very bad with maps.....

--- document/src/main/java/com/yahoo/document/json/JsonFeedReader.java
+++ document/src/main/java/com/yahoo/document/json/JsonFeedReader.java
@@ -25,7 +25,7 @@ import com.yahoo.vespaxmlparser.VespaXMLFeedReader.Operation;
 public class JsonFeedReader implements FeedReader {
     private final JsonReader reader;
     private InputStream stream;
-    private static final JsonFactory jsonFactory = new JsonFactory();
+    private static final JsonFactory jsonFactory = new JsonFactory().disable(JsonFactory.Feature.CANONICALIZE_FIELD_NAMES);
 
     public JsonFeedReader(InputStream stream, DocumentTypeManager docMan) {
         reader = new JsonReader(docMan, stream, jsonFactory);
