55a4e2d31e3f2ba6387fa298d2e3c7368aaedfc8
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-03-30 13:10:14 +0200

    Remove dangling Javadoc parameter description

--- config-model/src/main/java/com/yahoo/searchdefinition/derived/AttributeFields.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/derived/AttributeFields.java
@@ -67,7 +67,6 @@ public class AttributeFields extends Derived implements AttributesConfig.Produce
 
     /**
      * Derives one attribute. TODO: Support non-default named attributes
-     * @param field
      */
     private void deriveAttributes(ImmutableSDField field) {
         for (Attribute fieldAttribute : field.getAttributes().values()) {
