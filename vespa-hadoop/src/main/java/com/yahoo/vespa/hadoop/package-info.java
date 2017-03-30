15917981e649a9e79ee82c3d7676f451ed6ac144
Kristian Aune <kraune@yahoo-inc.com>
2017-03-30 13:42:43 +0200

    package-info for publi API and javadoc

---
+++ vespa-hadoop/src/main/java/com/yahoo/vespa/hadoop/package-info.java
@@ -0,0 +1,12 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * com.yahoo.vespa.hadoop contains classes and utilities
+ * to enable feeding directly to Vespa endpoints from pig and mapreduce.
+ * It is a minimal layer over the Vespa HTTP client.
+ **/
+@ExportPackage
+@PublicApi
+package com.yahoo.vespa.hadoop;
+
+import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.osgi.annotation.ExportPackage;
