991f757f872712f5640478894c46df53239774ad
Kristian Aune <kraune@yahoo-inc.com>
2017-03-31 09:26:20 +0200

    add pig and mapreduce packages

---
+++ vespa-hadoop/src/main/java/com/yahoo/vespa/hadoop/pig/package-info.java
@@ -0,0 +1,12 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * com.yahoo.vespa.hadoop.pig contains classes and utilities
+ * to enable feeding directly to Vespa endpoints from pig.
+ * It is a minimal layer over the Vespa HTTP client.
+ **/
+@ExportPackage
+@PublicApi
+package com.yahoo.vespa.hadoop.pig;
+
+import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.osgi.annotation.ExportPackage;
