8eb8e1ca571bbb2748dafc47cbbbbc9acfe13ff3
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-04-03 16:44:59 +0200

    Forbid document references in streaming search

--- config-model/src/main/java/com/yahoo/vespa/model/application/validation/StreamingValidator.java
+++ config-model/src/main/java/com/yahoo/vespa/model/application/validation/StreamingValidator.java
@@ -1,17 +1,20 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.application.validation;
 
-import java.util.List;
-import java.util.logging.Level;
-
-import com.yahoo.config.model.deploy.DeployState;
-import com.yahoo.searchdefinition.document.Matching;
 import com.yahoo.config.application.api.DeployLogger;
+import com.yahoo.config.model.deploy.DeployState;
+import com.yahoo.document.DataType;
 import com.yahoo.document.NumericDataType;
+import com.yahoo.document.ReferenceDataType;
+import com.yahoo.searchdefinition.document.Attribute;
+import com.yahoo.searchdefinition.document.Matching;
 import com.yahoo.searchdefinition.document.SDField;
 import com.yahoo.vespa.model.VespaModel;
 import com.yahoo.vespa.model.search.AbstractSearchCluster;
 import com.yahoo.vespa.model.search.SearchCluster;
+
+import java.util.List;
+import java.util.logging.Level;
 
 
 /**
@@ -29,10 +32,11 @@ public class StreamingValidator extends Validator {
             SearchCluster sc = (SearchCluster) cluster;
             warnStreamingAttributes(sc, deployState.getDeployLogger());
             warnStreamingGramMatching(sc, deployState.getDeployLogger());
+            failStreamingDocumentReferences(sc);
         }
     }
 
-    private void warnStreamingGramMatching(SearchCluster sc, DeployLogger logger) {
+    private static void warnStreamingGramMatching(SearchCluster sc, DeployLogger logger) {
         if (sc.getSdConfig() != null) {
             for (SDField sd : sc.getSdConfig().getSearch().allConcreteFields()) {
                 if (sd.getMatching().getType().equals(Matching.Type.GRAM)) {
@@ -49,7 +53,7 @@ public class StreamingValidator extends Validator {
      * @param sc     a search cluster to be checked for attributes in streaming search
      * @param logger a DeployLogger
      */
-    private void warnStreamingAttributes(SearchCluster sc, DeployLogger logger) {
+    private static void warnStreamingAttributes(SearchCluster sc, DeployLogger logger) {
         if (sc.getSdConfig() != null) {
             for (SDField sd : sc.getSdConfig().getSearch().allConcreteFields()) {
                 if (sd.doesAttributing()) {
@@ -59,11 +63,24 @@ public class StreamingValidator extends Validator {
         }
     }
 
-    private void warnStreamingAttribute(SearchCluster sc, SDField sd, DeployLogger logger) {
+    private static void warnStreamingAttribute(SearchCluster sc, SDField sd, DeployLogger logger) {
         // If the field is numeric, we can't print this, because we may have converted the field to
         // attribute indexing ourselves (IntegerIndex2Attribute)
         if (sd.getDataType() instanceof NumericDataType) return;
         logger.log(Level.WARNING, "For streaming search cluster '" + sc.getClusterName() +
                 "', SD field '" + sd.getName() + "': 'attribute' has same match semantics as 'index'.");
+    }
+
+    private static void failStreamingDocumentReferences(SearchCluster sc) {
+        for (Attribute attribute : sc.getSdConfig().getAttributeFields().attributes()) {
+            DataType dataType = attribute.getDataType();
+            if (dataType instanceof ReferenceDataType) {
+                String errorMessage = String.format(
+                        "For streaming search cluster '%s': Attribute '%s' has type '%s'. " +
+                                "Document references and imported fields are not allowed in streaming search.",
+                        sc.getClusterName(), attribute.getName(), dataType.getName());
+                throw new IllegalArgumentException(errorMessage);
+            }
+        }
     }
 }
