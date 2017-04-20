2decf3c4e35923b739254e389c26971ee8649a20
Haavard <havardpe@yahoo-inc.com>
2017-04-20 12:39:57 +0000

    added handle-based low-level socket options with testing

---
+++ vespalib/src/vespa/vespalib/net/socket_options.h
@@ -0,0 +1,20 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+namespace vespalib {
+
+/**
+ * Low-level functions used to adjust various socket related
+ * options. Return values indicate success/failure.
+ **/
+struct SocketOptions {
+    static bool set_blocking(int fd, bool value);
+    static bool set_nodelay(int fd, bool value);
+    static bool set_reuse_addr(int fd, bool value);
+    static bool set_ipv6_only(int fd, bool value);
+    static bool set_keepalive(int fd, bool value);
+    static bool set_linger(int fd, bool enable, int value);
+};
+
+} // namespace vespalib
