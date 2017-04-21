30cd1687c29f6159d799e859ba8c6cb4d74c381e
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-04-21 13:37:41 +0200

    Remove some netty usage in jdisc_http_service

--- jdisc_http_service/src/main/java/com/yahoo/jdisc/http/core/HeaderFieldsUtil.java
+++ jdisc_http_service/src/main/java/com/yahoo/jdisc/http/core/HeaderFieldsUtil.java
@@ -3,10 +3,7 @@ package com.yahoo.jdisc.http.core;
 
 import com.ning.http.client.RequestBuilder;
 import com.yahoo.jdisc.HeaderFields;
-import org.jboss.netty.handler.codec.http.HttpChunkTrailer;
-import org.jboss.netty.handler.codec.http.HttpHeaders;
-import org.jboss.netty.handler.codec.http.HttpMessage;
-import org.jboss.netty.handler.codec.http.HttpResponse;
+import com.yahoo.jdisc.http.HttpHeaders;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
@@ -27,24 +24,12 @@ public class HeaderFieldsUtil {
             HttpHeaders.Names.CONTENT_LENGTH,
             HttpHeaders.Names.TRANSFER_ENCODING));
 
-    public static void copyHeaders(com.yahoo.jdisc.Response src, HttpResponse dst) {
-        copyHeaderFields(src.headers(), newSimpleHeaders(dst));
-    }
-
     public static void copyHeaders(com.yahoo.jdisc.Request src, RequestBuilder dst) {
-        copyHeaderFields(src.headers(), newSimpleHeaders(dst));
-    }
-
-    public static void copyTrailers(com.yahoo.jdisc.Response src, HttpResponse dst) {
-        copyTrailers(src, newSimpleHeaders(dst));
-    }
-
-    public static void copyTrailers(com.yahoo.jdisc.Response src, HttpChunkTrailer dst) {
-        copyTrailers(src, newSimpleHeaders(dst));
+        copyHeaderFields(src.headers(), dst::addHeader);
     }
 
     public static void copyTrailers(com.yahoo.jdisc.Request src, RequestBuilder dst) {
-        copyTrailers(src, newSimpleHeaders(dst));
+        copyTrailers(src, dst::addHeader);
     }
 
     public static void copyTrailers(com.yahoo.jdisc.Request src, ByteArrayOutputStream dst) {
@@ -57,17 +42,6 @@ public class HeaderFieldsUtil {
             return;
         }
         final HeaderFields trailers = ((com.yahoo.jdisc.http.HttpRequest)src).trailers();
-        synchronized (trailers) {
-            copyHeaderFields(trailers, dst);
-        }
-    }
-
-    @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-    public static void copyTrailers(com.yahoo.jdisc.Response src, SimpleHeaders dst) {
-        if (!(src instanceof com.yahoo.jdisc.http.HttpResponse)) {
-            return;
-        }
-        final HeaderFields trailers = ((com.yahoo.jdisc.http.HttpResponse)src).trailers();
         synchronized (trailers) {
             copyHeaderFields(trailers, dst);
         }
@@ -88,16 +62,6 @@ public class HeaderFieldsUtil {
         }
     }
 
-    private static SimpleHeaders newSimpleHeaders(final RequestBuilder dst) {
-        return new SimpleHeaders() {
-
-            @Override
-            public void addHeader(String name, String value) {
-                dst.addHeader(name, value);
-            }
-        };
-    }
-
     private static SimpleHeaders newSimpleHeaders(final ByteArrayOutputStream dst) {
         return new SimpleHeaders() {
 
@@ -115,28 +79,7 @@ public class HeaderFieldsUtil {
         };
     }
 
-    private static SimpleHeaders newSimpleHeaders(final HttpMessage dst) {
-        return new SimpleHeaders() {
-
-            @Override
-            public void addHeader(String name, String value) {
-                dst.addHeader(name, value);
-            }
-        };
-    }
-
-    private static SimpleHeaders newSimpleHeaders(final HttpChunkTrailer dst) {
-        return new SimpleHeaders() {
-
-            @Override
-            public void addHeader(String name, String value) {
-                dst.addHeader(name, value);
-            }
-        };
-    }
-
-    private static interface SimpleHeaders {
-
-        public void addHeader(String name, String value);
+    private interface SimpleHeaders {
+        void addHeader(String name, String value);
     }
 }
