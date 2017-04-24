b42c161a6a058440e366dbb77d8a27c304b599ae
valerijf <valerijf@yahoo-inc.com>
2017-04-24 13:55:36 +0200

    Do not auto retire VMs

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/retire/RetireIPv4OnlyNodes.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/retire/RetireIPv4OnlyNodes.java
@@ -1,6 +1,7 @@
 package com.yahoo.vespa.hosted.provision.maintenance.retire;
 
 import com.google.common.net.InetAddresses;
+import com.yahoo.config.provision.Flavor;
 import com.yahoo.vespa.hosted.provision.Node;
 
 import java.net.Inet4Address;
@@ -12,6 +13,7 @@ public class RetireIPv4OnlyNodes implements RetirementPolicy {
 
     @Override
     public boolean shouldRetire(Node node) {
+        if (node.flavor().getType() == Flavor.Type.VIRTUAL_MACHINE) return false;
         return node.ipAddresses().stream()
                 .map(InetAddresses::forString)
                 .allMatch(address -> address instanceof Inet4Address);
