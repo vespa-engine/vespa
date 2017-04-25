b5fae56ca6def8dec9ff40c20e8227cd05f90cc4
Henning Baldersheim <balder@yahoo-inc.com>
2017-04-25 19:00:08 +0200

    This change triggers the override warning ..... Not on system included files....

--- documentapi/src/vespa/documentapi/messagebus/messages/searchresultmessage.cpp
+++ documentapi/src/vespa/documentapi/messagebus/messages/searchresultmessage.cpp
@@ -1,6 +1,6 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/documentapi/messagebus/messages/searchresultmessage.h>
+#include "searchresultmessage.h"
 
 using vdslib::SearchResult;
 
