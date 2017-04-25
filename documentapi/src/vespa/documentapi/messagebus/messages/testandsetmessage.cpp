720f8ae492eec26925425e16801538ef4f56c946
Henning Baldersheim <balder@yahoo-inc.com>
2017-04-25 22:10:10 +0200

    Add missing file.

---
+++ documentapi/src/vespa/documentapi/messagebus/messages/testandsetmessage.cpp
@@ -0,0 +1,9 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include "testandsetmessage.h"
+
+namespace documentapi {
+
+TestAndSetMessage::~TestAndSetMessage() { }
+
+}
