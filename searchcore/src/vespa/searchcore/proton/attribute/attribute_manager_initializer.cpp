bcf40a691b7412ff847c24921d01ef2a0d298ab3
Tor Egge <Tor.Egge@yahoo-inc.com>
2017-04-26 12:53:20 +0000

    Wipe removed attributes at startup.

--- searchcore/src/vespa/searchcore/proton/attribute/attribute_manager_initializer.cpp
+++ searchcore/src/vespa/searchcore/proton/attribute/attribute_manager_initializer.cpp
@@ -123,6 +123,7 @@ AttributeManagerInitializer::run()
 {
     _attrMgr->addExtraAttribute(_documentMetaStore);
     _attrMgr->addInitializedAttributes(_attributesResult.get());
+    _attrMgr->wipeHistory(_configSerialNum);
     *_attrMgrResult = _attrMgr;
 }
 
