1ebf4beb3c6f0c1646a46983ef85964c5e165c3b
Henning Baldersheim <balder@yahoo-inc.com>
2017-04-29 00:53:10 +0200

    Add override

--- vespamalloc/src/tests/test.cpp
+++ vespamalloc/src/tests/test.cpp
@@ -1,6 +1,5 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <vespa/fastos/thread.h>
 
 namespace vespamalloc {
