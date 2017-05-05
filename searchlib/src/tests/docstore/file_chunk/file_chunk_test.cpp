b0ea3ea8a5d65ca8e8298e50bebc984882e8f767
Geir Storli <geirst@yahoo-inc.com>
2017-05-05 13:40:42 +0000

    Add skeleton for unit tests for docstore file chunk.

---
+++ searchlib/src/tests/docstore/file_chunk/file_chunk_test.cpp
@@ -0,0 +1,12 @@
+// Copyright 2017 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/searchlib/docstore/filechunk.h>
+
+#include <vespa/log/log.h>
+LOG_SETUP("file_chunk_test");
+
+using namespace search;
+
+TEST_MAIN() { TEST_RUN_ALL(); }
+
