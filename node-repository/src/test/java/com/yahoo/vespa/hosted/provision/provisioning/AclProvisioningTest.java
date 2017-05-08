08844b1e7481437e1f63c63ec34d0d4236f51486
Jon Bratseth <jonbratseth@yahoo.com>
2017-05-08 10:30:09 +0200

    Revert "Revert "Prefer lower cost nodes over even distribution""

--- node-repository/src/test/java/com/yahoo/vespa/hosted/provision/provisioning/AclProvisioningTest.java
+++ node-repository/src/test/java/com/yahoo/vespa/hosted/provision/provisioning/AclProvisioningTest.java
@@ -23,6 +23,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.yahoo.vespa.hosted.provision.provisioning.ProvisioningTester.createConfig;
