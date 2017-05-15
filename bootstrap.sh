b0721db7219bd8777ec3018afbf9064a284d1f63
Arne H Juul <arnej@yahoo-inc.com>
2017-05-15 11:59:13 +0200

    fix "bootstrap default" same way

--- bootstrap.sh
+++ bootstrap.sh
@@ -84,6 +84,7 @@ case "$MODE" in
         mvn_install -am -pl filedistributionmanager,jrt,linguistics,messagebus -rf config-lib
         ;;
     default)
+        mvn_install -am -rf yolean -pl vespajlib
         mvn_install -am -pl filedistributionmanager -rf config-lib
         ;;
 esac
