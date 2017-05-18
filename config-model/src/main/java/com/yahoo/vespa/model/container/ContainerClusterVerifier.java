242ef8cb23fbaeb9ed3f82222362641ef95d13da
Henning Baldersheim <balder@yahoo-inc.com>
2017-05-18 13:51:30 +0200

    @author last and a simple javadoc

--- config-model/src/main/java/com/yahoo/vespa/model/container/ContainerClusterVerifier.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/ContainerClusterVerifier.java
@@ -3,21 +3,23 @@ package com.yahoo.vespa.model.container;
 import com.yahoo.vespa.model.container.component.Component;
 
 /**
- * @author baldersheim
  *
  * Operations on a container cluster are verified against this verifier
  * This can be used for ignoring, consitency checking.
+ *
+ *  * @author baldersheim
+ *
  */
 public interface ContainerClusterVerifier {
     /**
-     *
+     * Return true if you accept the component.
      * @param component name of component that wants to be added
      * @return true if you accept it
      */
     boolean acceptComponent(Component component);
 
     /**
-     *
+     * Return true if you accept the container.
      * @param container container to add
      * @return true if you accept it
      */
