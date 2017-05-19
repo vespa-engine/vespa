8a0bcd3a36dbce45aa96b366f6917e2b7cd0595b
toby <smorgrav@yahoo-inc.com>
2017-05-19 13:23:42 +0200

    Add unit test to DockerHostCapacity

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/DockerHostCapacity.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/DockerHostCapacity.java
@@ -38,6 +38,7 @@ public class DockerHostCapacity {
         if (comp == 0) {
             comp = freeCapacityOf(hostB, false).compare(freeCapacityOf(hostA, false));
             if (comp == 0) {
+                // If resources are equal - we want to assign to the one with the most IPaddresses free
                 comp = freeIPs(hostB) - freeIPs(hostA);
             }
         }
@@ -98,9 +99,12 @@ public class DockerHostCapacity {
      * Calculate the remaining capacity for the dockerHost.
      */
     private ResourceCapacity freeCapacityOf(Node dockerHost, boolean includeHeadroom) {
+        // Only hosts have free capacity
+        if (!dockerHost.type().equals(NodeType.host)) return new ResourceCapacity();
+
         ResourceCapacity hostCapacity = new ResourceCapacity(dockerHost);
         for (Node container : allNodes.childNodes(dockerHost).asList()) {
-            // Until we have migrated we might have docker containers unallocated
+            // Until we have migrated we might have docker containers unallocated - TODO check off if headroom tenant is safe
             if (includeHeadroom || !(container.allocation().isPresent() && container.allocation().get().owner().tenant().value().equals("headroom"))) {
                 hostCapacity.subtract(container);
             }
