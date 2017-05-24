b0cc8eed7b8017f466eae951dbbacfc4b902193c
Henning Baldersheim <balder@yahoo-inc.com>
2017-05-24 11:28:39 +0200

    Include what you need yourself, and not more.

--- searchcore/src/vespa/searchcore/util/eventloop.h
+++ searchcore/src/vespa/searchcore/util/eventloop.h
@@ -1,9 +1,8 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// Copyright (C) 1999-2003 Fast Search & Transfer ASA
-// Copyright (C) 2003 Overture Services Norway AS
 
 #pragma once
 
+#include <cstddef>
 
 class FastS_TimeOut
 {
--- searchcore/src/vespa/searchcore/util/log.cpp
+++ searchcore/src/vespa/searchcore/util/log.cpp
@@ -1,9 +1,8 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// Copyright (C) 2000-2003 Fast Search & Transfer ASA
-// Copyright (C) 2003 Overture Services Norway AS
-#include <vespa/fastos/fastos.h>
-#include <vespa/log/log.h>
+
 #include <vespa/searchcore/util/log.h>
+
+#include <vespa/log/log.h>
 LOG_SETUP(".searchcore.util.log");
 
 /**
