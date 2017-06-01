e7a298c5498f8c93e2021016ea0371d4d978fdb3
gjoranv <gv@yahoo-inc.com>
2017-06-01 21:20:39 +0200

    Add system metric set to the default consumer.

--- config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultMetricsConsumer.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultMetricsConsumer.java
@@ -1,21 +1,29 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.admin.monitoring;
 
+import com.google.common.collect.ImmutableList;
+
+import static com.yahoo.vespa.model.admin.monitoring.SystemMetrics.systemMetricSet;
+import static java.util.Collections.emptyList;
+
 /**
- * This class sets up the default 'vespa' metrics consumer.
+ * This class sets up the default 'Vespa' metrics consumer.
  *
- * @author <a href="mailto:trygve@yahoo-inc.com">Trygve Bolsø Berdal</a>
+ * @author trygve
  * @author gjoranv
  */
 public class DefaultMetricsConsumer {
 
     public static final String VESPA_CONSUMER_ID = "Vespa";
 
-    private static final MetricSet vespaMetricSet = new VespaMetricSet();
+    private static final MetricSet defaultConsumerMetrics = new MetricSet("default-consumer",
+                                                                          emptyList(),
+                                                                          ImmutableList.of(new VespaMetricSet(),
+                                                                                           systemMetricSet));
 
     @SuppressWarnings("UnusedDeclaration")
     public static MetricsConsumer getDefaultMetricsConsumer() {
-        return new MetricsConsumer(VESPA_CONSUMER_ID, vespaMetricSet);
+        return new MetricsConsumer(VESPA_CONSUMER_ID, defaultConsumerMetrics);
     }
 
 }
