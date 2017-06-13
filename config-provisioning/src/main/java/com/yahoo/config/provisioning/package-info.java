ed8a765d3c4f9b071b686a0bb8d305a46f8720c1
Harald Musum <musum@yahoo-inc.com>
2017-06-13 14:07:58 +0200

    Remove public api for now

--- config-provisioning/src/main/java/com/yahoo/config/provisioning/package-info.java
+++ config-provisioning/src/main/java/com/yahoo/config/provisioning/package-info.java
@@ -1,7 +1,5 @@
 // Copyright 2016 Yahoo Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
-@PublicApi
 package com.yahoo.config.provisioning;
 
-import com.yahoo.api.annotations.PublicApi;
 import com.yahoo.osgi.annotation.ExportPackage;
