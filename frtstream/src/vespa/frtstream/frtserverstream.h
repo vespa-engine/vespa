47877aefe3207afd6ebc4d9ade795d0e4a174a03
Henning Baldersheim <balder@yahoo-inc.com>
2017-06-19 16:59:59 +0200

    Correct include syntax and depend on correct library.

--- frtstream/src/vespa/frtstream/frtserverstream.h
+++ frtstream/src/vespa/frtstream/frtserverstream.h
@@ -1,7 +1,7 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
 
-#include <vespa/frtstream/frtstream.h>
+#include "frtstream.h"
 #include <vespa/fnet/frt/rpcrequest.h>
 
 namespace frtstream {
