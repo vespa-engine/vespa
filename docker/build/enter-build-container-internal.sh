4bfe4190fe97ad2c49467e2c531dab3535c04a3e
gjoranv <gv@yahoo-inc.com>
2017-06-27 15:37:59 +0200

    Move enter-build-container-internal.sh into build/ folder.
    + cd to vespa dir to avoid repeated '../'

--- docker/build/enter-build-container-internal.sh
+++ docker/build/enter-build-container-internal.sh
@@ -9,15 +9,15 @@ if [ $# -ne 0 ]; then
 fi
 
 USERNAME=builder
-DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-cd $DIR
+VESPA_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd)
+cd $VESPA_DIR
 
-CALLER_UID=$(stat -c "%u" $DIR)
+CALLER_UID=$(stat -c "%u" $VESPA_DIR)
 
 if [ $CALLER_UID -ne 0 ]; then
   # We are in a system that maps the uids and this dir is not owned by root
   # Create a user with same uid and gid to avoid mixing
-  CALLER_GID=$(stat -c "%g" $DIR)
+  CALLER_GID=$(stat -c "%g" $VESPA_DIR)
 
   groupadd -f -g $CALLER_GID $USERNAME
   useradd -u $CALLER_UID -g $CALLER_GID $USERNAME
@@ -26,12 +26,11 @@ else
   USERNAME=root
 fi
 
-su -c "mkdir -p $DIR/../.ccache" - $USERNAME
-su -c "ln -sf $DIR/../.ccache \$HOME/.ccache" - $USERNAME
+su -c "mkdir -p $VESPA_DIR/.ccache" - $USERNAME
+su -c "ln -sf $VESPA_DIR/.ccache \$HOME/.ccache" - $USERNAME
 
-su -c "mkdir -p $DIR/../.m2" - $USERNAME
-su -c "ln -sf $DIR/../.m2 \$HOME/.m2" - $USERNAME
+su -c "mkdir -p $VESPA_DIR/.m2" - $USERNAME
+su -c "ln -sf $VESPA_DIR/.m2 \$HOME/.m2" - $USERNAME
 
-cd $DIR/.. 
 su $USERNAME
 
