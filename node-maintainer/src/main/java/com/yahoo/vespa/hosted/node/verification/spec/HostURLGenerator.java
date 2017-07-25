f4fb361c496c7efbdfe55e5f45313273503fde9e
Ola Aunrønning <olaa@yahoo-inc.com>
2017-07-25 09:31:59 +0200

    Changed variable names

--- node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/spec/HostURLGenerator.java
+++ node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/spec/HostURLGenerator.java
@@ -8,11 +8,11 @@ import java.net.URL;
  */
 public class HostURLGenerator {
 
-    private static final String API_PATH_PREFIX= "/nodes/v2/node/";
+    private static final String NODE_HOSTNAME_PREFIX= "/nodes/v2/node/";
 
-    protected URL generateNodeInfoUrl(String zoneHostName) throws MalformedURLException {
+    protected URL generateNodeInfoUrl(String configServerHostName) throws MalformedURLException {
         String nodeHostName = getEnvironmentVariable("HOSTNAME");
-        return new URL(zoneHostName + API_PATH_PREFIX + nodeHostName);
+        return new URL(configServerHostName + NODE_HOSTNAME_PREFIX + nodeHostName);
     }
 
     protected String getEnvironmentVariable(String variableName) {
