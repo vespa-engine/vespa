d668a46f57c557e2b24d925f5499e42ea92e8051
Ola Aunrønning <olaa@yahoo-inc.com>
2017-07-26 16:19:18 +0200

    Added test for SpecVerifier

--- node-maintainer/src/test/java/com/yahoo/vespa/hosted/node/verification/spec/SpecVerifierTest.java
+++ node-maintainer/src/test/java/com/yahoo/vespa/hosted/node/verification/spec/SpecVerifierTest.java
@@ -1,29 +1,36 @@
 package com.yahoo.vespa.hosted.node.verification.spec;
 
+import com.yahoo.vespa.hosted.node.verification.commons.CommandExecutor;
 import com.yahoo.vespa.hosted.node.verification.mock.MockCommandExecutor;
-import com.yahoo.yolean.chain.Before;
+import com.yahoo.vespa.hosted.node.verification.spec.noderepo.NodeRepoJsonModel;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
 public class SpecVerifierTest {
 
+    private SpecVerifier specVerifier;
     private MockCommandExecutor mockCommandExecutor;
+    private static final String PATH = Paths.get(".").toAbsolutePath().normalize().toString();
+    private static final String RESOURCE_PATH = "file://" + PATH + "/src/test/java/com/yahoo/vespa/hosted/node/verification/spec/resources/";
 
-    @Test
-    public void verifySpec() throws Exception {
+    @Before
+    public void setup() {
+        mockCommandExecutor = new MockCommandExecutor();
+        specVerifier = new SpecVerifier();
     }
 
     @Test
-    public void makeYamasSpecReport() throws Exception {
-    }
+    public void getNodeRepositoryJSON_should_return_valid_nodeRepoJSONModel() throws Exception {
+        mockCommandExecutor.addCommand("echo nodeRepo.json");
+        NodeRepoJsonModel actualNodeRepoJsonModel = specVerifier.getNodeRepositoryJSON(RESOURCE_PATH, mockCommandExecutor);
+        NodeRepoJsonModel expectedNodeRepoJsonModel = new NodeRepoJsonModel();
+        String expectedIpv6Address = "2001:4998:c:2940::111c";
+        assertEquals(expectedIpv6Address, actualNodeRepoJsonModel.getIpv6Address());
 
-    @Test
-    public void getNodeRepositoryJSON() throws Exception {
-    }
-
-    @Test
-    public void main() throws Exception {
     }
 
 }
