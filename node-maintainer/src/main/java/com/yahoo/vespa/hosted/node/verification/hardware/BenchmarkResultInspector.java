49b4df84d382d1f2d4b9dd8253b2a532bad670d5
sindre <sindre.grostad@icloud.com>
2017-08-08 10:34:43 +0200

    cleanup

--- node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/hardware/BenchmarkResultInspector.java
+++ node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/hardware/BenchmarkResultInspector.java
@@ -16,15 +16,15 @@ public class BenchmarkResultInspector {
     public static BenchmarkReport makeBenchmarkReport(BenchmarkResults benchmarkResults) {
         BenchmarkReport benchmarkReport = new BenchmarkReport();
         double cpuCyclesPerSec = benchmarkResults.getCpuCyclesPerSec();
-        if ( cpuCyclesPerSec < CPU_FREQUENCY_LOWER_LIMIT) {
+        if (cpuCyclesPerSec < CPU_FREQUENCY_LOWER_LIMIT) {
             benchmarkReport.setCpuCyclesPerSec(cpuCyclesPerSec);
         }
         double memoryWriteSpeed = benchmarkResults.getMemoryWriteSpeedGBs();
-        if ( memoryWriteSpeed < MEMORY_WRITE_SPEED_LOWER_LIMIT) {
+        if (memoryWriteSpeed < MEMORY_WRITE_SPEED_LOWER_LIMIT) {
             benchmarkReport.setMemoryWriteSpeedGBs(memoryWriteSpeed);
         }
         double memoryReadSpeed = benchmarkResults.getMemoryReadSpeedGBs();
-        if ( memoryReadSpeed < MEMORY_READ_SPEED_LOWER_LIMIT) {
+        if (memoryReadSpeed < MEMORY_READ_SPEED_LOWER_LIMIT) {
             benchmarkReport.setMemoryReadSpeedGBs(memoryReadSpeed);
         }
         double diskSpeed = benchmarkResults.getDiskSpeedMbs();
