b86bd891cd80f38f5d828d2e512f2e15beef45f4
sindre <sindre.grostad@icloud.com>
2017-08-08 09:57:56 +0200

    Added verificationReport test and removed skipwords in parseInstructions

--- node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/commons/ParseInstructions.java
+++ node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/commons/ParseInstructions.java
@@ -11,8 +11,6 @@ public class ParseInstructions {
     private final int valueElementIndex;
     private final String splitRegex;
     private final ArrayList<String> searchWords;
-    private String skipWord;
-    private String skipUntilKeyword;
 
     public ParseInstructions(int searchElementIndex, int returnElementNum, String splitRegex, ArrayList<String> searchWords) {
         this.searchElementIndex = searchElementIndex;
@@ -35,22 +33,6 @@ public class ParseInstructions {
 
     public ArrayList<String> getSearchWords() {
         return searchWords;
-    }
-
-    public void setSkipWord(String skipWord) {
-        this.skipWord = skipWord;
-    }
-
-    public void setSkipUntilKeyword(String skipUntilWord) {
-        this.skipUntilKeyword = skipUntilWord;
-    }
-
-    public String getSkipWord() {
-        return this.skipWord;
-    }
-
-    public String getSkipUntilKeyword() {
-        return this.skipUntilKeyword;
     }
 
 }
