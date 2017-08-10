03a28079b42fed21621af79323eb4dd06194ca25
Ola Aunrønning <olaa@yahoo-inc.com>
2017-08-10 09:16:14 +0200

    Report is printed to std out

--- node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/commons/ReportSender.java
+++ node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/commons/ReportSender.java
@@ -7,17 +7,10 @@ import com.yahoo.vespa.hosted.node.verification.commons.noderepo.NodeRepoJsonMod
 import com.yahoo.vespa.hosted.node.verification.commons.report.BenchmarkReport;
 import com.yahoo.vespa.hosted.node.verification.commons.report.HardwareDivergenceReport;
 import com.yahoo.vespa.hosted.node.verification.commons.report.SpecVerificationReport;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ReportSender {
@@ -31,19 +24,9 @@ public class ReportSender {
             report = "{\"hardwareDivergence\": null}";
         }
         else {
-            report = "{\"hardwareDivergence\": " + om.writeValueAsString(hardwareDivergenceReport) + "}";
+            report = "{\"hardwareDivergence\": \"" + om.writeValueAsString(hardwareDivergenceReport) + "\"}";
         }
-        HttpPatch httpPatch = new HttpPatch(nodeInfoUrls.get(0).toString());
-        httpPatch.setEntity(new StringEntity(report));
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        try {
-            CloseableHttpResponse httpResponse = httpClient.execute(httpPatch);
-            logger.log(Level.INFO, "Response code: " + httpResponse.getStatusLine().getStatusCode());
-
-        } catch (ClientProtocolException e) {
-          System.out.println("Failed to patch node repo - Invalid URL");
-        }
-        httpClient.close();
+        System.out.println(om.writeValueAsString(report));
     }
 
     public static void reportBenchmarkResults(BenchmarkReport benchmarkReport, ArrayList<URL> nodeInfoUrls) throws IOException {
