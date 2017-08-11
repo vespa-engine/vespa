bbe37ed65de1c614b5f3548878c9ef1ed0580c7c
Henning Baldersheim <balder@yahoo-inc.com>
2017-08-11 11:04:57 +0200

    Document behavioral change between gcc 6 and 7.

--- vespamalloc/src/tests/test1/testatomic.cpp
+++ vespamalloc/src/tests/test1/testatomic.cpp
@@ -112,7 +112,11 @@ int Test::Main()
     {
         std::atomic<vespamalloc::TaggedPtr> taggedPtr;
         ASSERT_EQUAL(16u, sizeof(vespamalloc::TaggedPtr));
+#if __GNUC__ < 7
         ASSERT_TRUE(taggedPtr.is_lock_free());
+#else
+        ASSERT_FALSE(taggedPtr.is_lock_free());
+#endif
     }
 
     testSwap<uint32_t>(6);
