182ef6c7d837822fc979323c9d593edabcb0eb54
sindre <sindre.grostad@icloud.com>
2017-08-16 09:58:58 +0200

    Add tests for reporting when old HardwareDivergence is wrong and when not existing

--- node-maintainer/src/test/java/com/yahoo/vespa/hosted/node/verification/commons/report/ReportSenderTest.java
+++ node-maintainer/src/test/java/com/yahoo/vespa/hosted/node/verification/commons/report/ReportSenderTest.java
@@ -18,15 +18,25 @@ public class ReportSenderTest {
 
     private final ByteArrayOutputStream println = new ByteArrayOutputStream();
     private static final String ABSOLUTE_PATH = Paths.get(".").toAbsolutePath().normalize().toString();
-    private static final String RESOURCE_PATH = "src/test/java/com/yahoo/vespa/hosted/node/verification/spec/resources/nodeInfoTest.json";
-    private static final String URL_RESOURCE_PATH = "file://" + ABSOLUTE_PATH + "/" + RESOURCE_PATH;
-    private static ArrayList<URL> nodeInfoUrls;
+    private static final String RESOURCE_PATH_TO_VALID_HARDWARE_DIVERGENCE = "src/test/java/com/yahoo/vespa/hosted/node/verification/spec/resources/nodeInfoTest.json";
+    private static final String RESOURCE_PATH_TO_INVALID_HARDWARE_DIVERGENCE = "src/test/java/com/yahoo/vespa/hosted/node/verification/spec/resources/nodeInfoNotInterpretableHardwareDivergence.json";
+    private static final String RESOURCE_PATH_TO_EMPTY_HARDWARE_DIVERGENCE = "src/test/java/com/yahoo/vespa/hosted/node/verification/spec/resources/nodeRepo.json";
+    private static final String URL_VALID_RESOURCE_PATH = "file://" + ABSOLUTE_PATH + "/" + RESOURCE_PATH_TO_VALID_HARDWARE_DIVERGENCE;
+    private static final String URL_INVALID_RESOURCE_PATH = "file://" + ABSOLUTE_PATH + "/" + RESOURCE_PATH_TO_INVALID_HARDWARE_DIVERGENCE;
+    private static final String URL_EMPTY_RESOURCE_PATH = "file://" + ABSOLUTE_PATH + "/" + RESOURCE_PATH_TO_EMPTY_HARDWARE_DIVERGENCE;
+    private static ArrayList<URL> nodeInfoUrlsToValidHardwareDivergence;
+    private static ArrayList<URL> nodeInfoUrlsToNOTValidHardwareDivergence;
+    private static ArrayList<URL> nodeInfoUrlsWithNoHardwareDivergence;
 
     @Before
     public void setup() throws IOException {
         System.setOut(new PrintStream(println));
-        URL nodeInfoUrlWithAlreadyExistingHardwareDivergence = new URL(URL_RESOURCE_PATH);
-        nodeInfoUrls = new ArrayList<>(Arrays.asList(nodeInfoUrlWithAlreadyExistingHardwareDivergence));
+        URL nodeInfoUrlWithAlreadyExistingHardwareDivergence = new URL(URL_VALID_RESOURCE_PATH);
+        nodeInfoUrlsToValidHardwareDivergence = new ArrayList<>(Arrays.asList(nodeInfoUrlWithAlreadyExistingHardwareDivergence));
+        URL nodeInfoUrlWithExistingButWrongHardwareDivergence = new URL(URL_INVALID_RESOURCE_PATH);
+        nodeInfoUrlsToNOTValidHardwareDivergence = new ArrayList<>(Arrays.asList(nodeInfoUrlWithExistingButWrongHardwareDivergence));
+        URL nodeInfoUrlWithNoHardwareDivergence = new URL(URL_EMPTY_RESOURCE_PATH);
+        nodeInfoUrlsWithNoHardwareDivergence = new ArrayList<>(Arrays.asList(nodeInfoUrlWithNoHardwareDivergence));
     }
 
     @After
@@ -42,7 +52,7 @@ public class ReportSenderTest {
         benchmarkReport.setCpuCyclesPerSec(cpuCyclesPerSec);
         benchmarkReport.setMemoryReadSpeedGBs(memoryReadSpeedGBs);
         String expectedReport = "{\"benchmarkReport\":{\"cpuCyclesPerSec\":0.3,\"memoryReadSpeedGBs\":0.1}}";
-        ReportSender.reportBenchmarkResults(benchmarkReport,nodeInfoUrls);
+        ReportSender.reportBenchmarkResults(benchmarkReport, nodeInfoUrlsToValidHardwareDivergence);
         assertEquals(expectedReport, println.toString());
     }
 
@@ -50,7 +60,7 @@ public class ReportSenderTest {
     public void reportBenchmarkResults_should_should_update_already_existing_hardwareDivergence_prints_null_when_empty_benchmarkReport() throws Exception {
         BenchmarkReport benchmarkReport = new BenchmarkReport();
         String expectedReport = "null";
-        ReportSender.reportBenchmarkResults(benchmarkReport, nodeInfoUrls);
+        ReportSender.reportBenchmarkResults(benchmarkReport, nodeInfoUrlsToValidHardwareDivergence);
         assertEquals(expectedReport, println.toString());
     }
 
@@ -62,7 +72,25 @@ public class ReportSenderTest {
         specVerificationReport.setActualDiskSpaceAvailable(actualDiskSpaceAvailable);
         specVerificationReport.setActualIpv6Connection(actualIpv6Connection);
         String expectedReport = "{\"specVerificationReport\":{\"actualDiskSpaceAvailable\":150.0,\"actualIpv6Connection\":false},\"benchmarkReport\":{\"cpuCyclesPerSec\":0.5}}";
-        ReportSender.reportSpecVerificationResults(specVerificationReport, nodeInfoUrls);
+        ReportSender.reportSpecVerificationResults(specVerificationReport, nodeInfoUrlsToValidHardwareDivergence);
+        assertEquals(expectedReport, println.toString());
+    }
+
+    @Test
+    public void reportSpecVerificationResults_make_new_correct_hardwareDivergence_because_old_is_wrong() throws Exception {
+        SpecVerificationReport specVerificationReport = new SpecVerificationReport();
+        double actualDiskSpaceAvailable = 150D;
+        specVerificationReport.setActualDiskSpaceAvailable(actualDiskSpaceAvailable);
+        String expectedReport = "{\"specVerificationReport\":{\"actualDiskSpaceAvailable\":150.0}}";
+        ReportSender.reportSpecVerificationResults(specVerificationReport, nodeInfoUrlsToNOTValidHardwareDivergence);
+        assertEquals(expectedReport, println.toString());
+    }
+
+    @Test
+    public void reportSpecVerificationResults_make_new_empty_hardwareDivergence_because_there_is_no_old() throws Exception {
+        SpecVerificationReport specVerificationReport = new SpecVerificationReport();
+        String expectedReport = "null";
+        ReportSender.reportSpecVerificationResults(specVerificationReport, nodeInfoUrlsWithNoHardwareDivergence);
         assertEquals(expectedReport, println.toString());
     }
 
