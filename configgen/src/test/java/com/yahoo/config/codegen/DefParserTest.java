759d8b3f5d743f18a84ddd4c2549eb6ec557a576
gjoranv <gv@oath.com>
2017-08-16 14:02:00 +0200

    Imports only.

--- configgen/src/test/java/com/yahoo/config/codegen/DefParserTest.java
+++ configgen/src/test/java/com/yahoo/config/codegen/DefParserTest.java
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.config.codegen;
 
-import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
