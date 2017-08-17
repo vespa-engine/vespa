b56e7e401c792da1b6470213e54730cdd7ba843c
Geir Storli <geirst@yahoo-inc.com>
2017-08-17 13:01:29 +0200

    Remove build rpath that is not needed.

--- bootstrap-cpp.sh
+++ bootstrap-cpp.sh
@@ -29,6 +29,5 @@ cmake3 \
     -DEXTRA_LINK_DIRECTORY="/opt/vespa-boost/lib;/opt/vespa-libtorrent/lib;/opt/vespa-zookeeper-c-client/lib;/opt/vespa-cppunit/lib;/usr/lib64/llvm3.9/lib" \
     -DEXTRA_INCLUDE_DIRECTORY="/opt/vespa-boost/include;/opt/vespa-libtorrent/include;/opt/vespa-zookeeper-c-client/include;/opt/vespa-cppunit/include;/usr/include/llvm3.9" \
     -DCMAKE_INSTALL_RPATH="/opt/vespa/lib64;/opt/vespa-boost/lib;/opt/vespa-libtorrent/lib;/opt/vespa-zookeeper-c-client/lib;/opt/vespa-cppunit/lib;/usr/lib/jvm/java-1.8.0/jre/lib/amd64/server;/usr/include/llvm3.9" \
-    -DCMAKE_BUILD_RPATH=/opt/vespa/lib64 \
     -DVALGRIND_UNIT_TESTS=no \
     "${SOURCE_DIR}"
