b5535425c95403b90a3b311947b0961758f8e3eb
sindre <sindre.grostad@icloud.com>
2017-08-18 15:42:57 +0200

    Changed write location for diskbenchmark

--- node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/hardware/benchmarks/DiskBenchmark.java
+++ node-maintainer/src/main/java/com/yahoo/vespa/hosted/node/verification/hardware/benchmarks/DiskBenchmark.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
  */
 public class DiskBenchmark implements Benchmark {
 
-    private static final String DISK_BENCHMARK_COMMAND = "(dd if=/dev/zero of=/tmp/output conv=fdatasync bs=4G count=4; rm -f /tmp/output;) 2>&1 | grep bytes | awk  '{ print $8 \" \" $9 }'";
+    private static final String DISK_BENCHMARK_COMMAND = "(dd if=/dev/zero of=/home/y/tmp/output conv=fdatasync bs=4G count=4; rm -f /home/y/tmp/output;) 2>&1 | grep bytes | awk  '{ print $8 \" \" $9 }'";
     private static final String KILO_BYTE_SEARCH_WORD = "kB/s";
     private static final String MEGA_BYTE_SEARCH_WORD = "MB/s";
     private static final String GIGA_BYTE_SEARCH_WORD = "GB/s";
