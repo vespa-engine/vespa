be11fc0adb411f7ff2b13cdee911620bc5ba0d76
Henning Baldersheim <balder@yahoo-inc.com>
2017-08-22 11:47:54 +0200

    Move instead of copy.

--- searchlib/src/vespa/searchlib/aggregation/group.cpp
+++ searchlib/src/vespa/searchlib/aggregation/group.cpp
@@ -206,10 +206,10 @@ Group & Group::operator = (const Group & rhs) = default;
 Group::~Group() { }
 
 Group &
-Group::partialCopy(const Group & rhs) {
+Group::partialMove(Group &rhs) {
     setId(*rhs._id);
     _rank = rhs._rank;
-    _aggr.partialCopy(rhs._aggr);
+    _aggr.partialMove(rhs._aggr);
     return *this;
 }
 
@@ -373,7 +373,7 @@ void
 Group::Value::mergeLevel(const Group & protoType, const Value & b) {
     for (ChildP *it(b._children), *mt(b._children + b.getChildrenSize()); it != mt; ++it) {
         ChildP g(new Group(protoType));
-        g->partialCopy(**it);
+        g->partialMove(**it);
         addChild(g);
     }
 }
@@ -735,10 +735,10 @@ Group::Value::swap(Value & rhs)
 
 
 void
-Group::Value::partialCopy(const Value & rhs) {
+Group::Value::partialMove(Value &rhs) {
     uint32_t totalAggrSize = getAggrSize() + getExprSize();
     for(size_t i(0), m(totalAggrSize); i < m; i++) {
-        _aggregationResults[i] = rhs._aggregationResults[i];
+        _aggregationResults[i] = std::move(rhs._aggregationResults[i]);
     }
     for(size_t i(0), m(getAggrSize()); i < m; i++) {
         getAggr(i)->reset();
