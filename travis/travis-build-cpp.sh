9acc48024441c5ce38c0e3a6a30726e8b0a951c3
Bjørn Christian Seime <bjorncs@oath.com>
2017-08-25 14:16:08 +0200

    Improve ccache logging

--- travis/travis-build-cpp.sh
+++ travis/travis-build-cpp.sh
@@ -10,8 +10,10 @@ mkdir "${BUILD_DIR}"
 export CCACHE_SIZE="1G"
 export CCACHE_COMPRESS=1
 NUM_THREADS=4
+ccache --print-config
 
 cd ${BUILD_DIR}
 bash ${SOURCE_DIR}/bootstrap-cpp.sh ${SOURCE_DIR} ${BUILD_DIR}
 make -j ${NUM_THREADS}
 ctest3 --output-on-failure -j ${NUM_THREADS}
+ccache --show-stats
