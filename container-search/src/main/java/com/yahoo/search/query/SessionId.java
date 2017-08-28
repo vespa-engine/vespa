62153b6e19e30cf3d7b6c48a8ffd4dc63b8d45aa
Henning Baldersheim <balder@yahoo-inc.com>
2017-08-28 12:01:49 +0200

    Add info about enabled caches and their keys.

--- container-search/src/main/java/com/yahoo/search/query/SessionId.java
+++ container-search/src/main/java/com/yahoo/search/query/SessionId.java
@@ -22,4 +22,19 @@ public class SessionId {
     }
 
     public Utf8String asUtf8String() { return id; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SessionId sessionId = (SessionId) o;
+
+        return id.equals(sessionId.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
