eb1927ad7ddb3b84891e8c0c664d779fc6f69d41
Bjørn Christian Seime <bjorncs@oath.com>
2017-09-04 16:23:16 +0200

    Support KeyService impls with versioned secrets

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/security/KeyService.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/security/KeyService.java
@@ -5,9 +5,14 @@ package com.yahoo.vespa.hosted.controller.api.integration.security;
  * A service for retrieving secrets, such as API keys, private keys and passwords.
  *
  * @author mpolden
+ * @author bjorncs
  */
 public interface KeyService {
 
     String getSecret(String key);
+
+    default String getSecret(String key, int version) {
+        throw new UnsupportedOperationException("KeyService implementation does not support versioned secrets");
+    }
 
 }
