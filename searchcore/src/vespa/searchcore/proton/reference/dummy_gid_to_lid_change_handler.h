00bdcf1fa030d9fb2c49a6d7b927cb0bcc2dddef
Tor Egge <Tor.Egge@oath.com>
2017-09-06 08:39:10 +0000

    Move API comments from implementation headers to interface header.

--- searchcore/src/vespa/searchcore/proton/reference/dummy_gid_to_lid_change_handler.h
+++ searchcore/src/vespa/searchcore/proton/reference/dummy_gid_to_lid_change_handler.h
@@ -24,23 +24,10 @@ public:
     DummyGidToLidChangeHandler();
     virtual ~DummyGidToLidChangeHandler();
 
-    /**
-     * Notify gid to lid mapping change.
-     */
     virtual void notifyPut(GlobalId gid, uint32_t lid, SerialNum serialNum) override;
     virtual void notifyRemove(GlobalId gid, SerialNum serialNum) override;
     virtual void notifyRemoveDone(GlobalId gid, SerialNum serialNum) override;
-
-    /*
-     * Add listener unless a listener with matching docTypeName and
-     * name already exists.
-     */
     virtual void addListener(std::unique_ptr<IGidToLidChangeListener> listener) override;
-
-    /**
-     * Remove listeners with matching docTypeName unless name is present in
-     * keepNames.
-     */
     virtual void removeListeners(const vespalib::string &docTypeName,
                                  const std::set<vespalib::string> &keepNames) override;
 };
