81be66108ab02a443562e36c0d4de94c2114c202
Arne Juul <arnej@yahoo-inc.com>
2017-09-19 08:37:28 +0000

    garbage collect outdated scripts

--- vespalib/fix-hg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-td=${0%.sh}.pl
-if [ -f "$td" ]; then
-    find . -name '*.h' -o -name '*.hpp' | xargs $td
-else
-    echo "Could not find '${td}'"
-fi
