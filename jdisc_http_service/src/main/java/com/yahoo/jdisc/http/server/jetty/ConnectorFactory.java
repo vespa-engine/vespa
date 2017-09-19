cdbb2483af54f9f82a1aa5328eb25cbcef948051
Arnstein Ressem <aressem@gmail.com>
2017-09-19 22:17:15 +0200

    Revert "Bjorncs/client certificate"

--- jdisc_http_service/src/main/java/com/yahoo/jdisc/http/server/jetty/ConnectorFactory.java
+++ jdisc_http_service/src/main/java/com/yahoo/jdisc/http/server/jetty/ConnectorFactory.java
@@ -135,8 +135,6 @@ public class ConnectorFactory {
         Ssl sslConfig = connectorConfig.ssl();
 
         final SslContextFactory factory = new SslContextFactory();
-        factory.setNeedClientAuth(sslConfig.needClientAuth());
-
         if (!sslConfig.excludeProtocol().isEmpty()) {
             final String[] prots = new String[sslConfig.excludeProtocol().size()];
             for (int i = 0; i < prots.length; i++) {
