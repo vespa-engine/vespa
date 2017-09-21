d98eca3239d8d531dcb93504daeee2c01190ee2f
Leandro Alves <leandroalves@yahoo-inc.com>
2017-09-21 14:48:20 +0200

    adds cluster cost metrics class

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/MockMetricsService.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/MockMetricsService.java
@@ -4,6 +4,9 @@ package com.yahoo.vespa.hosted.controller.integration;
 import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.Zone;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author bratseth
  */
@@ -17,6 +20,13 @@ public class MockMetricsService implements com.yahoo.vespa.hosted.controller.api
     @Override
     public DeploymentMetrics getDeploymentMetrics(ApplicationId application, Zone zone) {
         return new DeploymentMetrics(1, 2, 3, 4, 5);
+    }
+
+    @Override
+    public List<ClusterCostMetrics> getClusterCostMetrics(ApplicationId application, Zone zone) {
+        CostMetrics costMetrics = new CostMetrics(55.54, 69.90, 34.59);
+        ClusterCostMetrics clusterCostMetrics = new ClusterCostMetrics("default", costMetrics);
+        return Collections.singletonList(clusterCostMetrics);
     }
 
 }
