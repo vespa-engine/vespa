3061a57e93cc1bd4f6354bdb05335160e1282aa0
Valerij Fredriksen <valerijf@oath.com>
2017-09-25 13:22:25 +0200

    Add tryLock()

--- vespajlib/src/main/java/com/yahoo/concurrent/classlock/ClassLocking.java
+++ vespajlib/src/main/java/com/yahoo/concurrent/classlock/ClassLocking.java
@@ -2,36 +2,40 @@ package com.yahoo.concurrent.classlock;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * @author valerijf
  */
 public class ClassLocking {
     private final Map<Class<?>, ClassLock> classLocks = new HashMap<>();
-    private final Object monitor = new Object();
 
-    public ClassLock lock(Class<?> clazz) {
-        synchronized (monitor) {
-            while(classLocks.containsKey(clazz)) {
-                try {
-                    monitor.wait();
-                } catch (InterruptedException ignored) { }
-            }
-
-            ClassLock classLock = new ClassLock(this, clazz);
-            classLocks.put(clazz, classLock);
-            return classLock;
-        }
+    public synchronized ClassLock lock(Class<?> clazz) {
+        return tryLock(clazz, () -> true);
     }
 
-    void unlock(Class<?> clazz, ClassLock classLock) {
-        synchronized (monitor) {
-            if (classLock.equals(classLocks.get(clazz))) {
-                classLocks.remove(clazz);
-                monitor.notifyAll();
-            } else {
-                throw new IllegalArgumentException("Lock has already been released");
+    public synchronized ClassLock tryLock(Class<?> clazz, Supplier<Boolean> continueRetrying) {
+        while(classLocks.containsKey(clazz)) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {
+                if (! continueRetrying.get()) {
+                    throw new LockInterruptException();
+                }
             }
+        }
+
+        ClassLock classLock = new ClassLock(this, clazz);
+        classLocks.put(clazz, classLock);
+        return classLock;
+    }
+
+    synchronized void unlock(Class<?> clazz, ClassLock classLock) {
+        if (classLock.equals(classLocks.get(clazz))) {
+            classLocks.remove(clazz);
+            notifyAll();
+        } else {
+            throw new IllegalArgumentException("Lock has already been released");
         }
     }
 }
