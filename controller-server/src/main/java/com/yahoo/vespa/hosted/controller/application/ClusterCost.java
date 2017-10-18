8f417af81fe1d5c011636eb9eeedf5a3c68775cf
toby <smorgrav@yahoo-inc.com>
2017-10-18 12:46:34 +0200

    Avoid NPE when cluster utilizations are updated but not cluster info

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/application/ClusterCost.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/application/ClusterCost.java
@@ -1,6 +1,8 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.application;
 
+import java.util.Objects;
+
 /**
  * Calculate utilization relative to the target utilization,
  * tco and waste for one cluster of one deployment.
@@ -35,6 +37,9 @@ public class ClusterCost {
      */
     public ClusterCost(ClusterInfo clusterInfo,
                        ClusterUtilization systemUtilization) {
+
+        Objects.requireNonNull(clusterInfo, "Cluster info cannot be null");
+        Objects.requireNonNull(systemUtilization, "Cluster utilization cannot be null");
 
         this.clusterInfo = clusterInfo;
         this.systemUtilization = systemUtilization;
