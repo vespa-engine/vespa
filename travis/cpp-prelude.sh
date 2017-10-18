d7d34517898e037e2fc229e37b285576545685b9
Bjørn Christian Seime <bjorncs@oath.com>
2017-10-18 14:28:54 +0200

    Configure ccache through cli interface
    It looks like the Travis-CI VM contains some configuration in its .ccache
    directory that overrides whatever we configures through ccache
    environment variables.

--- travis/cpp-prelude.sh
+++ travis/cpp-prelude.sh
@@ -2,6 +2,6 @@
 # Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 set -e
 
-export CCACHE_MAXSIZE="1250M"
-export CCACHE_COMPRESS=1
+ccache --max-size=1250M
+ccache --set-config=compression=true
 ccache --print-config
