81a24b587e6271fb9275a8333bf4b7ce3172f4a3
Håvard Pettersen <havardpe@oath.com>
2017-10-20 13:34:53 +0000

    use static functions for low-level operation eval

--- eval/src/vespa/eval/eval/operation.cpp
+++ eval/src/vespa/eval/eval/operation.cpp
@@ -69,6 +69,10 @@ template <typename T> void Op1<T>::accept(OperationVisitor &visitor) const {
     visitor.visit(static_cast<const T&>(*this));
 }
 
+template <typename T> double Op1<T>::eval(double a) const {
+    return T::f(a);
+}
+
 template <typename T> void Op2<T>::accept(OperationVisitor &visitor) const {
     visitor.visit(static_cast<const T&>(*this));
 }
@@ -77,48 +81,93 @@ template <typename T> std::unique_ptr<BinaryOperation> Op2<T>::clone() const {
     return std::make_unique<T>();
 }
 
+template <typename T> double Op2<T>::eval(double a, double b) const {
+    return T::f(a, b);
+}
+
 namespace operation {
-double Neg::eval(double a) const { return -a; }
-double Not::eval(double a) const { return (a != 0.0) ? 0.0 : 1.0; }
-double Add::eval(double a, double b) const { return (a + b); }
-double Sub::eval(double a, double b) const { return (a - b); }
-double Mul::eval(double a, double b) const { return (a * b); }
-double Div::eval(double a, double b) const { return (a / b); }
-double Mod::eval(double a, double b) const { return std::fmod(a, b); }
-double Pow::eval(double a, double b) const { return std::pow(a, b); }
-double Equal::eval(double a, double b) const { return (a == b) ? 1.0 : 0.0; }
-double NotEqual::eval(double a, double b) const { return (a != b) ? 1.0 : 0.0; }
-double Approx::eval(double a, double b) const { return approx_equal(a, b); }
-double Less::eval(double a, double b) const { return (a < b) ? 1.0 : 0.0; }
-double LessEqual::eval(double a, double b) const { return (a <= b) ? 1.0 : 0.0; }
-double Greater::eval(double a, double b) const { return (a > b) ? 1.0 : 0.0; }
-double GreaterEqual::eval(double a, double b) const { return (a >= b) ? 1.0 : 0.0; }
-double And::eval(double a, double b) const { return ((a != 0.0) && (b != 0.0)) ? 1.0 : 0.0; }
-double Or::eval(double a, double b) const { return ((a != 0.0) || (b != 0.0)) ? 1.0 : 0.0; }
-double Cos::eval(double a) const { return std::cos(a); }
-double Sin::eval(double a) const { return std::sin(a); }
-double Tan::eval(double a) const { return std::tan(a); }
-double Cosh::eval(double a) const { return std::cosh(a); }
-double Sinh::eval(double a) const { return std::sinh(a); }
-double Tanh::eval(double a) const { return std::tanh(a); }
-double Acos::eval(double a) const { return std::acos(a); }
-double Asin::eval(double a) const { return std::asin(a); }
-double Atan::eval(double a) const { return std::atan(a); }
-double Exp::eval(double a) const { return std::exp(a); }
-double Log10::eval(double a) const { return std::log10(a); }
-double Log::eval(double a) const { return std::log(a); }
-double Sqrt::eval(double a) const { return std::sqrt(a); }
-double Ceil::eval(double a) const { return std::ceil(a); }
-double Fabs::eval(double a) const { return std::fabs(a); }
-double Floor::eval(double a) const { return std::floor(a); }
-double Atan2::eval(double a, double b) const { return std::atan2(a, b); }
-double Ldexp::eval(double a, double b) const { return std::ldexp(a, b); }
-double Min::eval(double a, double b) const { return std::min(a, b); }
-double Max::eval(double a, double b) const { return std::max(a, b); }
-double IsNan::eval(double a) const { return std::isnan(a) ? 1.0 : 0.0; }
-double Relu::eval(double a) const { return std::max(a, 0.0); }
-double Sigmoid::eval(double a) const { return 1.0 / (1.0 + std::exp(-1.0 * a)); }
+double Neg::f(double a) { return -a; }
+double Not::f(double a) { return (a != 0.0) ? 0.0 : 1.0; }
+double Add::f(double a, double b) { return (a + b); }
+double Sub::f(double a, double b) { return (a - b); }
+double Mul::f(double a, double b) { return (a * b); }
+double Div::f(double a, double b) { return (a / b); }
+double Mod::f(double a, double b) { return std::fmod(a, b); }
+double Pow::f(double a, double b) { return std::pow(a, b); }
+double Equal::f(double a, double b) { return (a == b) ? 1.0 : 0.0; }
+double NotEqual::f(double a, double b) { return (a != b) ? 1.0 : 0.0; }
+double Approx::f(double a, double b) { return approx_equal(a, b); }
+double Less::f(double a, double b) { return (a < b) ? 1.0 : 0.0; }
+double LessEqual::f(double a, double b) { return (a <= b) ? 1.0 : 0.0; }
+double Greater::f(double a, double b) { return (a > b) ? 1.0 : 0.0; }
+double GreaterEqual::f(double a, double b) { return (a >= b) ? 1.0 : 0.0; }
+double And::f(double a, double b) { return ((a != 0.0) && (b != 0.0)) ? 1.0 : 0.0; }
+double Or::f(double a, double b) { return ((a != 0.0) || (b != 0.0)) ? 1.0 : 0.0; }
+double Cos::f(double a) { return std::cos(a); }
+double Sin::f(double a) { return std::sin(a); }
+double Tan::f(double a) { return std::tan(a); }
+double Cosh::f(double a) { return std::cosh(a); }
+double Sinh::f(double a) { return std::sinh(a); }
+double Tanh::f(double a) { return std::tanh(a); }
+double Acos::f(double a) { return std::acos(a); }
+double Asin::f(double a) { return std::asin(a); }
+double Atan::f(double a) { return std::atan(a); }
+double Exp::f(double a) { return std::exp(a); }
+double Log10::f(double a) { return std::log10(a); }
+double Log::f(double a) { return std::log(a); }
+double Sqrt::f(double a) { return std::sqrt(a); }
+double Ceil::f(double a) { return std::ceil(a); }
+double Fabs::f(double a) { return std::fabs(a); }
+double Floor::f(double a) { return std::floor(a); }
+double Atan2::f(double a, double b) { return std::atan2(a, b); }
+double Ldexp::f(double a, double b) { return std::ldexp(a, b); }
+double Min::f(double a, double b) { return std::min(a, b); }
+double Max::f(double a, double b) { return std::max(a, b); }
+double IsNan::f(double a) { return std::isnan(a) ? 1.0 : 0.0; }
+double Relu::f(double a) { return std::max(a, 0.0); }
+double Sigmoid::f(double a) { return 1.0 / (1.0 + std::exp(-1.0 * a)); }
 } // namespace vespalib::eval::operation
+
+template struct Op1<operation::Neg>;
+template struct Op1<operation::Not>;
+template struct Op2<operation::Add>;
+template struct Op2<operation::Sub>;
+template struct Op2<operation::Mul>;
+template struct Op2<operation::Div>;
+template struct Op2<operation::Mod>;
+template struct Op2<operation::Pow>;
+template struct Op2<operation::Equal>;
+template struct Op2<operation::NotEqual>;
+template struct Op2<operation::Approx>;
+template struct Op2<operation::Less>;
+template struct Op2<operation::LessEqual>;
+template struct Op2<operation::Greater>;
+template struct Op2<operation::GreaterEqual>;
+template struct Op2<operation::And>;
+template struct Op2<operation::Or>;
+template struct Op1<operation::Cos>;
+template struct Op1<operation::Sin>;
+template struct Op1<operation::Tan>;
+template struct Op1<operation::Cosh>;
+template struct Op1<operation::Sinh>;
+template struct Op1<operation::Tanh>;
+template struct Op1<operation::Acos>;
+template struct Op1<operation::Asin>;
+template struct Op1<operation::Atan>;
+template struct Op1<operation::Exp>;
+template struct Op1<operation::Log10>;
+template struct Op1<operation::Log>;
+template struct Op1<operation::Sqrt>;
+template struct Op1<operation::Ceil>;
+template struct Op1<operation::Fabs>;
+template struct Op1<operation::Floor>;
+template struct Op2<operation::Atan2>;
+template struct Op2<operation::Ldexp>;
+template struct Op2<operation::Min>;
+template struct Op2<operation::Max>;
+template struct Op1<operation::IsNan>;
+template struct Op1<operation::Relu>;
+template struct Op1<operation::Sigmoid>;
 
 } // namespace vespalib::eval
 } // namespace vespalib
