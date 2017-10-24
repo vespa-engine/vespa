4b617b430590a67ffc6f4c01bc061c63f6d2d7b3
Morten Tokle <mortent@oath.com>
2017-10-24 13:36:22 +0200

    Update copyright

--- container-core/src/main/java/com/yahoo/container/core/identity/package-info.java
+++ container-core/src/main/java/com/yahoo/container/core/identity/package-info.java
@@ -1,6 +1,4 @@
-/**
- * @author mortent
- */
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
 package com.yahoo.container.core.identity;
 
