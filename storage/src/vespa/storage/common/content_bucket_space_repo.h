2098113d6785eef82ec31c3473b92b80fb630c9e
Geir Storli <geirst@oath.com>
2017-11-02 12:22:05 +0000

    Add repo for all bucket spaces (with associated bucket databases) on a content node.

---
+++ storage/src/vespa/storage/common/content_bucket_space_repo.h
@@ -0,0 +1,24 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+#pragma once
+
+#include "content_bucket_space.h"
+#include <vespa/document/bucket/bucketspace.h>
+#include <unordered_map>
+
+namespace storage {
+
+/**
+ * Class managing the set of bucket spaces (with associated bucket databases) on a content node.
+ */
+class ContentBucketSpaceRepo {
+private:
+    using BucketSpaceMap = std::unordered_map<document::BucketSpace, ContentBucketSpace::UP, document::BucketSpace::hash>;
+
+    BucketSpaceMap _map;
+
+public:
+    ContentBucketSpaceRepo();
+    ContentBucketSpace *get(document::BucketSpace bucketSpace) const;
+};
+
+}
