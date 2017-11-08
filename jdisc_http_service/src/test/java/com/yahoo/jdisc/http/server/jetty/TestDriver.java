85130776922d3c1d72bf720c4fc5ad32fc7bb2da
Bjørn Christian Seime <bjorncs@oath.com>
2017-11-08 16:41:50 +0100

    Move JKSKeyStore to separate package

--- jdisc_http_service/src/test/java/com/yahoo/jdisc/http/server/jetty/TestDriver.java
+++ jdisc_http_service/src/test/java/com/yahoo/jdisc/http/server/jetty/TestDriver.java
@@ -6,7 +6,7 @@ import com.google.inject.Module;
 import com.yahoo.jdisc.application.ContainerBuilder;
 import com.yahoo.jdisc.handler.RequestHandler;
 import com.yahoo.jdisc.http.ConnectorConfig;
-import com.yahoo.jdisc.http.ssl.JKSKeyStore;
+import com.yahoo.jdisc.http.ssl.jks.JKSKeyStore;
 import com.yahoo.jdisc.http.test.SslContextFactory;
 import com.yahoo.jdisc.http.ssl.SslKeyStore;
 
