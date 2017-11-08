052db46020b4cf2f3d9836561c71017b203a6251
Henning Baldersheim <balder@yahoo-inc.com>
2017-11-08 11:23:08 +0100

    Add a simple Receiver interface so that we can keep the RPC stuff one place and make the FileServer more testable.

--- configserver/src/test/java/com/yahoo/vespa/config/server/filedistribution/FileServerTest.java
+++ configserver/src/test/java/com/yahoo/vespa/config/server/filedistribution/FileServerTest.java
@@ -1,5 +1,6 @@
 package com.yahoo.vespa.config.server.filedistribution;
 
+import com.yahoo.config.FileReference;
 import com.yahoo.io.IOUtils;
 import org.junit.Test;
 
@@ -7,7 +8,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
@@ -39,6 +43,26 @@ public class FileServerTest {
         assertFalse(fs.hasFile("12x"));
         createCleanDir("12x");
         assertFalse(fs.hasFile("12x"));
+        cleanup();
+    }
+
+    private static class FileReceiver implements FileServer.Receiver {
+        CompletableFuture<byte []> content;
+        FileReceiver(CompletableFuture<byte []> content) {
+            this.content = content;
+        }
+        @Override
+        public void receive(FileReference reference, String filename, byte[] content, FileServer.ReplayStatus status) {
+            this.content.complete(content);
+        }
+    }
+    @Test
+    public void requireThatWeCanReplayFile() throws IOException, InterruptedException, ExecutionException {
+        createCleanDir("12y");
+        IOUtils.writeFile("12y/f1", "dummy-data", true);
+        CompletableFuture<byte []> content = new CompletableFuture<>();
+        fs.startFileServing("12y", new FileReceiver(content));
+        assertEquals(new String(content.get()), "dummy-data");
         cleanup();
     }
 
