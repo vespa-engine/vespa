8774fbb9f192c54a7758e44fd7eeb0f4df9991a6
Harald Musum <musum@oath.com>
2017-11-09 12:55:33 +0100

    Move to correct package

--- filedistribution/src/main/java/com/yahoo/vespa/filedistribution/FileDownloader.java
+++ filedistribution/src/main/java/com/yahoo/vespa/filedistribution/FileDownloader.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.config.proxy.filedistribution;
+package com.yahoo.vespa.filedistribution;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
