b7c9dd831f2505791bba339a51a029b2172350af
Henning Baldersheim <balder@yahoo-inc.com>
2017-11-10 13:26:26 +0100

    Revert "Revert "Wire in deployment so that we can copy the file in when the old way i…""

---
+++ configserver/src/main/java/com/yahoo/vespa/config/server/filedistribution/AddFileInterface.java
@@ -0,0 +1,9 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.config.server.filedistribution;
+
+import com.yahoo.config.FileReference;
+
+public interface AddFileInterface {
+    FileReference addFile(String relativePath);
+    FileReference addFile(String relativePath, FileReference reference);
+}
