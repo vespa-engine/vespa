c0b972c26f5e3b201c363928197ca81b694552b2
Bjørn Christian Seime <bjorncs@oath.com>
2017-11-14 10:44:23 +0100

    Move JksKeyStore to test source directory

--- jdisc_http_service/src/test/java/com/yahoo/jdisc/http/JksKeyStore.java
+++ jdisc_http_service/src/test/java/com/yahoo/jdisc/http/JksKeyStore.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.jdisc.http.ssl.jks;
+package com.yahoo.jdisc.http;
 
 import com.yahoo.jdisc.http.ssl.SslKeyStore;
 
