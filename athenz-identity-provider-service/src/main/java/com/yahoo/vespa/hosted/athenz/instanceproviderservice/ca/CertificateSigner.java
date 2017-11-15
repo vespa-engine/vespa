17254344dd9b6c2018896322f067b4d0ae1818ae
Valerij Fredriksen <valerijf@oath.com>
2017-11-15 13:55:23 +0100

    Remove unnecessary JcaPKCS10CertificationRequest

--- athenz-identity-provider-service/src/main/java/com/yahoo/vespa/hosted/athenz/instanceproviderservice/ca/CertificateSigner.java
+++ athenz-identity-provider-service/src/main/java/com/yahoo/vespa/hosted/athenz/instanceproviderservice/ca/CertificateSigner.java
@@ -15,17 +15,13 @@ import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.Extensions;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
-import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest;
 
 import java.math.BigInteger;
 import java.security.PrivateKey;
-import java.security.Provider;
-import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 import java.time.Clock;
 import java.time.Duration;
@@ -89,9 +85,8 @@ public class CertificateSigner {
         Date notAfter = Date.from(clock.instant().plus(CERTIFICATE_EXPIRATION));
 
         try {
-            PublicKey publicKey = new JcaPKCS10CertificationRequest(certReq).getPublicKey();
-            X509v3CertificateBuilder caBuilder = new JcaX509v3CertificateBuilder(
-                    issuer, BigInteger.valueOf(clock.millis()), notBefore, notAfter, certReq.getSubject(), publicKey)
+            X509v3CertificateBuilder caBuilder = new X509v3CertificateBuilder(issuer, BigInteger.valueOf(clock.millis()),
+                    notBefore, notAfter, certReq.getSubject(), certReq.getSubjectPublicKeyInfo())
 
                     // Set Basic Constraints to false
                     .addExtension(Extension.basicConstraints, true, new BasicConstraints(false));
