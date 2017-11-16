b07e68d6e9a0220136e0f33506ffc37864ed0a3b
Harald Musum <musum@yahoo-inc.com>
2017-11-16 18:05:30 +0100

    Revert "Revert "Hmusum/move new filedistribution classes""

--- filedistribution/src/test/java/com/yahoo/vespa/filedistribution/FileDownloaderTest.java
+++ filedistribution/src/test/java/com/yahoo/vespa/filedistribution/FileDownloaderTest.java
@@ -1,4 +1,6 @@
-package com.yahoo.vespa.config.proxy.filedistribution;
+//  Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+package com.yahoo.vespa.filedistribution;
 
 import com.yahoo.config.FileReference;
 import com.yahoo.io.IOUtils;
