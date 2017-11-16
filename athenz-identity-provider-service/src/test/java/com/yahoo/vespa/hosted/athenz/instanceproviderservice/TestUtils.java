6d0a38f454795f4be2945fa4c7213f2a02243805
Bjørn Christian Seime <bjorncs@oath.com>
2017-11-16 13:49:25 +0100

    Remove AthenzInstanceProviderService and related classes

---
+++ athenz-identity-provider-service/src/test/java/com/yahoo/vespa/hosted/athenz/instanceproviderservice/TestUtils.java
@@ -0,0 +1,35 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.athenz.instanceproviderservice;
+
+import com.google.common.collect.ImmutableMap;
+import com.yahoo.config.provision.Zone;
+import com.yahoo.vespa.hosted.athenz.instanceproviderservice.config.AthenzProviderServiceConfig;
+
+/**
+ * @author bjorncs
+ */
+public class TestUtils {
+
+    private static final int PORT = 12345;
+
+    public static AthenzProviderServiceConfig getAthenzProviderConfig(String domain,
+                                                                      String service,
+                                                                      String dnsSuffix,
+                                                                      Zone zone) {
+        AthenzProviderServiceConfig.Zones.Builder zoneConfig =
+                new AthenzProviderServiceConfig.Zones.Builder()
+                        .serviceName(service)
+                        .secretVersion(0)
+                        .domain(domain)
+                        .secretName("s3cr3t");
+        return new AthenzProviderServiceConfig(
+                new AthenzProviderServiceConfig.Builder()
+                        .zones(ImmutableMap.of(zone.environment().value() + "." + zone.region().value(), zoneConfig))
+                        .port(PORT)
+                        .certDnsSuffix(dnsSuffix)
+                        .ztsUrl("localhost/zts")
+                        .athenzPrincipalHeaderName("Athenz-Principal-Auth")
+                        .apiPath(""));
+    }
+
+}
