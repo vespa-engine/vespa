9efd85f896acc012c5a670b3b89a9bd5608b05c1
Jon Marius Venstad <jvenstad@yahoo-inc.com>
2017-11-17 11:18:21 +0100

    Removed redundant code

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/deployment/DeploymentTrigger.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/deployment/DeploymentTrigger.java
@@ -363,18 +363,12 @@ public class DeploymentTrigger {
      * downgrade production nodes which we are not guaranteed to support.
      */
     private boolean isOnNewerVersionInProductionThan(Version version, Application application, JobType job) {
-        if ( ! isProduction(job)) return false;
+        if (job.isProduction()) return false;
         Optional<Zone> zone = job.zone(controller.system());
         if ( ! zone.isPresent()) return false;
         Deployment existingDeployment = application.deployments().get(zone.get());
         if (existingDeployment == null) return false;
         return existingDeployment.version().isAfter(version);
-    }
-    
-    private boolean isProduction(JobType job) {
-        Optional<Zone> zone = job.zone(controller.system());
-        if ( ! zone.isPresent()) return false; // arbitrary
-        return zone.get().environment() == Environment.prod;
     }
     
     private boolean acceptNewRevisionNow(LockedApplication application) {
