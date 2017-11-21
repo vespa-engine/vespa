1c0db7b54de5661b59d9aafbd8d32b27c670a1bd
Arne Juul <arnej@yahoo-inc.com>
2017-11-21 08:51:18 +0000

    also implement MetricsProducer for state API

---
+++ staging_vespalib/src/vespa/vespalib/metrics/producer.cpp
@@ -0,0 +1,32 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include "producer.h"
+#include "metrics_collector.h"
+#include "json_formatter.h"
+
+namespace vespalib {
+namespace metrics {
+
+Producer::Producer(std::shared_ptr<MetricsCollector> m)
+    : _manager(m)
+{}
+
+vespalib::string
+Producer::getMetrics(const vespalib::string &)
+{
+    Snapshot snap = _manager->snapshot();
+    JsonFormatter fmt(snap);
+    return fmt.asString();
+}
+
+vespalib::string
+Producer::getTotalMetrics(const vespalib::string &)
+{
+    // not implemented
+    return "";
+}
+
+
+
+} // namespace vespalib::metrics
+} // namespace vespalib
---
+++ staging_vespalib/src/vespa/vespalib/metrics/producer.h
@@ -0,0 +1,22 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+#pragma once
+
+#include <memory>
+#include <vespa/vespalib/net/metrics_producer.h>
+
+namespace vespalib {
+namespace metrics {
+
+class MetricsCollector;
+
+class Producer : public vespalib::MetricsProducer {
+private:
+    std::shared_ptr<MetricsCollector> _manager;
+public:
+    Producer(std::shared_ptr<MetricsCollector> m);
+    vespalib::string getMetrics(const vespalib::string &consumer) override;
+    vespalib::string getTotalMetrics(const vespalib::string &consumer) override;
+};
+
+} // namespace vespalib::metrics
+} // namespace vespalib
