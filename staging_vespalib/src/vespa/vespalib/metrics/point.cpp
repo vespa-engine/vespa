901ced337cf001eb4254535eeeeeab2a1294b606
Arne Juul <arnej@yahoo-inc.com>
2017-11-23 13:56:35 +0000

    add static empty point

--- staging_vespalib/src/vespa/vespalib/metrics/point.cpp
+++ staging_vespalib/src/vespa/vespalib/metrics/point.cpp
@@ -1,2 +1,10 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #include "point.h"
+
+namespace vespalib {
+namespace metrics {
+
+Point Point::empty(0);
+
+} // namespace vespalib::metrics
+} // namespace vespalib
