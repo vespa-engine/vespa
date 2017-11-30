133a4c7b51a33e329d8805adf3028afdc888eabe
Håvard Pettersen <havardpe@oath.com>
2017-11-30 15:21:06 +0000

    replace appropriate sub-expressions with dense xw product

--- eval/src/vespa/eval/tensor/dense/dense_tensor_function_compiler.cpp
+++ eval/src/vespa/eval/tensor/dense/dense_tensor_function_compiler.cpp
@@ -1,6 +1,7 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "dense_dot_product_function.h"
+#include "dense_xw_product_function.h"
 #include "dense_tensor_function_compiler.h"
 #include <vespa/eval/eval/operation.h>
 #include <vespa/vespalib/test/insertion_operators.h>
@@ -15,39 +16,64 @@ namespace tensor {
 
 namespace {
 
-bool
-willReduceAllDimensions(const std::vector<vespalib::string> &dimensions)
-{
-    return (dimensions.empty() || (dimensions.size() == 1));
-}
-
-bool
-is1dDenseTensor(const ValueType &type)
-{
+bool is1dDenseTensor(const ValueType &type) {
     return (type.is_dense() && (type.dimensions().size() == 1));
 }
 
-bool
-isCompatibleTensorsForDotProduct(const ValueType &lhsType, const ValueType &rhsType)
-{
-    return (is1dDenseTensor(lhsType) &&
+bool isConcreteDenseTensor(const ValueType &type, size_t d) {
+    return (type.is_dense() && (type.dimensions().size() == d) && !type.is_abstract());
+}
+
+bool isDenseDotProduct(const ValueType &res, const ValueType &lhsType, const ValueType &rhsType) {
+    return (res.is_double() &&
+            is1dDenseTensor(lhsType) &&
             is1dDenseTensor(rhsType) &&
             (lhsType.dimensions()[0].name == rhsType.dimensions()[0].name));
 }
 
-struct DotProductFunctionCompiler
+bool isDenseXWProduct(const ValueType &res, const ValueType &vec, const ValueType &mat) {
+    if (isConcreteDenseTensor(res, 1) &&
+        isConcreteDenseTensor(vec, 1) &&
+        isConcreteDenseTensor(mat, 2))
+    {
+        size_t res_idx = mat.dimension_index(res.dimensions()[0].name);
+        size_t vec_idx = mat.dimension_index(vec.dimensions()[0].name);
+        size_t npos = ValueType::Dimension::npos;
+        if ((res_idx != npos) && (vec_idx != npos) && (res_idx != vec_idx)) {
+            return ((mat.dimensions()[res_idx].size == res.dimensions()[0].size) &&
+                    (mat.dimensions()[vec_idx].size == vec.dimensions()[0].size));
+        }
+    }
+    return false;
+}
+
+const TensorFunction &createDenseXWProduct(const ValueType &res, const Inject &vec, const Inject &mat, Stash &stash) {
+    bool common_is_inner = (mat.result_type.dimension_index(vec.result_type.dimensions()[0].name) == 1);
+    return stash.create<DenseXWProductFunction>(res, vec.tensor_id, mat.tensor_id,
+                                                vec.result_type.dimensions()[0].size,
+                                                res.dimensions()[0].size,
+                                                common_is_inner);
+}
+
+struct InnerProductFunctionCompiler
 {
     static const TensorFunction &compile(const Node &expr, Stash &stash) {
         const Reduce *reduce = as<Reduce>(expr);
-        if (reduce && (reduce->aggr == Aggr::SUM) && willReduceAllDimensions(reduce->dimensions)) {
+        if (reduce && (reduce->aggr == Aggr::SUM)) {
             const Join *join = as<Join>(reduce->tensor);
             if (join && (join->function == Mul::f)) {
-                const Inject *lhsTensor = as<Inject>(join->lhs_tensor);
-                const Inject *rhsTensor = as<Inject>(join->rhs_tensor);
-                if (lhsTensor && rhsTensor &&
-                    isCompatibleTensorsForDotProduct(lhsTensor->result_type, rhsTensor->result_type))
-                {
-                    return stash.create<DenseDotProductFunction>(lhsTensor->tensor_id, rhsTensor->tensor_id);
+                const Inject *lhs = as<Inject>(join->lhs_tensor);
+                const Inject *rhs = as<Inject>(join->rhs_tensor);
+                if (lhs && rhs) {
+                    if (isDenseDotProduct(expr.result_type, lhs->result_type, rhs->result_type)) {
+                        return stash.create<DenseDotProductFunction>(lhs->tensor_id, rhs->tensor_id);
+                    }
+                    if (isDenseXWProduct(expr.result_type, lhs->result_type, rhs->result_type)) {
+                        return createDenseXWProduct(expr.result_type, *lhs, *rhs, stash);
+                    }
+                    if (isDenseXWProduct(expr.result_type, rhs->result_type, lhs->result_type)) {
+                        return createDenseXWProduct(expr.result_type, *rhs, *lhs, stash);
+                    }
                 }
             }
         }
@@ -60,7 +86,7 @@ struct DotProductFunctionCompiler
 const TensorFunction &
 DenseTensorFunctionCompiler::compile(const eval::tensor_function::Node &expr, Stash &stash)
 {
-    return DotProductFunctionCompiler::compile(expr, stash);    
+    return InnerProductFunctionCompiler::compile(expr, stash);
 }
 
 } // namespace tensor
