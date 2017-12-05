6cd083305fbb31158bb24cc0bc5ac8dcbb43cefb
Bjørn Christian Seime <bjorncs@oath.com>
2017-12-05 14:27:21 +0100

    Fix invalid id bug introduced after recent refactoring

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/athenz/impl/ZmsClientImpl.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/athenz/impl/ZmsClientImpl.java
@@ -66,11 +66,11 @@ public class ZmsClientImpl implements ZmsClient {
         List<TenantRoleAction> tenantRoleActions = createTenantRoleActions();
         log("putProviderResourceGroupRoles(" +
                         "tenantDomain=%s, providerDomain=%s, service=%s, resourceGroup=%s, roleActions=%s)",
-                tenantDomain, service.getDomain().id(), service.getFullName(), applicationName, tenantRoleActions);
+                tenantDomain, service.getDomain().id(), service.getName(), applicationName, tenantRoleActions);
         runOrThrow(() -> {
             ProviderResourceGroupRoles resourceGroupRoles = new ProviderResourceGroupRoles()
                     .setDomain(service.getDomain().id())
-                    .setService(service.getFullName())
+                    .setService(service.getName())
                     .setTenant(tenantDomain.id())
                     .setResourceGroup(applicationName.id())
                     .setRoles(tenantRoleActions);
