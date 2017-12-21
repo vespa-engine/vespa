d5f215d88e52c3c7a45ad35411e3d8fc71ea89a5
Henning Baldersheim <balder@yahoo-inc.com>
2017-12-21 10:41:16 +0000

    Use cheap and_modulator instead of prime modulator to avoid costly division.

--- eval/src/vespa/eval/tensor/sparse/sparse_tensor.cpp
+++ eval/src/vespa/eval/tensor/sparse/sparse_tensor.cpp
@@ -188,4 +188,5 @@ SparseTensor::reduce(join_fun_t op,
 
 }
 
-VESPALIB_HASH_MAP_INSTANTIATE(vespalib::tensor::SparseTensorAddressRef, double);
+VESPALIB_HASH_MAP_INSTANTIATE_H_E_M(vespalib::tensor::SparseTensorAddressRef, double, vespalib::hash<vespalib::tensor::SparseTensorAddressRef>,
+        std::equal_to<vespalib::tensor::SparseTensorAddressRef>, vespalib::hashtable_base::and_modulator);
