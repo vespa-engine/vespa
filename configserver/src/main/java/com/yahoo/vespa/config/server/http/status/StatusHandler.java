6f71863a4dfc3d7eefd561482c2ff14c7ad4eb9d
Harald Musum <musum@oath.com>
2018-01-02 17:54:41 +0100

    Rewrite status handler
    Rewrite and simplify to use Slime and a regular handler instead of Jersey,
    so it's not necessary to manually update handler when config changes
    (which had not been done in years)

---
+++ configserver/src/main/java/com/yahoo/vespa/config/server/http/status/StatusHandler.java
@@ -0,0 +1,69 @@
+package com.yahoo.vespa.config.server.http.status;
+
+import com.google.inject.Inject;
+import com.yahoo.cloud.config.ConfigserverConfig;
+import com.yahoo.config.model.api.ModelFactory;
+import com.yahoo.config.provision.Version;
+import com.yahoo.container.jdisc.HttpRequest;
+import com.yahoo.container.jdisc.HttpResponse;
+import com.yahoo.slime.Cursor;
+import com.yahoo.slime.JsonFormat;
+import com.yahoo.vespa.config.ConfigPayload;
+import com.yahoo.vespa.config.SlimeUtils;
+import com.yahoo.vespa.config.server.GlobalComponentRegistry;
+import com.yahoo.vespa.config.server.http.HttpConfigResponse;
+import com.yahoo.vespa.config.server.http.HttpHandler;
+import com.yahoo.vespa.config.server.http.JSONResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.yahoo.jdisc.http.HttpResponse.Status.OK;
+
+
+public class StatusHandler extends HttpHandler {
+
+    private final ConfigserverConfig config;
+    private final List<String> modelVersions;
+
+    @Inject
+    public StatusHandler(Context ctx, GlobalComponentRegistry componentRegistry) {
+        super(ctx);
+        this.config = componentRegistry.getConfigserverConfig();
+        this.modelVersions = componentRegistry.getModelFactoryRegistry().getFactories().stream()
+                .map(ModelFactory::getVersion)
+                .map(Version::toString)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public HttpResponse handleGET(HttpRequest req) {
+        return new StatusResponse(OK, config, modelVersions);
+    }
+
+    private static class StatusResponse extends JSONResponse {
+
+        StatusResponse(int status, ConfigserverConfig config, List<String> modelVersions) {
+            super(status);
+
+            Cursor configCursor = object.setObject("configserverConfig");
+            SlimeUtils.copyObject(ConfigPayload.fromInstance(config).getSlime().get(), configCursor);
+
+            Cursor modelVersionsCursor = object.setArray("modelVersions");
+            modelVersions.forEach(modelVersionsCursor::addString);
+        }
+
+        @Override
+        public void render(OutputStream outputStream) throws IOException {
+            new JsonFormat(true).encode(outputStream, object);
+        }
+
+        @Override
+        public String getContentType() {
+            return HttpConfigResponse.JSON_CONTENT_TYPE;
+        }
+    }
+
+}
---
+++ configserver/src/test/java/com/yahoo/vespa/config/server/http/status/StatusHandlerTest.java
@@ -0,0 +1,40 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.config.server.http.status;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yahoo.cloud.config.ConfigserverConfig;
+import com.yahoo.container.jdisc.HttpRequest;
+import com.yahoo.container.jdisc.HttpResponse;
+import com.yahoo.vespa.config.server.TestComponentRegistry;
+import com.yahoo.vespa.config.server.http.SessionHandlerTest;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.yahoo.jdisc.http.HttpRequest.Method.GET;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author hmusum
+ */
+public class StatusHandlerTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+
+    @Test
+    public void require_that_handler_works() throws IOException {
+        TestComponentRegistry componentRegistry = new TestComponentRegistry.Builder().build();
+        StatusHandler handler = new StatusHandler(StatusHandler.testOnlyContext(), componentRegistry);
+
+        HttpResponse response = handler.handle(HttpRequest.createTestRequest("/status", GET));
+        JsonNode jsonNode = mapper.readTree(SessionHandlerTest.getRenderedString(response));
+
+        ConfigserverConfig expectedConfig = componentRegistry.getConfigserverConfig();
+        assertEquals(expectedConfig.rpcport(), jsonNode.get("configserverConfig").get("rpcport").asInt());
+        assertEquals(expectedConfig.applicationDirectory(), jsonNode.get("configserverConfig").get("applicationDirectory").asText());
+
+        assertEquals("6.10.0", jsonNode.get("modelVersions").get(0).asText());
+    }
+
+}
