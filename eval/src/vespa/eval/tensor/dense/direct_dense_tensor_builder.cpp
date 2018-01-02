31d32f92159949e18c121865240f447a8a32ba84
Henning Baldersheim <balder@yahoo-inc.com>
2018-01-02 03:06:22 +0100

    Inline simple method called in innerloop.

--- eval/src/vespa/eval/tensor/dense/direct_dense_tensor_builder.cpp
+++ eval/src/vespa/eval/tensor/dense/direct_dense_tensor_builder.cpp
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "direct_dense_tensor_builder.h"
-#include <cassert>
 
 namespace vespalib::tensor {
 
@@ -20,18 +19,6 @@ calculateCellsSize(const ValueType &type)
     return cellsSize;
 }
 
-size_t
-calculateCellAddress(const Address &address, const ValueType &type)
-{
-    assert(address.size() == type.dimensions().size());
-    size_t result = 0;
-    for (size_t i = 0; i < address.size(); ++i) {
-        result *= type.dimensions()[i].size;
-        result += address[i];
-    }
-    return result;
-}
-
 }
 
 DirectDenseTensorBuilder::~DirectDenseTensorBuilder() = default;
@@ -40,14 +27,6 @@ DirectDenseTensorBuilder::DirectDenseTensorBuilder(const ValueType &type_in)
     : _type(type_in),
       _cells(calculateCellsSize(_type))
 {
-}
-
-void
-DirectDenseTensorBuilder::insertCell(const Address &address, double cellValue)
-{
-    size_t cellAddress = calculateCellAddress(address, _type);
-    assert(cellAddress < _cells.size());
-    _cells[cellAddress] = cellValue;
 }
 
 Tensor::UP
--- eval/src/vespa/eval/tensor/dense/direct_dense_tensor_builder.h
+++ eval/src/vespa/eval/tensor/dense/direct_dense_tensor_builder.h
@@ -19,10 +19,20 @@ private:
     eval::ValueType _type;
     Cells _cells;
 
+    static size_t calculateCellAddress(const Address &address, const eval::ValueType &type) {
+        size_t result = 0;
+        for (size_t i = 0; i < address.size(); ++i) {
+            result *= type.dimensions()[i].size;
+            result += address[i];
+        }
+        return result;
+    }
 public:
     DirectDenseTensorBuilder(const eval::ValueType &type_in);
     ~DirectDenseTensorBuilder();
-    void insertCell(const Address &address, double cellValue);
+    void insertCell(const Address &address, double cellValue) {
+        _cells[calculateCellAddress(address, _type)] = cellValue;
+    }
     Tensor::UP build();
 };
 
