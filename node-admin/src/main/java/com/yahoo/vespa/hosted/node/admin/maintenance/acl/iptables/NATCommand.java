eceda7755cf390e6b7032ee8e36bd5d75fe8ee47
toby <smorgrav@yahoo-inc.com>
2018-01-05 14:37:48 +0100

    Move license notice on top

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/acl/iptables/NATCommand.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/acl/iptables/NATCommand.java
@@ -1,4 +1,5 @@
-package com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables;// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.node.admin.maintenance.acl.iptables;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
