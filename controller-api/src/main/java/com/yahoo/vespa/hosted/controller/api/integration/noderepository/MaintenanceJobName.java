68352cb9b78c9a471d4a63e75a1c270c83ff1406
Bjørn Christian Seime <bjorncs@oath.com>
2018-01-08 14:50:21 +0100

    Open-source NodeRepositoryClient and ProvisionResource

---
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/noderepository/MaintenanceJobName.java
@@ -0,0 +1,11 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.controller.api.integration.noderepository;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MaintenanceJobName {
+    @JsonProperty("name")
+    public String name;
+}
