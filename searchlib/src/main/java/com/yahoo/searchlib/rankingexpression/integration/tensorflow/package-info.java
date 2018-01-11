38dd96ffc6f24e5a138850bffccbfb4a79d74801
Jon Bratseth <bratseth@yahoo-inc.com>
2018-01-11 15:22:23 +0100

    Javadoc

--- searchlib/src/main/java/com/yahoo/searchlib/rankingexpression/integration/tensorflow/package-info.java
+++ searchlib/src/main/java/com/yahoo/searchlib/rankingexpression/integration/tensorflow/package-info.java
@@ -1,6 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 /**
- * Execution engine for ranking expressions
+ * Tensorflow integration
  */
 @ExportPackage
 package com.yahoo.searchlib.rankingexpression.integration.tensorflow;
