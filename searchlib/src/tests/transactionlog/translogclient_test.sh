128ea9157d8374d493cdef3059abad1dbfbc11be
Henning Baldersheim <balder@yahoo-inc.com>
2018-01-12 14:25:10 +0100

    Revert accidental commit.

--- searchlib/src/tests/transactionlog/translogclient_test.sh
+++ searchlib/src/tests/transactionlog/translogclient_test.sh
@@ -3,3 +3,4 @@
 set -e
 rm -rf test7 test8 test9 test10 test11 test12 test13 testremove
 $VALGRIND ./searchlib_translogclient_test_app
+rm -rf test7 test8 test9 test10 test11 test12 test13 testremove
