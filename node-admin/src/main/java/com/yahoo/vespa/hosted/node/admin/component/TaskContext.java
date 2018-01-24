8ad1b3e52610a3102c76042dd2459112a6782873
Håkon Hallingstad <hakon@oath.com>
2018-01-24 21:02:23 +0100

    Remove ProcessApi

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/component/TaskContext.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/component/TaskContext.java
@@ -1,8 +1,6 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.component;
 
-import com.yahoo.vespa.hosted.node.admin.task.util.process.ProcessApi;
-
 import java.nio.file.FileSystem;
 import java.util.EnumSet;
 import java.util.logging.Logger;
@@ -18,7 +16,6 @@ public interface TaskContext {
     }
 
     FileSystem fileSystem();
-    default ProcessApi processApi() { return null; }
 
     void logSystemModification(Logger logger, String actionDescription);
 }
