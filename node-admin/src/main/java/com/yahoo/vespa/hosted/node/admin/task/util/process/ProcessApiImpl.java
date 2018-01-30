d1cf204835e11f6e0b3065d6401f9ecf85679607
Håkon Hallingstad <hakon@oath.com>
2018-01-30 17:26:41 +0100

    Add systemctl support

---
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/task/util/process/ProcessApiImpl.java
@@ -0,0 +1,52 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.node.admin.task.util.process;
+
+import com.yahoo.vespa.hosted.node.admin.task.util.file.UnixPath;
+
+import java.nio.file.Path;
+
+/**
+ * @author hakonhall
+ */
+public class ProcessApiImpl implements ProcessApi {
+    private final Process process;
+    private final Path processOutputPath;
+
+    ProcessApiImpl(Process process, Path processOutputPath) {
+        this.process = process;
+        this.processOutputPath = processOutputPath;
+    }
+
+    @Override
+    public void waitForTermination() {
+        while (true) {
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                // ignoring
+                continue;
+            }
+
+            return;
+        }
+    }
+
+    @Override
+    public int exitCode() { return process.exitValue(); }
+
+    @Override
+    public String getUtf8Output() {
+        return new UnixPath(processOutputPath).readUtf8File();
+    }
+
+    @Override
+    public void close() {
+        // TODO: Should kill process if still alive?
+        processOutputPath.toFile().delete();
+    }
+
+    @Override
+    public Path getProcessOutputPath() {
+        return processOutputPath;
+    }
+}
