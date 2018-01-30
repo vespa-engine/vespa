9869141c46d299e84ac9285a3ee575e49cb1970a
Harald Musum <musum@oath.com>
2018-01-30 09:33:06 +0100

    Fix bug where nextContent() always returned all data

---
+++ filedistribution/src/test/java/com/yahoo/vespa/filedistribution/FileReferenceDataTest.java
@@ -0,0 +1,28 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.filedistribution;
+
+import com.yahoo.config.FileReference;
+import com.yahoo.text.Utf8;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileReferenceDataTest {
+
+    @Test
+    public void testDataBlob() {
+        String content = "blob";
+        FileReferenceData fileReferenceData =
+                new FileReferenceDataBlob(new FileReference("ref"), "foo", FileReferenceData.Type.compressed, Utf8.toBytes(content));
+        ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+        assertEquals(4, fileReferenceData.nextContent(byteBuffer));
+        assertEquals(content, Utf8.toString(Arrays.copyOfRange(byteBuffer.array(), 0, 4)));
+
+        // nextContent() will always return everything for FileReferenceDataBlob, so nothing more should be read
+        assertEquals(-1, fileReferenceData.nextContent(byteBuffer));
+    }
+
+}
