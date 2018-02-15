cb8b53aa79c38d4ae5f1421993b08eadb148a04a
Valerij Fredriksen <valerijf@oath.com>
2018-02-15 12:45:06 +0100

    Add method to read KeyStore with BC provider to KeyStoreOptions

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/util/KeyStoreOptions.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/util/KeyStoreOptions.java
@@ -1,7 +1,14 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
 
 public class KeyStoreOptions {
     public final Path path;
@@ -12,5 +19,14 @@ public class KeyStoreOptions {
         this.path = path;
         this.password = password;
         this.type = type;
+    }
+
+    public KeyStore loadKeyStoreWithBcProvider()
+            throws IOException, NoSuchProviderException, KeyStoreException, CertificateException, NoSuchAlgorithmException {
+        try (FileInputStream in = new FileInputStream(path.toFile())) {
+            KeyStore keyStore = KeyStore.getInstance(type, "BC");
+            keyStore.load(in, password);
+            return keyStore;
+        }
     }
 }
