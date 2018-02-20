1ec2db1684e5f1ca0e50364c9741fdfa1e135503
Morten Tokle <morten.tokle@gmail.com>
2018-02-20 20:35:37 +0100

    Revert "Refactor identityprovider. Add SiaIdentityProvider"

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identity/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/**
- * @author mortent
- */
-@ExportPackage
-package com.yahoo.vespa.athenz.identity;
-
-import com.yahoo.osgi.annotation.ExportPackage;
