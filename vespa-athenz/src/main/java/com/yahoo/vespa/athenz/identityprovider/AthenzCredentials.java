04e863924729c2039381b114687c13b458860b34
Morten Tokle <mortent@oath.com>
2018-02-22 15:56:35 +0100

    move identityprovider package to vespa-athenz

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/AthenzCredentials.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/AthenzCredentials.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.hosted.athenz.identityprovider;
+package com.yahoo.vespa.athenz.identityprovider;
 
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
--- vespa-athenz/src/test/java/com/yahoo/vespa/athenz/identityprovider/CryptoUtilsTest.java
+++ vespa-athenz/src/test/java/com/yahoo/vespa/athenz/identityprovider/CryptoUtilsTest.java
@@ -1,5 +1,5 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.hosted.athenz.identityprovider;
+package com.yahoo.vespa.athenz.identityprovider;
 
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.junit.Test;
