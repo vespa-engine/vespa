f3eba4322142a64de07d6759ed935938ffd64f39
Jon Marius Venstad <jvenstad@yahoo-inc.com>
2018-02-22 13:18:11 +0100

    Add dummy for container tests

---
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/stubs/MockBuildService.java
@@ -0,0 +1,12 @@
+package com.yahoo.vespa.hosted.controller.api.integration.stubs;
+
+import com.yahoo.vespa.hosted.controller.api.integration.BuildService;
+
+public class MockBuildService implements BuildService {
+
+    @Override
+    public boolean trigger(BuildJob buildJob) {
+        return true;
+    }
+
+}
