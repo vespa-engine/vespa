473f18dae9fa78eb93100ac237902b5e10214248
Arne Juul <arnej@yahoo-inc.com>
2018-03-09 10:29:09 +0000

    GC leftover code

--- eval/src/vespa/eval/eval/tensor_function.cpp
+++ eval/src/vespa/eval/eval/tensor_function.cpp
@@ -166,59 +166,6 @@ Map::compile_self(Stash &) const
     return Instruction(op_tensor_map, to_param(_function));
 }
 
-vespalib::string
-name_of(map_fun_t fun)
-{
-    if (fun == operation::Neg::f) return "-";
-    if (fun == operation::Not::f) return "!";
-    if (fun == operation::Cos::f) return "cos";
-    if (fun == operation::Sin::f) return "sin";
-    if (fun == operation::Tan::f) return "tan";
-    if (fun == operation::Cosh::f) return "cosh";
-    if (fun == operation::Sinh::f) return "sinh";
-    if (fun == operation::Tanh::f) return "tanh";
-    if (fun == operation::Acos::f) return "acos";
-    if (fun == operation::Asin::f) return "asin";
-    if (fun == operation::Atan::f) return "atan";
-    if (fun == operation::Exp::f) return "exp";
-    if (fun == operation::Log10::f) return "log10";
-    if (fun == operation::Log::f) return "log";
-    if (fun == operation::Sqrt::f) return "sqrt";
-    if (fun == operation::Ceil::f) return "ceil";
-    if (fun == operation::Fabs::f) return "fabs";
-    if (fun == operation::Floor::f) return "floor";
-    if (fun == operation::IsNan::f) return "isnan";
-    if (fun == operation::Relu::f) return "relu";
-    if (fun == operation::Sigmoid::f) return "sigmoid";
-    if (fun == operation::Elu::f) return "elu";
-    return "[other map function]";
-}
-
-vespalib::string
-name_of(join_fun_t fun)
-{
-    if (fun == operation::Add::f) return "+";
-    if (fun == operation::Sub::f) return "-";
-    if (fun == operation::Mul::f) return "*";
-    if (fun == operation::Div::f) return "/";
-    if (fun == operation::Mod::f) return "%";
-    if (fun == operation::Pow::f) return "^";
-    if (fun == operation::Equal::f) return "==";
-    if (fun == operation::NotEqual::f) return "!=";
-    if (fun == operation::Approx::f) return "~";
-    if (fun == operation::Less::f) return "<";
-    if (fun == operation::LessEqual::f) return "<=";
-    if (fun == operation::Greater::f) return ">";
-    if (fun == operation::GreaterEqual::f) return ">=";
-    if (fun == operation::And::f) return "&&";
-    if (fun == operation::Or::f) return "||";
-    if (fun == operation::Atan2::f) return "atan2";
-    if (fun == operation::Ldexp::f) return "ldexp";
-    if (fun == operation::Min::f) return "min";
-    if (fun == operation::Max::f) return "max";
-    return "[other join function]";
-}
-
 void
 Map::dump_tree(DumpTarget &target) const
 {
