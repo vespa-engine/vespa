dc0c9568ac92b7401cbbdf462bb92fe7604b4f58
Henning Baldersheim <balder@yahoo-inc.com>
2018-03-16 17:50:44 +0100

    No need for an array when you just need a count

--- config-model/src/main/java/com/yahoo/vespa/model/content/storagecluster/FileStorProducer.java
+++ config-model/src/main/java/com/yahoo/vespa/model/content/storagecluster/FileStorProducer.java
@@ -3,11 +3,7 @@ package com.yahoo.vespa.model.content.storagecluster;
 
 import com.yahoo.vespa.config.content.StorFilestorConfig;
 import com.yahoo.vespa.model.builder.xml.dom.ModelElement;
-import com.yahoo.vespa.model.content.PriorityMapping;
 import com.yahoo.vespa.model.content.cluster.ContentCluster;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Serves stor-filestor for storage clusters.
@@ -19,7 +15,7 @@ public class FileStorProducer implements StorFilestorConfig.Producer {
             return new FileStorProducer(parent, getThreads(clusterElem));
         }
 
-       private List<StorFilestorConfig.Threads.Builder> getThreads(ModelElement clusterElem) {
+       private Integer getThreads(ModelElement clusterElem) {
            ModelElement tuning = clusterElem.getChild("tuning");
            if (tuning == null) {
                return null;
@@ -29,44 +25,34 @@ public class FileStorProducer implements StorFilestorConfig.Producer {
                return null;
            }
 
-           List<StorFilestorConfig.Threads.Builder> retVal = new ArrayList<>();
-
-           PriorityMapping mapping = new PriorityMapping(clusterElem);
-
-           for (ModelElement thread : threads.subElements("thread")) {
-               String priorityName = thread.getStringAttribute("lowest-priority");
-               if (priorityName == null) {
-                   priorityName = "LOWEST";
-               }
-
-               Integer count = thread.getIntegerAttribute("count");
-               if (count == null) {
-                   count = 1;
-               }
-
-               for (int i = 0; i < count; ++i) {
-                   retVal.add(new StorFilestorConfig.Threads.Builder().lowestpri(mapping.getPriorityMapping(priorityName)));
-               }
+           Integer count = threads.getIntegerAttribute("count");
+           if (count != null) {
+               return count;
            }
 
-           return retVal;
+           // Backward compatible fallback
+           int numThreads = 0;
+           for (ModelElement thread : threads.subElements("thread")) {
+               count = thread.getIntegerAttribute("count");
+               numThreads += (count == null) ? 1 : count;
+           }
+
+           return numThreads;
        }
     }
 
-    private List<StorFilestorConfig.Threads.Builder> threads;
+    private Integer numThreads;
     private ContentCluster cluster;
 
-    public FileStorProducer(ContentCluster parent, List<StorFilestorConfig.Threads.Builder> threads) {
-        this.threads = threads;
+    public FileStorProducer(ContentCluster parent, Integer numThreads) {
+        this.numThreads = numThreads;
         this.cluster = parent;
     }
 
     @Override
     public void getConfig(StorFilestorConfig.Builder builder) {
-        if (threads != null) {
-            for (StorFilestorConfig.Threads.Builder t : threads) {
-                builder.threads.add(t);
-            }
+        if (numThreads != null) {
+            builder.num_threads(numThreads);
         }
         builder.enable_multibit_split_optimalization(cluster.getPersistence().enableMultiLevelSplitting());
     }
