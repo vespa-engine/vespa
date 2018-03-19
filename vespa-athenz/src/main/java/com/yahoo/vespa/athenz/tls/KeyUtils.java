36909a522751c5dbb2c8b6ba8499e2e637720e3d
Bjørn Christian Seime <bjorncs@oath.com>
2018-03-19 15:54:32 +0100

    Remove direct use of Crypto

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/tls/KeyUtils.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/tls/KeyUtils.java
@@ -1,9 +1,13 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.athenz.tls;
 
+import com.yahoo.athenz.auth.util.Crypto;
+
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 /**
  * @author bjorncs
@@ -25,5 +29,13 @@ public class KeyUtils {
 
     public static KeyPair generateKeypair(KeyAlgorithm algorithm) {
         return generateKeypair(algorithm, -1);
+    }
+
+    public static PublicKey extractPublicKey(PrivateKey privateKey) {
+        return Crypto.extractPublicKey(privateKey);
+    }
+
+    public static PrivateKey fromPemEncodedPrivateKey(String pem) {
+        return Crypto.loadPrivateKey(pem);
     }
 }
