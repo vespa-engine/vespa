c64d15af3cc717dddb54d0d59df313351a0a83db
Harald Musum <musum@oath.com>
2018-03-20 13:19:07 +0100

    Fix test

--- configserver/src/test/java/com/yahoo/vespa/config/server/model/LbServicesProducerTest.java
+++ configserver/src/test/java/com/yahoo/vespa/config/server/model/LbServicesProducerTest.java
@@ -89,7 +89,8 @@ public class LbServicesProducerTest {
     private LbServicesConfig createModelAndGetLbServicesConfig(RegionName regionName) throws IOException, SAXException {
         final Zone zone = new Zone(Environment.prod, regionName);
         Map<TenantName, Map<ApplicationId, ApplicationInfo>> testModel = createTestModel(new DeployState.Builder()
-                                                                                                 .properties(new DeployProperties.Builder().zone(zone).build())
+                                                                                                 .zone(zone)
+                                                                                                 .properties(new DeployProperties.Builder().build())
                                                                                                  .zone(zone));
         return getLbServicesConfig(new Zone(Environment.prod, regionName), testModel);
     }
