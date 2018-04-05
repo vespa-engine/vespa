e30d96e297539b29a8272502e4e7ec774c7db491
Henning Baldersheim <balder@yahoo-inc.com>
2018-04-05 19:44:43 +0200

    Export vespa.config

---
+++ configdefinitions/src/main/java/com/yahoo/vespa/config/package-info.java
@@ -0,0 +1,5 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+@ExportPackage
+package com.yahoo.vespa.config;
+
+import com.yahoo.osgi.annotation.ExportPackage;
