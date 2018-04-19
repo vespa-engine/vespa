97b5dc407f526da94405cd3a3cde88d617f7685b
Bjørn Christian Seime <bjorncs@oath.com>
2018-04-19 15:56:29 +0200

    Remove duplicate type for signed identity document

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/client/AthenzCredentials.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/client/AthenzCredentials.java
@@ -1,6 +1,8 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.athenz.identityprovider.client;
 
+import com.yahoo.vespa.athenz.identityprovider.api.bindings.SignedIdentityDocument;
+
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 
