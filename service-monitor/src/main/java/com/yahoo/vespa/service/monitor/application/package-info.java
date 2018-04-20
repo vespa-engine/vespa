5095ff2fbc441cc871e28bb02c313dd57d8a4863
Valerij Fredriksen <valerijf@oath.com>
2018-04-20 13:00:22 +0200

    Add ExportPackage for application

---
+++ service-monitor/src/main/java/com/yahoo/vespa/service/monitor/application/package-info.java
@@ -0,0 +1,8 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/*
+ * @author freva
+ */
+@ExportPackage
+package com.yahoo.vespa.service.monitor.application;
+
+import com.yahoo.osgi.annotation.ExportPackage;
