cdfdb9f134a6decc89993ea1dc6a2376ec675a74
Valerij Fredriksen <valerijf@oath.com>
2018-04-25 13:17:23 +0200

    SecretAgentScheduleMaker -> SecretAgentCheckConfig

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/util/SecretAgentCheckConfig.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/util/SecretAgentCheckConfig.java
@@ -8,11 +8,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Helper class to generate and write the secret-agent schedule file.
+ * Helper class to generate and write the secret-agent check config files.
  *
  * @author freva
  */
-public class SecretAgentScheduleMaker {
+public class SecretAgentCheckConfig {
     private final String id;
     private final int interval;
     private final Path checkExecutable;
@@ -20,19 +20,19 @@ public class SecretAgentScheduleMaker {
     private String user = "nobody";
     private final Map<String, Object> tags = new LinkedHashMap<>();
 
-    public SecretAgentScheduleMaker(String id, int interval, Path checkExecutable, String... arguments) {
+    public SecretAgentCheckConfig(String id, int interval, Path checkExecutable, String... arguments) {
         this.id = id;
         this.interval = interval;
         this.checkExecutable = checkExecutable;
         this.arguments = arguments;
     }
 
-    public SecretAgentScheduleMaker withRunAsUser(String user) {
+    public SecretAgentCheckConfig withRunAsUser(String user) {
         this.user = user;
         return this;
     }
 
-    public SecretAgentScheduleMaker withTag(String tagKey, Object tagValue) {
+    public SecretAgentCheckConfig withTag(String tagKey, Object tagValue) {
         tags.put(tagKey, tagValue);
         return this;
     }
@@ -40,11 +40,11 @@ public class SecretAgentScheduleMaker {
     public void writeTo(Path yamasAgentDirectory) throws IOException {
         if (! Files.exists(yamasAgentDirectory)) yamasAgentDirectory.toFile().mkdirs();
         Path scheduleFilePath = yamasAgentDirectory.resolve(id + ".yaml");
-        Files.write(scheduleFilePath, toString().getBytes());
+        Files.write(scheduleFilePath, render().getBytes());
         scheduleFilePath.toFile().setReadable(true, false); // Give everyone read access to the schedule file
     }
 
-    public String toString() {
+    public String render() {
         StringBuilder stringBuilder = new StringBuilder()
                 .append("- id: ").append(id).append("\n")
                 .append("  interval: ").append(interval).append("\n")
