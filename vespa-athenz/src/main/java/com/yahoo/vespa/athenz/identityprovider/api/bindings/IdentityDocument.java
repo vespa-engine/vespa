0e176ef33b0e3fb4278c31d8b0d24b934df5180f
Morten Tokle <morten.tokle@gmail.com>
2018-04-30 10:02:22 +0200

    Revert "Skip sending ipaddresses in CSR"

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/api/bindings/IdentityDocument.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/api/bindings/IdentityDocument.java
@@ -2,7 +2,6 @@
 package com.yahoo.vespa.athenz.identityprovider.api.bindings;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.Instant;
@@ -13,7 +12,6 @@ import java.util.Set;
  * @author bjorncs
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class IdentityDocument {
 
     @JsonProperty("provider-unique-id")
