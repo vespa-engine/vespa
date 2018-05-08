ffcdd640393978666c08e40ed02764d2fc3e8c42
Geir Storli <geirst@oath.com>
2018-05-08 15:18:24 +0000

    Ensure consistent memory stats snapshot by a process.

--- staging_vespalib/src/vespa/vespalib/util/process_memory_stats.h
+++ staging_vespalib/src/vespa/vespalib/util/process_memory_stats.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <cstdint>
+#include <vespa/vespalib/stllike/string.h>
 
 namespace vespalib {
 
@@ -18,17 +19,27 @@ class ProcessMemoryStats
     uint64_t _mappings_count; // number of mappings
                               // (limited by sysctl vm.max_map_count)
 
+    static ProcessMemoryStats createStatsFromSmaps();
+
 public:
     ProcessMemoryStats();
-    static ProcessMemoryStats create(); // based on /proc/self/smaps
+    /**
+     * Sample memory stats for the current process based on reading the file /proc/self/smaps.
+     *
+     * Samples are taken until two consecutive memory stats are similar given the size epsilon.
+     * This ensures a somewhat consistent memory stats snapshot.
+     */
+    static ProcessMemoryStats create(uint64_t sizeEpsilon = 1 * 1024 * 1024);
     uint64_t getMappedVirt() const { return _mapped_virt; }
     uint64_t getMappedRss() const { return _mapped_rss; }
     uint64_t getAnonymousVirt() const { return _anonymous_virt; }
     uint64_t getAnonymousRss() const { return _anonymous_rss; }
     uint64_t getMappingsCount() const { return _mappings_count; }
+    bool similarTo(const ProcessMemoryStats &rhs, uint64_t sizeEpsilon) const;
+    vespalib::string toString() const;
 
     /** for unit tests only */
     ProcessMemoryStats(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
 };
 
-} // namespace vespalib
+}
