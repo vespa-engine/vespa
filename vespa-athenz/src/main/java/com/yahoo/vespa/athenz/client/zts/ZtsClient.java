6e8c0dfb89d021d8e6e9d8c1f9a982feae5efb46
Bjørn Christian Seime <bjorncs@oath.com>
2018-05-11 18:09:51 +0200

    Add methods to ZtsClient for retrieving role token and certificate

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/client/zts/ZtsClient.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/client/zts/ZtsClient.java
@@ -1,8 +1,14 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.athenz.client.zts;
 
+import com.yahoo.vespa.athenz.api.AthenzDomain;
 import com.yahoo.vespa.athenz.api.AthenzService;
+import com.yahoo.vespa.athenz.api.ZToken;
 import com.yahoo.vespa.athenz.tls.Pkcs10Csr;
+
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
+import java.time.Duration;
 
 /**
  * Interface for a ZTS client.
@@ -34,6 +40,53 @@ public interface ZtsClient extends AutoCloseable {
                                      String instanceId,
                                      boolean requestServiceToken,
                                      Pkcs10Csr csr);
+
+    /**
+     * Fetch a role token for the target domain
+     *
+     * @param domain Target domain
+     * @return A role token
+     */
+    ZToken getRoleToken(AthenzDomain domain);
+
+    /**
+     * Fetch a role token for the target domain and role
+     *
+     * @param domain Target domain
+     * @param roleName Target role
+     * @return A role token
+     */
+    ZToken getRoleToken(AthenzDomain domain, String roleName);
+
+    /**
+     * Fetch role certificate for the target domain and role
+     *
+     * @param domain Target domain
+     * @param roleName Target role
+     * @param expiry Certificate expiry
+     * @param keyPair Key pair which will be used to generate CSR (certificate signing request)
+     * @param cloud The cloud suffix used in DNS SAN entries
+     * @return A role certificate
+     */
+    X509Certificate getRoleCertificate(AthenzDomain domain,
+                                       String roleName,
+                                       Duration expiry,
+                                       KeyPair keyPair,
+                                       String cloud);
+
+    /**
+     * Fetch role certificate for the target domain and role
+     *
+     * @param domain Target domain
+     * @param roleName Target role
+     * @param keyPair Key pair which will be used to generate CSR (certificate signing request)
+     * @param cloud The cloud suffix used in DNS SAN entries
+     * @return A role certificate
+     */
+    X509Certificate getRoleCertificate(AthenzDomain domain,
+                                       String roleName,
+                                       KeyPair keyPair,
+                                       String cloud);
 
     void close();
 }
