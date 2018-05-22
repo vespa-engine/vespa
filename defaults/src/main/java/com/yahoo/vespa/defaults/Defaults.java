258bec0a21196a15012d0311dde896c2e01bc95f
Arne Juul <arnej@yahoo-inc.com>
2018-05-22 13:44:48 +0000

    update documentation comments

--- defaults/src/main/java/com/yahoo/vespa/defaults/Defaults.java
+++ defaults/src/main/java/com/yahoo/vespa/defaults/Defaults.java
@@ -90,7 +90,10 @@ public class Defaults {
 
 
     /**
-     * Compute the host name that identifies myself
+     * Compute the host name that identifies myself.
+     * Detection of the hostname is now done before starting any Vespa
+     * programs and provided in the environment variable VESPA_HOSTNAME;
+     * if that variable isn't set a default of "localhost" is always returned.
      * @return the vespa host name
      **/
     public String vespaHostname() { return vespaHost; }
