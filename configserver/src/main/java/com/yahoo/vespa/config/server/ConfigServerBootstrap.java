eeb39b8c511cf4f04441d6c1c26ff9c20db8ccb2
Harald Musum <musum@oath.com>
2018-06-05 14:55:09 +0200

    Add initiallyInRotation to vip status config and inject config into VipStatus
    Use config value for inital value of isInRotation() when nothing
    is known about backend clusters

--- configserver/src/main/java/com/yahoo/vespa/config/server/ConfigServerBootstrap.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/ConfigServerBootstrap.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Executors;
  * Main component that bootstraps and starts config server threads.
  *
  * If config server has been upgraded to a new version since the last time it was running it will redeploy all
- * applications. If that is done syúccessfully the RPC server will start and the health status code will change from
+ * applications. If that is done successfully the RPC server will start and the health status code will change from
  * 'initializing' to 'up' and the config server will be put into rotation (start serving status.html with 200 OK)
  *
  * @author Ulf Lilleengen
