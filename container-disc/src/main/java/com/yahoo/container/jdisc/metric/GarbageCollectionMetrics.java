7b97b3250cfb17ba51e21bec2d7442d496ca566d
Olli Virtanen <olli.virtanen@oath.com>
2018-06-07 17:11:42 +0200

    Corrected metrics key, again

--- container-disc/src/main/java/com/yahoo/container/jdisc/metric/GarbageCollectionMetrics.java
+++ container-disc/src/main/java/com/yahoo/container/jdisc/metric/GarbageCollectionMetrics.java
@@ -16,9 +16,8 @@ import java.util.Map;
  * @author ollivir
  */
 public class GarbageCollectionMetrics {
-    private static final String GC_PREFIX = "jdisc.gc.";
-    private static final String GC_COUNT = GC_PREFIX + ".count";
-    private static final String GC_TIME = GC_PREFIX + ".ms";
+    private static final String GC_COUNT = "jdisc.gc.count";
+    private static final String GC_TIME = "jdisc.gc.ms";
     private static final String DIMENSION_KEY = "gcName";
 
     public static final Duration REPORTING_INTERVAL = Duration.ofSeconds(62);
