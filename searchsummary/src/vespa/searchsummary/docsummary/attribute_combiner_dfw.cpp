ad65bb0f8b5f5c3ca9c9c40a1b03e3b5179bbc40
Tor Egge <Tor.Egge@oath.com>
2018-06-08 09:31:20 +0000

    Use helper class to collect list of nested field names for array or struct / map of struct.

--- searchsummary/src/vespa/searchsummary/docsummary/attribute_combiner_dfw.cpp
+++ searchsummary/src/vespa/searchsummary/docsummary/attribute_combiner_dfw.cpp
@@ -18,6 +18,76 @@ using search::attribute::CollectionType;
 
 namespace search::docsummary {
 
+namespace {
+
+class StructFields
+{
+    std::vector<vespalib::string> _mapFields;
+    std::vector<vespalib::string> _arrayFields;
+    bool _hasMapKey;
+    bool _error;
+
+public:
+    StructFields(const vespalib::string &fieldName, const IAttributeManager &attrMgr);
+    ~StructFields();
+    const std::vector<vespalib::string> &getMapFields() const { return _mapFields; }
+    const std::vector<vespalib::string> &getArrayFields() const { return _arrayFields; }
+    bool hasMapKey() const { return _hasMapKey; }
+    bool getError() const { return _error; }
+};
+
+
+StructFields::StructFields(const vespalib::string &fieldName, const IAttributeManager &attrMgr)
+    : _mapFields(),
+      _arrayFields(),
+      _hasMapKey(false),
+      _error(false)
+{
+    // Note: Doesn't handle imported attributes
+    std::vector<AttributeGuard> attrs;
+    attrMgr.getAttributeList(attrs);
+    vespalib::string prefix = fieldName + ".";
+    vespalib::string keyName = prefix + "key";
+    vespalib::string valuePrefix = prefix + "value.";
+    for (const auto &guard : attrs) {
+        vespalib::string name = guard->getName();
+        if (name.substr(0, prefix.size()) != prefix) {
+            continue;
+        }
+        auto collType = guard->getCollectionType();
+        if (collType != CollectionType::Type::ARRAY) {
+            LOG(warning, "Attribute %s is not an array attribute", name.c_str());
+            _error = true;
+            break;
+        }
+        if (name.substr(0, valuePrefix.size()) == valuePrefix) {
+            _mapFields.emplace_back(name.substr(valuePrefix.size()));
+        } else {
+            _arrayFields.emplace_back(name.substr(prefix.size()));
+            if (name == keyName) {
+                _hasMapKey = true;
+            }
+        }
+    }
+    if (!_error) {
+        std::sort(_arrayFields.begin(), _arrayFields.end());
+        std::sort(_mapFields.begin(), _mapFields.end());
+        if (!_mapFields.empty()) {
+            if (!_hasMapKey) {
+                LOG(warning, "Missing key attribute '%s', have value attributes for map", keyName.c_str());
+                _error = true;
+            } else if (_arrayFields.size() != 1u) {
+                LOG(warning, "Could not determine if field '%s' is array or map of struct", fieldName.c_str());
+                _error = true;
+            }
+        }
+    }
+}
+
+StructFields::~StructFields() = default;
+
+}
+
 AttributeCombinerDFW::AttributeCombinerDFW(const vespalib::string &fieldName)
     : IDocsumFieldWriter(),
       _stateIndex(0),
@@ -43,48 +113,14 @@ AttributeCombinerDFW::setFieldWriterStateIndex(uint32_t fieldWriterStateIndex)
 std::unique_ptr<IDocsumFieldWriter>
 AttributeCombinerDFW::create(const vespalib::string &fieldName, IAttributeManager &attrMgr)
 {
-    // Note: Doesn't handle imported attributes
-    std::vector<AttributeGuard> attrs;
-    attrMgr.getAttributeList(attrs);
-    vespalib::string prefix = fieldName + ".";
-    vespalib::string keyName = prefix + "key";
-    vespalib::string valuePrefix = prefix + "value.";
-    std::vector<vespalib::string> mapFields;
-    std::vector<vespalib::string> arrayFields;
-    bool foundKey = false;
-    for (const auto &guard : attrs) {
-        vespalib::string name = guard->getName();
-        if (name.substr(0, prefix.size()) != prefix) {
-            continue;
-        }
-        auto collType = guard->getCollectionType();
-        if (collType != CollectionType::Type::ARRAY) {
-            LOG(warning, "Attribute %s is not an array attribute", name.c_str());
-            return std::unique_ptr<IDocsumFieldWriter>();
-        }
-        if (name.substr(0, valuePrefix.size()) == valuePrefix) {
-            mapFields.emplace_back(name.substr(valuePrefix.size()));
-        } else {
-            arrayFields.emplace_back(name.substr(prefix.size()));
-            if (name == keyName) {
-                foundKey = true;
-            }
-        }
-    }
-    if (!mapFields.empty()) {
-        if (!foundKey) {
-            LOG(warning, "Missing key attribute '%s', have value attributes for map", keyName.c_str());
-            return std::unique_ptr<IDocsumFieldWriter>();
-        }
-        if (arrayFields.size() != 1u) {
-            LOG(warning, "Could not determine if field '%s' is array or map of struct", fieldName.c_str());
-            return std::unique_ptr<IDocsumFieldWriter>();
-        }
+    StructFields structFields(fieldName, attrMgr);
+    if (structFields.getError()) {
+        return std::unique_ptr<IDocsumFieldWriter>();
+    } else if (!structFields.getMapFields().empty()) {
         LOG(warning, "map of struct is not yet supported for field '%s'", fieldName.c_str());
         return std::unique_ptr<IDocsumFieldWriter>();
     }
-    std::sort(arrayFields.begin(), arrayFields.end());
-    return std::make_unique<ArrayAttributeCombinerDFW>(fieldName, arrayFields);
+    return std::make_unique<ArrayAttributeCombinerDFW>(fieldName, structFields.getArrayFields());
 }
 
 void
