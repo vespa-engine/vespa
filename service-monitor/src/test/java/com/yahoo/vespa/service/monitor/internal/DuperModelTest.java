57f30e34c4ac8a731b962cdb9f11c5e8b20b4d61
Håkon Hallingstad <hakon@oath.com>
2018-06-08 10:51:56 +0200

    Fix test

--- service-monitor/src/test/java/com/yahoo/vespa/service/monitor/internal/DuperModelTest.java
+++ service-monitor/src/test/java/com/yahoo/vespa/service/monitor/internal/DuperModelTest.java
@@ -41,7 +41,7 @@ public class DuperModelTest {
     @Test
     public void toApplicationInstanceInSingleTenantMode() {
         when(statusProvider.getStatus(any(), any(), any(), any())).thenReturn(ServiceStatus.NOT_CHECKED);
-        ConfigserverConfig config = ConfigserverUtil.createExampleConfigserverConfig();
+        ConfigserverConfig config = ConfigserverUtil.createExampleConfigserverConfig(true, false);
         DuperModel duperModel = new DuperModel(config);
         SuperModel superModel = mock(SuperModel.class);
         ApplicationInfo superModelApplicationInfo = mock(ApplicationInfo.class);
