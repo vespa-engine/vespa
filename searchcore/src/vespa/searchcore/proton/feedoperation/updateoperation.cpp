49ea989662f402f6c96737636b497724a4bb457f
Henning Baldersheim <balder@oath.com>
2018-06-12 09:44:46 +0200

    Add missing bytebuffer.h include

--- searchcore/src/vespa/searchcore/proton/feedoperation/updateoperation.cpp
+++ searchcore/src/vespa/searchcore/proton/feedoperation/updateoperation.cpp
@@ -2,6 +2,7 @@
 #include "updateoperation.h"
 #include <vespa/document/base/exceptions.h>
 #include <vespa/document/update/documentupdate.h>
+#include <vespa/document/util/bytebuffer.h>
 #include <cassert>
 
 #include <vespa/log/log.h>
