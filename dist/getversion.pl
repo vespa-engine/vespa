54ed5ef9cd4714c004da92e7fb9fcae72ba50038
Arne Juul <arnej@yahoo-inc.com>
2018-06-13 11:13:30 +0200

    simplify more

--- dist/getversion.pl
+++ dist/getversion.pl
@@ -6,14 +6,13 @@ use POSIX qw(strftime);
 $srcdir = ".";
 
 my $printmap = 0;
-my $tagtype = "";
 
 while ($opt = shift) {
     if ($opt =~ m/^-/) {
         if ($opt eq "-M") {
 	    $printmap = 1;
 	} elsif ($opt eq "-T") {
-	    $tagtype = shift;
+	    die "option '-T' is removed";
 	} else {
 	    print STDERR "ERROR: unknown option '$opt' for getversion\n";
 	    print "error\n";
@@ -24,32 +23,21 @@ while ($opt = shift) {
     }
 }
 
-if (!defined($srcdir)) {
-    die "srcdir must be set";
-}
-
 # Read current major-minor release
 sub read_head_version() {
     my $file = "$srcdir/VERSION";
     if (! -f $file) {
-        die "Unable to locate version file";
+        die "Unable to locate version file in $srcdir";
     }
     open(my $fd, "< $file") ||
         die "Unable to open VERSION: $!";
     my $version = <$fd>;
     chomp($version);
     close($fd);
-
     return $version;
 }
 
-if ( ! -d $srcdir ) {
-    print STDERR "ERROR: bad directory '$srcdir' for getversion\n";
-    print "error\n";
-    exit 1;
-}
-
-# assume HEAD if all else fails
+# assume HEAD
 my $mainver = read_head_version();
 
 # date adding logic
@@ -58,11 +46,8 @@ my $mainver = read_head_version();
 # vbuild/mbuild also has some logic for this:
 
 $dateadd = $ENV{"VBUILD_VERSION_DATE"};
-$buildtime = $ENV{"CVSBUILDTIME"};
 
-if ($buildtime && $buildtime =~ m/^(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)$/ ) {
-    $dateadd = ".$1$2$3.$4$5$6";
-} elsif ($dateadd) {
+if ($dateadd) {
     1;
 } else {
     $dateadd = (strftime ".%Y%m%d.%H%M%S", gmtime);
@@ -72,9 +57,6 @@ $tag = "HEAD";
 
 if (defined $ENV{FACTORY_VESPA_VERSION}) { 
     $version = $ENV{FACTORY_VESPA_VERSION};
-} elsif ($tagtype eq 'tag') {
-    # only for exact tag, do not add date:
-    $version = $mainver;
 } else {
     $version = $mainver . $dateadd;
 }
@@ -83,7 +65,6 @@ if ($printmap) {
     # other useful information
 
     chomp($ostype = `uname -s`);
-
     chomp($osver = `uname -r`);
     chomp($osarch = `uname -m`);
 
