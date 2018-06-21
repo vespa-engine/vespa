aa41e19ecc9692b3b839725383e3e7706d3042fb
Arne Juul <arnej@yahoo-inc.com>
2018-06-21 07:20:21 +0200

    use new C++ noreturn syntax

--- vespalib/src/vespa/vespalib/util/hdr_abort.h
+++ vespalib/src/vespa/vespalib/util/hdr_abort.h
@@ -3,9 +3,10 @@
 
 namespace vespalib {
 
-extern void hdr_abort(const char *message,
-                      const char *file,
-                      unsigned int line) __attribute__((__noreturn__));
+[[noreturn]] extern void
+hdr_abort(const char *message,
+          const char *file,
+          unsigned int line);
 
 #define HDR_ABORT(msg) \
   (vespalib::hdr_abort(msg, __FILE__, __LINE__))
