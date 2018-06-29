a248733c1a5a6270ff28eac6d0d4017a03de7e52
Jon Bratseth <bratseth@oath.com>
2018-06-29 11:41:09 +0200

    Implement additional method

--- configserver/src/test/java/com/yahoo/vespa/config/server/deploy/MockDeployer.java
+++ configserver/src/test/java/com/yahoo/vespa/config/server/deploy/MockDeployer.java
@@ -5,6 +5,7 @@ import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.Deployment;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 /**
@@ -22,6 +23,11 @@ public class MockDeployer implements com.yahoo.config.provision.Deployer {
     @Override
     public Optional<Deployment> deployFromLocalActive(ApplicationId application, Duration timeout) {
         lastDeployed = application;
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Instant> lastDeployTime(ApplicationId application) {
         return Optional.empty();
     }
 
