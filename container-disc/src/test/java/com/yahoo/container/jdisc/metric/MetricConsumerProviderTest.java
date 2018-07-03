b266657d52b30d926b40292d46e872cb1252ac60
Olli Virtanen <olli.virtanen@oath.com>
2018-07-03 13:37:56 +0200

    Close servers earlier in the shutdown process

--- container-disc/src/test/java/com/yahoo/container/jdisc/metric/MetricConsumerProviderTest.java
+++ container-disc/src/test/java/com/yahoo/container/jdisc/metric/MetricConsumerProviderTest.java
@@ -2,14 +2,12 @@
 package com.yahoo.container.jdisc.metric;
 
 import com.yahoo.jdisc.application.MetricConsumer;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assume.assumeTrue;
 
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
