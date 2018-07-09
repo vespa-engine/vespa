3588ab015c45e5e8682e9a9299cabec25937d9d8
Bjørn Christian Seime <bjorncs@oath.com>
2018-07-09 15:13:33 +0200

    Move NTokenValidator to vespa-athenz + load pub keys from file
    - Move NTokenValidator from controller-server to vespa-athenz
    - Remodel ZmsKeystore as AthenzTruststore
    - Use file-backed truststore on controller (replaces download of public keys)
    - Remove ZmsClient.getPublicKey/getPublicKeys

---
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/utils/ntoken/AthenzConfTruststore.java
@@ -0,0 +1,52 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.athenz.utils.ntoken;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.yahoo.athenz.auth.util.Crypto;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * A {@link AthenzTruststore} that is backed by athenz.conf
+ *
+ * @author bjorncs
+ */
+public class AthenzConfTruststore implements AthenzTruststore {
+
+    private final Map<String, PublicKey> publicKeys;
+
+    public AthenzConfTruststore(Path athenzConfFile) {
+        this.publicKeys = loadPublicKeys(athenzConfFile);
+    }
+
+    @Override
+    public Optional<PublicKey> getPublicKey(String keyId) {
+        return Optional.of(publicKeys.get(keyId));
+    }
+
+    private static Map<String, PublicKey> loadPublicKeys(Path athenzConfFile) {
+        try {
+            Map<String, PublicKey> publicKeys = new HashMap<>();
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(athenzConfFile.toFile());
+            ArrayNode keysArray = (ArrayNode) root.get("ztsPublicKeys");
+            for (JsonNode keyEntry : keysArray) {
+                String keyId = keyEntry.get("id").textValue();
+                String encodedPublicKey = keyEntry.get("key").textValue();
+                PublicKey publicKey = Crypto.loadPublicKey(Crypto.ybase64DecodeString(encodedPublicKey));
+                publicKeys.put(keyId, publicKey);
+            }
+            return publicKeys;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}
