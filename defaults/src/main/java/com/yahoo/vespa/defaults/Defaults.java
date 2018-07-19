84f11ac019f2ef36bd8885c871014ef52ab154f3
Arne Juul <arnej@yahoo-inc.com>
2018-07-19 06:52:46 +0200

    fix typo

--- defaults/src/main/java/com/yahoo/vespa/defaults/Defaults.java
+++ defaults/src/main/java/com/yahoo/vespa/defaults/Defaults.java
@@ -39,7 +39,7 @@ public class Defaults {
         vespaPortBase = findVespaPortBase(19000);
         vespaPortConfigServerRpc = findConfigServerPort(vespaPortBase + 70);
         vespaPortConfigServerHttp = vespaPortConfigServerRpc + 1;
-        vespaPortConfigProxyRpc = findConfigProxyPort(vespaPortBase + 91);
+        vespaPortConfigProxyRpc = findConfigProxyPort(vespaPortBase + 90);
     }
     static private String findVespaHome() {
         Optional<String> vespaHomeEnv = Optional.ofNullable(System.getenv("VESPA_HOME"));
