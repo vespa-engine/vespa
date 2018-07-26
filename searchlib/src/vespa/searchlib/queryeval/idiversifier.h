e78f728e723f15b2423c7d5556e4bf9bc47c2c6f
Henning Baldersheim <balder@oath.com>
2018-07-26 13:37:49 +0200

    Restructure for code reuse and hiding implementation.

---
+++ searchlib/src/vespa/searchlib/queryeval/idiversifier.h
@@ -0,0 +1,16 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+#include <cstdint>
+
+namespace search::queryeval {
+
+struct IDiversifier {
+    virtual ~IDiversifier() {}
+    /**
+     * Will tell if this document should be kept, and update state for further filtering.
+     */
+    virtual bool accepted(uint32_t docId) = 0;
+};
+}
