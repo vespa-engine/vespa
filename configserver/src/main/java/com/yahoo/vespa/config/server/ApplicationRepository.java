f0e1c27a4ca453232a45ca23438aa929fc485eed
Harald Musum <musum@oath.com>
2018-08-13 13:56:59 +0200

    Use non-legacy method deleteApplication in more zones

--- configserver/src/main/java/com/yahoo/vespa/config/server/ApplicationRepository.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/ApplicationRepository.java
@@ -283,14 +283,15 @@ public class ApplicationRepository implements com.yahoo.config.provision.Deploye
      */
     public boolean delete(ApplicationId applicationId) {
         List<String> hostedZonesToUseDeleteApplication =
-                Arrays.asList("dev.us-east-1", "dev.corp-us-east-1", "test.us-east-1",
-                              "prod.corp-us-east-1", "prod.aws-us-east-1a", "prod.aws-us-west-1b");
-        boolean useDeleteApplicationLegacyInThisZone = !hostedZonesToUseDeleteApplication.contains(zone().toString());
+                Arrays.asList("dev.us-east-1", "dev.corp-us-east-1",
+                              "test.us-east-1", "staging.us-east-3",
+                              "prod.aws-us-east-1a", "prod.aws-us-west-1b",
+                              "prod.corp-us-east-1", "prod.us-central-1");
+        boolean useDeleteApplicationLegacy = !hostedZonesToUseDeleteApplication.contains(zone().toString());
 
         // TODO: Use deleteApplication() in all zones
         if (configserverConfig.deleteApplicationLegacy() ||
-                (configserverConfig.hostedVespa() && zone().system() == SystemName.main &&
-                        useDeleteApplicationLegacyInThisZone)) {
+                (configserverConfig.hostedVespa() && zone().system() == SystemName.main && useDeleteApplicationLegacy)) {
             return deleteApplicationLegacy(applicationId);
         } else {
             return deleteApplication(applicationId);
