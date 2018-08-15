08ccf012e1063d5082623457368c4529b22c3f6f
Bjørn Christian Seime <bjorncs@oath.com>
2018-08-15 15:19:49 +0200

    Revert "Use ServiceIdentitySslSocketFactory in ConfigServerApiImpl"
    This reverts commit 09869d3ba2590ee29b3349eb68da9e75fd6bd525.

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/configserver/ConfigServerApiImpl.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/configserver/ConfigServerApiImpl.java
@@ -4,13 +4,13 @@ package com.yahoo.vespa.hosted.node.admin.configserver;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yahoo.config.provision.HostName;
-import com.yahoo.vespa.athenz.identity.ServiceIdentitySslSocketFactory;
+import com.yahoo.vespa.athenz.api.AthenzService;
+import com.yahoo.vespa.athenz.identity.ServiceIdentityProvider;
 import com.yahoo.vespa.athenz.identity.SiaIdentityProvider;
 import com.yahoo.vespa.athenz.tls.AthenzIdentityVerifier;
 import com.yahoo.vespa.hosted.node.admin.component.ConfigServerInfo;
 import com.yahoo.vespa.hosted.node.admin.util.PrefixLogger;
 import org.apache.http.HttpHeaders;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
@@ -19,19 +19,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -47,7 +40,6 @@ import static java.util.Collections.singleton;
  * content-type application/json
  *
  * @author dybdahl
- * @author bjorncs
  */
 public class ConfigServerApiImpl implements ConfigServerApi {
     private static final PrefixLogger NODE_ADMIN_LOGGER = PrefixLogger.getNodeAdminLogger(ConfigServerApiImpl.class);
@@ -56,7 +48,15 @@ public class ConfigServerApiImpl implements ConfigServerApi {
 
     private final List<URI> configServers;
 
-    private final CloseableHttpClient client;
+    private Runnable runOnClose = () -> {};
+
+    /**
+     * The 'client' may be periodically re-created through calls to setSSLConnectionSocketFactory.
+     *
+     * The 'client' reference must be volatile because it is set and read in different threads, and visibility
+     * of changes is only guaranteed for volatile variables.
+     */
+    private volatile SelfCloseableHttpClient client;
 
     public static ConfigServerApiImpl create(ConfigServerInfo info, SiaIdentityProvider provider) {
         return new ConfigServerApiImpl(
@@ -77,20 +77,29 @@ public class ConfigServerApiImpl implements ConfigServerApi {
     private ConfigServerApiImpl(Collection<URI> configServers,
                                 HostnameVerifier verifier,
                                 SiaIdentityProvider identityProvider) {
-        this(configServers, createClient(new SSLConnectionSocketFactory(new ServiceIdentitySslSocketFactory(identityProvider), verifier)));
+        this(configServers, createClient(identityProvider.getIdentitySslContext(), verifier));
+
+        // Register callback for updates to the SSLContext
+        ServiceIdentityProvider.Listener listener = (SSLContext sslContext, AthenzService identity) -> {
+            this.client = createClient(sslContext, verifier);
+        };
+        identityProvider.addIdentityListener(listener);
+        this.runOnClose = () -> identityProvider.removeIdentityListener(listener);
     }
 
-    private ConfigServerApiImpl(Collection<URI> configServers, CloseableHttpClient client) {
+    private ConfigServerApiImpl(Collection<URI> configServers, SelfCloseableHttpClient client) {
         this.configServers = randomizeConfigServerUris(configServers);
         this.client = client;
     }
 
-    public static ConfigServerApiImpl createForTesting(List<URI> configServerHosts) {
-        return new ConfigServerApiImpl(configServerHosts, createClient(SSLConnectionSocketFactory.getSocketFactory()));
+    public static ConfigServerApiImpl createForTestingWithSocketFactory(
+            List<URI> configServerHosts,
+            SSLConnectionSocketFactory socketFactory) {
+        return new ConfigServerApiImpl(configServerHosts, new SelfCloseableHttpClient(socketFactory));
     }
 
     static ConfigServerApiImpl createForTestingWithClient(List<URI> configServerHosts,
-                                                          CloseableHttpClient client) {
+                                                          SelfCloseableHttpClient client) {
         return new ConfigServerApiImpl(configServerHosts, client);
     }
 
@@ -174,42 +183,18 @@ public class ConfigServerApiImpl implements ConfigServerApi {
 
     @Override
     public void close() {
-        try {
-            client.close();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        runOnClose.run();
+        client.close();
     }
 
     private void setContentTypeToApplicationJson(HttpRequestBase request) {
         request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
     }
 
-    private static CloseableHttpClient createClient(SSLConnectionSocketFactory socketFactory) {
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", PlainConnectionSocketFactory.getSocketFactory())
-                .register("https", socketFactory)
-                .build();
-
-        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-        cm.setMaxTotal(200); // Increase max total connections to 200, which should be enough
-
-        // Have experienced hang in socket read, which may have been because of
-        // system defaults, therefore set explicit timeouts. Set arbitrarily to
-        // 15s > 10s used by Orchestrator lock timeout.
-        int timeoutMs = 15_000;
-        RequestConfig requestBuilder = RequestConfig.custom()
-                .setConnectTimeout(timeoutMs) // establishment of connection
-                .setConnectionRequestTimeout(timeoutMs) // connection from connection manager
-                .setSocketTimeout(timeoutMs) // waiting for data
-                .build();
-
-        return HttpClientBuilder.create()
-                .setDefaultRequestConfig(requestBuilder)
-                .disableAutomaticRetries()
-                .setUserAgent("node-admin")
-                .setConnectionManager(cm)
-                .build();
+    private static SelfCloseableHttpClient createClient(
+            SSLContext sslContext, HostnameVerifier configServerVerifier) {
+        return new SelfCloseableHttpClient(
+                new SSLConnectionSocketFactory(sslContext, configServerVerifier));
     }
 
     // Shuffle config server URIs to balance load
