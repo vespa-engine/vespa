aafec2c07f9eaf166bd91c8f818256f86d146716
Håkon Hallingstad <hakon@oath.com>
2018-08-31 15:02:41 +0200

    Swap version and release

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/task/util/yum/YumPackageNameTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/task/util/yum/YumPackageNameTest.java
@@ -12,6 +12,16 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class YumPackageNameTest {
+    @Test
+    public void testBuilder() {
+        YumPackageName yumPackage = new YumPackageName.Builder("docker")
+                .setEpoch("2")
+                .setVersion("1.12.6")
+                .setRelease("71.git3e8e77d.el7.centos.1")
+                .setArchitecture("x86_64")
+                .build();
+        assertEquals("2:docker-1.12.6-71.git3e8e77d.el7.centos.1.x86_64", yumPackage.toName());
+    }
 
     @Test
     public void testAllValidFormats() {
