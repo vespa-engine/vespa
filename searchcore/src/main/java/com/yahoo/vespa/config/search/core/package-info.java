ef4331171ee1e46c9bff909da76ce9d6c6477fbf
Jon Bratseth <bratseth@oath.com>
2018-09-05 15:32:43 +0200

    Move to core

--- searchcore/src/main/java/com/yahoo/vespa/config/search/core/package-info.java
+++ searchcore/src/main/java/com/yahoo/vespa/config/search/core/package-info.java
@@ -1,7 +1,7 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 @ExportPackage
-package com.yahoo.vespa.config.search;
+package com.yahoo.vespa.config.search.core;
 
 import com.yahoo.osgi.annotation.ExportPackage;
 
