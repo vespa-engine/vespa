646e8457e29516732749c0abb8b46db66db6ff64
gjoranv <gv@oath.com>
2018-09-05 15:09:18 +0200

    Remove legacy config container.jdisc.config.http-server

--- config-model/src/main/java/com/yahoo/vespa/model/container/http/xml/JettyConnectorBuilder.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/http/xml/JettyConnectorBuilder.java
@@ -15,26 +15,15 @@ import java.util.logging.Logger;
  * @author Einar M R Rosenvinge
  */
 public class JettyConnectorBuilder extends VespaDomBuilder.DomConfigProducerBuilder<ConnectorFactory>  {
-    private static final Logger log = Logger.getLogger(JettyConnectorBuilder.class.getName());
 
     @Override
     protected ConnectorFactory doBuild(AbstractConfigProducer ancestor, Element serverSpec) {
         String name = XmlHelper.getIdString(serverSpec);
         int port = HttpBuilder.readPort(serverSpec, ancestor.getRoot().getDeployState());
 
-        Element legacyServerConfig = XML.getChild(serverSpec, "config");
-        if (legacyServerConfig != null) {
-            String configName = legacyServerConfig.getAttribute("name");
-            if (configName.equals("container.jdisc.config.http-server")) {
-                ancestor.deployLogger().log(Level.WARNING, "The config 'container.jdisc.config.http-server' is deprecated and will be removed in a later version of Vespa."
-                        + " Please use 'jdisc.http.connector' instead, see http://docs.vespa.ai/documentation/jdisc/http-server-and-filters.html#configuring-jetty-server");
-            } else {
-                legacyServerConfig = null;
-            }
-        }
         Element sslKeystoreConfigurator = XML.getChild(serverSpec, "ssl-keystore-configurator");
         Element sslTruststoreConfigurator = XML.getChild(serverSpec, "ssl-truststore-configurator");
-        return new ConnectorFactory(name, port, legacyServerConfig, sslKeystoreConfigurator, sslTruststoreConfigurator);
+        return new ConnectorFactory(name, port, sslKeystoreConfigurator, sslTruststoreConfigurator);
     }
 
 }
