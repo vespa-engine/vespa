38ae213aad060dcd031024f355b45c4dfe6f3582
gjoranv <gv@oath.com>
2018-09-07 16:07:53 +0200

    Remove legacy 'yamas' element under 'admin'
    - Use 'monitoring' instead.

--- config-model/src/test/java/com/yahoo/vespa/model/builder/xml/dom/DomAdminV2BuilderTest.java
+++ config-model/src/test/java/com/yahoo/vespa/model/builder/xml/dom/DomAdminV2BuilderTest.java
@@ -72,7 +72,7 @@ public class DomAdminV2BuilderTest extends DomBuilderTest {
                         "    <configserver hostalias=\"mockhost\"/>" +
                         "  </configservers>" +
                         "    <adminserver hostalias=\"mockhost\"/>" +
-                        "  <yamas systemname=\"foo\"/>" +
+                        "  <monitoring systemname=\"foo\"/>" +
                         "</admin>").getDocumentElement();
     }
 
@@ -100,7 +100,7 @@ public class DomAdminV2BuilderTest extends DomBuilderTest {
                         "    <configserver hostalias=\"mockhost\"/>" +
                         "  </configservers>" +
                         "    <adminserver hostalias=\"mockhost\"/>" +
-                        "  <yamas systemname=\"foo\" interval=\"300\"/>" +
+                        "  <monitoring systemname=\"foo\" interval=\"300\"/>" +
                         "</admin>").getDocumentElement();
     }
 
@@ -186,7 +186,7 @@ public class DomAdminV2BuilderTest extends DomBuilderTest {
         Element servicesYamasIllegalInterval = XML.getDocument(
                         "<admin version=\"2.0\">" +
                         "  <adminserver hostalias=\"mockhost\"/>" +
-                        "  <yamas interval=\"5\"/>" +
+                        "  <monitoring interval=\"5\"/>" +
                         "</admin>").getDocumentElement();
         Admin admin = buildAdmin(servicesYamasIllegalInterval);
     }
