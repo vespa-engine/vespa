c5cd5a7cf5ad938c6539294b592fbd6b8a4c0aee
Henning Baldersheim <balder@oath.com>
2018-09-26 11:27:35 +0200

    Add newline and rewrite error handling to a linear sequence of guards.

--- vespalib/src/apps/vespa-drop-file-from-cache/drop_file_from_cache.cpp
+++ vespalib/src/apps/vespa-drop-file-from-cache/drop_file_from_cache.cpp
@@ -9,24 +9,24 @@
 int main(int argc, char **argv) {
     char errorBuf[200];
     if (argc != 2) {
-        fprintf(stderr, "%s <filename>", argv[0]);
+        fprintf(stderr, "%s <filename>\n", argv[0]);
         return 1;
     }
     const char *fileName = argv[1];
     int fh = open(fileName, O_RDONLY);
-    if (fh != -1) {
-        int retval = 0;
-        int err = posix_fadvise(fh, 0, 0, POSIX_FADV_DONTNEED);
-        if (err != 0) {
-            const char *errorString = strerror_r(errno, errorBuf, sizeof(errorBuf));
-            fprintf(stderr, "posix_fadvise failed: %s", errorString);
-            retval = 3;
-        }
-        close(fh);
-        return retval;
-    } else {
+    if (fh == -1) {
         const char *errorString = strerror_r(errno, errorBuf, sizeof(errorBuf));
-        fprintf(stderr, "Failed opening file %s: %s", fileName, errorString);
+        fprintf(stderr, "Failed opening file %s: %s\n", fileName, errorString);
         return 2;
     }
+
+    int retval = 0;
+    int err = posix_fadvise(fh, 0, 0, POSIX_FADV_DONTNEED);
+    if (err != 0) {
+        const char *errorString = strerror_r(errno, errorBuf, sizeof(errorBuf));
+        fprintf(stderr, "posix_fadvise failed: %s\n", errorString);
+        retval = 3;
+    }
+    close(fh);
+    return retval;
 }
