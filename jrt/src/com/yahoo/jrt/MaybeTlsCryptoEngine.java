9bd9e745301c2518434e69cbc7aff7dd648cbdfa
Håvard Pettersen <havardpe@oath.com>
2018-09-26 06:07:26 +0000

    address naming concerns

--- jrt/src/com/yahoo/jrt/MaybeTlsCryptoEngine.java
+++ jrt/src/com/yahoo/jrt/MaybeTlsCryptoEngine.java
@@ -7,28 +7,28 @@ import java.nio.channels.SocketChannel;
  * A crypto engine that supports both tls encrypted connections and
  * unencrypted connections. The use of tls for incoming connections is
  * auto-detected using clever heuristics. The use of tls for outgoing
- * connections is controlled by the useTls flag given to the
+ * connections is controlled by the useTlsWhenClient flag given to the
  * constructor.
  **/
 public class MaybeTlsCryptoEngine implements CryptoEngine {
 
     private final TlsCryptoEngine tlsEngine;
-    private final boolean useTls;
+    private final boolean useTlsWhenClient;
 
-    public MaybeTlsCryptoEngine(TlsCryptoEngine tlsEngine, boolean useTls) {
+    public MaybeTlsCryptoEngine(TlsCryptoEngine tlsEngine, boolean useTlsWhenClient) {
         this.tlsEngine = tlsEngine;
-        this.useTls = useTls;
+        this.useTlsWhenClient = useTlsWhenClient;
     }
 
     @Override public CryptoSocket createCryptoSocket(SocketChannel channel, boolean isServer) {
         if (isServer) {
             return new MaybeTlsCryptoSocket(channel, tlsEngine);
-        } else if (useTls) {
+        } else if (useTlsWhenClient) {
             return tlsEngine.createCryptoSocket(channel, false);
         } else {
             return new NullCryptoSocket(channel);
         }
     }
 
-    @Override public String toString() { return "MaybeTlsCryptoEngine(useTls:" + useTls + ")"; }
+    @Override public String toString() { return "MaybeTlsCryptoEngine(useTlsWhenClient:" + useTlsWhenClient + ")"; }
 }
