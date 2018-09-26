7274c2c57bbc2fd24b092478af5d65d14c18213d
Bjørn Christian Seime <bjorncs@oath.com>
2018-09-26 17:34:11 +0200

    Remove AccessLogEntry.setURI/getURI

--- jdisc_http_service/src/test/java/com/yahoo/jdisc/http/server/jetty/AccessLogRequestLogTest.java
+++ jdisc_http_service/src/test/java/com/yahoo/jdisc/http/server/jetty/AccessLogRequestLogTest.java
@@ -15,11 +15,12 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertTrue;
 
 /**
- * @author bakksjo
+ * @author Oyvind Bakksjo
+ * @author bjorncs
  */
-@SuppressWarnings("deprecation") // AccessLogEntry.setURI/getURI are deprecated
 public class AccessLogRequestLogTest {
     @Test
     public void requireThatQueryWithUnquotedSpecialCharactersIsHandled() {
@@ -30,7 +31,8 @@ public class AccessLogRequestLogTest {
 
         AccessLogRequestLog.populateAccessLogEntryFromHttpServletRequest(httpServletRequest, accessLogEntry);
 
-        assertThat(accessLogEntry.getURI(), is(not(nullValue())));
+        assertThat(accessLogEntry.getRawPath(), is(not(nullValue())));
+        assertTrue(accessLogEntry.getRawQuery().isPresent());
     }
 
     @Test
@@ -44,34 +46,9 @@ public class AccessLogRequestLogTest {
 
         AccessLogRequestLog.populateAccessLogEntryFromHttpServletRequest(httpServletRequest, accessLogEntry);
 
-        assertThat(accessLogEntry.getURI().toString(), is(path + '?' + query));
+        assertThat(accessLogEntry.getRawPath(), is(path));
+        assertThat(accessLogEntry.getRawQuery().get(), is(query));
 
-    }
-
-    @Test
-    public void requireThatNoQueryPartIsHandledWhenRequestIsMalformed() {
-        final HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
-        final String path = "/s>earch/";
-        when(httpServletRequest.getRequestURI()).thenReturn(path);
-        final String query = null;
-        when(httpServletRequest.getQueryString()).thenReturn(query);
-        final AccessLogEntry accessLogEntry = new AccessLogEntry();
-
-        AccessLogRequestLog.populateAccessLogEntryFromHttpServletRequest(httpServletRequest, accessLogEntry);
-
-        assertThat(accessLogEntry.getURI().toString(), is("/s%3Eearch/"));
-
-    }
-
-    @Test
-    public void invalid_percent_escape_patterns_in_query_string_are_escaped() {
-        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
-        when(httpServletRequest.getRequestURI()).thenReturn("/search/");
-        when(httpServletRequest.getQueryString()).thenReturn("q=%%2");
-
-        AccessLogEntry accessLogEntry = new AccessLogEntry();
-        AccessLogRequestLog.populateAccessLogEntryFromHttpServletRequest(httpServletRequest, accessLogEntry);
-        assertThat(accessLogEntry.getURI().toString(), is("/search/?q=%25%252"));
     }
 
     @Test
