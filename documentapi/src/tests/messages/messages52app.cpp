1dce891d58da218700fbb806b34f0c324175c135
Tor Egge <Tor.Egge@broadpark.no>
2018-09-28 21:32:58 +0200

    Only allow document api protocol version 6.221 (C++)

--- documentapi/src/tests/messages/messages52app.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-#include "messages52test.h"
-
-TEST_APPHOOK(Messages52Test);
