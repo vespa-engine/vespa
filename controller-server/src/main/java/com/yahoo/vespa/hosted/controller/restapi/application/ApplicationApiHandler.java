697513612152dceff2ed41540b9c820e60928420
Frode Lundgren <frodelu@frode.lu>
2018-09-28 12:54:10 +0200

    Revert "Revert "Adding getConfigServerApiUris() to include both cfg servers and VIP [VESPA-12701]""

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/application/ApplicationApiHandler.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/application/ApplicationApiHandler.java
@@ -639,7 +639,7 @@ public class ApplicationApiHandler extends LoggingRequestHandler {
         ApplicationView applicationView = controller.getApplicationView(tenantName, applicationName, instanceName, environment, region);
         ServiceApiResponse response = new ServiceApiResponse(ZoneId.from(environment, region),
                                                              new ApplicationId.Builder().tenant(tenantName).applicationName(applicationName).instanceName(instanceName).build(),
-                                                             controller.zoneRegistry().getConfigServerUris(ZoneId.from(environment, region)),
+                                                             controller.zoneRegistry().getConfigServerApiUris(ZoneId.from(environment, region)),
                                                              request.getUri());
         response.setResponse(applicationView);
         return response;
@@ -649,7 +649,7 @@ public class ApplicationApiHandler extends LoggingRequestHandler {
         Map<?,?> result = controller.getServiceApiResponse(tenantName, applicationName, instanceName, environment, region, serviceName, restPath);
         ServiceApiResponse response = new ServiceApiResponse(ZoneId.from(environment, region),
                                                              new ApplicationId.Builder().tenant(tenantName).applicationName(applicationName).instanceName(instanceName).build(),
-                                                             controller.zoneRegistry().getConfigServerUris(ZoneId.from(environment, region)),
+                                                             controller.zoneRegistry().getConfigServerApiUris(ZoneId.from(environment, region)),
                                                              request.getUri());
         response.setResponse(result, serviceName, restPath);
         return response;
