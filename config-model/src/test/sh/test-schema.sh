5d306f8d4f553da45036a5c24442a57a8a0eedbc
Bjørn Christian Seime <bjorncs@oath.com>
2018-10-05 17:56:26 +0200

    Don't build twice

--- config-model/src/test/sh/test-schema.sh
+++ config-model/src/test/sh/test-schema.sh
@@ -2,9 +2,6 @@
 # Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 set -e
 
-pushd src/main && make clean && make all
-popd
-
 jar="target/jing.jar"
 mainclass="com/thaiopensource/relaxng/util/Driver"
 
