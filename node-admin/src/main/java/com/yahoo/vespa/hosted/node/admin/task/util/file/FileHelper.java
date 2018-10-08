281c869f79053ceea0145d6fdbc18d7c6c54b9aa
Valerij Fredriksen <valerijf@oath.com>
2018-10-08 12:58:48 +0200

    Create new FileHelper

---
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/task/util/file/FileHelper.java
@@ -0,0 +1,263 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.node.admin.task.util.file;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static com.yahoo.vespa.hosted.node.admin.task.util.file.IOExceptionUtil.uncheck;
+
+/**
+ * @author freva
+ */
+public class FileHelper {
+
+    /**
+     * Creates a {@link FileStreamBuilder} that will by default match all files and all directories
+     * under the given basePath.
+     */
+    public FileStreamBuilder streamContents(Path basePath) {
+        return new FileStreamBuilder(basePath, true, true);
+    }
+
+    /**
+     * Creates a {@link FileStreamBuilder} that will by default match all files and no directories
+     * under the given basePath.
+     */
+    public FileStreamBuilder streamFiles(Path basePath) {
+        return new FileStreamBuilder(basePath, true, false);
+    }
+
+    /**
+     * Creates a {@link FileStreamBuilder} that will by default match all directories and no files
+     * under the given basePath.
+     */
+    public FileStreamBuilder streamDirectories(Path basePath) {
+        return new FileStreamBuilder(basePath, false, true);
+    }
+
+    public class FileStreamBuilder {
+        private final Path basePath;
+        private Predicate<FileAttributes> fileFilter;
+        private Predicate<FileAttributes> directoryFilter;
+        private boolean includeBase = true;
+        private boolean recursive = false;
+
+        public FileStreamBuilder(Path basePath, boolean includeFiles, boolean includeDirectories) {
+            this.basePath = basePath;
+            this.fileFilter = path -> includeFiles;
+            this.directoryFilter = path -> includeDirectories;
+        }
+
+        /**
+         * Filter that will be used to match files under the base path. Files include everything that
+         * is not a directory (such as symbolic links)
+         */
+        public FileStreamBuilder filterFile(Predicate<FileAttributes> fileFilter) {
+            this.fileFilter = fileFilter;
+            return this;
+        }
+
+        /**
+         * Filter that will be used to match directories under the base path.
+         *
+         * NOTE: When a directory is matched, all of its sub-directories and files are also matched
+         */
+        public FileStreamBuilder filterDirectory(Predicate<FileAttributes> directoryFilter) {
+            this.directoryFilter = directoryFilter;
+            return this;
+        }
+
+        /**
+         * Whether the search should be recursive.
+         *
+         * WARNING: When using {@link #delete()} and matching directories, make sure that the directories
+         * either are already empty or that recursive is set
+         */
+        public FileStreamBuilder recursive(boolean recursive) {
+            this.recursive = recursive;
+            return this;
+        }
+
+        /**
+         * Whether the base path should also be considered (i.e. checked against the correspoding filter).
+         * When using {@link #delete()} with directories, this is the difference between
+         * `rm -rf basePath` (true) and `rm -rf basePath/*` (false)
+         */
+        public FileStreamBuilder includeBase(boolean includeBase) {
+            this.includeBase = includeBase;
+            return this;
+        }
+
+        public int delete() {
+            int[] numDeletions = { 0 }; // :(
+            forEach(attributes -> {
+                if (deleteIfExists(attributes.path()))
+                    numDeletions[0]++;
+            });
+
+            return numDeletions[0];
+        }
+
+        public List<FileAttributes> list() {
+            LinkedList<FileAttributes> list = new LinkedList<>();
+            forEach(list::add);
+            return list;
+        }
+
+        public Stream<FileAttributes> stream() {
+            return list().stream();
+        }
+
+        public void forEachPath(Consumer<Path> action) {
+            forEach(attributes -> action.accept(attributes.path()));
+        }
+
+        /** Applies a given consumer to all the matching {@link FileHelper.FileAttributes} */
+        public void forEach(Consumer<FileAttributes> action) {
+            applyForEachToMatching(basePath, fileFilter, directoryFilter, recursive, includeBase, action);
+        }
+    }
+
+    /**
+     * <p> This method walks a file tree rooted at a given starting file. The file tree traversal is
+     * <em>depth-first</em>: The filter function is applied in pre-order (NLR), but the given
+     * {@link Consumer} will be called in post-order (LRN).
+     */
+    private void applyForEachToMatching(Path basePath, Predicate<FileAttributes> fileFilter, Predicate<FileAttributes> directoryFilter,
+                                        boolean recursive, boolean includeBase, Consumer<FileAttributes> action) {
+        try {
+            Files.walkFileTree(basePath, Collections.emptySet(), recursive ? Integer.MAX_VALUE : 1, new SimpleFileVisitor<Path>() {
+                private Stack<FileAttributes> matchingDirectoryStack = new Stack<>();
+                private int currentLevel = -1;
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    currentLevel++;
+
+                    FileAttributes attributes = new FileAttributes(dir, attrs);
+                    if (!matchingDirectoryStack.empty() || directoryFilter.test(attributes))
+                        matchingDirectoryStack.push(attributes);
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    // When we find a directory at the max depth given to Files.walkFileTree, the directory
+                    // will be passed to visitFile() rather than (pre|post)VisitDirectory
+                    if (attrs.isDirectory()) {
+                        preVisitDirectory(file, attrs);
+                        return postVisitDirectory(file, null);
+                    }
+
+                    FileAttributes attributes = new FileAttributes(file, attrs);
+                    if (!matchingDirectoryStack.isEmpty() || fileFilter.test(attributes))
+                        action.accept(attributes);
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                    if (!matchingDirectoryStack.isEmpty()) {
+                        FileAttributes attributes = matchingDirectoryStack.pop();
+                        if (currentLevel != 0 || includeBase)
+                            action.accept(attributes);
+                    }
+
+                    currentLevel--;
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (NoSuchFileException ignored) {
+
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+
+    // Ideally, we would reuse the FileAttributes in this package, but unfortunately we only get
+    // BasicFileAttributes and not PosixFileAttributes from FileVisitor
+    public static class FileAttributes {
+        private final Path path;
+        private final BasicFileAttributes attributes;
+
+        FileAttributes(Path path, BasicFileAttributes attributes) {
+            this.path = path;
+            this.attributes = attributes;
+        }
+
+        public Path path() { return path; }
+        public String filename() { return path.getFileName().toString(); }
+        public Instant lastModifiedTime() { return attributes.lastModifiedTime().toInstant(); }
+        public boolean isRegularFile() { return attributes.isRegularFile(); }
+        public boolean isDirectory() { return attributes.isDirectory(); }
+        public long size() { return attributes.size(); }
+    }
+
+
+    // Filters
+    public static Predicate<FileAttributes> olderThan(Duration duration) {
+        return attrs -> Duration.between(attrs.lastModifiedTime(), Instant.now()).compareTo(duration) > 0;
+    }
+
+    public static Predicate<FileAttributes> youngerThan(Duration duration) {
+        return olderThan(duration).negate();
+    }
+
+    public static Predicate<FileAttributes> largerThan(long sizeInBytes) {
+        return attrs -> attrs.size() > sizeInBytes;
+    }
+
+    public static Predicate<FileAttributes> smallerThan(long sizeInBytes) {
+        return largerThan(sizeInBytes).negate();
+    }
+
+    public static Predicate<FileAttributes> nameMatches(Pattern pattern) {
+        return attrs -> pattern.matcher(attrs.filename()).matches();
+    }
+
+    public static Predicate<FileAttributes> nameStartsWith(String string) {
+        return attrs -> attrs.filename().startsWith(string);
+    }
+
+    public static Predicate<FileAttributes> nameEndsWith(String string) {
+        return attrs -> attrs.filename().endsWith(string);
+    }
+
+
+    // Other helpful methods that no not throw checked exceptions
+    public boolean moveIfExists(Path from, Path to) {
+        try {
+            Files.move(from, to);
+            return true;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public boolean deleteIfExists(Path path) {
+        return uncheck(() -> Files.deleteIfExists(path));
+    }
+
+    public Path createDirectories(Path path) {
+        return uncheck(() -> Files.createDirectories(path));
+    }
+}
---
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/task/util/file/FileHelperTest.java
@@ -0,0 +1,202 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.node.admin.task.util.file;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author freva
+ */
+@RunWith(Enclosed.class)
+public class FileHelperTest {
+
+    public static class GeneralLogicTests {
+        @Rule
+        public TemporaryFolder folder = new TemporaryFolder();
+        private final FileHelper fileHelper = new FileHelper();
+
+        @Test
+        public void delete_all_files_non_recursive() {
+            int numDeleted = fileHelper.streamFiles(testRoot())
+                    .delete();
+
+            assertEquals(3, numDeleted);
+            assertRecursiveContents("test", "test/file.txt", "test/data.json", "test/subdir-1", "test/subdir-1/file", "test/subdir-2");
+        }
+
+        @Test
+        public void delete_all_files_recursive() {
+            int numDeleted = fileHelper.streamFiles(testRoot())
+                    .recursive(true)
+                    .delete();
+
+            assertEquals(6, numDeleted);
+            assertRecursiveContents("test", "test/subdir-1", "test/subdir-2");
+        }
+
+        @Test
+        public void delete_with_filter_recursive() {
+            int numDeleted = fileHelper.streamFiles(testRoot())
+                    .filterFile(FileHelper.nameEndsWith(".json"))
+                    .recursive(true)
+                    .delete();
+
+            assertEquals(3, numDeleted);
+            assertRecursiveContents("test.txt", "test", "test/file.txt", "test/subdir-1", "test/subdir-1/file", "test/subdir-2");
+        }
+
+        @Test
+        public void delete_directory_with_filter() {
+            int numDeleted = fileHelper.streamDirectories(testRoot())
+                    .filterDirectory(FileHelper.nameStartsWith("subdir"))
+                    .recursive(true)
+                    .delete();
+
+            assertEquals(3, numDeleted);
+            assertRecursiveContents("file-1.json", "test.json", "test.txt", "test", "test/file.txt", "test/data.json");
+        }
+
+        @Test
+        public void delete_all_contents() {
+            int numDeleted = fileHelper.streamContents(testRoot())
+                    .recursive(true)
+                    .includeBase(false)
+                    .delete();
+
+            assertEquals(9, numDeleted);
+            assertTrue(Files.exists(testRoot()));
+            assertRecursiveContents();
+        }
+
+        @Test
+        public void delete_everything() {
+            int numDeleted = fileHelper.streamContents(testRoot())
+                    .recursive(true)
+                    .delete();
+
+            assertEquals(10, numDeleted);
+            assertFalse(Files.exists(testRoot()));
+        }
+
+        @Before
+        public void setup() throws IOException {
+            Path root = testRoot();
+
+            Files.createFile(root.resolve("file-1.json"));
+            Files.createFile(root.resolve("test.json"));
+            Files.createFile(root.resolve("test.txt"));
+
+            Files.createDirectories(root.resolve("test"));
+            Files.createFile(root.resolve("test/file.txt"));
+            Files.createFile(root.resolve("test/data.json"));
+
+            Files.createDirectories(root.resolve("test/subdir-1"));
+            Files.createFile(root.resolve("test/subdir-1/file"));
+
+            Files.createDirectories(root.resolve("test/subdir-2"));
+        }
+
+        private Path testRoot() {
+            return folder.getRoot().toPath();
+        }
+
+        private void assertRecursiveContents(String... relativePaths) {
+            Set<String> expectedPaths = new HashSet<>(Arrays.asList(relativePaths));
+            Set<String> actualPaths = recursivelyListContents(testRoot()).stream()
+                    .map(testRoot()::relativize)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
+
+            assertEquals(expectedPaths, actualPaths);
+        }
+
+        private List<Path> recursivelyListContents(Path basePath) {
+            try (Stream<Path> pathStream = Files.list(basePath)) {
+                List<Path> paths = new LinkedList<>();
+                pathStream.forEach(path -> {
+                    paths.add(path);
+                    if (Files.isDirectory(path))
+                        paths.addAll(recursivelyListContents(path));
+                });
+                return paths;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+
+    public static class FilterUnitTests {
+
+        private final BasicFileAttributes attributes = mock(BasicFileAttributes.class);
+
+        @Test
+        public void age_filter_test() {
+            Path path = Paths.get("/my/fake/path");
+            when(attributes.lastModifiedTime()).thenReturn(FileTime.from(Instant.now().minus(Duration.ofHours(1))));
+            FileHelper.FileAttributes fileAttributes = new FileHelper.FileAttributes(path, attributes);
+
+            assertFalse(FileHelper.olderThan(Duration.ofMinutes(61)).test(fileAttributes));
+            assertTrue(FileHelper.olderThan(Duration.ofMinutes(59)).test(fileAttributes));
+
+            assertTrue(FileHelper.youngerThan(Duration.ofMinutes(61)).test(fileAttributes));
+            assertFalse(FileHelper.youngerThan(Duration.ofMinutes(59)).test(fileAttributes));
+        }
+
+        @Test
+        public void size_filters() {
+            Path path = Paths.get("/my/fake/path");
+            when(attributes.size()).thenReturn(100L);
+            FileHelper.FileAttributes fileAttributes = new FileHelper.FileAttributes(path, attributes);
+
+            assertFalse(FileHelper.largerThan(101).test(fileAttributes));
+            assertTrue(FileHelper.largerThan(99).test(fileAttributes));
+
+            assertTrue(FileHelper.smallerThan(101).test(fileAttributes));
+            assertFalse(FileHelper.smallerThan(99).test(fileAttributes));
+        }
+
+        @Test
+        public void filename_filters() {
+            Path path = Paths.get("/my/fake/path/some-12352-file.json");
+            FileHelper.FileAttributes fileAttributes = new FileHelper.FileAttributes(path, attributes);
+
+            assertTrue(FileHelper.nameStartsWith("some-").test(fileAttributes));
+            assertFalse(FileHelper.nameStartsWith("som-").test(fileAttributes));
+
+            assertTrue(FileHelper.nameEndsWith(".json").test(fileAttributes));
+            assertFalse(FileHelper.nameEndsWith("file").test(fileAttributes));
+
+            assertTrue(FileHelper.nameMatches(Pattern.compile("some-[0-9]+-file.json")).test(fileAttributes));
+            assertTrue(FileHelper.nameMatches(Pattern.compile("^some-[0-9]+-file.json$")).test(fileAttributes));
+            assertFalse(FileHelper.nameMatches(Pattern.compile("some-[0-9]-file.json")).test(fileAttributes));
+        }
+    }
+}
