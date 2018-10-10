a89f2f3535d2aa561d9158aa668ed874f5af6d25
Bjørn Christian Seime <bjorncs@oath.com>
2018-10-10 16:05:14 +0200

    Make Json default output format for vespaget and vespavisit
    - Also introduce --xmloutput parameter

--- vespaclient-java/src/main/java/com/yahoo/vespavisit/VdsVisit.java
+++ vespaclient-java/src/main/java/com/yahoo/vespavisit/VdsVisit.java
@@ -345,16 +345,15 @@ public class VdsVisit {
 
         options.addOption(Option.builder()
                 .longOpt("jsonoutput")
-                .desc("Output documents as JSON")
+                .desc("Output documents as JSON (default format)")
                 .hasArg(false)
                 .build());
 
         options.addOption(Option.builder("x")
               .longOpt("xmloutput")
-              .desc("Output documents as XML (default format)")
+              .desc("Output documents as XML")
               .hasArg(false)
               .build());
-
 
         options.addOption(Option.builder()
                 .longOpt("bucketspace")
@@ -586,7 +585,7 @@ public class VdsVisit {
             if (jsonOutput && xmlOutput) {
                 throw new IllegalArgumentException("Cannot combine both xml and json output");
             }
-            allParams.setJsonOutput((!jsonOutput && !xmlOutput) ? false : jsonOutput); // TODO Vespa 7 Change default to JSON
+            allParams.setJsonOutput(!xmlOutput);
 
             allParams.setVisitorParameters(params);
             return allParams;
