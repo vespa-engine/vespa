433b7aad5a4427971c867f3d28e100e967fb0ffa
gjoranv <gv@oath.com>
2018-10-10 15:31:09 +0200

    Remove deprecated apis in linguistics.

--- linguistics/src/main/java/com/yahoo/language/process/StemMode.java
+++ linguistics/src/main/java/com/yahoo/language/process/StemMode.java
@@ -22,26 +22,4 @@ public enum StemMode {
         this.value = value;
     }
 
-    /**
-     * Returns the stem mode as an int
-     *
-     * @deprecated do not use
-     */
-    // TODO: Remove on Vespa 7
-    @Deprecated // OK
-    public int getValue() {
-        return value;
-    }
-
-    @Deprecated // OK
-    // TODO: Remove on Vespa 7
-    public static StemMode valueOf(int value) {
-        for (StemMode mode : values()) {
-            if (mode.value == value) {
-                return mode;
-            }
-        }
-        return NONE;
-    }
-
 }
