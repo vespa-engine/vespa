b30ffa6d4a6b8fd298003ed828c274e8006286f8
gjoranv <gv@oath.com>
2018-10-10 15:31:09 +0200

    Remove deprecated apis in linguistics.

--- linguistics/src/main/java/com/yahoo/language/simple/kstem/CharacterUtils.java
+++ linguistics/src/main/java/com/yahoo/language/simple/kstem/CharacterUtils.java
@@ -24,16 +24,6 @@ public abstract class CharacterUtils {
   public static CharacterUtils getInstance() {
     return JAVA_5;
   }
-  
-  /** 
-   * explicitly returns a version matching java 4 semantics 
-   * @deprecated Only for n-gram backwards compat
-   */
-  // TODO: Remove on Vespa 7
-  @Deprecated // OK
-  public static CharacterUtils getJava4Instance() {
-    return JAVA_4;
-  }
 
   /**
    * Returns the code point at the given index of the {@link CharSequence}.
