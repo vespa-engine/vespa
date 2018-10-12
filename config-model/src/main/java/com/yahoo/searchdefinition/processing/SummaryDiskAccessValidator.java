3c202499a84914d66fadde9fbc0a8ac2fdeb96e3
Henning Baldersheim <balder@yahoo-inc.com>
2018-10-12 14:34:47 +0200

    Do not warn until Vespa 7

--- config-model/src/main/java/com/yahoo/searchdefinition/processing/SummaryDiskAccessValidator.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/processing/SummaryDiskAccessValidator.java
@@ -42,10 +42,11 @@ public class SummaryDiskAccessValidator extends Processor {
                         throw new IllegalArgumentException(summaryField + " in " + summary + " references " +
                                                            source + ", but this field does not exist");
                     if ( ! isInMemory(field) && ! summary.isFromDisk()) {
-                        deployLogger.log(Level.WARNING, summaryField + " in " + summary + " references " +
-                                                        source + ", which is not an attribute: Using this " +
-                                                        "summary will cause disk accesses. " +
-                                                        "Set 'from-disk' on this summary class to silence this warning.");
+                        // TODO: Set to warning on vespa 7
+                        deployLogger.log(Level.FINE, summaryField + " in " + summary + " references " +
+                                                     source + ", which is not an attribute: Using this " +
+                                                     "summary will cause disk accesses. " +
+                                                     "Set 'from-disk' on this summary class to silence this warning.");
                     }
                 }
             }
