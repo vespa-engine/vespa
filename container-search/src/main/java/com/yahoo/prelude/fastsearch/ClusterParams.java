169592d762bcfeab01aae2ee94eed6e25a452e4d
Arne Juul <arnej@yahoo-inc.com>
2018-10-18 07:40:02 +0000

    remove container.legacy-emulation config

--- container-search/src/main/java/com/yahoo/prelude/fastsearch/ClusterParams.java
+++ container-search/src/main/java/com/yahoo/prelude/fastsearch/ClusterParams.java
@@ -1,8 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.prelude.fastsearch;
 
-import com.yahoo.container.search.LegacyEmulationConfig;
-
 /**
  * Helper class for carrying around cluster-related
  * config parameters to the FastSearcher class.
@@ -12,21 +10,12 @@ import com.yahoo.container.search.LegacyEmulationConfig;
 public class ClusterParams {
 
     public final String searcherName;
-    public final LegacyEmulationConfig emulation;
-
-    /**
-     * For testcases only
-     */
-    public ClusterParams(String name) {
-        this(name, new LegacyEmulationConfig(new LegacyEmulationConfig.Builder()));
-    }
 
     /**
      * Make up full ClusterParams
      */
-    public ClusterParams(String name, LegacyEmulationConfig cfg) {
+    public ClusterParams(String name) {
         this.searcherName = name;
-        this.emulation = cfg;
     }
 
 }
