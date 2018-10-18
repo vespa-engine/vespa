4f6043b866dc39865eb680bd62fd31cfe1eb9a4a
Martin Polden <mpolden@mpolden.no>
2018-10-18 10:25:16 +0200

    Add SSL provider for configuring controller HTTPS connector

---
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/tls/SecretStoreMock.java
@@ -0,0 +1,27 @@
+// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.controller.tls;
+
+import com.google.inject.Inject;
+import com.yahoo.security.KeyUtils;
+import com.yahoo.security.X509CertificateUtils;
+import com.yahoo.vespa.hosted.controller.tls.config.TlsConfig;
+
+/**
+ * A secret store mock that's pre-populated with a certificate and key.
+ *
+ * @author mpolden
+ */
+@SuppressWarnings("unused") // Injected
+public class SecretStoreMock extends com.yahoo.vespa.hosted.controller.integration.SecretStoreMock {
+
+    @Inject
+    public SecretStoreMock(TlsConfig config) {
+        addKeyPair(config);
+    }
+
+    private void addKeyPair(TlsConfig config) {
+        setSecret(config.privateKeySecret(), KeyUtils.toPem(Keys.keyPair.getPrivate()));
+        setSecret(config.certificateSecret(), X509CertificateUtils.toPem(Keys.certificate));
+    }
+
+}
