e4fbdc2b8574d79344a43563b8f1ddb3db130b21
Martin Polden <mpolden@mpolden.no>
2018-10-25 12:37:20 +0200

    Revert "Remove PR instances"
    This reverts commit c49cdc0e0ef900cb4cda93bcfdb88692767ae46e.

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/ControllerMaintenance.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/ControllerMaintenance.java
@@ -48,7 +48,6 @@ public class ControllerMaintenance extends AbstractComponent {
     private final OsVersionStatusUpdater osVersionStatusUpdater;
     private final JobRunner jobRunner;
     private final ContactInformationMaintainer contactInformationMaintainer;
-    private final DatabaseMaintainer databaseMaintainer;
 
     @SuppressWarnings("unused") // instantiated by Dependency Injection
     public ControllerMaintenance(MaintainerConfig maintainerConfig, ApiAuthorityConfig apiAuthorityConfig, Controller controller, CuratorDb curator,
@@ -75,7 +74,6 @@ public class ControllerMaintenance extends AbstractComponent {
         osUpgraders = osUpgraders(controller, jobControl);
         osVersionStatusUpdater = new OsVersionStatusUpdater(controller, maintenanceInterval, jobControl);
         contactInformationMaintainer = new ContactInformationMaintainer(controller, Duration.ofHours(12), jobControl, organization, apiAuthorityConfig);
-        databaseMaintainer = new DatabaseMaintainer(controller, maintenanceInterval, jobControl);
     }
 
     public Upgrader upgrader() { return upgrader; }
@@ -102,7 +100,6 @@ public class ControllerMaintenance extends AbstractComponent {
         osVersionStatusUpdater.deconstruct();
         jobRunner.deconstruct();
         contactInformationMaintainer.deconstruct();
-        databaseMaintainer.deconstruct();
     }
 
     /** Create one OS upgrader per cloud found in the zone registry of controller */
