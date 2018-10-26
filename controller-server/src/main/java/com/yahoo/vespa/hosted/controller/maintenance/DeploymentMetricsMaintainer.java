3e1c1bbf27ece0cc3846df31f13bdedf68453f18
olaaun <olaa@oath.com>
2018-10-26 10:22:00 +0200

    Revert "Olaaun/metric maintaier use api (#7402)" (#7461)
    This reverts commit 57ba28e794416ae7cc6e14660249459dac7df937.

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/DeploymentMetricsMaintainer.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/DeploymentMetricsMaintainer.java
@@ -2,23 +2,15 @@
 package com.yahoo.vespa.hosted.controller.maintenance;
 
 import com.yahoo.config.provision.HostName;
-import com.yahoo.slime.Cursor;
-import com.yahoo.slime.Slime;
-import com.yahoo.vespa.config.SlimeUtils;
 import com.yahoo.vespa.hosted.controller.Application;
 import com.yahoo.vespa.hosted.controller.ApplicationController;
 import com.yahoo.vespa.hosted.controller.Controller;
 import com.yahoo.vespa.hosted.controller.api.integration.MetricsService;
 import com.yahoo.vespa.hosted.controller.application.Deployment;
+import com.yahoo.vespa.hosted.controller.application.DeploymentMetrics;
 import com.yahoo.vespa.hosted.controller.application.RotationStatus;
-import com.yahoo.vespa.hosted.controller.authority.config.ApiAuthorityConfig;
 import com.yahoo.yolean.Exceptions;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -45,12 +37,10 @@ public class DeploymentMetricsMaintainer extends Maintainer {
     private static final int applicationsToUpdateInParallel = 10;
 
     private final ApplicationController applications;
-    private final List<String> baseUris;
 
-    public DeploymentMetricsMaintainer(Controller controller, Duration duration, JobControl jobControl, ApiAuthorityConfig apiAuthorityConfig) {
+    public DeploymentMetricsMaintainer(Controller controller, Duration duration, JobControl jobControl) {
         super(controller, duration, jobControl);
         this.applications = controller.applications();
-        baseUris = apiAuthorityConfig.authorities();
     }
 
     @Override
@@ -61,27 +51,37 @@ public class DeploymentMetricsMaintainer extends Maintainer {
 
         // Run parallel stream inside a custom ForkJoinPool so that we can control the number of threads used
         ForkJoinPool pool = new ForkJoinPool(applicationsToUpdateInParallel);
-        Slime slime = new Slime();
-        Cursor cursor = slime.setArray();
         pool.submit(() -> {
             applicationList.parallelStream().forEach(application -> {
-                Cursor applicationCursor = cursor.addObject();
-                applicationCursor.setString("applicationId", application.id().serializedForm());
-                Cursor applicationMetrics = applicationCursor.setObject("applicationMetrics");
-                fillApplicationMetrics(applicationMetrics, application);
-                Cursor rotationStatus = applicationCursor.setArray("rotationStatus");
-                fillRotationStatus(rotationStatus, application);
-                Cursor deploymentArray = applicationCursor.setArray("deploymentMetrics");
-                for (Deployment deployment : application.deployments().values()) {
-                    Cursor deploymentEntry = deploymentArray.addObject();
-                    fillDeploymentMetrics(deploymentEntry, application, deployment);
+                try {
+                    applications.lockIfPresent(application.id(), locked ->
+                            applications.store(locked.with(controller().metricsService().getApplicationMetrics(application.id()))));
+
+                    applications.lockIfPresent(application.id(), locked ->
+                            applications.store(locked.withRotationStatus(rotationStatus(application))));
+
+                    for (Deployment deployment : application.deployments().values()) {
+                        MetricsService.DeploymentMetrics deploymentMetrics = controller().metricsService()
+                                                                                         .getDeploymentMetrics(application.id(), deployment.zone());
+                        DeploymentMetrics newMetrics = new DeploymentMetrics(deploymentMetrics.queriesPerSecond(),
+                                                                             deploymentMetrics.writesPerSecond(),
+                                                                             deploymentMetrics.documentCount(),
+                                                                             deploymentMetrics.queryLatencyMillis(),
+                                                                             deploymentMetrics.writeLatencyMillis());
+
+                        applications.lockIfPresent(application.id(), locked ->
+                                applications.store(locked.with(deployment.zone(), newMetrics)
+                                                         .recordActivityAt(controller().clock().instant(), deployment.zone())));
+                    }
+                } catch (Exception e) {
+                    failures.incrementAndGet();
+                    lastException.set(e);
                 }
             });
         });
         pool.shutdown();
         try {
             pool.awaitTermination(30, TimeUnit.MINUTES);
-            feedMetrics(slime);
             if (lastException.get() != null) {
                 log.log(Level.WARNING, String.format("Failed to query metrics service for %d/%d applications. Last error: %s. Retrying in %s",
                                                      failures.get(),
@@ -91,8 +91,6 @@ public class DeploymentMetricsMaintainer extends Maintainer {
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            log.log(Level.WARNING, "Unable to feed metrics to API", e);
         }
     }
 
@@ -106,40 +104,6 @@ public class DeploymentMetricsMaintainer extends Maintainer {
                                return result;
                            })
                            .orElseGet(Collections::emptyMap);
-    }
-
-    private void fillApplicationMetrics(Cursor applicationCursor, Application application) {
-        MetricsService.ApplicationMetrics metrics = controller().metricsService().getApplicationMetrics(application.id());
-        applicationCursor.setDouble("queryServiceQuality", metrics.queryServiceQuality());
-        applicationCursor.setDouble("writeServiceQuality", metrics.writeServiceQuality());
-    }
-
-    private void fillRotationStatus(Cursor rotationStatusCursor, Application application) {
-        Map<HostName, RotationStatus> rotationStatus = rotationStatus(application);
-        for (Map.Entry<HostName, RotationStatus> entry : rotationStatus.entrySet()) {
-            Cursor rotationStatusEntry = rotationStatusCursor.addObject();
-            rotationStatusEntry.setString("hostname", entry.getKey().value());
-            rotationStatusEntry.setString("rotationStatus", entry.getValue().toString());
-        }
-    }
-
-    private void fillDeploymentMetrics(Cursor deploymentCursor, Application application, Deployment deployment) {
-        MetricsService.DeploymentMetrics deploymentMetrics = controller().metricsService()
-                .getDeploymentMetrics(application.id(), deployment.zone());
-        deploymentCursor.setString("zoneId", deployment.zone().value());
-        deploymentCursor.setDouble("queriesPerSecond", deploymentMetrics.queriesPerSecond());
-        deploymentCursor.setDouble("writesPerSecond", deploymentMetrics.writesPerSecond());
-        deploymentCursor.setDouble("documentCount", deploymentMetrics.documentCount());
-        deploymentCursor.setDouble("queryLatencyMillis", deploymentMetrics.queryLatencyMillis());
-        deploymentCursor.setDouble("writeLatencyMillis", deploymentMetrics.writeLatencyMillis());
-    }
-
-    private void feedMetrics(Slime slime) throws IOException {
-        String uri = baseUris.get(0) + "/metricforwarding/v1/deploymentmetrics/"; // For now, we only feed to one controller
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost httpPost = new HttpPost(uri);
-        httpPost.setEntity(new ByteArrayEntity(SlimeUtils.toJsonBytes(slime)));
-        httpClient.execute(httpPost);
     }
 
     private static RotationStatus from(com.yahoo.vespa.hosted.controller.api.integration.routing.RotationStatus status) {
--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/ClusterUtilizationMaintainerTest.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/ClusterUtilizationMaintainerTest.java
@@ -3,45 +3,24 @@ package com.yahoo.vespa.hosted.controller.maintenance;
 
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
-import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.ClusterSpec;
 import com.yahoo.config.provision.Environment;
 import com.yahoo.vespa.hosted.controller.ControllerTester;
 import com.yahoo.vespa.hosted.controller.application.Deployment;
-import com.yahoo.vespa.hosted.controller.authority.config.ApiAuthorityConfig;
 import com.yahoo.vespa.hosted.controller.persistence.MockCuratorDb;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.time.Duration;
-import java.util.List;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getAllServeEvents;
-import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.junit.Assert.assertEquals;
 
 /**
  * @author smorgrav
  */
 public class ClusterUtilizationMaintainerTest {
 
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(4443);
-
     @Test
     public void maintain() {
-        wireMockRule.stubFor(post(urlEqualTo("/metricforwarding/v1/clusterutilization"))
-                .willReturn(aResponse().withStatus(200)));
         ControllerTester tester = new ControllerTester();
         ApplicationId app = tester.createAndDeploy("tenant1", "domain1", "app1", Environment.dev, 123).id();
 
@@ -49,16 +28,12 @@ public class ClusterUtilizationMaintainerTest {
         Deployment deployment = tester.controller().applications().get(app).get().deployments().values().stream().findAny().get();
         Assert.assertEquals(0, deployment.clusterUtils().size());
 
-        ApiAuthorityConfig.Builder apiAuthorityConfigBuilder = new ApiAuthorityConfig.Builder().authorities("http://localhost:4443/");
-        ApiAuthorityConfig apiAuthorityConfig = new ApiAuthorityConfig(apiAuthorityConfigBuilder);
-        ClusterUtilizationMaintainer maintainer = new ClusterUtilizationMaintainer(tester.controller(), Duration.ofHours(1), new JobControl(new MockCuratorDb()), apiAuthorityConfig);
-        maintainer.maintain();
+        ClusterUtilizationMaintainer mainainer = new ClusterUtilizationMaintainer(tester.controller(), Duration.ofHours(1), new JobControl(new MockCuratorDb()));
+        mainainer.maintain();
 
-        List<ServeEvent> allServeEvents = getAllServeEvents();
-        assertEquals(allServeEvents.size(), 1);
-        LoggedRequest request = findAll(postRequestedFor(urlEqualTo("/metricforwarding/v1/clusterutilization"))).get(0);
-        String expectedBody = "[{\"applicationId\":\"tenant1:app1:default\",\"deployments\":[{\"zoneId\":\"dev.us-east-1\",\"clusterUtil\":[{\"clusterSpecId\":\"default\",\"cpu\":0.5554,\"memory\":0.6990000000000001,\"disk\":0.34590000000000004,\"diskBusy\":0.0}]}]}]";
-        assertEquals(expectedBody, new String(request.getBody()));
+        deployment = tester.controller().applications().get(app).get().deployments().values().stream().findAny().get();
+        Assert.assertEquals(1, deployment.clusterUtils().size());
+        Assert.assertEquals(0.5554, deployment.clusterUtils().get(ClusterSpec.Id.from("default")).getCpu(), Double.MIN_VALUE);
     }
 
 }
