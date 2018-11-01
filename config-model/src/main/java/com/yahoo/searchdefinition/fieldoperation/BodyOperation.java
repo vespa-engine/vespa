8651a728de93b78804485e389d3380aad26d10bb
Arne Juul <arnej@yahoo-inc.com>
2018-11-01 09:48:56 +0000

    Revert "remove "header" and "body" keywords"
    This reverts commit e24c1df653d6470089f05786d74e729bb8f82990.

---
+++ config-model/src/main/java/com/yahoo/searchdefinition/fieldoperation/BodyOperation.java
@@ -0,0 +1,14 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.searchdefinition.fieldoperation;
+
+import com.yahoo.searchdefinition.document.SDField;
+
+/**
+ * @author <a href="mailto:einarmr@yahoo-inc.com">Einar M R Rosenvinge</a>
+ */
+public class BodyOperation implements FieldOperation {
+    public void apply(SDField field) {
+        field.setHeader(false);
+        field.setHeaderOrBodyDefined(true);
+    }
+}
