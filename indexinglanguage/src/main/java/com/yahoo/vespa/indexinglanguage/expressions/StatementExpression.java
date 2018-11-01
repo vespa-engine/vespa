87d4502071d4671502e09d3890fbe701e1592c63
Jon Bratseth <bratseth@oath.com>
2018-11-01 11:12:17 +0100

    Make SimpleLinguistics simple again
    - Remove SimpleLinguistics config and optional use of Optimaize
    - Add Optimaize to OpennlpLinguistics; on by default and config to disable

--- indexinglanguage/src/main/java/com/yahoo/vespa/indexinglanguage/expressions/StatementExpression.java
+++ indexinglanguage/src/main/java/com/yahoo/vespa/indexinglanguage/expressions/StatementExpression.java
@@ -92,7 +92,7 @@ public final class StatementExpression extends ExpressionList<Expression> {
     /** Creates an expression with simple lingustics for testing */
     @SuppressWarnings("deprecation")
     public static StatementExpression fromString(String expression) throws ParseException {
-        return fromString(expression, new SimpleLinguistics(false));
+        return fromString(expression, new SimpleLinguistics());
     }
 
     public static StatementExpression fromString(String expression, Linguistics linguistics) throws ParseException {
--- linguistics/src/main/java/com/yahoo/language/opennlp/OpenNlpLinguistics.java
+++ linguistics/src/main/java/com/yahoo/language/opennlp/OpenNlpLinguistics.java
@@ -1,14 +1,43 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.language.opennlp;
 
+import com.google.inject.Inject;
+import com.yahoo.language.detect.Detector;
 import com.yahoo.language.process.Tokenizer;
+import com.yahoo.language.simple.SimpleDetector;
 import com.yahoo.language.simple.SimpleLinguistics;
 
+/**
+ * Returns a linguistics implementation based on OpenNlp,
+ * and (optionally, default on) Optimaize for language detection.
+ */
 public class OpenNlpLinguistics extends SimpleLinguistics {
+
+    private final Detector detector;
+
+    public OpenNlpLinguistics() {
+        this(true);
+    }
+
+    @Inject
+    public OpenNlpLinguistics(OpennlpLinguisticsConfig config) {
+        this(config.detector().enableOptimaize());
+    }
+
+    public OpenNlpLinguistics(boolean enableOptimaize) {
+        this(enableOptimaize ? new OptimaizeDetector() : new SimpleDetector());
+    }
+
+    private OpenNlpLinguistics(Detector detector) {
+        this.detector = detector;
+    }
 
     @Override
     public Tokenizer getTokenizer() {
         return new OpenNlpTokenizer(getNormalizer(), getTransformer());
     }
+
+    @Override
+    public Detector getDetector() { return detector; }
 
 }
