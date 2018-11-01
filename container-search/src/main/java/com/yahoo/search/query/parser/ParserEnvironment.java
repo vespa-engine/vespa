cec5793cfff60c31ff5f2e537014d7b44991f2b8
Jon Bratseth <bratseth@oath.com>
2018-11-01 13:37:34 +0100

    Follow API change

--- container-search/src/main/java/com/yahoo/search/query/parser/ParserEnvironment.java
+++ container-search/src/main/java/com/yahoo/search/query/parser/ParserEnvironment.java
@@ -18,7 +18,7 @@ import com.yahoo.search.searchchain.Execution;
 public final class ParserEnvironment {
 
     private IndexFacts indexFacts = new IndexFacts();
-    private Linguistics linguistics = new SimpleLinguistics(false);
+    private Linguistics linguistics = new SimpleLinguistics();
     private SpecialTokens specialTokens = new SpecialTokens();
 
     public IndexFacts getIndexFacts() {
