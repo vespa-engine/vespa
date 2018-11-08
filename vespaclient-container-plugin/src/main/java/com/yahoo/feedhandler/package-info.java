40ee7df9f46a3a39377e3309e8273dc9b8308ff9
Jon Bratseth <bratseth@oath.com>
2018-11-08 16:42:45 +0100

    Remove old feedapi

--- vespaclient-container-plugin/src/main/java/com/yahoo/feedhandler/package-info.java
+++
@@ -1,3 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// TODO: This implements the old, deprecated document http API. Remove this package on Vespa 7.
-package com.yahoo.feedhandler;
