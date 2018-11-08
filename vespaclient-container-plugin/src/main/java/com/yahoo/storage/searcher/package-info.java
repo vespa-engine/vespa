ed717c1d3247ed32da0646106f410d6ca0ff120b
Jon Bratseth <bratseth@oath.com>
2018-11-08 16:42:45 +0100

    Remove old feedapi

--- vespaclient-container-plugin/src/main/java/com/yahoo/storage/searcher/package-info.java
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// TODO: This implements the old, deprecated document http API. Remove this package on Vespa 7.
-@ExportPackage
-package com.yahoo.storage.searcher;
-
-import com.yahoo.osgi.annotation.ExportPackage;
