56bf86189a0ce2562bf54715954945edf94e3f45
Jon Bratseth <bratseth@oath.com>
2018-11-09 10:29:45 +0100

    Remove wrong TODO - this is needed for vespafeeder

--- vespaclient-core/src/main/java/com/yahoo/feedhandler/package-info.java
+++ vespaclient-core/src/main/java/com/yahoo/feedhandler/package-info.java
@@ -1,5 +1,4 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// TODO: This implements the old, deprecated document HTTP= API. Remove on Vespa 7
 /**
  * Exported not as an API, but specifically for SSBE.
  */
