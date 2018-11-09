98ec1160dd15da368d78f97ad47fa556f6bfb049
Jon Bratseth <bratseth@oath.com>
2018-11-09 10:05:37 +0100

    Remove template support

--- container-search/src/main/java/com/yahoo/prelude/templates/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage
-@PublicApi
-package com.yahoo.prelude.templates;
-
-import com.yahoo.api.annotations.PublicApi;
-import com.yahoo.osgi.annotation.ExportPackage;
