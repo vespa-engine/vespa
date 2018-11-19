130a3ddf424ba38e6a848c6a50b856c87b546b1e
gjoranv <gv@oath.com>
2018-11-19 15:12:53 +0100

    Do not provide apache httpclient and httpcore.

--- container-search-and-docproc/src/main/java/org/apache/http/message/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 4, minor = 3, micro = 3))
-package org.apache.http.message;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
-
