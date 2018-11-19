66e7d51e6cd72be6cd0db20c4f3c45fd562b49c2
gjoranv <gv@oath.com>
2018-11-19 15:12:53 +0100

    Do not provide apache httpclient and httpcore.

--- container-search-and-docproc/src/main/java/org/apache/commons/codec/language/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 1, minor = 4, micro = 0))
-package org.apache.commons.codec.language;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
-
--- container-search-and-docproc/src/main/java/org/apache/http/impl/conn/package-info.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/*
- * Exported due to usage in HTTPSearcher API
- */
-@ExportPackage(version = @Version(major = 4, minor = 3, micro = 6))
-package org.apache.http.impl.conn;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
