9421feeb51b1907f35296ed4f649bcc2074a9dd7
Arne Juul <arnej@yahoo-inc.com>
2018-11-20 14:31:11 +0000

    wire in Deserializer6

--- document/src/test/java/com/yahoo/document/datatypes/StringTestCase.java
+++ document/src/test/java/com/yahoo/document/datatypes/StringTestCase.java
@@ -50,7 +50,7 @@ public class StringTestCase extends AbstractTypesTest {
         data.position(0);
 
         StringFieldValue tmp = new StringFieldValue();
-        DocumentDeserializer deser = DocumentDeserializerFactory.create42(null, data);
+        DocumentDeserializer deser = DocumentDeserializerFactory.create6(null, data);
         tmp.deserialize(deser);
         java.lang.String foo2 = tmp.getString();
 
@@ -111,7 +111,7 @@ public class StringTestCase extends AbstractTypesTest {
 
         tmp = new StringFieldValue();
 
-        deser = DocumentDeserializerFactory.create42(null, data);
+        deser = DocumentDeserializerFactory.create6(null, data);
         tmp.deserialize(deser);
         java.lang.String blah2 = tmp.getString();
 
@@ -129,7 +129,7 @@ public class StringTestCase extends AbstractTypesTest {
         data.getBuf().position(0);
 
         StringFieldValue tmp = new StringFieldValue();
-        DocumentDeserializer deser = DocumentDeserializerFactory.create42(null, data.getBuf());
+        DocumentDeserializer deser = DocumentDeserializerFactory.create6(null, data.getBuf());
         tmp.deserialize(deser);
         java.lang.String test2 = tmp.getString();
         assertEquals(test, test2);
@@ -150,7 +150,7 @@ public class StringTestCase extends AbstractTypesTest {
         serializer.write(f, stringFieldValue);
         buffer.flip();
 
-        DocumentDeserializer deserializer = DocumentDeserializerFactory.create42(man, buffer);
+        DocumentDeserializer deserializer = DocumentDeserializerFactory.create6(man, buffer);
         StringFieldValue stringFieldValue2 = new StringFieldValue();
         deserializer.read(f, stringFieldValue2);
 
@@ -220,7 +220,7 @@ public class StringTestCase extends AbstractTypesTest {
         serializer.write(doc);
         buffer.flip();
 
-        DocumentDeserializer deserializer = DocumentDeserializerFactory.create42(manager, buffer);
+        DocumentDeserializer deserializer = DocumentDeserializerFactory.create6(manager, buffer);
         return new Document(deserializer);
     }
 
