a1923b8a6e499676e4dd4289dc649af53ea89cb2
Arne Juul <arnej@yahoo-inc.com>
2018-11-20 11:44:23 +0000

    update or add comments around deprecated methods

--- document/src/main/java/com/yahoo/document/Document.java
+++ document/src/main/java/com/yahoo/document/Document.java
@@ -346,7 +346,7 @@ public class Document extends StructuredFieldValue {
     }
 
     @SuppressWarnings("deprecation")
-    @Deprecated
+    @Deprecated // remove before Vespa 8
     public void serializeHeader(Serializer data) throws SerializationException {
         if (data instanceof DocumentWriter) {
             if (data instanceof com.yahoo.document.serialization.VespaDocumentSerializer42) {
@@ -362,7 +362,7 @@ public class Document extends StructuredFieldValue {
         }
     }
 
-    @Deprecated
+    @Deprecated // remove before Vespa 8
     public void serializeBody(Serializer data) throws SerializationException {
         if (getBody().getFieldCount() > 0) {
             if (data instanceof FieldWriter) {
