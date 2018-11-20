cbfcaccfafe767f55a0879483af41eee93af8016
Arne Juul <arnej@yahoo-inc.com>
2018-11-20 14:35:25 +0000

    split header/body is very deprecated

--- document/src/main/java/com/yahoo/document/DocumentTypeManager.java
+++ document/src/main/java/com/yahoo/document/DocumentTypeManager.java
@@ -273,6 +273,7 @@ public class DocumentTypeManager {
         return new Document(data);
     }
 
+    @Deprecated // TODO remove before Vespa 8
     public Document createDocument(GrowableByteBuffer header, GrowableByteBuffer body) {
         DocumentDeserializer data = DocumentDeserializerFactory.create42(this, header, body);
         return new Document(data);
