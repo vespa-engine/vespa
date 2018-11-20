5fe3af9f64e7794965663cc6f4dfe2b06dd05f8d
gjoranv <gv@oath.com>
2018-11-20 10:37:32 +0100

    Move TODO to Vespa 8

--- document/src/main/java/com/yahoo/vespaxmlparser/package-info.java
+++ document/src/main/java/com/yahoo/vespaxmlparser/package-info.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// TODO: Remove this package on Vespa 7
+// TODO: Remove this package on Vespa 8
 @ExportPackage
 package com.yahoo.vespaxmlparser;
 
