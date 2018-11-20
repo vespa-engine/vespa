50d760b84f920f00f4d71214c491a41c325563de
Arne Juul <arnej@yahoo-inc.com>
2018-11-20 14:31:11 +0000

    wire in Deserializer6

--- document/src/main/java/com/yahoo/document/serialization/DocumentDeserializerFactory.java
+++ document/src/main/java/com/yahoo/document/serialization/DocumentDeserializerFactory.java
@@ -13,10 +13,18 @@ public class DocumentDeserializerFactory {
 
     /**
      * Creates a de-serializer for the current head document format.
-     * This format is an extension of the 4.2 format.
+     * This format is an extension of the 6.x format.
      */
     public static DocumentDeserializer createHead(DocumentTypeManager manager, GrowableByteBuffer buf) {
         return new VespaDocumentDeserializerHead(manager, buf);
+    }
+
+    /**
+     * Creates a de-serializer for the 6.x document format.
+     * This format is an extension of the 4.2 format.
+     */
+    public static DocumentDeserializer create6(DocumentTypeManager manager, GrowableByteBuffer buf) {
+        return new VespaDocumentDeserializer6(manager, buf);
     }
 
     /**
