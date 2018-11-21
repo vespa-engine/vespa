1a881203a2995e0c6835106a5e11d4095ff96dba
gjoranv <gv@oath.com>
2018-11-21 15:43:50 +0100

    Open java.base/java.io
    .. to allow com.yahoo.io.NativeIO to use reflective access on
       java.io.FileDescriptor.fd

--- container-disc/src/main/sh/vespa-start-container-daemon.sh
+++ container-disc/src/main/sh/vespa-start-container-daemon.sh
@@ -189,6 +189,7 @@ exec $numactlcmd $envcmd java \
         -XX:HeapDumpPath="${VESPA_HOME}/var/crash" \
         -XX:+ExitOnOutOfMemoryError \
         --illegal-access=warn \
+        --add-opens=java.base/java.io=ALL-UNNAMED \
         --add-opens=java.base/java.lang=ALL-UNNAMED \
         --add-opens=java.base/java.net=ALL-UNNAMED \
         --add-opens=java.base/jdk.internal.loader=ALL-UNNAMED \
