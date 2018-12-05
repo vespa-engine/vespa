38437ab8f6af54dc4b23cb2c205f920a1926b3ed
Bjørn Christian Seime <bjorncs@oath.com>
2018-12-05 19:11:35 +0100

    Remove whitelisting of AES-CBC ciphers

--- security-utils/src/main/java/com/yahoo/security/tls/DefaultTlsContext.java
+++ security-utils/src/main/java/com/yahoo/security/tls/DefaultTlsContext.java
@@ -28,11 +28,7 @@ public class DefaultTlsContext implements TlsContext {
             "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
             "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
             "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-            "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
-            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256");
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
 
     private static final Logger log = Logger.getLogger(DefaultTlsContext.class.getName());
 
