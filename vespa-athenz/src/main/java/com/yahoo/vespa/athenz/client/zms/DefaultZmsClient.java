07c716b0d7271b5d3c61c3d071fe75bfba022203
Morten Tokle <mortent@oath.com>
2018-12-17 11:35:51 +0100

    Add missing parameters to hasAccess

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/client/zms/DefaultZmsClient.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/client/zms/DefaultZmsClient.java
@@ -122,7 +122,8 @@ public class DefaultZmsClient extends ClientBase implements ZmsClient {
 
     @Override
     public boolean hasAccess(AthenzResourceName resource, String action, AthenzIdentity identity) {
-        URI uri = zmsUrl.resolve(String.format("access/%s/%s", action, resource.toResourceNameString()));
+        URI uri = zmsUrl.resolve(String.format("access/%s/%s?domain=%s&principal=%s",
+                                               action, resource.toResourceNameString(), identity.getDomainName(), identity.getName()));
         HttpUriRequest request = RequestBuilder.get()
                 .setUri(uri)
                 .build();
