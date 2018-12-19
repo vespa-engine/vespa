5363582b36789a9100f965966cd16ee95c75088d
Jon Bratseth <bratseth@oath.com>
2018-12-19 15:54:00 +0100

    Revert "Bratseth/search container deprecations"

--- container-core/src/main/java/com/yahoo/container/Container.java
+++ container-core/src/main/java/com/yahoo/container/Container.java
@@ -3,11 +3,13 @@ package com.yahoo.container;
 
 import com.yahoo.component.AbstractComponent;
 import com.yahoo.component.provider.ComponentRegistry;
+import com.yahoo.container.core.config.BundleLoader;
 import com.yahoo.filedistribution.fileacquirer.FileAcquirer;
 import com.yahoo.filedistribution.fileacquirer.FileAcquirerFactory;
 import com.yahoo.jdisc.handler.RequestHandler;
 import com.yahoo.jdisc.service.ClientProvider;
 import com.yahoo.jdisc.service.ServerProvider;
+import com.yahoo.osgi.Osgi;
 import com.yahoo.vespa.config.ConfigTransformer;
 import com.yahoo.vespa.config.UrlDownloader;
 
@@ -32,12 +34,20 @@ public class Container {
     private volatile FileAcquirer fileAcquirer;
     private volatile UrlDownloader urlDownloader;
 
+    private volatile BundleLoader bundleLoader;
+
     private static Logger logger = Logger.getLogger(Container.class.getName());
 
     // TODO: Make this final again.
     private static Container instance = new Container();
 
     public static Container get() { return instance; }
+
+    /** @deprecated do not use */
+    @Deprecated // TODO: Remove
+    public void setOsgi(Osgi osgi) {
+        bundleLoader = new BundleLoader(osgi);
+    }
 
     public void shutdown() {
         if (fileAcquirer != null)
@@ -49,6 +59,14 @@ public class Container {
     //Used to acquire files originating from the application package.
     public FileAcquirer getFileAcquirer() {
         return fileAcquirer;
+    }
+
+    /** @deprecated do not use */
+    @Deprecated // TODO: Remove
+    public BundleLoader getBundleLoader() {
+        if (bundleLoader == null)
+            bundleLoader = new BundleLoader(null);
+        return bundleLoader;
     }
 
     /**
