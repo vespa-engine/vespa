5ce5a17409e60a47fdd07493f4b37f3f57ba9992
Valerij Fredriksen <valerijf@oath.com>
2019-01-09 15:15:44 +0100

    Schedule NodeAgents using the NodeSpecs from list

---
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/nodeagent/NodeAgentFactory.java
@@ -0,0 +1,10 @@
+// Copyright 2019 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.node.admin.nodeagent;
+
+/**
+ * @author freva
+ */
+@FunctionalInterface
+public interface NodeAgentFactory {
+    NodeAgent create(NodeAgentContextSupplier contextSupplier);
+}
