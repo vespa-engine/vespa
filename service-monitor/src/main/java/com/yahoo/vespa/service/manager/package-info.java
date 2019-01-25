f8f570e44e41512182991e8a4a977e4dda8070f7
Håkon Hallingstad <hakon@oath.com>
2019-01-25 13:19:17 +0100

    Export UnionMonitorManager

---
+++ service-monitor/src/main/java/com/yahoo/vespa/service/manager/package-info.java
@@ -0,0 +1,8 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/*
+ * @author hakonhall
+ */
+@ExportPackage
+package com.yahoo.vespa.service.manager;
+
+import com.yahoo.osgi.annotation.ExportPackage;
