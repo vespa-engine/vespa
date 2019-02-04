7285aa2d3f2e8cd6cee90c063c4c29076adb094e
Jon Bratseth <bratseth@oath.com>
2019-02-04 13:56:02 +0100

    Revert "Bratseth/disallow dash rebased"

--- config-application-package/src/main/java/com/yahoo/config/model/application/provider/FilesApplicationPackage.java
+++ config-application-package/src/main/java/com/yahoo/config/model/application/provider/FilesApplicationPackage.java
@@ -617,7 +617,6 @@ public class FilesApplicationPackage implements ApplicationPackage {
     @Override
     public Reader getRankingExpression(String name) {
         try {
-            File file = expressionFileNameToFile(name);
             return IOUtils.createReader(expressionFileNameToFile(name), "utf-8");
         }
         catch (IOException e) {
