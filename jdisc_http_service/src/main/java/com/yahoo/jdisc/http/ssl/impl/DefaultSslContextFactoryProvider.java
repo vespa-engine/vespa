f444dd86cfae0736353271463350fd6bc517e4ba
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-02-07 15:40:18 +0100

    Convert JDisc http connectors to https when TLS is configured through env vars

--- jdisc_http_service/src/main/java/com/yahoo/jdisc/http/ssl/impl/DefaultSslContextFactoryProvider.java
+++ jdisc_http_service/src/main/java/com/yahoo/jdisc/http/ssl/impl/DefaultSslContextFactoryProvider.java
@@ -1,7 +1,11 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.ssl.impl;
 
+import com.yahoo.component.AbstractComponent;
 import com.yahoo.jdisc.http.ssl.SslContextFactoryProvider;
+import com.yahoo.security.tls.ReloadingTlsContext;
+import com.yahoo.security.tls.TlsContext;
+import com.yahoo.security.tls.TransportSecurityUtils;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 /**
@@ -9,9 +13,23 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
  *
  * @author bjorncs
  */
-public class DefaultSslContextFactoryProvider implements SslContextFactoryProvider {
+public class DefaultSslContextFactoryProvider extends AbstractComponent implements SslContextFactoryProvider {
+
+    private final TlsContext tlsContext = TransportSecurityUtils.getConfigFile()
+            .map(configFile -> new ReloadingTlsContext(configFile, TransportSecurityUtils.getInsecureAuthorizationMode()))
+            .orElse(null);
+
     @Override
     public SslContextFactory getInstance(String containerId, int port) {
-        throw new UnsupportedOperationException();
+        if (tlsContext != null) {
+            return new TlsContextManagedSslContextFactory(tlsContext);
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public void deconstruct() {
+        tlsContext.close();
     }
 }
---
+++ jdisc_http_service/src/main/java/com/yahoo/jdisc/http/ssl/impl/TlsContextManagedSslContextFactory.java
@@ -0,0 +1,42 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.jdisc.http.ssl.impl;
+
+import com.yahoo.security.tls.TlsContext;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import javax.net.ssl.SSLEngine;
+import java.net.InetSocketAddress;
+
+/**
+ * A Jetty {@link SslContextFactory} backed by {@link TlsContext}.
+ * Overrides methods that are used by Jetty to construct ssl sockets and ssl engines.
+ *
+ * @author bjorncs
+ */
+class TlsContextManagedSslContextFactory extends SslContextFactory {
+
+    private final TlsContext tlsContext;
+
+    TlsContextManagedSslContextFactory(TlsContext tlsContext) {
+        this.tlsContext = tlsContext;
+    }
+
+    @Override protected void doStart() { } // Override default behaviour
+    @Override protected void doStop() { } // Override default behaviour
+
+    @Override
+    public SSLEngine newSSLEngine() {
+        return tlsContext.createSslEngine();
+    }
+
+    @Override
+    public SSLEngine newSSLEngine(InetSocketAddress address) {
+        return tlsContext.createSslEngine(address.getHostString(), address.getPort());
+    }
+
+    @Override
+    public SSLEngine newSSLEngine(String host, int port) {
+        return tlsContext.createSslEngine(host, port);
+    }
+
+}
