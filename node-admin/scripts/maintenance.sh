d24ed410855d67ba6b4d4dfeb4186265b63a4734
Håkon Hallingstad <hakon@verizonmedia.com>
2019-02-15 20:44:50 +0100

    Remove maintenance.sh

--- node-admin/scripts/maintenance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-java \
-        -cp $VESPA_HOME/lib/jars/node-maintainer-jar-with-dependencies.jar \
-        -Dvespa.log.target=file:$VESPA_HOME/logs/vespa/maintainer.log \
-        "$@"
