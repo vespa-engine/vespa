4a72acb369b8dce91db24a64dfd36a139c1325a9
Håkon Hallingstad <hakon@verizonmedia.com>
2019-02-20 12:01:27 +0100

    Type field on node reports

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/configserver/noderepository/reports/BaseReportTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/configserver/noderepository/reports/BaseReportTest.java
@@ -4,6 +4,8 @@ package com.yahoo.vespa.hosted.node.admin.configserver.noderepository.reports;
 import com.yahoo.test.json.JsonTestHelper;
 import org.junit.Test;
 
+import static com.yahoo.vespa.hosted.node.admin.configserver.noderepository.reports.BaseReport.Type.UNSPECIFIED;
+import static com.yahoo.vespa.hosted.node.admin.configserver.noderepository.reports.BaseReport.Type.SOFT_FAIL;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -13,35 +15,64 @@ import static org.junit.Assert.assertTrue;
  */
 public class BaseReportTest {
     private static final String JSON_1 = "{\"createdMillis\": 1, \"description\": \"desc\"}";
+    private static final String JSON_2 = "{\"createdMillis\": 1, \"description\": \"desc\", \"type\": \"SOFT_FAIL\"}";
+
     @Test
-    public void testSerialization() {
-        JsonTestHelper.assertJsonEquals(new BaseReport(1L, "desc").toJsonNode(),
+    public void testSerialization1() {
+        JsonTestHelper.assertJsonEquals(new BaseReport(1L, "desc", SOFT_FAIL).toJsonNode(),
+                JSON_2);
+        JsonTestHelper.assertJsonEquals(new BaseReport(null, "desc", SOFT_FAIL).toJsonNode(),
+                "{\"description\": \"desc\", \"type\": \"SOFT_FAIL\"}");
+        JsonTestHelper.assertJsonEquals(new BaseReport(1L, null, SOFT_FAIL).toJsonNode(),
+                "{\"createdMillis\": 1, \"type\": \"SOFT_FAIL\"}");
+        JsonTestHelper.assertJsonEquals(new BaseReport(null, null, SOFT_FAIL).toJsonNode(),
+                "{\"type\": \"SOFT_FAIL\"}");
+
+        JsonTestHelper.assertJsonEquals(new BaseReport(1L, "desc", null).toJsonNode(),
                 JSON_1);
-        JsonTestHelper.assertJsonEquals(new BaseReport(null, "desc").toJsonNode(),
+        JsonTestHelper.assertJsonEquals(new BaseReport(null, "desc", null).toJsonNode(),
                 "{\"description\": \"desc\"}");
-        JsonTestHelper.assertJsonEquals(new BaseReport(1L, null).toJsonNode(),
+        JsonTestHelper.assertJsonEquals(new BaseReport(1L, null, null).toJsonNode(),
                 "{\"createdMillis\": 1}");
-        JsonTestHelper.assertJsonEquals(new BaseReport(null, null).toJsonNode(),
+        JsonTestHelper.assertJsonEquals(new BaseReport(null, null, null).toJsonNode(),
                 "{}");
     }
 
     @Test
     public void testShouldUpdate() {
-        BaseReport report = new BaseReport(1L, "desc");
+        BaseReport report = new BaseReport(1L, "desc", SOFT_FAIL);
         assertFalse(report.updates(report));
-        assertFalse(new BaseReport(1L, "desc").updates(report));
-        assertFalse(new BaseReport(2L, "desc").updates(report));
-        assertFalse(new BaseReport(null, "desc").updates(report));
 
-        assertTrue(new BaseReport(1L, "desc 2").updates(report));
-        assertTrue(new BaseReport(1L, null).updates(report));
+        // createdMillis is ignored
+        assertFalse(new BaseReport(1L, "desc", SOFT_FAIL).updates(report));
+        assertFalse(new BaseReport(2L, "desc", SOFT_FAIL).updates(report));
+        assertFalse(new BaseReport(null, "desc", SOFT_FAIL).updates(report));
+
+        // description is not ignored
+        assertTrue(new BaseReport(1L, "desc 2", SOFT_FAIL).updates(report));
+        assertTrue(new BaseReport(1L, null, SOFT_FAIL).updates(report));
+
+        // type is not ignored
+        assertTrue(new BaseReport(1L, "desc", null).updates(report));
+        assertTrue(new BaseReport(1L, "desc", BaseReport.Type.HARD_FAIL).updates(report));
     }
 
     @Test
     public void testJsonSerialization() {
-        BaseReport report = BaseReport.fromJson(JSON_1);
+        BaseReport report = BaseReport.fromJson(JSON_2);
         assertEquals(1L, (long) report.getCreatedMillisOrNull());
         assertEquals("desc", report.getDescriptionOrNull());
-        JsonTestHelper.assertJsonEquals(report.toJson(), JSON_1);
+        assertEquals(SOFT_FAIL, report.getTypeOrNull());
+        JsonTestHelper.assertJsonEquals(report.toJson(), JSON_2);
+    }
+
+    @Test
+    public void settingInformationalIgnoresType() {
+        BaseReport report = BaseReport.fromJson(JSON_2);
+        assertEquals(BaseReport.Type.SOFT_FAIL, report.getTypeOrNull());
+        report.setType(UNSPECIFIED);
+        JsonTestHelper.assertJsonEquals(JSON_1, report.toJson());
+        report.setType(null);
+        JsonTestHelper.assertJsonEquals(JSON_1, report.toJson());
     }
 }
