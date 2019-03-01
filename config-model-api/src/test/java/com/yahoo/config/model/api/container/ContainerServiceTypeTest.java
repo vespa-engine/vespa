7ddfb4146fc566a0e2183994c279db07d2e518b3
gjoranv <gv@verizonmedia.com>
2019-03-01 17:15:10 +0100

    Add unit test to ensure names don't change.

---
+++ config-model-api/src/test/java/com/yahoo/config/model/api/container/ContainerServiceTypeTest.java
@@ -0,0 +1,31 @@
+package com.yahoo.config.model.api.container;
+
+import org.junit.Test;
+
+import static com.yahoo.config.model.api.container.ContainerServiceType.CLUSTERCONTROLLER_CONTAINER;
+import static com.yahoo.config.model.api.container.ContainerServiceType.CONTAINER;
+import static com.yahoo.config.model.api.container.ContainerServiceType.LOGSERVER_CONTAINER;
+import static com.yahoo.config.model.api.container.ContainerServiceType.METRICS_PROXY_CONTAINER;
+import static com.yahoo.config.model.api.container.ContainerServiceType.QRSERVER;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author gjoranv
+ */
+public class ContainerServiceTypeTest {
+
+    @Test
+    public void new_values_are_not_added_without_updating_tests() {
+        assertEquals(5, ContainerServiceType.values().length);
+    }
+
+    @Test
+    public void service_names_do_not_change() {
+        assertEquals("container", CONTAINER.serviceName);
+        assertEquals("qrserver", QRSERVER.serviceName);
+        assertEquals("container-clustercontroller", CLUSTERCONTROLLER_CONTAINER.serviceName);
+        assertEquals("logserver-container", LOGSERVER_CONTAINER.serviceName);
+        assertEquals("metrics-proxy-container", METRICS_PROXY_CONTAINER.serviceName);
+    }
+
+}
