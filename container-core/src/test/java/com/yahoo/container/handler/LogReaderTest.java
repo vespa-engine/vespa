246109c478351f26e75929790ab0221968a57fbe
Valerij Fredriksen <valerijf@verizonmedia.com>
2019-03-07 16:46:11 +0100

    Stabilize LogReaderTest

--- container-core/src/test/java/com/yahoo/container/handler/LogReaderTest.java
+++ container-core/src/test/java/com/yahoo/container/handler/LogReaderTest.java
@@ -1,14 +1,18 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.container.handler;
 
+import com.yahoo.vespa.test.file.TestFileSystem;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
 import java.time.Instant;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -18,7 +22,20 @@ import static org.junit.Assert.assertEquals;
 
 public class LogReaderTest {
 
-    private final Path logDirectory = Paths.get("src/test/resources/logfolder/");
+    private final FileSystem fileSystem = TestFileSystem.create();
+    private final Path logDirectory = fileSystem.getPath("/opt/vespa/logs");
+
+    @Before
+    public void setup() throws IOException {
+        Files.createDirectories(logDirectory.resolve("subfolder"));
+
+        Files.setLastModifiedTime(
+                Files.write(logDirectory.resolve("log1.log"), "This is one log file\n".getBytes()),
+                FileTime.from(Instant.ofEpochMilli(123)));
+        Files.setLastModifiedTime(
+                Files.write(logDirectory.resolve("subfolder/log2.log"), "This is another log file\n".getBytes()),
+                FileTime.from(Instant.ofEpochMilli(234)));
+    }
 
     @Test
     public void testThatFilesAreWrittenCorrectlyToOutputStream() throws Exception{
