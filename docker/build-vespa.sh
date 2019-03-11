0fd4e9e5702566e9d0976fe5f24392f92befa6a0
Håkon Hallingstad <hakon@verizonmedia.com>
2019-03-11 17:01:49 +0100

    Make it easier to call build-vespa.sh

--- docker/build-vespa.sh
+++ docker/build-vespa.sh
@@ -14,5 +14,10 @@ VESPA_VERSION=$1
 DOCKER_IMAGE="centos:latest"
 
 docker pull ${DOCKER_IMAGE}
-docker run --rm -v $(pwd)/..:/vespa --entrypoint /vespa/docker/build/build-vespa-internal.sh "$DOCKER_IMAGE" "$VESPA_VERSION" "$(id -u)" "$(id -g)"
 
+# The RPMs will be put in the same directory as this script (/vespa/docker
+# within the container)
+docker run -w /vespa --rm -v $(pwd)/..:/vespa \
+       --entrypoint /vespa/docker/build/build-vespa-internal.sh \
+       "$DOCKER_IMAGE" \
+       "$VESPA_VERSION" "$(id -u)" "$(id -g)" /tmp/rpmbuild /vespa/docker
--- docker/build/build-vespa-internal.sh
+++ docker/build/build-vespa-internal.sh
@@ -2,16 +2,22 @@
 # Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 set -e
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <vespa version> <caller uid> <caller gid>"
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 <vespa version> <caller uid> <caller gid> <tmp build dir> <rpm dest dir>"
   echo "This script should not be called manually."
   exit 1
 fi
 VESPA_VERSION=$1
 CALLER_UID=$2
 CALLER_GID=$3
+BUILD_DIR=$4
+DEST_DIR=$5
 
-cd /vespa
+if ! [ -x ./dist.sh ]; then
+    echo ". is not a vespa-engine/vespa root directory"
+    exit 1
+fi
+
 ./dist.sh ${VESPA_VERSION}
 
 yum -y install epel-release
@@ -19,8 +25,7 @@ yum -y install centos-release-scl
 
 yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/g/vespa/vespa/repo/epel-7/group_vespa-vespa-epel-7.repo
 
-yum-builddep -y --setopt="centos-sclo-rh-source.skip_if_unavailable=true" ~/rpmbuild/SPECS/vespa-${VESPA_VERSION}.spec
-rpmbuild -bb ~/rpmbuild/SPECS/vespa-${VESPA_VERSION}.spec
-chown ${CALLER_UID}:${CALLER_GID} ~/rpmbuild/RPMS/x86_64/*.rpm
-mv ~/rpmbuild/RPMS/x86_64/*.rpm /vespa/docker 
-
+yum-builddep -y --setopt="centos-sclo-rh-source.skip_if_unavailable=true" $BUILD_DIR/SPECS/vespa-${VESPA_VERSION}.spec
+rpmbuild -bb $BUILD_DIR/SPECS/vespa-${VESPA_VERSION}.spec
+chown ${CALLER_UID}:${CALLER_GID} $BUILD_DIR/RPMS/x86_64/*.rpm
+mv $BUILD_DIR/RPMS/x86_64/*.rpm $DEST_DIR
