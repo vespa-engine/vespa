4d44c3f37eba27b984d599eccb047614579e8b39
Arne Juul <arnej@yahoo-inc.com>
2019-03-13 11:30:45 +0000

    remove old code

--- configd/src/apps/sentinel/config-handler.cpp
+++ configd/src/apps/sentinel/config-handler.cpp
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "config-handler.h"
-#include "command-connection.h"
 #include "output-connection.h"
 
 #include <vespa/vespalib/net/simple_metric_snapshot.h>
