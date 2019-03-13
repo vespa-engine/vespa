6ff41ca29c8fb36e7a59f0649f746051e1310a6f
Arne Juul <arnej@yahoo-inc.com>
2019-03-13 11:28:25 +0000

    remove telnet API, use state server

--- configd/src/apps/sentinel/state-api.cpp
+++ configd/src/apps/sentinel/state-api.cpp
@@ -1,30 +1,3 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "state-api.h"
-#include <vespa/vespalib/util/host_name.h>
-#include <vespa/vespalib/util/stringfmt.h>
-
-namespace {
-
-std::map<vespalib::string, vespalib::string> noParams;
-
-} // namespace <unnamed>
-
-
-namespace config {
-namespace sentinel {
-
-vespalib::string
-StateApi::get(const char *path) const
-{
-    return myStateApi.get(host_and_port, path, noParams);
-}
-
-void
-StateApi::bound(int port)
-{
-    host_and_port = vespalib::make_string("%s:%d", vespalib::HostName::get().c_str(), port);
-}
-
-} // namespace config::sentinel
-} // namespace config
