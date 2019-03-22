90cc2bce893af67d0fbb6b16ea29276d9ee5573a
Jon Bratseth <bratseth@verizonmedia.com>
2019-03-22 14:17:06 +0100

    Remove testutils dependency

--- vespa-http-client/src/test/java/com/yahoo/vespa/http/client/JsonTestHelper.java
+++ vespa-http-client/src/test/java/com/yahoo/vespa/http/client/JsonTestHelper.java
@@ -1,5 +1,5 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.test.json;
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.http.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -7,13 +7,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Joiner;
 import org.hamcrest.MatcherAssert;
 
-import java.io.UncheckedIOException;
-
 import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
 
-/**
- * @author Vegard Sjonfjell
- */
+import java.io.UncheckedIOException;
+
 public class JsonTestHelper {
 
     private static final ObjectMapper mapper = new ObjectMapper();
