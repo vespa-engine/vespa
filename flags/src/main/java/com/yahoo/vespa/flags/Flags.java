3ea51936a1b297ee1ddf3d8b29a29e7fac0ab4d2
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-03-28 11:40:29 +0100

    Introduce 'use-https-load-balancer-upstream' feature flag

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -119,6 +119,13 @@ public class Flags {
             "Takes effect at redeployment",
             APPLICATION_ID);
 
+    public static final UnboundBooleanFlag USE_HTTPS_LOAD_BALANCER_UPSTREAM = defineFeatureFlag(
+            "use-https-load-balancer-upstream", false,
+            "Use https between load balancer and upstream containers",
+            "Takes effect at redeployment",
+            APPLICATION_ID);
+
+
     /** WARNING: public for testing: All flags should be defined in {@link Flags}. */
     public static UnboundBooleanFlag defineFeatureFlag(String flagId, boolean defaultValue, String description,
                                                        String modificationEffect, FetchVector.Dimension... dimensions) {
