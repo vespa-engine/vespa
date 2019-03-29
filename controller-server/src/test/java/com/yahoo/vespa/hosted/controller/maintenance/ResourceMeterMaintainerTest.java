5609e38927a64e5e1163919ea852c007be1bcaeb
andreer <andreer@verizonmedia.com>
2019-03-29 11:31:21 +0100

    add basic metrics to metering

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/ResourceMeterMaintainerTest.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/ResourceMeterMaintainerTest.java
@@ -6,6 +6,7 @@ import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.vespa.hosted.controller.api.integration.resource.ResourceSnapshot;
 import com.yahoo.vespa.hosted.controller.api.integration.stubs.MockResourceSnapshotConsumer;
 import com.yahoo.vespa.hosted.controller.integration.NodeRepositoryClientMock;
+import com.yahoo.vespa.hosted.controller.integration.MetricsMock;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -19,13 +20,14 @@ import static org.junit.Assert.*;
 public class ResourceMeterMaintainerTest {
 
     private final double DELTA = Double.MIN_VALUE;
-    NodeRepositoryClientMock nodeRepository = new NodeRepositoryClientMock();
-    MockResourceSnapshotConsumer snapshotConsumer = new MockResourceSnapshotConsumer();
+    private NodeRepositoryClientMock nodeRepository = new NodeRepositoryClientMock();
+    private MockResourceSnapshotConsumer snapshotConsumer = new MockResourceSnapshotConsumer();
+    private MetricsMock metrics = new MetricsMock();
 
     @Test
     public void testMaintainer() {
         ControllerTester tester = new ControllerTester();
-        ResourceMeterMaintainer resourceMeterMaintainer = new ResourceMeterMaintainer(tester.controller(), Duration.ofMinutes(5), new JobControl(tester.curator()), nodeRepository, tester.clock(), snapshotConsumer);
+        ResourceMeterMaintainer resourceMeterMaintainer = new ResourceMeterMaintainer(tester.controller(), Duration.ofMinutes(5), new JobControl(tester.curator()), nodeRepository, tester.clock(), metrics, snapshotConsumer);
         resourceMeterMaintainer.maintain();
         Map<ApplicationId, ResourceSnapshot> consumedResources = snapshotConsumer.consumedResources();
 
@@ -42,5 +44,7 @@ public class ResourceMeterMaintainerTest {
         assertEquals(96, app2.getResourceAllocation().getMemoryGb(), DELTA);
         assertEquals(2000, app2.getResourceAllocation().getDiskGb(), DELTA);
 
+        assertEquals(tester.clock().millis()/1000, metrics.getMetric("metering_last_reported"));
+        assertEquals(4448.0d, (Double) metrics.getMetric("metering_total_reported"), DELTA);
     }
 }
