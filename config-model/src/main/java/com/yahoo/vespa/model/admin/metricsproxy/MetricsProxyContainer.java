680f2e880caacc040a0e766db890033f0181f6c2
gjoranv <gv@verizonmedia.com>
2019-04-04 11:00:19 +0200

    Install metrics-proxy on MetricsProxyContainerClusters.
    * Add metrics-proxy dep to config-model.
    * Install metrics-proxy bundle for MPConatinerClusters.
    * Add metrics-proxy components and generate configs.
    * Put node specific components to the Container, not the cluster.

--- config-model/src/main/java/com/yahoo/vespa/model/admin/metricsproxy/MetricsProxyContainer.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/metricsproxy/MetricsProxyContainer.java
@@ -1,22 +1,62 @@
+/*
+ * Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+ */
+
 package com.yahoo.vespa.model.admin.metricsproxy;
 
+import ai.vespa.metricsproxy.metric.dimensions.NodeDimensions;
+import ai.vespa.metricsproxy.metric.dimensions.NodeDimensionsConfig;
+import ai.vespa.metricsproxy.rpc.RpcConnector;
+import ai.vespa.metricsproxy.rpc.RpcConnectorConfig;
+import ai.vespa.metricsproxy.service.VespaServices;
+import ai.vespa.metricsproxy.service.VespaServicesConfig;
 import com.yahoo.config.model.api.container.ContainerServiceType;
 import com.yahoo.config.model.producer.AbstractConfigProducer;
+import com.yahoo.config.provision.ClusterMembership;
 import com.yahoo.vespa.model.container.Container;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static com.yahoo.config.model.api.container.ContainerServiceType.METRICS_PROXY_CONTAINER;
+import static com.yahoo.vespa.model.admin.metricsproxy.MetricsProxyContainer.NodeDimensionNames.CANONICAL_FLAVOR;
+import static com.yahoo.vespa.model.admin.metricsproxy.MetricsProxyContainer.NodeDimensionNames.CLUSTER_ID;
+import static com.yahoo.vespa.model.admin.metricsproxy.MetricsProxyContainer.NodeDimensionNames.CLUSTER_TYPE;
+import static com.yahoo.vespa.model.admin.metricsproxy.MetricsProxyContainer.NodeDimensionNames.FLAVOR;
+import static com.yahoo.vespa.model.admin.metricsproxy.MetricsProxyContainerCluster.METRICS_PROXY_BUNDLE_NAME;
 
 /**
  * Container running a metrics proxy.
  *
  * @author gjoranv
  */
-public class MetricsProxyContainer extends Container {
+public class MetricsProxyContainer extends Container implements
+        NodeDimensionsConfig.Producer,
+        RpcConnectorConfig.Producer,
+        VespaServicesConfig.Producer
+{
 
-    public MetricsProxyContainer(AbstractConfigProducer parent, int index) {
+    static final class NodeDimensionNames {
+        static final String FLAVOR = "flavor";
+        static final String CANONICAL_FLAVOR = "canonicalFlavor";
+        static final String CLUSTER_TYPE = "clustertype";
+        static final String CLUSTER_ID = "clusterid";
+    }
+
+    private final boolean isHostedVespa;
+
+    public MetricsProxyContainer(AbstractConfigProducer parent, int index, boolean isHostedVespa) {
         super(parent, "" + index, index);
+        this.isHostedVespa = isHostedVespa;
         setProp("clustertype", "admin");
         setProp("index", String.valueOf(index));
+        addNodeSpecificComponents();
+    }
+
+    private void addNodeSpecificComponents() {
+        addMetricsProxyComponent(NodeDimensions.class);
+        addMetricsProxyComponent(RpcConnector.class);
+        addMetricsProxyComponent(VespaServices.class);
     }
 
     @Override
@@ -43,6 +83,38 @@ public class MetricsProxyContainer extends Container {
     protected void tagServers() {
         super.tagServers();
         portsMeta.on(numHttpServerPorts).tag("rpc").tag("metrics");
+    }
+
+    @Override
+    public void getConfig(RpcConnectorConfig.Builder builder) {
+        builder.port(getRelativePort(0));
+    }
+
+    @Override
+    public void getConfig(VespaServicesConfig.Builder builder) {
+        builder.service.addAll(VespaServicesConfigGenerator.generate(getHostResource().getServices()));
+    }
+
+    @Override
+    public void getConfig(NodeDimensionsConfig.Builder builder) {
+        Map<String, String> dimensions = new LinkedHashMap<>();
+        if (isHostedVespa) {
+            getHostResource().getFlavor().ifPresent(flavor -> {
+                dimensions.put(FLAVOR, flavor.name());
+                dimensions.put(CANONICAL_FLAVOR, flavor.canonicalName());
+            });
+
+            getHostResource().primaryClusterMembership().map(ClusterMembership::cluster).ifPresent(cluster -> {
+                dimensions.put(CLUSTER_TYPE, cluster.type().name());
+                dimensions.put(CLUSTER_ID, cluster.id().value());
+            });
+
+            builder.dimensions(dimensions);
+        }
+    }
+
+    private void  addMetricsProxyComponent(Class<?> componentClass) {
+        addSimpleComponent(componentClass.getName(), null, METRICS_PROXY_BUNDLE_NAME);
     }
 
 }
---
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/metricsproxy/VespaServicesConfigGenerator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+ */
+
+package com.yahoo.vespa.model.admin.metricsproxy;
+
+import ai.vespa.metricsproxy.service.VespaServicesConfig;
+import com.yahoo.vespa.model.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author gjoranv
+ */
+public class VespaServicesConfigGenerator {
+
+    public static List<VespaServicesConfig.Service.Builder> generate(List<Service> services) {
+        return services.stream()
+                .filter(VespaServicesConfigGenerator::doIncludeServiceMetrics)
+                .map(VespaServicesConfigGenerator::toServiceBuilder)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean doIncludeServiceMetrics(Service s) {
+        return s.getStartupCommand() != null || s.getServiceType().equals("configserver") || s.getServiceType().equals("config-sentinel");
+    }
+
+    private static VespaServicesConfig.Service.Builder toServiceBuilder(Service service) {
+        VespaServicesConfig.Service.Builder builder = new VespaServicesConfig.Service.Builder()
+                .configId(service.getConfigId())
+                .name(service.getServiceName())
+                .port(service.getHealthPort())
+                .healthport(service.getHealthPort());
+
+        service.getDefaultMetricDimensions().forEach((name, value) -> builder.dimension(toServiceDimensionBuilder(name, value)));
+        return builder;
+    }
+
+    private static VespaServicesConfig.Service.Dimension.Builder toServiceDimensionBuilder(String name, String value) {
+        return new VespaServicesConfig.Service.Dimension.Builder()
+                .key(name)
+                .value(value);
+    }
+
+}
