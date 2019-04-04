591523227148b1eec0dab6d07cedd326baffe603
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-04-04 13:04:13 +0200

    Use VespaHttpClientBuilder in service-monitor

--- service-monitor/src/main/java/com/yahoo/vespa/service/health/ApacheHttpClient.java
+++ service-monitor/src/main/java/com/yahoo/vespa/service/health/ApacheHttpClient.java
@@ -1,6 +1,7 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.service.health;
 
+import com.yahoo.security.tls.https.VespaHttpClientBuilder;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,9 +11,10 @@ import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
 
@@ -32,12 +34,7 @@ class ApacheHttpClient implements AutoCloseable {
         T handle(CloseableHttpResponse httpResponse) throws Exception;
     }
 
-    static CloseableHttpClient makeCloseableHttpClient(URL url, Duration timeout, Duration keepAlive, ConnectionSocketFactory socketFactory) {
-        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register(url.getProtocol(),socketFactory)
-                .build();
-
-        HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(registry);
+    static CloseableHttpClient makeCloseableHttpClient(Duration timeout, Duration keepAlive) {
 
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectTimeout((int) timeout.toMillis()) // establishment of connection
@@ -57,16 +54,23 @@ class ApacheHttpClient implements AutoCloseable {
                     }
                 };
 
-        return HttpClients.custom()
+        return VespaHttpClientBuilder.create(ApacheHttpClient::createConnectionManager)
                 .setKeepAliveStrategy(keepAliveStrategy)
-                .setConnectionManager(connectionManager)
                 .disableAutomaticRetries()
                 .setDefaultRequestConfig(requestConfig)
                 .build();
     }
 
-    ApacheHttpClient(URL url, Duration timeout, Duration keepAlive, ConnectionSocketFactory socketFactory) {
-        this(url, makeCloseableHttpClient(url, timeout, keepAlive, socketFactory));
+    private static HttpClientConnectionManager createConnectionManager(SSLConnectionSocketFactory sslConnectionSocketFactory) {
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", sslConnectionSocketFactory)
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .build();
+        return new BasicHttpClientConnectionManager(registry);
+    }
+
+    ApacheHttpClient(URL url, Duration timeout, Duration keepAlive) {
+        this(url, makeCloseableHttpClient(timeout, keepAlive));
     }
 
     ApacheHttpClient(URL url, CloseableHttpClient client) {
