2354e794031cec8bcb654e676338bc0ddcbb71d6
Tor Brede Vekterli <vekterli@verizonmedia.com>
2019-04-10 09:26:46 +0000

    Add missing header pragma directive

--- storageapi/src/vespa/storageapi/mbusprot/protobuf_includes.h
+++ storageapi/src/vespa/storageapi/mbusprot/protobuf_includes.h
@@ -1,5 +1,7 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
+#pragma once
+
 // Disable warnings emitted by protoc generated files
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsuggest-override"
