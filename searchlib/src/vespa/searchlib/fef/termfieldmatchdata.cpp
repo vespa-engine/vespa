ebc59fde988574af8f326ce88da8de60b909d58e
Henning Baldersheim <balder@yahoo-inc.com>
2019-05-09 08:31:00 +0000

    Disable address-of-packed-member warning

--- searchlib/src/vespa/searchlib/fef/termfieldmatchdata.cpp
+++ searchlib/src/vespa/searchlib/fef/termfieldmatchdata.cpp
@@ -84,9 +84,13 @@ void sswap(T * a, T * b) {
 void
 TermFieldMatchData::swap(TermFieldMatchData &rhs)
 {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress-of-packed-member"
+    // TODO Reconsider packing to avoid bad practice. gcc 9 does not like it.
     sswap(&_docId, &rhs._docId);
     sswap(&_fieldId, &rhs._fieldId);
     sswap(&_sz, &rhs._sz);
+#pragma GCC diagnostic pop
     char tmp[sizeof(_data)];
     memcpy(tmp, &rhs._data, sizeof(_data));
     memcpy(&rhs._data, &_data, sizeof(_data));
