eab929f7e354d4081dd4c97551ff78073454ab6d
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-05-20 16:08:12 +0200

    Add RpcAuthorizer interface with multi-tenant implementation

---
+++ configserver/src/main/java/com/yahoo/vespa/config/server/rpc/security/RpcAuthorizer.java
@@ -0,0 +1,19 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.config.server.rpc.security;
+
+import com.yahoo.jrt.Request;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Authorization logic for configserver's RPC method
+ *
+ * @author bjorncs
+ */
+public interface RpcAuthorizer {
+
+    CompletableFuture<Void> authorizeConfigRequest(Request request);
+
+    CompletableFuture<Void> authorizeFileRequest(Request request);
+
+}
