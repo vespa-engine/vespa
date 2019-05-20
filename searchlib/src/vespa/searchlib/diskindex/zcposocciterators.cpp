fb2f3da8363619faf2c20888352f0c2f95773da8
Tor Egge <Tor.Egge@broadpark.no>
2019-05-20 13:05:48 +0200

    Add support for not decoding normal features in zc posting iterator. Remove unneeded test iterator.

--- searchlib/src/vespa/searchlib/diskindex/zcposocciterators.cpp
+++ searchlib/src/vespa/searchlib/diskindex/zcposocciterators.cpp
@@ -14,10 +14,12 @@ using search::index::PostingListCounts;
 
 template <bool bigEndian, bool dynamic_k>
 ZcRareWordPosOccIterator<bigEndian, dynamic_k>::
-ZcRareWordPosOccIterator(Position start, uint64_t bitLength, uint32_t docIdLimit, bool decode_cheap_features,
-                          const PosOccFieldsParams *fieldsParams,
-                          const TermFieldMatchDataArray &matchData)
-    : ZcRareWordPostingIterator<bigEndian, dynamic_k>(matchData, start, docIdLimit, decode_cheap_features),
+ZcRareWordPosOccIterator(Position start, uint64_t bitLength, uint32_t docIdLimit,
+                         bool decode_normal_features, bool decode_cheap_features,
+                         const PosOccFieldsParams *fieldsParams,
+                         const TermFieldMatchDataArray &matchData)
+    : ZcRareWordPostingIterator<bigEndian, dynamic_k>(matchData, start, docIdLimit,
+                                                      decode_normal_features, decode_cheap_features),
       _decodeContextReal(start.getOccurences(), start.getBitOffset(), bitLength, fieldsParams)
 {
     assert(!matchData.valid() || (fieldsParams->getNumFields() == matchData.size()));
@@ -26,11 +28,13 @@ ZcRareWordPosOccIterator(Position start, uint64_t bitLength, uint32_t docIdLimit
 
 template <bool bigEndian, bool dynamic_k>
 ZcPosOccIterator<bigEndian, dynamic_k>::
-ZcPosOccIterator(Position start, uint64_t bitLength, uint32_t docIdLimit, bool decode_cheap_features,
+ZcPosOccIterator(Position start, uint64_t bitLength, uint32_t docIdLimit,
+                 bool decode_normal_features, bool decode_cheap_features,
                  uint32_t minChunkDocs, const PostingListCounts &counts,
                  const PosOccFieldsParams *fieldsParams,
                  const TermFieldMatchDataArray &matchData)
-    : ZcPostingIterator<bigEndian>(minChunkDocs, dynamic_k, counts, matchData, start, docIdLimit, decode_cheap_features),
+    : ZcPostingIterator<bigEndian>(minChunkDocs, dynamic_k, counts, matchData, start, docIdLimit,
+                                   decode_normal_features, decode_cheap_features),
       _decodeContextReal(start.getOccurences(), start.getBitOffset(), bitLength, fieldsParams)
 {
     assert(!matchData.valid() || (fieldsParams->getNumFields() == matchData.size()));
@@ -51,15 +55,15 @@ create_zc_posocc_iterator(const PostingListCounts &counts, bitcompression::Posit
     assert((num_docs == counts._numDocs) || ((num_docs == posting_params._min_chunk_docs) && (num_docs < counts._numDocs)));
     if (num_docs < posting_params._min_skip_docs) {
         if (posting_params._dynamic_k) {
-            return std::make_unique<ZcRareWordPosOccIterator<bigEndian, true>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_cheap_features, &fields_params, match_data);
+            return std::make_unique<ZcRareWordPosOccIterator<bigEndian, true>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_features, posting_params._encode_cheap_features, &fields_params, match_data);
         } else {
-            return std::make_unique<ZcRareWordPosOccIterator<bigEndian, false>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_cheap_features, &fields_params, match_data);
+            return std::make_unique<ZcRareWordPosOccIterator<bigEndian, false>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_features, posting_params._encode_cheap_features, &fields_params, match_data);
         }
     } else {
         if (posting_params._dynamic_k) {
-            return std::make_unique<ZcPosOccIterator<bigEndian, true>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_cheap_features, posting_params._min_chunk_docs, counts, &fields_params, match_data);
+            return std::make_unique<ZcPosOccIterator<bigEndian, true>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_features, posting_params._encode_cheap_features, posting_params._min_chunk_docs, counts, &fields_params, match_data);
         } else {
-            return std::make_unique<ZcPosOccIterator<bigEndian, false>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_cheap_features, posting_params._min_chunk_docs, counts, &fields_params, match_data);
+            return std::make_unique<ZcPosOccIterator<bigEndian, false>>(start, bit_length, posting_params._doc_id_limit, posting_params._encode_features, posting_params._encode_cheap_features, posting_params._min_chunk_docs, counts, &fields_params, match_data);
         }
     }
 }
