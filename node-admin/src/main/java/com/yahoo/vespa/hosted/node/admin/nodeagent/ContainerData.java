762090f4bdc47ae2bbde55809b258ef011d61e46
Harald Musum <musum@verizonmedia.com>
2019-05-22 08:07:15 +0200

    Remove unused method in interface and remove default implementation

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/nodeagent/ContainerData.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/nodeagent/ContainerData.java
@@ -18,18 +18,11 @@ public interface ContainerData {
     void addFile(Path pathInContainer, String data);
 
     /**
-     * Remove file in container at path.
-     *
-     * @param pathInContainer The path to the file inside the container, absolute or relative root /.
-     */
-    default void removeFile(Path pathInContainer) { /* TODO: Remove this when implementing classes have been updated */ }
-
-    /**
      * Symlink to a file in container at path.
      *
      * @param symlink The path to the symlink inside the container, absolute or relative root /.
      * @param target The path to the target file for the symbolic link inside the container, absolute or relative root /.
      */
-    default void createSymlink(Path symlink, Path target) { /* TODO: Remove this when implementing classes have been updated */ }
+    void createSymlink(Path symlink, Path target);
 }
 
