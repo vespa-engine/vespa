8ea3c40687adbdbd81ee6b4c1e029c543bf45eb9
Jon Marius Venstad <jvenstad@yahoo-inc.com>
2019-05-24 14:08:10 +0200

    Add InThreadExecutorService for test purposes

---
+++ vespajlib/src/main/java/com/yahoo/concurrent/InThreadExecutorService.java
@@ -0,0 +1,49 @@
+package com.yahoo.concurrent;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * {@link ExecutorService} implementation that runs all tasks in the calling thread. Useful for tests.
+ *
+ * @author jonmv
+ */
+public class InThreadExecutorService extends AbstractExecutorService {
+
+    private boolean isShutdown = false;
+
+    @Override
+    public void shutdown() {
+        isShutdown = true;
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        shutdown();
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return isShutdown;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return isShutdown;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) {
+        return true;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        command.run();
+    }
+
+}
