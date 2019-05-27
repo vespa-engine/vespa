ea99d9064c25de9da79ca41da6124f79817676e1
Geir Storli <geirst@verizonmedia.com>
2019-05-27 13:16:13 +0000

    Rewrite storageframework tests from cppunit to gtest.

--- storageframework/src/tests/thread/taskthreadtest.cpp
+++ storageframework/src/tests/thread/taskthreadtest.cpp
@@ -1,47 +1,35 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/storageframework/generic/thread/taskthread.h>
-#include <vespa/vdstestlib/cppunit/macros.h>
+#include <vespa/vespalib/gtest/gtest.h>
 
-namespace storage {
-namespace framework {
-
-struct TaskThreadTest : public CppUnit::TestFixture
-{
-
-    void testNormalUsage();
-
-    CPPUNIT_TEST_SUITE(TaskThreadTest);
-    CPPUNIT_TEST(testNormalUsage);
-    CPPUNIT_TEST_SUITE_END();
-};
-
-CPPUNIT_TEST_SUITE_REGISTRATION(TaskThreadTest);
+namespace storage::framework {
 
 namespace {
-    struct Task {
-        std::string _name;
-        uint8_t _priority;
-        
-        Task(const std::string& name, uint8_t priority)
-            : _name(name), _priority(priority) {}
-        
-        bool operator<(const Task& other) const {
-            return (_priority > other._priority);
-        }
-        uint8_t getPriority() const { return _priority; }
-    };
 
-    struct MyThread : public TaskThread<Task> {
-        MyThread(ThreadLock& lock) : TaskThread<Task>(lock) {}
-        ThreadWaitInfo doNonCriticalTick(ThreadIndex) override {
-            return ThreadWaitInfo::NO_MORE_CRITICAL_WORK_KNOWN;
-        }
-    };
+struct Task {
+    std::string _name;
+    uint8_t _priority;
+
+    Task(const std::string& name, uint8_t priority)
+        : _name(name), _priority(priority) {}
+
+    bool operator<(const Task& other) const {
+        return (_priority > other._priority);
+    }
+    uint8_t getPriority() const { return _priority; }
+};
+
+struct MyThread : public TaskThread<Task> {
+    MyThread(ThreadLock& lock) : TaskThread<Task>(lock) {}
+    ThreadWaitInfo doNonCriticalTick(ThreadIndex) override {
+        return ThreadWaitInfo::NO_MORE_CRITICAL_WORK_KNOWN;
+    }
+};
+
 }
 
-void
-TaskThreadTest::testNormalUsage()
+TEST(TaskThreadTest, test_normal_usage)
 {
     TickingThreadPool::UP pool(TickingThreadPool::createDefault("testApp"));
 
@@ -50,19 +38,17 @@ TaskThreadTest::testNormalUsage()
     t.addTask(Task("b", 3));
     t.addTask(Task("c", 8));
     t.addTask(Task("d", 4));
-    CPPUNIT_ASSERT(t.empty()); // Still empty before critical tick has run
+    EXPECT_TRUE(t.empty()); // Still empty before critical tick has run
     dynamic_cast<TickingThread&>(t).doCriticalTick(0);
-    CPPUNIT_ASSERT(!t.empty());
-    CPPUNIT_ASSERT_EQUAL(3, (int) t.peek().getPriority());
+    EXPECT_TRUE(!t.empty());
+    EXPECT_EQ(3, t.peek().getPriority());
     std::ostringstream ost;
     while (!t.empty()) {
         Task task(t.peek());
         ost << task._name << '(' << ((int) task.getPriority()) << ") ";
         t.pop();
     }
-    CPPUNIT_ASSERT_EQUAL(std::string("b(3) d(4) a(6) c(8) "), ost.str());
+    EXPECT_EQ(std::string("b(3) d(4) a(6) c(8) "), ost.str());
 }
 
-
-} // framework
-} // storage
+}
