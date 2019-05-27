1e728900616a0c132e9d9f481bebe58de5226730
Valerij Fredriksen <valerijf@verizonmedia.com>
2019-05-27 21:30:59 +0200

    Add PublicCd aws-us-east-1c prod, test and staging to JobType

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/deployment/JobType.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/deployment/JobType.java
@@ -1,7 +1,6 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.api.integration.deployment;
 
-import com.google.common.collect.ImmutableMap;
 import com.yahoo.config.provision.Environment;
 import com.yahoo.config.provision.RegionName;
 import com.yahoo.config.provision.SystemName;
@@ -13,6 +12,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.yahoo.config.provision.SystemName.PublicCd;
 import static com.yahoo.config.provision.SystemName.cd;
 import static com.yahoo.config.provision.SystemName.main;
 import static com.yahoo.config.provision.SystemName.vaas;
@@ -24,12 +24,14 @@ public enum JobType {
                             Map.of()),
 
     systemTest             ("system-test",
-                            Map.of(main, ZoneId.from("test"   , "us-east-1"),
-                                   cd  , ZoneId.from("test"   , "cd-us-central-1"))),
+                            Map.of(main    , ZoneId.from("test", "us-east-1"),
+                                   cd      , ZoneId.from("test", "cd-us-central-1"),
+                                   PublicCd, ZoneId.from("test", "aws-us-east-1c"))),
 
     stagingTest            ("staging-test",
-                            Map.of(main, ZoneId.from("staging", "us-east-3"),
-                                   cd  , ZoneId.from("staging", "cd-us-central-1"))),
+                            Map.of(main    , ZoneId.from("staging", "us-east-3"),
+                                   cd      , ZoneId.from("staging", "cd-us-central-1"),
+                                   PublicCd, ZoneId.from("staging", "aws-us-east-1c"))),
 
     productionUsEast3      ("production-us-east-3",
                             Map.of(main, ZoneId.from("prod"   , "us-east-3"))),
@@ -54,6 +56,9 @@ public enum JobType {
 
     productionAwsUsEast1a  ("production-aws-us-east-1a",
                             Map.of(main, ZoneId.from("prod"   , "aws-us-east-1a"))),
+
+    productionAwsUsEast1c  ("production-aws-us-east-1c",
+                            Map.of(PublicCd, ZoneId.from("prod", "aws-us-east-1c"))),
 
     productionAwsUsWest2a  ("production-aws-us-west-2a",
                             Map.of(main, ZoneId.from("prod"   , "aws-us-west-2a"))),
