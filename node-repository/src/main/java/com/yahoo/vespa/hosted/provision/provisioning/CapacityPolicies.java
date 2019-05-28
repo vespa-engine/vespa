816654b954367ff64b37705c04b66c05a02cebc5
Jon Bratseth <bratseth@verizonmedia.com>
2019-05-28 11:18:28 +0200

    Correct vcpu for admin

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/CapacityPolicies.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/CapacityPolicies.java
@@ -84,7 +84,7 @@ public class CapacityPolicies {
 
     private NodeResources defaultNodeResources(ClusterSpec.Type clusterType) {
         if (zone.system() != SystemName.cd && clusterType == ClusterSpec.Type.admin)
-            return new NodeResources(0, 3, 50);
+            return new NodeResources(0.5, 3, 50);
 
         if (zone.system() == SystemName.PublicCd && clusterType == ClusterSpec.Type.admin && zone.environment() != Environment.prod)
             return new NodeResources(1, 3, 50);
