696f59cb6a45e7db6e0c0d843af44c9ccbcd7f09
gjoranv <gv@verizonmedia.com>
2019-06-03 16:01:22 +0200

    Ensure always valid json from JsonMetricsRenderingException

---
+++ metrics-proxy/src/main/java/ai/vespa/metricsproxy/metric/model/json/JsonRenderingException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+ */
+
+package ai.vespa.metricsproxy.metric.model.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.WARNING;
+
+/**
+ * An exception whose message is always valid json or, if all fails, a plain text string.
+ *
+ * @author gjoranv
+ */
+public class JsonRenderingException extends RuntimeException {
+
+    private static Logger log = Logger.getLogger(JsonRenderingException.class.getName());
+
+    JsonRenderingException(String message) {
+        super(message);
+    }
+
+    public String getMessageAsJson() {
+        return wrap(getMessage());
+    }
+
+    private static String wrap(String message) {
+        try {
+            return new ObjectMapper().writeValueAsString(Map.of("error", message));
+        } catch (IOException e) {
+            log.log(WARNING, "Could not encode error message to json:", e);
+            return "Could not encode error message to json, check the log for details.";
+        }
+    }
+
+}
