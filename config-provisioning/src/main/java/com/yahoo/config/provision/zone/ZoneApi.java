92de7e989d6d15bba047ef6b775790f8c6886f5e
Håkon Hallingstad <hakon@verizonmedia.com>
2019-06-05 23:29:31 +0200

    Add ZoneApi

---
+++ config-provisioning/src/main/java/com/yahoo/config/provision/zone/ZoneApi.java
@@ -0,0 +1,20 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.config.provision.zone;
+
+import com.yahoo.config.provision.CloudName;
+import com.yahoo.config.provision.Environment;
+import com.yahoo.config.provision.RegionName;
+import com.yahoo.config.provision.SystemName;
+
+/**
+ * @author hakonhall
+ */
+public interface ZoneApi {
+    SystemName getSystemName();
+
+    ZoneId getId();
+    default Environment getEnvironment() { return getId().environment(); }
+    default RegionName getRegionName() { return getId().region(); }
+
+    CloudName getCloudName();
+}
