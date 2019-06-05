8fbc011e80f4b210580a5b41bfa1f67b5a1aa722
Håvard Pettersen <havardpe@oath.com>
2019-06-05 12:04:29 +0000

    remove dense tensor builder

--- eval/src/tests/tensor/dense_dot_product_function/dense_dot_product_function_test.cpp
+++ eval/src/tests/tensor/dense_dot_product_function/dense_dot_product_function_test.cpp
@@ -5,7 +5,6 @@
 #include <vespa/eval/tensor/default_tensor_engine.h>
 #include <vespa/eval/tensor/dense/dense_dot_product_function.h>
 #include <vespa/eval/tensor/dense/dense_tensor.h>
-#include <vespa/eval/tensor/dense/dense_tensor_builder.h>
 #include <vespa/eval/tensor/dense/dense_tensor_view.h>
 #include <vespa/eval/eval/test/tensor_model.hpp>
 #include <vespa/eval/eval/test/eval_fixture.h>
