6180f7b865b18ca1a3524d32885ba1a991d934ee
Harald Musum <musum@verizonmedia.com>
2019-06-10 21:52:06 +0200

    Fix test

--- athenz-identity-provider-service/src/test/java/com/yahoo/vespa/hosted/athenz/instanceproviderservice/TestUtils.java
+++ athenz-identity-provider-service/src/test/java/com/yahoo/vespa/hosted/athenz/instanceproviderservice/TestUtils.java
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.athenz.instanceproviderservice;
 
-import com.yahoo.config.provision.Zone;
 import com.yahoo.vespa.hosted.athenz.instanceproviderservice.config.AthenzProviderServiceConfig;
 
 /**
@@ -11,8 +10,7 @@ public class TestUtils {
 
     public static AthenzProviderServiceConfig getAthenzProviderConfig(String domain,
                                                                       String service,
-                                                                      String dnsSuffix,
-                                                                      Zone zone) {
+                                                                      String dnsSuffix) {
         AthenzProviderServiceConfig.Builder zoneConfig =
                 new AthenzProviderServiceConfig.Builder()
                         .serviceName(service)
@@ -22,8 +20,7 @@ public class TestUtils {
                         .ztsUrl("localhost/zts")
                         .secretName("s3cr3t");
         return new AthenzProviderServiceConfig(
-                new AthenzProviderServiceConfig.Builder()
-                        .athenzCaTrustStore("/dummy/path/to/athenz-ca.jks"));
+                zoneConfig.athenzCaTrustStore("/dummy/path/to/athenz-ca.jks"));
     }
 
 }
