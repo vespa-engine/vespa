bd2cd6f6e6f9b9c6f0a82907a9bd4e0c6f62ee68
Jon Marius Venstad <jvenstad@yahoo-inc.com>
2019-06-14 12:43:43 +0200

    Use correct field name when deserialising test config

---
+++ hosted-api/src/test/java/ai/vespa/hosted/api/TestConfigTest.java
@@ -0,0 +1,37 @@
+package ai.vespa.hosted.api;
+
+import com.yahoo.config.provision.ApplicationId;
+import com.yahoo.config.provision.SystemName;
+import com.yahoo.config.provision.zone.ZoneId;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jonmv
+ */
+public class TestConfigTest {
+
+    @Test
+    public void testDeserialization() throws IOException {
+        TestConfig config = TestConfig.fromJson(Files.readAllBytes(Paths.get("src/test/resources/test-config.json")));
+        assertEquals(ApplicationId.from("t", "a", "i"),
+                     config.application());
+        assertEquals(ZoneId.from("dev", "aws-us-east-1c"),
+                     config.zone());
+        assertEquals(SystemName.PublicCd,
+                     config.system());
+        assertEquals(Map.of(ZoneId.from("dev", "aws-us-east-1c"),
+                            Map.of("default", URI.create("https://dev.endpoint:443/")),
+                            ZoneId.from("prod", "aws-us-east-1a"),
+                            Map.of("default", URI.create("https://prod.endpoint:443/"))),
+                     config.deployments());
+    }
+
+}
