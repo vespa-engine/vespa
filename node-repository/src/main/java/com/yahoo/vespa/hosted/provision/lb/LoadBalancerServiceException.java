65a5316c0891059ec1dad5ad28f34265cceaaead
Martin Polden <mpolden@mpolden.no>
2019-06-21 13:42:18 +0200

    Use common exception class for transient provisioning exceptions

---
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/lb/LoadBalancerServiceException.java
@@ -0,0 +1,17 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.provision.lb;
+
+import com.yahoo.config.provision.TransientException;
+
+/**
+ * Transient exception thrown on behalf of a {@link LoadBalancerService}.
+ *
+ * @author mpolden
+ */
+public class LoadBalancerServiceException extends TransientException {
+
+    public LoadBalancerServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
