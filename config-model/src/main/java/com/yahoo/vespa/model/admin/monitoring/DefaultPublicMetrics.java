453636520bf5be50e3eff5643b628e0f7ced0443
gjoranv <gv@verizonmedia.com>
2019-06-28 13:59:21 +0200

    Add content and storage metrics to the 'default' metric set.

--- config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultPublicMetrics.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/DefaultPublicMetrics.java
@@ -29,6 +29,8 @@ public class DefaultPublicMetrics {
 
     private static Set<Metric> getAllMetrics() {
         return ImmutableSet.<Metric>builder()
+                .addAll(getContentMetrics())
+                .addAll(getStorageMetrics())
                 .addAll(getContainerMetrics())
                 .addAll(getQrserverMetrics())
                 .build();
@@ -60,6 +62,55 @@ public class DefaultPublicMetrics {
         metrics.add(new Metric("degraded_queries.rate"));
         metrics.add(new Metric("failed_queries.rate"));
         metrics.add(new Metric("serverActiveThreads.average"));
+
+        return metrics;
+    }
+
+    private static Set<Metric> getContentMetrics() {
+        Set<Metric> metrics = new LinkedHashSet<>();
+
+        metrics.add(new Metric("content.proton.docsum.docs.rate"));
+        metrics.add(new Metric("content.proton.docsum.latency.average"));
+
+        metrics.add(new Metric("content.proton.transport.query.count.rate"));
+        metrics.add(new Metric("content.proton.transport.query.latency.average"));
+
+        metrics.add(new Metric("content.proton.documentdb.documents.total.last"));
+        metrics.add(new Metric("content.proton.documentdb.documents.ready.last"));
+        metrics.add(new Metric("content.proton.documentdb.documents.active.last"));
+        metrics.add(new Metric("content.proton.documentdb.index.docs_in_memory.last"));
+        metrics.add(new Metric("content.proton.documentdb.disk_usage.last"));
+
+        metrics.add(new Metric("content.proton.documentdb.job.total.average"));
+        metrics.add(new Metric("content.proton.documentdb.job.attribute_flush.average"));
+        metrics.add(new Metric("content.proton.documentdb.job.disk_index_fusion.average"));
+        metrics.add(new Metric("content.proton.documentdb.job.document_store_compact.average"));
+        metrics.add(new Metric("content.proton.documentdb.job.memory_index_flush.average"));
+
+        metrics.add(new Metric("content.proton.resource_usage.disk.average"));
+        metrics.add(new Metric("content.proton.resource_usage.disk_utilization.average"));
+        metrics.add(new Metric("content.proton.resource_usage.memory.average"));
+
+        metrics.add(new Metric("content.proton.documentdb.ready.document_store.memory_usage.allocated_bytes.average"));
+        metrics.add(new Metric("content.proton.documentdb.ready.document_store.cache.hit_rate.average"));
+        metrics.add(new Metric("content.proton.documentdb.index.memory_usage.allocated_bytes.average"));
+
+        metrics.add(new Metric("content.proton.documentdb.matching.docs_matched.rate"));
+        metrics.add(new Metric("content.proton.documentdb.matching.docs_reranked.rate"));
+        metrics.add(new Metric("content.proton.documentdb.matching.rank_profile.query_latency.average"));
+        metrics.add(new Metric("content.proton.documentdb.matching.rank_profile.rerank_time.average"));
+
+        return metrics;
+    }
+
+    private static Set<Metric> getStorageMetrics() {
+        Set<Metric> metrics = new LinkedHashSet<>();
+
+        metrics.add(new Metric("vds.filestor.alldisks.allthreads.put.sum.count.rate"));
+        metrics.add(new Metric("vds.filestor.alldisks.allthreads.remove.sum.count.rate"));
+        metrics.add(new Metric("vds.filestor.alldisks.allthreads.get.sum.count.rate"));
+        metrics.add(new Metric("vds.filestor.alldisks.allthreads.update.sum.count.rate"));
+        metrics.add(new Metric("vds.filestor.alldisks.allthreads.visit.sum.count.rate"));
 
         return metrics;
     }
