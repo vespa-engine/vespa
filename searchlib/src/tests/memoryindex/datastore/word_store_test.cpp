1ae930e0fc1c6e93faa226274d48dc3f8f51db7e
Geir Storli <geirst@verizonmedia.com>
2019-06-28 07:25:39 +0000

    Rewrite word store tests to gtest.

--- searchlib/src/tests/memoryindex/datastore/word_store_test.cpp
+++ searchlib/src/tests/memoryindex/datastore/word_store_test.cpp
@@ -1,24 +1,15 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+#include <vespa/searchlib/memoryindex/word_store.h>
+#include <vespa/vespalib/gtest/gtest.h>
+
 #include <vespa/log/log.h>
 LOG_SETUP("word_store_test");
-#include <vespa/vespalib/testkit/testapp.h>
-#include <vespa/searchlib/memoryindex/word_store.h>
 
 using namespace search::datastore;
 
-namespace search {
-namespace memoryindex {
+namespace search::memoryindex {
 
-class Test : public vespalib::TestApp {
-private:
-    void requireThatWordsCanBeAddedAndRetrieved();
-    void requireThatAddWordTriggersChangeOfBuffer();
-public:
-    int Main() override;
-};
-
-void
-Test::requireThatWordsCanBeAddedAndRetrieved()
+TEST(WordStoreTest, words_can_be_added_and_retrieved)
 {
     std::string w1 = "require";
     std::string w2 = "that";
@@ -32,19 +23,18 @@ Test::requireThatWordsCanBeAddedAndRetrieved()
     uint32_t w1p = WordStore::RefType::pad(w1s);
     uint32_t w2s = w2.size() + 1;
     uint32_t w2p = WordStore::RefType::pad(w2s);
-    EXPECT_EQUAL(invp,                    WordStore::RefType(r1).offset());
-    EXPECT_EQUAL(invp + w1s + w1p,        WordStore::RefType(r2).offset());
-    EXPECT_EQUAL(invp + w1s + w1p + w2s + w2p, WordStore::RefType(r3).offset());
-    EXPECT_EQUAL(0u, WordStore::RefType(r1).bufferId());
-    EXPECT_EQUAL(0u, WordStore::RefType(r2).bufferId());
-    EXPECT_EQUAL(0u, WordStore::RefType(r3).bufferId());
-    EXPECT_EQUAL(std::string("require"), ws.getWord(r1));
-    EXPECT_EQUAL(std::string("that"),    ws.getWord(r2));
-    EXPECT_EQUAL(std::string("words"),   ws.getWord(r3));
+    EXPECT_EQ(invp, WordStore::RefType(r1).offset());
+    EXPECT_EQ(invp + w1s + w1p, WordStore::RefType(r2).offset());
+    EXPECT_EQ(invp + w1s + w1p + w2s + w2p, WordStore::RefType(r3).offset());
+    EXPECT_EQ(0u, WordStore::RefType(r1).bufferId());
+    EXPECT_EQ(0u, WordStore::RefType(r2).bufferId());
+    EXPECT_EQ(0u, WordStore::RefType(r3).bufferId());
+    EXPECT_EQ(std::string("require"), ws.getWord(r1));
+    EXPECT_EQ(std::string("that"), ws.getWord(r2));
+    EXPECT_EQ(std::string("words"), ws.getWord(r3));
 }
 
-void
-Test::requireThatAddWordTriggersChangeOfBuffer()
+TEST(WordStoreTest, add_word_triggers_change_of_buffer)
 {
     WordStore ws;
     size_t word = 0;
@@ -54,7 +44,7 @@ Test::requireThatAddWordTriggersChangeOfBuffer()
         sprintf(wordStr, "%6zu", word);
         // all words uses 12 bytes (include padding)
         EntryRef r = ws.addWord(std::string(wordStr));
-        EXPECT_EQUAL(std::string(wordStr), ws.getWord(r));
+        EXPECT_EQ(std::string(wordStr), ws.getWord(r));
         uint32_t bufferId = WordStore::RefType(r).bufferId();
         if (bufferId > lastId) {
             LOG(info,
@@ -68,23 +58,11 @@ Test::requireThatAddWordTriggersChangeOfBuffer()
         }
     }
     LOG(info, "Added %zu words in 4 buffers", word);
-    EXPECT_EQUAL(2047u, word);
-    EXPECT_EQUAL(4u, lastId);
-}
-
-int
-Test::Main()
-{
-    TEST_INIT("word_store_test");
-
-    requireThatWordsCanBeAddedAndRetrieved();
-    requireThatAddWordTriggersChangeOfBuffer();
-
-    TEST_DONE();
+    EXPECT_EQ(2047u, word);
+    EXPECT_EQ(4u, lastId);
 }
 
 }
-}
 
-TEST_APPHOOK(search::memoryindex::Test);
+GTEST_MAIN_RUN_ALL_TESTS()
 
