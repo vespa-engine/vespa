ce05a635b0cadb7fecdc06a92d90ab6477ae529b
Martin Polden <mpolden@mpolden.no>
2019-07-12 09:37:31 +0200

    Audit log /flags/v1 on controller

---
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/restapi/flags/AuditedFlagsApiTest.java
@@ -0,0 +1,57 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.hosted.controller.restapi.flags;
+
+import com.yahoo.application.container.handler.Request;
+import com.yahoo.vespa.athenz.api.AthenzIdentity;
+import com.yahoo.vespa.athenz.api.AthenzUser;
+import com.yahoo.vespa.hosted.controller.auditlog.AuditLog;
+import com.yahoo.vespa.hosted.controller.restapi.ContainerControllerTester;
+import com.yahoo.vespa.hosted.controller.restapi.ControllerContainerTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author mpolden
+ */
+public class AuditedFlagsApiTest extends ControllerContainerTest {
+
+    private static final String responses = "src/test/java/com/yahoo/vespa/hosted/controller/restapi/flags/responses/";
+    private static final AthenzIdentity operator = AthenzUser.fromUserId("operatorUser");
+
+    private ContainerControllerTester tester;
+
+    @Before
+    public void before() {
+        addUserToHostedOperatorRole(operator);
+        tester = new ContainerControllerTester(container, responses);
+    }
+
+    @Test
+    public void test_audit_logging() {
+        var body = "{\n" +
+                   "  \"id\": \"id1\",\n" +
+                   "  \"rules\": [\n" +
+                   "    {\n" +
+                   "      \"value\": true\n" +
+                   "    }\n" +
+                   "  ]\n" +
+                   "}";
+        assertResponse(new Request("http://localhost:8080/flags/v1/data/id1?force=true", body, Request.Method.PUT),
+                       "", 200);
+        var log = tester.controller().auditLogger().readLog();
+        assertEquals(1, log.entries().size());
+        var entry = log.entries().get(0);
+        assertEquals(operator.getFullName(), entry.principal());
+        assertEquals(AuditLog.Entry.Method.PUT, entry.method());
+        assertEquals("/flags/v1/data/id1?force=true", entry.resource());
+        assertEquals(body, log.entries().get(0).data().get());
+    }
+
+    private void assertResponse(Request request, String body, int statusCode) {
+        addIdentityToRequest(request, operator);
+        tester.assertResponse(request, body, statusCode);
+    }
+
+}
