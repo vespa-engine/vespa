6d029b5c3d9b65e5896390a4b426887784fe0511
Harald Musum <musum@verizonmedia.com>
2019-07-24 22:13:18 +0200

    Turn off verbose gc for logserver container, too noisy

--- config-model/src/main/java/com/yahoo/vespa/model/admin/LogserverContainerCluster.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/LogserverContainerCluster.java
@@ -3,11 +3,12 @@ package com.yahoo.vespa.model.admin;
 
 import com.yahoo.config.model.deploy.DeployState;
 import com.yahoo.config.model.producer.AbstractConfigProducer;
+import com.yahoo.search.config.QrStartConfig;
 import com.yahoo.vespa.model.container.ContainerCluster;
 import com.yahoo.vespa.model.container.component.Handler;
 
 /**
- * @author gjoranv
+ * @author hmusum
  */
 public class LogserverContainerCluster extends ContainerCluster<LogserverContainer> {
 
@@ -20,6 +21,14 @@ public class LogserverContainerCluster extends ContainerCluster<LogserverContain
 
     @Override
     protected void doPrepare(DeployState deployState) { }
+
+    // Switch off verbose:gc, it's very noisy when Xms < Xmx
+    @Override
+    public void getConfig(QrStartConfig.Builder builder) {
+        super.getConfig(builder);
+        // This takes effect via vespa-start-container-daemon:configure_gcopts
+        builder.jvm.verbosegc(false);
+    }
 
     private void addLogHandler() {
         Handler<?> logHandler = Handler.fromClassName(ContainerCluster.LOG_HANDLER_CLASS);
