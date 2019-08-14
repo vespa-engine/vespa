9b562a3489fba79bd0faf776687291088815621d
Jon Marius Venstad <venstad@gmail.com>
2019-08-14 17:19:04 +0200

    Add custom path to PATH to run mvn?

--- vespa-testrunner-components/src/test/java/com/yahoo/vespa/hosted/testrunner/TestRunnerTest.java
+++ vespa-testrunner-components/src/test/java/com/yahoo/vespa/hosted/testrunner/TestRunnerTest.java
@@ -15,7 +15,6 @@ import java.util.logging.LogRecord;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Unit tests relying on a UNIX shell >_<
