01a6d4335330aaa71d6d870d6dc4396715730f1d
Valerij Fredriksen <valerijf@verizonmedia.com>
2019-08-14 12:46:37 +0200

    Use aws region in AwsEventFecther

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/AwsEventReporterMaintainer.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/AwsEventReporterMaintainer.java
@@ -1,7 +1,7 @@
 package com.yahoo.vespa.hosted.controller.maintenance;
 
 import com.yahoo.config.provision.CloudName;
-import com.yahoo.config.provision.zone.ZoneList;
+import com.yahoo.config.provision.zone.ZoneApi;
 import com.yahoo.vespa.hosted.controller.Controller;
 import com.yahoo.vespa.hosted.controller.api.integration.aws.AwsEventFetcher;
 import com.yahoo.vespa.hosted.controller.api.integration.aws.CloudEvent;
@@ -10,8 +10,10 @@ import com.yahoo.vespa.hosted.controller.api.integration.organization.IssueHandl
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * @author mgimle
@@ -22,27 +24,26 @@ public class AwsEventReporterMaintainer extends Maintainer {
 
     private final IssueHandler issueHandler;
     private final AwsEventFetcher eventFetcher;
-    private final ZoneList cloudZones;
+    private final Set<String> awsRegions;
 
     AwsEventReporterMaintainer(Controller controller, Duration interval, JobControl jobControl,
                                IssueHandler issueHandler, AwsEventFetcher eventFetcher) {
         super(controller, interval, jobControl);
-        this.cloudZones = awsZones(controller);
         this.issueHandler = issueHandler;
         this.eventFetcher = eventFetcher;
-    }
-
-    private ZoneList awsZones(Controller controller) {
-        return controller.zoneRegistry().zones()
+        this.awsRegions = controller.zoneRegistry().zones()
                 .ofCloud(CloudName.from("aws"))
-                .reachable();
+                .reachable()
+                .zones().stream()
+                .map(ZoneApi::getCloudNativeRegionName)
+                .collect(Collectors.toSet());
     }
 
     @Override
     protected void maintain() {
         log.log(Level.INFO, "Fetching events for cloud hosts.");
-        for (var cloudZoneId : cloudZones.ids()) {
-            List<CloudEvent> events = eventFetcher.getEvents(cloudZoneId);
+        for (var awsRegion : awsRegions) {
+            List<CloudEvent> events = eventFetcher.getEvents(awsRegion);
             for (var event : events) {
                 Issue issue = eventFetcher.createIssue(event);
                 if (!issueHandler.issueExists(issue)) {
