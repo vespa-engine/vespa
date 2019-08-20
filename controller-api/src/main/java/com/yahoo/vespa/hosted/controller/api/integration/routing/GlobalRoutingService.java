f6d25e48ed90df783527c53e217e46d5153e396e
Martin Polden <mpolden@mpolden.no>
2019-08-20 14:21:25 +0200

    Update javadoc

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/routing/GlobalRoutingService.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/routing/GlobalRoutingService.java
@@ -1,14 +1,13 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.api.integration.routing;
 
 import java.util.Map;
 
 /**
- * A global routing service.
+ * A service containing the health status of global rotations.
  *
  * @author mpolden
  */
-// TODO: Remove once DeploymentMetricsMaintainer starts providing rotation status
 public interface GlobalRoutingService {
 
     /** Returns the health status for each endpoint behind the given rotation name */
