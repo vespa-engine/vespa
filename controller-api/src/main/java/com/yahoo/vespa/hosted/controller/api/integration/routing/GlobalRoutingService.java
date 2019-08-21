390dc9e7f62d9c3cce0e6f5a63aaab1c7e9771a6
Martin Polden <mpolden@mpolden.no>
2019-08-21 15:30:33 +0200

    Change getHealthStatus return type

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/routing/GlobalRoutingService.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/routing/GlobalRoutingService.java
@@ -1,6 +1,8 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.api.integration.routing;
 
+import com.yahoo.config.provision.zone.ZoneId;
+
 import java.util.Map;
 
 /**
@@ -10,7 +12,7 @@ import java.util.Map;
  */
 public interface GlobalRoutingService {
 
-    /** Returns the health status for each endpoint behind the given rotation name */
-    Map<String, RotationStatus> getHealthStatus(String rotationName);
+    /** Returns the health status of each zone behind the given rotation name */
+    Map<ZoneId, RotationStatus> getHealthStatus(String rotationName);
 
 }
