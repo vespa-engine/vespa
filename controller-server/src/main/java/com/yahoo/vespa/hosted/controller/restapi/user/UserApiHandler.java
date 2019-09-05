5549ed6df77e23249027fe204a683a15f604fe13
Torbjørn Smørgrav <smorgrav@users.noreply.github.com>
2019-09-05 22:00:11 +0200

    Return user object instead of user id from listUser in UserManagement (#10455)

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/user/UserApiHandler.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/user/UserApiHandler.java
@@ -14,6 +14,7 @@ import com.yahoo.slime.Inspector;
 import com.yahoo.slime.Slime;
 import com.yahoo.vespa.config.SlimeUtils;
 import com.yahoo.vespa.hosted.controller.api.integration.user.Roles;
+import com.yahoo.vespa.hosted.controller.api.integration.user.User;
 import com.yahoo.vespa.hosted.controller.api.integration.user.UserId;
 import com.yahoo.vespa.hosted.controller.api.integration.user.UserManagement;
 import com.yahoo.vespa.hosted.controller.api.role.Role;
@@ -129,11 +130,11 @@ public class UserApiHandler extends LoggingRequestHandler {
         for (Role role : roles)
             rolesArray.addString(valueOf(role));
 
-        Map<UserId, List<Role>> memberships = new LinkedHashMap<>();
+        Map<User, List<Role>> memberships = new LinkedHashMap<>();
         List<Role> allRoles = new ArrayList<>(superRoles); // Membership in a super role may imply membership in a role.
         allRoles.addAll(roles);
         for (Role role : allRoles)
-            for (UserId user : users.listUsers(role)) {
+            for (User user : users.listUsers(role)) {
                 memberships.putIfAbsent(user, new ArrayList<>());
                 memberships.get(user).add(role);
             }
@@ -141,7 +142,15 @@ public class UserApiHandler extends LoggingRequestHandler {
         Cursor usersArray = root.setArray("users");
         memberships.forEach((user, userRoles) -> {
             Cursor userObject = usersArray.addObject();
-            userObject.setString("name", user.value());
+            userObject.setString("name", user.name());
+            userObject.setString("email", user.email());
+            if (user.nickname() != null) {
+                userObject.setString("nickname", user.nickname());
+            }
+            if (user.picture()!= null) {
+                userObject.setString("picture", user.picture());
+            }
+
             Cursor rolesObject = userObject.setObject("roles");
             for (Role role : roles) {
                 Cursor roleObject = rolesObject.setObject(valueOf(role));
@@ -174,12 +183,14 @@ public class UserApiHandler extends LoggingRequestHandler {
         String roleName = require("roleName", Inspector::asString, requestObject);
         UserId user = new UserId(require("user", Inspector::asString, requestObject));
         Role role = Roles.toRole(TenantName.from(tenantName), roleName);
-        if (   role.definition() == RoleDefinition.tenantOwner
-            && users.listUsers(role).equals(List.of(user)))
+        List<User> currentUsers = users.listUsers(role);
+        if (role.definition() == RoleDefinition.tenantOwner
+                && currentUsers.size() == 1
+                && currentUsers.get(0).email().equals(user.value()))
             throw new IllegalArgumentException("Can't remove the last owner of a tenant.");
 
         users.removeUsers(role, List.of(user));
-        return new MessageResponse(user + " is no longer a member of " + role);
+        return new MessageResponse(user+" is no longer a member of "+role);
     }
 
     private HttpResponse removeApplicationRoleMember(String tenantName, String applicationName, HttpRequest request) {
