a237e58ae4aed91b5d3da15f72bb2a2d4c139c56
Jon Marius Venstad <venstad@gmail.com>
2019-09-19 08:31:12 +0200

    Revert reading new data immediately, as some some writes may be lost during upgrade

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/persistence/CuratorDb.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/persistence/CuratorDb.java
@@ -339,8 +339,7 @@ public class CuratorDb {
     }
 
     public Optional<Instance> readInstance(ApplicationId application) {
-        return readSlime(instancePath(application)).or(() -> readSlime(applicationPath(application)))
-                                                   .map(instanceSerializer::fromSlime);
+        return readSlime(applicationPath(application)).map(instanceSerializer::fromSlime);
     }
 
     public List<Instance> readInstances() {
@@ -352,11 +351,10 @@ public class CuratorDb {
     }
 
     private Stream<ApplicationId> readInstanceIds() {
-        return Stream.concat(curator.getChildren(applicationRoot).stream()
-                                    .filter(id -> id.split(":").length == 3),
-                             curator.getChildren(instanceRoot).stream())
-                     .distinct()
-                     .map(ApplicationId::fromSerializedForm);
+        return curator.getChildren(applicationRoot).stream()
+                      .filter(id -> id.split(":").length == 3)
+                      .distinct()
+                      .map(ApplicationId::fromSerializedForm);
     }
 
     private List<Instance> readInstances(Predicate<ApplicationId> instanceFilter) {
