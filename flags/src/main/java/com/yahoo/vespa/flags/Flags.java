54ce791b5e9b8e57575c051e72bbf7bc8d830da6
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-09-23 15:56:10 +0200

    Add application id as filter dimension

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -166,7 +166,8 @@ public class Flags {
     public static final UnboundBooleanFlag HEALTH_CHECK_ON_4081 = defineFeatureFlag(
             "health-check-on-4081", false,
             "Change nginx to send health check requests on port 4081 instead of 4080.",
-            "Takes effect on routing container redeployment");
+            "Takes effect on routing container redeployment",
+            APPLICATION_ID);
 
     /** WARNING: public for testing: All flags should be defined in {@link Flags}. */
     public static UnboundBooleanFlag defineFeatureFlag(String flagId, boolean defaultValue, String description,
