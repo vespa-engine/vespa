90224e5c6bcd74ba1c8bb88c104cfa5b8556057a
Jon Bratseth <bratseth@verizonmedia.com>
2019-09-24 21:56:57 +0200

    Remove unnecessary import

--- container-search/src/test/java/com/yahoo/search/grouping/result/GroupListTestCase.java
+++ container-search/src/test/java/com/yahoo/search/grouping/result/GroupListTestCase.java
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.search.grouping.result;
 
-import com.yahoo.search.Query;
 import com.yahoo.search.grouping.Continuation;
 import org.junit.Test;
 
