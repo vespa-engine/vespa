a67d55136f07974d7258d8918dc4fbeb0901322e
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-10-03 16:08:38 +0200

    Enable TLSv1.3 for servers/clients based on TlsContext

--- security-utils/src/main/java/com/yahoo/security/tls/TlsContext.java
+++ security-utils/src/main/java/com/yahoo/security/tls/TlsContext.java
@@ -24,7 +24,7 @@ public interface TlsContext extends AutoCloseable {
             "TLS_AES_256_GCM_SHA384", // TLSv1.3
             "TLS_CHACHA20_POLY1305_SHA256"); // TLSv1.3
 
-    Set<String> ALLOWED_PROTOCOLS = Set.of("TLSv1.2"); // TODO Enable TLSv1.3
+    Set<String> ALLOWED_PROTOCOLS = Set.of("TLSv1.2", "TLSv1.3");
 
     SSLContext context();
 
