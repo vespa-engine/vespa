fb443a6825e5355a4041de75bcfcbf238fe4936b
Martin Polden <mpolden@mpolden.no>
2019-10-07 14:42:47 +0200

    Remove legacy NodeVersion formats

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/persistence/OsVersionStatusSerializerTest.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/persistence/OsVersionStatusSerializerTest.java
@@ -6,16 +6,12 @@ import com.yahoo.component.Version;
 import com.yahoo.config.provision.CloudName;
 import com.yahoo.config.provision.HostName;
 import com.yahoo.config.provision.zone.ZoneId;
-import com.yahoo.vespa.config.SlimeUtils;
 import com.yahoo.vespa.hosted.controller.versions.NodeVersion;
 import com.yahoo.vespa.hosted.controller.versions.NodeVersions;
 import com.yahoo.vespa.hosted.controller.versions.OsVersion;
 import com.yahoo.vespa.hosted.controller.versions.OsVersionStatus;
 import org.junit.Test;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.List;
 
@@ -45,26 +41,6 @@ public class OsVersionStatusSerializerTest {
         OsVersionStatus status = new OsVersionStatus(versions.build());
         OsVersionStatus serialized = serializer.fromSlime(serializer.toSlime(status));
         assertEquals(status.versions(), serialized.versions());
-    }
-
-    @Test
-    public void testLegacySerialization() throws Exception {
-        var data = Files.readAllBytes(Paths.get("src/test/java/com/yahoo/vespa/hosted/controller/persistence/testdata/os-version-status-legacy-format.json"));
-        var serializer = new OsVersionStatusSerializer(new OsVersionSerializer(), new NodeVersionSerializer());
-        var versions = ImmutableMap.of(
-                new OsVersion(Version.fromString("7.42"), CloudName.from("yahoo")),
-                NodeVersions.EMPTY.with(List.of(new NodeVersion(HostName.from("node1"), ZoneId.from("prod", "us-north-1"),
-                                                                Version.fromString("7.42"), Version.emptyVersion, Instant.EPOCH),
-                                                new NodeVersion(HostName.from("node2"), ZoneId.from("test", "us-north-2"),
-                                                                Version.fromString("7.42"), Version.emptyVersion, Instant.EPOCH))));
-
-        var deserialized = serializer.fromSlime(SlimeUtils.jsonToSlime(data));
-        assertEquals(versions, deserialized.versions());
-
-
-        var serialized = new String(SlimeUtils.toJsonBytes(serializer.toSlime(new OsVersionStatus(versions))), StandardCharsets.UTF_8);
-        assertEquals("{\"versions\":[{\"version\":\"7.42.0\",\"cloud\":\"yahoo\",\"nodeVersions\":[{\"hostname\":\"node1\",\"zone\":\"prod.us-north-1\",\"wantedVersion\":\"0.0.0\",\"changedAt\":0},{\"hostname\":\"node2\",\"zone\":\"test.us-north-2\",\"wantedVersion\":\"0.0.0\",\"changedAt\":0}],\"nodes\":[{\"hostname\":\"node1\",\"version\":\"7.42.0\",\"region\":\"us-north-1\",\"environment\":\"prod\"},{\"hostname\":\"node2\",\"version\":\"7.42.0\",\"region\":\"us-north-2\",\"environment\":\"test\"}]}]}",
-                     serialized);
     }
 
 }
