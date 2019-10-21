ecbe5f08da00e7eb922b6912c59cefd3dad5dc45
gjoranv <gv@verizonmedia.com>
2019-10-21 16:37:41 +0200

    Move tests to package c.y.container.plugin

--- bundle-plugin-test/integration-test/src/test/java/com/yahoo/container/plugin/BundleTest.java
+++ bundle-plugin-test/integration-test/src/test/java/com/yahoo/container/plugin/BundleTest.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo;
+package com.yahoo.container.plugin;
 
 import com.yahoo.osgi.maven.ProjectBundleClassPaths;
 import com.yahoo.vespa.config.VespaVersion;
