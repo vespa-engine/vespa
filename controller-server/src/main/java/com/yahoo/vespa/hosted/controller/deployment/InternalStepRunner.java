e6b0037b63304c39c93ba35bfee2d23480c2c842
Jon Marius Venstad <venstad@gmail.com>
2019-10-31 11:58:25 +0100

    Let OUT_OF_CAPACITY fail test jobs, for retry on a higher level

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/deployment/InternalStepRunner.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/deployment/InternalStepRunner.java
@@ -250,8 +250,7 @@ public class InternalStepRunner implements StepRunner {
             return Optional.of(running);
         }
         catch (ConfigServerException e) {
-            if (   e.getErrorCode() == OUT_OF_CAPACITY && type.isTest()
-                || e.getErrorCode() == ACTIVATION_CONFLICT
+            if (   e.getErrorCode() == ACTIVATION_CONFLICT
                 || e.getErrorCode() == APPLICATION_LOCK_FAILURE
                 || e.getErrorCode() == PARENT_HOST_NOT_READY
                 || e.getErrorCode() == CERTIFICATE_NOT_READY
@@ -560,10 +559,12 @@ public class InternalStepRunner implements StepRunner {
     private Optional<RunStatus> report(RunId id, DualLogger logger) {
         try {
             controller.jobController().active(id).ifPresent(run -> {
-                JobReport report = JobReport.ofJob(run.id().application(),
+                        JobReport report = JobReport.ofJob(run.id().application(),
                                                    run.id().type(),
                                                    run.id().number(),
-                                                   run.hasFailed() ? Optional.of(DeploymentJobs.JobError.unknown) : Optional.empty());
+                                                   ! run.hasFailed() ? Optional.empty()
+                                                                     : Optional.of(run.status() == outOfCapacity ? DeploymentJobs.JobError.outOfCapacity
+                                                                                                                 : DeploymentJobs.JobError.unknown));
                 controller.applications().deploymentTrigger().notifyOfCompletion(report);
 
                 if (run.hasFailed())
