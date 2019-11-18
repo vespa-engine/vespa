3646c57fe14c84ef8c62fade5922dad4f9ae38d3
Jon Bratseth <bratseth@verizonmedia.com>
2019-11-18 13:22:52 +0100

    Optimize imports

--- container-search/src/main/java/com/yahoo/prelude/query/SameElementItem.java
+++ container-search/src/main/java/com/yahoo/prelude/query/SameElementItem.java
@@ -1,7 +1,6 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.prelude.query;
 
-import com.google.common.annotations.Beta;
 import com.yahoo.protect.Validator;
 
 import java.nio.ByteBuffer;
