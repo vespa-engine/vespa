ee87cc8d429e3a0ead04ace9438267d75f17dec3
Harald Musum <musum@verizonmedia.com>
2019-12-05 21:21:47 +0100

    Revert "Use shell script wrapper for vespa-rpc-invoke."

--- fnet/src/tests/examples/examples_test.cpp
+++ fnet/src/tests/examples/examples_test.cpp
@@ -75,7 +75,7 @@ TEST("usage") {
         EXPECT_FALSE(runProc(proc, done));
     }
     {
-        SlaveProc proc("exec ../../examples/frt/rpc/vespa-rpc-invoke-bin");
+        SlaveProc proc("exec ../../examples/frt/rpc/vespa-rpc-invoke");
         EXPECT_FALSE(runProc(proc, done));
     }
     {
@@ -197,7 +197,7 @@ TEST_MT_F("rpc invoke", 2, std::atomic<bool>()) {
         EXPECT_TRUE(runProc(proc, f1));
     } else {
         TEST_BARRIER();
-        EXPECT_TRUE(runProc(vespalib::make_string("exec ../../examples/frt/rpc/vespa-rpc-invoke-bin tcp/localhost:%d frt.rpc.echo "
+        EXPECT_TRUE(runProc(vespalib::make_string("exec ../../examples/frt/rpc/vespa-rpc-invoke tcp/localhost:%d frt.rpc.echo "
                                                   "b:1 h:2 i:4 l:8 f:0.5 d:0.25 s:foo",
                                                   PORT0).c_str()));
         f1 = true;
