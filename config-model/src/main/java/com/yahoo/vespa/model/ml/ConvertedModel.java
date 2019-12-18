d57cfc7220266e90570ad4fe4e3140bc1a9093d6
Jon Marius Venstad <jonmv@users.noreply.github.com>
2019-12-18 15:46:50 +0100

    Revert "Add option to use default signature for onnx models"

--- config-model/src/main/java/com/yahoo/vespa/model/ml/ConvertedModel.java
+++ config-model/src/main/java/com/yahoo/vespa/model/ml/ConvertedModel.java
@@ -1,7 +1,6 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.ml;
 
-import ai.vespa.rankingexpression.importer.IntermediateGraph;
 import ai.vespa.rankingexpression.importer.configmodelview.ImportedMlFunction;
 import com.google.common.collect.ImmutableMap;
 import com.yahoo.collections.Pair;
@@ -161,16 +160,13 @@ public class ConvertedModel {
         ExpressionFunction expression = expressions.get(arguments.toName());
         if (expression != null) return expression;
 
-        expression = expressions.get(IntermediateGraph.defaultSignature() + "." + arguments.toName());
-        if (expression != null) return expression;
-
-        if (arguments.signature().isEmpty()) {
+        if ( ! arguments.signature().isPresent()) {
             if (expressions.size() > 1)
                 throw new IllegalArgumentException("Multiple candidate expressions " + missingExpressionMessageSuffix());
             return expressions.values().iterator().next();
         }
 
-        if (arguments.output().isEmpty()) {
+        if ( ! arguments.output().isPresent()) {
             List<Map.Entry<String, ExpressionFunction>> entriesWithTheRightPrefix =
                     expressions.entrySet().stream().filter(entry -> entry.getKey().startsWith(arguments.signature().get() + ".")).collect(Collectors.toList());
             if (entriesWithTheRightPrefix.size() < 1)
