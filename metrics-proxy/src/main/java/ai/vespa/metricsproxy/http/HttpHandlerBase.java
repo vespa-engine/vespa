fdb3dc9c329252764a1fa45ec4eb38c199a19827
gjoranv <gv@verizonmedia.com>
2019-12-19 11:02:34 +0100

    Simplify HttpHandlerBase to make it more generic and reusable.

--- metrics-proxy/src/main/java/ai/vespa/metricsproxy/http/HttpHandlerBase.java
+++ metrics-proxy/src/main/java/ai/vespa/metricsproxy/http/HttpHandlerBase.java
@@ -32,14 +32,8 @@ import static java.util.logging.Level.WARNING;
  */
 public abstract class HttpHandlerBase extends ThreadedHttpRequestHandler {
 
-    protected final ValuesFetcher valuesFetcher;
-
-    protected HttpHandlerBase(Executor executor,
-                              MetricsManager metricsManager,
-                              VespaServices vespaServices,
-                              MetricsConsumers metricsConsumers) {
+    protected HttpHandlerBase(Executor executor) {
         super(executor);
-        valuesFetcher = new ValuesFetcher(metricsManager, vespaServices, metricsConsumers);
     }
 
     protected abstract Optional<HttpResponse> doHandle(URI requestUri, Path apiPath, String consumer);
