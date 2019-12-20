6fd7ce9f240062feef130f25a173da415f76e4be
Harald Musum <musum@verizonmedia.com>
2019-12-20 08:04:06 +0100

    Revert "Upgrade to Curator 2.13.0"

--- zkfacade/src/main/java/org/apache/curator/framework/api/package-info.java
+++ zkfacade/src/main/java/org/apache/curator/framework/api/package-info.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 2, minor = 13, micro = 0))
+@ExportPackage(version = @Version(major = 2, minor = 9, micro = 1))
 package org.apache.curator.framework.api;
 import com.yahoo.osgi.annotation.ExportPackage;
 import com.yahoo.osgi.annotation.Version;
