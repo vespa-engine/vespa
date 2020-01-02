6953fc7d41d090cf56537223f6e86b1f3c2c58d3
andreer <andreer@verizonmedia.com>
2020-01-02 15:57:19 +0100

    re-add disk write test feature flag

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -171,6 +171,12 @@ public class Flags {
              "Takes effect on next host admin tick",
             NODE_TYPE, HOSTNAME, APPLICATION_ID);
 
+    public static final UnboundBooleanFlag ENABLE_DISK_WRITE_TEST = defineFeatureFlag(
+            "enable-disk-write-test", false,
+            "Regularly issue a small write to disk and fail the host if it is not successful",
+            "Takes effect on next node agent tick (but does not clear existing failure reports)",
+            HOSTNAME);
+
     /** WARNING: public for testing: All flags should be defined in {@link Flags}. */
     public static UnboundBooleanFlag defineFeatureFlag(String flagId, boolean defaultValue, String description,
                                                        String modificationEffect, FetchVector.Dimension... dimensions) {
