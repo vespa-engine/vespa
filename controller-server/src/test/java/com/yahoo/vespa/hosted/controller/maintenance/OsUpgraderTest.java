b9964ac57af1d86b9596150fe913c542792324b0
Martin Polden <mpolden@mpolden.no>
2020-01-08 14:45:44 +0100

    Avoid checking every node when setting upgrade target

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/OsUpgraderTest.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/maintenance/OsUpgraderTest.java
@@ -13,7 +13,6 @@ import com.yahoo.vespa.hosted.controller.application.SystemApplication;
 import com.yahoo.vespa.hosted.controller.integration.NodeRepositoryMock;
 import com.yahoo.vespa.hosted.controller.integration.ZoneApiMock;
 import com.yahoo.vespa.hosted.controller.versions.NodeVersion;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -35,16 +34,9 @@ public class OsUpgraderTest {
     private static final ZoneApi zone4 = ZoneApiMock.newBuilder().withId("prod.us-east-3").build();
     private static final ZoneApi zone5 = ZoneApiMock.newBuilder().withId("prod.us-north-1").withCloud("other").build();
 
-    private ControllerTester tester;
-    private OsVersionStatusUpdater statusUpdater;
-
-    @Before
-    public void before() {
-        tester = new ControllerTester();
-        statusUpdater = new OsVersionStatusUpdater(tester.controller(), Duration.ofDays(1),
-                                                   new JobControl(tester.controller().curator()));
-    }
-
+    private final ControllerTester tester = new ControllerTester();
+    private final OsVersionStatusUpdater statusUpdater = new OsVersionStatusUpdater(tester.controller(), Duration.ofDays(1),
+                                                                                    new JobControl(tester.controller().curator()));
     @Test
     public void upgrade_os() {
         OsUpgrader osUpgrader = osUpgrader(
