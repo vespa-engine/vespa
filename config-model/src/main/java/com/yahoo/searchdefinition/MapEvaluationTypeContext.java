3e43db28312f1eb281c24e42b6aa522f7ec66297
Jon Bratseth <bratseth@verizonmedia.com>
2020-01-09 11:48:15 +0100

    Optimize imports

--- config-model/src/main/java/com/yahoo/searchdefinition/MapEvaluationTypeContext.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/MapEvaluationTypeContext.java
@@ -13,17 +13,14 @@ import com.yahoo.searchlib.rankingexpression.rule.ReferenceNode;
 import com.yahoo.tensor.TensorType;
 import com.yahoo.tensor.evaluation.TypeContext;
 
-import java.sql.Ref;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
