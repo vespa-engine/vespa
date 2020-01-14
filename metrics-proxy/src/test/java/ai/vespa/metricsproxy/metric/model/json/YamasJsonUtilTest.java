fd2b59ab0c025e63c353edd4657b6582abb4097b
gjoranv <gv@verizonmedia.com>
2020-01-14 16:07:22 +0100

    Enable the default public metrics consumer in hosted Vespa.
    - Always filter this consumer from yamas routing, as the effect
      of including it is uncertain.

--- metrics-proxy/src/test/java/ai/vespa/metricsproxy/metric/model/json/YamasJsonUtilTest.java
+++ metrics-proxy/src/test/java/ai/vespa/metricsproxy/metric/model/json/YamasJsonUtilTest.java
@@ -5,10 +5,15 @@ import ai.vespa.metricsproxy.metric.model.MetricsPacket;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Set;
 
+import static ai.vespa.metricsproxy.core.VespaMetrics.VESPA_CONSUMER_ID;
+import static ai.vespa.metricsproxy.http.ValuesFetcher.DEFAULT_PUBLIC_CONSUMER_ID;
 import static ai.vespa.metricsproxy.metric.model.ServiceId.toServiceId;
+import static ai.vespa.metricsproxy.metric.model.json.YamasJsonUtil.YAMAS_ROUTING;
 import static ai.vespa.metricsproxy.metric.model.json.YamasJsonUtil.toMetricsPackets;
 import static java.util.Collections.singleton;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -47,6 +52,26 @@ public class YamasJsonUtilTest {
     @Test
     public void empty_consumers_is_translated_to_null_routing_in_json_model() {
         MetricsPacket packet = new MetricsPacket.Builder(toServiceId("foo"))
+                .build();
+        YamasJsonModel jsonModel = YamasJsonUtil.toYamasArray(singleton(packet)).metrics.get(0);
+        assertNull(jsonModel.routing);
+    }
+
+    @Test
+    public void default_public_consumer_is_filtered_from_yamas_routing() {
+        MetricsPacket packet = new MetricsPacket.Builder(toServiceId("foo"))
+                .addConsumers(Set.of(VESPA_CONSUMER_ID, DEFAULT_PUBLIC_CONSUMER_ID))
+                .build();
+        YamasJsonModel jsonModel = YamasJsonUtil.toYamasArray(singleton(packet)).metrics.get(0);
+        List<String> namespaces = jsonModel.routing.get(YAMAS_ROUTING).namespaces;
+        assertEquals(1, namespaces.size());
+        assertEquals(VESPA_CONSUMER_ID.id, namespaces.get(0));
+    }
+
+    @Test
+    public void only_default_public_consumer_yields_null_routing_in_json_model() {
+        MetricsPacket packet = new MetricsPacket.Builder(toServiceId("foo"))
+                .addConsumers(Set.of(DEFAULT_PUBLIC_CONSUMER_ID))
                 .build();
         YamasJsonModel jsonModel = YamasJsonUtil.toYamasArray(singleton(packet)).metrics.get(0);
         assertNull(jsonModel.routing);
