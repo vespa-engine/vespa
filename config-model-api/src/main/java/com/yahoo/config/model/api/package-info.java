962a5ba165ea3fb81d31d7f4dbe2bc1558fd41ce
andreer <andreer@verizonmedia.com>
2020-01-21 12:01:04 +0100

    use abi checker on config model api
    this will make any model changes obviously explicit in the change log,
    hopefully leading to less build breaking changes (like mine yesterday).

--- config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
@@ -1,5 +1,7 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
+@PublicApi // Not really "public", only annotated as such to enable the ABI checker plugin
 package com.yahoo.config.model.api;
 
 import com.yahoo.osgi.annotation.ExportPackage;
+import com.yahoo.api.annotations.PublicApi;
