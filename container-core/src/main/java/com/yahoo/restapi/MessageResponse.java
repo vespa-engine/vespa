c3f347d7b524307cb1486500b80359221c13d05b
Martin Polden <mpolden@mpolden.no>
2020-01-23 10:57:41 +0100

    Simplify MessageResponse

--- container-core/src/main/java/com/yahoo/restapi/MessageResponse.java
+++ container-core/src/main/java/com/yahoo/restapi/MessageResponse.java
@@ -1,33 +1,23 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.restapi;
 
-import com.yahoo.container.jdisc.HttpResponse;
-import com.yahoo.slime.JsonFormat;
 import com.yahoo.slime.Slime;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * A 200 ok response with a message in JSON.
  *
  * @author bratseth
  */
-public class MessageResponse extends HttpResponse {
-
-    private final Slime slime = new Slime();
+public class MessageResponse extends SlimeJsonResponse {
 
     public MessageResponse(String message) {
-        super(200);
+        super(slime(message));
+    }
+
+    private static Slime slime(String message) {
+        var slime = new Slime();
         slime.setObject().setString("message", message);
+        return slime;
     }
-
-    @Override
-    public void render(OutputStream stream) throws IOException {
-        new JsonFormat(true).encode(stream, slime);
-    }
-
-    @Override
-    public String getContentType() { return "application/json"; }
 
 }
