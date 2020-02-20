b53e4c36d56adfb83974a18438c92dc282e3a7e2
Ola Aunrønning <olaa@verizonmedia.com>
2020-02-20 15:50:06 +0100

    Add some info and debug logging

--- metrics-proxy/src/main/java/ai/vespa/metricsproxy/telegraf/TelegrafRegistry.java
+++ metrics-proxy/src/main/java/ai/vespa/metricsproxy/telegraf/TelegrafRegistry.java
@@ -1,9 +1,12 @@
 // Copyright 2020 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.metricsproxy.telegraf;
 
+import com.yahoo.log.LogLevel;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author olaa
@@ -12,11 +15,15 @@ public class TelegrafRegistry {
 
     private static final List<Telegraf> telegrafInstances = Collections.synchronizedList(new ArrayList<>());
 
+    private static final Logger logger = Logger.getLogger(TelegrafRegistry.class.getName());
+
     public void addInstance(Telegraf telegraf) {
+        logger.log(LogLevel.DEBUG, () -> "Adding Telegraf instance to registry");
         telegrafInstances.add(telegraf);
     }
 
     public void removeInstance(Telegraf telegraf) {
+        logger.log(LogLevel.DEBUG, () -> "Removing Telegraf instance from registry");
         telegrafInstances.remove(telegraf);
     }
 
