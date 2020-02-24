4c6f04d564a4cf2bf714c91c4ad646df27d54e30
Henning Baldersheim <balder@yahoo-inc.com>
2020-02-24 22:49:51 +0000

    Add benchmark for sequenced task executor.

---
+++ searchlib/src/tests/common/sequencedtaskexecutor/sequencedtaskexecutor_benchmark.cpp
@@ -0,0 +1,24 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include <vespa/searchlib/common/sequencedtaskexecutor.h>
+#include <vespa/vespalib/util/lambdatask.h>
+#include <atomic>
+
+using search::SequencedTaskExecutor;
+using ExecutorId = search::ISequencedTaskExecutor::ExecutorId;
+
+int main(int argc, char *argv[]) {
+    unsigned long numTasks = 1000000;
+    unsigned numThreads = 4;
+    std::atomic<long> counter(0);
+    if (argc > 1)
+        numTasks = atol(argv[1]);
+    if (argc > 2)
+        numThreads = atoi(argv[2]);
+
+    SequencedTaskExecutor executor(numThreads);
+    for (unsigned long tid(0); tid < numTasks; tid++) {
+        executor.executeTask(ExecutorId(tid%numThreads), vespalib::makeLambdaTask([&counter] { counter++; }));
+    }
+    return 0;
+}
