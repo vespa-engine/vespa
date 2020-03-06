90fe3477affd61b0e787d77a1cc04ca1c581dcf8
Henning Baldersheim <balder@yahoo-inc.com>
2020-03-06 16:19:06 +0000

    GC unused imports

--- container-search/src/main/java/com/yahoo/search/dispatch/Dispatcher.java
+++ container-search/src/main/java/com/yahoo/search/dispatch/Dispatcher.java
@@ -31,13 +31,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A dispatcher communicates with search nodes to perform queries and fill hits.
