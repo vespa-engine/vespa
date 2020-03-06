e1f336862841347ee48481d0f2cfd53284ccd2d1
Arnstein Ressem <aressem@gmail.com>
2020-03-06 18:45:59 +0100

    Revert "Upgrade from net.jpountz.lz4 1.3.0 to org.lz4 1.7.1"

--- vespajlib/src/main/java/net/jpountz/util/package-info.java
+++
@@ -1,5 +0,0 @@
-// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 1, minor = 7, micro = 1))
-package net.jpountz.util;
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
