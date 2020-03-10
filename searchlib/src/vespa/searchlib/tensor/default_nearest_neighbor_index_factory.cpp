03bd3b755650d828e63458379d5c120da63d7221
Henning Baldersheim <balder@yahoo-inc.com>
2020-03-10 14:26:49 +0000

    Moved to hw accelrated module.

--- searchlib/src/vespa/searchlib/tensor/default_nearest_neighbor_index_factory.cpp
+++ searchlib/src/vespa/searchlib/tensor/default_nearest_neighbor_index_factory.cpp
@@ -18,21 +18,12 @@ class LevelZeroGenerator : public RandomLevelGenerator {
 };
 
 DistanceFunction::UP
-make_distance_function(size_t vector_size, ValueType::CellType cell_type)
+make_distance_function(ValueType::CellType cell_type)
 {
-    bool hw_accelerated = (vector_size % 32) == 0;
-    if (hw_accelerated) {
-        if (cell_type == ValueType::CellType::FLOAT) {
-            return std::make_unique<HwAccelSquaredEuclideanDistance<float, 32>>();
-        } else {
-            return std::make_unique<HwAccelSquaredEuclideanDistance<double, 32>>();
-        }
+    if (cell_type == ValueType::CellType::FLOAT) {
+        return std::make_unique<SquaredEuclideanDistance<float>>();
     } else {
-        if (cell_type == ValueType::CellType::FLOAT) {
-            return std::make_unique<SquaredEuclideanDistance<float>>();
-        } else {
-            return std::make_unique<SquaredEuclideanDistance<double>>();
-        }
+        return std::make_unique<SquaredEuclideanDistance<double>>();
     }
 }
 
@@ -50,12 +41,13 @@ DefaultNearestNeighborIndexFactory::make(const DocVectorAccess& vectors,
                                          vespalib::eval::ValueType::CellType cell_type,
                                          const search::attribute::HnswIndexParams& params) const
 {
+    (void) vector_size;
     uint32_t m = params.max_links_per_node();
     HnswIndex::Config cfg(m * 2,
                           m,
                           params.neighbors_to_explore_at_insert(),
                           true);
-    return std::make_unique<HnswIndex>(vectors, make_distance_function(vector_size, cell_type), make_random_level_generator(m), cfg);
+    return std::make_unique<HnswIndex>(vectors, make_distance_function(cell_type), make_random_level_generator(m), cfg);
 }
 
 }
--- searchlib/src/vespa/searchlib/tensor/distance_functions.h
+++ searchlib/src/vespa/searchlib/tensor/distance_functions.h
@@ -30,30 +30,4 @@ public:
 template class SquaredEuclideanDistance<float>;
 template class SquaredEuclideanDistance<double>;
 
-/**
- * Hardware accelerated calculation of the square of the standard Euclidean distance.
- *
- * Uses vector instructions to perform calculation on smaller vectors of size VectorSizeBytes.
- * Caller must ensure that the size of the tensor vector is a multiple of VectorSizeBytes.
- */
-template <typename FloatType, size_t VectorSizeBytes>
-class HwAccelSquaredEuclideanDistance : public DistanceFunction {
-public:
-    HwAccelSquaredEuclideanDistance()
-        : _computer(vespalib::hwaccelrated::IAccelrated::getAccelrator())
-    {}
-    double calc(const vespalib::tensor::TypedCells& lhs, const vespalib::tensor::TypedCells& rhs) const override {
-        auto lhs_vector = lhs.typify<FloatType>();
-        auto rhs_vector = rhs.typify<FloatType>();
-
-        size_t sz = lhs_vector.size();
-        assert(sz == rhs_vector.size());
-        return _computer->squaredEuclidianDistance(&lhs_vector[0], &rhs_vector[0], sz);
-    }
-    vespalib::hwaccelrated::IAccelrated::UP _computer;
-};
-
-template class HwAccelSquaredEuclideanDistance<float, 32>;
-template class HwAccelSquaredEuclideanDistance<double, 32>;
-
 }
