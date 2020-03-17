cbc6b21dad61e115bd73c43707f04e0bee6744af
Jon Bratseth <bratseth@verizonmedia.com>
2020-03-17 12:37:15 +0100

    Correct typo

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/Rebalancer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/Rebalancer.java
@@ -92,7 +92,7 @@ public class Rebalancer extends Maintainer {
         for (Node node : allNodes.nodeType(NodeType.tenant).state(Node.State.active)) {
             if (node.parentHostname().isEmpty()) continue;
             if (node.allocation().get().owner().instance().isTester()) continue;
-            if (bestMove.node.allocation().get().owner().application().value().equals("lsbe-dictionaries")) continue; // TODO: Remove
+            if (node.allocation().get().owner().application().value().equals("lsbe-dictionaries")) continue; // TODO: Remove
             for (Node toHost : allNodes.nodeType(NodeType.host).state(NodePrioritizer.ALLOCATABLE_HOST_STATES)) {
                 if (toHost.hostname().equals(node.parentHostname().get())) continue;
                 if ( ! capacity.freeCapacityOf(toHost).satisfies(node.flavor().resources())) continue;
