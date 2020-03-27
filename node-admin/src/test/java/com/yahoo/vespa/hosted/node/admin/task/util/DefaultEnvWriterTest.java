97e0a1a1970a195685be8c97656e63c287fc1209
Håkon Hallingstad <hakon@verizonmedia.com>
2020-03-27 11:53:58 +0100

    Log when updating default-env.txt

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/task/util/DefaultEnvWriterTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/task/util/DefaultEnvWriterTest.java
@@ -1,6 +1,7 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.task.util;
 
+import com.yahoo.vespa.hosted.node.admin.component.TaskContext;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -9,11 +10,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author bjorncs
@@ -26,6 +32,8 @@ public class DefaultEnvWriterTest {
     private static final Path EXAMPLE_FILE = Paths.get("src/test/resources/default-env-example.txt");
     private static final Path EXPECTED_RESULT_FILE = Paths.get("src/test/resources/default-env-rewritten.txt");
 
+    private final TaskContext context = mock(TaskContext.class);
+
     @Test
     public void default_env_is_correctly_rewritten() throws IOException {
         Path tempFile = temporaryFolder.newFile().toPath();
@@ -36,14 +44,16 @@ public class DefaultEnvWriterTest {
         writer.addFallback("VESPA_CONFIGSERVER", "new-fallback-configserver");
         writer.addOverride("VESPA_TLS_CONFIG_FILE", "/override/path/to/config.file");
 
-        boolean modified = writer.updateFile(tempFile);
+        boolean modified = writer.updateFile(context, tempFile);
 
         assertTrue(modified);
         assertEquals(Files.readString(EXPECTED_RESULT_FILE), Files.readString(tempFile));
+        verify(context, times(1)).log(any(Logger.class), any(String.class));
 
-        modified = writer.updateFile(tempFile);
+        modified = writer.updateFile(context, tempFile);
         assertFalse(modified);
         assertEquals(Files.readString(EXPECTED_RESULT_FILE), Files.readString(tempFile));
+        verify(context, times(1)).log(any(Logger.class), any(String.class));
     }
 
     @Test
