441eb67040d88d99cfbc7043e4ba62533f5ca36a
Arne Juul <arnej@verizonmedia.com>
2020-04-20 08:26:58 +0000

    cosmetic fixes in unit test

--- vespalib/src/tests/stllike/replace_variable_test.cpp
+++ vespalib/src/tests/stllike/replace_variable_test.cpp
@@ -7,17 +7,23 @@ using namespace vespalib;
 
 TEST(ReplaceVariableTest, simple_usage)
 {
+    // replace one variable
     EXPECT_EQ("vv", replace_variable("x", "x", "vv"));
     EXPECT_EQ("f(vv)", replace_variable("f(x)", "x", "vv"));
+    EXPECT_EQ("f(vv)", replace_variable("f(myvariablename)", "myvariablename", "vv"));
     EXPECT_EQ("vv(f)", replace_variable("x(f)", "x", "vv"));
     EXPECT_EQ("3*vv", replace_variable("3*x", "x", "vv"));
-    EXPECT_EQ("f(vv,vv,y)", replace_variable("f(x,x,y)", "x", "vv"));
 
+    // replace variable multiple times
+    EXPECT_EQ("vv(vv,vv*vv)+vv", replace_variable("x(x,x*x)+x", "x", "vv"));
+
+    // do not replace variable when substring of a word
     EXPECT_EQ("f(xx)", replace_variable("f(xx)", "x", "vv"));
     EXPECT_EQ("f(ax)", replace_variable("f(ax)", "x", "vv"));
     EXPECT_EQ("f(xa)", replace_variable("f(xa)", "x", "vv"));
     EXPECT_EQ("f(axa)", replace_variable("f(axa)", "x", "vv"));
 
+    // variable names can contain underscore '_'
     EXPECT_EQ("f(vv)", replace_variable("f(x_y)", "x_y", "vv"));
 }
 
