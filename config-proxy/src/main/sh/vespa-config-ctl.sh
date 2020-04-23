293c968385780de950d417fe25f3b11ef04d3c65
Henning Baldersheim <balder@yahoo-inc.com>
2020-04-23 09:21:08 +0000

    Extend the timeout for config-proxy to start from 1m to 3m.

--- config-proxy/src/main/sh/vespa-config-ctl.sh
+++ config-proxy/src/main/sh/vespa-config-ctl.sh
@@ -130,7 +130,7 @@ case $1 in
 
         echo "Waiting for config proxy to start"
         fail=true
-        for ((sleepcount=0;$sleepcount<600;sleepcount=$sleepcount+1)) ; do
+        for ((sleepcount=0;$sleepcount<1800;sleepcount=$sleepcount+1)) ; do
             sleep 0.1
             if [ -f $P_CONFIG_PROXY ] && kill -0 `cat $P_CONFIG_PROXY` && vespa-ping-configproxy -s $hname 2>/dev/null
             then
