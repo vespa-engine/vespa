7ba5b90e295197f986a35870f6aff24ab3f4f3c3
Tor Egge <Tor.Egge@verizonmedia.com>
2020-04-26 22:15:34 +0200

    Use llvm 8 on RHEL 8.

--- default_build_settings.cmake
+++ default_build_settings.cmake
@@ -17,7 +17,7 @@ endfunction()
 
 function(setup_vespa_default_build_settings_rhel_8_1)
   message("-- Setting up default build settings for rhel 8.1")
-  set(DEFAULT_VESPA_LLVM_VERSION "7" PARENT_SCOPE)
+  set(DEFAULT_VESPA_LLVM_VERSION "8" PARENT_SCOPE)
 endfunction()
 
 function(setup_vespa_default_build_settings_centos_7)
