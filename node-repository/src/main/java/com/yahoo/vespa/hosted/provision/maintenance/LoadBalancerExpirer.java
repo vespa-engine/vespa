5b37a6937f8dc3f88255c289fd7c0e18f8a04579
Jon Bratseth <bratseth@gmail.com>
2020-04-30 23:22:26 +0200

    Pass node-repository to avoid construction cycle

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/LoadBalancerExpirer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/LoadBalancerExpirer.java
@@ -99,7 +99,7 @@ public class LoadBalancerExpirer extends NodeRepositoryMaintainer {
             // Remove any real no longer allocated to this application
             reals.removeIf(real -> !allocatedNodes.contains(real.hostname().value()));
             try {
-                service.create(lb.id().application(), lb.id().cluster(), reals, true);
+                service.create(lb.id().application(), lb.id().cluster(), reals, true, nodeRepository());
                 db.writeLoadBalancer(lb.with(lb.instance().withReals(reals)));
             } catch (Exception e) {
                 failed.add(lb.id());
