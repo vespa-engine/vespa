f40301f96ea7e783ee844de9b8a124324c212128
Harald Musum <musum@verizonmedia.com>
2020-05-03 11:34:56 +0200

    Add hashCode and equals

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/noderepository/NodeOwner.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/noderepository/NodeOwner.java
@@ -4,6 +4,8 @@ package com.yahoo.vespa.hosted.controller.api.integration.noderepository;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 /**
  * @author mpolden
  */
@@ -29,5 +31,20 @@ public class NodeOwner {
 
     public String getInstance() {
         return instance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NodeOwner nodeOwner = (NodeOwner) o;
+        return Objects.equals(tenant, nodeOwner.tenant) &&
+               Objects.equals(application, nodeOwner.application) &&
+               Objects.equals(instance, nodeOwner.instance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tenant, application, instance);
     }
 }
