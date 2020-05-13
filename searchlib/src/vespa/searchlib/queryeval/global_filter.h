433b4402498648f0f78c2eea8b38a46864420d39
Arne Juul <arnej@verizonmedia.com>
2020-05-13 12:08:44 +0000

    add class description

--- searchlib/src/vespa/searchlib/queryeval/global_filter.h
+++ searchlib/src/vespa/searchlib/queryeval/global_filter.h
@@ -7,6 +7,14 @@
 
 namespace search::queryeval {
 
+/**
+ * Hold ownership of a global filter that can be taken
+ * into account by adaptive query operators.  The owned
+ * bitvector should be a white-list (documents that may
+ * possibly become hits have their bit set, documents
+ * that are certain to be filtered away should have theirs
+ * cleared).
+ **/
 class GlobalFilter : public std::enable_shared_from_this<GlobalFilter>
 {
 private:
