323d5c78992f5cb2ec84a938fcd0e0ddb3705565
Martin Polden <mpolden@mpolden.no>
2020-05-19 15:45:32 +0200

    Stop supporting legacy serialization format

--- node-repository/src/test/java/com/yahoo/vespa/hosted/provision/persistence/OsVersionChangeSerializerTest.java
+++ node-repository/src/test/java/com/yahoo/vespa/hosted/provision/persistence/OsVersionChangeSerializerTest.java
@@ -7,7 +7,6 @@ import com.yahoo.vespa.hosted.provision.os.OsVersionChange;
 import com.yahoo.vespa.hosted.provision.os.OsVersionTarget;
 import org.junit.Test;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Map;
@@ -29,27 +28,6 @@ public class OsVersionChangeSerializerTest {
         ));
         var serialized = OsVersionChangeSerializer.fromJson(OsVersionChangeSerializer.toJson(change));
         assertEquals(serialized, change);
-    }
-
-    @Test
-    public void legacy_serialization() {
-        // Read old format
-        var change = new OsVersionChange(Map.of(
-                NodeType.host, new OsVersionTarget(NodeType.host, Version.fromString("1.2.3"), Optional.empty(), Optional.empty()),
-                NodeType.proxyhost, new OsVersionTarget(NodeType.proxyhost, Version.fromString("4.5.6"), Optional.empty(), Optional.empty()),
-                NodeType.confighost, new OsVersionTarget(NodeType.confighost, Version.fromString("7.8.9"), Optional.empty(), Optional.empty())
-        ));
-        var legacyFormat = "{\"host\":{\"version\":\"1.2.3\"},\"proxyhost\":{\"version\":\"4.5.6\"},\"confighost\":{\"version\":\"7.8.9\"}}";
-        assertEquals(change, OsVersionChangeSerializer.fromJson(legacyFormat.getBytes(StandardCharsets.UTF_8)));
-
-        // Write format supported by both old and new serializer
-        var oldFormat = "{\"targets\":[{\"nodeType\":\"host\",\"version\":\"1.2.3\"}," +
-                        "{\"nodeType\":\"proxyhost\",\"version\":\"4.5.6\"}," +
-                        "{\"nodeType\":\"confighost\",\"version\":\"7.8.9\"}]," +
-                        "\"host\":{\"version\":\"1.2.3\"}," +
-                        "\"proxyhost\":{\"version\":\"4.5.6\"}," +
-                        "\"confighost\":{\"version\":\"7.8.9\"}}";
-        assertEquals(oldFormat, new String(OsVersionChangeSerializer.toJson(change), StandardCharsets.UTF_8));
     }
 
 }
