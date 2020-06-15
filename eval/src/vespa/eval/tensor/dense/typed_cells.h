68291c2297a684f447f743305e1d1fc8d39f72d3
Arne Juul <arnej@verizonmedia.com>
2020-06-15 07:28:46 +0000

    GC old way of selecting functions by cell type

--- eval/src/vespa/eval/tensor/dense/typed_cells.h
+++ eval/src/vespa/eval/tensor/dense/typed_cells.h
@@ -48,15 +48,6 @@ struct TypedCells {
 };
 
 template <typename TGT, typename... Args>
-decltype(auto) dispatch_0(CellType ct, Args &&...args) {
-    switch (ct) {
-        case CellType::DOUBLE: return TGT::template call<double>(std::forward<Args>(args)...);
-        case CellType::FLOAT:  return TGT::template call<float>(std::forward<Args>(args)...);
-    }
-    abort();
-}
-
-template <typename TGT, typename... Args>
 decltype(auto) dispatch_1(const TypedCells &a, Args &&...args) {
     switch (a.type) {
         case CellType::DOUBLE: return TGT::call(a.unsafe_typify<double>(), std::forward<Args>(args)...);
@@ -70,24 +61,6 @@ decltype(auto) dispatch_2(A1 &&a, const TypedCells &b, Args &&...args) {
     switch (b.type) {
         case CellType::DOUBLE: return dispatch_1<TGT>(std::forward<A1>(a), b.unsafe_typify<double>(), std::forward<Args>(args)...);
         case CellType::FLOAT:  return dispatch_1<TGT>(std::forward<A1>(a), b.unsafe_typify<float>(),  std::forward<Args>(args)...);
-    }
-    abort();
-}
-
-template <typename T, typename... Args>
-decltype(auto) select_1(CellType a_type) {
-    switch(a_type) {
-    case CellType::DOUBLE: return T::template get_fun<double, Args...>();
-    case CellType::FLOAT:  return T::template get_fun<float, Args...>();
-    }
-    abort();
-}
-
-template <typename T>
-decltype(auto) select_2(CellType a_type, CellType b_type) {
-    switch(b_type) {
-    case CellType::DOUBLE: return select_1<T, double>(a_type);
-    case CellType::FLOAT:  return select_1<T, float>(a_type);
     }
     abort();
 }
