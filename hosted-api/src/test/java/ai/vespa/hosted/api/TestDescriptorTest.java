2a9fef2d14bb4735ad59244f1af8a7b6971fd8f8
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2020-06-22 16:58:28 +0200

    Move unit test to same location as class being tested

--- hosted-api/src/test/java/ai/vespa/hosted/api/TestDescriptorTest.java
+++ hosted-api/src/test/java/ai/vespa/hosted/api/TestDescriptorTest.java
@@ -1,6 +1,6 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package ai.vespa.hosted.api;
 
-import com.yahoo.vespa.testrunner.TestDescriptor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
