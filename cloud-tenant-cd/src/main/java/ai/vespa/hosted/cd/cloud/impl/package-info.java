b80c5c32aaac5b21194d85f1c98964ad0b5898eb
Morten Tokle <mortent@verizonmedia.com>
2020-07-02 00:36:24 +0200

    Export Test runtime implementation

---
+++ cloud-tenant-cd/src/main/java/ai/vespa/hosted/cd/cloud/impl/package-info.java
@@ -0,0 +1,9 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+/**
+ * @author mortent
+ */
+@ExportPackage
+package ai.vespa.hosted.cd.cloud.impl;
+
+import com.yahoo.osgi.annotation.ExportPackage;
