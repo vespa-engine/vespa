5b36667f9e7a5dc1825790226c80bd534f468b3f
Arne Juul <arnej@verizonmedia.com>
2020-07-08 06:20:30 +0000

    handle possible zcurve suffix

---
+++ searchcore/src/vespa/searchcore/proton/matching/resolveviewvisitor.cpp
@@ -0,0 +1,25 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+
+#include "resolveviewvisitor.h"
+#include <vespa/document/datatype/positiondatatype.h>
+#include <vespa/log/log.h>
+
+LOG_SETUP(".proton.matching.resolveviewvisitor");
+
+namespace proton::matching {
+
+void
+ResolveViewVisitor::visit(ProtonLocationTerm &n) {
+    n.resolve(_resolver, _indexEnv);
+    if (n.numFields() == 0) {
+        auto oldView = n.getView();
+        auto newView = document::PositionDataType::getZCurveFieldName(oldView);
+        n.setView(newView);
+        n.resolve(_resolver, _indexEnv);
+        LOG(info, "ProtonLocationTerm found %zu field after view change %s -> %s",
+            n.numFields(), oldView.c_str(), newView.c_str());
+    }
+}
+
+} // namespace
