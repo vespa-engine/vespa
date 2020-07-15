8e9dff29a3cadac8725453d5234970604560a715
Arne Juul <arnej@verizonmedia.com>
2020-07-15 13:54:57 +0000

    avoid negated flag

--- searchsummary/src/vespa/searchsummary/docsummary/getdocsumargs.cpp
+++ searchsummary/src/vespa/searchsummary/docsummary/getdocsumargs.cpp
@@ -8,7 +8,7 @@ GetDocsumArgs::GetDocsumArgs()
     : _ranking(),
       _resultClassName(),
       _dumpFeatures(false),
-      _no_locations(false),
+      _locations_possible(true),
       _stackItems(0),
       _stackDump(),
       _location(),
@@ -28,6 +28,7 @@ GetDocsumArgs::initFromDocsumRequest(const search::engine::DocsumRequest &req)
     _stackItems        = req.stackItems;
     _stackDump         = req.stackDump;
     _location          = req.location;
+    _locations_possible = true;
     _timeout           = req.getTimeLeft();
     _propertiesMap     = req.propertiesMap;
 }
