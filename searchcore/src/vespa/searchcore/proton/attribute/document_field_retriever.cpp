837df3c6a5ec87494a3d42799390d8a4a102a403
Henning Baldersheim <balder@yahoo-inc.com>
2020-08-13 10:20:39 +0200

    Revert "Revert "Revert "Revert "- Change APi for fetching documents to support optimal document retri…""""

--- searchcore/src/vespa/searchcore/proton/attribute/document_field_retriever.cpp
+++ searchcore/src/vespa/searchcore/proton/attribute/document_field_retriever.cpp
@@ -120,8 +120,7 @@ void
 DocumentFieldRetriever::populate(DocumentIdT lid,
                                  Document &doc,
                                  const document::Field & field,
-                                 const IAttributeVector &attr,
-                                 bool isIndexField)
+                                 const IAttributeVector &attr)
 {
     switch (attr.getBasicType()) {
     case BasicType::BOOL:
@@ -138,12 +137,6 @@ DocumentFieldRetriever::populate(DocumentIdT lid,
         setValue<double>(lid, doc, field, attr);
         break;
     case BasicType::STRING:
-        // If it is a stringfield we also need to check if
-        // it is an index field. In that case we shall
-        // keep the original in order to preserve annotations.
-        if (isIndexField) {
-            break;
-        }
         setValue<const char *>(lid, doc, field, attr);
         break;
     case BasicType::PREDICATE:
--- searchcore/src/vespa/searchcore/proton/server/documentretriever.h
+++ searchcore/src/vespa/searchcore/proton/server/documentretriever.h
@@ -3,20 +3,38 @@
 #pragma once
 
 #include "documentretrieverbase.h"
+#include <vespa/vespalib/stllike/hash_map.h>
 
 namespace search {
-struct IDocumentMetaStore;
-class IAttributeManager;
-class IDocumentStore;
-namespace index { class Schema; }
-}  // namespace search
+    struct IDocumentMetaStore;
+    class IAttributeManager;
+    class IDocumentStore;
+}
+namespace search::index { class Schema; }
 
 namespace proton {
 
-class DocumentRetriever : public DocumentRetrieverBase {
+struct IFieldInfo {
+    virtual ~IFieldInfo() = default;
+    virtual bool isFieldAttribute(const document::Field & field) const = 0;
+};
+
+class FieldSetAttributeDB {
+public:
+    FieldSetAttributeDB(const IFieldInfo & fieldInfo);
+    ~FieldSetAttributeDB();
+    bool areAllFieldsAttributes(uint64_t key, const document::Field::Set & set) const;
+private:
+    using FieldSetAttributeMap = vespalib::hash_map<uint64_t, bool>;
+    const IFieldInfo               & _fieldInfo;
+    mutable FieldSetAttributeMap     _isFieldSetAttributeOnly;
+    mutable std::mutex               _lock;
+};
+
+class DocumentRetriever : public DocumentRetrieverBase,
+                          public IFieldInfo {
 public:
     typedef std::vector<std::pair<const document::Field *, vespalib::string>> PositionFields;
-    using AttributeFields = std::vector<const document::Field *>;
     DocumentRetriever(const DocTypeName &docTypeName,
                       const document::DocumentTypeRepo &repo,
                       const search::index::Schema &schema,
@@ -25,15 +43,22 @@ public:
                       const search::IDocumentStore &doc_store);
     ~DocumentRetriever() override;
 
-    document::Document::UP getDocument(search::DocumentIdT lid) const override;
+    document::Document::UP getFullDocument(search::DocumentIdT lid) const override;
     void visitDocuments(const LidVector & lids, search::IDocumentVisitor & visitor, ReadConsistency) const override;
+    DocumentUP getPartialDocument(search::DocumentIdT lid, const document::DocumentId &, const document::FieldSet &) const override;
     void populate(search::DocumentIdT lid, document::Document & doc) const;
+    bool needFetchFromDocStore(const document::FieldSet &) const;
 private:
+    void populate(search::DocumentIdT lid, document::Document & doc, const document::Field::Set & attributeFields) const;
+
+    bool isFieldAttribute(const document::Field & field) const override;
     const search::index::Schema     &_schema;
     const search::IAttributeManager &_attr_manager;
     const search::IDocumentStore    &_doc_store;
     PositionFields                   _possiblePositionFields;
-    AttributeFields                  _attributeFields;
+    document::Field::Set             _attributeFields;
+    bool                             _areAllFieldsAttributes;
+    FieldSetAttributeDB              _fieldSetAttributeInfo;
 
     const search::IAttributeManager * getAttrMgr() const override;
 };
