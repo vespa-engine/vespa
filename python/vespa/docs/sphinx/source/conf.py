4e28ab3d8242a5294bc6bffacc0478b0657f8d7b
tmartins <thigm85@gmail.com>
2020-08-26 19:21:36 +0200

    set sphinx master doc to index

--- python/vespa/docs/sphinx/source/conf.py
+++ python/vespa/docs/sphinx/source/conf.py
@@ -40,6 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
