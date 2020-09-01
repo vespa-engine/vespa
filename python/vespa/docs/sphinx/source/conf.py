5b5e81ada288d8523983c1bfe1448f5843dd67a9
tmartins <thigm85@gmail.com>
2020-09-01 21:33:45 +0200

    try readthedocs theme

--- python/vespa/docs/sphinx/source/conf.py
+++ python/vespa/docs/sphinx/source/conf.py
@@ -28,7 +28,7 @@ author = "Vespa team"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "nbsphinx"]
+extensions = ["sphinx.ext.autodoc", "nbsphinx", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -45,7 +45,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
