9e4866649050b1b70ceee00f73db622a235df348
Geir Storli <geirst@verizonmedia.com>
2020-09-11 11:34:44 +0000

    Decouple CachingRpcTargetResolver from slobrok and fnet implementations and write unit tests.

---
+++ storage/src/tests/storageserver/rpc/caching_rpc_target_resolver_test.cpp
@@ -0,0 +1,134 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include <vespa/slobrok/imirrorapi.h>
+#include <vespa/storage/storageserver/rpc/caching_rpc_target_resolver.h>
+#include <vespa/storageapi/messageapi/storagemessage.h>
+#include <vespa/vdslib/state/nodetype.h>
+#include <vespa/vespalib/gtest/gtest.h>
+
+using namespace storage::rpc;
+using slobrok::api::IMirrorAPI;
+using storage::api::StorageMessageAddress;
+using storage::lib::NodeType;
+
+class MockMirror : public IMirrorAPI {
+public:
+    using Mappings = std::map<vespalib::string, IMirrorAPI::SpecList>;
+    Mappings mappings;
+    uint32_t gen;
+    MockMirror() : mappings(), gen(1) {}
+    SpecList lookup(const std::string& pattern) const override {
+        auto itr = mappings.find(pattern);
+        if (itr != mappings.end()) {
+            return itr->second;
+        }
+        return {};
+    }
+    uint32_t updates() const override { return gen; }
+    bool ready() const override { return true; }
+    void inc_gen() { ++gen; }
+};
+
+class MockWrappedFrtTarget : public WrappedFrtTarget {
+private:
+    bool& _valid;
+public:
+    MockWrappedFrtTarget(bool& valid) : _valid(valid) {}
+    FRT_Target* get() override { return nullptr; }
+    bool is_valid() const override { return _valid; }
+};
+
+class MockTargetFactory : public RpcTargetFactory {
+public:
+    mutable bool valid_target;
+
+    MockTargetFactory() : valid_target(true) {}
+    std::unique_ptr<RpcTarget> make_target(const vespalib::string& connection_spec, uint32_t slobrok_gen) const override {
+        return std::make_unique<RpcTarget>(std::make_unique<MockWrappedFrtTarget>(valid_target),
+                connection_spec, slobrok_gen);
+    }
+};
+
+class CachingRpcTargetResolverTest : public ::testing::Test {
+public:
+    MockMirror mirror;
+    MockTargetFactory factory;
+    CachingRpcTargetResolver resolver;
+    StorageMessageAddress address_0;
+    StorageMessageAddress address_1;
+    vespalib::string spec_0;
+    vespalib::string spec_1;
+
+    CachingRpcTargetResolverTest()
+        : mirror(),
+          factory(),
+          resolver(mirror, factory),
+          address_0("my_cluster", NodeType::STORAGE, 5),
+          address_1("my_cluster", NodeType::DISTRIBUTOR, 7),
+          spec_0("tcp/my:41"),
+          spec_1("tcp/my:42")
+    {
+        add_mapping(address_0, spec_0);
+    }
+    void add_mapping(const StorageMessageAddress& address, const vespalib::string& connection_spec) {
+        mirror.mappings[to_slobrok_id(address)] = {{to_slobrok_id(address), connection_spec}};
+    }
+    vespalib::string to_slobrok_id(const storage::api::StorageMessageAddress& address) const {
+        return CachingRpcTargetResolver::address_to_slobrok_id(address);
+    }
+};
+
+TEST_F(CachingRpcTargetResolverTest, converts_storage_message_address_to_slobrok_id)
+{
+    EXPECT_EQ("storage/cluster.my_cluster/storage/5", to_slobrok_id(address_0));
+    EXPECT_EQ("storage/cluster.my_cluster/distributor/7", to_slobrok_id(address_1));
+}
+
+TEST_F(CachingRpcTargetResolverTest, resolves_rpc_target_and_caches_result)
+{
+    auto target_a = resolver.resolve_rpc_target(address_0);
+    ASSERT_TRUE(target_a);
+    EXPECT_EQ(spec_0, target_a->_spec);
+    EXPECT_EQ(1, target_a->_slobrok_gen);
+    auto target_b = resolver.resolve_rpc_target(address_0);
+    ASSERT_TRUE(target_b);
+    EXPECT_EQ(target_a.get(), target_b.get());
+    EXPECT_EQ(spec_0, target_b->_spec);
+    EXPECT_EQ(1, target_b->_slobrok_gen);
+}
+
+TEST_F(CachingRpcTargetResolverTest, cached_rpc_target_is_updated_when_slobrok_generation_changes)
+{
+    auto target_a = resolver.resolve_rpc_target(address_0);
+    mirror.inc_gen();
+    auto target_b = resolver.resolve_rpc_target(address_0);
+    EXPECT_EQ(target_a.get(), target_b.get());
+    EXPECT_EQ(2, target_b->_slobrok_gen);
+}
+
+TEST_F(CachingRpcTargetResolverTest, new_rpc_target_is_created_if_connection_spec_changes)
+{
+    auto target_a = resolver.resolve_rpc_target(address_0);
+    add_mapping(address_0, spec_1);
+    mirror.inc_gen();
+    auto target_b = resolver.resolve_rpc_target(address_0);
+    EXPECT_NE(target_a.get(), target_b.get());
+    EXPECT_EQ(spec_1, target_b->_spec);
+    EXPECT_EQ(2, target_b->_slobrok_gen);
+}
+
+TEST_F(CachingRpcTargetResolverTest, new_rpc_target_is_created_if_raw_target_is_invalid)
+{
+    auto target_a = resolver.resolve_rpc_target(address_0);
+    factory.valid_target = false;
+    auto target_b = resolver.resolve_rpc_target(address_0);
+    EXPECT_NE(target_a.get(), target_b.get());
+    EXPECT_EQ(spec_0, target_b->_spec);
+    EXPECT_EQ(1, target_b->_slobrok_gen);
+}
+
+TEST_F(CachingRpcTargetResolverTest, null_rpc_target_is_returned_if_slobrok_id_is_not_found)
+{
+    auto target = resolver.resolve_rpc_target(address_1);
+    EXPECT_FALSE(target);
+}
--- storage/src/vespa/storage/storageserver/rpc/caching_rpc_target_resolver.cpp
+++ storage/src/vespa/storage/storageserver/rpc/caching_rpc_target_resolver.cpp
@@ -1,28 +1,30 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #include "caching_rpc_target_resolver.h"
 #include "shared_rpc_resources.h"
-#include <vespa/fnet/frt/supervisor.h>
 #include <vespa/fnet/frt/target.h>
-#include <vespa/slobrok/sbmirror.h>
+#include <vespa/slobrok/imirrorapi.h>
 #include <vespa/storageapi/messageapi/storagemessage.h>
 #include <vespa/vespalib/stllike/asciistream.h>
 #include <vespa/vespalib/stllike/hash_map.hpp>
+#include <cassert>
 
 #include <vespa/log/log.h>
 LOG_SETUP(".storage.caching_rpc_target_resolver");
 
 namespace storage::rpc {
 
-CachingRpcTargetResolver::CachingRpcTargetResolver(SharedRpcResources& rpc_resources)
-    : _rpc_resources(rpc_resources)
+CachingRpcTargetResolver::CachingRpcTargetResolver(const slobrok::api::IMirrorAPI& slobrok_mirror,
+                                                   const RpcTargetFactory& target_factory)
+    : _slobrok_mirror(slobrok_mirror),
+      _target_factory(target_factory),
+      _targets_rwmutex()
 {
 }
 
 CachingRpcTargetResolver::~CachingRpcTargetResolver() = default;
 
-namespace {
-
-vespalib::string address_to_slobrok_id(const api::StorageMessageAddress& address) {
+vespalib::string
+CachingRpcTargetResolver::address_to_slobrok_id(const api::StorageMessageAddress& address) {
     vespalib::asciistream as;
     as << "storage/cluster." << address.getCluster()
        << '/' << ((address.getNodeType() == lib::NodeType::STORAGE) ? "storage" : "distributor")
@@ -30,53 +32,76 @@ vespalib::string address_to_slobrok_id(const api::StorageMessageAddress& address
     return as.str();
 }
 
+std::shared_ptr<RpcTarget>
+CachingRpcTargetResolver::lookup_target(const vespalib::string& slobrok_id, uint32_t curr_slobrok_gen) {
+    std::shared_lock lock(_targets_rwmutex);
+    auto itr = _targets.find(slobrok_id);
+    if ((itr != _targets.end())
+        && itr->second->_target->is_valid()
+        && (itr->second->_slobrok_gen == curr_slobrok_gen)) {
+        return itr->second;
+    }
+    return {};
 }
 
-// TODO ensure this is robust and performant wrt. visitor clients constantly bumping
-// slobrok generations by registering new sessions all the time.
+std::shared_ptr<RpcTarget>
+CachingRpcTargetResolver::consider_update_target(const vespalib::string& slobrok_id,
+                                                 const vespalib::string& connection_spec,
+                                                 uint32_t curr_slobrok_gen,
+                                                 const UniqueLock& targets_lock) {
+    (void) targets_lock;
+    // If address has the same spec as the existing target, just reuse it.
+    auto itr = _targets.find(slobrok_id);
+    if ((itr != _targets.end())
+        && (itr->second->_target->is_valid())
+        && (itr->second->_spec == connection_spec))
+    {
+        LOG(info, "Updating existing mapping '%s' -> '%s' (gen %u) to gen %u",
+            slobrok_id.c_str(), connection_spec.c_str(), itr->second->_slobrok_gen, curr_slobrok_gen);
+        itr->second->_slobrok_gen = curr_slobrok_gen;
+        return itr->second;
+    }
+    return {};
+}
+
+std::shared_ptr<RpcTarget>
+CachingRpcTargetResolver::insert_new_target_mapping(const vespalib::string& slobrok_id,
+                                                    const vespalib::string& connection_spec,
+                                                    uint32_t curr_slobrok_gen,
+                                                    const UniqueLock& targets_lock) {
+    (void) targets_lock;
+    auto target = _target_factory.make_target(connection_spec, curr_slobrok_gen); // TODO expensive inside lock?
+    assert(target);
+    std::shared_ptr<RpcTarget> rpc_target(std::move(target));
+    _targets[slobrok_id] = rpc_target;
+    LOG(info, "Added mapping '%s' -> '%s' at gen %u", slobrok_id.c_str(), connection_spec.c_str(), curr_slobrok_gen);
+    return rpc_target;
+}
+
 std::shared_ptr<RpcTarget>
 CachingRpcTargetResolver::resolve_rpc_target(const api::StorageMessageAddress& address) {
     // TODO or map directly from address to target instead of going via stringification? Needs hashing, if so.
-    auto sb_id = address_to_slobrok_id(address);
-    const uint32_t current_sb_gen = _rpc_resources.slobrok_mirror().updates();
-    {
-        std::shared_lock lock(_targets_rwmutex);
-        auto target_iter = _targets.find(sb_id);
-        if ((target_iter != _targets.end())
-            && target_iter->second->_target->IsValid()
-            && (target_iter->second->_sb_generation == current_sb_gen))
-        {
-            return target_iter->second;
-        }
+    auto slobrok_id = address_to_slobrok_id(address);
+    const uint32_t curr_slobrok_gen = _slobrok_mirror.updates();
+    if (auto result = lookup_target(slobrok_id, curr_slobrok_gen)) {
+        return result;
     }
-    auto specs = _rpc_resources.slobrok_mirror().lookup(sb_id); // FIXME string type mismatch; implicit conv!
+    auto specs = _slobrok_mirror.lookup(slobrok_id); // FIXME string type mismatch; implicit conv!
     if (specs.empty()) {
-        LOG(info, "Found no mapping for %s", sb_id.c_str());
+        // TODO: Replace all info logging with debug logging.
+        LOG(info, "Found no mapping for '%s'", slobrok_id.c_str());
         // TODO return potentially stale existing target if no longer existing in SB?
         // TODO or clear any existing mapping?
         return {};
     }
-    const auto& candidate_spec = specs[0].second; // Always use first spec in list. TODO correct?
+    // Note: We don't use wildcards so there is a 1-to-1 mapping between service name / slobrok id and connection spec.
+    assert(specs.size() == 1);
+    const auto& connection_spec = specs[0].second;
     std::unique_lock lock(_targets_rwmutex);
-    // If address has the same spec as the existing target, just reuse it.
-    auto target_iter = _targets.find(sb_id);
-    if ((target_iter != _targets.end())
-        && (target_iter->second->_target->IsValid())
-        && (target_iter->second->_spec == candidate_spec))
-    {
-        LOG(info, "Updating existing mapping %s -> %s (gen %u) to gen %u",
-            sb_id.c_str(), candidate_spec.c_str(), target_iter->second->_sb_generation, current_sb_gen);
-        target_iter->second->_sb_generation = current_sb_gen;
-        return target_iter->second;
+    if (auto result = consider_update_target(slobrok_id, connection_spec, curr_slobrok_gen, lock)) {
+        return result;
     }
-    // Insert new mapping or update the old one.
-    auto* raw_target = _rpc_resources.supervisor().GetTarget(candidate_spec.c_str()); // TODO expensive inside lock?
-    assert(raw_target);
-    auto rpc_target = std::make_shared<RpcTarget>(raw_target, candidate_spec, current_sb_gen);
-    _targets[sb_id] = rpc_target;
-    LOG(info, "Added mapping %s -> %s at gen %u", sb_id.c_str(), candidate_spec.c_str(), current_sb_gen);
-    return rpc_target;
+    return insert_new_target_mapping(slobrok_id, connection_spec, curr_slobrok_gen, lock);
 }
-
 
 }
