cee32bc63ccea339eadaf9b92335d783c86fc500
Arne Juul <arnej@verizonmedia.com>
2020-09-24 06:55:50 +0000

    add new generic rename and test

---
+++ eval/src/tests/instruction/generic_rename/generic_rename_test.cpp
@@ -0,0 +1,128 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include <vespa/eval/eval/simple_value.h>
+#include <vespa/eval/eval/value_codec.h>
+#include <vespa/eval/instruction/generic_rename.h>
+#include <vespa/eval/eval/interpreted_function.h>
+#include <vespa/eval/eval/test/tensor_model.hpp>
+#include <vespa/vespalib/util/stringfmt.h>
+#include <vespa/vespalib/gtest/gtest.h>
+
+using namespace vespalib;
+using namespace vespalib::eval;
+using namespace vespalib::eval::test;
+
+using vespalib::make_string_short::fmt;
+
+std::vector<Layout> rename_layouts = {
+    {x(3)},
+    {x(3),y(5)},
+    {x(3),y(5),z(7)},
+    float_cells({x(3),y(5),z(7)}),
+    {x({"a","b","c"})},
+    {x({"a","b","c"}),y({"foo","bar"})},
+    {x({"a","b","c"}),y({"foo","bar"}),z({"i","j","k","l"})},
+    float_cells({x({"a","b","c"}),y({"foo","bar"}),z({"i","j","k","l"})}),
+    {x(3),y({"foo", "bar"}),z(7)},
+    {x({"a","b","c"}),y(5),z({"i","j","k","l"})},
+    float_cells({x({"a","b","c"}),y(5),z({"i","j","k","l"})})
+};
+
+struct FromTo {
+    std::vector<vespalib::string> from;
+    std::vector<vespalib::string> to;
+};
+
+std::vector<FromTo> rename_from_to = {
+    { {"x"}, {"x_renamed"} },
+    { {"x"}, {"z_was_x"} },
+    { {"x", "y"}, {"y", "x"} },
+    { {"x", "z"}, {"z", "x"} },
+    { {"x", "y", "z"}, {"a", "b", "c"} },
+    { {"z"}, {"a"} },
+    { {"y"}, {"z_was_y"} },
+    { {"y"}, {"b"} }
+};
+
+
+TEST(GenericRenameTest, dense_rename_plan_can_be_created_and_executed) {
+    auto lhs = ValueType::from_spec("tensor(a[2],c[3],d{},e[5],g[7],h{})");
+    std::vector<vespalib::string> from({"a", "c", "e"});
+    std::vector<vespalib::string>   to({"f", "a", "b"});
+    ValueType renamed = lhs.rename(from, to);
+    auto plan = instruction::DenseRenamePlan(lhs, renamed, from, to);
+    std::vector<size_t> expect_loop = {15,2,7};
+    std::vector<size_t> expect_stride = {7,105,1};
+    EXPECT_EQ(plan.subspace_size, 210);
+    EXPECT_EQ(plan.loop_cnt, expect_loop);
+    EXPECT_EQ(plan.stride, expect_stride);
+    std::vector<int> out;
+    int want[3][5][2][7];
+    size_t counter = 0;
+    for (size_t a = 0; a < 2; ++a) {
+        for (size_t c = 0; c < 3; ++c) {
+            for (size_t e = 0; e < 5; ++e) {
+                for (size_t g = 0; g < 7; ++g) {
+                    want[c][e][a][g] = counter++;
+                }
+            }
+        }
+    }
+    std::vector<int> expect(210);
+    memcpy(&expect[0], &want[0], 210*sizeof(int));
+    auto move_cell = [&](size_t offset) { out.push_back(offset); };
+    plan.execute(0, move_cell);
+    EXPECT_EQ(out, expect);
+}
+
+TEST(GenericRenameTest, sparse_rename_plan_can_be_created) {
+    auto lhs = ValueType::from_spec("tensor(a{},c{},d[3],e{},g{},h[5])");
+    std::vector<vespalib::string> from({"a", "c", "e"});
+    std::vector<vespalib::string>   to({"f", "a", "b"});
+    ValueType renamed = lhs.rename(from, to);
+    auto plan = instruction::SparseRenamePlan(lhs, renamed, from, to);
+    EXPECT_EQ(plan.mapped_dims, 4);
+    std::vector<size_t> expect = {2,0,1,3};
+    EXPECT_EQ(plan.output_dimensions, expect);
+}
+
+TensorSpec simple_tensor_rename(const TensorSpec &a, const FromTo &ft) {
+    Stash stash;
+    const auto &engine = SimpleTensorEngine::ref();
+    auto lhs = engine.from_spec(a);
+    const auto &result = engine.rename(*lhs, ft.from, ft.to, stash);
+    return engine.to_spec(result);
+}
+
+TensorSpec perform_generic_rename(const TensorSpec &a, const ValueType &res_type,
+                              const FromTo &ft, const ValueBuilderFactory &factory)
+{
+    Stash stash;
+    auto lhs = value_from_spec(a, factory);
+    auto my_op = instruction::GenericRename::make_instruction(lhs->type(), res_type, ft.from, ft.to, factory, stash);
+    InterpretedFunction::EvalSingle single(my_op);
+    return spec_from_value(single.eval(std::vector<Value::CREF>({*lhs})));
+}
+
+void test_generic_rename(const ValueBuilderFactory &factory) {
+    for (const auto & layout : rename_layouts) {
+        TensorSpec lhs = spec(layout, N());
+        ValueType lhs_type = ValueType::from_spec(lhs.type());
+        // printf("lhs_type: %s\n", lhs_type.to_spec().c_str());
+        for (const auto & from_to : rename_from_to) {
+            ValueType renamed_type = lhs_type.rename(from_to.from, from_to.to);
+            if (renamed_type.is_error()) continue;
+            // printf("type %s -> %s\n", lhs_type.to_spec().c_str(), renamed_type.to_spec().c_str());
+            SCOPED_TRACE(fmt("\n===\nLHS: %s\n===\n", lhs.to_string().c_str()));
+            auto expect = simple_tensor_rename(lhs, from_to);
+            auto actual = perform_generic_rename(lhs, renamed_type, from_to, factory);
+            EXPECT_EQ(actual, expect);
+        }
+    }
+}
+
+TEST(GenericRenameTest, generic_rename_works_for_simple_values) {
+    test_generic_rename(SimpleValueBuilderFactory::get());
+}
+
+GTEST_MAIN_RUN_ALL_TESTS()
---
+++ eval/src/vespa/eval/instruction/generic_rename.cpp
@@ -0,0 +1,197 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include "generic_rename.h"
+#include <vespa/eval/eval/value.h>
+#include <vespa/vespalib/util/stash.h>
+#include <vespa/vespalib/util/typify.h>
+#include <cassert>
+
+namespace vespalib::eval::instruction {
+
+using State = InterpretedFunction::State;
+using Instruction = InterpretedFunction::Instruction;
+
+namespace {
+
+template <typename T, typename IN> uint64_t wrap_param(const IN &value_in) {
+    const T &value = value_in;
+    static_assert(sizeof(uint64_t) == sizeof(&value));
+    return (uint64_t)&value;
+}
+
+template <typename T> const T &unwrap_param(uint64_t param) {
+    return *((const T *)param);
+}
+
+const vespalib::string &
+find_rename(const vespalib::string & original,
+            const std::vector<vespalib::string> &from,
+            const std::vector<vespalib::string> &to)
+{
+    for (size_t i = 0; i < from.size(); ++i) {
+        if (original == from[i]) {
+            return to[i];
+        }
+    }
+    return original;
+}
+
+size_t
+find_index_of(const vespalib::string & name,
+              const std::vector<ValueType::Dimension> & dims)
+{
+    for (size_t i = 0; i < dims.size(); ++i) {
+        if (name == dims[i].name) {
+            return i;
+        }
+    }
+    abort(); // should not happen
+}
+
+struct RenameParam {
+    ValueType res_type;
+    SparseRenamePlan sparse_plan;
+    DenseRenamePlan dense_plan;
+    const ValueBuilderFactory &factory;
+    RenameParam(const ValueType &lhs_type, const ValueType &output_type,
+                const std::vector<vespalib::string> &rename_dimension_from,
+                const std::vector<vespalib::string> &rename_dimension_to,
+                const ValueBuilderFactory &factory_in)
+        : res_type(output_type),
+          sparse_plan(lhs_type, output_type, rename_dimension_from, rename_dimension_to),
+          dense_plan(lhs_type, output_type, rename_dimension_from, rename_dimension_to),
+          factory(factory_in)
+    {
+        assert(!res_type.is_error());
+        assert(lhs_type.cell_type() == res_type.cell_type());
+    }
+    ~RenameParam();
+};
+RenameParam::~RenameParam() = default;
+
+template <typename CT>
+std::unique_ptr<Value>
+generic_rename(const Value &a,
+               const SparseRenamePlan &sparse_plan, const DenseRenamePlan &dense_plan,
+               const ValueType &res_type, const ValueBuilderFactory &factory)
+{
+    auto cells = a.cells().typify<CT>();
+    std::vector<vespalib::stringref> output_address(sparse_plan.mapped_dims);
+    std::vector<vespalib::stringref*> input_address;
+    for (size_t maps_to : sparse_plan.output_dimensions) {
+        input_address.push_back(&output_address[maps_to]);
+    }
+    auto builder = factory.create_value_builder<CT>(res_type,
+                                                    sparse_plan.mapped_dims,
+                                                    dense_plan.subspace_size,
+                                                    a.index().size());
+    auto view = a.index().create_view({});
+    view->lookup({});
+    size_t subspace;
+    while (view->next_result(input_address, subspace)) {
+        CT *dst = builder->add_subspace(output_address).begin();
+        size_t input_offset = dense_plan.subspace_size * subspace;
+        auto copy_cells = [&](size_t input_idx) { *dst++ = cells[input_idx]; };
+        dense_plan.execute(input_offset, copy_cells);
+    }
+    return builder->build(std::move(builder));
+}
+
+template <typename CT>
+void my_generic_rename_op(State &state, uint64_t param_in) {
+    const auto &param = unwrap_param<RenameParam>(param_in);
+    const Value &a = state.peek(0);
+    auto res_value = generic_rename<CT>(a, param.sparse_plan, param.dense_plan,
+                                        param.res_type, param.factory);
+    auto &result = state.stash.create<std::unique_ptr<Value>>(std::move(res_value));
+    const Value &result_ref = *(result.get());
+    state.pop_push(result_ref);
+}
+
+struct SelectGenericRenameOp {
+    template <typename CT> static auto invoke() {
+        return my_generic_rename_op<CT>;
+    }
+};
+
+} // namespace <unnamed>
+
+//-----------------------------------------------------------------------------
+ 
+SparseRenamePlan::SparseRenamePlan(const ValueType &input_type,
+                                   const ValueType &output_type,
+                                   const std::vector<vespalib::string> &from,
+                                   const std::vector<vespalib::string> &to)
+    : output_dimensions()
+{
+    const auto in_dims = input_type.mapped_dimensions();
+    const auto out_dims = output_type.mapped_dimensions();
+    mapped_dims = in_dims.size();
+    assert(mapped_dims == out_dims.size());
+    for (const auto & dim : in_dims) {
+        const auto & renamed_to = find_rename(dim.name, from, to);
+        size_t index = find_index_of(renamed_to, out_dims);
+        assert(index < mapped_dims);
+        output_dimensions.push_back(index);
+    }
+    assert(output_dimensions.size() == mapped_dims);
+}
+
+SparseRenamePlan::~SparseRenamePlan() = default;
+
+DenseRenamePlan::DenseRenamePlan(const ValueType &lhs_type,
+                                 const ValueType &output_type,
+                                 const std::vector<vespalib::string> &from,
+                                 const std::vector<vespalib::string> &to)
+    : loop_cnt(),
+      stride(),
+      subspace_size(output_type.dense_subspace_size())
+{
+    assert (subspace_size == lhs_type.dense_subspace_size());
+    const auto lhs_dims = lhs_type.nontrivial_indexed_dimensions();
+    const auto out_dims = output_type.nontrivial_indexed_dimensions();
+    size_t num_dense_dims = lhs_dims.size();
+    assert(num_dense_dims == out_dims.size());
+    std::vector<size_t> lhs_loopcnt(num_dense_dims);
+    std::vector<size_t> lhs_stride(num_dense_dims, 1);
+    size_t lhs_size = 1;
+    for (size_t i = num_dense_dims; i-- > 0; ) {
+        lhs_stride[i] = lhs_size;
+        lhs_loopcnt[i] = lhs_dims[i].size;
+        lhs_size *= lhs_loopcnt[i];
+    }
+    assert(lhs_size == subspace_size);
+    size_t prev_index = num_dense_dims;
+    for (const auto & dim : out_dims) {
+        const auto & renamed_from = find_rename(dim.name, to, from);
+        size_t index = find_index_of(renamed_from, lhs_dims);
+        assert(index < num_dense_dims);
+        if (prev_index + 1 == index) {
+            assert(stride.back() == lhs_stride[index] * lhs_loopcnt[index]);
+            loop_cnt.back() *= lhs_loopcnt[index];
+            stride.back() = lhs_stride[index];
+        } else {
+            loop_cnt.push_back(lhs_loopcnt[index]);
+            stride.push_back(lhs_stride[index]);
+        }
+        prev_index = index;
+    }
+}
+
+DenseRenamePlan::~DenseRenamePlan() = default;
+
+InterpretedFunction::Instruction
+GenericRename::make_instruction(const ValueType &lhs_type, const ValueType &output_type,
+                                const std::vector<vespalib::string> &rename_dimension_from,
+                                const std::vector<vespalib::string> &rename_dimension_to,
+                                const ValueBuilderFactory &factory, Stash &stash)
+{
+    auto &param = stash.create<RenameParam>(lhs_type, output_type,
+                                            rename_dimension_from, rename_dimension_to,
+                                            factory);
+    auto fun = typify_invoke<1,TypifyCellType,SelectGenericRenameOp>(output_type.cell_type());
+    return Instruction(fun, wrap_param<RenameParam>(param));
+}
+
+} // namespace
+
