432dffc30c93e2de1fd2ac037ca1ed1b00a91da3
Valerij Fredriksen <valerij92@gmail.com>
2020-09-24 19:58:26 +0200

    Log internal restarts

--- configserver/src/main/java/com/yahoo/vespa/config/server/ApplicationRepository.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/ApplicationRepository.java
@@ -322,12 +322,16 @@ public class ApplicationRepository implements com.yahoo.config.provision.Deploye
         activate(tenant, sessionId, prepareParams.getTimeoutBudget(), prepareParams.force());
 
         if (prepareParams.internalRestart() && !result.configChangeActions().getRestartActions().isEmpty()) {
+            result.deployLogger().log(Level.INFO, result.configChangeActions().getRestartActions().format());
             Set<String> hostnames = result.configChangeActions().getRestartActions().getEntries().stream()
                     .flatMap(entry -> entry.getServices().stream())
                     .map(ServiceInfo::getHostName)
                     .collect(Collectors.toUnmodifiableSet());
 
             hostProvisioner.get().restart(applicationId, HostFilter.from(hostnames, Set.of(), Set.of(), Set.of()));
+            result.deployLogger().log(Level.INFO, String.format("Scheduled service restart of %d nodes: %s",
+                    hostnames.size(), hostnames.stream().sorted().collect(Collectors.joining(", "))));
+
             ConfigChangeActions newActions = new ConfigChangeActions(new RestartActions(), result.configChangeActions().getRefeedActions());
             return new PrepareResult(result.sessionId(), newActions, result.deployLogger());
         }
