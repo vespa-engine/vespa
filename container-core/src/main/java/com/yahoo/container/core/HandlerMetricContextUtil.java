11405e52f2853e44df0944bf7bbee13dc2e617a5
Jon Marius Venstad <venstad@gmail.com>
2020-09-29 08:39:35 +0200

    Revert "Revert "Jonmv/async doc v1 implementation""
    This reverts commit c6aded1606112a54969f56403085ca90d61dac8f.

---
+++ container-core/src/main/java/com/yahoo/container/core/HandlerMetricContextUtil.java
@@ -0,0 +1,54 @@
+package com.yahoo.container.core;
+
+import com.yahoo.jdisc.Metric;
+import com.yahoo.jdisc.Request;
+import com.yahoo.jdisc.application.BindingMatch;
+import com.yahoo.jdisc.application.UriPattern;
+import com.yahoo.jdisc.handler.ResponseHandler;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Common HTTP request handler metrics code.
+ *
+ * @author jonmv
+ */
+public class HandlerMetricContextUtil {
+
+    public static void onHandle(Request request, Metric metric, Class<?> handlerClass) {
+        metric.add("handled.requests", 1, contextFor(request, metric, handlerClass));
+    }
+
+    public static void onHandled(Request request, Metric metric, Class<?> handlerClass) {
+        metric.set("handled.latency", request.timeElapsed(TimeUnit.MILLISECONDS), contextFor(request, metric, handlerClass));
+    }
+
+    public static Metric.Context contextFor(Request request, Metric metric, Class<?> handlerClass) {
+        return contextFor(request, Map.of(), metric, handlerClass);
+    }
+
+    public static Metric.Context contextFor(Request request, Map<String, String> extraDimensions, Metric metric, Class<?> handlerClass) {
+        BindingMatch<?> match = request.getBindingMatch();
+        if (match == null) return null;
+        UriPattern matched = match.matched();
+        if (matched == null) return null;
+        String name = matched.toString();
+        String endpoint = request.headers().containsKey("Host") ? request.headers().get("Host").get(0) : null;
+
+        Map<String, String> dimensions = new HashMap<>();
+        dimensions.put("handler", name);
+        if (endpoint != null) {
+            dimensions.put("endpoint", endpoint);
+        }
+        URI uri = request.getUri();
+        dimensions.put("scheme", uri.getScheme());
+        dimensions.put("port", Integer.toString(uri.getPort()));
+        dimensions.put("handler-name", handlerClass.getName());
+        dimensions.putAll(extraDimensions);
+        return metric.createContext(dimensions);
+    }
+
+}
