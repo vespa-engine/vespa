dcc0cface9773937c2e08efd8d297ec3076ec8fe
Tor Egge <Tor.Egge@broadpark.no>
2020-10-04 18:57:35 +0200

    Explicitly instantiate NumericFieldValue.

--- document/src/vespa/document/fieldvalue/numericfieldvalue.cpp
+++ document/src/vespa/document/fieldvalue/numericfieldvalue.cpp
@@ -1,6 +1,7 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "numericfieldvalue.h"
+#include "numericfieldvalue.hpp"
 #include <vespa/vespalib/util/xmlstream.h>
 
 namespace document {
@@ -13,5 +14,11 @@ NumericFieldValueBase::printXml(XmlOutputStream& out) const
     out << vespalib::xml::XmlContent(getAsString());
 }
 
+template class NumericFieldValue<float>;
+template class NumericFieldValue<double>;
+template class NumericFieldValue<int8_t>;
+template class NumericFieldValue<int16_t>;
+template class NumericFieldValue<int32_t>;
+template class NumericFieldValue<int64_t>;
 
 } // document
