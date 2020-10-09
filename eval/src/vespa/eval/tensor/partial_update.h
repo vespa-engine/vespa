5d182e182abe3c3eb380218c219b80fc0323bc7c
Arne Juul <arnej@verizonmedia.com>
2020-10-09 11:58:54 +0000

    fix formatting of comments

--- eval/src/vespa/eval/tensor/partial_update.h
+++ eval/src/vespa/eval/tensor/partial_update.h
@@ -11,25 +11,31 @@ struct TensorPartialUpdate {
     using Value = vespalib::eval::Value;
     using ValueBuilderFactory = vespalib::eval::ValueBuilderFactory;
 
-    // make a copy of the input, but apply function(oldvalue, modifier.cellvalue)
-    // to cells which also exist in the "modifier".
-    // modifier.type() must be sparse with exactly the same dimension names
-    // as the input type.
-    // returns null pointer if this constraint is violated.
+    /**
+     *  Make a copy of the input, but apply function(oldvalue, modifier.cellvalue)
+     *  to cells which also exist in the "modifier".
+     *  The modifier type must be sparse with exactly the same dimension names
+     *  as the input type.
+     *  Returns null pointer if this constraint is violated.
+     **/
     static Value::UP modify(const Value &input, join_fun_t function,
                             const Value &modifier, const ValueBuilderFactory &factory);
 
-    // make a copy of the input, but add or overwrite cells from add_cells.
-    // requires same type for input and add_cells.
-    // returns null pointer if this constraint is violated.
+    /**
+     *  Make a copy of the input, but add or overwrite cells from add_cells.
+     *  Requires same type for input and add_cells.
+     *  Returns null pointer if this constraint is violated.
+     **/
     static Value::UP add(const Value &input, const Value &add_cells, const ValueBuilderFactory &factory);
 
-    // make a copy of the input, but remove cells present in remove_spec.
-    // remove_spec must be a sparse tensor, with exactly the mapped dimensions
-    // that the input value has.
-    // cell values in remove_spec are ignored.
-    // not valid for dense tensors, since removing cells for those are impossible.
-    // returns null pointer if these constraints are violated.
+    /**
+     *  Make a copy of the input, but remove cells present in remove_spec.
+     *  The remove_spec must be a sparse tensor, with exactly the mapped dimensions
+     *  that the input value has.
+     *  Cell values in remove_spec are ignored.
+     *  Not valid for dense tensors, since removing cells for those are impossible.
+     *  Returns null pointer if these constraints are violated.
+     **/
     static Value::UP remove(const Value &input, const Value &remove_spec, const ValueBuilderFactory &factory);
 };
 
