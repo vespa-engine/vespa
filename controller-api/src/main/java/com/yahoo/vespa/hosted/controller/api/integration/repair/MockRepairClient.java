a098c83474d4368252726a9c5f13b591e6b841d2
Ola Aunrønning <olaa@verizonmedia.com>
2020-10-25 21:29:32 +0100

    Add ticket creation functionality to HostRepairClient

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/repair/MockRepairClient.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/repair/MockRepairClient.java
@@ -1,7 +1,9 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.api.integration.repair;
 
+import com.yahoo.config.provision.HostName;
 import com.yahoo.config.provision.zone.ZoneApi;
+import com.yahoo.config.provision.zone.ZoneId;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.Node;
 
 import java.util.ArrayList;
@@ -18,6 +20,11 @@ public class MockRepairClient implements HostRepairClient {
     @Override
     public void updateRepairStatus(ZoneApi zone, Map<Node, RepairTicketReport> nodes) {
         updatedNodes.addAll(nodes.keySet());
+    }
+
+    @Override
+    public String createTicket(HostName hostname, String colo, ZoneId zoneId, String description, String category) {
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     public List<Node> getUpdatedNodes() {
