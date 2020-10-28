fbd8ca020a6d97882c554585d911889d1b9f69ea
Lester Solbakken <lesters@oath.com>
2020-10-28 08:46:32 +0100

    Reapply resolving onnx model types
    This reverts commit de5e15528fca545b2a9ccbb1386c5590e11fa383.

--- config-model/src/main/java/com/yahoo/searchdefinition/OnnxModel.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/OnnxModel.java
@@ -2,14 +2,22 @@
 package com.yahoo.searchdefinition;
 
 import com.yahoo.config.FileReference;
+import com.yahoo.path.Path;
+import com.yahoo.searchlib.rankingexpression.ExpressionFunction;
+import com.yahoo.searchlib.rankingexpression.Reference;
+import com.yahoo.tensor.TensorType;
 import com.yahoo.vespa.model.AbstractService;
 import com.yahoo.vespa.model.utils.FileSender;
+import onnx.Onnx;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * A global ONNX model distributed using file distribution, similar to ranking constants.
@@ -21,16 +29,16 @@ public class OnnxModel {
     public enum PathType {FILE, URI};
 
     private final String name;
+    private PathType pathType = PathType.FILE;
     private String path = null;
     private String fileReference = "";
-    private List<OnnxNameMapping> inputMap = new ArrayList<>();
-    private List<OnnxNameMapping> outputMap = new ArrayList<>();
+    private String defaultOutput = null;
+    private Map<String, String> inputMap = new HashMap<>();
+    private Map<String, String> outputMap = new HashMap<>();
 
-    public PathType getPathType() {
-        return pathType;
-    }
-
-    private PathType pathType = PathType.FILE;
+    private Map<String, Onnx.TypeProto> inputTypes = new HashMap<>();
+    private Map<String, Onnx.TypeProto> outputTypes = new HashMap<>();
+    private Map<String, TensorType>     vespaTypes = new HashMap<>();
 
     public OnnxModel(String name) {
         this.name = name;
@@ -49,21 +57,52 @@ public class OnnxModel {
     }
 
     public void setUri(String uri) {
-        Objects.requireNonNull(uri, "uri cannot be null");
-        this.path = uri;
-        this.pathType = PathType.URI;
+        throw new IllegalArgumentException("URI for ONNX models are not currently supported");
+    }
+
+    public PathType getPathType() {
+        return pathType;
+    }
+
+    public void setDefaultOutput(String onnxName) {
+        Objects.requireNonNull(onnxName, "Name cannot be null");
+        this.defaultOutput = onnxName;
     }
 
     public void addInputNameMapping(String onnxName, String vespaName) {
+        addInputNameMapping(onnxName, vespaName, true);
+    }
+
+    public void addInputNameMapping(String onnxName, String vespaName, boolean overwrite) {
         Objects.requireNonNull(onnxName, "Onnx name cannot be null");
         Objects.requireNonNull(vespaName, "Vespa name cannot be null");
-        this.inputMap.add(new OnnxNameMapping(onnxName, vespaName));
+        if (overwrite || ! inputMap.containsKey(onnxName)) {
+            inputMap.put(onnxName, vespaName);
+        }
     }
 
     public void addOutputNameMapping(String onnxName, String vespaName) {
+        addOutputNameMapping(onnxName, vespaName, true);
+    }
+
+    public void addOutputNameMapping(String onnxName, String vespaName, boolean overwrite) {
         Objects.requireNonNull(onnxName, "Onnx name cannot be null");
         Objects.requireNonNull(vespaName, "Vespa name cannot be null");
-        this.outputMap.add(new OnnxNameMapping(onnxName, vespaName));
+        if (overwrite || ! outputMap.containsKey(onnxName)) {
+            outputMap.put(onnxName, vespaName);
+        }
+    }
+
+    public void addInputType(String onnxName, Onnx.TypeProto type) {
+        Objects.requireNonNull(onnxName, "Onnx name cannot be null");
+        Objects.requireNonNull(type, "Tensor type cannot be null");
+        inputTypes.put(onnxName, type);
+    }
+
+    public void addOutputType(String onnxName, Onnx.TypeProto type) {
+        Objects.requireNonNull(onnxName, "Onnx name cannot be null");
+        Objects.requireNonNull(type, "Tensor type cannot be null");
+        outputTypes.put(onnxName, type);
     }
 
     /** Initiate sending of this constant to some services over file distribution */
@@ -76,11 +115,16 @@ public class OnnxModel {
 
     public String getName() { return name; }
     public String getFileName() { return path; }
+    public Path   getFilePath() { return Path.fromString(path); }
     public String getUri() { return path; }
     public String getFileReference() { return fileReference; }
 
-    public List<OnnxNameMapping> getInputMap() { return Collections.unmodifiableList(inputMap); }
-    public List<OnnxNameMapping> getOutputMap() { return Collections.unmodifiableList(outputMap); }
+    public Map<String, String> getInputMap() { return Collections.unmodifiableMap(inputMap); }
+    public Map<String, String> getOutputMap() { return Collections.unmodifiableMap(outputMap); }
+
+    public String getDefaultOutput() {
+        return defaultOutput;
+    }
 
     public void validate() {
         if (path == null || path.isEmpty())
@@ -90,23 +134,151 @@ public class OnnxModel {
     public String toString() {
         StringBuilder b = new StringBuilder();
         b.append("onnx-model '").append(name)
-         .append(pathType == PathType.FILE ? "' from file '" : " from uri ").append(path)
-         .append("' with ref '").append(fileReference)
-         .append("'");
+                .append(pathType == PathType.FILE ? "' from file '" : " from uri ").append(path)
+                .append("' with ref '").append(fileReference)
+                .append("'");
         return b.toString();
     }
 
-    public static class OnnxNameMapping {
-        private String onnxName;
-        private String vespaName;
-
-        private OnnxNameMapping(String onnxName, String vespaName) {
-            this.onnxName = onnxName;
-            this.vespaName = vespaName;
+    /**
+     * Return the tensor type for an ONNX model for the given context.
+     * An ONNX model can have dynamic/symbolic dimension sizes. If so, the output
+     * type depends on the input types for the given context (rank profile).
+     */
+    public TensorType getTensorType(String onnxName, MapEvaluationTypeContext context) {
+        Onnx.TypeProto onnxOutputType = outputTypes.get(onnxName);
+        if (onnxOutputType == null) {
+            throw new IllegalArgumentException("Could not find type for output '" + onnxName + "' " + "in '" + name + "'");
         }
-        public String getOnnxName() { return onnxName; }
-        public String getVespaName() { return vespaName; }
-        public void setVespaName(String vespaName) { this.vespaName = vespaName; }
+        if (allDimensionSizesAreKnown(onnxOutputType)) {
+            return vespaTypes.computeIfAbsent(onnxName, v -> typeFrom(onnxOutputType));
+        }
+        return getTensorTypeWithUnknownDimensions(onnxOutputType, context);
+    }
+
+    private static boolean allDimensionSizesAreKnown(Onnx.TypeProto type) {
+        return type.getTensorType().getShape().getDimList().stream().noneMatch(d ->
+                (d.hasDimParam() && ! d.hasDimValue()) || d.getDimValue() == -1);
+    }
+
+    private TensorType getTensorTypeWithUnknownDimensions(Onnx.TypeProto onnxOutputType, MapEvaluationTypeContext context) {
+        long unboundSize = 0;
+        Map<String, Long> symbolicSizes = new HashMap<>();
+
+        for (String onnxInputName : inputTypes.keySet()) {
+            Onnx.TypeProto onnxType = inputTypes.get(onnxInputName);
+            if (allDimensionSizesAreKnown(onnxType)) {
+                continue;
+            }
+
+            Optional<TensorType> vespaType = resolveInputType(onnxInputName, context);
+            if (vespaType.isEmpty()) {
+                return TensorType.empty;
+            }
+
+            var onnxDimensions = onnxType.getTensorType().getShape().getDimList();
+            var vespaDimensions = vespaType.get().dimensions();
+            if (vespaDimensions.size() != onnxDimensions.size()) {
+                return TensorType.empty;
+            }
+
+            for (int i = 0; i < vespaDimensions.size(); ++i) {
+                if (vespaDimensions.get(i).size().isEmpty()) {
+                    continue;
+                }
+                Long size = vespaDimensions.get(i).size().get();
+
+                // Handle dimensions with size -1 - typically batch dimensions
+                if (onnxDimensions.get(i).getDimValue() == -1) {
+                    if (unboundSize != 0 && unboundSize != size) {
+                        throw new IllegalArgumentException("Found conflicting sizes for unbound dimension " +
+                                "for type '" + onnxOutputType + "' in ONNX model '" + name + "'");
+                    }
+                    unboundSize = size;
+
+                // Handle dimensions with symbolic names
+                } else if (onnxDimensions.get(i).hasDimParam()) {
+                    String symbolicName = onnxDimensions.get(i).getDimParam();
+                    if (symbolicSizes.containsKey(symbolicName) && ! symbolicSizes.get(symbolicName).equals(size)) {
+                        throw new IllegalArgumentException("Found conflicting sizes for symbolic dimension '" +
+                                symbolicName + "' for input '" + onnxInputName + "' in ONNX model '" + name + "'");
+                    }
+                    symbolicSizes.put(symbolicName, size);
+                }
+            }
+        }
+        return typeFrom(onnxOutputType, symbolicSizes, unboundSize);
+    }
+
+    private Optional<TensorType> resolveInputType(String onnxInputName, MapEvaluationTypeContext context) {
+        String source = inputMap.get(onnxInputName);
+        if (source != null) {
+            // Source is either a simple reference (query/attribute/constant)...
+            Optional<Reference> reference = Reference.simple(source);
+            if (reference.isPresent()) {
+                return Optional.of(context.getType(reference.get()));
+            }
+            // ... or a function
+            ExpressionFunction func = context.getFunction(source);
+            if (func != null) {
+                return Optional.of(func.getBody().type(context));
+            }
+        }
+        return Optional.empty();  // if this context does not contain this input
+    }
+
+    private static TensorType typeFrom(Onnx.TypeProto type) {
+        return typeFrom(type, null, 0);
+    }
+
+    private static TensorType typeFrom(Onnx.TypeProto type, Map<String, Long> symbolicSizes, long unboundSize) {
+        String dimensionPrefix = "d"; // standard naming convention: d0, d1, ...
+        Onnx.TensorShapeProto shape = type.getTensorType().getShape();
+        TensorType.Builder builder = new TensorType.Builder(toValueType(type.getTensorType().getElemType()));
+        for (int i = 0; i < shape.getDimCount(); ++ i) {
+            String dimensionName = dimensionPrefix + i;
+            Onnx.TensorShapeProto.Dimension onnxDimension = shape.getDim(i);
+            long onnxDimensionSize = onnxDimension.getDimValue();
+            if (onnxDimension.hasDimParam() && symbolicSizes != null && symbolicSizes.containsKey(onnxDimension.getDimParam())) {
+                onnxDimensionSize = symbolicSizes.get(onnxDimension.getDimParam());
+            }
+            if (onnxDimensionSize == 0 && symbolicSizes != null) {
+                // This is for the case where all symbolic dimensions have
+                // different names, but can be resolved to a single dimension size.
+                Set<Long> unknownSizes = new HashSet<>(symbolicSizes.values());
+                if (unknownSizes.size() == 1) {
+                    onnxDimensionSize = unknownSizes.iterator().next();
+                }
+            }
+            if (onnxDimensionSize < 0) {
+                onnxDimensionSize = unboundSize;
+            }
+            if (onnxDimensionSize <= 0) {
+                throw new IllegalArgumentException("Unable to determine fixed dimension size when converting from " +
+                        "ONNX type: " + type + " to Vespa tensor type.");
+            }
+            builder.indexed(dimensionName, onnxDimensionSize);
+        }
+        return builder.build();
+    }
+
+    private static TensorType.Value toValueType(Onnx.TensorProto.DataType dataType) {
+        switch (dataType) {
+            case FLOAT: return TensorType.Value.FLOAT;
+            case DOUBLE: return TensorType.Value.DOUBLE;
+            // Imperfect conversion, for now:
+            case BOOL: return TensorType.Value.FLOAT;
+            case INT8: return TensorType.Value.FLOAT;
+            case INT16: return TensorType.Value.FLOAT;
+            case INT32: return TensorType.Value.FLOAT;
+            case INT64: return TensorType.Value.FLOAT;
+            case UINT8: return TensorType.Value.FLOAT;
+            case UINT16: return TensorType.Value.FLOAT;
+            case UINT32: return TensorType.Value.FLOAT;
+            case UINT64: return TensorType.Value.FLOAT;
+            default: throw new IllegalArgumentException("A ONNX tensor with data type " + dataType +
+                    " cannot be converted to a Vespa tensor type");
+        }
     }
 
 }
--- config-model/src/test/java/com/yahoo/searchdefinition/processing/RankingExpressionWithOnnxModelTestCase.java
+++ config-model/src/test/java/com/yahoo/searchdefinition/processing/RankingExpressionWithOnnxModelTestCase.java
@@ -25,43 +25,68 @@ public class RankingExpressionWithOnnxModelTestCase {
         OnnxModelsConfig.Builder builder = new OnnxModelsConfig.Builder();
         ((OnnxModelsConfig.Producer) db).getConfig(builder);
         OnnxModelsConfig config = new OnnxModelsConfig(builder);
-        assertEquals(3, config.model().size());
+        assertEquals(6, config.model().size());
 
-        assertEquals("my_model", config.model(1).name());
+        assertEquals("my_model", config.model(0).name());
+        assertEquals(3, config.model(0).input().size());
+        assertEquals("second/input:0", config.model(0).input(0).name());
+        assertEquals("constant(my_constant)", config.model(0).input(0).source());
+        assertEquals("first_input", config.model(0).input(1).name());
+        assertEquals("attribute(document_field)", config.model(0).input(1).source());
+        assertEquals("third_input", config.model(0).input(2).name());
+        assertEquals("rankingExpression(my_function)", config.model(0).input(2).source());
+        assertEquals(3, config.model(0).output().size());
+        assertEquals("path/to/output:0", config.model(0).output(0).name());
+        assertEquals("out", config.model(0).output(0).as());
+        assertEquals("path/to/output:1", config.model(0).output(1).name());
+        assertEquals("path_to_output_1", config.model(0).output(1).as());
+        assertEquals("path/to/output:2", config.model(0).output(2).name());
+        assertEquals("path_to_output_2", config.model(0).output(2).as());
+
+        assertEquals("files_model_onnx", config.model(1).name());
         assertEquals(3, config.model(1).input().size());
-        assertEquals("first_input", config.model(1).input(0).name());
-        assertEquals("attribute(document_field)", config.model(1).input(0).source());
-        assertEquals("second/input:0", config.model(1).input(1).name());
-        assertEquals("constant(my_constant)", config.model(1).input(1).source());
-        assertEquals("third_input", config.model(1).input(2).name());
-        assertEquals("rankingExpression(my_function)", config.model(1).input(2).source());
-        assertEquals(1, config.model(1).output().size());
+        assertEquals(3, config.model(1).output().size());
         assertEquals("path/to/output:0", config.model(1).output(0).name());
-        assertEquals("out", config.model(1).output(0).as());
-
-        assertEquals("files_ranking_model_onnx", config.model(0).name());
-        assertEquals(0, config.model(0).input().size());
-        assertEquals(2, config.model(0).output().size());
-        assertEquals("path/to/output:1", config.model(0).output(0).name());
-        assertEquals("path_to_output_1", config.model(0).output(0).as());
-        assertEquals("path/to/output:2", config.model(0).output(1).name());
-        assertEquals("path_to_output_2", config.model(0).output(1).as());
+        assertEquals("path_to_output_0", config.model(1).output(0).as());
+        assertEquals("path/to/output:1", config.model(1).output(1).name());
+        assertEquals("path_to_output_1", config.model(1).output(1).as());
+        assertEquals("path/to/output:2", config.model(1).output(2).name());
+        assertEquals("path_to_output_2", config.model(1).output(2).as());
+        assertEquals("files_model_onnx", config.model(1).name());
 
         assertEquals("another_model", config.model(2).name());
         assertEquals("third_input", config.model(2).input(2).name());
         assertEquals("rankingExpression(another_function)", config.model(2).input(2).source());
+
+        assertEquals("files_summary_model_onnx", config.model(3).name());
+        assertEquals(3, config.model(3).input().size());
+        assertEquals(3, config.model(3).output().size());
+
+        assertEquals("dynamic_model", config.model(5).name());
+        assertEquals(1, config.model(5).input().size());
+        assertEquals(1, config.model(5).output().size());
+        assertEquals("rankingExpression(my_function)", config.model(5).input(0).source());
+
+        assertEquals("unbound_model", config.model(4).name());
+        assertEquals(1, config.model(4).input().size());
+        assertEquals(1, config.model(4).output().size());
+        assertEquals("rankingExpression(my_function)", config.model(4).input(0).source());
+
+
     }
 
     private void assertTransformedFeature(DocumentDatabase db) {
         RankProfilesConfig.Builder builder = new RankProfilesConfig.Builder();
         ((RankProfilesConfig.Producer) db).getConfig(builder);
         RankProfilesConfig config = new RankProfilesConfig(builder);
-        assertEquals(5, config.rankprofile().size());
+        assertEquals(8, config.rankprofile().size());
 
         assertEquals("test_model_config", config.rankprofile(2).name());
         assertEquals("rankingExpression(my_function).rankingScript", config.rankprofile(2).fef().property(0).name());
         assertEquals("vespa.rank.firstphase", config.rankprofile(2).fef().property(2).name());
-        assertEquals("onnxModel(my_model).out", config.rankprofile(2).fef().property(2).value());
+        assertEquals("rankingExpression(firstphase)", config.rankprofile(2).fef().property(2).value());
+        assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(2).fef().property(3).name());
+        assertEquals("onnxModel(my_model).out{d0:1}", config.rankprofile(2).fef().property(3).value());
 
         assertEquals("test_generated_model_config", config.rankprofile(3).name());
         assertEquals("rankingExpression(my_function).rankingScript", config.rankprofile(3).fef().property(0).name());
@@ -69,16 +94,34 @@ public class RankingExpressionWithOnnxModelTestCase {
         assertEquals("rankingExpression(second_input).rankingScript", config.rankprofile(3).fef().property(4).name());
         assertEquals("rankingExpression(third_input).rankingScript", config.rankprofile(3).fef().property(6).name());
         assertEquals("vespa.rank.firstphase", config.rankprofile(3).fef().property(8).name());
-        assertEquals("onnxModel(files_ranking_model_onnx).path_to_output_1", config.rankprofile(3).fef().property(8).value());
+        assertEquals("rankingExpression(firstphase)", config.rankprofile(3).fef().property(8).value());
+        assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(3).fef().property(9).name());
+        assertEquals("onnxModel(files_model_onnx).path_to_output_1{d0:1}", config.rankprofile(3).fef().property(9).value());
 
         assertEquals("test_summary_features", config.rankprofile(4).name());
         assertEquals("rankingExpression(another_function).rankingScript", config.rankprofile(4).fef().property(0).name());
         assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(4).fef().property(3).name());
         assertEquals("1", config.rankprofile(4).fef().property(3).value());
         assertEquals("vespa.summary.feature", config.rankprofile(4).fef().property(4).name());
-        assertEquals("onnxModel(another_model).out", config.rankprofile(4).fef().property(4).value());
+        assertEquals("onnxModel(files_summary_model_onnx).path_to_output_2", config.rankprofile(4).fef().property(4).value());
         assertEquals("vespa.summary.feature", config.rankprofile(4).fef().property(5).name());
-        assertEquals("onnxModel(files_ranking_model_onnx).path_to_output_2", config.rankprofile(4).fef().property(5).value());
+        assertEquals("onnxModel(another_model).out", config.rankprofile(4).fef().property(5).value());
+
+        assertEquals("test_dynamic_model", config.rankprofile(5).name());
+        assertEquals("rankingExpression(my_function).rankingScript", config.rankprofile(5).fef().property(0).name());
+        assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(5).fef().property(3).name());
+        assertEquals("onnxModel(dynamic_model).my_output{d0:0, d1:1}", config.rankprofile(5).fef().property(3).value());
+
+        assertEquals("test_dynamic_model_2", config.rankprofile(6).name());
+        assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(6).fef().property(5).name());
+        assertEquals("onnxModel(dynamic_model).my_output{d0:0, d1:2}", config.rankprofile(6).fef().property(5).value());
+
+        assertEquals("test_unbound_model", config.rankprofile(7).name());
+        assertEquals("rankingExpression(my_function).rankingScript", config.rankprofile(7).fef().property(0).name());
+        assertEquals("rankingExpression(firstphase).rankingScript", config.rankprofile(7).fef().property(3).name());
+        assertEquals("onnxModel(unbound_model).my_output{d0:0, d1:1}", config.rankprofile(7).fef().property(3).value());
+
+
     }
 
 }
