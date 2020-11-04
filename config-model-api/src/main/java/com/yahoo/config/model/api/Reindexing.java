0efcc461407e031eba295f043a90df9e8d96ef1e
Jon Marius Venstad <venstad@gmail.com>
2020-11-04 09:21:57 +0100

    Use Optional<Status> to signal no reidexing for a type

--- config-model-api/src/main/java/com/yahoo/config/model/api/Reindexing.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/Reindexing.java
@@ -2,7 +2,7 @@
 package com.yahoo.config.model.api;
 
 import java.time.Instant;
-import java.util.Map;
+import java.util.Optional;
 
 /**
  * Status of reindexing for the documents of an application.
@@ -11,11 +11,8 @@ import java.util.Map;
  */
 public interface Reindexing {
 
-    /** No reindexing should be done for this document type and cluster. */
-    Status NO_REINDEXING = () -> Instant.MAX;
-
     /** Reindexing status for a given application, cluster and document type. */
-    default Status status(String cluster, String documentType) { return NO_REINDEXING; }
+    default Optional<Status> status(String cluster, String documentType) { return Optional.empty(); }
 
     /** Reindexing status of a given document type in a given cluster in a given application. */
     interface Status {
