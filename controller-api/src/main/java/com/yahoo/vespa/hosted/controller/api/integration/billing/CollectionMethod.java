ce0b949e123efd729a27e576ef4a62e11fb97103
smorgrav <smorgrav@verizonmedia.com>
2020-11-06 08:46:09 +0100

    Add collection method enumerations - change default

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/billing/CollectionMethod.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/billing/CollectionMethod.java
@@ -1,6 +1,8 @@
 package com.yahoo.vespa.hosted.controller.api.integration.billing;
 
 public enum CollectionMethod {
-    AUTO,
-    INVOICE
+    NONE,
+    EPAY,
+    INVOICE,
+    AUTO // Deprecated - this has never been serialized and can be removed in subsequent release
 }
