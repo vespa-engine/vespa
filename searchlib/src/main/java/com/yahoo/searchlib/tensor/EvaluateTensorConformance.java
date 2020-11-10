7f468e2ce0c4c4845a308655ac80bd9fee14a5f2
Arne Juul <arnej@verizonmedia.com>
2020-11-10 16:17:02 +0000

    fix copyright, untabify

--- searchlib/src/main/java/com/yahoo/searchlib/tensor/EvaluateTensorConformance.java
+++ searchlib/src/main/java/com/yahoo/searchlib/tensor/EvaluateTensorConformance.java
@@ -1,4 +1,5 @@
-// Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
 package com.yahoo.searchlib.tensor;
 
 import com.yahoo.slime.Cursor;
@@ -74,7 +75,7 @@ public class EvaluateTensorConformance {
     private boolean testCase(String test, int count) {
         boolean okAndEqual = false;
         try {
-	    Slime input = SlimeUtils.jsonToSlime(test);
+            Slime input = SlimeUtils.jsonToSlime(test);
             Slime result = new Slime();
             var top = result.setObject();
             SlimeUtils.copyObject(input.get(), top);
