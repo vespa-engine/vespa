8898c57ea0a2376b8ab77aa9e675f51afac99b84
Bjørn Christian Seime <bjorn.christian@seime.no>
2020-11-11 09:30:51 +0100

    Revert "Revert "Bjorncs/install clustercontroller reindexer" MERGEOK"

---
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/clustercontroller/ReindexingContext.java
@@ -0,0 +1,33 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.model.admin.clustercontroller;
+
+import com.yahoo.config.model.api.Reindexing;
+import com.yahoo.documentmodel.NewDocumentType;
+
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * Context required to configure automatic reindexing for a given cluster controller cluster (for a given content cluster).
+ *
+ * @author bjorncs
+ */
+public class ReindexingContext {
+
+    private final Reindexing reindexing;
+    private final String contentClusterName;
+    private final Collection<NewDocumentType> documentTypes;
+
+    public ReindexingContext(
+            Reindexing reindexing,
+            String contentClusterName,
+            Collection<NewDocumentType> documentTypes) {
+        this.reindexing = Objects.requireNonNull(reindexing);
+        this.contentClusterName = Objects.requireNonNull(contentClusterName);
+        this.documentTypes = Objects.requireNonNull(documentTypes);
+    }
+
+    public Reindexing reindexing() { return reindexing; }
+    public String contentClusterName() { return contentClusterName; }
+    public Collection<NewDocumentType> documentTypes() { return documentTypes; }
+}
