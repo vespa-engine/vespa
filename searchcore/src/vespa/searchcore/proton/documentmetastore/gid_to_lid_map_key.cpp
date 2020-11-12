758bea3ea07fc5d5bd4ed7cfb20fa4d859676d92
Tor Egge <Tor.Egge@broadpark.no>
2020-11-12 10:45:51 +0100

    Inline default GidToLidMapKey constructor.

--- searchcore/src/vespa/searchcore/proton/documentmetastore/gid_to_lid_map_key.cpp
+++ searchcore/src/vespa/searchcore/proton/documentmetastore/gid_to_lid_map_key.cpp
@@ -7,18 +7,6 @@ using document::GlobalId;
 
 namespace proton::documentmetastore {
 
-GidToLidMapKey::GidToLidMapKey()
-    : _lid(FIND_DOC_ID),
-      _gid_key(0u)
-{
-}
-
-GidToLidMapKey::GidToLidMapKey(uint32_t lid, uint32_t gid_key)
-    : _lid(lid),
-      _gid_key(gid_key)
-{
-}
-
 GidToLidMapKey::GidToLidMapKey(uint32_t lid, const GlobalId& gid)
     : GidToLidMapKey(lid, GlobalId::BucketOrderCmp::gid_key32(gid))
 {
--- searchcore/src/vespa/searchcore/proton/documentmetastore/gid_to_lid_map_key.h
+++ searchcore/src/vespa/searchcore/proton/documentmetastore/gid_to_lid_map_key.h
@@ -19,8 +19,16 @@ class GidToLidMapKey {
     static constexpr uint32_t FIND_DOC_ID = std::numeric_limits<uint32_t>::max();
 
 public:
-    GidToLidMapKey();
-    GidToLidMapKey(uint32_t lid, uint32_t gid_key);
+    GidToLidMapKey() noexcept
+        : _lid(FIND_DOC_ID),
+          _gid_key(0u)
+    {
+    }
+    GidToLidMapKey(uint32_t lid, uint32_t gid_key) noexcept
+        : _lid(lid),
+          _gid_key(gid_key)
+    {
+    }
     GidToLidMapKey(uint32_t lid, const document::GlobalId &gid);
     static GidToLidMapKey make_find_key(const document::GlobalId &gid);
 
