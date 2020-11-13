f6f99ac314d5bf5659e8489d9c959033c2129155
Jon Marius Venstad <venstad@gmail.com>
2020-11-13 08:10:06 +0100

    Make reindexing trigger exactly 28 days after previous instant

--- configserver/src/test/java/com/yahoo/vespa/config/server/maintenance/ReindexingMaintainerTest.java
+++ configserver/src/test/java/com/yahoo/vespa/config/server/maintenance/ReindexingMaintainerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import java.time.Instant;
 
-import static com.yahoo.vespa.config.server.maintenance.ReindexingMaintainer.withConvergenceOn;
+import static com.yahoo.vespa.config.server.maintenance.ReindexingMaintainer.withNewReady;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,35 +16,39 @@ public class ReindexingMaintainerTest {
 
     @Test
     public void testReadyComputation() {
-        ApplicationReindexing reindexing = ApplicationReindexing.ready(Instant.ofEpochMilli(1 << 20))
+        ApplicationReindexing reindexing = ApplicationReindexing.ready(Instant.EPOCH)
                                                                 .withPending("one", "a", 10)
                                                                 .withPending("two", "b", 20)
-                                                                .withReady("one", Instant.EPOCH)
-                                                                .withReady("one", "a", Instant.ofEpochMilli(1))
+                                                                .withReady("one", Instant.ofEpochMilli(2))
+                                                                .withReady("one", "a", Instant.ofEpochMilli(3))
                                                                 .withReady("two", Instant.ofEpochMilli(2 << 10))
                                                                 .withReady("two", "b", Instant.ofEpochMilli(2))
                                                                 .withReady("two", "c", Instant.ofEpochMilli(3));
 
         // Nothing happens without convergence.
         assertEquals(reindexing,
-                     withConvergenceOn(reindexing, () -> -1L, Instant.EPOCH));
+                     withNewReady(reindexing, () -> -1L, Instant.EPOCH));
 
         // Status for (one, a) changes, but not (two, b).
 
-        assertEquals(reindexing.withReady("one", "a", Instant.EPOCH).withoutPending("one", "a"),
-                     withConvergenceOn(reindexing, () -> 19L, Instant.EPOCH));
+        assertEquals(reindexing.withReady("one", "a", Instant.EPOCH)
+                               .withoutPending("one", "a"),
+                     withNewReady(reindexing, () -> 19L, Instant.EPOCH));
 
-        Instant later = Instant.ofEpochMilli(2).plus(ReindexingMaintainer.reindexingInterval);
-        assertEquals(reindexing.withReady("one", later)         // Had EPOCH as previous, so is updated, overwriting status for "a".
-                               .withReady("two", "b", later)    // Got config convergence, so is updated.
-                               .withoutPending("one", "a")
-                               .withoutPending("two", "b"),
-                     withConvergenceOn(reindexing, () -> 20L, later));
+        Instant later = Instant.EPOCH.plus(ReindexingMaintainer.reindexingInterval.multipliedBy(3));
+        assertEquals(reindexing.withoutPending("one", "a")      // Converged, no longer pending.
+                               .withReady(later),               // Had EPOCH as previous, so is updated, overwriting all more specific status.
+                     withNewReady(reindexing, () -> 19L, later.plusMillis(1)));
+
+        assertEquals(reindexing.withoutPending("one", "a")      // Converged, no longer pending.
+                               .withoutPending("two", "b")      // Converged, no LOnger pending.
+                               .withReady(later),               // Had EPOCH as previous, so is updated, overwriting all more specific status.
+                     withNewReady(reindexing, () -> 20L, later.plusMillis(1)));
 
         // Verify generation supplier isn't called when no pending document types.
-        withConvergenceOn(reindexing.withoutPending("one", "a").withoutPending("two", "b"),
-                          () -> { throw new AssertionError("not supposed to run"); },
-                          later);
+        withNewReady(reindexing.withoutPending("one", "a").withoutPending("two", "b"),
+                     () -> { throw new AssertionError("not supposed to run"); },
+                     later);
     }
 
 }
