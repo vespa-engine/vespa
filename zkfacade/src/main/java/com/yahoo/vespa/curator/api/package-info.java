f9f322671c7115eeeedb64d09305834575751d07
Martin Polden <mpolden@mpolden.no>
2020-11-16 11:02:53 +0100

    Add VespaCurator interface

---
+++ zkfacade/src/main/java/com/yahoo/vespa/curator/api/package-info.java
@@ -0,0 +1,10 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * @author mpolden
+ */
+@PublicApi
+@ExportPackage
+package com.yahoo.vespa.curator.api;
+
+import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.osgi.annotation.ExportPackage;
