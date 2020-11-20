278967c98483da6b1308d34bd7b2b77e2f690288
Jon Marius Venstad <jonmv@users.noreply.github.com>
2020-11-20 21:34:38 +0100

    Revert "Jonmv/reindexing rest api"

--- container-core/src/main/java/com/yahoo/restapi/Path.java
+++ container-core/src/main/java/com/yahoo/restapi/Path.java
@@ -42,6 +42,24 @@ public class Path {
     private final Map<String, String> values = new HashMap<>();
     private String rest = "";
 
+    /**
+     * @deprecated use {@link #Path(URI)} for correct handling of URL encoded paths.
+     */
+    @Deprecated
+    public Path(String path) {
+        this(path, "");
+    }
+
+    /**
+     * @deprecated use {@link #Path(URI, String)} for correct handling of URL encoded paths.
+     */
+    @Deprecated
+    public Path(String path, String optionalPrefix) {
+        this.optionalPrefix = optionalPrefix;
+        this.pathString = path;
+        this.elements = path.split("/");
+    }
+
     public Path(URI uri) {
         this(uri, "");
     }
