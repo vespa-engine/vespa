9de0ac3fb140d563b8172c11608a299f515a3d26
Tor Egge <Tor.Egge@broadpark.no>
2020-11-20 12:14:28 +0100

    Update comments.

--- searchcore/src/vespa/searchcore/proton/reference/i_gid_to_lid_change_handler.h
+++ searchcore/src/vespa/searchcore/proton/reference/i_gid_to_lid_change_handler.h
@@ -38,9 +38,13 @@ public:
     virtual void removeListeners(const vespalib::string &docTypeName,
                                  const std::set<vespalib::string> &keepNames) = 0;
     /**
-     * Notify gid to lid mapping change.
+     * Notify pending gid to lid mapping change. Passed on to listeners later
+     * when force commit has made changes visible.
      */
     virtual void notifyPut(IDestructorCallbackSP context, GlobalId gid, uint32_t lid, SerialNum serial_num) = 0;
+    /**
+     * Notify removal of gid. Passed on to listeners at once.
+     */
     virtual void notifyRemove(IDestructorCallbackSP context, GlobalId gid, SerialNum serialNum) = 0;
     virtual std::unique_ptr<IPendingGidToLidChanges> grab_pending_changes() = 0;
 };
