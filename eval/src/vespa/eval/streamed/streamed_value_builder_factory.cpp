037e961d3f75ae7d193be425ce064eed5f4be610
Arne Juul <arnej@verizonmedia.com>
2020-11-24 21:17:29 +0000

    fix arguments (names and explicit)

--- eval/src/vespa/eval/streamed/streamed_value_builder_factory.cpp
+++ eval/src/vespa/eval/streamed/streamed_value_builder_factory.cpp
@@ -6,23 +6,26 @@
 namespace vespalib::eval {
 
 struct SelectStreamedValueBuilder {
-    template <typename T, typename ...Args>
-    static std::unique_ptr<ValueBuilderBase> invoke(const ValueType &type, Args &&...args)
+    template <typename T>
+    static std::unique_ptr<ValueBuilderBase> invoke(
+        const ValueType &type, size_t num_mapped, 
+        size_t subspace_size, size_t expected_subspaces)
     {
         assert(check_cell_type<T>(type.cell_type()));
-        return std::make_unique<StreamedValueBuilder<T>>(type, std::forward<Args>(args)...);
+        return std::make_unique<StreamedValueBuilder<T>>(
+            type, num_mapped, subspace_size, expected_subspaces);
     }
 };
 
 std::unique_ptr<ValueBuilderBase>
 StreamedValueBuilderFactory::create_value_builder_base(const ValueType &type,
-                                                       size_t num_mapped_in,
-                                                       size_t subspace_size_in,
+                                                       size_t num_mapped,
+                                                       size_t subspace_size,
                                                        size_t expected_subspaces) const
 {
     return typify_invoke<1,TypifyCellType,SelectStreamedValueBuilder>(
         type.cell_type(),
-        type, num_mapped_in, subspace_size_in, expected_subspaces);
+        type, num_mapped, subspace_size, expected_subspaces);
 }
 
 StreamedValueBuilderFactory::~StreamedValueBuilderFactory() = default;
