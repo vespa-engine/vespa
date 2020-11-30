56892a683c82dd669d4ad51fc2b01dd01e7d4103
Jon Bratseth <bratseth@gmail.com>
2020-11-30 22:10:31 +0100

    Log exception

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/NodeMetricsDbMaintainer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/NodeMetricsDbMaintainer.java
@@ -1,23 +1,18 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.provision.maintenance;
 
-import com.yahoo.collections.Pair;
 import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.jdisc.Metric;
 import com.yahoo.vespa.hosted.provision.NodeRepository;
-import com.yahoo.vespa.hosted.provision.autoscale.MetricSnapshot;
 import com.yahoo.vespa.hosted.provision.autoscale.MetricsFetcher;
 import com.yahoo.vespa.hosted.provision.autoscale.MetricsDb;
 import com.yahoo.yolean.Exceptions;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 /**
- * Maintainer which keeps the node metric db up to date by periodically fetching metrics from all
- * active nodes.
+ * Maintainer which keeps the node metric db up to date by periodically fetching metrics from all active nodes.
  *
  * @author bratseth
  */
@@ -48,7 +43,7 @@ public class NodeMetricsDbMaintainer extends NodeRepositoryMaintainer {
             catch (Exception e) {
                 // TODO: Don't warn if this only happens occasionally
                 if (warnings++ < maxWarningsPerInvocation)
-                    log.log(Level.WARNING, "Could not update metrics for " + application + ": " + Exceptions.toMessageString(e));
+                    log.log(Level.WARNING, "Could not update metrics for " + application + ": " + Exceptions.toMessageString(e), e);
             }
         }
         metricsDb.gc();
