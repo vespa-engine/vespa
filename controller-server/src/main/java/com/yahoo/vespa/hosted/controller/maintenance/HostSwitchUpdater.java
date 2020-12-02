99bcfff52bf4982e214e31a75cab31651b03490c
Martin Polden <mpolden@mpolden.no>
2020-12-02 15:21:05 +0100

    Limit HostSwitchUpdater to relevant systems

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/HostSwitchUpdater.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/HostSwitchUpdater.java
@@ -1,6 +1,7 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.maintenance;
 
+import com.yahoo.config.provision.SystemName;
 import com.yahoo.vespa.hosted.controller.Controller;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.Node;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.NodeRepository;
@@ -8,8 +9,10 @@ import com.yahoo.vespa.hosted.controller.api.integration.entity.NodeEntity;
 import com.yahoo.vespa.hosted.controller.api.integration.noderepository.NodeRepositoryNode;
 
 import java.time.Duration;
+import java.util.EnumSet;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
@@ -19,10 +22,12 @@ import java.util.stream.Collectors;
  */
 public class HostSwitchUpdater extends ControllerMaintainer {
 
+    private static final Logger log = Logger.getLogger(HostSwitchUpdater.class.getName());
+
     private final NodeRepository nodeRepository;
 
     public HostSwitchUpdater(Controller controller, Duration interval) {
-        super(controller, interval);
+        super(controller, interval, null, EnumSet.of(SystemName.cd, SystemName.main));
         this.nodeRepository = controller.serviceRegistry().configServer().nodeRepository();
     }
 
@@ -31,14 +36,22 @@ public class HostSwitchUpdater extends ControllerMaintainer {
         Map<String, NodeEntity> nodeEntities = controller().serviceRegistry().entityService().listNodes().stream()
                                                            .collect(Collectors.toMap(NodeEntity::hostname,
                                                                                      Function.identity()));
-        for (var zone : controller().zoneRegistry().zones().controllerUpgraded().all().ids()) {
-            for (var node : nodeRepository.list(zone)) {
-                NodeEntity nodeEntity = nodeEntities.get(node.hostname().value());
-                if (!shouldUpdate(node, nodeEntity)) continue;
+        int nodesUpdated = 0;
+        try {
+            for (var zone : controller().zoneRegistry().zones().controllerUpgraded().all().ids()) {
+                for (var node : nodeRepository.list(zone)) {
+                    NodeEntity nodeEntity = nodeEntities.get(node.hostname().value());
+                    if (!shouldUpdate(node, nodeEntity)) continue;
 
-                NodeRepositoryNode updatedNode = new NodeRepositoryNode();
-                updatedNode.setSwitchHostname(nodeEntity.switchHostname().get());
-                nodeRepository.patchNode(zone, node.hostname().value(), updatedNode);
+                    NodeRepositoryNode updatedNode = new NodeRepositoryNode();
+                    updatedNode.setSwitchHostname(nodeEntity.switchHostname().get());
+                    nodeRepository.patchNode(zone, node.hostname().value(), updatedNode);
+                    nodesUpdated++;
+                }
+            }
+        } finally {
+            if (nodesUpdated > 0) {
+                log.info("Updated switch hostname for " + nodesUpdated + " node(s)");
             }
         }
         return true;
