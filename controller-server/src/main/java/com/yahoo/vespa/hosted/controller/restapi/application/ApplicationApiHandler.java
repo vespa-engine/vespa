fbe17005def2d74da5b8ddf35a80dcf546c355b9
Jon Bratseth <bratseth@gmail.com>
2020-12-07 11:06:27 +0100

    Compare just numbers

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/application/ApplicationApiHandler.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/restapi/application/ApplicationApiHandler.java
@@ -699,7 +699,8 @@ public class ApplicationApiHandler extends LoggingRequestHandler {
             toSlime(cluster.min(), clusterObject.setObject("min"));
             toSlime(cluster.max(), clusterObject.setObject("max"));
             toSlime(cluster.current(), clusterObject.setObject("current"));
-            if (cluster.target().isPresent() && ! cluster.target().get().equals(cluster.current()))
+            if (cluster.target().isPresent()
+                && ! cluster.target().get().justNumbers().equals(cluster.current().justNumbers()))
                 toSlime(cluster.target().get(), clusterObject.setObject("target"));
             cluster.suggested().ifPresent(suggested -> toSlime(suggested, clusterObject.setObject("suggested")));
             scalingEventsToSlime(cluster.scalingEvents(), clusterObject.setArray("scalingEvents"));
