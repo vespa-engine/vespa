9014ab220f9a931fe572b2b3d5d2279d63b21090
Harald Musum <musum@verizonmedia.com>
2020-12-08 12:35:02 +0100

    Add support for ZooKeeper server 3.6.2

---
+++ zookeeper-server/zookeeper-server-3.6.2/src/main/java/com/yahoo/vespa/zookeeper/ReconfigurableVespaZooKeeperServer.java
@@ -0,0 +1,26 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.zookeeper;
+
+import com.google.inject.Inject;
+import com.yahoo.cloud.config.ZookeeperServerConfig;
+import com.yahoo.component.AbstractComponent;
+
+import java.nio.file.Path;
+
+/**
+ * Starts or reconfigures zookeeper cluster
+ *
+ * @author hmusum
+ */
+public class ReconfigurableVespaZooKeeperServer extends AbstractComponent implements VespaZooKeeperServer {
+
+    @Inject
+    public ReconfigurableVespaZooKeeperServer(Reconfigurer reconfigurer, ZookeeperServerConfig zookeeperServerConfig) {
+        reconfigurer.startOrReconfigure(zookeeperServerConfig, this);
+    }
+
+    public void start(Path configFilePath) {
+        new ZooKeeperServer().start(configFilePath);
+    }
+
+}
---
+++ zookeeper-server/zookeeper-server-3.6.2/src/main/java/com/yahoo/vespa/zookeeper/VespaZooKeeperServerImpl.java
@@ -0,0 +1,35 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.zookeeper;
+
+import com.google.inject.Inject;
+import com.yahoo.cloud.config.ZookeeperServerConfig;
+import com.yahoo.component.AbstractComponent;
+
+import java.nio.file.Path;
+
+/**
+ * Main component controlling startup and stop of zookeeper server
+ *
+ * @author Ulf Lilleengen
+ * @author Harald Musum
+ */
+public class VespaZooKeeperServerImpl extends AbstractComponent implements VespaZooKeeperServer {
+
+    private final ZooKeeperRunner zooKeeperRunner;
+
+    @Inject
+    public VespaZooKeeperServerImpl(ZookeeperServerConfig zookeeperServerConfig) {
+        this.zooKeeperRunner = new ZooKeeperRunner(zookeeperServerConfig, this);
+    }
+
+    @Override
+    public void deconstruct() {
+        zooKeeperRunner.shutdown();
+        super.deconstruct();
+    }
+
+    public void start(Path configFilePath) {
+        new ZooKeeperServer().start(configFilePath);
+    }
+
+}
