c92956a208e8243b7eb9e14eb906844a5bd01cd4
Geir Storli <geirst@verizonmedia.com>
2020-12-15 13:41:39 +0000

    Add explorer for the shared thread pools used by proton and it's document databases.

--- searchcore/src/vespa/searchcore/proton/server/executor_threading_service_explorer.cpp
+++ searchcore/src/vespa/searchcore/proton/server/executor_threading_service_explorer.cpp
@@ -1,25 +1,20 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
+#include "executor_explorer_utils.h"
 #include "executor_threading_service_explorer.h"
 #include "executorthreadingservice.h"
 #include <vespa/vespalib/data/slime/cursor.h>
 #include <vespa/vespalib/util/adaptive_sequenced_executor.h>
-#include <vespa/vespalib/util/blockingthreadstackexecutor.h>
 #include <vespa/vespalib/util/sequencedtaskexecutor.h>
-#include <vespa/vespalib/util/singleexecutor.h>
-#include <vespa/vespalib/util/threadexecutor.h>
-#include <vespa/vespalib/util/threadstackexecutor.h>
 
 using vespalib::AdaptiveSequencedExecutor;
-using vespalib::BlockingThreadStackExecutor;
 using vespalib::ISequencedTaskExecutor;
 using vespalib::SequencedTaskExecutor;
-using vespalib::SingleExecutor;
-using vespalib::SyncableThreadExecutor;
-using vespalib::ThreadStackExecutor;
 using vespalib::slime::Cursor;
 
 namespace proton {
+
+using explorer::convert_executor_to_slime;
 
 namespace {
 
@@ -27,39 +22,6 @@ void
 set_type(Cursor& object, const vespalib::string& type)
 {
     object.setString("type", type);
-}
-
-void
-convert_syncable_executor_to_slime(const SyncableThreadExecutor& executor, const vespalib::string& type, Cursor& object)
-{
-    set_type(object, type);
-    object.setLong("num_threads", executor.getNumThreads());
-    object.setLong("task_limit", executor.getTaskLimit());
-}
-
-void
-convert_single_executor_to_slime(const SingleExecutor& executor, Cursor& object)
-{
-    convert_syncable_executor_to_slime(executor, "SingleExecutor", object);
-    object.setLong("watermark", executor.get_watermark());
-    object.setDouble("reaction_time_sec", vespalib::to_s(executor.get_reaction_time()));
-}
-
-void
-convert_executor_to_slime(const SyncableThreadExecutor* executor, Cursor& object)
-{
-    if (executor == nullptr) {
-        return;
-    }
-    if (const auto* single = dynamic_cast<const SingleExecutor*>(executor)) {
-        convert_single_executor_to_slime(*single, object);
-    } else if (const auto* blocking = dynamic_cast<const BlockingThreadStackExecutor*>(executor)) {
-        convert_syncable_executor_to_slime(*blocking, "BlockingThreadStackExecutor", object);
-    } else if (const auto* thread = dynamic_cast<const ThreadStackExecutor*>(executor)) {
-        convert_syncable_executor_to_slime(*thread, "ThreadStackExecutor", object);
-    } else {
-        convert_syncable_executor_to_slime(*executor, "SyncableThreadExecutor", object);
-    }
 }
 
 void
