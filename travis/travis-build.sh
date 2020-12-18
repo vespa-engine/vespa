e05249a2380721ed4c491b386c5da4ae0f92e97b
Arnstein Ressem <aressem@verizonmedia.com>
2020-12-18 08:34:52 +0100

    Only set -e

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-set -euo pipefail
+set -e
 
 export SOURCE_DIR=/source
 export NUM_THREADS=6
