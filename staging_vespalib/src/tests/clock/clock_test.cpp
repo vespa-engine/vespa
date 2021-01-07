f8ab035439b7b4a1d835cef51d6dfc3d82165bc9
Henning Baldersheim <balder@yahoo-inc.com>
2021-01-07 22:05:47 +0000

    Make the test deterministic by waiting for clock to tick at least once.

--- staging_vespalib/src/tests/clock/clock_test.cpp
+++ staging_vespalib/src/tests/clock/clock_test.cpp
@@ -7,7 +7,14 @@
 using vespalib::Clock;
 using vespalib::duration;
 using vespalib::steady_time;
+using vespalib::steady_clock;
 
+void waitForMovement(steady_time start, Clock & clock, vespalib::duration timeout) {
+    steady_time startOsClock = steady_clock::now();
+    while ((clock.getTimeNS() <= start) && ((steady_clock::now() - startOsClock) < timeout)) {
+        std::this_thread::sleep_for(1ms);
+    }
+}
 
 TEST("Test that clock is ticking forward") {
 
@@ -15,10 +22,12 @@ TEST("Test that clock is ticking forward") {
     FastOS_ThreadPool pool(0x10000);
     ASSERT_TRUE(pool.NewThread(clock.getRunnable(), nullptr) != nullptr);
     steady_time start = clock.getTimeNS();
-    std::this_thread::sleep_for(1s);
+    waitForMovement(start, clock, 10s);
     steady_time stop = clock.getTimeNS();
     EXPECT_TRUE(stop > start);
     std::this_thread::sleep_for(1s);
+    start = clock.getTimeNS();
+    waitForMovement(start, clock, 10s);
     clock.stop();
     steady_time stop2 = clock.getTimeNS();
     EXPECT_TRUE(stop2 > stop);
