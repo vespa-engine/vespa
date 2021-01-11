cf970e2c71c003118b5cc4233973a223afb7f4ed
Tor Egge <Tor.Egge@broadpark.no>
2021-01-11 16:15:41 +0100

    Include <vespa/vespalib/stllike/hashtable.hpp> when needed.

--- eval/src/vespa/eval/eval/fast_addr_map.cpp
+++ eval/src/vespa/eval/eval/fast_addr_map.cpp
@@ -1,6 +1,7 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "fast_addr_map.h"
+#include <vespa/vespalib/stllike/hashtable.hpp>
 
 namespace vespalib::eval {
 
