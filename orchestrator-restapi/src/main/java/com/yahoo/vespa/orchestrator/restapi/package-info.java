e0258d02c2830f5b95b7c5b9efd05f8f16b6cf00
Jon Marius Venstad <venstad@gmail.com>
2021-01-12 08:13:35 +0100

    Export API package

---
+++ orchestrator-restapi/src/main/java/com/yahoo/vespa/orchestrator/restapi/package-info.java
@@ -0,0 +1,7 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+@ExportPackage
+@PublicApi
+package com.yahoo.vespa.orchestrator.restapi;
+
+import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.osgi.annotation.ExportPackage;
