1105b88537eb28bdbae23c4d967e2fbfa0ad9857
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-01-19 13:55:33 +0100

    Extend ZstdCompressor with more low-level API methods

--- vespajlib/src/test/java/com/yahoo/compress/ZstdCompressorTest.java
+++ vespajlib/src/test/java/com/yahoo/compress/ZstdCompressorTest.java
@@ -4,6 +4,7 @@ package com.yahoo.compress;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author bjorncs
@@ -17,6 +18,20 @@ class ZstdCompressorTest {
         byte[] compressedData = compressor.compress(inputData, 0, inputData.length);
         byte[] decompressedData = compressor.decompress(compressedData, 0, compressedData.length);
         assertArrayEquals(inputData, decompressedData);
+    }
+
+    @Test
+    void compressed_size_is_less_than_uncompressed() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            builder.append("The quick brown fox jumps over the lazy dog").append('\n');
+        }
+        byte[] inputData = builder.toString().getBytes();
+        ZstdCompressor compressor = new ZstdCompressor();
+        byte[] compressedData = compressor.compress(inputData, 0, inputData.length);
+        assertTrue(
+                compressedData.length < inputData.length,
+                () -> "Compressed size is " + compressedData.length + " while uncompressed size is " + inputData.length);
     }
 
 }
