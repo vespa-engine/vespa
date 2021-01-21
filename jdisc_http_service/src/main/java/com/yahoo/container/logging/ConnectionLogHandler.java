a40c504053bd636ba7b62eaf819163c799f8b4c8
Morten Tokle <mortent@verizonmedia.com>
2021-01-21 09:34:01 +0100

    Log directly to LogFileHandler

--- jdisc_http_service/src/main/java/com/yahoo/container/logging/ConnectionLogHandler.java
+++ jdisc_http_service/src/main/java/com/yahoo/container/logging/ConnectionLogHandler.java
@@ -2,18 +2,17 @@
 
 package com.yahoo.container.logging;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
  * @author mortent
  */
 class ConnectionLogHandler {
-    public final Logger connection = Logger.getAnonymousLogger();
     private final LogFileHandler logFileHandler;
 
     public ConnectionLogHandler(String clusterName) {
-        connection.setUseParentHandlers(false);
-
         LogFormatter lf = new LogFormatter();
         lf.messageOnly(true);
         logFileHandler = new LogFileHandler(
@@ -22,13 +21,14 @@ class ConnectionLogHandler {
                 "0 60 ...",
                 String.format("ConnectionLog.%s", clusterName),
                 lf);
-        this.logFileHandler.setFormatter(lf);
-        connection.addHandler(this.logFileHandler);
+    }
+
+    public void log(String message) {
+        logFileHandler.publish(new LogRecord(Level.INFO, message));
     }
 
     public void shutdown() {
         logFileHandler.close();
-        connection.removeHandler(logFileHandler);
         logFileHandler.shutdown();
     }
 }
