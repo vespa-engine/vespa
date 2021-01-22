adb50113166649a26008db2933616f1bad35bee2
Morten Tokle <morten.tokle@gmail.com>
2021-01-22 11:03:43 +0100

    Revert "Revert "Remove locks in access logging [run-systemtest]""

--- jdisc_http_service/src/main/java/com/yahoo/container/logging/ConnectionLogHandler.java
+++ jdisc_http_service/src/main/java/com/yahoo/container/logging/ConnectionLogHandler.java
@@ -2,33 +2,30 @@
 
 package com.yahoo.container.logging;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
  * @author mortent
  */
 class ConnectionLogHandler {
-    public final Logger connection = Logger.getAnonymousLogger();
     private final LogFileHandler logFileHandler;
 
     public ConnectionLogHandler(String clusterName) {
-        connection.setUseParentHandlers(false);
-
-        LogFormatter lf = new LogFormatter();
-        lf.messageOnly(true);
         logFileHandler = new LogFileHandler(
                 LogFileHandler.Compression.ZSTD,
                 String.format("logs/vespa/qrs/ConnectionLog.%s.%s", clusterName, "%Y%m%d%H%M%S"),
                 "0 60 ...",
-                String.format("ConnectionLog.%s", clusterName),
-                lf);
-        this.logFileHandler.setFormatter(lf);
-        connection.addHandler(this.logFileHandler);
+                String.format("ConnectionLog.%s", clusterName));
+    }
+
+    public void log(String message) {
+        logFileHandler.publish(new LogRecord(Level.INFO, message));
     }
 
     public void shutdown() {
         logFileHandler.close();
-        connection.removeHandler(logFileHandler);
         logFileHandler.shutdown();
     }
 }
