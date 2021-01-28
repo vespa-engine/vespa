b7d3a81397199aa8d165a45f6c864ae791f492a2
Morten Tokle <mortent@verizonmedia.com>
2021-01-28 08:39:13 +0100

    aws parameter store

--- jdisc-cloud-aws/src/main/java/com/yahoo/jdisc/cloud/aws/AwsParameterStore.java
+++ jdisc-cloud-aws/src/main/java/com/yahoo/jdisc/cloud/aws/AwsParameterStore.java
@@ -1,11 +1,13 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-package com.yahoo.vespa.aws;
+package com.yahoo.jdisc.cloud.aws;
 
 import com.yahoo.container.jdisc.secretstore.SecretStore;
 
+/**
+ * @author mortent
+ */
 public class AwsParameterStore implements SecretStore {
-
 
     @Override
     public String getSecret(String key) {
