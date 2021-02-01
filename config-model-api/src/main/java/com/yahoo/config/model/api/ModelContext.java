400f598a456fe32c16e1a266c191211af73e73c2
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-02-01 15:53:33 +0100

    Specify removeAfter

--- config-model-api/src/main/java/com/yahoo/config/model/api/ModelContext.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/ModelContext.java
@@ -64,7 +64,7 @@ public interface ModelContext {
      *  - Remove below method once all config-model versions in hosted production include changes from 1)
      */
     interface FeatureFlags {
-        @ModelFeatureFlag(owners = {"bjorncs", "jonmv"}) default boolean enableAutomaticReindexing() { return true; }
+        @ModelFeatureFlag(owners = {"bjorncs", "jonmv"}, removeAfter = "7.352") default boolean enableAutomaticReindexing() { return true; }
         @ModelFeatureFlag(owners = {"jonmv"}) default double reindexerWindowSizeIncrement() { return 0.2; }
         @ModelFeatureFlag(owners = {"baldersheim"}, comment = "Revisit in May or June 2021") default double defaultTermwiseLimit() { throw new UnsupportedOperationException("TODO specify default value"); }
         @ModelFeatureFlag(owners = {"vekterli"}) default boolean useThreePhaseUpdates() { throw new UnsupportedOperationException("TODO specify default value"); }
