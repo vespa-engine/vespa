62ca0f7cd508269c15b498e46be335607a11ec5e
Harald Musum <musum@verizonmedia.com>
2021-02-09 13:30:44 +0100

    Set dynamicReconfiguration per container in cluster container cluster

--- config-model/src/main/java/com/yahoo/vespa/model/admin/clustercontroller/ClusterControllerContainerCluster.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/clustercontroller/ClusterControllerContainerCluster.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.admin.clustercontroller;
 
 import com.yahoo.config.model.api.Reindexing;
@@ -12,8 +12,7 @@ import com.yahoo.vespa.model.container.ContainerCluster;
  * @author gjoranv
  * @author bjorncs
  */
-public class ClusterControllerContainerCluster extends ContainerCluster<ClusterControllerContainer>
-{
+public class ClusterControllerContainerCluster extends ContainerCluster<ClusterControllerContainer> {
 
     private final ReindexingContext reindexingContext;
 
