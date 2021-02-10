eec74f978511a6e707ee6e2060ef967720e8850b
andreer <andreer@verizonmedia.com>
2021-02-10 11:21:49 +0100

    use applicationId as dimension for s3 log sync flag

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -218,7 +218,7 @@ public class Flags {
             "sync-host-logs-to-s3-bucket", "", List.of("andreer", "valerijf"), "2021-02-10", "2021-03-01",
             "Host-admin should sync host logs to an S3 bucket named by this flag. If left empty, sync is disabled",
             "Takes effect on next run of S3 log sync task in host-admin",
-            ZONE_ID
+            APPLICATION_ID
     );
 
     /** WARNING: public for testing: All flags should be defined in {@link Flags}. */
