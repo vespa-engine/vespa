78049cc83d76315a6350804a7891ba0bbbfd474a
gjoranv <gv@verizonmedia.com>
2021-02-17 15:42:03 +0100

    Add package javadoc from the README file
    .. to prepare for merging processing into container-core.

--- processing/src/main/java/com/yahoo/processing/package-info.java
+++ processing/src/main/java/com/yahoo/processing/package-info.java
@@ -1,4 +1,16 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * Java library for request-response data processing.
+ *
+ * This library defines request-response processing as an operation which
+ * accepts a Request and produces a Response containing Data by executing
+ * a Chain of processing components in a single worker thread using method
+ * calls for chaining, i.e a synchronous processing model.
+ * Data for the Response may optionally be produced asynchronously.
+ *
+ * The processing model can be implemented by subtyping in frameworks defining
+ * a processing model (with a richer, more specific API) for a particular domain.
+ */
 @ExportPackage
 @PublicApi package com.yahoo.processing;
 
