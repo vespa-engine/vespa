a884f7b81f25b596b92aac35792ba1986186abb1
Håkon Hallingstad <hakon.hallingstad@gmail.com>
2021-02-26 15:21:48 +0100

    Revert "Enable group suspension by default, attempt 2 [run-systemtest]"

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -200,7 +200,7 @@ public class Flags {
             APPLICATION_ID);
 
     public static final UnboundBooleanFlag GROUP_SUSPENSION = defineFeatureFlag(
-            "group-suspension", true,
+            "group-suspension", false,
             List.of("hakon"), "2021-01-22", "2021-03-22",
             "Allow all content nodes in a hierarchical group to suspend at the same time",
             "Takes effect on the next suspension request to the Orchestrator.",
