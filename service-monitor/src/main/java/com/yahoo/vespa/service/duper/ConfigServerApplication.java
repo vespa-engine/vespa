e2c3f926e38d21d220aa6258a206ae9e4914a8a3
Martin Polden <mpolden@mpolden.no>
2021-03-02 12:02:05 +0100

    Simplify

--- service-monitor/src/main/java/com/yahoo/vespa/service/duper/ConfigServerApplication.java
+++ service-monitor/src/main/java/com/yahoo/vespa/service/duper/ConfigServerApplication.java
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.service.duper;
 
-import com.yahoo.component.Version;
 import com.yahoo.config.provision.ClusterSpec;
 import com.yahoo.config.provision.NodeType;
 import com.yahoo.vespa.applicationmodel.ServiceType;
@@ -15,15 +14,6 @@ public class ConfigServerApplication extends ConfigServerLikeApplication {
 
     public ConfigServerApplication() {
         super("zone-config-servers", NodeType.config, ClusterSpec.Type.admin, ServiceType.CONFIG_SERVER);
-    }
-
-    @Override
-    public ClusterSpec getClusterSpecWithVersion(Version version) {
-        return ClusterSpec.request(getClusterSpecType(), getClusterSpecId())
-                          .vespaVersion(version)
-                          .stateful(true)
-                          .exclusive(true)
-                          .build();
     }
 
 }
--- service-monitor/src/main/java/com/yahoo/vespa/service/duper/ControllerApplication.java
+++ service-monitor/src/main/java/com/yahoo/vespa/service/duper/ControllerApplication.java
@@ -1,7 +1,6 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.service.duper;
 
-import com.yahoo.component.Version;
 import com.yahoo.config.provision.ClusterSpec;
 import com.yahoo.config.provision.NodeType;
 import com.yahoo.vespa.applicationmodel.ServiceType;
@@ -15,15 +14,6 @@ public class ControllerApplication extends ConfigServerLikeApplication {
 
     public ControllerApplication() {
         super("controller", NodeType.controller, ClusterSpec.Type.container, ServiceType.CONTROLLER);
-    }
-
-    @Override
-    public ClusterSpec getClusterSpecWithVersion(Version version) {
-        return ClusterSpec.request(getClusterSpecType(), getClusterSpecId())
-                          .vespaVersion(version)
-                          .stateful(true)
-                          .exclusive(true)
-                          .build();
     }
 
 }
