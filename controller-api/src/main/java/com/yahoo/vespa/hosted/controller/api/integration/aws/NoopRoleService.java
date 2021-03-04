09f55fcf4212ba16dc1d13ef5c3ed4a8d92c4826
Ola Aunrønning <olaa@verizonmedia.com>
2021-03-04 17:07:54 +0100

    Delete tenant policy requires role

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/aws/NoopRoleService.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/aws/NoopRoleService.java
@@ -1,7 +1,6 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.api.integration.aws;
 
-import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.TenantName;
 
 import java.util.Collections;
@@ -27,7 +26,7 @@ public class NoopRoleService implements RoleService {
     }
 
     @Override
-    public void deleteTenantPolicy(TenantName tenant, String policyName) { }
+    public void deleteTenantPolicy(TenantName tenant, String policyName, String role) { }
 
     @Override
     public void maintainRoles(List<TenantName> tenants) { }
