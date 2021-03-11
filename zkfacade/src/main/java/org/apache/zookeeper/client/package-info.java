12c4321277085553a612752185518009effec65b
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-03-11 15:50:55 +0100

    Export org.apache.zookeeper.client from zkfacade

---
+++ zkfacade/src/main/java/org/apache/zookeeper/client/package-info.java
@@ -0,0 +1,10 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * Required for export of {@link org.apache.zookeeper.client.ZKClientConfig}.
+ *
+ * @author bjorncs
+ */
+@ExportPackage
+package org.apache.zookeeper.client;
+
+import com.yahoo.osgi.annotation.ExportPackage;
