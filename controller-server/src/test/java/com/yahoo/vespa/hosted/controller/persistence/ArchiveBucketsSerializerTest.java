20990e807ef0432d50d223eb7c0cc68290e16284
andreer <andreer@verizonmedia.com>
2021-03-25 12:41:38 +0100

    stabilize test

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/persistence/ArchiveBucketsSerializerTest.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/persistence/ArchiveBucketsSerializerTest.java
@@ -4,6 +4,7 @@ import com.yahoo.vespa.hosted.controller.api.identifiers.TenantId;
 import com.yahoo.vespa.hosted.controller.api.integration.archive.ArchiveBucket;
 import org.junit.Test;
 
+import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -13,11 +14,15 @@ public class ArchiveBucketsSerializerTest {
 
     @Test
     public void serdes() {
-        ArchiveBucket archiveBucket1 = new ArchiveBucket("bucket1Arn", Optional.of("key1Arn"), Set.of(new TenantId("vespa"), new TenantId("kraune")));
-        ArchiveBucket archiveBucket2 = new ArchiveBucket("bucket2Arn", Optional.empty(), Set.of());
-        Set<ArchiveBucket> testBuckets = Set.of(archiveBucket1, archiveBucket2);
+        var testTenants = new LinkedHashSet<TenantId>();
+        testTenants.add(new TenantId("tenant1"));
+        testTenants.add(new TenantId("tenant2"));
 
-        String zkData = "{\"buckets\":[{\"bucketArn\":\"bucket1Arn\",\"keyArn\":\"key1Arn\",\"tenantIds\":[\"vespa\",\"kraune\"]},{\"bucketArn\":\"bucket2Arn\",\"tenantIds\":[]}]}";
+        var testBuckets = new LinkedHashSet<ArchiveBucket>();
+        testBuckets.add(new ArchiveBucket("bucket1Arn", Optional.of("key1Arn"), testTenants));
+        testBuckets.add(new ArchiveBucket("bucket2Arn", Optional.empty(), Set.of()));
+
+        String zkData = "{\"buckets\":[{\"bucketArn\":\"bucket1Arn\",\"keyArn\":\"key1Arn\",\"tenantIds\":[\"tenant1\",\"tenant2\"]},{\"bucketArn\":\"bucket2Arn\",\"tenantIds\":[]}]}";
 
         assertEquals(ArchiveBucketsSerializer.toSlime(testBuckets).toString(), zkData);
         assertEquals(ArchiveBucketsSerializer.fromJsonString(zkData), testBuckets);
