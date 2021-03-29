8a2d6c1c4110ddf508f3a467eeb2ab031998591a
gjoranv <gv@verizonmedia.com>
2021-03-29 21:35:59 +0200

    Remove jdisc_messagebus_service and messagebus-disc modules.
    - They have been merged into container-messagebus

--- jdisc_messagebus_service/src/main/java/com/yahoo/messagebus/shared/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/**
- * Not a public API, exported for use in internal components.
- */
-@ExportPackage
-package com.yahoo.messagebus.shared;
-
-import com.yahoo.osgi.annotation.ExportPackage;
