123f805cb9c042122f9577e7903f3a861d9160e1
Tor Brede Vekterli <vekterli@verizonmedia.com>
2021-04-06 13:05:49 +0200

    Bump 3-phase update feature flag expiry date

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -104,7 +104,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag USE_THREE_PHASE_UPDATES = defineFeatureFlag(
             "use-three-phase-updates", false,
-            List.of("vekterli"), "2020-12-02", "2021-04-01",
+            List.of("vekterli"), "2020-12-02", "2021-06-01",
             "Whether to enable the use of three-phase updates when bucket replicas are out of sync.",
             "Takes effect at redeployment",
             ZONE_ID, APPLICATION_ID);
