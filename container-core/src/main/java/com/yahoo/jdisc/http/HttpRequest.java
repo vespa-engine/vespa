2dd8e481eac655f6b9dfe9adab3707d184ba90d9
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-04-09 17:55:39 +0200

    Add enum variant for HTTP/2.0

--- container-core/src/main/java/com/yahoo/jdisc/http/HttpRequest.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/HttpRequest.java
@@ -47,7 +47,8 @@ public class HttpRequest extends Request implements ServletOrJdiscHttpRequest {
 
     public enum Version {
         HTTP_1_0("HTTP/1.0"),
-        HTTP_1_1("HTTP/1.1");
+        HTTP_1_1("HTTP/1.1"),
+        HTTP_2_0("HTTP/2.0");
 
         private final String str;
 
