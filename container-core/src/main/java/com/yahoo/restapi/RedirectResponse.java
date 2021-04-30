5152b13d01819a3baec82728df1adb2cec3c7d2d
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-04-30 09:56:32 +0200

    Add redirect response

---
+++ container-core/src/main/java/com/yahoo/restapi/RedirectResponse.java
@@ -0,0 +1,27 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.restapi;
+
+import com.yahoo.jdisc.Response;
+
+import java.net.URI;
+
+/**
+ * A HTTP redirect response
+ *
+ * @author bjorncs
+ */
+public class RedirectResponse extends MessageResponse {
+
+    private RedirectResponse(int statusCode, URI location) {
+        super(statusCode, "Moved to " + location.toString());
+        headers().add("Location", location.toString());
+    }
+
+    public static RedirectResponse found(URI location) {
+        return new RedirectResponse(Response.Status.FOUND, location);
+    }
+
+    public static RedirectResponse movedPermanently(URI location) {
+        return new RedirectResponse(Response.Status.MOVED_PERMANENTLY, location);
+    }
+}
