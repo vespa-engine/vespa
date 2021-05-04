9bb76fccc1c128920650bd5a55f4ee4a2af554e7
Jon Bratseth <bratseth@gmail.com>
2021-05-04 22:55:28 +0200

    Revert "Revert "Bratseth/special tokens""

--- config-model/src/main/java/com/yahoo/vespa/model/container/docproc/DocprocChain.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/docproc/DocprocChain.java
@@ -23,8 +23,9 @@ public class DocprocChain extends Chain<DocumentProcessor> {
     }
 
     /**
-     * The field name schema map that applies to this whole chain
-     * @return doctype,from → to
+     * The field name schema map that applies to this whole chain.
+     *
+     * @return doctype, from → to
      */
     public Map<Pair<String,String>,String> fieldNameSchemaMap() {
         return fieldNameSchemaMap;
---
+++ linguistics/src/test/java/com/yahoo/language/process/SpecialTokensTestCase.java
@@ -0,0 +1,40 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.language.process;
+
+import com.yahoo.vespa.configdefinition.SpecialtokensConfig;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author bratseth
+ */
+public class SpecialTokensTestCase {
+
+    @Test
+    public void testSpecialTokensConfig() {
+        var builder = new SpecialtokensConfig.Builder();
+        var tokenBuilder = new SpecialtokensConfig.Tokenlist.Builder();
+        tokenBuilder.name("default");
+
+        var tokenListBuilder1 = new SpecialtokensConfig.Tokenlist.Tokens.Builder();
+        tokenListBuilder1.token("c++");
+        tokenListBuilder1.replace("cpp");
+        tokenBuilder.tokens(tokenListBuilder1);
+
+        var tokenListBuilder2 = new SpecialtokensConfig.Tokenlist.Tokens.Builder();
+        tokenListBuilder2.token("...");
+        tokenBuilder.tokens(tokenListBuilder2);
+
+        builder.tokenlist(tokenBuilder);
+
+        var registry = new SpecialTokenRegistry(builder.build());
+
+        var defaultTokens = registry.getSpecialTokens("default");
+        assertEquals("default", defaultTokens.name());
+        assertEquals(2, defaultTokens.asMap().size());
+        assertEquals("cpp", defaultTokens.asMap().get("c++"));
+        assertEquals("...", defaultTokens.asMap().get("..."));
+    }
+
+}
