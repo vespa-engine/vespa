4f23aee7fe1df916e8b7fd6cf513134d5bd54f7e
Jon Marius Venstad <venstad@gmail.com>
2021-05-20 18:48:48 +0200

    Make AbstractService.remove public

--- config-model/src/main/java/com/yahoo/vespa/model/AbstractService.java
+++ config-model/src/main/java/com/yahoo/vespa/model/AbstractService.java
@@ -109,7 +109,7 @@ public abstract class AbstractService extends AbstractConfigProducer<AbstractCon
     }
 
     @Override
-    protected void remove() {
+    public void remove() {
         super.remove();
         if (hostResource != null)
             hostResource.deallocateService(this);
