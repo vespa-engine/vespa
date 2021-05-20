77fb1ee37fae8118dae52f7993fc885c49654414
Håvard Pettersen <havardpe@oath.com>
2021-05-20 10:37:24 +0000

    write empty config file for ranking expressions
    as part of config snapshot

--- searchcore/src/vespa/searchcore/proton/server/fileconfigmanager.cpp
+++ searchcore/src/vespa/searchcore/proton/server/fileconfigmanager.cpp
@@ -327,6 +327,7 @@ FileConfigManager::loadConfig(const DocumentDBConfig &currentSnapshot,
     config::DirSpec spec(snapDir);
 
     addEmptyFile(snapDir, "ranking-constants.cfg");
+    addEmptyFile(snapDir, "ranking-expressions.cfg");
     addEmptyFile(snapDir, "onnx-models.cfg");
     addEmptyFile(snapDir, "imported-fields.cfg");
 
