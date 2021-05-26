3f610821578899c8f3a7d60d2821157cc9c172c4
Harald Musum <musum@verizonmedia.com>
2021-05-26 11:25:14 +0200

    Cleanup of some ZooKeeper-related components
    Do not retry setting up ZooKeeperClient, that is already done in
    ConfigCurator
    Some other minor cleanup

--- configserver/src/main/java/com/yahoo/vespa/config/server/deploy/ZooKeeperDeployer.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/deploy/ZooKeeperDeployer.java
@@ -1,4 +1,4 @@
-// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.config.server.deploy;
 
 import com.yahoo.config.application.api.ApplicationPackage;
@@ -11,7 +11,6 @@ import java.util.Map;
 
 /**
  * Interface for initializing zookeeper and deploying an application package to zookeeper.
- * Initialize must be called before each deploy.
  *
  * @author Ulf Lilleengen
  */
@@ -24,16 +23,16 @@ public class ZooKeeperDeployer {
     }
 
     /**
-     * Deploys an application package to zookeeper. initialize() must be called before calling this method.
+     * Deploys an application package to zookeeper
      *
      * @param applicationPackage the application package to persist.
      * @param fileRegistryMap the file registries to persist.
-     * @param allocatedHosts the provisioning info to persist.
+     * @param allocatedHosts the allocated hosts to persist.
      * @throws IOException if deploying fails
      */
     public void deploy(ApplicationPackage applicationPackage, Map<Version, FileRegistry> fileRegistryMap, 
                        AllocatedHosts allocatedHosts) throws IOException {
-        zooKeeperClient.setupZooKeeper();
+        zooKeeperClient.initialize();
         zooKeeperClient.write(applicationPackage);
         zooKeeperClient.write(fileRegistryMap);
         zooKeeperClient.write(allocatedHosts);
