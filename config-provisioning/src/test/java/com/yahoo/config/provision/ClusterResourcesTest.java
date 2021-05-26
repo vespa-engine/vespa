291391e306c04aeeb52d7b0ef58f64dcd8f29d4f
Jon Bratseth <bratseth@oath.com>
2021-05-26 18:03:47 +0200

    Update config-provisioning/src/test/java/com/yahoo/config/provision/ClusterResourcesTest.java
    Co-authored-by: Valerij Fredriksen <freva@users.noreply.github.com>

--- config-provisioning/src/test/java/com/yahoo/config/provision/ClusterResourcesTest.java
+++ config-provisioning/src/test/java/com/yahoo/config/provision/ClusterResourcesTest.java
@@ -14,9 +14,6 @@ public class ClusterResourcesTest {
     public void testCost() {
         ClusterResources r1 = new ClusterResources(3, 1, new NodeResources(2,  8, 50, 1));
         ClusterResources r2 = new ClusterResources(3, 1, new NodeResources(2, 16, 50, 1));
-        System.out.println(r1.cost()*24*30);
-        System.out.println(r2.cost()*24*30);
-        System.out.println((r1.cost()*24*30 + r2.cost()*24*30) * 1.05);
         assertEquals(1.818, r1.cost() + r2.cost(), 0.01);
     }
 
