87be3ceb55a7e767a5eb7d24a8ff6f53add30033
Jon Marius Venstad <venstad@gmail.com>
2021-06-02 09:04:31 +0200

    Rename lock to monitor, update some doc

--- vespa-feed-client/src/main/java/ai/vespa/feed/client/HttpRequestStrategy.java
+++ vespa-feed-client/src/main/java/ai/vespa/feed/client/HttpRequestStrategy.java
@@ -16,15 +16,15 @@ import java.util.logging.Logger;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.util.logging.Level.FINE;
 import static java.util.logging.Level.INFO;
 
 /**
  * Controls request execution and retries:
  * <ul>
- *     <li>Retry all IO exceptions; however</li>
- *     <li>abort everything if more than 10% of requests result in an exception for some time.</li>
- *     <li>Whenever throttled, limit inflight to one less than the current; and</li>
- *     <li>on every successful response, increase inflight limit by 0.1.</li>
+ *     <li>Whenever throttled (429, 503), set target inflight to 0.9 * current, and retry over a different connection;</li>
+ *     <li>retry other transient errors (500, 502 and IOException) a specified number of times, for specified operation types;</li>
+ *     <li>and on every successful response, increase target inflight by 0.1.</li>
  * </ul>
  *
  * @author jonmv
@@ -34,7 +34,7 @@ class HttpRequestStrategy implements RequestStrategy {
     private static final Logger log = Logger.getLogger(HttpRequestStrategy.class.getName());
 
     private final Map<DocumentId, CompletableFuture<Void>> inflightById = new HashMap<>();
-    private final Object lock = new Object();
+    private final Object monitor = new Object();
     private final Clock clock;
     private final RetryStrategy wrapped;
     private final long maxInflight;
@@ -82,7 +82,7 @@ class HttpRequestStrategy implements RequestStrategy {
 
     void success() {
         Instant now = clock.instant();
-        synchronized (lock) {
+        synchronized (monitor) {
             ++consecutiveSuccesses;
             lastSuccess = now;
             targetInflight = min(targetInflight + 0.1, maxInflight);
@@ -91,7 +91,7 @@ class HttpRequestStrategy implements RequestStrategy {
 
     void failure() {
         Instant threshold = clock.instant().minusSeconds(300);
-        synchronized (lock) {
+        synchronized (monitor) {
             consecutiveSuccesses = 0;
             if (lastSuccess.isBefore(threshold))
                 failed = true;
@@ -105,15 +105,18 @@ class HttpRequestStrategy implements RequestStrategy {
             return false;
         }
 
-        log.log(INFO, () -> "Status code " + response.getCode() + " (" + response.getBodyText() + ") on attempt " + attempt +
-                            " at " + request + ", " + consecutiveSuccesses + " successes since last error");
-
         if (response.getCode() == 429 || response.getCode() == 503) { // Throttling; reduce target inflight.
-            synchronized (lock) {
+            synchronized (monitor) {
                 targetInflight = max(inflight * 0.9, minInflight);
             }
+            log.log(FINE, () -> "Status code " + response.getCode() + " (" + response.getBodyText() + ") on attempt " + attempt +
+                                " at " + request + ", " + consecutiveSuccesses + " successes since last error");
+
             return true;
         }
+
+        log.log(INFO, () -> "Status code " + response.getCode() + " (" + response.getBodyText() + ") on attempt " + attempt +
+                            " at " + request + ", " + consecutiveSuccesses + " successes since last error");
 
         failure();
         if (response.getCode() != 500 && response.getCode() != 502)
@@ -126,7 +129,7 @@ class HttpRequestStrategy implements RequestStrategy {
     private void acquireSlot() {
         try {
             while (inflight >= targetInflight)
-                lock.wait();
+                monitor.wait();
 
             ++inflight;
         }
@@ -138,12 +141,12 @@ class HttpRequestStrategy implements RequestStrategy {
     // Must hold lock.
     private void releaseSlot() {
         for (long i = --inflight; i < targetInflight; i++)
-            lock.notify();
+            monitor.notify();
     }
 
     @Override
     public boolean hasFailed() {
-        synchronized (lock) {
+        synchronized (monitor) {
             return failed;
         }
     }
@@ -157,7 +160,7 @@ class HttpRequestStrategy implements RequestStrategy {
 
         // Get the previous inflight operation for this doc-id, or acquire a send slot.
         CompletableFuture<Void> previous;
-        synchronized (lock) {
+        synchronized (monitor) {
             previous = inflightById.put(documentId, blocker);
             if (previous == null)
                 acquireSlot();
@@ -187,7 +190,7 @@ class HttpRequestStrategy implements RequestStrategy {
 
             // ... or accept the outcome and mark the operation as complete.
             CompletableFuture<Void> current;
-            synchronized (lock) {
+            synchronized (monitor) {
                 current = inflightById.get(documentId);
                 if (current == blocker) {   // Release slot and clear map if no other operations enqueued for this doc-id ...
                     releaseSlot();
