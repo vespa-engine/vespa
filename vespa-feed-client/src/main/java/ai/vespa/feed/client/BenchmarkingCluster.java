46c91ff61b279ef89cf41a9b28f921c42c9de464
Jon Marius Venstad <venstad@gmail.com>
2021-06-09 14:03:41 +0200

    Add BenchmarkingCluster to the mix

---
+++ vespa-feed-client/src/main/java/ai/vespa/feed/client/BenchmarkingCluster.java
@@ -0,0 +1,105 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package ai.vespa.feed.client;
+
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.util.Objects.requireNonNull;
+
+public class BenchmarkingCluster implements Cluster {
+
+    private final Cluster delegate;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor(runnable -> {
+        Thread thread = new Thread(runnable, "cluster-stats-collector");
+        thread.setDaemon(true);
+        return thread;
+    });
+
+    private final AtomicLong requests = new AtomicLong();
+    private long results = 0;
+    private long responses = 0;
+    private final long[] responsesByCode = new long[600];
+    private long exceptions = 0;
+    private long totalLatencyMillis = 0;
+    private long minLatencyMillis = 0;
+    private long maxLatencyMillis = 0;
+    private long bytesSent = 0;
+    private long bytesReceived = 0;
+
+    public BenchmarkingCluster(Cluster delegate) {
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public void dispatch(SimpleHttpRequest request, CompletableFuture<SimpleHttpResponse> vessel) {
+        requests.incrementAndGet();
+        long startMillis = System.currentTimeMillis();
+        delegate.dispatch(request, vessel);
+        vessel.whenCompleteAsync((response, thrown) -> {
+                                     results++;
+                                     if (thrown == null) {
+                                         responses++;
+                                         responsesByCode[response.getCode()]++;
+                                         long latency = System.currentTimeMillis() - startMillis;
+                                         totalLatencyMillis += latency;
+                                         minLatencyMillis = Math.min(minLatencyMillis, latency);
+                                         maxLatencyMillis = Math.max(maxLatencyMillis, latency);
+                                         bytesSent += request.getBodyBytes() == null ? 0 : request.getBodyBytes().length;
+                                         bytesReceived += response.getBodyBytes() == null ? 0 : response.getBodyBytes().length;
+                                     }
+                                     else
+                                         exceptions++;
+                                 },
+                                 executor);
+    }
+
+    @Override
+    public Stats stats() {
+        try {
+            try {
+                return executor.submit(this::getStats).get();
+            }
+            catch (RejectedExecutionException ignored) {
+                executor.awaitTermination(10, TimeUnit.SECONDS);
+                return getStats();
+            }
+        }
+        catch (InterruptedException | ExecutionException ignored) {
+            throw new RuntimeException(ignored);
+        }
+    }
+
+    private Stats getStats() {
+        Map<Integer, Long> responses = new HashMap<>();
+        for (int code = 0; code < responsesByCode.length; code++)
+            if (responsesByCode[code] > 0)
+                responses.put(code, responsesByCode[code]);
+
+        return new Stats(requests.get(),
+                         responses,
+                         exceptions,
+                         requests.get() - results,
+                         totalLatencyMillis / this.responses,
+                         minLatencyMillis,
+                         maxLatencyMillis,
+                         bytesSent,
+                         bytesReceived);
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+        executor.shutdown();
+    }
+
+}
--- vespa-feed-client/src/main/java/ai/vespa/feed/client/Cluster.java
+++ vespa-feed-client/src/main/java/ai/vespa/feed/client/Cluster.java
@@ -5,10 +5,12 @@ import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 
 import java.io.Closeable;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 /**
- * Allows dispatch to a Vespa cluster. {@link #dispatch} should be called by a single thread, i.e., it is not thread-safe.
+ * Allows dispatch to a Vespa cluster.
  */
 interface Cluster extends Closeable {
 
@@ -17,5 +19,90 @@ interface Cluster extends Closeable {
 
     @Override
     default void close() { }
+
+    default Stats stats() { return new Stats(0, Collections.emptyMap(), 0, 0, 0, 0, 0, 0, 0); }
+
+    class Stats {
+
+        private final long requests;
+        private final Map<Integer, Long> responsesByCode;
+        private final long inflight;
+        private final long exceptions;
+        private final long averageLatencyMillis;
+        private final long minLatencyMillis;
+        private final long maxLatencyMillis;
+        private final long bytesSent;
+        private final long bytesReceived;
+
+        public Stats(long requests, Map<Integer, Long> responsesByCode, long exceptions, long inflight,
+                     long averageLatencyMillis, long minLatencyMillis, long maxLatencyMillis,
+                     long bytesSent, long bytesReceived) {
+            this.requests = requests;
+            this.responsesByCode = responsesByCode;
+            this.exceptions = exceptions;
+            this.inflight = inflight;
+            this.averageLatencyMillis = averageLatencyMillis;
+            this.minLatencyMillis = minLatencyMillis;
+            this.maxLatencyMillis = maxLatencyMillis;
+            this.bytesSent = bytesSent;
+            this.bytesReceived = bytesReceived;
+        }
+
+        public long requests() {
+            return requests;
+        }
+
+        public long responses() {
+            return requests - inflight;
+        }
+
+        public Map<Integer, Long> responsesByCode() {
+            return responsesByCode;
+        }
+
+        public long exceptions() {
+            return exceptions;
+        }
+
+        public long inflight() {
+            return inflight;
+        }
+
+        public long averageLatencyMillis() {
+            return averageLatencyMillis;
+        }
+
+        public long minLatencyMillis() {
+            return minLatencyMillis;
+        }
+
+        public long maxLatencyMillis() {
+            return maxLatencyMillis;
+        }
+
+        public long bytesSent() {
+            return bytesSent;
+        }
+
+        public long bytesReceived() {
+            return bytesReceived;
+        }
+
+        @Override
+        public String toString() {
+            return "Stats{" +
+                   "requests=" + requests +
+                   ", responsesByCode=" + responsesByCode +
+                   ", exceptions=" + exceptions +
+                   ", inflight=" + inflight +
+                   ", averageLatencyMillis=" + averageLatencyMillis +
+                   ", minLatencyMillis=" + minLatencyMillis +
+                   ", maxLatencyMillis=" + maxLatencyMillis +
+                   ", bytesSent=" + bytesSent +
+                   ", bytesReceived=" + bytesReceived +
+                   '}';
+        }
+
+    }
 
 }
