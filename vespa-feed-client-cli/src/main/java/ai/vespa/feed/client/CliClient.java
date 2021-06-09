e68933c235f1a43c5f8542c6e41838dbf2e9cd27
Bjørn Christian Seime <bjorn.christian@seime.no>
2021-06-09 13:50:23 +0200

    Revert "Bjorncs/json feeder"

--- vespa-feed-client-cli/src/main/java/ai/vespa/feed/client/CliClient.java
+++ vespa-feed-client-cli/src/main/java/ai/vespa/feed/client/CliClient.java
@@ -55,11 +55,11 @@ public class CliClient {
                 return 0;
             }
             try (InputStream in = createFeedInputStream(cliArgs);
-                 JsonFeeder feeder = createJsonFeeder(cliArgs)) {
+                 JsonStreamFeeder feeder = createJsonFeeder(cliArgs)) {
                 if (cliArgs.benchmarkModeEnabled()) {
                     printBenchmarkResult(feeder.benchmark(in));
                 } else {
-                    feeder.feedMany(in);
+                    feeder.feed(in);
                 }
             }
             return 0;
@@ -85,9 +85,9 @@ public class CliClient {
         return builder.build();
     }
 
-    private static JsonFeeder createJsonFeeder(CliArguments cliArgs) throws CliArguments.CliArgumentsException, IOException {
+    private static JsonStreamFeeder createJsonFeeder(CliArguments cliArgs) throws CliArguments.CliArgumentsException, IOException {
         FeedClient feedClient = createFeedClient(cliArgs);
-        JsonFeeder.Builder builder = JsonFeeder.builder(feedClient);
+        JsonStreamFeeder.Builder builder = JsonStreamFeeder.builder(feedClient);
         cliArgs.timeout().ifPresent(builder::withTimeout);
         cliArgs.route().ifPresent(builder::withRoute);
         cliArgs.traceLevel().ifPresent(builder::withTracelevel);
@@ -98,7 +98,7 @@ public class CliClient {
         return cliArgs.readFeedFromStandardInput() ? systemIn : Files.newInputStream(cliArgs.inputFile().get());
     }
 
-    private void printBenchmarkResult(JsonFeeder.BenchmarkResult result) throws IOException {
+    private void printBenchmarkResult(JsonStreamFeeder.BenchmarkResult result) throws IOException {
         JsonFactory factory = new JsonFactory();
         try (JsonGenerator generator = factory.createGenerator(systemOut).useDefaultPrettyPrinter()) {
             generator.writeStartObject();
