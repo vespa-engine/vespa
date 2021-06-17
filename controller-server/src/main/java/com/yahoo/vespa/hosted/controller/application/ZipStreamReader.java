4dbb2edfbb79242b745fd3d587c467f000f689f0
Martin Polden <mpolden@mpolden.no>
2021-06-17 16:36:25 +0200

    Less Guava

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/application/ZipStreamReader.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/application/ZipStreamReader.java
@@ -1,15 +1,14 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.application;
 
-import com.google.common.collect.ImmutableList;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
@@ -21,19 +20,19 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipStreamReader {
 
-    private final ImmutableList<ZipEntryWithContent> entries;
+    private final List<ZipEntryWithContent> entries = new ArrayList<>();
     private final int maxEntrySizeInBytes;
 
     public ZipStreamReader(InputStream input, Predicate<String> entryNameMatcher, int maxEntrySizeInBytes) {
         this.maxEntrySizeInBytes = maxEntrySizeInBytes;
         try (ZipInputStream zipInput = new ZipInputStream(input)) {
-            ImmutableList.Builder<ZipEntryWithContent> builder = new ImmutableList.Builder<>();
             ZipEntry zipEntry;
+
             while (null != (zipEntry = zipInput.getNextEntry())) {
                 if (!entryNameMatcher.test(requireName(zipEntry.getName()))) continue;
-                builder.add(new ZipEntryWithContent(zipEntry, readContent(zipInput)));
+                entries.add(new ZipEntryWithContent(zipEntry, readContent(zipInput)));
             }
-            entries = builder.build();
+
         } catch (IOException e) {
             throw new UncheckedIOException("IO error reading zip content", e);
         }
@@ -79,10 +78,10 @@ public class ZipStreamReader {
         }
     }
 
-    public List<ZipEntryWithContent> entries() { return entries; }
+    public List<ZipEntryWithContent> entries() { return Collections.unmodifiableList(entries); }
 
     private static String requireName(String name) {
-        if (Arrays.asList(name.split("/")).contains("..") ||
+        if (List.of(name.split("/")).contains("..") ||
             !trimTrailingSlash(name).equals(Path.of(name).normalize().toString())) {
             throw new IllegalArgumentException("Unexpected non-normalized path found in zip content: '" + name + "'");
         }
