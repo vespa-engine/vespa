5ed747aa6a21ee462804ff2440d7ab6e7ad4cafb
Lester Solbakken <lesters@oath.com>
2021-06-21 12:31:54 +0200

    Remove unneccesary import

--- searchlib/src/main/java/com/yahoo/searchlib/rankingexpression/rule/Function.java
+++ searchlib/src/main/java/com/yahoo/searchlib/rankingexpression/rule/Function.java
@@ -1,7 +1,6 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.searchlib.rankingexpression.rule;
 
-import com.yahoo.tensor.Tensor;
 import com.yahoo.tensor.functions.ScalarFunctions;
 
 import java.io.Serializable;
