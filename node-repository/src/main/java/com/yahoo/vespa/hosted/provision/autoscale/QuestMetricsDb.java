25bbbcd9a1676f4031cdec7b8fa3dcf244dca3b5
Jon Bratseth <bratseth@gmail.com>
2021-06-25 23:02:29 +0200

    Create root dir before logging config

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/autoscale/QuestMetricsDb.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/autoscale/QuestMetricsDb.java
@@ -72,12 +72,13 @@ public class QuestMetricsDb extends AbstractComponent implements MetricsDb {
         if (dataDir.startsWith(Defaults.getDefaults().vespaHome())
             && ! new File(Defaults.getDefaults().vespaHome()).exists())
             dataDir = "data"; // We're injected, but not on a node with Vespa installed
-        this.dataDir = dataDir;
-
+        IOUtils.createDirectory(dataDir);
         // silence Questdb's custom logging system
         IOUtils.writeFile(new File(dataDir, "quest-log.conf"), new byte[0]);
         System.setProperty("out", dataDir + "/quest-log.conf");
         CairoConfiguration configuration = new DefaultCairoConfiguration(dataDir);
+
+        this.dataDir = dataDir;
         engine = new CairoEngine(configuration);
         sqlCompiler = ThreadLocal.withInitial(() -> new SqlCompiler(engine));
         nodeTable = new Table(dataDir, "metrics", clock);
