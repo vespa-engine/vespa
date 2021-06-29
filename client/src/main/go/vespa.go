66e3af8f52b37a9e2822761eae1f1e4a45ec37f6
Jon Bratseth <bratseth@gmail.com>
2021-06-29 21:40:51 +0200

    Http reequest

--- client/src/main/go/vespa.go
+++ client/src/main/go/vespa.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello world!")
+    resp, err := http.Get("http://localhost:19071/ApplicationStatus")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("Response status: ", resp.Status)
+    scanner := bufio.NewScanner(resp.Body)
+    for i := 0; scanner.Scan() && i < 5; i++ {
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        panic(err)
+    }
 }
