b55147670eba8a1b8c7a0c75077d2f865bc50499
Harald Musum <musum@verizonmedia.com>
2021-07-14 13:01:17 +0200

    Revert "Do not use serializable NodeRepositoryNode internally in controller"

--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/NodeRepositoryMock.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/NodeRepositoryMock.java
@@ -1,6 +1,7 @@
 // Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.integration;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.yahoo.collections.Pair;
 import com.yahoo.component.Version;
 import com.yahoo.config.provision.ApplicationId;
@@ -17,11 +18,13 @@ import com.yahoo.vespa.hosted.controller.api.integration.configserver.Node;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.NodeRepoStats;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.NodeRepository;
 import com.yahoo.vespa.hosted.controller.api.integration.configserver.TargetVersions;
+import com.yahoo.vespa.hosted.controller.api.integration.noderepository.NodeList;
 import com.yahoo.vespa.hosted.controller.api.integration.noderepository.NodeRepositoryNode;
 import com.yahoo.vespa.hosted.controller.api.integration.noderepository.NodeState;
 
 import java.net.URI;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -30,6 +33,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
@@ -46,12 +50,115 @@ public class NodeRepositoryMock implements NodeRepository {
     private final Map<DeploymentId, Pair<Double, Double>> trafficFractions = new HashMap<>();
     private final Map<ZoneId, Map<TenantName, URI>> archiveUris = new HashMap<>();
 
+    // A separate/alternative list of NodeRepositoryNode nodes.
+    // Methods operating with Node and NodeRepositoryNode lives separate lives.
+    private final Map<ZoneId, List<NodeRepositoryNode>> nodeRepoNodes = new HashMap<>();
+
     private boolean allowPatching = false;
     private boolean hasSpareCapacity = false;
 
+    /** Add or update given nodes in zone */
+    public void putNodes(ZoneId zone, List<Node> nodes) {
+        Map<HostName, Node> zoneNodes = nodeRepository.computeIfAbsent(zone, (k) -> new HashMap<>());
+        for (var node : nodes) {
+            zoneNodes.put(node.hostname(), node);
+        }
+    }
+
+    public void putNode(ZoneId zone, Node node) {
+        nodeRepository.computeIfAbsent(zone, (k) -> new HashMap<>()).put(node.hostname(), node);
+    }
+
+    public void putApplication(ZoneId zone, Application application) {
+        applications.putIfAbsent(zone, new HashMap<>());
+        applications.get(zone).put(application.id(), application);
+    }
+
+    @Override
+    public NodeRepoStats getStats(ZoneId zone) {
+        List<ApplicationStats> applicationStats =
+            applications.containsKey(zone)
+                ? applications.get(zone).keySet().stream()
+                              .map(id -> new ApplicationStats(id, Load.zero(), 0, 0))
+                              .collect(Collectors.toList())
+               : List.of();
+
+        return new NodeRepoStats(Load.zero(), Load.zero(), applicationStats);
+    }
+
+    public Pair<Double, Double> getTrafficFraction(ApplicationId application, ZoneId zone) {
+        return trafficFractions.get(new DeploymentId(application, zone));
+    }
+
+    /** Add or update given node in zone */
+    public void putNodes(ZoneId zone, Node node) {
+        putNodes(zone, Collections.singletonList(node));
+    }
+
+    /** Remove given nodes from zone */
+    public void removeNodes(ZoneId zone, List<Node> nodes) {
+        nodes.forEach(node -> nodeRepository.get(zone).remove(node.hostname()));
+    }
+
+    /** Remove all nodes in all zones */
+    public void clear() {
+        nodeRepository.clear();
+        nodeRepoNodes.clear();
+    }
+
+    /** Replace nodes in zone with given nodes */
+    public void setNodes(ZoneId zone, List<Node> nodes) {
+        nodeRepository.put(zone, nodes.stream().collect(Collectors.toMap(Node::hostname, Function.identity())));
+    }
+
+    public Node require(HostName hostName) {
+        return nodeRepository.values().stream()
+                             .map(zoneNodes -> zoneNodes.get(hostName))
+                             .filter(Objects::nonNull)
+                             .findFirst()
+                             .orElseThrow(() -> new NoSuchElementException("No node with the hostname " + hostName + " is known."));
+    }
+
+    /** Replace nodes in zone with a fixed set of nodes */
+    public void setFixedNodes(ZoneId zone) {
+        var nodeA = new Node.Builder()
+                .hostname(HostName.from("hostA"))
+                .parentHostname(HostName.from("parentHostA"))
+                .state(Node.State.active)
+                .type(NodeType.tenant)
+                .owner(ApplicationId.from("tenant1", "app1", "default"))
+                .currentVersion(Version.fromString("7.42"))
+                .wantedVersion(Version.fromString("7.42"))
+                .currentOsVersion(Version.fromString("7.6"))
+                .wantedOsVersion(Version.fromString("7.6"))
+                .serviceState(Node.ServiceState.expectedUp)
+                .resources(new NodeResources(24, 24, 500, 1))
+                .clusterId("clusterA")
+                .clusterType(Node.ClusterType.container)
+                .exclusiveTo(ApplicationId.from("t1", "a1", "i1"))
+                .build();
+        var nodeB = new Node.Builder()
+                .hostname(HostName.from("hostB"))
+                .parentHostname(HostName.from("parentHostB"))
+                .state(Node.State.active)
+                .type(NodeType.tenant)
+                .owner(ApplicationId.from("tenant2", "app2", "default"))
+                .currentVersion(Version.fromString("7.42"))
+                .wantedVersion(Version.fromString("7.42"))
+                .currentOsVersion(Version.fromString("7.6"))
+                .wantedOsVersion(Version.fromString("7.6"))
+                .serviceState(Node.ServiceState.expectedUp)
+                .resources(new NodeResources(40, 24, 500, 1))
+                .cost(20)
+                .clusterId("clusterB")
+                .clusterType(Node.ClusterType.container)
+                .build();
+        setNodes(zone, List.of(nodeA, nodeB));
+    }
+
     @Override
     public void addNodes(ZoneId zone, Collection<NodeRepositoryNode> nodes) {
-        throw new UnsupportedOperationException();
+        nodeRepoNodes.put(zone, new ArrayList<>(nodes));
     }
 
     @Override
@@ -64,15 +171,20 @@ public class NodeRepositoryMock implements NodeRepository {
         var existing = list(zone, List.of(HostName.from(hostName)));
         if (existing.size() != 1) throw new IllegalArgumentException("Node " + hostName + " not found in " + zone);
 
-        var node = Node.builder(existing.get(0))
-                       .state(Node.State.valueOf(nodeState.name()))
-                       .build();
+        var node = new Node.Builder(existing.get(0))
+                .state(Node.State.valueOf(nodeState.name()))
+                .build();
         putNodes(zone, node);
     }
 
     @Override
-    public Node getNode(ZoneId zone, String hostname) {
+    public NodeRepositoryNode getNode(ZoneId zone, String hostname) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public NodeList listNodes(ZoneId zone) {
+        return new NodeList(nodeRepoNodes.get(zone));
     }
 
     @Override
@@ -106,18 +218,6 @@ public class NodeRepositoryMock implements NodeRepository {
     }
 
     @Override
-    public NodeRepoStats getStats(ZoneId zone) {
-        List<ApplicationStats> applicationStats =
-                applications.containsKey(zone)
-                        ? applications.get(zone).keySet().stream()
-                                      .map(id -> new ApplicationStats(id, Load.zero(), 0, 0))
-                                      .collect(Collectors.toList())
-                        : List.of();
-
-        return new NodeRepoStats(Load.zero(), Load.zero(), applicationStats);
-    }
-
-    @Override
     public Map<TenantName, URI> getArchiveUris(ZoneId zone) {
         return Map.copyOf(archiveUris.getOrDefault(zone, Map.of()));
     }
@@ -144,7 +244,7 @@ public class NodeRepositoryMock implements NodeRepository {
         nodeRepository.getOrDefault(zone, Map.of()).values()
                       .stream()
                       .filter(node -> node.type() == type)
-                      .map(node -> Node.builder(node).wantedVersion(version).build())
+                      .map(node -> new Node.Builder(node).wantedVersion(version).build())
                       .forEach(node -> putNodes(zone, node));
     }
 
@@ -161,7 +261,7 @@ public class NodeRepositoryMock implements NodeRepository {
         nodeRepository.getOrDefault(zone, Map.of()).values()
                       .stream()
                       .filter(node -> node.type() == type)
-                      .map(node -> Node.builder(node).wantedOsVersion(version).build())
+                      .map(node -> new Node.Builder(node).wantedOsVersion(version).build())
                       .forEach(node -> putNodes(zone, node));
     }
 
@@ -190,20 +290,15 @@ public class NodeRepositoryMock implements NodeRepository {
         if (existing.size() != 1) throw new IllegalArgumentException("Node " + hostName + " not found in " + zoneId);
 
         // Note: Only supports switchHostname, modelName and wantToRetire
-        Node.Builder newNode = Node.builder(existing.get(0));
+        Node.Builder newNode = new Node.Builder(existing.get(0));
         if (node.getSwitchHostname() != null)
             newNode.switchHostname(node.getSwitchHostname());
         if (node.getModelName() != null)
             newNode.modelName(node.getModelName());
         if (node.getWantToRetire() != null)
             newNode.wantToRetire(node.getWantToRetire());
-
-        Map<String, String> reports = new HashMap<>();
-        for (var kv : node.getReports().entrySet()) {
-            if (kv.getValue() == null) continue; // Null value clears a report
-            reports.put(kv.getKey(), kv.getValue().toString());
-        }
-        newNode.reports(reports);
+        if (!node.getReports().isEmpty())
+            newNode.reports(node.getReports());
 
         putNodes(zoneId, newNode.build());
     }
@@ -218,83 +313,6 @@ public class NodeRepositoryMock implements NodeRepository {
         return hasSpareCapacity;
     }
 
-    /** Add or update given nodes in zone */
-    public void putNodes(ZoneId zone, List<Node> nodes) {
-        Map<HostName, Node> zoneNodes = nodeRepository.computeIfAbsent(zone, (k) -> new HashMap<>());
-        for (var node : nodes) {
-            zoneNodes.put(node.hostname(), node);
-        }
-    }
-
-    /** Add or update given node in zone */
-    public void putNodes(ZoneId zone, Node node) {
-        putNodes(zone, List.of(node));
-    }
-
-    public void putApplication(ZoneId zone, Application application) {
-        applications.computeIfAbsent(zone, (k) -> new HashMap<>())
-                    .put(application.id(), application);
-    }
-
-    public Pair<Double, Double> getTrafficFraction(ApplicationId application, ZoneId zone) {
-        return trafficFractions.get(new DeploymentId(application, zone));
-    }
-
-    /** Remove given nodes from zone */
-    public void removeNodes(ZoneId zone, List<Node> nodes) {
-        nodes.forEach(node -> nodeRepository.get(zone).remove(node.hostname()));
-    }
-
-    /** Remove all nodes in all zones */
-    public void clear() {
-        nodeRepository.clear();
-    }
-
-    public Node require(HostName hostName) {
-        return nodeRepository.values().stream()
-                             .map(zoneNodes -> zoneNodes.get(hostName))
-                             .filter(Objects::nonNull)
-                             .findFirst()
-                             .orElseThrow(() -> new NoSuchElementException("No node with the hostname " + hostName + " is known."));
-    }
-
-    /** Add a fixed set of nodes to given zone */
-    public void addFixedNodes(ZoneId zone) {
-        var nodeA = Node.builder()
-                        .hostname(HostName.from("hostA"))
-                        .parentHostname(HostName.from("parentHostA"))
-                        .state(Node.State.active)
-                        .type(NodeType.tenant)
-                        .owner(ApplicationId.from("tenant1", "app1", "default"))
-                        .currentVersion(Version.fromString("7.42"))
-                        .wantedVersion(Version.fromString("7.42"))
-                        .currentOsVersion(Version.fromString("7.6"))
-                        .wantedOsVersion(Version.fromString("7.6"))
-                        .serviceState(Node.ServiceState.expectedUp)
-                        .resources(new NodeResources(24, 24, 500, 1))
-                        .clusterId("clusterA")
-                        .clusterType(Node.ClusterType.container)
-                        .exclusiveTo(ApplicationId.from("t1", "a1", "i1"))
-                        .build();
-        var nodeB = Node.builder()
-                        .hostname(HostName.from("hostB"))
-                        .parentHostname(HostName.from("parentHostB"))
-                        .state(Node.State.active)
-                        .type(NodeType.tenant)
-                        .owner(ApplicationId.from("tenant2", "app2", "default"))
-                        .currentVersion(Version.fromString("7.42"))
-                        .wantedVersion(Version.fromString("7.42"))
-                        .currentOsVersion(Version.fromString("7.6"))
-                        .wantedOsVersion(Version.fromString("7.6"))
-                        .serviceState(Node.ServiceState.expectedUp)
-                        .resources(new NodeResources(40, 24, 500, 1))
-                        .cost(20)
-                        .clusterId("clusterB")
-                        .clusterType(Node.ClusterType.container)
-                        .build();
-        putNodes(zone, List.of(nodeA, nodeB));
-    }
-
     public Optional<Duration> osUpgradeBudget(ZoneId zone, NodeType type, Version version) {
         return Optional.ofNullable(osUpgradeBudgets.get(Objects.hash(zone, type, version)));
     }
@@ -302,10 +320,10 @@ public class NodeRepositoryMock implements NodeRepository {
     public void doUpgrade(DeploymentId deployment, Optional<HostName> hostName, Version version) {
         modifyNodes(deployment, hostName, node -> {
             assert node.wantedVersion().equals(version);
-            return Node.builder(node)
-                       .currentVersion(version)
-                       .currentDockerImage(node.wantedDockerImage())
-                       .build();
+            return new Node.Builder(node)
+                    .currentVersion(version)
+                    .currentDockerImage(node.wantedDockerImage())
+                    .build();
         });
     }
 
@@ -318,29 +336,23 @@ public class NodeRepositoryMock implements NodeRepository {
     }
 
     public void requestRestart(DeploymentId deployment, Optional<HostName> hostname) {
-        modifyNodes(deployment, hostname, node -> Node.builder(node).wantedRestartGeneration(node.wantedRestartGeneration() + 1).build());
+        modifyNodes(deployment, hostname, node -> new Node.Builder(node).wantedRestartGeneration(node.wantedRestartGeneration() + 1).build());
     }
 
     public void doRestart(DeploymentId deployment, Optional<HostName> hostname) {
-        modifyNodes(deployment, hostname, node -> Node.builder(node).restartGeneration(node.restartGeneration() + 1).build());
+        modifyNodes(deployment, hostname, node -> new Node.Builder(node).restartGeneration(node.restartGeneration() + 1).build());
     }
 
     public void requestReboot(DeploymentId deployment, Optional<HostName> hostname) {
-        modifyNodes(deployment, hostname, node -> Node.builder(node).wantedRebootGeneration(node.wantedRebootGeneration() + 1).build());
+        modifyNodes(deployment, hostname, node -> new Node.Builder(node).wantedRebootGeneration(node.wantedRebootGeneration() + 1).build());
     }
 
     public void doReboot(DeploymentId deployment, Optional<HostName> hostname) {
-        modifyNodes(deployment, hostname, node -> Node.builder(node).rebootGeneration(node.rebootGeneration() + 1).build());
+        modifyNodes(deployment, hostname, node -> new Node.Builder(node).rebootGeneration(node.rebootGeneration() + 1).build());
     }
 
-    public void addReport(ZoneId zoneId, HostName hostName, String reportId, String report) {
-        Node node = nodeRepository.getOrDefault(zoneId, Map.of()).get(hostName);
-        if (node == null) throw new IllegalArgumentException("No node named " + hostName + " in " + zoneId);
-
-        Map<String, String> reports = new HashMap<>(node.reports());
-        reports.put(reportId, report);
-        Node newNode = Node.builder(node).reports(reports).build();
-        putNodes(zoneId, newNode);
+    public void addReport(ZoneId zoneId, HostName hostName, String reportId, JsonNode report) {
+        nodeRepository.get(zoneId).get(hostName).reports().put(reportId, report);
     }
 
     public NodeRepositoryMock allowPatching(boolean allowPatching) {
