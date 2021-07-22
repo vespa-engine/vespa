0a6f2ed7a619db57917ba10bdd8017041783e1a7
Jon Bratseth <bratseth@gmail.com>
2021-07-22 12:45:46 +0200

    Verify content type

--- client/go/src/github.com/vespa-engine/vespa/cmd/deploy_test.go
+++ client/go/src/github.com/vespa-engine/vespa/cmd/deploy_test.go
@@ -14,4 +14,5 @@ func TestDeployCommand(t *testing.T) {
 	             "\x1b[32mSuccess \n",
 	             executeCommand(t, []string{"deploy", "testdata/application.zip"}))
     assert.Equal(t, "http://127.0.0.1:19071/application/v2/tenant/default/prepareandactivate", lastRequest.URL.String())
+    assert.Equal(t, "application/zip", lastRequest.Header.Get("Content-Type"))
 }
