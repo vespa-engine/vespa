be61bbd6136bf32bb60779530459fa8319261a9c
Kristian Aune <kraune@verizonmedia.com>
2021-08-03 11:05:27 +0200

    make runnable not only from cwd

--- vespa-feed-client-cli/src/main/sh/vespa-feed-client-standalone.sh
+++ vespa-feed-client-cli/src/main/sh/vespa-feed-client-standalone.sh
@@ -5,5 +5,5 @@ exec java \
 -Djava.awt.headless=true \
 -Xms128m -Xmx2048m \
 --add-opens=java.base/sun.security.ssl=ALL-UNNAMED  \
--Djava.util.logging.config.file=logging.properties \
--cp vespa-feed-client-cli-jar-with-dependencies.jar ai.vespa.feed.client.CliClient "$@"
+-Djava.util.logging.config.file=`dirname $0`/logging.properties \
+-cp `dirname $0`/vespa-feed-client-cli-jar-with-dependencies.jar ai.vespa.feed.client.CliClient "$@"
