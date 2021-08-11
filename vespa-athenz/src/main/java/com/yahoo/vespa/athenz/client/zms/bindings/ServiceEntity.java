845e0c79a19b8a2352a0db9c77e0c85f86870875
andreer <andreer@verizonmedia.com>
2021-08-11 15:26:21 +0200

    create, put and delete athenz services

---
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/client/zms/bindings/ServiceEntity.java
@@ -0,0 +1,25 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.athenz.client.zms.bindings;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author andreer
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceEntity {
+    public final String name;
+
+    @JsonCreator
+    public ServiceEntity(@JsonProperty("name") String name) {
+        this.name = name;
+    }
+
+    @JsonGetter("name")
+    public String name() {
+        return name;
+    }
+}
