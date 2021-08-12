55a43637250a858a70477c8544e5a736ac293b87
Jon Bratseth <bratseth@gmail.com>
2021-08-12 12:43:28 +0200

    Declare flags in viper

--- client/go/src/github.com/vespa-engine/vespa/cmd/root.go
+++ client/go/src/github.com/vespa-engine/vespa/cmd/root.go
@@ -6,9 +6,8 @@ package cmd
 
 import (
     "github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
-
-
 
 var (
 	// flags
@@ -25,7 +24,12 @@ DO`,
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&ContainerTarget, "container-target", "c", "http://127.0.0.1:8080", "The target of commands to application containers")
+	viper.BindPFlag("container-target", rootCmd.PersistentFlags().Lookup("container-target"))
+	viper.SetDefault("container-target", "http://127.0.0.1:8080")
+
 	rootCmd.PersistentFlags().StringVarP(&DeployTarget, "deploy-target", "d", "http://127.0.0.1:19071", "The target of deploy commands.")
+	viper.BindPFlag("deploy-target", rootCmd.PersistentFlags().Lookup("deploy-target"))
+	viper.SetDefault("deploy-target", "http://127.0.0.1:19071")
 }
 
 // Execute executes the root command.
--- client/go/src/github.com/vespa-engine/vespa/go.mod
+++ client/go/src/github.com/vespa-engine/vespa/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
