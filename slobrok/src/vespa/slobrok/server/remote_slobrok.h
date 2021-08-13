4fe5d5167df46dc8bc30e87deadb951d118145c0
Arne Juul <arnej@verizonmedia.com>
2021-08-13 10:54:06 +0000

    let RemoteSlobrok do its own initial health check

--- slobrok/src/vespa/slobrok/server/remote_slobrok.h
+++ slobrok/src/vespa/slobrok/server/remote_slobrok.h
@@ -23,8 +23,7 @@ class ExchangeManager;
  *
  * Handles one single partner slobrok
  **/
-class RemoteSlobrok: public IRpcServerManager,
-                     public FRT_IRequestWait
+class RemoteSlobrok: public FRT_IRequestWait
 {
 private:
     class Reconnecter : public FNET_Task
@@ -42,11 +41,12 @@ private:
         void PerformTask() override;
     };
 
+    vespalib::string     _name;
+    vespalib::string     _spec;
     ExchangeManager     &_exchanger;
     RpcServerManager    &_rpcsrvmanager;
     FRT_Target          *_remote;
     ServiceMapMirror     _serviceMapMirror;
-    ManagedRpcServer     _rpcserver;
     Reconnecter          _reconnecter;
     int                  _failCnt;
 
@@ -55,16 +55,18 @@ private:
     FRT_RPCRequest      *_remAddReq;
     FRT_RPCRequest      *_remRemReq;
     FRT_RPCRequest      *_remFetchReq;
+    FRT_RPCRequest      *_checkServerReq;
 
     std::deque<std::unique_ptr<NamedService>> _pending;
     void pushMine();
     void doPending();
+    void handleCheckServerResult();
     void handleFetchResult();
 
 public:
     RemoteSlobrok(const RemoteSlobrok&) = delete;
     RemoteSlobrok& operator= (const RemoteSlobrok&) = delete;
-    RemoteSlobrok(const std::string &name, const std::string &spec, ExchangeManager &manager);
+    RemoteSlobrok(const vespalib::string &name, const vespalib::string &spec, ExchangeManager &manager);
     ~RemoteSlobrok() override;
 
     void fail();
@@ -73,16 +75,14 @@ public:
     void maybePushMine();
     void maybeStartFetch();
     void invokeAsync(FRT_RPCRequest *req, double timeout, FRT_IRequestWait *rwaiter);
-    const std::string & getName() const { return _rpcserver.getName(); }
-    const std::string & getSpec() const { return _rpcserver.getSpec(); }
+    const vespalib::string & getName() const { return _name; }
+    const vespalib::string & getSpec() const { return _spec; }
     ServiceMapMirror &remoteMap() { return _serviceMapMirror; }
     void shutdown();
+    FRT_Supervisor *getSupervisor();
 
     // interfaces implemented:
-    void notifyFailedRpcSrv(ManagedRpcServer *rpcsrv, std::string errmsg) override;
-    void notifyOkRpcSrv(ManagedRpcServer *rpcsrv) override;
     void RequestDone(FRT_RPCRequest *req) override;
-    FRT_Supervisor *getSupervisor() override;
 };
 
 //-----------------------------------------------------------------------------
