2bddc841afefd89d3de25d83f6b97b465923e455
Arne Juul <arnej@verizonmedia.com>
2021-08-16 06:21:05 +0000

    add consensus map and wire its inputs

--- slobrok/src/vespa/slobrok/server/sbenv.h
+++ slobrok/src/vespa/slobrok/server/sbenv.h
@@ -10,6 +10,7 @@
 #include "ok_state.h"
 #include "local_rpc_monitor_map.h"
 #include "metrics_producer.h"
+#include "union_service_map.h"
 #include <vespa/config-slobroks.h>
 #include <vespa/slobrok/cfg.h>
 #include <vespa/vespalib/net/simple_health_producer.h>
@@ -56,6 +57,7 @@ private:
     MetricsProducer                            _metrics;
     vespalib::SimpleComponentConfigProducer    _components;
     LocalRpcMonitorMap                         _localRpcMonitorMap;
+    UnionServiceMap                            _consensusMap;
     ServiceMapHistory                          _globalVisibleHistory;
 
 public:
@@ -80,6 +82,10 @@ public:
 
     ServiceMapHistory& localHistory() {
         return _localRpcMonitorMap.history();
+    }
+
+    UnionServiceMap& consensusMap() {
+        return _consensusMap;
     }
 
     const std::string & mySpec() const { return _me; }
