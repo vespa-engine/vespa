4478aeeb245a208c8e4a4c4c9d217136de03f609
Jon Bratseth <bratseth@gmail.com>
2021-08-24 09:50:31 +0200

    Move modules to root

--- client/go/build.sh
+++ client/go/build.sh
@@ -1,7 +1,14 @@
 # Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 # Execute from this directory to build the command-line client to bin/vespa
-export GOPATH=`pwd`
-cd "$GOPATH/src/"
-go test ./...
+cd util
+go test
+cd ..
+
+cd cmd
+go test
+cd ..
+
+export GOBIN=`pwd`/bin
 go install
+
--- client/go/src/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/vespa-engine/vespa
-
-go 1.16
-
-require (
-	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.8.1
-	github.com/stretchr/testify v1.7.0
-)
