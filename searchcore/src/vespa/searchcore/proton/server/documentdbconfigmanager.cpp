fd9dcecdca6df1b351224d5351c489d4198c27cf
Arne Juul <arnej@verizonmedia.com>
2021-08-30 07:57:24 +0000

    signal timeout instead of continuing triggering core dumps

--- searchcore/src/vespa/searchcore/proton/server/documentdbconfigmanager.cpp
+++ searchcore/src/vespa/searchcore/proton/server/documentdbconfigmanager.cpp
@@ -8,6 +8,7 @@
 #include <vespa/searchcore/config/config-ranking-constants.h>
 #include <vespa/searchcore/config/config-ranking-expressions.h>
 #include <vespa/searchcore/config/config-onnx-models.h>
+#include <vespa/config/common/exceptions.h>
 #include <vespa/config-imported-fields.h>
 #include <vespa/config-rank-profiles.h>
 #include <vespa/config-summarymap.h>
@@ -281,6 +282,9 @@ vespalib::string resolve_file(config::RpcFileAcquirer &fileAcquirer, vespalib::T
         }
     }
     LOG(info, "Got file path from file acquirer: '%s' (%s, ref='%s')", filePath.c_str(), desc.c_str(), fileref.c_str());
+    if (filePath == "") {
+        throw config::ConfigTimeoutException("could not get file path from file acquirer");
+    }
     return filePath;
 }
 
