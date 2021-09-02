9b3933f48b71a93b668aa157abf4babb43b99861
Henning Baldersheim <balder@yahoo-inc.com>
2021-09-02 13:50:09 +0200

    Revert "Revert "Update defaults to reflect target of code.""

--- config-model/src/test/java/com/yahoo/searchdefinition/derived/RankProfilesTestCase.java
+++ config-model/src/test/java/com/yahoo/searchdefinition/derived/RankProfilesTestCase.java
@@ -1,6 +1,7 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.searchdefinition.derived;
 
+import com.yahoo.config.model.deploy.TestProperties;
 import com.yahoo.searchdefinition.parser.ParseException;
 import org.junit.Test;
 
@@ -14,6 +15,6 @@ import java.io.IOException;
 public class RankProfilesTestCase extends AbstractExportingTestCase {
     @Test
     public void testRankProfiles() throws IOException, ParseException {
-        assertCorrectDeriving("rankprofiles");
+        assertCorrectDeriving("rankprofiles", null, new TestProperties(), new TestableDeployLogger());
     }
 }
