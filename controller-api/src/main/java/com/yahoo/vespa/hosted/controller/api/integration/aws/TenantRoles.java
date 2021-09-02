6fd748393db38f6c5fda4724b7b22f9e7e519ade
Morten Tokle <mortent@verizonmedia.com>
2021-09-02 12:43:58 +0200

    Create correct iam host roles

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/aws/TenantRoles.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/aws/TenantRoles.java
@@ -6,17 +6,23 @@ package com.yahoo.vespa.hosted.controller.api.integration.aws;
  */
 public class TenantRoles {
     private final String hostRole;
+    private final String tenantHostRole;
     private final String tenantHostServiceRole;
     private final String containerRole;
 
-    public TenantRoles(String hostRole, String tenantHostServiceRole, String containerRole) {
+    public TenantRoles(String hostRole, String tenantHostRole, String tenantHostServiceRole, String containerRole) {
         this.hostRole = hostRole;
+        this.tenantHostRole = tenantHostRole;
         this.tenantHostServiceRole = tenantHostServiceRole;
         this.containerRole = containerRole;
     }
 
     public String hostRole() {
         return hostRole;
+    }
+
+    public String tenantHostRole() {
+        return tenantHostRole;
     }
 
     public String hostServiceRole() {
