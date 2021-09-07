ce4a4b0d069031bef6222f924e2b8337610ebec8
Harald Musum <musum@yahooinc.com>
2021-09-07 12:21:07 +0200

    Extract method for validating checksum

--- config/src/main/java/com/yahoo/config/subscription/impl/PayloadChecksum.java
+++ config/src/main/java/com/yahoo/config/subscription/impl/PayloadChecksum.java
@@ -1,7 +1,18 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.config.subscription.impl;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class handling various type of checksums of config definition payload or config payload,
+ * md5 and xxhash64 are the supported types at the moment.
+ *
+ * @author hmusum
+ */
 public class PayloadChecksum {
 
+    private static final Pattern hexChecksumPattern = Pattern.compile("[0-9a-zA-Z]+");
 
     private final String checksum;
     private final Type type;
@@ -19,6 +30,19 @@ public class PayloadChecksum {
 
     public Type type() { return type; }
 
-    enum Type { MD5, XXHASH64 /* not in use yet */ }
+    public enum Type {MD5, XXHASH64}
+
+    public boolean valid() {
+        if (checksum.equals("")) return true;  // Empty checksum is ok (e.g. when running 'vespa-get-config')
+
+        if (type == Type.MD5 && checksum.length() != 32) {
+            return false;
+        } else if (type == Type.XXHASH64 && checksum.length() != 16) {
+            return false;
+        }
+
+        Matcher m = hexChecksumPattern.matcher(checksum);
+        return m.matches();
+    }
 
 }
