051988486809d9d63ee6fca9dcfe557b81b56bf8
Henning Baldersheim <balder@yahoo-inc.com>
2021-09-09 09:29:42 +0200

    Use the one in vespajlib.

--- metrics-proxy/src/main/java/ai/vespa/metricsproxy/metric/model/DimensionId.java
+++ metrics-proxy/src/main/java/ai/vespa/metricsproxy/metric/model/DimensionId.java
@@ -1,7 +1,7 @@
 // Copyright 2020 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.metricsproxy.metric.model;
 
-import com.yahoo.yolean.concurrent.CopyOnWriteHashMap;
+import com.yahoo.concurrent.CopyOnWriteHashMap;
 
 import java.util.Map;
 import java.util.Objects;
