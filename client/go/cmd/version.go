25ea74b5887bedc8f75f27737011011142a8c2d0
Martin Polden <mpolden@mpolden.no>
2021-09-13 11:34:51 +0200

    Include more info in version command

--- client/go/cmd/version.go
+++ client/go/cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"runtime"
 
 	"github.com/spf13/cobra"
 	"github.com/vespa-engine/vespa/client/go/build"
@@ -17,6 +18,6 @@ var versionCmd = &cobra.Command{
 	DisableAutoGenTag: true,
 	Args:              cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Print("vespa version ", build.Version)
+		log.Printf("vespa version %s compiled with %v on %v/%v", build.Version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
 	},
 }
