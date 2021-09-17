a4c0083194606c969b5562d7ac4a8908d972d447
Jon Bratseth <bratseth@gmail.com>
2021-09-17 10:16:54 +0200

    Provide a (non-working) encoder by default

--- config-model/src/main/java/com/yahoo/vespa/model/container/ContainerCluster.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/ContainerCluster.java
@@ -115,7 +115,6 @@ public abstract class ContainerCluster<CONTAINER extends Container>
     public static final String APPLICATION_STATUS_HANDLER_CLASS = "com.yahoo.container.handler.observability.ApplicationStatusHandler";
     public static final String BINDINGS_OVERVIEW_HANDLER_CLASS = BindingsOverviewHandler.class.getName();
     public static final String LOG_HANDLER_CLASS = com.yahoo.container.handler.LogHandler.class.getName();
-    public static final String DEFAULT_LINGUISTICS_PROVIDER = "com.yahoo.language.provider.DefaultLinguisticsProvider";
     public static final String CMS = "-XX:+UseConcMarkSweepGC -XX:MaxTenuringThreshold=15 -XX:NewRatio=1";
     public static final String G1GC = "-XX:+UseG1GC -XX:MaxTenuringThreshold=15";
 
