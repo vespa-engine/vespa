bc736edc5b7ee54f4beb6a53ab11a8fe9d75673f
gjoranv <gv@verizonmedia.com>
2021-09-23 16:59:45 +0200

    Move ConfigVerification to config-proxy
    .. to allow decoupling config-bundle from http-utils.

--- config-proxy/src/main/java/com/yahoo/vespa/config/proxy/ConfigVerification.java
+++ config-proxy/src/main/java/com/yahoo/vespa/config/proxy/ConfigVerification.java
@@ -1,5 +1,5 @@
 // Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.config;
+package com.yahoo.vespa.config.proxy;
 
 import ai.vespa.util.http.hc5.VespaHttpClientBuilder;
 import com.yahoo.slime.ArrayTraverser;
