ecf0372911a9e42d6c5422e4b6275d63a8598ef6
Harald Musum <musum@yahooinc.com>
2021-10-01 09:38:37 +0200

    Remove default implementation, should always be implemented

--- config-provisioning/src/main/java/com/yahoo/config/provision/Deployer.java
+++ config-provisioning/src/main/java/com/yahoo/config/provision/Deployer.java
@@ -1,4 +1,4 @@
-// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.config.provision;
 
 import java.time.Duration;
@@ -68,7 +68,7 @@ public interface Deployer {
     /** Whether the deployer is bootstrapping, some users of the deployer will want to hold off with deployments in that case. */
     default boolean bootstrapping() { return false; }
 
-    /** Timeout in server, clients can use this to set correct client timeout */
-    default Duration serverDeployTimeout() { return Duration.ofMinutes(30); }
+    /** Timeout for deploy in server, clients can use this to set correct client timeout */
+    Duration serverDeployTimeout();
 
 }
