58db7dade792660dc8c55fd753a797678a15ad34
gjoranv <gv@verizonmedia.com>
2021-10-04 12:17:56 +0200

    Add equals and hashCode for Cluster.

--- hosted-zone-api/src/main/java/ai/vespa/cloud/Cluster.java
+++ hosted-zone-api/src/main/java/ai/vespa/cloud/Cluster.java
@@ -2,6 +2,7 @@ package ai.vespa.cloud;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The properties of a cluster of nodes.
@@ -14,6 +15,7 @@ public class Cluster {
     private final List<Integer> indices;
 
     public Cluster(int size, List<Integer> indices) {
+        Objects.requireNonNull(indices, "Indices cannot be null!");
         this.size = size;
         this.indices = Collections.unmodifiableList(indices);
     }
@@ -24,6 +26,20 @@ public class Cluster {
     /** Returns a list of node indices in this cluster. */
     public List<Integer> indices() {
         return indices;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cluster cluster = (Cluster) o;
+        return size == cluster.size &&
+                indices.equals(cluster.indices);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, indices);
     }
 
 }
