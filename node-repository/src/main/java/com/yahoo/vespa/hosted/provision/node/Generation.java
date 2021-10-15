fb5e4c3c899b0ba0586d50c847204c0fc38bd419
Martin Polden <mpolden@mpolden.no>
2021-10-15 09:20:03 +0200

    Remove findbugs annotation

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/node/Generation.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/node/Generation.java
@@ -1,8 +1,6 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.provision.node;
 
-import javax.annotation.concurrent.Immutable;
-
 /**
  * An immutable generation, with wanted and current generation fields.  Wanted generation
  * is increased when an action (restart services or reboot are the available
@@ -10,7 +8,6 @@ import javax.annotation.concurrent.Immutable;
  *
  * @author hmusum
  */
-@Immutable
 public class Generation {
 
     private final long wanted;
