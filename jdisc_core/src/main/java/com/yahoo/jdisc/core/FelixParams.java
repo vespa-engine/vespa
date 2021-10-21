10e01540fd470c77c9b845b3d9c4f8240cfc946f
gjoranv <gv@verizonmedia.com>
2021-10-21 22:36:14 +0200

    Stop using osgi constant that is deprecated from osgi 1.10 (felix 7)

--- jdisc_core/src/main/java/com/yahoo/jdisc/core/FelixParams.java
+++ jdisc_core/src/main/java/com/yahoo/jdisc/core/FelixParams.java
@@ -54,7 +54,6 @@ public class FelixParams {
         Map<String, String> ret = new HashMap<>();
         ret.put(BundleCache.CACHE_ROOTDIR_PROP, cachePath);
         ret.put(Constants.FRAMEWORK_SYSTEMPACKAGES, exportPackages.toString());
-        ret.put(Constants.SUPPORTS_BOOTCLASSPATH_EXTENSION, "true");
         ret.put(Constants.FRAMEWORK_BOOTDELEGATION, "com.yourkit.runtime,com.yourkit.probes,com.yourkit.probes.builtin,com.singularity.*");
         ret.put(Constants.FRAMEWORK_BSNVERSION, Constants.FRAMEWORK_BSNVERSION_MANAGED);
         return ret;
