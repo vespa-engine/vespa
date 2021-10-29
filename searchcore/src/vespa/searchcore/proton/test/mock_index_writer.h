7ac9697eb5cb46a5c91a4f14a538e7840018dff6
Henning Baldersheim <balder@yahoo-inc.com>
2021-10-29 14:25:38 +0000

    Use vector of lids in IIndexWriter api.

--- searchcore/src/vespa/searchcore/proton/test/mock_index_writer.h
+++ searchcore/src/vespa/searchcore/proton/test/mock_index_writer.h
@@ -3,9 +3,7 @@
 
 #include <vespa/searchcore/proton/index/i_index_writer.h>
 
-namespace proton {
-
-namespace test {
+namespace proton::test {
 
 /**
  * Mock of the IIndexWriter interface used for unit testing.
@@ -15,14 +13,12 @@ struct MockIndexWriter : public IIndexWriter
     IIndexManager::SP _idxMgr;
     MockIndexWriter() : _idxMgr() {}
     MockIndexWriter(const IIndexManager::SP &idxMgr) : _idxMgr(idxMgr) {}
-    virtual const IIndexManager::SP &getIndexManager() const override { return _idxMgr; }
-    virtual void put(search::SerialNum, const document::Document &, const search::DocumentIdT) override {}
-    virtual void remove(search::SerialNum, const search::DocumentIdT) override {}
-    virtual void commit(search::SerialNum, OnWriteDoneType) override {}
-    virtual void heartBeat(search::SerialNum) override {}
+    const IIndexManager::SP &getIndexManager() const override { return _idxMgr; }
+    void put(search::SerialNum, const document::Document &, const search::DocumentIdT) override {}
+    void removeDocs(search::SerialNum, LidVector) override {}
+    void commit(search::SerialNum, OnWriteDoneType) override {}
+    void heartBeat(search::SerialNum) override {}
     void compactLidSpace(search::SerialNum, const search::DocumentIdT) override {}
 };
 
-} // namespace test
-
-} // namespace proton
+}
