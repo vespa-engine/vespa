279086b790fab69a70aa00e982deb9d31cf2334b
Geir Storli <geirst@verizonmedia.com>
2021-11-01 10:09:47 +0000

    Extend expiry date on enable-feed-block-in-distributor.

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -179,7 +179,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag ENABLE_FEED_BLOCK_IN_DISTRIBUTOR = defineFeatureFlag(
             "enable-feed-block-in-distributor", true,
-            List.of("geirst"), "2021-01-27", "2021-11-01",
+            List.of("geirst"), "2021-01-27", "2022-01-31",
             "Enables blocking of feed in the distributor if resource usage is above limit on at least one content node",
             "Takes effect at redeployment",
             ZONE_ID, APPLICATION_ID);
