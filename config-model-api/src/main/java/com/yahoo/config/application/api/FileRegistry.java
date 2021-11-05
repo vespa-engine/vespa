073fc34e37e40c329269a8ff83a8ed492cd7fb90
Harald Musum <musum@yahooinc.com>
2021-11-05 07:23:28 +0100

    GC unused methods

--- config-model-api/src/main/java/com/yahoo/config/application/api/FileRegistry.java
+++ config-model-api/src/main/java/com/yahoo/config/application/api/FileRegistry.java
@@ -1,12 +1,10 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.config.application.api;
 
+import com.yahoo.config.FileReference;
+
 import java.nio.ByteBuffer;
 import java.util.List;
-
-import com.yahoo.config.FileReference;
-import com.yahoo.net.HostName;
-import net.jpountz.xxhash.XXHashFactory;
 
 
 /**
@@ -16,21 +14,8 @@ public interface FileRegistry {
 
     FileReference addFile(String relativePath);
     FileReference addUri(String uri);
-    /**
-     * @deprecated Remove after 7.455
-     */
-    @Deprecated
-    default FileReference addBlob(ByteBuffer blob) { return null; }
     FileReference addBlob(String name, ByteBuffer blob);
     default FileReference addApplicationPackage() { return addFile(""); }
-
-    /**
-     * Returns the name of the host which is the source of the files
-     * @deprecated Remove after 7.453
-     */
-    @Deprecated
-    default String fileSourceHost() { return HostName.getLocalhost(); }
-
     List<Entry> export();
 
     class Entry {
@@ -41,17 +26,6 @@ public interface FileRegistry {
             this.relativePath = relativePath;
             this.reference = reference;
         }
-    }
-
-    /**
-     * @deprecated Remove after 7.455
-     */
-    @Deprecated
-    static String blobName(ByteBuffer blob) {
-        blob.mark();
-        long blobHash = XXHashFactory.fastestJavaInstance().hash64().hash(blob, 0);
-        blob.reset();
-        return Long.toHexString(blobHash);
     }
 
 }
