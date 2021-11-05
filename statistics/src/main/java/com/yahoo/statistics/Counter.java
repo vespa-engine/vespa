8b100501faa14a595425c43e9d653e1e89f7b160
Henning Baldersheim <balder@yahoo-inc.com>
2021-11-05 21:26:20 +0100

    GC old way of collection metrics and logging to file.

--- statistics/src/main/java/com/yahoo/statistics/Counter.java
+++ statistics/src/main/java/com/yahoo/statistics/Counter.java
@@ -1,10 +1,8 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.statistics;
 
-
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.yahoo.log.event.Event;
 import com.yahoo.container.StatisticsConfig;
 
 
