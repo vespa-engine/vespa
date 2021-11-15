88936757e42187d2afadcf138deaaa73bbdea755
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-11-15 14:51:36 +0100

    Allow reflective access to sun.security.util

--- standalone-container/src/main/sh/standalone-container.sh
+++ standalone-container/src/main/sh/standalone-container.sh
@@ -169,6 +169,7 @@ StartCommand() {
         --add-opens=java.base/java.nio=ALL-UNNAMED \
         --add-opens=java.base/jdk.internal.loader=ALL-UNNAMED \
         --add-opens=java.base/sun.security.ssl=ALL-UNNAMED  \
+        --add-opens=java.base/sun.security.util=ALL-UNNAMED  \
         -Djava.library.path="$VESPA_HOME/lib64" \
         -Djava.awt.headless=true \
         -Dsun.rmi.dgc.client.gcInterval=3600000 \
