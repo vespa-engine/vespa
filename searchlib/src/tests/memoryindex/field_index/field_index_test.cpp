50f86a79569b2c01b38946a1a1b600193d0121c8
Tor Egge <Tor.Egge@online.no>
2021-11-15 23:30:04 +0100

    Use braces to signal lifetime.

--- searchlib/src/tests/memoryindex/field_index/field_index_test.cpp
+++ searchlib/src/tests/memoryindex/field_index/field_index_test.cpp
@@ -479,18 +479,19 @@ void
 myCommit(FieldIndexCollection &fieldIndexes, ISequencedTaskExecutor &pushThreads)
 {
     vespalib::Gate gate;
-    auto gate_callback = std::make_shared<vespalib::GateCallback>(gate);
-    uint32_t fieldId = 0;
-    for (auto &fieldIndex : fieldIndexes.getFieldIndexes()) {
-        pushThreads.execute(fieldId,
-                            [fieldIndex(fieldIndex.get()), gate_callback]()
-                            {
-                                (void) gate_callback;
-                                fieldIndex->commit();
-                            });
-        ++fieldId;
+    {
+        auto gate_callback = std::make_shared<vespalib::GateCallback>(gate);
+        uint32_t fieldId = 0;
+        for (auto &fieldIndex : fieldIndexes.getFieldIndexes()) {
+            pushThreads.execute(fieldId,
+                                [fieldIndex(fieldIndex.get()), gate_callback]()
+                                {
+                                    (void) gate_callback;
+                                    fieldIndex->commit();
+                                });
+            ++fieldId;
+        }
     }
-    gate_callback.reset();
     gate.await();
 }
 
