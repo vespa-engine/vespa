7c38639072247c471202ed3ed5e428c4aeb120d1
Håkon Hallingstad <hakon@yahooinc.com>
2021-11-25 16:42:31 +0100

    Also source enable-rh-git227.sh

--- screwdriver/build-vespa.sh
+++ screwdriver/build-vespa.sh
@@ -8,6 +8,7 @@ readonly NUM_THREADS=$(( $(nproc) + 2 ))
 
 source /etc/profile.d/enable-devtoolset-10.sh
 source /etc/profile.d/enable-rh-maven35.sh
+source /etc/profile.d/enable-rh-git227.sh
 
 export MALLOC_ARENA_MAX=1
 export MAVEN_OPTS="-Xss1m -Xms128m -Xmx2g"
