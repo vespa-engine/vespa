498b9b780efd90e1cce18b9f89d2f00036170219
Håkon Hallingstad <hakon@yahooinc.com>
2021-11-25 18:01:15 +0100

    Avoid concurrent go builds

--- screwdriver/build-vespa.sh
+++ screwdriver/build-vespa.sh
@@ -40,10 +40,10 @@ case $SHOULD_BUILD in
     mvn -V $VESPA_MAVEN_EXTRA_OPTS install
     ;;
   go)
-    make -C client/go -j ${NUM_THREADS}
+    make -C client/go
     ;;
   *)
-    make -C client/go -j ${NUM_THREADS}
+    make -C client/go
     ./bootstrap.sh java
     time mvn -V $VESPA_MAVEN_EXTRA_OPTS install
     cmake3 -DVESPA_UNPRIVILEGED=no .
