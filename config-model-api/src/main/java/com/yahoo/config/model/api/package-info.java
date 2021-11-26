674b46d95cf7e39d7461079a7047abf54c1d57a2
Morten Tokle <mortent@verizonmedia.com>
2021-11-26 13:06:35 +0100

    Track non-public parts of config-model-api as internal api

--- config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
@@ -1,6 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+@InternalApi
 @ExportPackage
 package com.yahoo.config.model.api;
 
 import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.api.annotations.InternalApi;
