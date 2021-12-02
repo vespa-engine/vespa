482a30d82ab06a8f8ddfbc1d3e1222daa0b3389f
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-12-02 16:46:27 +0100

    Add glob pattern helper that handles multiple alternative boundaries

--- security-utils/src/main/java/com/yahoo/security/tls/policy/HostGlobPattern.java
+++ security-utils/src/main/java/com/yahoo/security/tls/policy/HostGlobPattern.java
@@ -2,60 +2,32 @@
 package com.yahoo.security.tls.policy;
 
 import java.util.Objects;
-import java.util.regex.Pattern;
 
 /**
  * @author bjorncs
  */
 class HostGlobPattern implements RequiredPeerCredential.Pattern {
 
-    private final String pattern;
-    private final Pattern regexPattern;
+    private final GlobPattern globPattern;
 
     HostGlobPattern(String pattern) {
-        this.pattern = pattern;
-        this.regexPattern = toRegexPattern(pattern);
+        this.globPattern = new GlobPattern(pattern, new char[] {'.'});
     }
 
     @Override
     public String asString() {
-        return pattern;
+        return globPattern.asString();
     }
 
     @Override
     public boolean matches(String hostString) {
-        return regexPattern.matcher(hostString).matches();
-    }
-
-    private static Pattern toRegexPattern(String pattern) {
-        StringBuilder builder = new StringBuilder("^");
-        for (char c : pattern.toCharArray()) {
-            if (c == '*') {
-                // Note: we explicitly stop matching at a dot separator boundary.
-                // This is to make host name matching less vulnerable to dirty tricks.
-                builder.append("[^.]*");
-            } else if (c == '?') {
-                // Same applies for single chars; they should only match _within_ a dot boundary.
-                builder.append("[^.]");
-            } else if (isRegexMetaCharacter(c)){
-                builder.append("\\");
-                builder.append(c);
-            } else {
-                builder.append(c);
-            }
-        }
-        builder.append('$');
-        return Pattern.compile(builder.toString());
-    }
-
-    private static boolean isRegexMetaCharacter(char c) {
-        return "<([{\\^-=$!|]})?*+.>".indexOf(c) != -1; // note: includes '?' and '*'
+        return globPattern.matches(hostString);
     }
 
     @Override
     public String toString() {
         return "HostGlobPattern{" +
-                "pattern='" + pattern + '\'' +
+                "pattern='" + globPattern + '\'' +
                 '}';
     }
 
@@ -64,11 +36,11 @@ class HostGlobPattern implements RequiredPeerCredential.Pattern {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         HostGlobPattern that = (HostGlobPattern) o;
-        return Objects.equals(pattern, that.pattern);
+        return Objects.equals(globPattern, that.globPattern);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pattern);
+        return Objects.hash(globPattern);
     }
 }
