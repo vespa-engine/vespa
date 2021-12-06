005413e8664f0f59fc60599cee34355a8324af8f
Henning Baldersheim <balder@yahoo-inc.com>
2021-12-06 11:18:14 +0000

    Make tests selfcontained avoiding av enxtra cleanup step in a script.

--- searchlib/src/tests/transactionlog/translogclient_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-set -e
-rm -rf test7 test8 test9 test10 test11 test12 test13 testremove
-$VALGRIND ./searchlib_translogclient_test_app
-rm -rf test7 test8 test9 test10 test11 test12 test13 testremove
