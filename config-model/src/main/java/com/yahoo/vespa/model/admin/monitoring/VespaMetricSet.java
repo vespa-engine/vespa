58b08e33d0c38ffd43060c7bd958e659a287f847
Yngve Aasheim <yngveaasheim@users.noreply.github.com>
2021-12-15 14:42:31 +0100

    Fix metrics names

--- config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/VespaMetricSet.java
+++ config-model/src/main/java/com/yahoo/vespa/model/admin/monitoring/VespaMetricSet.java
@@ -777,10 +777,10 @@ public class VespaMetricSet {
         metrics.add(new Metric("vds.distributor.puts.sum.failures.wrongdistributor.rate"));
         metrics.add(new Metric("vds.distributor.puts.sum.failures.safe_time_not_reached.rate"));
         metrics.add(new Metric("vds.distributor.puts.sum.failures.storagefailure.rate"));
-        metrics.add(new Metric("vds.distributor.puts.sum.failures.failures.timeout.rate"));
-        metrics.add(new Metric("vds.distributor.puts.sum.failures.failures.busy.rate"));
-        metrics.add(new Metric("vds.distributor.puts.sum.failures.failures.inconsistent_bucket.rate"));
-        metrics.add(new Metric("vds.distributor.puts.sum.failures.failures.concurrent_mutations.rate"));        
+        metrics.add(new Metric("vds.distributor.puts.sum.failures.timeout.rate"));
+        metrics.add(new Metric("vds.distributor.puts.sum.failures.busy.rate"));
+        metrics.add(new Metric("vds.distributor.puts.sum.failures.inconsistent_bucket.rate"));
+        metrics.add(new Metric("vds.distributor.puts.sum.failures.concurrent_mutations.rate"));        
         metrics.add(new Metric("vds.distributor.removes.sum.latency.max"));
         metrics.add(new Metric("vds.distributor.removes.sum.latency.sum"));
         metrics.add(new Metric("vds.distributor.removes.sum.latency.count"));
