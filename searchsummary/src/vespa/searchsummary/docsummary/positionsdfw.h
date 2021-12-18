754886cee3a828a55bf69b463d399ffa74bd8e22
Arne H Juul <arnej@yahooinc.com>
2021-12-18 00:16:28 +0000

    configurable position rendering
    * in summary field writers

--- searchsummary/src/vespa/searchsummary/docsummary/positionsdfw.h
+++ searchsummary/src/vespa/searchsummary/docsummary/positionsdfw.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "attributedfw.h"
+#include "resultconfig.h"
 #include <vespa/searchlib/common/geo_location_spec.h>
 
 namespace search::docsummary {
@@ -35,30 +36,35 @@ public:
 class AbsDistanceDFW : public LocationAttrDFW
 {
 private:
+    bool _useV8geoPositions;
+    double kmMinDistance(uint32_t docid, GetDocsumsState *state,
+                         const std::vector<const GeoLoc *> &locations);
     uint64_t findMinDistance(uint32_t docid, GetDocsumsState *state,
                              const std::vector<const GeoLoc *> &locations);
 public:
-    AbsDistanceDFW(const vespalib::string & attrName);
+    AbsDistanceDFW(const vespalib::string & attrName, bool useV8geoPositions);
 
     bool IsGenerated() const override { return true; }
     void insertField(uint32_t docid, GetDocsumsState *state,
                      ResType type, vespalib::slime::Inserter &target) override;
+
+    static UP create(const char *attribute_name, IAttributeManager *index_man, bool useV8geoPositions);
+
 };
 
 //--------------------------------------------------------------------------
 
 class PositionsDFW : public AttrDFW
 {
+private:
+    bool _useV8geoPositions;
 public:
     typedef std::unique_ptr<PositionsDFW> UP;
-
-    PositionsDFW(const vespalib::string & attrName);
-
+    PositionsDFW(const vespalib::string & attrName, bool useV8geoPositions);
     bool IsGenerated() const override { return true; }
     void insertField(uint32_t docid, GetDocsumsState *state, ResType type, vespalib::slime::Inserter &target) override;
+    static UP create(const char *attribute_name, IAttributeManager *index_man, bool useV8geoPositions);
 };
 
-PositionsDFW::UP createPositionsDFW(const char *attribute_name, IAttributeManager *index_man);
-AbsDistanceDFW::UP createAbsDistanceDFW(const char *attribute_name, IAttributeManager *index_man);
 
 }
