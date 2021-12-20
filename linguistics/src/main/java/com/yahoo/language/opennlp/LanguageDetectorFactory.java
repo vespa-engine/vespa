a8908be0652c8213dbe232c3a94fe2d74994842c
Jon Marius Venstad <venstad@gmail.com>
2021-12-20 13:42:19 +0100

    Override ngram creation with something less silly

--- linguistics/src/main/java/com/yahoo/language/opennlp/LanguageDetectorFactory.java
+++ linguistics/src/main/java/com/yahoo/language/opennlp/LanguageDetectorFactory.java
@@ -1,7 +1,6 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.language.opennlp;
 
-import opennlp.tools.langdetect.DefaultLanguageDetectorContextGenerator;
 import opennlp.tools.langdetect.LanguageDetectorContextGenerator;
 import opennlp.tools.util.normalizer.EmojiCharSequenceNormalizer;
 import opennlp.tools.util.normalizer.NumberCharSequenceNormalizer;
