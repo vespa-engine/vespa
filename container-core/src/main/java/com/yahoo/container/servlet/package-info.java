a4658d0d0df841e8d65238c39497aed5ff1b9f1d
gjoranv <gv@verizonmedia.com>
2022-01-03 17:11:13 +0100

    Remove package-info for empty c.y.container.servlet package

--- container-core/src/main/java/com/yahoo/container/servlet/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-// TODO Vespa 8 Remove export package
-@ExportPackage
-package com.yahoo.container.servlet;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-
