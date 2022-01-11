2627f74bff87b619393cf74fe36228c9cb5ad4ed
Jon Bratseth <bratseth@gmail.com>
2022-01-11 18:45:55 +0100

    Propagate the original term in the evaluation

---
+++ container-search/src/test/java/com/yahoo/prelude/semantics/test/MusicTestCase.java
@@ -0,0 +1,19 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.prelude.semantics.test;
+
+import org.junit.Test;
+
+/**
+ * Tests the rewriting in the semanticsearcher system test
+ *
+ * @author bratseth
+ */
+public class MusicTestCase {
+
+    @Test
+    public void testMusic() {
+        var tester = new RuleBaseTester("music.sr");
+        tester.assertSemantics("AND song:together artist:youngbloods", "together by youngbloods");
+    }
+
+}
