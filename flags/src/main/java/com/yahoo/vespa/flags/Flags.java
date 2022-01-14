bc07e5e59b04e70f79b7a7d19951578a3d16c6b0
andreer <andreer@verizonmedia.com>
2022-01-14 11:17:11 +0100

    remove cameo api url flag

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -240,16 +240,9 @@ public class Flags {
 
     public static final UnboundBooleanFlag DELETE_UNMAINTAINED_CERTIFICATES = defineFeatureFlag(
             "delete-unmaintained-certificates", false,
-            List.of("andreer"), "2021-09-23", "2022-02-01",
+            List.of("andreer"), "2021-09-23", "2022-02-14",
             "Whether to delete certificates that are known by provider but not by controller",
             "Takes effect on next run of EndpointCertificateMaintainer"
-    );
-
-    public static final UnboundBooleanFlag USE_NEW_ENDPOINT_CERTIFICATE_PROVIDER_URL = defineFeatureFlag(
-            "use-new-endpoint-certificate-provider-url", true,
-            List.of("andreer"), "2021-12-14", "2022-01-14",
-            "Use the new URL for the endpoint certificate provider API",
-            "Takes effect immediately"
     );
 
     public static final UnboundBooleanFlag ENABLE_TENANT_DEVELOPER_ROLE = defineFeatureFlag(
