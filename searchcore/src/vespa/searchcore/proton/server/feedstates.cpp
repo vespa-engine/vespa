291daab336706cf85b23f46bda6c2a226700591a
Tor Egge <Tor.Egge@online.no>
2022-01-25 12:53:10 +0100

    Throttle replay.

--- searchcore/src/vespa/searchcore/proton/server/feedstates.cpp
+++ searchcore/src/vespa/searchcore/proton/server/feedstates.cpp
@@ -5,6 +5,7 @@
 #include "ifeedview.h"
 #include "ireplayconfig.h"
 #include "replaypacketdispatcher.h"
+#include "replay_throttling_policy.h"
 #include <vespa/searchcore/proton/bucketdb/ibucketdbhandler.h>
 #include <vespa/searchcore/proton/feedoperation/operations.h>
 #include <vespa/searchcore/proton/common/eventlogger.h>
@@ -23,6 +24,7 @@ using search::SerialNum;
 using vespalib::Executor;
 using vespalib::makeLambdaTask;
 using vespalib::IDestructorCallback;
+using vespalib::SharedOperationThrottler;
 using vespalib::make_string;
 using proton::bucketdb::IBucketDBHandler;
 
@@ -54,25 +56,36 @@ class TransactionLogReplayPacketHandler : public IReplayPacketHandler {
     FeedConfigStore &_config_store;
     IIncSerialNum   &_inc_serial_num;
     CommitTimeTracker _commitTimeTracker;
+    std::unique_ptr<SharedOperationThrottler> _throttler;
+
+    static std::unique_ptr<SharedOperationThrottler> make_throttler(const ReplayThrottlingPolicy& replay_throttling_policy) {
+        auto& params = replay_throttling_policy.get_params();
+        if (!params.has_value()) {
+            return SharedOperationThrottler::make_unlimited_throttler();
+        }
+        return SharedOperationThrottler::make_dynamic_throttler(params.value());
+    }
 
 public:
     TransactionLogReplayPacketHandler(IFeedView *& feed_view_ptr,
                                       IBucketDBHandler &bucketDBHandler,
                                       IReplayConfig &replay_config,
                                       FeedConfigStore &config_store,
+                                      const ReplayThrottlingPolicy& replay_throttling_policy,
                                       IIncSerialNum &inc_serial_num)
         : _feed_view_ptr(feed_view_ptr),
           _bucketDBHandler(bucketDBHandler),
           _replay_config(replay_config),
           _config_store(config_store),
           _inc_serial_num(inc_serial_num),
-          _commitTimeTracker(5ms)
+          _commitTimeTracker(5ms),
+          _throttler(make_throttler(replay_throttling_policy))
     { }
 
     ~TransactionLogReplayPacketHandler() override = default;
 
     FeedToken make_replay_feed_token() {
-        vespalib::SharedOperationThrottler::Token throttler_token;
+        SharedOperationThrottler::Token throttler_token = _throttler->blocking_acquire_one();
         return std::make_shared<feedtoken::ReplayState>(std::move(throttler_token));
     }
 
@@ -180,10 +193,11 @@ ReplayTransactionLogState::ReplayTransactionLogState(
         IBucketDBHandler &bucketDBHandler,
         IReplayConfig &replay_config,
         FeedConfigStore &config_store,
+        const ReplayThrottlingPolicy &replay_throttling_policy,
         IIncSerialNum& inc_serial_num)
     : FeedState(REPLAY_TRANSACTION_LOG),
       _doc_type_name(name),
-      _packet_handler(std::make_unique<TransactionLogReplayPacketHandler>(feed_view_ptr, bucketDBHandler, replay_config, config_store, inc_serial_num))
+      _packet_handler(std::make_unique<TransactionLogReplayPacketHandler>(feed_view_ptr, bucketDBHandler, replay_config, config_store, replay_throttling_policy, inc_serial_num))
 { }
 
 ReplayTransactionLogState::~ReplayTransactionLogState() = default;
