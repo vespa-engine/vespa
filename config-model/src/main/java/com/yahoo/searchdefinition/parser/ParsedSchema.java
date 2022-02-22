d043870768e245c3b9c6293dfbc2bb48842e511c
Arne H Juul <arnej@yahooinc.com>
2022-02-22 12:48:02 +0000

    convert some easy productions

--- config-model/src/main/java/com/yahoo/searchdefinition/parser/ParsedSchema.java
+++ config-model/src/main/java/com/yahoo/searchdefinition/parser/ParsedSchema.java
@@ -1,6 +1,10 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.searchdefinition.parser;
 
+import com.yahoo.searchdefinition.OnnxModel;
+import com.yahoo.searchdefinition.RankingConstant;
+import com.yahoo.searchdefinition.document.Stemming;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,21 +17,22 @@ public class ParsedSchema {
     }
 
     String getName() { return name; }
-    void inherit(String other) { inherited.add(other); }
+
     void addDocument(ParsedDocument document) {}
+    void addImportedField(String asFieldName, String refFieldName, String foregnFieldName) {}
+    void addOnnxModel(OnnxModel model) {}
+    void addRankingConstant(RankingConstant constant) {}
+    void enableRawAsBase64(boolean value) {}
+    void inherit(String other) { inherited.add(other); }
+    void setStemming(Stemming value) {}
 
     /*
     private final List<ParsedField> fields = new ArrayList<>();
     List<ParsedField> getFields() { return fields; }
     void addField(ParsedField field) { fields.add(field); }
-    void addOnnxModel(Object model) {}
-    void addImportedField(String asFieldName, String refFieldName, String foregnFieldName) {}
     void addAnnotation(ParsedAnnotation annotation) {}
     void addIndex(ParsedIndex index) {}
-    void enableRawAsBase64(boolean value) {}
     void addStruct(ParsedStruct struct) {}
-    void setStemming(String value) {}
-    void addRankingConstant(Object constant) {}
     void addFieldSet(ParsedFieldSet fieldSet) {}
     void addDocumentSummary(ParsedDocumentSummary docsum) {}
     void addRankProfile(ParsedRankProfile profile) {}
