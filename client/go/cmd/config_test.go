81fd159e4d81e7f7267befd7f5eea4cd715970f1
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2022-02-24 11:01:00 +0100

    Avoid leaking config settings from config_test/TestConfig to other tests

--- client/go/cmd/config_test.go
+++ client/go/cmd/config_test.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -29,6 +30,9 @@ func TestConfig(t *testing.T) {
 	assertConfigCommand(t, "", homeDir, "config", "set", "wait", "60")
 	assertConfigCommandErr(t, "Error: wait option must be an integer >= 0, got \"foo\"\n", homeDir, "config", "set", "wait", "foo")
 	assertConfigCommand(t, "wait = 60\n", homeDir, "config", "get", "wait")
+
+	// Avoid leaking config settings to other tests
+	viper.Reset()
 }
 
 func assertConfigCommand(t *testing.T, expected, homeDir string, args ...string) {
