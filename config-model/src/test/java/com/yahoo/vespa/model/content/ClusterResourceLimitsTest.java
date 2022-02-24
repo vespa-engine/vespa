24d719637a8b047021741f517e8b85dfc2e69f33
Geir Storli <geirst@yahooinc.com>
2022-02-24 13:03:19 +0000

    Give content nodes more headroom when deriving resource limit for disk.
    This ensures content nodes can handle larger spikes in transient disk usage during disk index fusion.
    This is a follow up from https://github.com/vespa-engine/vespa/pull/21179.

--- config-model/src/test/java/com/yahoo/vespa/model/content/ClusterResourceLimitsTest.java
+++ config-model/src/test/java/com/yahoo/vespa/model/content/ClusterResourceLimitsTest.java
@@ -62,11 +62,11 @@ public class ClusterResourceLimitsTest {
 
     @Test
     public void content_node_limits_are_derived_from_cluster_controller_limits_if_not_set() {
-        assertLimits(0.4, 0.7, 0.7, 0.85,
+        assertLimits(0.4, 0.7, 0.76, 0.85,
                 new Fixture().ctrlDisk(0.4).ctrlMemory(0.7));
-        assertLimits(0.4, 0.8, 0.7, 0.9,
+        assertLimits(0.4, 0.8, 0.76, 0.9,
                 new Fixture().ctrlDisk(0.4));
-        assertLimits(0.75, 0.7, 0.875, 0.85,
+        assertLimits(0.75, 0.7, 0.9, 0.85,
                 new Fixture().ctrlMemory(0.7));
     }
 
@@ -76,7 +76,7 @@ public class ClusterResourceLimitsTest {
                 new Fixture().ctrlDisk(0.4).ctrlMemory(0.7).nodeDisk(0.9).nodeMemory(0.95));
         assertLimits(0.4, 0.8, 0.95, 0.9,
                 new Fixture().ctrlDisk(0.4).nodeDisk(0.95));
-        assertLimits(0.75, 0.7, 0.875, 0.95,
+        assertLimits(0.75, 0.7, 0.9, 0.95,
                 new Fixture().ctrlMemory(0.7).nodeMemory(0.95));
     }
 
@@ -86,15 +86,15 @@ public class ClusterResourceLimitsTest {
                 new Fixture().nodeDisk(0.9).nodeMemory(0.95));
         assertLimits(0.89, 0.8, 0.9, 0.9,
                 new Fixture().nodeDisk(0.9));
-        assertLimits(0.75, 0.94, 0.875, 0.95,
+        assertLimits(0.75, 0.94, 0.9, 0.95,
                 new Fixture().nodeMemory(0.95));
-        assertLimits(0.75, 0.0, 0.875, 0.005,
+        assertLimits(0.75, 0.0, 0.9, 0.005,
                 new Fixture().nodeMemory(0.005));
     }
 
     @Test
     public void limits_are_derived_from_the_other_if_not_set() {
-        assertLimits(0.6, 0.94, 0.8, 0.95,
+        assertLimits(0.6, 0.94, 0.84, 0.95,
                 new Fixture().ctrlDisk(0.6).nodeMemory(0.95));
         assertLimits(0.89, 0.7, 0.9, 0.85,
                 new Fixture().ctrlMemory(0.7).nodeDisk(0.9));
@@ -102,7 +102,7 @@ public class ClusterResourceLimitsTest {
 
     @Test
     public void default_resource_limits_when_feed_block_is_enabled_in_distributor() {
-        assertLimits(0.75, 0.8, 0.875, 0.9,
+        assertLimits(0.75, 0.8, 0.9, 0.9,
                 new Fixture(true));
     }
 
@@ -125,7 +125,7 @@ public class ClusterResourceLimitsTest {
 
         // Verify that limits from feature flags are used
         assertLimits(0.85, 0.90, limits.getClusterControllerLimits());
-        assertLimits(0.925, 0.95, limits.getContentNodeLimits());
+        assertLimits(0.94, 0.95, limits.getContentNodeLimits());
     }
 
     @Test
