c72ede0ea57ffc81390b496d8461b66dde2c8eb0
Tor Egge <Tor.Egge@online.no>
2022-02-27 00:12:13 +0100

    Use pointers to functions to hide delete from compiler.

--- vespamalloc/src/tests/doubledelete/doubledelete.cpp
+++ vespamalloc/src/tests/doubledelete/doubledelete.cpp
@@ -3,12 +3,19 @@
 
 void *savedptr;
 
+void delete_ptr_real(char *ptr)
+{
+    delete ptr;
+}
+
+void (*delete_ptr)(char *ptr) = delete_ptr_real;
+
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
     char * a = new char;
     savedptr = a;
-    delete a;
-    delete a;
+    delete_ptr(a);
+    delete_ptr(a);
 }
