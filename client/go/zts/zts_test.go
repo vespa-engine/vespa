ede3641bda7b109bbd941c69817542505126d124
Martin Polden <mpolden@mpolden.no>
2022-02-28 09:53:51 +0100

    Implement basic ZTS client

---
+++ client/go/zts/zts_test.go
@@ -0,0 +1,25 @@
+package zts
+
+import (
+	"crypto/tls"
+	"testing"
+
+	"github.com/vespa-engine/vespa/client/go/mock"
+)
+
+func TestAccessToken(t *testing.T) {
+	httpClient := mock.HTTPClient{}
+	client, err := NewClient("http://example.com", &httpClient)
+	if err != nil {
+		t.Fatal(err)
+	}
+	httpClient.NextResponse(200, `{"access_token": "foo bar"}`)
+	token, err := client.AccessToken("vespa.vespa", tls.Certificate{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "foo bar"
+	if token != want {
+		t.Errorf("got %q, want %q", token, want)
+	}
+}
