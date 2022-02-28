a958c6a1e6a24ae93ee92cfcf3f8c44cb32ebcf1
Jon Marius Venstad <venstad@gmail.com>
2022-02-28 10:23:28 +0100

    Let fat-jar test runner fail production tests when no tests are present

--- vespa-testrunner-components/src/main/java/com/yahoo/vespa/hosted/testrunner/TestProfile.java
+++ vespa-testrunner-components/src/main/java/com/yahoo/vespa/hosted/testrunner/TestProfile.java
@@ -10,7 +10,7 @@ public enum TestProfile {
     SYSTEM_TEST("system, com.yahoo.vespa.tenant.systemtest.base.SystemTest", true),
     STAGING_SETUP_TEST("staging-setup", false),
     STAGING_TEST("staging, com.yahoo.vespa.tenant.systemtest.base.StagingTest", true),
-    PRODUCTION_TEST("production, com.yahoo.vespa.tenant.systemtest.base.ProductionTest", false);
+    PRODUCTION_TEST("production, com.yahoo.vespa.tenant.systemtest.base.ProductionTest", true);
 
     private final String group;
     private final boolean failIfNoTests;
