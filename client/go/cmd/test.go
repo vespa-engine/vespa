f05709eb4179dc562083409797889770c74a5ef0
Martin Polden <mpolden@mpolden.no>
2022-03-01 10:35:06 +0100

    Render parameter in man page

--- client/go/cmd/test.go
+++ client/go/cmd/test.go
@@ -29,7 +29,7 @@ func init() {
 }
 
 var testCmd = &cobra.Command{
-	Use:   "test <tests directory or test file>",
+	Use:   "test test-directory-or-file",
 	Short: "Run a test suite, or a single test",
 	Long: `Run a test suite, or a single test
 
