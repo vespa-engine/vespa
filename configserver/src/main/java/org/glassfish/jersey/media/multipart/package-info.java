763bf5fed51522db0d697f8b68e5ce9ea65d4746
gjoranv <gv@verizonmedia.com>
2022-03-17 13:26:20 +0100

    Add package exports for jersey packages used by controller.

---
+++ configserver/src/main/java/org/glassfish/jersey/media/multipart/package-info.java
@@ -0,0 +1,5 @@
+@ExportPackage(version = @Version(major = 2, minor = 25))
+package org.glassfish.jersey.media.multipart;
+
+import com.yahoo.osgi.annotation.ExportPackage;
+import com.yahoo.osgi.annotation.Version;
