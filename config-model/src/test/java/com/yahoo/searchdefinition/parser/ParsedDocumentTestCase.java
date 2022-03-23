2c584d7918b14c57e204f5f117b890cd6b1f280e
Arne H Juul <arnej@yahooinc.com>
2022-03-23 09:59:07 +0000

    track error message change

--- config-model/src/test/java/com/yahoo/searchdefinition/parser/ParsedDocumentTestCase.java
+++ config-model/src/test/java/com/yahoo/searchdefinition/parser/ParsedDocumentTestCase.java
@@ -21,7 +21,10 @@ public class ParsedDocumentTestCase {
         var e = assertThrows(IllegalArgumentException.class, () ->
                              doc.addField(new ParsedField("zap", stringType)));
         System.err.println("As expected: "+e);
-        assertEquals("document 'foo' error: already has field zap", e.getMessage());
+        assertEquals("document 'foo' error: Duplicate (case insensitively) field 'zap' in document type 'foo'", e.getMessage());
+        e = assertThrows(IllegalArgumentException.class, () ->
+                         doc.addField(new ParsedField("ZAP", stringType)));
+        assertEquals("document 'foo' error: Duplicate (case insensitively) field 'ZAP' in document type 'foo'", e.getMessage());
     }
 
 }
