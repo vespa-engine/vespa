a42f7d37368cda2775c4a68d6393e0edc90d7ebb
Ola Aunrønning <olaa@verizonmedia.com>
2022-03-30 14:00:47 +0200

    Enable access API in public

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/role/Policy.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/role/Policy.java
@@ -190,7 +190,7 @@ enum Policy {
 
     accessRequests(Privilege.grant(Action.all())
             .on(PathGroup.accessRequests, PathGroup.accessRequestApproval)
-            .in(SystemName.PublicCd)),
+            .in(SystemName.PublicCd, SystemName.Public)),
 
     /** Invoice management */
     hostedAccountant(Privilege.grant(Action.all())
