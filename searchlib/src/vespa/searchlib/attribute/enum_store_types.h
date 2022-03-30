b848f308c12f71ef90d829727ddcb838415dda97
Geir Storli <geirst@yahooinc.com>
2022-03-30 12:10:29 +0000

    Implement IMultiValueAttribute interface in MultiValueAttribute.

--- searchlib/src/vespa/searchlib/attribute/enum_store_types.h
+++ searchlib/src/vespa/searchlib/attribute/enum_store_types.h
@@ -2,16 +2,17 @@
 
 #pragma once
 
-#include <vespa/vespalib/datastore/entryref.h>
+#include <vespa/vespalib/datastore/atomic_entry_ref.h>
 #include <vespa/vespalib/stllike/allocator.h>
 #include <vector>
 
 namespace search::enumstore {
 
-using Index = vespalib::datastore::EntryRef;
-using InternalIndex = vespalib::datastore::EntryRefT<22>;
-using IndexVector = std::vector<Index, vespalib::allocator_large<Index>>;
+using AtomicIndex = vespalib::datastore::AtomicEntryRef;
 using EnumHandle = uint32_t;
 using EnumVector = std::vector<uint32_t, vespalib::allocator_large<uint32_t>>;
+using Index = vespalib::datastore::EntryRef;
+using IndexVector = std::vector<Index, vespalib::allocator_large<Index>>;
+using InternalIndex = vespalib::datastore::EntryRefT<22>;
 
 }
