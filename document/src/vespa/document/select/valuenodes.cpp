88e5f78b00b6b2fa1d75ba5d0964430e8c66c760
Henning Baldersheim <balder@yahoo-inc.com>
2022-04-07 10:58:48 +0000

    Move when you can

--- document/src/vespa/document/select/valuenodes.cpp
+++ document/src/vespa/document/select/valuenodes.cpp
@@ -263,7 +263,7 @@ IteratorHandler::onPrimitive(uint32_t fid, const Content& fv) {
     if (!_firstValue && getVariables().empty()) {
         _firstValue = getInternalValue(fv.getValue());
     } else {
-        _values.emplace_back(getVariables(), Value::SP(getInternalValue(fv.getValue()).release()));
+        _values.emplace_back(std::move(getVariables()), Value::SP(getInternalValue(fv.getValue()).release()));
     }
 }
 
