78255bac1f7a7314dbdfd2bdaa8107fdc7e9b1d3
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2022-04-20 11:05:58 +0200

    Include dashboard url in notification messages

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/notify/Notifier.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/notify/Notifier.java
@@ -1,17 +1,21 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.notify;
 
+import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.Environment;
 import com.yahoo.text.Text;
+import com.yahoo.vespa.hosted.controller.api.integration.deployment.RunId;
 import com.yahoo.vespa.hosted.controller.api.integration.organization.Mail;
 import com.yahoo.vespa.hosted.controller.api.integration.organization.Mailer;
 import com.yahoo.vespa.hosted.controller.api.integration.organization.MailerException;
+import com.yahoo.vespa.hosted.controller.api.integration.zone.ZoneRegistry;
 import com.yahoo.vespa.hosted.controller.notification.Notification;
 import com.yahoo.vespa.hosted.controller.notification.NotificationSource;
 import com.yahoo.vespa.hosted.controller.persistence.CuratorDb;
 import com.yahoo.vespa.hosted.controller.tenant.CloudTenant;
 import com.yahoo.vespa.hosted.controller.tenant.TenantContacts;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -26,12 +30,14 @@ import java.util.stream.Collectors;
  */
 public class Notifier {
     private final CuratorDb curatorDb;
+    private final ZoneRegistry zoneRegistry;
     private final Mailer mailer;
 
     private static final Logger log = Logger.getLogger(Notifier.class.getName());
 
-    public Notifier(CuratorDb curatorDb, Mailer mailer) {
+    public Notifier(CuratorDb curatorDb, ZoneRegistry zoneRegistry, Mailer mailer) {
         this.curatorDb = Objects.requireNonNull(curatorDb);
+        this.zoneRegistry = Objects.requireNonNull(zoneRegistry);
         this.mailer = Objects.requireNonNull(mailer);
     }
 
@@ -88,12 +94,30 @@ public class Notifier {
     }
 
     private Mail mailOf(Notification n, Collection<String> recipients) {
-        var subject = Text.format("[%s] Vespa Notification for %s", n.level().toString().toUpperCase(), n.type().name());
+        var source = n.source();
+        var subject = Text.format("[%s] %s Vespa Notification for %s - %s", n.level().toString().toUpperCase(), n.type().name(), source.tenant(), source.application());
         var body = new StringBuilder();
         body.append("Source: ").append(n.source().toString()).append("\n")
                 .append("\n")
-                .append(String.join("\n", n.messages()));
+                .append(String.join("\n", n.messages()))
+                .append("\n")
+                .append(url(source).toString());
         return new Mail(recipients, subject.toString(), body.toString());
+    }
+
+    private URI url(NotificationSource source) {
+        if (source.application().isPresent() && source.instance().isPresent()) {
+            if (source.jobType().isPresent() && source.runNumber().isPresent()) {
+                return zoneRegistry.dashboardUrl(
+                        new RunId(ApplicationId.from(source.tenant(),
+                                source.application().get(),
+                                source.instance().get()),
+                                source.jobType().get(),
+                                source.runNumber().getAsLong()));
+            }
+            return zoneRegistry.dashboardUrl(ApplicationId.from(source.tenant(), source.application().get(), source.instance().get()));
+        }
+        return zoneRegistry.dashboardUrl(source.tenant());
     }
 
 }
--- controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/ZoneRegistryMock.java
+++ controller-server/src/test/java/com/yahoo/vespa/hosted/controller/integration/ZoneRegistryMock.java
@@ -199,6 +199,11 @@ public class ZoneRegistryMock extends AbstractComponent implements ZoneRegistry 
     }
 
     @Override
+    public URI dashboardUrl(TenantName tenantName) {
+        return URI.create("https://dashboard.tld/" + tenantName);
+    }
+
+    @Override
     public URI dashboardUrl(RunId id) {
         return URI.create("https://dashboard.tld/" + id.application() + "/" + id.type().jobName() + "/" + id.number());
     }
