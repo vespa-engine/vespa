61902beb898b3c84a343706b2c626798f77bb829
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2022-04-24 22:11:32 +0200

    Pretty print application id source, with standard serialization

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/notify/Notifier.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/notify/Notifier.java
@@ -95,7 +95,7 @@ public class Notifier {
 
     private Mail mailOf(Notification n, Collection<String> recipients) {
         var source = n.source();
-        var subject = Text.format("[%s] %s Vespa Notification for %s - %s", n.level().toString().toUpperCase(), n.type().name(), source.tenant(), source.application());
+        var subject = Text.format("[%s] %s Vespa Notification for %s", n.level().toString().toUpperCase(), n.type().name(), applicationIdSource(source));
         var body = new StringBuilder();
         body.append("Source: ").append(n.source().toString()).append("\n")
                 .append("\n")
@@ -103,6 +103,14 @@ public class Notifier {
                 .append("\n")
                 .append(url(source).toString());
         return new Mail(recipients, subject.toString(), body.toString());
+    }
+
+    private String applicationIdSource(NotificationSource source) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(source.tenant().value());
+        source.application().ifPresent(applicationName -> sb.append(".").append(applicationName.value()));
+        source.instance().ifPresent(instanceName -> sb.append(".").append(instanceName.value()));
+        return sb.toString();
     }
 
     private URI url(NotificationSource source) {
