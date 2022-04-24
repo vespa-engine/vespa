081933a076d05498879e5e197f2c3ee843a4b282
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2022-04-24 22:02:22 +0200

    Add support for html content

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/organization/Mail.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/organization/Mail.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * A message with a subject and a nonempty set of recipients.
@@ -16,18 +17,29 @@ public class Mail {
     private final Collection<String> recipients;
     private final String subject;
     private final String message;
+    private final Optional<String> htmlMessage;
 
     public Mail(Collection<String> recipients, String subject, String message) {
+        this(recipients, subject, message, Optional.empty());
+    }
+
+    public Mail(Collection<String> recipients, String subject, String message, String htmlMessage) {
+        this(recipients, subject, message, Optional.of(htmlMessage));
+    }
+
+    Mail(Collection<String> recipients, String subject, String message, Optional<String> htmlMessage) {
         if (recipients.isEmpty())
             throw new IllegalArgumentException("Empty recipient list is not allowed.");
         recipients.forEach(Objects::requireNonNull);
         this.recipients = ImmutableList.copyOf(recipients);
         this.subject = Objects.requireNonNull(subject);
         this.message = Objects.requireNonNull(message);
+        this.htmlMessage = Objects.requireNonNull(htmlMessage);
     }
 
     public Collection<String> recipients() { return recipients; }
     public String subject() { return subject; }
     public String message() { return message; }
+    public Optional<String> htmlMessage() { return htmlMessage; }
 
 }
