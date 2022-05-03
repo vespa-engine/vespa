a5e0f7dcedb8c0fadbd78c59deef773d5d1ce941
jonmv <venstad@gmail.com>
2022-05-03 12:47:54 +0200

    Memoized is @Beta, and update ABI spec for new combiner

--- yolean/src/main/java/com/yahoo/yolean/concurrent/Memoized.java
+++ yolean/src/main/java/com/yahoo/yolean/concurrent/Memoized.java
@@ -1,5 +1,7 @@
 package com.yahoo.yolean.concurrent;
 
+import com.yahoo.api.annotations.Beta;
+
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -13,6 +15,7 @@ import static java.util.Objects.requireNonNull;
  *
  * @author jonmv
  */
+@Beta
 public class Memoized<T, E extends Exception> implements Supplier<T>, AutoCloseable {
 
     /**
