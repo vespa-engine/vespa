19825a5096115d7dc1028a6c7c386933b6cb7f68
Jon Bratseth <bratseth@gmail.com>
2022-05-05 12:58:05 +0200

    A double isn't an identifier

--- config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
+++ config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
@@ -19,6 +19,8 @@ public class SchemaInfoTestCase {
                 "    inputs {" +
                 "      query(foo) tensor<float>(x[10])" +
                 "      query(bar) tensor(key{},x[1000])" +
+                "      query(myDouble1) double" +
+                "      query(myDouble2) double" +
                 "    }" +
                 "  }";
         List<String> schemas = List.of("type1", "type2");
@@ -46,11 +48,15 @@ public class SchemaInfoTestCase {
                 tester.assertRankProfile(schema, 5, "rankfeatures", false, true);
                 var inputs = tester.assertRankProfile(schema, 6, "inputs", false, false);
 
-                assertEquals(2, inputs.input().size());
+                assertEquals(4, inputs.input().size());
                 assertEquals("query(foo)", inputs.input(0).name());
                 assertEquals("tensor<float>(x[10])", inputs.input(0).type());
                 assertEquals("query(bar)", inputs.input(1).name());
                 assertEquals("tensor(key{},x[1000])", inputs.input(1).type());
+                assertEquals("query(myDouble1)", inputs.input(2).name());
+                assertEquals("tensor()", inputs.input(2).type());
+                assertEquals("query(myDouble2)", inputs.input(3).name());
+                assertEquals("tensor()", inputs.input(3).type());
 
                 assertEquals(2, schema.summaryclass().size());
                 assertEquals("default", schema.summaryclass(0).name());
