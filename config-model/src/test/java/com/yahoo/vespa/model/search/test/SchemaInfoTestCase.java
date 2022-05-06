1f33495b8170f41f8d4a6a53d17e4471a464a8a0
Jon Bratseth <bratseth@gmail.com>
2022-05-06 16:31:47 +0200

    Parse mixed tensor short form

--- config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
+++ config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
@@ -14,6 +14,7 @@ public class SchemaInfoTestCase {
     /** Schema-info should contain all schemas, independent of clusters. */
     @Test
     public void requireThatSchemaInfoIsAvailable() {
+        List.of(1.0,2.0,3.0).toArray(new Double[3]);
         String inputs =
                 "  rank-profile inputs {" +
                 "    inputs {" +
@@ -23,7 +24,7 @@ public class SchemaInfoTestCase {
                 "      query(myDouble2) tensor()" +
                 "      query(myMap) tensor(key{}): { label1:1.0,\n \"label2\": 2.0, 'label3': 3.0 }" +
                 "      query(myVector) tensor(x[3]):\n\n[1 ,2.0,3]" +
- //               "      query(myMixed) tensor(key{},x[2]): { key1:[-1.0, 1.1], key2: [1,2]}" +
+                "      query(myMixed) tensor(key{},x[2]): { key1:[-1.0, 1.1], key2: [1,2]}" +
                 "    }" +
                 "  }";
         List<String> schemas = List.of("type1", "type2");
@@ -51,14 +52,14 @@ public class SchemaInfoTestCase {
                 tester.assertRankProfile(schema, 5, "rankfeatures", false, true);
                 var inputs = tester.assertRankProfile(schema, 6, "inputs", false, false);
 
-                assertEquals(6, inputs.input().size());
+                assertEquals(7, inputs.input().size());
                 assertInput("query(foo)", "tensor<float>(x[10])", inputs.input(0));
                 assertInput("query(bar)", "tensor(key{},x[1000])", inputs.input(1));
                 assertInput("query(myDouble1)", "tensor()", inputs.input(2));
                 assertInput("query(myDouble2)", "tensor()", inputs.input(3));
                 assertInput("query(myMap)", "tensor(key{})", inputs.input(4));
                 assertInput("query(myVector)", "tensor(x[3])", inputs.input(5));
- //               assertInput("query(myMixed)", "tensor(key{},x[2])", inputs.input(5));
+                assertInput("query(myMixed)", "tensor(key{},x[2])", inputs.input(6));
 
                 assertEquals(2, schema.summaryclass().size());
                 assertEquals("default", schema.summaryclass(0).name());
