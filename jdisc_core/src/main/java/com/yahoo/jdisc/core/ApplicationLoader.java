922d6893b7a6605c69ece9981b17224c6a1d9f37
gjoranv <gv@verizonmedia.com>
2022-05-09 17:57:24 +0200

    Reapply setup of vespalog in jdisc_core (#22509)
    Revert "Merge pull request #22515 from vespa-engine/revert-22509-setup-vespalog-in-jdisc_core"

--- jdisc_core/src/main/java/com/yahoo/jdisc/core/ApplicationLoader.java
+++ jdisc_core/src/main/java/com/yahoo/jdisc/core/ApplicationLoader.java
@@ -16,6 +16,8 @@ import com.yahoo.jdisc.application.OsgiHeader;
 import com.yahoo.jdisc.service.ContainerNotReadyException;
 import com.yahoo.jdisc.service.CurrentContainer;
 import com.yahoo.jdisc.statistics.ContainerWatchdogMetrics;
+import com.yahoo.log.impl.InitializeLog;
+import com.yahoo.log.LogSetup;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -46,6 +48,7 @@ public class ApplicationLoader implements BootstrapLoader, ContainerActivator, C
     private ApplicationInUseTracker applicationInUseTracker;
 
     public ApplicationLoader(OsgiFramework osgiFramework, Iterable<? extends Module> guiceModules) {
+        InitializeLog.init();
         this.osgiFramework = osgiFramework;
         this.guiceModules.install(new ApplicationEnvironmentModule(this));
         this.guiceModules.installAll(guiceModules);
@@ -198,6 +201,7 @@ public class ApplicationLoader implements BootstrapLoader, ContainerActivator, C
         try {
             watchdog.close();
             osgiFramework.stop();
+            LogSetup.cleanup();
         } catch (BundleException | InterruptedException e) {
             e.printStackTrace();
         }
