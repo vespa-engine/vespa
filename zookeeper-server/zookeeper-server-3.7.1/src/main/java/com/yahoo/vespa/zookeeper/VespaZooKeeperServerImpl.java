162abbd04eb3062451415bab1117be11b19c5bf6
Harald Musum <musum@yahooinc.com>
2022-05-11 09:31:37 +0200

    Add support for zookeeper server version 3.7.1

---
+++ zookeeper-server/zookeeper-server-3.7.1/src/main/java/com/yahoo/vespa/zookeeper/VespaZooKeeperServerImpl.java
@@ -0,0 +1,47 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.zookeeper;
+
+import com.yahoo.component.annotation.Inject;
+import com.yahoo.cloud.config.ZookeeperServerConfig;
+import com.yahoo.component.AbstractComponent;
+
+import java.nio.file.Path;
+import java.time.Duration;
+
+/**
+ * @author Ulf Lilleengen
+ * @author Harald Musum
+ */
+public class VespaZooKeeperServerImpl extends AbstractComponent implements VespaZooKeeperServer {
+
+    private final VespaQuorumPeer peer;
+    private final ZooKeeperRunner runner;
+
+    @Inject
+    public VespaZooKeeperServerImpl(ZookeeperServerConfig zookeeperServerConfig) {
+        this.peer = new VespaQuorumPeer();
+        this.runner = new ZooKeeperRunner(zookeeperServerConfig, this);
+    }
+
+    @Override
+    public void deconstruct() {
+        runner.shutdown();
+        super.deconstruct();
+    }
+
+    @Override
+    public void shutdown() {
+        peer.shutdown(Duration.ofMinutes(1));
+    }
+
+    @Override
+    public void start(Path configFilePath) {
+        peer.start(configFilePath);
+    }
+
+    @Override
+    public boolean reconfigurable() {
+        return false;
+    }
+
+}
