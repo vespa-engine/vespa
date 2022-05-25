4b976b7218be5f9c05e4ea3d6254ef346ef23a84
Tor Brede Vekterli <vekterli@yahooinc.com>
2022-05-25 13:49:56 +0000

    Add support for cross-thread stack dumping to vespalib
    There's no existing API for conveniently getting the stack trace of any other thread
    than your own, so we have to turn things around a bit and make the target thread
    dump its own stack in a way that we can then safely access and return it.
    
    We allow for hooking SIGUSR2 at process startup time and use this as our dedicated
    stack dump signal internally. The process of stack dumping is then basically:
    
     1. Caller sends SIGUSR2 to the target thread. This happens within a global mutex
        that ensures no other callers can attempt to dump stack at the same time. This
        is because we use a single, globally shared state between caller and target
        (the same signal handler function is used by all threads).
     2. Caller acquire-polls (with 1ms sleep) for target thread signal handler completion.
        Fancy technologies such as mutexes and condition variables are not safe to use
        from within a signal handler and therefore cannot be used.
     3. Target thread suddenly finds itself in Narnia (signal handler). Since the
        signal is the magical SIGUSR2, it proceeds to dump its stack frame addresses in
        a shared buffer. It then toggles completion, with release semantics, before
        returning to its regularly scheduled programming.
     4. Caller exits poll-loop and assembles a complete stack trace from the frame
        addresses in the shared buffer, demangled and shiny.
    
    Signal-safe stack frame dumping currently uses Boost.Stacktrace, which is a
    header-only dependency and appears to use libunwind under the hood.

--- vespalib/src/tests/signalhandler/signalhandler_test.cpp
+++ vespalib/src/tests/signalhandler/signalhandler_test.cpp
@@ -1,20 +1,20 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/vespalib/util/signalhandler.h>
+#include <vespa/vespalib/gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <latch>
+#include <thread>
 #include <unistd.h>
 
 #include <vespa/log/log.h>
 LOG_SETUP("signalhandler_test");
 
 using namespace vespalib;
+using namespace ::testing;
 
-TEST_SETUP(Test);
-
-int
-Test::Main()
+TEST(SignalHandlerTest, signal_handler_can_intercept_hooked_signals)
 {
-    TEST_INIT("signalhandler_test");
     EXPECT_TRUE(!SignalHandler::INT.check());
     EXPECT_TRUE(!SignalHandler::TERM.check());
     SignalHandler::INT.ignore();
@@ -32,6 +32,60 @@ Test::Main()
     SignalHandler::TERM.clear();
     EXPECT_TRUE(!SignalHandler::INT.check());
     EXPECT_TRUE(!SignalHandler::TERM.check());
-    EXPECT_EQUAL(0, system("res=`./vespalib_victim_app`; test \"$res\" = \"GOT TERM\""));
-    TEST_DONE();
+    EXPECT_EQ(0, system("res=`./vespalib_victim_app`; test \"$res\" = \"GOT TERM\""));
 }
+
+void my_cool_function(std::latch&, std::latch&) __attribute__((noinline));
+
+// Could have used a single std::barrier<no op functor> here, but when using explicit
+// phase latches it sort of feels like the semantics are more immediately obvious.
+void my_cool_function(std::latch& arrival_latch, std::latch& departure_latch) {
+    arrival_latch.arrive_and_wait();
+    // Twiddle thumbs in departure latch until main test thread has dumped our stack
+    departure_latch.arrive_and_wait();
+}
+
+TEST(SignalHandlerTest, can_dump_stack_of_another_thread)
+{
+    SignalHandler::enable_cross_thread_stack_tracing();
+
+    std::latch arrival_latch(2);
+    std::latch departure_latch(2);
+
+    std::thread t([&]{
+        my_cool_function(arrival_latch, departure_latch);
+    });
+    arrival_latch.arrive_and_wait();
+
+    auto trace = SignalHandler::get_cross_thread_stack_trace(t.native_handle());
+    EXPECT_THAT(trace, HasSubstr("my_cool_function"));
+
+    departure_latch.arrive_and_wait();
+    t.join();
+}
+
+TEST(SignalHandlerTest, dumping_stack_of_an_ex_thread_does_not_crash)
+{
+    SignalHandler::enable_cross_thread_stack_tracing();
+    std::thread t([]() noexcept {
+        // Do a lot of nothing at all.
+    });
+    auto tid = t.native_handle();
+    t.join();
+    auto trace = SignalHandler::get_cross_thread_stack_trace(tid);
+    EXPECT_EQ(trace, "(pthread_kill() failed; could not get backtrace)");
+}
+
+string my_totally_tubular_and_groovy_function() __attribute__((noinline));
+string my_totally_tubular_and_groovy_function() {
+    return SignalHandler::get_cross_thread_stack_trace(pthread_self());
+}
+
+TEST(SignalHandlerTest, can_get_stack_trace_of_own_thread)
+{
+    SignalHandler::enable_cross_thread_stack_tracing(); // Technically not cross-thread in this case...!
+    auto trace = my_totally_tubular_and_groovy_function();
+    EXPECT_THAT(trace, HasSubstr("my_totally_tubular_and_groovy_function"));
+}
+
+GTEST_MAIN_RUN_ALL_TESTS()
--- vespalib/src/vespa/vespalib/util/signalhandler.h
+++ vespalib/src/vespa/vespalib/util/signalhandler.h
@@ -1,9 +1,11 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
 
+#include <vespa/vespalib/stllike/string.h>
 #include <csignal>
 #include <vector>
 #include <atomic>
+#include <pthread.h>
 
 namespace vespalib {
 
@@ -43,26 +45,41 @@ private:
      * dispatch the signal to the appropriate static signal handler
      * instance.
      *
+     * noinline to ensure consistent number of frames to skip for back-tracing.
+     *
      * @param signal the caught signal
      **/
-    static void handleSignal(int signal);
+    static void handleSignal(int signal) noexcept __attribute__((noinline));
 
     /**
      * This method is invoked by the common signal handling method to
      * dispatch the signal to the right static signal handler
      * instance.
+     *
+     * noinline to ensure consistent number of frames to skip for back-tracing.
      **/
-    void gotSignal();
+    void gotSignal() noexcept __attribute__((noinline));
+
+
+    /**
+     * Internal async signal-safe function used to dump frame addresses of a signal-interrupted
+     * thread to a shared buffer that will be read by the signalling thread.
+     *
+     * noinline to ensure consistent number of frames to skip for back-tracing.
+     */
+    static void dump_current_thread_stack_to_shared_state() noexcept __attribute__((noinline));
 
     /**
      * Create a signal handler for the given signal. This method is
      * private as all signal handlers should be created during
      * initialization.
      **/
-    SignalHandler(int signal);
+    explicit SignalHandler(int signal);
 
     SignalHandler(const SignalHandler &) = delete;
     SignalHandler &operator=(const SignalHandler &) = delete;
+
+    static SignalHandler USR2;
 
 public:
     static SignalHandler HUP;
@@ -105,6 +122,28 @@ public:
      * Stop catching this signal
      **/
     void unhook();
+
+    /**
+     * Hook in signal handler for cross-thread stack tracing.
+     *
+     * Must be called at application init time if cross-thread tracing is wanted.
+     */
+    static void enable_cross_thread_stack_tracing();
+
+    /**
+     * Get the stack trace of the current point of execution of the thread referenced
+     * by `thread_id`. This may be the same ID as the calling thread, in which case
+     * the current call stack is returned.
+     *
+     * Returned format is the same as that of vespalib::getStackTrace().
+     *
+     * Requires enable_cross_thread_stack_tracing() to have been called prior to creating
+     * the thread referenced by `thread_id`.
+     *
+     * Due to potentially heavy internal synchronization overhead, this is not a function
+     * that should be used in any kind of hot code path. Intended for debugging purposes.
+     */
+    static string get_cross_thread_stack_trace(pthread_t thread_id);
 
     static void shutdown();
 };
