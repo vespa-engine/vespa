04dbb36b74897819026d1f52836be069acab77bf
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2022-06-01 11:54:14 +0200

    Stop exporting org.json (including removal of deprecated APIs)

--- container-core/src/main/java/org/json/package-info.java
+++
@@ -1,5 +0,0 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage
-package org.json;
-// TODO Vespa 8: stop providing org.json
-import com.yahoo.osgi.annotation.ExportPackage;
