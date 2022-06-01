f275ced68bab98c4366f6d01f65f6c41c3ce5bd1
Jon Bratseth <bratseth@gmail.com>
2022-06-01 13:47:25 +0200

    Remove on Vespa 8

--- config-model/src/main/java/com/yahoo/vespa/model/generic/service/package-info.java
+++
@@ -1,5 +0,0 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage
-package com.yahoo.vespa.model.generic.service;
-
-import com.yahoo.osgi.annotation.ExportPackage;
