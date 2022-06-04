ae9e16a7a1164701e2715076d87af5cb27ce91ad
gjoranv <gv@verizonmedia.com>
2022-06-04 15:11:54 +0200

    Stop providing org.lz4:lz4-java and exporting net.jpountz packages
    - Not leaked from any public apis.
    - Must be added to container-test for unit tests with application

--- vespajlib/src/main/java/net/jpountz/lz4/package-info.java
+++
@@ -1,5 +0,0 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 1, minor = 7, micro = 1))
-package net.jpountz.lz4;
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
