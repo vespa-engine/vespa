64ccd645f8a88c0e452202416cf6f51f1ab6bb9b
Harald Musum <musum@yahooinc.com>
2022-06-05 16:16:31 +0200

    Don't use operation= in array user config
    Remove unnecessary config in one test, update other test to have
    2 items in list

--- config-model/src/main/java/com/yahoo/vespa/model/builder/UserConfigBuilder.java
+++ config-model/src/main/java/com/yahoo/vespa/model/builder/UserConfigBuilder.java
@@ -1,16 +1,19 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.model.builder;
 
-import com.yahoo.config.model.deploy.ConfigDefinitionStore;
 import com.yahoo.config.application.api.DeployLogger;
+import com.yahoo.config.model.deploy.ConfigDefinitionStore;
 import com.yahoo.config.model.producer.UserConfigRepo;
-import java.util.logging.Level;
 import com.yahoo.text.XML;
-import com.yahoo.vespa.config.*;
+import com.yahoo.vespa.config.ConfigDefinition;
+import com.yahoo.vespa.config.ConfigDefinitionKey;
+import com.yahoo.vespa.config.ConfigPayloadBuilder;
 import com.yahoo.vespa.model.builder.xml.dom.DomConfigPayloadBuilder;
 import org.w3c.dom.Element;
-
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -39,7 +42,7 @@ public class UserConfigBuilder {
             logger.logApplicationPackage(Level.WARNING, "Unable to find config definition '" + key.asFileName() +
                                          "'. Please ensure that the name is spelled correctly, and that the def file is included in a bundle.");
         }
-        ConfigPayloadBuilder payloadBuilder = new DomConfigPayloadBuilder(def.orElse(null), logger).build(element);
+        ConfigPayloadBuilder payloadBuilder = new DomConfigPayloadBuilder(def.orElse(null)).build(element);
         ConfigPayloadBuilder old = builderMap.get(key);
         if (old != null) {
             logger.logApplicationPackage(Level.WARNING, "Multiple overrides for " + key + " found. Applying in the order they are discovered");
