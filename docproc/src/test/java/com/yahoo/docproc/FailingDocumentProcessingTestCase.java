651306b9172bf91ed278fb717f2ab78f0e331022
gjoranv <gv@verizonmedia.com>
2022-06-07 12:33:32 +0200

    Revoke deprecated implementation classes in c.y.docproc from public

--- docproc/src/test/java/com/yahoo/docproc/FailingDocumentProcessingTestCase.java
+++ docproc/src/test/java/com/yahoo/docproc/FailingDocumentProcessingTestCase.java
@@ -1,6 +1,8 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.docproc;
 
+import com.yahoo.docproc.impl.DocprocService;
+import com.yahoo.docproc.impl.HandledProcessingException;
 import com.yahoo.document.DataType;
 import com.yahoo.document.DocumentId;
 import com.yahoo.document.DocumentPut;
