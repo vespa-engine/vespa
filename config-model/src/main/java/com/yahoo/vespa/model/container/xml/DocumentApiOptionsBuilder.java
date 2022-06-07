c4ee8c6f7fad73483a38692bc2411a1422f7644b
Jon Bratseth <bratseth@gmail.com>
2022-06-07 09:10:29 +0200

    Remove feed handler setup

--- config-model/src/main/java/com/yahoo/vespa/model/container/xml/DocumentApiOptionsBuilder.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/xml/DocumentApiOptionsBuilder.java
@@ -3,7 +3,6 @@ package com.yahoo.vespa.model.container.xml;
 
 import com.yahoo.text.XML;
 import com.yahoo.vespa.model.clients.ContainerDocumentApi;
-import com.yahoo.vespa.model.container.ContainerThreadpool;
 import org.w3c.dom.Element;
 
 import java.util.ArrayList;
@@ -20,13 +19,7 @@ public class DocumentApiOptionsBuilder {
 
 
     public static ContainerDocumentApi.Options build(Element spec) {
-        return new ContainerDocumentApi.Options(getBindings(spec), threadpoolOptions(spec, "http-client-api"));
-    }
-
-    private static ContainerThreadpool.UserOptions threadpoolOptions(Element spec, String elementName) {
-        Element element = XML.getChild(spec, elementName);
-        if (element == null) return null;
-        return ContainerThreadpool.UserOptions.fromXml(element).orElse(null);
+        return new ContainerDocumentApi.Options(getBindings(spec));
     }
 
     private static List<String> getBindings(Element spec) {
