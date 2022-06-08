29b0a8fdf226fb19b5f3da7d72923671c20d7e66
Arne H Juul <arnej@yahooinc.com>
2022-06-08 08:31:50 +0000

    disallow bad position field

--- searchlib/src/tests/features/prod_features.cpp
+++ searchlib/src/tests/features/prod_features.cpp
@@ -483,8 +483,6 @@ Test::testCloseness()
         TEST_DO(assertCloseness(1,   "pos", 0));
         assertCloseness(0.8, "pos", 1802661);
         assertCloseness(0,   "pos", 9013306);
-        // use non-existing attribute -> default distance
-        TEST_DO(assertCloseness(0, "no", 0));
         // two-argument version
         TEST_DO(assertCloseness(0.8, "field,pos", 1802661));
 
@@ -893,8 +891,10 @@ Test::testDistance()
 
         StringList params, in, out;
         FT_SETUP_FAIL(pt, params);
+	/*
         FT_SETUP_OK(pt, params.add("pos"), in,
                     out.add("out").add("index").add("latitude").add("longitude").add("km"));
+		    */
         FT_DUMP_EMPTY(_factory, "distance");
     }
 
