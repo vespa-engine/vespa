a2ae7d4cd039aac590003ec06dff5d407c659ad6
jonmv <venstad@gmail.com>
2022-06-11 08:48:02 +0200

    Rename to previsouly used names

--- vespa-osgi-testrunner/src/test/java/com/yahoo/vespa/test/samples/UsingTestRuntimeTest.java
+++ vespa-osgi-testrunner/src/test/java/com/yahoo/vespa/test/samples/UsingTestRuntimeTest.java
@@ -17,8 +17,8 @@ public class UsingTestRuntimeTest {
         assertEquals(Environment.test, runtime.zone().environment());
         assertEquals("name", runtime.zone().region());
         assertNull(runtime.deploymentToTest().endpoint("dummy"));
-        assertEquals("1.2.3", runtime.deploymentToTest().platformVersion());
-        assertEquals(321, runtime.deploymentToTest().applicationVersion());
+        assertEquals("1.2.3", runtime.deploymentToTest().platform());
+        assertEquals(321, runtime.deploymentToTest().revision());
         assertEquals(1000, runtime.deploymentToTest().deployedAt().toEpochMilli());
     }
 
