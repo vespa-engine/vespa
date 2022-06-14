ba9bfeea242c9151468b2f2d599ffc9ede5dfd5b
Harald Musum <musum@yahooinc.com>
2022-06-14 10:27:02 +0200

    Set property clustertype for container cluster

--- config-model/src/main/java/com/yahoo/vespa/model/container/ContainerCluster.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/ContainerCluster.java
@@ -282,7 +282,8 @@ public abstract class ContainerCluster<CONTAINER extends Container>
         container.setOwner(this);
         container.setClusterName(name);
         container.setProp("clustername", name)
-                 .setProp("index", this.containers.size());
+                 .setProp("index", this.containers.size())
+                 .setProp("clustertype", "container");
         containers.add(container);
     }
 
