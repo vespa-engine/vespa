55363f1f98563d808b65e4868529b8bfbfb3f623
Leandro Alves <leandroalves@yahooinc.com>
2022-06-20 21:04:04 +0200

    Setting absolute import

--- client/js/app/vite.config.js
+++ client/js/app/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    sourcemap: 'hidden',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          return id.includes('node_modules') ? 'vendor' : 'main';
+        },
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, 'src/app'),
+    },
+  },
+});
