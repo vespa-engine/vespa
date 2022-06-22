9be2d3ba0d7e7a68d99bad06f514dfe8cff03ed5
Leandro Alves <leandroalves@yahooinc.com>
2022-06-22 12:47:27 +0200

    Add basic app router structure

---
+++ client/js/app/src/app/components/index.js
@@ -0,0 +1,2 @@
+export { App } from 'app/components/layout/app';
+export { Error } from 'app/components/layout/error';
---
+++ client/js/app/src/app/pages/query-builder/index.js
@@ -0,0 +1,1 @@
+export { QueryBuilder } from 'app/pages/query-builder/query-builder';
