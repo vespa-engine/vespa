4bf4fd5e9d6ec183cd4dc640106e122837f8a47d
Leandro Alves <leandroalves@yahooinc.com>
2022-06-23 13:42:17 +0200

    Add app provider

---
+++ client/js/app/src/app/libs/app-provider.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Global, MantineProvider } from '@mantine/core';
+import { defaultProps, defaultStyles } from 'app/styles/default';
+import { Colors } from 'app/styles/theme/colors';
+import { styles } from 'app/styles/global';
+import { getTheme } from 'app/styles/theme';
+
+function setColorResolver(theme) {
+  if (!theme.cr) theme.cr = new Colors(theme);
+  return theme;
+}
+
+export function AppProvider({ children }) {
+  return (
+    <MantineProvider
+      styles={defaultStyles}
+      defaultProps={defaultProps}
+      theme={getTheme()}
+    >
+      <Global styles={(theme) => styles(setColorResolver(theme))} />
+      {children}
+    </MantineProvider>
+  );
+}
