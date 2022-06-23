450408fa9a9d75a19f5ee5480db195892f5a206d
Leandro Alves <leandroalves@yahooinc.com>
2022-06-23 16:37:14 +0200

    Fix router navigation

--- client/js/app/src/app/main.jsx
+++ client/js/app/src/app/main.jsx
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'app/app';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
