d475ef8fd2a504b4a80926b65036cb08eb709a4e
Valerij Fredriksen <valerijf@yahooinc.com>
2022-06-27 12:02:55 +0200

    Create CSP response filter

---
+++ jdisc-security-filters/src/main/java/com/yahoo/jdisc/http/filter/security/csp/package-info.java
@@ -0,0 +1,8 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * @author freva
+ */
+@ExportPackage
+package com.yahoo.jdisc.http.filter.security.csp;
+
+import com.yahoo.osgi.annotation.ExportPackage;
