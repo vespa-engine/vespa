02a376fea1b1f11e45fdabfde8e96cebe3b83316
Bjørn Christian Seime <bjorn.christian@seime.no>
2022-06-29 17:56:58 +0200

    Revert "Bjorncs/application status handler"

--- container-search-and-docproc/src/test/java/com/yahoo/container/handler/observability/ApplicationStatusHandlerTest.java
+++ container-search-and-docproc/src/test/java/com/yahoo/container/handler/observability/ApplicationStatusHandlerTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * @author gjoranv
+ * @since 5.1.10
  */
 public class ApplicationStatusHandlerTest {
 
@@ -113,7 +114,7 @@ public class ApplicationStatusHandlerTest {
         Chain<Processor> chain = new Chain<>("myChain", new VoidProcessor(new ComponentId("voidProcessor")));
         chains.register(new ComponentId("myChain"), chain);
 
-        String json = ApplicationStatusHandler.renderChains(chains).toString();
+        String json = ApplicationStatusHandler.StatusResponse.renderChains(chains).toString();
         assertTrue(json.contains("myChain"));
         assertTrue(json.contains("voidProcessor"));
     }
