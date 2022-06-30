1171ecf2630f31e22f8f837095abc6f14ce669c8
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2022-06-30 11:35:01 +0200

    Reapply "Bjorncs/application status handler""

--- docproc/src/main/java/com/yahoo/docproc/jdisc/observability/package-info.java
+++ docproc/src/main/java/com/yahoo/docproc/jdisc/observability/package-info.java
@@ -1,5 +1,8 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+/**
+ * @author bjorncs
+ */
 @ExportPackage
-package com.yahoo.container.handler.observability;
+package com.yahoo.docproc.jdisc.observability;
 
 import com.yahoo.osgi.annotation.ExportPackage;
