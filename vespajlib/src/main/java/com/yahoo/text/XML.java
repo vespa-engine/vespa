1bf2a5b8a548db3f24e9da243b59f84d08503017
Andreas Eriksen <andreer@yahooinc.com>
2022-08-05 21:46:49 +0200

    use namespace unaware parser for bundle validator

--- vespajlib/src/main/java/com/yahoo/text/XML.java
+++ vespajlib/src/main/java/com/yahoo/text/XML.java
@@ -452,7 +452,17 @@ public class XML {
      * @throws RuntimeException if we fail to create one
      */
     public static DocumentBuilder getDocumentBuilder() {
-        return getDocumentBuilder("com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl", null);
+        return getDocumentBuilder("com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl", null, true);
+    }
+
+    /**
+     * Creates a new XML DocumentBuilder
+     *
+     * @return a DocumentBuilder
+     * @throws RuntimeException if we fail to create one
+     */
+    public static DocumentBuilder getDocumentBuilder(boolean namespaceAware) {
+        return getDocumentBuilder("com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl", null, namespaceAware);
     }
 
     /**
@@ -463,10 +473,10 @@ public class XML {
      * @throws RuntimeException if we fail to create one
      * @return a DocumentBuilder
      */
-    public static DocumentBuilder getDocumentBuilder(String implementation, ClassLoader classLoader) {
+    public static DocumentBuilder getDocumentBuilder(String implementation, ClassLoader classLoader, boolean namespaceAware) {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(implementation, classLoader);
-            factory.setNamespaceAware(true);
+            factory.setNamespaceAware(namespaceAware);
             // Disable include directives. If enabled this allows inclusion of any resource, such as file:/// and
             // http:///, and these are read even if the document eventually fails to parse
             factory.setXIncludeAware(false);
