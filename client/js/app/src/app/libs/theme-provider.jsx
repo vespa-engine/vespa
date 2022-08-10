1d6f5e3fb129649d5d04da1bf472d94889a0c775
Leandro Alves <leandroalves@yahooinc.com>
2022-08-10 23:13:43 +0200

    Upgrade Mantine

--- client/js/app/src/app/libs/theme-provider.jsx
+++ client/js/app/src/app/libs/theme-provider.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Global, MantineProvider } from '@mantine/core';
-import { defaultProps, defaultStyles } from 'app/styles/default';
 import { Colors } from 'app/styles/theme/colors';
 import { styles } from 'app/styles/global';
 import { getTheme } from 'app/styles/theme';
@@ -12,11 +11,7 @@ function setColorResolver(theme) {
 
 export function ThemeProvider({ children }) {
   return (
-    <MantineProvider
-      styles={defaultStyles}
-      defaultProps={defaultProps}
-      theme={getTheme()}
-    >
+    <MantineProvider theme={getTheme()}>
       <Global styles={(theme) => styles(setColorResolver(theme))} />
       {children}
     </MantineProvider>
