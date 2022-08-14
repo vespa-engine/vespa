e38ba25788c12ed42aaf6f6083ba7ddffa4fc04e
Leandro Alves <leandroalves@yahooinc.com>
2022-08-14 23:23:46 +0200

    Fix monospace font in textarea for query-tracer

--- client/js/app/src/app/pages/querytracer/query-tracer.jsx
+++ client/js/app/src/app/pages/querytracer/query-tracer.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
-import { Textarea } from '@mantine/core';
-import { Container } from 'app/components';
+import { Stack, Textarea } from '@mantine/core';
 import { DownloadJeager } from 'app/pages/querybuilder/query-response/download-jeager';
 
 export function QueryTracer() {
   const [response, setResponse] = useState('');
 
   return (
-    <Container>
+    <Stack>
       <Textarea
-        styles={{
-          root: { height: '100%' },
-          wrapper: { height: '100%' },
-          input: { height: '100%' },
-        }}
+        styles={(theme) => ({
+          input: {
+            fontFamily: theme.fontFamilyMonospace,
+            fontSize: theme.fontSizes.xs,
+          },
+        })}
         minRows={21}
         autosize
         value={response}
         onChange={({ target }) => setResponse(target.value)}
-      ></Textarea>
+      />
       <DownloadJeager fullWidth response={response} />
-    </Container>
+    </Stack>
   );
 }
