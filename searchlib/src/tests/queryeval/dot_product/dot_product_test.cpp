2831803a3af7e8802fd34df881d74e39e6ad1e10
Henning Baldersheim <balder@yahoo-inc.com>
2022-08-15 12:50:33 +0000

    Add clarifying comment.

--- searchlib/src/tests/queryeval/dot_product/dot_product_test.cpp
+++ searchlib/src/tests/queryeval/dot_product/dot_product_test.cpp
@@ -263,7 +263,10 @@ TEST_F("test Eager Matching Children", MockFixture(5)) {
 
 class IteratorChildrenVerifier : public search::test::IteratorChildrenVerifier {
 private:
-    SearchIterator::UP create(const std::vector<SearchIterator*> &children) const override {
+    SearchIterator::UP
+    create(const std::vector<SearchIterator*> &children) const override {
+        // This is a pragmatic and dirty workaround to make IteratorVerifier test
+        // not fail on unpack when accessing child match weights
         std::vector<fef::TermFieldMatchData*> no_child_match(children.size(), &_tfmd);
         MatchData::UP no_match_data;
         return DotProductSearch::create(children, _tfmd, false, no_child_match, _weights, std::move(no_match_data));
@@ -272,7 +275,8 @@ private:
 
 class WeightIteratorChildrenVerifier : public search::test::DwaIteratorChildrenVerifier {
 private:
-    SearchIterator::UP create(std::vector<DocumentWeightIterator> && children) const override {
+    SearchIterator::UP
+    create(std::vector<DocumentWeightIterator> && children) const override {
         return DotProductSearch::create(_tfmd, false, _weights, std::move(children));
     }
 };
