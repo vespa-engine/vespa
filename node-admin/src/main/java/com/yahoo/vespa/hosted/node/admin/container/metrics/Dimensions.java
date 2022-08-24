96d6c9b0024e32e1c32bd6533c3672a9c8289cd1
Håkon Hallingstad <hakon@yahooinc.com>
2022-08-24 12:31:00 +0200

    Purge old containers dimension value

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/container/metrics/Dimensions.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/container/metrics/Dimensions.java
@@ -7,37 +7,12 @@ import java.util.Map;
 /**
  * @author freva
  */
-public class Dimensions {
+public record Dimensions(Map<String, String> dimensionsMap) {
 
     public static final Dimensions NONE = new Dimensions(Map.of());
 
-    private final Map<String, String> dimensionsMap;
-
     public Dimensions(Map<String, String> dimensionsMap) {
         this.dimensionsMap = Map.copyOf(dimensionsMap);
-    }
-
-    public Map<String, String> asMap() {
-        return dimensionsMap;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Dimensions that = (Dimensions) o;
-        return dimensionsMap.equals(that.dimensionsMap);
-    }
-
-    @Override
-    public int hashCode() {
-        return dimensionsMap.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return dimensionsMap.toString();
     }
 
     public static class Builder {
