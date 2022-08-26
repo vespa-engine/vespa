511de9a6cf21a8d31269016e4dd6ff6b9838df8d
Harald Musum <musum@yahooinc.com>
2022-08-26 14:56:06 +0200

    Use ZooKeeper server 3.8.0 by default

--- flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
@@ -130,7 +130,7 @@ public class PermanentFlags {
             HOSTNAME, NODE_TYPE, TENANT_ID, APPLICATION_ID, CLUSTER_TYPE, CLUSTER_ID, VESPA_VERSION);
 
     public static final UnboundStringFlag ZOOKEEPER_SERVER_VERSION = defineStringFlag(
-            "zookeeper-server-version", "3.7.1",  // Note: Nodes running Vespa 7 have 3.7.1 as the only available version
+            "zookeeper-server-version", "3.8.0",  // Note: Nodes running Vespa 7 have 3.7.1 as the only available version
             "ZooKeeper server version, a jar file zookeeper-server-<ZOOKEEPER_SERVER_VERSION>-jar-with-dependencies.jar must exist",
             "Takes effect on restart of Docker container",
             NODE_TYPE, APPLICATION_ID, HOSTNAME);
