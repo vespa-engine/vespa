dcebb3d6d0cc8bd0f0b301d992dfd7764e6f74c8
Martin Polden <mpolden@mpolden.no>
2022-08-30 12:42:13 +0200

    Avoid doing real DNS lookups in unit test

--- client/go/vespa/detect_hostname_test.go
+++ client/go/vespa/detect_hostname_test.go
@@ -3,6 +3,7 @@ package vespa
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"strings"
 	"testing"
@@ -11,20 +12,28 @@ import (
 )
 
 func TestDetectHostname(t *testing.T) {
+	lookupAddr := func(addr string) ([]string, error) {
+		return nil, fmt.Errorf("could not look up %s", addr)
+	}
+	lookupIP := func(host string) ([]net.IP, error) {
+		return nil, fmt.Errorf("no address found for %s", host)
+	}
+
 	t.Setenv("VESPA_HOSTNAME", "foo.bar")
-	got, err := FindOurHostname()
+	got, err := findOurHostname(lookupAddr, lookupIP)
+	assert.Nil(t, err)
 	assert.Equal(t, "foo.bar", got)
 	os.Unsetenv("VESPA_HOSTNAME")
-	got, err = findOurHostnameFrom("bar.foo.123")
+	got, err = findOurHostnameFrom("bar.foo.123", lookupAddr, lookupIP)
 	fmt.Fprintln(os.Stderr, "findOurHostname from bar.foo.123 returns:", got, "with error:", err)
 	assert.NotEqual(t, "", got)
 	parts := strings.Split(got, ".")
 	if len(parts) > 1 {
-		expanded, err2 := findOurHostnameFrom(parts[0])
+		expanded, err2 := findOurHostnameFrom(parts[0], lookupAddr, lookupIP)
 		fmt.Fprintln(os.Stderr, "findOurHostname from", parts[0], "returns:", expanded, "with error:", err2)
 		assert.Equal(t, got, expanded)
 	}
-	got, err = FindOurHostname()
+	got, err = findOurHostname(lookupAddr, lookupIP)
 	assert.NotEqual(t, "", got)
-	fmt.Fprintln(os.Stderr, "FindOurHostname() returns:", got, "with error:", err)
+	fmt.Fprintln(os.Stderr, "findOurHostname() returns:", got, "with error:", err)
 }
