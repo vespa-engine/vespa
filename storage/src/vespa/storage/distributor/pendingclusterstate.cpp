947b8c17af4ed513ce22615ddd595527985df46a
Tor Brede Vekterli <vekterli@yahooinc.com>
2022-09-14 14:17:46 +0000

    Add internal content node feature for not implicitly indexing active buckets

--- storage/src/vespa/storage/distributor/pendingclusterstate.cpp
+++ storage/src/vespa/storage/distributor/pendingclusterstate.cpp
@@ -340,8 +340,9 @@ PendingClusterState::update_node_supported_features_from_reply(uint16_t node, co
 {
     const auto& src_feat = reply.supported_node_features();
     NodeSupportedFeatures dest_feat;
-    dest_feat.unordered_merge_chaining  = src_feat.unordered_merge_chaining;
-    dest_feat.two_phase_remove_location = src_feat.two_phase_remove_location;
+    dest_feat.unordered_merge_chaining               = src_feat.unordered_merge_chaining;
+    dest_feat.two_phase_remove_location              = src_feat.two_phase_remove_location;
+    dest_feat.no_implicit_indexing_of_active_buckets = src_feat.no_implicit_indexing_of_active_buckets;
     // This will overwrite per bucket-space reply, but does not matter since it's independent of bucket space.
     _node_features.insert(std::make_pair(node, dest_feat));
 }
