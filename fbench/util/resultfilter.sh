1d62389c5d66d88df6d7389a8270611059eb351d
Henning Baldersheim <balder@yahoo-inc.com>
2022-09-16 15:57:04 +0000

    GC resultfilter.

--- fbench/util/resultfilter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-# This script converts an fbench summary report read from stdin to a
-# single line containing only the numerical values written to
-# stdout.
-
-sed -n "s/.*: *\([0-9.][0-9.]*\).*/\1/p" | tr '\n' ' '
-echo
