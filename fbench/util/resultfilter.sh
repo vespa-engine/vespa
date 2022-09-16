7791ec6a046a25a2af92024229ce9dfcbc1430bf
Henning Baldersheim <balder@yahoo-inc.com>
2022-09-16 15:39:40 +0000

    Use bash instead of perl

--- fbench/util/resultfilter.sh
+++ fbench/util/resultfilter.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/bash
 # Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 # This script converts an fbench summary report read from stdin to a
 # single line containing only the numerical values written to
 # stdout.
 
-while(<>) {
-    chomp();
-    if(/:\s*([-+]?[\d.]+)/) {
-	print $1, " ";
-    }
-}
-print "\n";
+sed -n "s/.*: *\([0-9.][0-9.]*\).*/\1/p" | tr '\n' ' '
+echo
