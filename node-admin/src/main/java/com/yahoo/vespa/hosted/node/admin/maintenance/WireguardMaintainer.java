e2882b9a3a06969a40a16ca55d495a5e1a641b11
gjoranv <gv@verizonmedia.com>
2022-10-07 11:52:40 +0200

    Make WireguardMaintainer an interface.

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/WireguardMaintainer.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/maintenance/WireguardMaintainer.java
@@ -1,43 +1,14 @@
 package com.yahoo.vespa.hosted.node.admin.maintenance;
 
-import com.yahoo.vespa.hosted.node.admin.nodeadmin.ConvergenceException;
 import com.yahoo.vespa.hosted.node.admin.nodeagent.NodeAgentContext;
-import com.yahoo.vespa.hosted.node.admin.task.util.process.CommandResult;
-import com.yahoo.vespa.hosted.node.admin.task.util.process.Terminal;
 
 /**
  * Ensures that wireguard-go is running on the host.
  *
  * @author gjoranv
  */
-public class WireguardMaintainer {
+public interface WireguardMaintainer {
 
-    private static final String WIREGUARD_GO = "/path/wireguard-go wg0";
-    private static final String WIREGUARD_SOCK_FILE= "/var/run/wireguard/wg0.sock";
-
-    private final Terminal terminal;
-
-
-    public WireguardMaintainer(Terminal terminal) {
-        this.terminal = terminal;
-    }
-
-
-    public void converge(NodeAgentContext context) {
-
-        // TODO?: check if any containers are restarted
-
-        terminal.newCommandLine(context)
-                .add("rm", "-f", WIREGUARD_SOCK_FILE)
-                .executeSilently();
-
-        CommandResult result  = terminal.newCommandLine(context)
-                .add(WIREGUARD_GO, "wg0")
-                .executeSilently();
-
-        if (result.getExitCode() != 0) {
-            throw ConvergenceException.ofError("Failed to start wireguard-go: " + result.getOutput());
-        }
-    }
+    void converge(NodeAgentContext context);
 
 }
