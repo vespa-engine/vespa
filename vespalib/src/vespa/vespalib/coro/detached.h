8568274e54d065ae904907537dcf4ce6a9581bfa
Håvard Pettersen <havardpe@yahooinc.com>
2022-10-12 09:40:03 +0000

    more coroutines

--- vespalib/src/vespa/vespalib/coro/detached.h
+++ vespalib/src/vespa/vespalib/coro/detached.h
@@ -7,6 +7,18 @@
 
 namespace vespalib::coro {
 
+/**
+ * coroutine return type
+ *
+ * The coroutine is eager (will not suspend in initial_suspend) and
+ * self destroying (will not suspend in final_suspend). The return
+ * value gives no way of interacting with the coroutine. Without any
+ * co_await operations this acts similar to a normal subroutine. Note
+ * that letting a detached coroutine wait for a Lazy<T> will
+ * essentially attach it to the Lazy<T> as a continuation and resume
+ * it, but will require the Lazy<T> not to be deleted mid flight
+ * (started but not completed).
+ **/
 struct Detached {
     struct promise_type {
         Detached get_return_object() { return {}; }
--- vespalib/src/vespa/vespalib/coro/lazy.h
+++ vespalib/src/vespa/vespalib/coro/lazy.h
@@ -5,9 +5,22 @@
 #include <concepts>
 #include <coroutine>
 #include <optional>
+#include <exception>
 
 namespace vespalib::coro {
 
+/**
+ * coroutine return type
+ *
+ * The coroutine is lazy (will suspend in initial_suspend) and
+ * destroyed form the outside (will suspend in final_suspend). Waiting
+ * for a Lazy<T> using co_await will use symmetric transfer to suspend
+ * the waiting coroutine and resuming this one. The waiting coroutine
+ * is registered as a continuation and will be resumed again once the
+ * result is avalable (also using symmetric transfer). The result is
+ * assumed to be produced asynchronously. If you need to access it
+ * from the outside (in that specific thread); use sync_wait.
+ **/
 template <std::movable T>
 class [[nodiscard]] Lazy {
 public:
@@ -18,47 +31,73 @@ public:
             struct awaiter {
                 bool await_ready() const noexcept { return false; }
                 std::coroutine_handle<> await_suspend(Handle handle) const noexcept {
-                    auto waiter = handle.promise().waiter;
-                    return waiter ? waiter : std::noop_coroutine();
+                    return handle.promise().waiter;
                 }
                 void await_resume() const noexcept {}
             };
             return awaiter();
         }
-        void return_value(T ret_value) noexcept {
+        void return_value(const T &ret_value) noexcept(std::is_nothrow_move_constructible_v<T>) requires(std::copy_constructible<T>) {
+            value = ret_value;
+        }
+        void return_value(T &&ret_value) noexcept(std::is_nothrow_move_constructible_v<T>) {
             value = std::move(ret_value);
         }
-        static void unhandled_exception() { std::terminate(); }
+        void unhandled_exception() noexcept {
+            exception = std::current_exception();
+        }
         std::optional<T> value;
+        std::exception_ptr exception;
         std::coroutine_handle<> waiter;
         promise_type(promise_type &&) = delete;
         promise_type(const promise_type &) = delete;
-        promise_type() : value(std::nullopt), waiter(nullptr) {}
+        promise_type() : value(std::nullopt), exception(), waiter(std::noop_coroutine()) {}
+        T &result() & {
+            if (exception) {
+                std::rethrow_exception(exception);
+            }
+            return *value;
+        }
+        T &&result() && {
+            if (exception) {
+                std::rethrow_exception(exception);
+            }
+            return std::move(*value);
+        }
     };
     using Handle = std::coroutine_handle<promise_type>;
 
 private:
     Handle _handle;
-
+    
+    struct awaiter_base {
+        Handle handle;
+        awaiter_base(Handle handle_in) noexcept : handle(handle_in) {}
+        bool await_ready() const noexcept { return handle.done(); }
+        Handle await_suspend(std::coroutine_handle<> waiter) const noexcept {
+            handle.promise().waiter = waiter;
+            return handle;
+        }
+    };
+    
 public:
     Lazy(const Lazy &) = delete;
     Lazy &operator=(const Lazy &) = delete;
     explicit Lazy(Handle handle_in) noexcept : _handle(handle_in) {}
     Lazy(Lazy &&rhs) noexcept : _handle(std::exchange(rhs._handle, nullptr)) {}
-    auto operator co_await() {
-        struct awaiter {
-            Handle handle;
-            bool await_ready() const noexcept {
-                return handle.done();
-            }
-            Handle await_suspend(std::coroutine_handle<> waiter) const noexcept {
-                handle.promise().waiter = waiter;
-                return handle;
-            }
-            T &await_resume() const noexcept {
-                return *handle.promise().value;
-            }
-            awaiter(Handle handle_in) : handle(handle_in) {}
+    auto operator co_await() & noexcept {
+        struct awaiter : awaiter_base {
+            using awaiter_base::handle;
+            awaiter(Handle handle_in) noexcept : awaiter_base(handle_in) {}
+            decltype(auto) await_resume() const { return handle.promise().result(); }
+        };
+        return awaiter(_handle);
+    }
+    auto operator co_await() && noexcept {
+        struct awaiter : awaiter_base {
+            using awaiter_base::handle;
+            awaiter(Handle handle_in) noexcept : awaiter_base(handle_in) {}
+            decltype(auto) await_resume() const { return std::move(handle.promise()).result(); }
         };
         return awaiter(_handle);
     }
