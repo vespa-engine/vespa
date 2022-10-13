0f7ffd08b1263ba1dfb038b8896150d0d90acdbf
gjoranv <gjoranv@gmail.com>
2022-10-13 13:50:41 +0200

    Start wireguard on configserver hosts (#24345)
    Co-authored-by: gjoranv <gv@verizonmedia.com>

--- node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/nodeagent/NodeAgentImplTest.java
+++ node-admin/src/test/java/com/yahoo/vespa/hosted/node/admin/nodeagent/NodeAgentImplTest.java
@@ -789,7 +789,7 @@ public class NodeAgentImplTest {
         return new NodeAgentImpl(contextSupplier, nodeRepository, orchestrator, containerOperations,
                                  () -> RegistryCredentials.none, storageMaintainer, flagSource,
                                  List.of(credentialsMaintainer), Optional.of(aclMaintainer), Optional.of(healthChecker),
-                                 clock, warmUpDuration, VespaServiceDumper.DUMMY_INSTANCE);
+                                 clock, warmUpDuration, VespaServiceDumper.DUMMY_INSTANCE, Optional.empty());
     }
 
     private void mockGetContainer(DockerImage dockerImage, boolean isRunning) {
