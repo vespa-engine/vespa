b50442af088764830a19e93dfead9ce2441da997
jonmv <venstad@gmail.com>
2022-10-21 14:19:40 +0200

    Replace gson with jackson in abi-check plugin

--- abi-check-plugin/src/main/java/com/yahoo/abicheck/signature/JavaClassSignature.java
+++ abi-check-plugin/src/main/java/com/yahoo/abicheck/signature/JavaClassSignature.java
@@ -1,6 +1,11 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.abicheck.signature;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -12,12 +17,16 @@ public class JavaClassSignature {
   public final Set<String> methods;
   public final Set<String> fields;
 
-  public JavaClassSignature(String superClass, Set<String> interfaces, List<String> attributes,
-      Set<String> methods, Set<String> fields) {
+  public JavaClassSignature(@JsonProperty("superClass") String superClass,
+                            @JsonProperty("interfaces") Set<String> interfaces,
+                            @JsonProperty("attributes") List<String> attributes,
+                            @JsonProperty("methods") Set<String> methods,
+                            @JsonProperty("fields") Set<String> fields) {
     this.superClass = superClass;
     this.interfaces = interfaces;
     this.attributes = attributes;
     this.methods = methods;
     this.fields = fields;
   }
+
 }
