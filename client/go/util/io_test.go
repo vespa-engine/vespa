a171610a7db5abef10bb4fc412130092bc1b1bed
Arne Juul <arnej@yahooinc.com>
2022-10-25 09:22:48 +0000

    avoid dereferencing nil, add unit test

---
+++ client/go/util/io_test.go
@@ -0,0 +1,43 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package util
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPathExists(t *testing.T) {
+	assert.Equal(t, true, PathExists("io.go"))
+	assert.Equal(t, false, PathExists("nosuchthing.go"))
+
+	tmpDir := t.TempDir()
+	err := os.MkdirAll(tmpDir+"/no", 0755)
+	assert.Nil(t, err)
+	err = os.MkdirAll(tmpDir+"/no/such", 0)
+	assert.Nil(t, err)
+	assert.Equal(t, false, PathExists(tmpDir+"/no/such/thing.go"))
+}
+
+func TestIsDir(t *testing.T) {
+	tmpDir := t.TempDir()
+	err := os.MkdirAll(tmpDir+"/no", 0755)
+	assert.Nil(t, err)
+	assert.Equal(t, true, IsDirectory(tmpDir+"/no"))
+	err = os.MkdirAll(tmpDir+"/no/such", 0)
+	assert.Nil(t, err)
+	assert.Equal(t, true, IsDirectory(tmpDir+"/no/such"))
+	assert.Equal(t, false, IsDirectory(tmpDir+"/no/such/thing.go"))
+}
+
+func TestIsRegularFile(t *testing.T) {
+	assert.Equal(t, true, IsRegularFile("io.go"))
+	assert.Equal(t, false, IsRegularFile("."))
+	tmpDir := t.TempDir()
+	err := os.MkdirAll(tmpDir+"/no", 0755)
+	assert.Nil(t, err)
+	err = os.MkdirAll(tmpDir+"/no/such", 0)
+	assert.Nil(t, err)
+	assert.Equal(t, false, IsRegularFile(tmpDir+"/no/such/thing.go"))
+}
