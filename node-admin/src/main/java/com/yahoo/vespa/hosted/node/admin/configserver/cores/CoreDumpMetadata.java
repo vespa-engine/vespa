68061e1097d2863a4823c4876c9ec689e5780622
Håkon Hallingstad <hakon@yahooinc.com>
2022-11-01 08:09:30 +0100

    Export com.yahoo.vespa.hosted.node.admin.configserver.cores

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/configserver/cores/CoreDumpMetadata.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/configserver/cores/CoreDumpMetadata.java
@@ -1,7 +1,6 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.configserver.cores;
 
-import com.yahoo.component.Version;
 import com.yahoo.config.provision.DockerImage;
 
 import java.nio.file.Path;
