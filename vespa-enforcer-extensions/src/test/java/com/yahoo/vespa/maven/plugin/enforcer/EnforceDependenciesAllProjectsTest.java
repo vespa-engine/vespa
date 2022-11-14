91ae1fb4acd2c61bf3effaa7af05a52eff4223ce
Bjørn Christian Seime <bjorncs@yahooinc.com>
2022-11-14 11:07:52 +0100

    Classify dependencies based on test vs non-test usage

--- vespa-enforcer-extensions/src/test/java/com/yahoo/vespa/maven/plugin/enforcer/EnforceDependenciesAllProjectsTest.java
+++ vespa-enforcer-extensions/src/test/java/com/yahoo/vespa/maven/plugin/enforcer/EnforceDependenciesAllProjectsTest.java
@@ -1,6 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.maven.plugin.enforcer;
 
+import com.yahoo.vespa.maven.plugin.enforcer.EnforceDependenciesAllProjects.Dependencies;
 import com.yahoo.vespa.maven.plugin.enforcer.EnforceDependenciesAllProjects.Dependency;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.junit.jupiter.api.Test;
@@ -29,26 +30,34 @@ class EnforceDependenciesAllProjectsTest {
 
     @Test
     void succeeds_dependencies_matches_spec() {
-        SortedSet<Dependency> dependencies = new TreeSet<>(Set.of(
+        SortedSet<Dependency> nonTest = new TreeSet<>(Set.of(
                 Dependency.fromString("com.example:foo:1.2.3"),
                 Dependency.fromString("com.example:bar:2.3.4")));
+        SortedSet<Dependency> testOnly = new TreeSet<>(Set.of(
+                Dependency.fromString("com.example:testfoo:1.2.3"),
+                Dependency.fromString("com.example:testbar:2.3.4")));
         Path specFile = Paths.get("src/test/resources/allowed-dependencies.txt");
-        assertDoesNotThrow(() -> validateDependencies(dependencies, specFile, POM_FILE, "my-dep-enforcer"));
+        Dependencies deps = new Dependencies(nonTest, testOnly);
+        assertDoesNotThrow(() -> validateDependencies(deps, specFile, POM_FILE, "my-dep-enforcer"));
     }
 
     @Test
     void fails_on_forbidden_dependency() {
-        SortedSet<Dependency> dependencies = new TreeSet<>(Set.of(
+        SortedSet<Dependency> nonTest = new TreeSet<>(Set.of(
                 Dependency.fromString("com.example:foo:1.2.3"),
                 Dependency.fromString("com.example:bar:2.3.4"),
                 Dependency.fromString("com.example:foobar:3.4.5")));
+        SortedSet<Dependency> testOnly = new TreeSet<>(Set.of(
+                Dependency.fromString("com.example:testfoo:1.2.3"),
+                Dependency.fromString("com.example:testbar:2.3.4")));
         Path specFile = Paths.get("src/test/resources/allowed-dependencies.txt");
+        Dependencies deps = new Dependencies(nonTest, testOnly);
         var exception = assertThrows(EnforcerRuleException.class,
-                                     () -> validateDependencies(dependencies, specFile, POM_FILE, "my-dep-enforcer"));
+                                     () -> validateDependencies(deps, specFile, POM_FILE, "my-dep-enforcer"));
         String expectedErrorMessage =
                 """
                 The dependency enforcer failed:
-                Forbidden dependencies:
+                Forbidden non-test dependencies:
                  - com.example:foobar:3.4.5
                 Maven dependency validation failed. If this change was intentional, update the dependency spec by running:
                 $ mvn validate -DdependencyEnforcer.writeSpec -pl my-dep-enforcer -f /vespa-src/pom.xml
@@ -58,16 +67,21 @@ class EnforceDependenciesAllProjectsTest {
 
     @Test
     void fails_on_missing_dependency() {
-        SortedSet<Dependency> dependencies = new TreeSet<>(Set.of(
-                Dependency.fromString("com.example:foo:1.2.3")));
+        SortedSet<Dependency> nonTest = new TreeSet<>(Set.of(
+                Dependency.fromString("com.example:bar:2.3.4")));
+        SortedSet<Dependency> testOnly = new TreeSet<>(Set.of(
+                Dependency.fromString("com.example:testfoo:1.2.3")));
         Path specFile = Paths.get("src/test/resources/allowed-dependencies.txt");
+        Dependencies deps = new Dependencies(nonTest, testOnly);
         var exception = assertThrows(EnforcerRuleException.class,
-                                     () -> validateDependencies(dependencies, specFile, POM_FILE, "my-dep-enforcer"));
+                                     () -> validateDependencies(deps, specFile, POM_FILE, "my-dep-enforcer"));
         String expectedErrorMessage =
                 """
                 The dependency enforcer failed:
-                Removed dependencies:
-                 - com.example:bar:2.3.4
+                Removed non-test dependencies:
+                 - com.example:foo:1.2.3
+                Removed test-only dependencies:
+                 - com.example:testbar:2.3.4
                 Maven dependency validation failed. If this change was intentional, update the dependency spec by running:
                 $ mvn validate -DdependencyEnforcer.writeSpec -pl my-dep-enforcer -f /vespa-src/pom.xml
                 """;
@@ -76,11 +90,15 @@ class EnforceDependenciesAllProjectsTest {
 
     @Test
     void writes_valid_spec_file(@TempDir Path tempDir) throws IOException {
-        SortedSet<Dependency> dependencies = new TreeSet<>(Set.of(
+        SortedSet<Dependency> nonTest = new TreeSet<>(Set.of(
                 Dependency.fromString("com.example:foo:1.2.3"),
                 Dependency.fromString("com.example:bar:2.3.4")));
+        SortedSet<Dependency> testOnly = new TreeSet<>(Set.of(
+                Dependency.fromString("com.example:testfoo:1.2.3"),
+                Dependency.fromString("com.example:testbar:2.3.4")));
+        Dependencies deps = new Dependencies(nonTest, testOnly);
         Path outputFile = tempDir.resolve("allowed-dependencies.txt");
-        writeDependencySpec(outputFile, dependencies);
+        writeDependencySpec(outputFile, deps);
         assertEquals(
                 Files.readString(Paths.get("src/test/resources/allowed-dependencies.txt")),
                 Files.readString(outputFile));
