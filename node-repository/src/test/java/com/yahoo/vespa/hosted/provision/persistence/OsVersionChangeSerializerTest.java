f4005dee3246e6de803230d5c12a3af6f05385ad
Martin Polden <mpolden@mpolden.no>
2022-11-18 15:30:57 +0100

    Less Guava

--- node-repository/src/test/java/com/yahoo/vespa/hosted/provision/persistence/OsVersionChangeSerializerTest.java
+++ node-repository/src/test/java/com/yahoo/vespa/hosted/provision/persistence/OsVersionChangeSerializerTest.java
@@ -8,6 +8,7 @@ import com.yahoo.vespa.hosted.provision.os.OsVersionTarget;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,11 +19,11 @@ public class OsVersionChangeSerializerTest {
 
     @Test
     public void serialization() {
-        var change = new OsVersionChange(Map.of(
+        var change = new OsVersionChange(new TreeMap<>(Map.of(
                 NodeType.host, new OsVersionTarget(NodeType.host, Version.fromString("1.2.3")),
                 NodeType.proxyhost, new OsVersionTarget(NodeType.proxyhost, Version.fromString("4.5.6")),
                 NodeType.confighost, new OsVersionTarget(NodeType.confighost, Version.fromString("7.8.9"))
-        ));
+        )));
         var serialized = OsVersionChangeSerializer.fromJson(OsVersionChangeSerializer.toJson(change));
         assertEquals(serialized, change);
     }
