e6603ad4a1c5f19b92dd42f6e66abc2c92abb541
Henning Baldersheim <balder@yahoo-inc.com>
2022-11-21 18:12:22 +0100

    Add comment

--- vespajlib/src/main/java/com/yahoo/compress/Hasher.java
+++ vespajlib/src/main/java/com/yahoo/compress/Hasher.java
@@ -2,7 +2,12 @@ package com.yahoo.compress;
 
 import net.openhft.hashing.LongHashFunction;
 
+/**
+ * Utility for hashing providing multiple hashing methods
+ * @author baldersheim
+ */
 public class Hasher {
+    /** Uses net.openhft.hashing.LongHashFunction.xx3() */
     public static long xxh3(byte [] data) {
         return LongHashFunction.xx3().hashBytes(data);
     }
