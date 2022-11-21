0e73385a1fd007eeed646b620dbfc4d498b68f5f
Tor Brede Vekterli <vekterli@yahooinc.com>
2022-11-21 11:48:13 +0100

    Add standalone runner script for vespa-crypto-cli
    Useful when the script is run in a context where `VESPA_HOME` is not
    set. Should work both if the script is invoked directly or through a
    symbolic link.

---
+++ vespaclient-java/src/main/sh/vespa-crypto-cli-standalone.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+# Resolve symlink (if any) and normalize path
+program=$(readlink -f "$0")
+program_dir=$(dirname "$program")
+jarfile=$(readlink -f "$program_dir"/../../../target/vespaclient-java-jar-with-dependencies.jar)
+
+if ! test -e "$jarfile"
+then
+    echo "No such file: '$jarfile'" >&2
+    exit 1
+fi
+
+exec java \
+-Djava.awt.headless=true \
+-Xms128m -Xmx2048m \
+-cp "$jarfile" com.yahoo.vespa.security.tool.Main "$@"
