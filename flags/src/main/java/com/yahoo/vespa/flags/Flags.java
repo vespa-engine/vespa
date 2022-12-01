6176837ef481840cb47bd4c337b3195444005360
Tor Brede Vekterli <vekterli@yahooinc.com>
2022-12-01 10:52:17 +0100

    Feature flag expiry bump, festive julekalender '22 edition

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -218,7 +218,7 @@ public class Flags {
 
     public static final UnboundIntFlag MAX_ACTIVATION_INHIBITED_OUT_OF_SYNC_GROUPS = defineIntFlag(
             "max-activation-inhibited-out-of-sync-groups", 0,
-            List.of("vekterli"), "2021-02-19", "2022-12-01",
+            List.of("vekterli"), "2021-02-19", "2023-02-01",
             "Allows replicas in up to N content groups to not be activated " +
             "for query visibility if they are out of sync with a majority of other replicas",
             "Takes effect at redeployment",
@@ -340,7 +340,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag USE_TWO_PHASE_DOCUMENT_GC = defineFeatureFlag(
             "use-two-phase-document-gc", false,
-            List.of("vekterli"), "2022-08-24", "2022-12-01",
+            List.of("vekterli"), "2022-08-24", "2023-02-01",
             "Use two-phase document GC in content clusters",
             "Takes effect at redeployment",
             ZONE_ID, APPLICATION_ID);
@@ -413,7 +413,7 @@ public class Flags {
 
     public static final UnboundStringFlag CORE_ENCRYPTION_PUBLIC_KEY_ID = defineStringFlag(
             "core-encryption-public-key-id", "",
-            List.of("vekterli"), "2022-11-03", "2022-12-01",
+            List.of("vekterli"), "2022-11-03", "2023-02-01",
             "Specifies which public key to use for core dump encryption.",
             "Takes effect on the next tick.",
             ZONE_ID, NODE_TYPE, HOSTNAME);
