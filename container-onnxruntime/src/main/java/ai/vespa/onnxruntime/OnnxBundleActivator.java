2da7298a3b98f34a61fed78e283c79d12676803e
Arne Juul <arnej@yahooinc.com>
2022-12-02 14:19:53 +0000

    fix whitespace

--- container-onnxruntime/src/main/java/ai/vespa/onnxruntime/OnnxBundleActivator.java
+++ container-onnxruntime/src/main/java/ai/vespa/onnxruntime/OnnxBundleActivator.java
@@ -1,4 +1,5 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.                                                                                                                           
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
 package ai.vespa.onnxruntime;
 
 import org.osgi.framework.BundleActivator;
