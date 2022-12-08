40020b61856b3b4d0a33e2cc03427e46f1a94d84
Bjørn Christian Seime <bjorncs@yahooinc.com>
2022-12-08 14:02:21 +0100

    Add script to run vespa-feed-client cli from source

---
+++ vespa-feed-client-cli/vespa-feed-client-dev.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+# Resolve symlink (if any) and normalize path
+program=$(readlink -f "$0")
+program_dir=$(dirname "$program")
+
+exec java \
+-Djava.awt.headless=true \
+-Xms128m -Xmx2048m \
+-Djava.util.logging.config.file=$program_dir/src/main/resources/logging.properties \
+-cp $program_dir/target/vespa-feed-client-cli-jar-with-dependencies.jar ai.vespa.feed.client.impl.CliClient "$@"
