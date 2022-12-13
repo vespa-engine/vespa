0d6e7d3599dfc66277a184bde28009530edb0d30
Harald Musum <musum@verizonmedia.com>
2022-12-13 09:46:19 +0100

    Revert "Use the forward scheduler in proton also for disk-mem-util-sampler"

--- searchcore/src/vespa/searchcore/proton/server/disk_mem_usage_sampler.h
+++ searchcore/src/vespa/searchcore/proton/server/disk_mem_usage_sampler.h
@@ -2,14 +2,17 @@
 
 #pragma once
 
+#include <vespa/vespalib/util/time.h>
 #include "disk_mem_usage_filter.h"
-#include <vespa/searchcore/proton/common/i_scheduled_executor.h>
+
+class FNET_Transport;
 
 namespace vespalib { class IDestructorCallback; }
 
 namespace proton {
 
 class ITransientResourceUsageProvider;
+class ScheduledExecutor;
 
 /*
  * Class to sample disk and memory usage used for filtering write operations.
@@ -19,6 +22,7 @@ class DiskMemUsageSampler {
     std::filesystem::path  _path;
     vespalib::duration     _sampleInterval;
     vespalib::steady_time  _lastSampleTime;
+    std::unique_ptr<ScheduledExecutor> _periodicTimer;
     std::mutex             _lock;
     std::unique_ptr<vespalib::IDestructorCallback> _periodicHandle;
     std::vector<std::shared_ptr<const ITransientResourceUsageProvider>> _transient_usage_providers;
@@ -37,7 +41,8 @@ public:
             : filterConfig(),
               sampleInterval(60s),
               hwInfo()
-        { }
+        {
+        }
 
         Config(double memoryLimit_in,
                double diskLimit_in,
@@ -46,19 +51,23 @@ public:
             : filterConfig(memoryLimit_in, diskLimit_in),
               sampleInterval(sampleInterval_in),
               hwInfo(hwInfo_in)
-        { }
+        {
+        }
     };
 
-    DiskMemUsageSampler(const std::string &path_in, const HwInfo &config);
+    DiskMemUsageSampler(FNET_Transport & transport,
+                        const std::string &path_in,
+                        const Config &config);
 
     ~DiskMemUsageSampler();
 
-    void setConfig(const Config &config, IScheduledExecutor & executor);
+    void setConfig(const Config &config);
 
     const DiskMemUsageFilter &writeFilter() const { return _filter; }
     IDiskMemUsageNotifier &notifier() { return _filter; }
     void add_transient_usage_provider(std::shared_ptr<const ITransientResourceUsageProvider> provider);
     void remove_transient_usage_provider(std::shared_ptr<const ITransientResourceUsageProvider> provider);
 };
+
 
 } // namespace proton
