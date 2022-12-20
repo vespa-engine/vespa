acbdecc1f1b65e069dc9a5fc73575f0e8f653a98
Harald Musum <musum@yahooinc.com>
2022-12-20 09:56:43 +0100

    Update version in package-info.java

--- zkfacade/src/main/java/org/apache/curator/framework/api/transaction/package-info.java
+++ zkfacade/src/main/java/org/apache/curator/framework/api/transaction/package-info.java
@@ -1,5 +1,5 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-@ExportPackage(version = @Version(major = 5, minor = 3, micro = 0))
+@ExportPackage(version = @Version(major = 5, minor = 4, micro = 0))
 package org.apache.curator.framework.api.transaction;
 import com.yahoo.osgi.annotation.ExportPackage;
 import com.yahoo.osgi.annotation.Version;
