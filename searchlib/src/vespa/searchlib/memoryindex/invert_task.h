532395fd9a02c8ca77b1502a575eb710d374c91c
Henning Baldersheim <balder@yahoo-inc.com>
2023-01-10 07:41:07 +0000

    Use const to signal members that will not change

--- searchlib/src/vespa/searchlib/memoryindex/invert_task.h
+++ searchlib/src/vespa/searchlib/memoryindex/invert_task.h
@@ -27,7 +27,7 @@ class InvertTask : public vespalib::Executor::Task
     const std::vector<std::unique_ptr<FieldInverter>>&    _inverters;
     const std::vector<std::unique_ptr<UrlFieldInverter>>& _uri_inverters;
     const document::Document&                             _doc;
-    uint32_t                                              _lid;
+    const uint32_t                                        _lid;
     std::remove_reference_t<OnWriteDoneType>              _on_write_done;
 public:
     InvertTask(const DocumentInverterContext& inv_context, const InvertContext& context, const std::vector<std::unique_ptr<FieldInverter>>& inverters,  const std::vector<std::unique_ptr<UrlFieldInverter>>& uri_inverters, uint32_t lid, const document::Document& doc, OnWriteDoneType on_write_done);
