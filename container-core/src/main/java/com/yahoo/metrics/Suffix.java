81ff2fed4a50dbc2f9f82a9a59f2d880fb6358c1
gjoranv <gv@verizonmedia.com>
2023-01-10 12:16:12 +0100

    Add a global enum for container metric definitions.
    - Populated with only a few metrics at first.

---
+++ container-core/src/main/java/com/yahoo/metrics/Suffix.java
@@ -0,0 +1,24 @@
+package com.yahoo.metrics;
+
+public enum Suffix {
+    ninety_five_percentile("95percentile"),
+    ninety_nine_percentile("99percentile"),
+    average("average"),
+    count("count"),
+    last("last"),
+    max("max"),
+    min("min"),
+    rate("rate"),
+    sum("sum");
+
+    private final String suffix;
+
+    Suffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String suffix() {
+        return suffix;
+    }
+
+}
