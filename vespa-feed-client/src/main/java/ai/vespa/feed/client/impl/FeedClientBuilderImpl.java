e7087666ae770cb16c50f5706cc25e3f03fb92c9
jonmv <venstad@gmail.com>
2023-01-10 09:28:41 +0100

    Require non-null compression, and update doc

--- vespa-feed-client/src/main/java/ai/vespa/feed/client/impl/FeedClientBuilderImpl.java
+++ vespa-feed-client/src/main/java/ai/vespa/feed/client/impl/FeedClientBuilderImpl.java
@@ -205,7 +205,7 @@ public class FeedClientBuilderImpl implements FeedClientBuilder {
 
     @Override
     public FeedClientBuilderImpl setCompression(Compression compression) {
-        this.compression = compression;
+        this.compression = requireNonNull(compression);
         return this;
     }
 
