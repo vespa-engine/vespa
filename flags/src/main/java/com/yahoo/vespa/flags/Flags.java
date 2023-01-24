e61cbfc643b503d80ef018e2571c6cedfcdd1007
Harald Musum <musum@yahooinc.com>
2023-01-24 10:52:59 +0100

    Use zstd as preferred compression method for file distribution

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -288,16 +288,16 @@ public class Flags {
             APPLICATION_ID);
 
     public static final UnboundListFlag<String> FILE_DISTRIBUTION_ACCEPTED_COMPRESSION_TYPES = defineListFlag(
-            "file-distribution-accepted-compression-types", List.of("gzip", "lz4"), String.class,
-            List.of("hmusum"), "2022-07-05", "2023-02-01",
-            "´List of accepted compression types used when asking for a file reference. Valid values: gzip, lz4",
+            "file-distribution-accepted-compression-types", List.of("gzip", "lz4", "zstd"), String.class,
+            List.of("hmusum"), "2022-07-05", "2023-02-15",
+            "´List of accepted compression types used when asking for a file reference. Valid values: gzip, lz4, zstd",
             "Takes effect on restart of service",
             APPLICATION_ID);
 
     public static final UnboundListFlag<String> FILE_DISTRIBUTION_COMPRESSION_TYPES_TO_SERVE = defineListFlag(
-            "file-distribution-compression-types-to-use", List.of("lz4", "gzip"), String.class,
-            List.of("hmusum"), "2022-07-05", "2023-02-01",
-            "List of compression types to use (in preferred order), matched with accepted compression types when serving file references. Valid values: gzip, lz4",
+            "file-distribution-compression-types-to-use", List.of("zstd", "lz4", "gzip"), String.class,
+            List.of("hmusum"), "2022-07-05", "2023-02-15",
+            "List of compression types to use (in preferred order), matched with accepted compression types when serving file references. Valid values: gzip, lz4, zstd",
             "Takes effect on restart of service",
             APPLICATION_ID);
 
