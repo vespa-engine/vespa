75ced2208e64da01c99c7fff4b39c017ef2cb2b0
Ola Aunronning <olaa@yahooinc.com>
2023-02-03 18:42:23 +0100

    Add copyright header

--- metrics-proxy/src/main/java/ai/vespa/metricsproxy/metric/dimensions/BlocklistDimensions.java
+++ metrics-proxy/src/main/java/ai/vespa/metricsproxy/metric/dimensions/BlocklistDimensions.java
@@ -1,6 +1,6 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.metricsproxy.metric.dimensions;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
