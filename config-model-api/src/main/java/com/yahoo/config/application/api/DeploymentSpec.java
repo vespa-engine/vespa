9be10074a8b0e31c9dfcdcb2d8e9048c86b1fadc
Jon Bratseth <bratseth@gmail.com>
2023-02-07 11:50:10 +0100

    Add missing import!

--- config-model-api/src/main/java/com/yahoo/config/application/api/DeploymentSpec.java
+++ config-model-api/src/main/java/com/yahoo/config/application/api/DeploymentSpec.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
