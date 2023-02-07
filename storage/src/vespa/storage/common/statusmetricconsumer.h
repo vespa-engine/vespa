d587fe37052fa03c44a262e82fe95658971eb4c2
Henning Baldersheim <balder@yahoo-inc.com>
2023-02-07 10:25:20 +0000

    Use steady_time for statusmetricconsumer.

--- storage/src/vespa/storage/common/statusmetricconsumer.h
+++ storage/src/vespa/storage/common/statusmetricconsumer.h
@@ -11,27 +11,18 @@
 
 #include "storagecomponent.h"
 #include <vespa/storageframework/generic/status/statusreporter.h>
-#include <vespa/storageframework/generic/metric/metricupdatehook.h>
-#include <vespa/vespalib/util/document_runnable.h>
-#include <vespa/metrics/metricmanager.h>
-#include <map>
+#include <vespa/vespalib/util/jsonstream.h>
 
 namespace vespalib { class StringTokenizer; }
 namespace metrics { class MetricManager; }
 
 namespace storage {
 
-namespace framework { class MemoryToken; }
-
 class StatusMetricConsumer : public framework::StatusReporter,
-                             private framework::MetricUpdateHook,
                              private vespalib::JsonStreamTypes
 {
 public:
-    StatusMetricConsumer(
-            StorageComponentRegister&,
-            metrics::MetricManager&,
-            const std::string& name = "status");
+    StatusMetricConsumer(StorageComponentRegister&, metrics::MetricManager&, const std::string& name = "status");
     ~StatusMetricConsumer() override;
 
     // Metric reporting requires the "vespa.content.metrics_api" capability
@@ -40,14 +31,11 @@ public:
     }
     vespalib::string getReportContentType(const framework::HttpUrlPath&) const override;
     bool reportStatus(std::ostream& out, const framework::HttpUrlPath&) const override;
-    void updateMetrics(const MetricLockGuard & guard) override;
 private:
     metrics::MetricManager& _manager;
     StorageComponent        _component;
     std::string             _name;
     mutable std::mutex      _lock;
-    framework::SecondTime   _startTime;
-    framework::SecondTime   _processedTime;
 };
 
 } // storage
