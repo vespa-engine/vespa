a461f0e63f31494db1d0ab76d555cf9ee66a204c
bjormel <bjormel@verizonmedia.com>
2023-02-08 10:52:16 +0100

    Add debug logging for successfactor triggering alert

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/TrafficShareUpdater.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/TrafficShareUpdater.java
@@ -66,6 +66,8 @@ public class TrafficShareUpdater extends ControllerMaintainer {
         double successFactor = asSuccessFactor(attempts, failures);
         if ( successFactor == 0 )
             log.log(Level.WARNING, "Could not update traffic share on any applications", lastException);
+        else if ( successFactor < 0.9 )
+            log.log(Level.FINE, "Could not update traffic share on any applications", lastException);
         return successFactor;
     }
 
