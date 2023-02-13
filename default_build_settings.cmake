d9b9b45b5234eda663e55b5bab87268e895ab171
Tor Egge <Tor.Egge@online.no>
2023-02-13 11:09:31 +0100

    Add comments describing reason for adjustment to default cpu arch options.

--- default_build_settings.cmake
+++ default_build_settings.cmake
@@ -131,9 +131,11 @@ function(vespa_use_default_build_settings)
     if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
       if(APPLE AND (("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang") OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")))
       elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        # Avoid clang error on linux: _builtin_ia32_lddqu' needs target feature sse3
         set(DEFAULT_VESPA_CPU_ARCH_FLAGS "-march=westmere -mtune=haswell")
       else()
         if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 13.0)
+          # Temporary workaround for https://gcc.gnu.org/bugzilla/show_bug.cgi?id=108599
           set(DEFAULT_VESPA_CPU_ARCH_FLAGS "-march=ivybridge")
         else()
           set(DEFAULT_VESPA_CPU_ARCH_FLAGS "-mtune=intel")
