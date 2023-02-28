e98ea0903b425c5b118d38ddd63a3165d024adb6
Henning Baldersheim <balder@yahoo-inc.com>
2023-02-28 20:17:04 +0000

    Use typesafe time for UpdateHook

--- metrics/src/vespa/metrics/updatehook.h
+++ metrics/src/vespa/metrics/updatehook.h
@@ -26,17 +26,26 @@ private:
 class MetricManager;
 
 class UpdateHook {
-    const char* _name;
-    time_t      _nextCall;
-    uint32_t    _period;
-    friend class MetricManager;
-
 public:
     using MetricLockGuard = metrics::MetricLockGuard;
-    UpdateHook(const char* name) : _name(name), _nextCall(0), _period(0) {}
+    UpdateHook(const char* name) : _name(name), _nextCall(), _period(vespalib::duration::zero()) {}
     virtual ~UpdateHook() = default;
     virtual void updateMetrics(const MetricLockGuard & guard) = 0;
     const char* getName() const { return _name; }
+    void updateNextCall() { updateNextCall(_nextCall); }
+    void updateNextCall(time_point now) { _nextCall = now + _period; }
+    bool is_periodic() const noexcept { return _period == vespalib::duration::zero(); }
+    bool expired(time_point now) { return _nextCall <= now; }
+    bool has_valid_expiry() const noexcept { return _nextCall != time_point(); }
+    vespalib::duration getPeriod() const noexcept { return _period; }
+    time_point getNextCall() const noexcept { return _nextCall; }
+    // This should be moved to constructor
+    void setPeriod(vespalib::duration period) { _period = period; }
+    void setNextCall(time_point now) { _nextCall = now; }
+private:
+    const char*         _name;
+    time_point          _nextCall;
+    vespalib::duration  _period;
 };
 
 }
