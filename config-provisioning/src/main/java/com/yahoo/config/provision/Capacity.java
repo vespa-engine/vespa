37e81637eff4236f8f718a035deb2d87b030572f
Jon Bratseth <bratseth@gmail.com>
2023-03-01 16:08:02 +0100

    Restore - apparently too early to remove

--- config-provisioning/src/main/java/com/yahoo/config/provision/Capacity.java
+++ config-provisioning/src/main/java/com/yahoo/config/provision/Capacity.java
@@ -115,6 +115,16 @@ public final class Capacity {
         return new Capacity(min, max, groupSize, required, canFail, NodeType.tenant, cloudAccount, ClusterInfo.empty());
     }
 
+    // TODO: Remove after March 2023
+    public static Capacity from(ClusterResources min, ClusterResources max, boolean required, boolean canFail) {
+        return new Capacity(min, max, IntRange.empty(), required, canFail, NodeType.tenant, Optional.empty(), ClusterInfo.empty());
+    }
+
+    // TODO: Remove after March 2023
+    public static Capacity from(ClusterResources min, ClusterResources max, boolean required, boolean canFail, Optional<CloudAccount> cloudAccount) {
+        return new Capacity(min, max, IntRange.empty(), required, canFail, NodeType.tenant, cloudAccount, ClusterInfo.empty());
+    }
+
     public static Capacity from(ClusterResources min, ClusterResources max, IntRange groupSize, boolean required, boolean canFail,
                                 Optional<CloudAccount> cloudAccount, ClusterInfo clusterInfo) {
         return new Capacity(min, max, groupSize, required, canFail, NodeType.tenant, cloudAccount, clusterInfo);
