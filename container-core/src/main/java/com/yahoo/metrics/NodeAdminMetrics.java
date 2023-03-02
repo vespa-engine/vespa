80b95697ca43d3457a7462ae40d893374392e48a
yngveaasheim <yngve@yahooinc.com>
2023-03-02 15:05:31 +0100

    Add enums for more metrics sources

---
+++ container-core/src/main/java/com/yahoo/metrics/NodeAdminMetrics.java
@@ -0,0 +1,36 @@
+package com.yahoo.metrics;
+
+/**
+ * @author yngve
+ */
+public enum NodeAdminMetrics implements VespaMetrics {
+
+    WORKER_CONNECTIONS("worker.connections", Unit.CONNECTION, "Yahoo! Internal: Number of connections for the routing worker having most connections per node"),  // Hosted Vespa only (routing layer) TODO: Move to a better place
+    ENDPOINT_CERTIFICATE_EXPIRY_SECONDS("endpoint.certificate.expiry.seconds", Unit.SECOND, "Time until node endpoint certificate expires"),
+    NODE_CERTIFICATE_EXPIRY_SECONDS("node-certificate.expiry.seconds", Unit.SECOND, "Time until node certificate expires");
+
+
+    private final String name;
+    private final Unit unit;
+    private final String description;
+
+    NodeAdminMetrics(String name, Unit unit, String description) {
+        this.name = name;
+        this.unit = unit;
+        this.description = description;
+    }
+
+    public String baseName() {
+        return name;
+    }
+
+    public Unit unit() {
+        return unit;
+    }
+
+    public String description() {
+        return description;
+    }
+
+}
+
---
+++ container-core/src/main/java/com/yahoo/metrics/SentinelMetrics.java
@@ -0,0 +1,36 @@
+package com.yahoo.metrics;
+
+/**
+ * @author yngve
+ */
+public enum SentinelMetrics implements VespaMetrics {
+
+    SENTINEL_RESTARTS("sentinel.restarts", Unit.RESTART, "Number of service restarts done by the sentinel"),
+    SENTINEL_TOTAL_RESTARTS("sentinel.totalRestarts", Unit.RESTART, "Total number of service restarts done by the sentinel since the sentinel was started"),
+    SENTINEL_UPTIME("sentinel.uptime", Unit.SECOND, "Time  the sentinel has been running"),
+    SENTINEL_RUNNING("sentinel.running", Unit.INSTANCE, "Number of services the sentinel has running currently");
+
+
+    private final String name;
+    private final Unit unit;
+    private final String description;
+
+    SentinelMetrics(String name, Unit unit, String description) {
+        this.name = name;
+        this.unit = unit;
+        this.description = description;
+    }
+
+    public String baseName() {
+        return name;
+    }
+
+    public Unit unit() {
+        return unit;
+    }
+
+    public String description() {
+        return description;
+    }
+
+}
