69189de8d243192ae82f99fa636eb7969602cea5
Bjørn Christian Seime <bjorncs@yahooinc.com>
2023-03-02 12:53:54 +0100

    Correctly classify transient/retriable errors server side for legacy feed API

--- vespaclient-container-plugin/src/main/java/com/yahoo/vespa/http/server/ErrorCode.java
+++ vespaclient-container-plugin/src/main/java/com/yahoo/vespa/http/server/ErrorCode.java
@@ -1,6 +1,11 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.http.server;
 
+import com.yahoo.messagebus.Error;
+
+import java.util.Collection;
+import java.util.Set;
+
 /**
  * Return types for the server.
  *
@@ -28,6 +33,18 @@ enum ErrorCode {
 
     public boolean isTransient() {
         return _transient;
+    }
+
+    static ErrorCode fromBusError(Error mbusError) {
+        Collection<Integer> fatalsHandledAsTransient = Set.of(
+                com.yahoo.messagebus.ErrorCode.SEND_QUEUE_CLOSED,
+                com.yahoo.messagebus.ErrorCode.ILLEGAL_ROUTE,
+                com.yahoo.messagebus.ErrorCode.NO_SERVICES_FOR_ROUTE,
+                com.yahoo.messagebus.ErrorCode.NETWORK_ERROR,
+                com.yahoo.messagebus.ErrorCode.SEQUENCE_ERROR,
+                com.yahoo.messagebus.ErrorCode.NETWORK_SHUTDOWN,
+                com.yahoo.messagebus.ErrorCode.TIMEOUT);
+        return mbusError.isFatal() && !fatalsHandledAsTransient.contains(mbusError.getCode()) ? ERROR : TRANSIENT_ERROR;
     }
 
 }
