e98eaca0cd0e65906c075188472d7ca6dce4e611
Jon Bratseth <bratseth@gmail.com>
2023-03-02 13:16:24 +0100

    Only read generation from containers for now

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/autoscale/MetricsResponse.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/autoscale/MetricsResponse.java
@@ -162,8 +162,9 @@ public class MetricsResponse {
         generation { // application config generation active on the node
 
             @Override
-            public List<String> metricResponseNames() { return List.of("application_generation",
-                                                                       "content.proton.config.generation"); }
+            public List<String> metricResponseNames() {
+                return List.of("application_generation" /*, "content.proton.config.generation" */);
+            }
 
             @Override
             double computeFinal(ListMap<String, Double> values) {
