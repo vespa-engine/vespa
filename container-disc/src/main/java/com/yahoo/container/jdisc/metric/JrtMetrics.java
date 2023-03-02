9605e18705da7e98700c26d02c70c5b388a252e1
yngveaasheim <yngve@yahooinc.com>
2023-03-02 14:20:54 +0100

    Remove unused import

--- container-disc/src/main/java/com/yahoo/container/jdisc/metric/JrtMetrics.java
+++ container-disc/src/main/java/com/yahoo/container/jdisc/metric/JrtMetrics.java
@@ -1,7 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.container.jdisc.metric;
 
-import com.yahoo.jdisc.Container;
+// import com.yahoo.jdisc.Container;
 import com.yahoo.jdisc.Metric;
 import com.yahoo.jrt.TransportMetrics;
 import com.yahoo.metrics.ContainerMetrics;
