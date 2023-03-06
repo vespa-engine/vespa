2cfa570711225eded510122ac9e4d5be8d8a3a08
yngveaasheim <yngve@yahooinc.com>
2023-03-06 09:57:44 +0100

    Move routing layer metric to separete place

--- container-core/src/main/java/com/yahoo/metrics/NodeAdminMetrics.java
+++ container-core/src/main/java/com/yahoo/metrics/NodeAdminMetrics.java
@@ -5,7 +5,6 @@ package com.yahoo.metrics;
  */
 public enum NodeAdminMetrics implements VespaMetrics {
 
-    WORKER_CONNECTIONS("worker.connections", Unit.CONNECTION, "Yahoo! Internal: Number of connections for the routing worker having most connections per node"),  // Hosted Vespa only (routing layer) TODO: Move to a better place
     ENDPOINT_CERTIFICATE_EXPIRY_SECONDS("endpoint.certificate.expiry.seconds", Unit.SECOND, "Time until node endpoint certificate expires"),
     NODE_CERTIFICATE_EXPIRY_SECONDS("node-certificate.expiry.seconds", Unit.SECOND, "Time until node certificate expires");
 
