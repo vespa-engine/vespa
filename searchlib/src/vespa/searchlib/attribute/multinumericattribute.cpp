e1f2607f61dd674c47edc4d0e6bbcb9c914136d6
Tor Egge <Tor.Egge@online.no>
2023-03-24 13:08:17 +0100

    Use explicit instantiation of MultiValueNumericAttribute

--- searchlib/src/vespa/searchlib/attribute/multinumericattribute.cpp
+++ searchlib/src/vespa/searchlib/attribute/multinumericattribute.cpp
@@ -7,5 +7,18 @@ LOG_SETUP(".searchlib.attribute.multi_numeric_attribute");
 
 namespace search {
 
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int8_t>, int8_t>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int16_t>, int16_t>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int32_t>, int32_t>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int64_t>, int64_t>;
+template class MultiValueNumericAttribute<FloatingPointAttributeTemplate<float>, float>;
+template class MultiValueNumericAttribute<FloatingPointAttributeTemplate<double>, double>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int8_t>, multivalue::WeightedValue<int8_t>>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int16_t>, multivalue::WeightedValue<int16_t>>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int32_t>, multivalue::WeightedValue<int32_t>>;
+template class MultiValueNumericAttribute<IntegerAttributeTemplate<int64_t>, multivalue::WeightedValue<int64_t>>;
+template class MultiValueNumericAttribute<FloatingPointAttributeTemplate<float>, multivalue::WeightedValue<float>>;
+template class MultiValueNumericAttribute<FloatingPointAttributeTemplate<double>, multivalue::WeightedValue<double>>;
+
 } // namespace search
 
