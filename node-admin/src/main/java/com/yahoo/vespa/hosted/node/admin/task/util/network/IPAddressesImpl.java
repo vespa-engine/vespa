7a55160c3941aaed5d4db173337b865bb45c3e7f
Valerij Fredriksen <valerijf@yahooinc.com>
2023-04-14 11:26:36 +0200

    Throw UncheckedIOException instead

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/task/util/network/IPAddressesImpl.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/task/util/network/IPAddressesImpl.java
@@ -1,6 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.node.admin.task.util.network;
 
+import java.io.UncheckedIOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -14,7 +15,7 @@ public class IPAddressesImpl implements IPAddresses {
         try {
             return InetAddress.getAllByName(hostname);
         } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedIOException(e);
         }
     }
 }
