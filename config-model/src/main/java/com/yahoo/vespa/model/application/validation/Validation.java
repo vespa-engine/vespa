00312d366f6d9ddf0817481adc4c332a89d66be0
jonmv <venstad@gmail.com>
2023-04-19 14:23:04 +0200

    Forbid applications without a container cluster in hosted Vespa

--- config-model/src/main/java/com/yahoo/vespa/model/application/validation/Validation.java
+++ config-model/src/main/java/com/yahoo/vespa/model/application/validation/Validation.java
@@ -76,6 +76,7 @@ public class Validation {
         new StreamingValidator().validate(model, deployState);
         new RankSetupValidator(validationParameters.ignoreValidationErrors()).validate(model, deployState);
         new NoPrefixForIndexes().validate(model, deployState);
+        new ContainerInCloudValidator().validate(model, deployState);
         new DeploymentSpecValidator().validate(model, deployState);
         new ValidationOverridesValidator().validate(model, deployState);
         new ConstantValidator().validate(model, deployState);
