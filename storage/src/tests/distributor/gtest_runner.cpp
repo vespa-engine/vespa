53dc081d48be4f5850f4b609e6a031c014ae6425
Tor Brede Vekterli <vekterli@yahooinc.com>
2023-04-20 11:53:36 +0000

    Add NewestReplica equality tests and gmock matcher for distinct elements in a range
    Add a new `matchers` directory in vespalib which can be used as a repository for
    generic, reusable GMock matchers.
    
    Move distributor tests from using an explicit gtest runner to using `GTest::gmock_main`
    which serves the same purpose. Need to depend on a gmock target (not just gtest) to
    be able to link with stuff required for matchers.

--- storage/src/tests/distributor/gtest_runner.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-#include <vespa/vespalib/gtest/gtest.h>
-
-#include <vespa/log/log.h>
-LOG_SETUP("storage_distributor_gtest_runner");
-
-GTEST_MAIN_RUN_ALL_TESTS()
