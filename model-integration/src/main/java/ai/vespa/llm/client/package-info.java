29771cba3c234cf6f1661ce80d6038eebdb7892a
Jon Bratseth <bratseth@vespa.ai>
2023-04-20 13:35:07 +0200

    Export API

---
+++ model-integration/src/main/java/ai/vespa/llm/client/package-info.java
@@ -0,0 +1,11 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+@ExportPackage
+@PublicApi
+package ai.vespa.llm.client;
+
+import com.yahoo.api.annotations.PublicApi;
+import com.yahoo.osgi.annotation.ExportPackage;
+
+/**
+ * Clients to externally hosted large language models.
+ */
