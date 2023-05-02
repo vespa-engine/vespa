22cdb33a8ef8f235d0c63935bc9055b9cc7f8cc4
Martin Polden <mpolden@mpolden.no>
2023-05-02 15:10:10 +0200

    Cache ZTS access token until expiry

--- client/go/internal/cli/auth/zts/zts.go
+++ client/go/internal/cli/auth/zts/zts.go
@@ -3,22 +3,38 @@ package zts
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/vespa-engine/vespa/client/go/internal/util"
 )
 
-const DefaultURL = "https://zts.athenz.ouroath.com:4443"
+const (
+	DefaultURL  = "https://zts.athenz.ouroath.com:4443"
+	expirySlack = 5 * time.Minute
+)
 
 // Client is a client for Athenz ZTS, an authentication token service.
 type Client struct {
 	client   util.HTTPClient
 	tokenURL *url.URL
 	domain   string
+	now      func() time.Time
+	token    Token
+	mu       sync.Mutex
 }
+
+// Token is an access token retrieved from ZTS.
+type Token struct {
+	Value     string
+	ExpiresAt time.Time
+}
+
+func (t *Token) isExpired(now time.Time) bool { return t.ExpiresAt.Sub(now) < expirySlack }
 
 // NewClient creates a new client for an Athenz ZTS service located at serviceURL.
 func NewClient(client util.HTTPClient, domain, serviceURL string) (*Client, error) {
@@ -27,44 +43,60 @@ func NewClient(client util.HTTPClient, domain, serviceURL string) (*Client, erro
 		return nil, err
 	}
 	tokenURL.Path = "/zts/v1/oauth2/token"
-	return &Client{tokenURL: tokenURL, client: client, domain: domain}, nil
+	return &Client{tokenURL: tokenURL, client: client, domain: domain, now: time.Now}, nil
 }
 
 func (c *Client) Authenticate(request *http.Request) error {
-	accessToken, err := c.AccessToken()
-	if err != nil {
-		return err
+	now := c.now()
+	if c.token.isExpired(now) {
+		c.mu.Lock()
+		if c.token.isExpired(now) {
+			accessToken, err := c.AccessToken()
+			if err != nil {
+				c.mu.Unlock()
+				return err
+			}
+			c.token = accessToken
+		}
+		c.mu.Unlock()
 	}
 	if request.Header == nil {
 		request.Header = make(http.Header)
 	}
-	request.Header.Add("Authorization", "Bearer "+accessToken)
+	request.Header.Add("Authorization", "Bearer "+c.token.Value)
 	return nil
 }
 
 // AccessToken returns an access token within the domain configured in client c.
-func (c *Client) AccessToken() (string, error) {
-	// TODO(mpolden): This should cache and re-use tokens until expiry
+func (c *Client) AccessToken() (Token, error) {
 	data := fmt.Sprintf("grant_type=client_credentials&scope=%s:domain", c.domain)
 	req, err := http.NewRequest("POST", c.tokenURL.String(), strings.NewReader(data))
 	if err != nil {
-		return "", err
+		return Token{}, err
 	}
+	now := c.now()
 	response, err := c.client.Do(req, 10*time.Second)
 	if err != nil {
-		return "", err
+		return Token{}, err
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("zts: got status %d from %s", response.StatusCode, c.tokenURL.String())
+		return Token{}, fmt.Errorf("zts: got status %d from %s", response.StatusCode, c.tokenURL.String())
 	}
 	var ztsResponse struct {
 		AccessToken string `json:"access_token"`
+		ExpirySecs  int    `json:"expires_in"`
 	}
-	dec := json.NewDecoder(response.Body)
-	if err := dec.Decode(&ztsResponse); err != nil {
-		return "", err
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		return Token{}, err
 	}
-	return ztsResponse.AccessToken, nil
+	if err := json.Unmarshal(b, &ztsResponse); err != nil {
+		return Token{}, err
+	}
+	return Token{
+		Value:     ztsResponse.AccessToken,
+		ExpiresAt: now.Add(time.Duration(ztsResponse.ExpirySecs) * time.Second),
+	}, nil
 }
