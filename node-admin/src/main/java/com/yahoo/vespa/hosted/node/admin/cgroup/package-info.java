de066c21c4c48a37423fbc7c000a65a312788516
Håkon Hallingstad <hakon@yahooinc.com>
2023-05-02 14:12:28 +0200

    Add package-info.java for cgroup package

---
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/cgroup/package-info.java
@@ -0,0 +1,9 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+/**
+ * @author hakonhall
+ */
+@ExportPackage
+package com.yahoo.vespa.hosted.node.admin.cgroup;
+
+import com.yahoo.osgi.annotation.ExportPackage;
