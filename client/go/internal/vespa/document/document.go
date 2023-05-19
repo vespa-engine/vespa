3345f5e2228f2969aa27c1bc7f35d09af06f1f69
Martin Polden <mpolden@mpolden.no>
2023-05-19 12:42:23 +0200

    Always drop operations from the buffer

--- client/go/internal/vespa/document/document.go
+++ client/go/internal/vespa/document/document.go
@@ -211,7 +211,7 @@ func (d *Decoder) Decode() (Document, error) {
 	return doc, err
 }
 
-func (d *Decoder) readField(name string, doc *Document) error {
+func (d *Decoder) readField(name string, offset int64, doc *Document) error {
 	readId := false
 	switch name {
 	case "id", "put":
@@ -239,9 +239,9 @@ func (d *Decoder) readField(name string, doc *Document) error {
 		if _, err := d.readNext(jsonObjectStart); err != nil {
 			return err
 		}
-		start := d.dec.InputOffset() - 1
-		// Skip data between the most recent ending position of fields and current offset
-		d.buf.Next(int(start - d.fieldsEnd))
+		// Skip data between start of operation and start of fields
+		fieldsStart := d.dec.InputOffset() - 1
+		d.buf.Next(int(fieldsStart - offset))
 		depth := 1
 		for depth > 0 {
 			t, err := d.dec.ReadToken()
@@ -256,7 +256,7 @@ func (d *Decoder) readField(name string, doc *Document) error {
 			}
 		}
 		d.fieldsEnd = d.dec.InputOffset()
-		fields := d.buf.Next(int(d.fieldsEnd - start))
+		fields := d.buf.Next(int(d.fieldsEnd - fieldsStart))
 		doc.Body = make([]byte, 0, len(fieldsPrefix)+len(fields)+len(fieldsSuffix))
 		doc.Body = append(doc.Body, fieldsPrefix...)
 		doc.Body = append(doc.Body, fields...)
@@ -277,6 +277,7 @@ func (d *Decoder) readField(name string, doc *Document) error {
 }
 
 func (d *Decoder) decode() (Document, error) {
+	start := d.dec.InputOffset()
 	if err := d.guessMode(); err != nil {
 		return Document{}, err
 	}
@@ -300,13 +301,17 @@ loop:
 			if err != nil {
 				return Document{}, err
 			}
-			if err := d.readField(t.String(), &doc); err != nil {
+			if err := d.readField(t.String(), start, &doc); err != nil {
 				return Document{}, err
 			}
 		default:
 			if _, err := d.readNext(jsonObjectEnd); err != nil {
 				return Document{}, err
 			}
+			// Drop operation from the buffer
+			start = max(start, d.fieldsEnd)
+			end := d.dec.InputOffset()
+			d.buf.Next(int(end - start))
 			break loop
 		}
 	}
