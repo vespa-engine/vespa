60339a70a6be71d5bfa1a5848f17fdc4cf903b7d
gjoranv <gv@yahooinc.com>
2023-05-24 00:41:41 +0200

    Do not fail for non-exported publicApi packages.
    - There are valid cases, e.g. annotations and client libraries.

--- bundle-plugin/src/main/java/com/yahoo/container/plugin/classanalysis/PackageInfo.java
+++ bundle-plugin/src/main/java/com/yahoo/container/plugin/classanalysis/PackageInfo.java
@@ -9,12 +9,6 @@ import java.util.Optional;
  */
 record PackageInfo(String name, Optional<ExportPackageAnnotation> exportPackage, boolean isPublicApi) {
 
-    PackageInfo {
-        if (exportPackage.isEmpty() && isPublicApi) {
-            throw new IllegalArgumentException("Package %s is declared PublicApi, but is not exported.".formatted(name));
-        }
-    }
-
     PackageInfo hasExportPackageOrElse(PackageInfo other) {
         return exportPackage().isPresent() ? this : other;
     }
