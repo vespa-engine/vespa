2794cc991a90a56cf519c171f4da61d5dc6e3928
gjoranv <gv@yahooinc.com>
2023-06-09 16:55:40 +0200

    Test refactoring to prepare for more tests.

--- bundle-plugin-test/integration-test/src/test/java/com/yahoo/container/plugin/NonPublicApiDetectionTest.java
+++ bundle-plugin-test/integration-test/src/test/java/com/yahoo/container/plugin/NonPublicApiDetectionTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 import static com.yahoo.container.plugin.BundleTest.findBundleJar;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -20,15 +20,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class NonPublicApiDetectionTest {
 
-    private static Set<String> usedNonPublicApi;
+    private static Attributes mainAttributes;
 
     @BeforeAll
     public static void setup() {
         try {
             File componentJar = findBundleJar("non-public-api-usage");
-            Attributes mainAttributes = new JarFile(componentJar).getManifest().getMainAttributes();
-            var nonPublicApiAttribute = mainAttributes.getValue("X-JDisc-Non-PublicApi-Import-Package");
-            usedNonPublicApi = Arrays.stream(nonPublicApiAttribute.split(",")).collect(Collectors.toSet());
+            mainAttributes = new JarFile(componentJar).getManifest().getMainAttributes();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -36,6 +34,10 @@ public class NonPublicApiDetectionTest {
 
     @Test
     void usage_of_non_publicApi_packages_is_detected() {
+        var nonPublicApiAttribute = mainAttributes.getValue("X-JDisc-Non-PublicApi-Import-Package");
+        assertNotNull(nonPublicApiAttribute);
+        var usedNonPublicApi = Arrays.stream(nonPublicApiAttribute.split(",")).collect(Collectors.toSet());
+
         assertEquals(2, usedNonPublicApi.size());
         assertTrue(usedNonPublicApi.contains("ai.vespa.http"));
         assertTrue(usedNonPublicApi.contains("com.yahoo.io"));
