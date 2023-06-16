167d3a1987687842c3f7b90d84f94cf0f05413f6
Morten Tokle <mortent@yahooinc.com>
2023-06-16 13:21:14 +0200

    export package

---
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/dataplanetoken/package-info.java
@@ -0,0 +1,5 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+@ExportPackage
+package com.yahoo.vespa.hosted.controller.api.integration.dataplanetoken;
+
+import com.yahoo.osgi.annotation.ExportPackage;
