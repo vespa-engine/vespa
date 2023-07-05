4b5d9a1e79da69a0296cdfde3d20e2afd26e931f
jonmv <venstad@gmail.com>
2023-07-05 10:43:28 +0200

    Compute success factor for tenant role maintainer

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/TenantRoleMaintainer.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/maintenance/TenantRoleMaintainer.java
@@ -4,12 +4,17 @@ package com.yahoo.vespa.hosted.controller.maintenance;
 
 import com.yahoo.vespa.hosted.controller.Controller;
 import com.yahoo.vespa.hosted.controller.tenant.Tenant;
+import com.yahoo.yolean.Exceptions;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TenantRoleMaintainer extends ControllerMaintainer {
+
+    private static final Logger log = Logger.getLogger(TenantRoleMaintainer.class.getName());
 
     public TenantRoleMaintainer(Controller controller, Duration tenantRoleMaintainer) {
         super(controller, tenantRoleMaintainer);
@@ -23,21 +28,27 @@ public class TenantRoleMaintainer extends ControllerMaintainer {
                 .limit(5)
                 .toList();
 
+        double ok = 0, attempts = 0, total = 0;
         // Create separate athenz service for all tenants
-        tenants.forEach(roleService::createTenantRole);
+        for (Tenant tenant : tenants) {
+            ++attempts;
+            try {
+                roleService.createTenantRole(tenant);
+            }
+            catch (RuntimeException e) {
+                log.log(Level.WARNING, "Failed to create role for " + tenant.name() + ": " + Exceptions.toMessageString(e));
+            }
+            ++ok;
+        }
+        total += attempts == 0 ? 1 : ok / attempts;
 
-        var tenantsWithRoles = tenants.stream()
-                .map(Tenant::name)
-                .toList();
-        roleService.maintainRoles(tenantsWithRoles);
+        total += roleService.maintainRoles(tenants.stream().map(Tenant::name).toList());
 
         // Update last maintained timestamp
-        var updated = Instant.now(controller().clock());
-        tenants.forEach(t -> {
-            controller().tenants().updateLastTenantRolesMaintained(t.name(), updated);
-        });
+        var updated = controller().clock().instant();
+        for (Tenant tenant : tenants) controller().tenants().updateLastTenantRolesMaintained(tenant.name(), updated);
 
-        return 0.0;
+        return total * 0.5;
     }
 
 
