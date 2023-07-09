c133fa6369638dd500e81c3d6ee599e626c94168
Jon Bratseth <bratseth@vespa.ai>
2023-07-09 20:49:52 +0200

    Don't trim messages

--- vespajlib/src/main/java/com/yahoo/yolean/Exceptions.java
+++ vespajlib/src/main/java/com/yahoo/yolean/Exceptions.java
@@ -27,7 +27,6 @@ public class Exceptions {
         for (; t != null; t = t.getCause()) {
             message = getMessage(t);
             if (message == null) continue;
-            message = message.trim();
             if (message.isEmpty()) continue;
             if (message.equals(lastMessage)) continue;
             if (b.length() > 0) {
