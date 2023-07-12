54c45551d4deeb423e254e0f721806fd6b7f89c5
Jon Bratseth <bratseth@vespa.ai>
2023-07-12 08:58:02 +0200

    Remove duplicate of processing done by GroupIndices

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/Preparer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/Preparer.java
@@ -2,7 +2,6 @@
 package com.yahoo.vespa.hosted.provision.provisioning;
 
 import com.yahoo.config.provision.ApplicationId;
-import com.yahoo.config.provision.ClusterMembership;
 import com.yahoo.config.provision.ClusterSpec;
 import com.yahoo.config.provision.NodeAllocationException;
 import com.yahoo.vespa.hosted.provision.LockedNodeList;
@@ -12,7 +11,6 @@ import com.yahoo.vespa.hosted.provision.NodeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -64,7 +62,6 @@ class Preparer {
                                .filter(node -> node.parentHostname().isEmpty() || activeHosts.parentOf(node).isPresent())
                                .toList();
         }
-        moveToActiveGroup(surplusNodes, requested.groups(), cluster.group());
         return accepted.stream().filter(node -> ! surplusNodes.contains(node)).collect(Collectors.toList());
     }
 
@@ -85,20 +82,6 @@ class Preparer {
                 surplusNodes.add(node);
         }
         return surplusNodes;
-    }
-
-    /** Move nodes from unwanted groups to wanted groups to avoid lingering groups consisting of retired nodes */
-    private void moveToActiveGroup(List<Node> surplusNodes, int wantedGroups, Optional<ClusterSpec.Group> targetGroup) {
-        for (ListIterator<Node> i = surplusNodes.listIterator(); i.hasNext(); ) {
-            Node node = i.next();
-            ClusterMembership membership = node.allocation().get().membership();
-            ClusterSpec cluster = membership.cluster();
-            if (cluster.group().get().index() >= wantedGroups) {
-                ClusterSpec.Group newGroup = targetGroup.orElse(ClusterSpec.Group.from(0));
-                ClusterMembership newGroupMembership = membership.with(cluster.with(Optional.of(newGroup)));
-                i.set(node.with(node.allocation().get().with(newGroupMembership)));
-            }
-        }
     }
 
 }
