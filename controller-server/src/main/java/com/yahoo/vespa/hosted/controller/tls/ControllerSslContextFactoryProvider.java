adf859d798f6a4b785c7f93ec4653c362bd9d125
Andreas Eriksen <andreer@yahooinc.com>
2023-07-27 10:00:21 +0200

    controller tls always use latest cert/key pair (#27903)
    * controller tls always use latest cert/key pair
    
    * more descriptive exceptions

--- controller-server/src/main/java/com/yahoo/vespa/hosted/controller/tls/ControllerSslContextFactoryProvider.java
+++ controller-server/src/main/java/com/yahoo/vespa/hosted/controller/tls/ControllerSslContextFactoryProvider.java
@@ -1,6 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.controller.tls;
 
+import com.google.common.collect.Sets;
 import com.yahoo.component.annotation.Inject;
 import com.yahoo.container.jdisc.secretstore.SecretStore;
 import com.yahoo.jdisc.http.ssl.impl.TlsContextBasedProvider;
@@ -19,6 +20,7 @@ import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -45,9 +47,12 @@ public class ControllerSslContextFactoryProvider extends TlsContextBasedProvider
         this.truststore = KeyStoreBuilder.withType(KeyStoreType.JKS)
                 .fromFile(Paths.get(config.caTrustStore()))
                 .build();
+
+        TlsCredentials tlsCredentials = latestValidCredentials(secretStore, config);
+
         // Key store containing key pair from secret store
         this.keystore = KeyStoreBuilder.withType(KeyStoreType.JKS)
-                .withKeyEntry(getClass().getSimpleName(), privateKey(secretStore, config), certificates(secretStore, config))
+                .withKeyEntry(getClass().getSimpleName(), tlsCredentials.privateKey, tlsCredentials.certificates)
                 .build();
     }
 
@@ -65,17 +70,32 @@ public class ControllerSslContextFactoryProvider extends TlsContextBasedProvider
                 port != 443 ? PeerAuthentication.WANT : PeerAuthentication.DISABLED);
     }
 
+    record TlsCredentials(List<X509Certificate> certificates, PrivateKey privateKey){}
+
+    private static TlsCredentials latestValidCredentials(SecretStore secretStore, TlsConfig tlsConfig) {
+        int version = latestVersionInSecretStore(secretStore, tlsConfig);
+        return new TlsCredentials(certificates(secretStore, tlsConfig, version), privateKey(secretStore, tlsConfig, version));
+    }
+
+    private static int latestVersionInSecretStore(SecretStore secretStore, TlsConfig tlsConfig) {
+        var certVersions = new HashSet<>(secretStore.listSecretVersions(tlsConfig.certificateSecret()));
+        var keyVersions = new HashSet<>(secretStore.listSecretVersions(tlsConfig.privateKeySecret()));
+        return Sets.intersection(certVersions, keyVersions).stream().mapToInt(Integer::intValue).max().orElseThrow(
+                () -> new RuntimeException("No valid certificate versions found in secret store!")
+        );
+    }
+
     /** Get private key from secret store **/
-    private static PrivateKey privateKey(SecretStore secretStore, TlsConfig config) {
-        return KeyUtils.fromPemEncodedPrivateKey(secretStore.getSecret(config.privateKeySecret()));
+    private static PrivateKey privateKey(SecretStore secretStore, TlsConfig config, int version) {
+        return KeyUtils.fromPemEncodedPrivateKey(secretStore.getSecret(config.privateKeySecret(), version));
     }
 
     /**
      * Get certificate from secret store. If certificate secret contains multiple certificates, e.g. intermediate
      * certificates, the entire chain will be read
      */
-    private static List<X509Certificate> certificates(SecretStore secretStore, TlsConfig config) {
-        return X509CertificateUtils.certificateListFromPem(secretStore.getSecret(config.certificateSecret()));
+    private static List<X509Certificate> certificates(SecretStore secretStore, TlsConfig config, int version) {
+        return X509CertificateUtils.certificateListFromPem(secretStore.getSecret(config.certificateSecret(), version));
     }
 
 }
