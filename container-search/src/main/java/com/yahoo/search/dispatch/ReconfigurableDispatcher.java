7e9b3e95de91f67b2beceb57d6a24d59c0cb13b6
jonmv <venstad@gmail.com>
2023-08-16 14:13:53 +0200

    Use correct config id for dispatch config

--- container-search/src/main/java/com/yahoo/search/dispatch/ReconfigurableDispatcher.java
+++ container-search/src/main/java/com/yahoo/search/dispatch/ReconfigurableDispatcher.java
@@ -1,7 +1,9 @@
 package com.yahoo.search.dispatch;
 
 import com.yahoo.component.ComponentId;
+import com.yahoo.component.annotation.Inject;
 import com.yahoo.config.subscription.ConfigSubscriber;
+import com.yahoo.container.QrConfig;
 import com.yahoo.container.handler.VipStatus;
 import com.yahoo.messagebus.network.rpc.SlobrokConfigSubscriber;
 import com.yahoo.vespa.config.search.DispatchConfig;
@@ -22,16 +24,21 @@ public class ReconfigurableDispatcher extends Dispatcher {
 
     private final ConfigSubscriber subscriber;
 
-    public ReconfigurableDispatcher(ComponentId clusterId, DispatchConfig dispatchConfig, VipStatus vipStatus) {
+    @Inject
+    public ReconfigurableDispatcher(ComponentId clusterId, DispatchConfig dispatchConfig, QrConfig qrConfig, VipStatus vipStatus) {
         super(clusterId, dispatchConfig, new DispatchNodesConfig.Builder().build(), vipStatus);
         this.subscriber = new ConfigSubscriber();
-        this.subscriber.subscribe(this::updateWithNewConfig, DispatchNodesConfig.class, clusterId.stringValue());
+        this.subscriber.subscribe(this::updateWithNewConfig, DispatchNodesConfig.class, configId(clusterId, qrConfig));
     }
 
     @Override
     public void deconstruct() {
         subscriber.close();
         super.deconstruct();
+    }
+
+    private static String configId(ComponentId clusterId, QrConfig qrConfig) {
+        return qrConfig.clustername() + "/component/" + clusterId.getName();
     }
 
 }
