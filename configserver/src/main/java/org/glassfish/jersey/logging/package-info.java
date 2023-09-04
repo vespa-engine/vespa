5b60dfc46e42d7c78f68e74f71b76ae8ad41e14d
Bjørn Christian Seime <bjorncs@yahooinc.com>
2023-09-04 17:05:52 +0200

    Move Jersey out of open-source Vespa

--- configserver/src/main/java/org/glassfish/jersey/logging/package-info.java
+++
@@ -1,5 +0,0 @@
-@ExportPackage(version = @Version(major = 2, minor = 25))
-package org.glassfish.jersey.logging;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
--- configserver/src/main/java/org/glassfish/jersey/media/multipart/file/package-info.java
+++
@@ -1,5 +0,0 @@
-@ExportPackage(version = @Version(major = 2, minor = 25))
-package org.glassfish.jersey.media.multipart.file;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
