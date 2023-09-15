335116ff44844c2130f1e026a9f36631176e107b
Tor Egge <Tor.Egge@online.no>
2023-09-15 11:44:42 +0200

    Control folding for FoldedStringCompare::comareFolded using template arguments.

--- searchlib/src/vespa/searchlib/util/foldedstringcompare.cpp
+++ searchlib/src/vespa/searchlib/util/foldedstringcompare.cpp
@@ -15,6 +15,7 @@ size(const char *key)
     return Utf8ReaderForZTS::countChars(key);
 }
 
+template <bool fold_lhs, bool fold_rhs>
 int
 FoldedStringCompare::
 compareFolded(const char *key, const char *okey)
@@ -23,8 +24,8 @@ compareFolded(const char *key, const char *okey)
     Utf8ReaderForZTS oreader(okey);
 
     for (;;) {
-        uint32_t kval = LowerCase::convert(kreader.getChar());
-        uint32_t oval = LowerCase::convert(oreader.getChar());
+        uint32_t kval = fold_lhs ? LowerCase::convert(kreader.getChar()) : kreader.getChar();
+        uint32_t oval = fold_rhs ? LowerCase::convert(oreader.getChar()) : oreader.getChar();
 
         if (kval != oval) {
             if (kval < oval) {
@@ -40,6 +41,7 @@ compareFolded(const char *key, const char *okey)
 }
 
 
+template <bool fold_lhs, bool fold_rhs>
 int
 FoldedStringCompare::
 compareFoldedPrefix(const char *key, const char *okey, size_t prefixLen)
@@ -48,8 +50,8 @@ compareFoldedPrefix(const char *key, const char *okey, size_t prefixLen)
     Utf8ReaderForZTS oreader(okey);
 
     for (size_t j = 0; j < prefixLen; ++j ) {
-        uint32_t kval = LowerCase::convert(kreader.getChar());
-        uint32_t oval = LowerCase::convert(oreader.getChar());
+        uint32_t kval = fold_lhs ? LowerCase::convert(kreader.getChar()) : kreader.getChar();
+        uint32_t oval = fold_rhs ? LowerCase::convert(oreader.getChar()) : oreader.getChar();
 
         if (kval != oval) {
             if (kval < oval) {
@@ -58,7 +60,9 @@ compareFoldedPrefix(const char *key, const char *okey, size_t prefixLen)
                 return 1;
             }
         }
-        if (kval == 0) return 0;
+        if (kval == 0) {
+            return 0;
+        }
     }
     // reached end of prefix
     return 0;
@@ -68,9 +72,11 @@ int
 FoldedStringCompare::
 comparePrefix(const char *key, const char *okey, size_t prefixLen)
 {
-    int res = compareFoldedPrefix(key, okey, prefixLen);
-    if (res != 0) return res;
-    return strncmp(key, okey, prefixLen);
+    int res = compareFoldedPrefix<true, true>(key, okey, prefixLen);
+    if (res != 0) {
+        return res;
+    }
+    return compareFoldedPrefix<false, false>(key, okey, prefixLen);
 }
 
 
@@ -78,10 +84,22 @@ int
 FoldedStringCompare::
 compare(const char *key, const char *okey)
 {
-    int res = compareFolded(key, okey);
-    if (res != 0) return res;
+    int res = compareFolded<true, true>(key, okey);
+    if (res != 0) {
+        return res;
+    }
     return strcmp(key, okey);
 }
+
+template int FoldedStringCompare::compareFolded<false, false>(const char* key, const char* okey);
+template int FoldedStringCompare::compareFolded<false, true>(const char* key, const char* okey);
+template int FoldedStringCompare::compareFolded<true, false>(const char* key, const char* okey);
+template int FoldedStringCompare::compareFolded<true, true>(const char* key, const char* okey);
+
+template int FoldedStringCompare::compareFoldedPrefix<false, false>(const char* key, const char* okey, size_t prefixLen);
+template int FoldedStringCompare::compareFoldedPrefix<false, true>(const char* key, const char* okey, size_t prefixLen);
+template int FoldedStringCompare::compareFoldedPrefix<true, false>(const char* key, const char* okey, size_t prefixLen);
+template int FoldedStringCompare::compareFoldedPrefix<true, true>(const char* key, const char* okey, size_t prefixLen);
 
 } // namespace search
 
