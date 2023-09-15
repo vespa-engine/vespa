7f2be58127647a95c9bc44d1f7100e65a3fcfc09
Tor Egge <Tor.Egge@online.no>
2023-09-15 16:44:00 +0200

    Add DfaStringComparator.

---
+++ searchlib/src/vespa/searchlib/attribute/dfa_string_comparator.h
@@ -0,0 +1,34 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+#include "i_enum_store.h"
+#include <vespa/vespalib/datastore/unique_store_string_comparator.h>
+
+namespace search::attribute {
+
+/**
+ * Less-than comparator used for comparing next candidate string
+ * (successor) from vespa::fuzzy::LevenshteinDfa with strings stored
+ * in an enum store as part of a dictionary iterator seek, skipping
+ * entries that don't match the fuzzy term.
+ *
+ * The code points from the candidate string are not folded during
+ * the comparison.
+ */
+
+class DfaStringComparator : public vespalib::datastore::UniqueStoreStringComparator<IEnumStore::InternalIndex>
+{
+public:
+    using ParentType = vespalib::datastore::UniqueStoreStringComparator<IEnumStore::InternalIndex>;
+    using DataStoreType = ParentType::DataStoreType;
+private:
+    using ParentType::get;
+
+public:
+    DfaStringComparator(const DataStoreType& data_store, const char* candidate);
+
+    bool less(const vespalib::datastore::EntryRef lhs, const vespalib::datastore::EntryRef rhs) const override;
+};
+
+}
