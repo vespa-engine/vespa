1f63858ebac9b11c6b452165f44f86c58b947610
Morten Tokle <mortent@yahooinc.com>
2023-09-21 13:32:02 +0200

    Improve test

--- jdisc-security-filters/src/test/java/com/yahoo/jdisc/http/filter/security/cloud/CloudTokenDataPlaneFilterTest.java
+++ jdisc-security-filters/src/test/java/com/yahoo/jdisc/http/filter/security/cloud/CloudTokenDataPlaneFilterTest.java
@@ -167,21 +167,26 @@ class CloudTokenDataPlaneFilterTest {
     }
 
     @Test
-    void allows_empty_clients() {
-        var emptyClientsFilter = new CloudTokenDataPlaneFilter(
-                new CloudTokenDataPlaneFilterConfig.Builder()
-                        .tokenContext(TOKEN_CONTEXT)
-                        .build(),
-                clock);
-
+    void rejects_tokens_on_empty_clients() {
         var req = FilterTestUtils.newRequestBuilder()
                 .withMethod(Method.GET)
                 .withHeader("Authorization", "Bearer " + UNKNOWN_TOKEN.secretTokenString())
                 .build();
         var responseHandler = new MockResponseHandler();
-        emptyClientsFilter.filter(req, responseHandler);
+        newFilterWithEmptyClientsConfig().filter(req, responseHandler);
         assertNotNull(responseHandler.getResponse());
         assertEquals(FORBIDDEN, responseHandler.getResponse().getStatus());
+    }
+
+    @Test
+    void rejects_missing_tokens_on_empty_clients() {
+        var req = FilterTestUtils.newRequestBuilder()
+                .withMethod(Method.GET)
+                .build();
+        var responseHandler = new MockResponseHandler();
+        newFilterWithEmptyClientsConfig().filter(req, responseHandler);
+        assertNotNull(responseHandler.getResponse());
+        assertEquals(UNAUTHORIZED, responseHandler.getResponse().getStatus());
     }
 
     private CloudTokenDataPlaneFilter newFilterWithClientsConfig() {
@@ -205,6 +210,14 @@ class CloudTokenDataPlaneFilterTest {
                                                         .expirations(TOKEN_EXPIRATION.toString()))
                                         .permissions(WRITE.asString())
                                         .id(TOKEN_FEED_CLIENT)))
+                        .build(),
+                clock);
+    }
+
+    private CloudTokenDataPlaneFilter newFilterWithEmptyClientsConfig() {
+        return new CloudTokenDataPlaneFilter(
+                new CloudTokenDataPlaneFilterConfig.Builder()
+                        .tokenContext(TOKEN_CONTEXT)
                         .build(),
                 clock);
     }
