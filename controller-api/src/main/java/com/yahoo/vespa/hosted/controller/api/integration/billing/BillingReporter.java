7d7d0ccc4820988df376e3ece3c63540f41d8994
Øyvind Grønnesby <oyving@yahooinc.com>
2023-09-26 11:08:13 +0200

    Report customer status periodically

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/billing/BillingReporter.java
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/billing/BillingReporter.java
@@ -1,5 +1,10 @@
 package com.yahoo.vespa.hosted.controller.api.integration.billing;
 
+import com.yahoo.vespa.hosted.controller.tenant.BillingReference;
+import com.yahoo.vespa.hosted.controller.tenant.CloudTenant;
+
 public interface BillingReporter {
-    double maintain();
+    double maintainResources();
+
+    BillingReference maintainTenant(CloudTenant tenant);
 }
