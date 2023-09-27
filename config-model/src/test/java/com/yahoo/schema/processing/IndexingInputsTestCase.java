9e1fd455fb3bd911bd6183c42e3d513c3f6278af
Jon Bratseth <bratseth@vespa.ai>
2023-09-27 17:43:17 +0200

    Simplify tests

--- config-model/src/test/java/com/yahoo/schema/processing/IndexingInputsTestCase.java
+++ config-model/src/test/java/com/yahoo/schema/processing/IndexingInputsTestCase.java
@@ -6,43 +6,104 @@ import com.yahoo.schema.parser.ParseException;
 import com.yahoo.yolean.Exceptions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
-import static com.yahoo.schema.processing.AssertSearchBuilder.assertBuildFails;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author Simon Thoresen Hult
+ * @author bratseth
  */
 public class IndexingInputsTestCase {
 
     @Test
-    void requireThatExtraFieldInputExtraFieldThrows() throws IOException, ParseException {
-        assertBuildFails("src/test/examples/indexing_extra_field_input_extra_field.sd",
-                "For schema 'indexing_extra_field_input_extra_field', field 'bar': Indexing script refers " +
-                        "to field 'bar' which is neither a field in document type " +
-                        "'indexing_extra_field_input_extra_field' nor a mutable attribute");
+    void requireThatExtraFieldInputExtraFieldThrows() throws ParseException {
+        try {
+            var schema = """
+                    search indexing_extra_field_input_extra_field {
+                        document indexing_extra_field_input_extra_field {
+                        }
+                        field foo type string {
+                        }
+                        field bar type string {
+                            indexing: input bar | index
+                        }
+                    }
+                   """;
+            ApplicationBuilder.createFromString(schema);
+            fail("Expected exception");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("For schema 'indexing_extra_field_input_extra_field', field 'bar': Indexing script refers " +
+                         "to field 'bar' which is neither a field in document type " +
+                         "'indexing_extra_field_input_extra_field' nor a mutable attribute",
+                         Exceptions.toMessageString(e));
+        }
     }
 
     @Test
-    void requireThatExtraFieldInputImplicitThrows() throws IOException, ParseException {
-        assertBuildFails("src/test/examples/indexing_extra_field_input_implicit.sd",
-                "For schema 'indexing_extra_field_input_implicit', field 'foo': " +
-                "For expression '{ tokenize normalize stem:\"BEST\" | index foo; }': Expected string input, but no input is specified");
+    void requireThatExtraFieldInputImplicitThrows() throws ParseException {
+        try {
+            var schema = """
+                    search indexing_extra_field_input_implicit {
+                        document indexing_extra_field_input_implicit {
+                        }
+                        field foo type string {
+                            indexing: index
+                        }
+                    }
+                   """;
+            ApplicationBuilder.createFromString(schema);
+            fail("Expected exception");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("For schema 'indexing_extra_field_input_implicit', field 'foo': " +
+                         "For expression '{ tokenize normalize stem:\"BEST\" | index foo; }': Expected string input, but no input is specified",
+                         Exceptions.toMessageString(e));
+        }
     }
 
     @Test
-    void requireThatExtraFieldInputNullThrows() throws IOException, ParseException {
-        assertBuildFails("src/test/examples/indexing_extra_field_input_null.sd",
-                "For schema 'indexing_extra_field_input_null', field 'foo': Indexing script refers to field " +
-                        "'foo' which is neither a field in document type 'indexing_extra_field_input_null' nor a mutable attribute");
+    void requireThatExtraFieldInputNullThrows() throws ParseException {
+        try {
+            var schema = """
+                    search indexing_extra_field_input_null {
+                        document indexing_extra_field_input_null {
+                        }
+                        field foo type string {
+                            indexing: input foo | index
+                        }
+                    }
+                   """;
+            ApplicationBuilder.createFromString(schema);
+            fail("Expected exception");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("For schema 'indexing_extra_field_input_null', field 'foo': Indexing script refers to field " +
+                         "'foo' which is neither a field in document type 'indexing_extra_field_input_null' nor a mutable attribute",
+                         Exceptions.toMessageString(e));
+        }
     }
 
     @Test
-    void requireThatExtraFieldInputSelfThrows() throws IOException, ParseException {
-        assertBuildFails("src/test/examples/indexing_extra_field_input_self.sd",
-                "For schema 'indexing_extra_field_input_self', field 'foo': Indexing script refers to field " +
-                        "'foo' which is neither a field in document type 'indexing_extra_field_input_self' nor a mutable attribute");
+    void requireThatExtraFieldInputSelfThrows() throws ParseException {
+        try {
+            var schema = """
+                        search indexing_extra_field_input_self {
+                            document indexing_extra_field_input_self {
+                            }
+                            field foo type string {
+                                indexing: input foo | index
+                            }
+                        }
+                    """;
+            ApplicationBuilder.createFromString(schema);
+            fail("Expected exception");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("For schema 'indexing_extra_field_input_self', field 'foo': Indexing script refers to field " +
+                         "'foo' which is neither a field in document type 'indexing_extra_field_input_self' nor a mutable attribute",
+                         Exceptions.toMessageString(e));
+        }
     }
 
     @Test
@@ -92,6 +153,7 @@ public class IndexingInputsTestCase {
                     }
                     """;
             ApplicationBuilder.createFromString(schema);
+            fail("Expected exception");
         }
         catch (IllegalArgumentException e) {
             assertEquals("For schema 'test', field 'derived1': For expression '{ attribute derived1; }': " +
