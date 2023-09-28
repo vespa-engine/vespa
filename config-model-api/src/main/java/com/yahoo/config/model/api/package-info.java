c0c620ed621f52096df7d1dc6dcfa4970c47d363
Harald Musum <musum@yahooinc.com>
2023-09-28 09:32:28 +0200

    Add note about these API being internal public APIs

--- config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/package-info.java
@@ -1,6 +1,6 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
-@PublicApi
+@PublicApi // Internal public API, not a real public API
 package com.yahoo.config.model.api;
 
 import com.yahoo.osgi.annotation.ExportPackage;
