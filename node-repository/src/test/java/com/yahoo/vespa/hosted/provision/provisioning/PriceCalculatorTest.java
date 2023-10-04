cf61645cfc7ee1bd2cdaac9041ce9e0b91e278f5
Jon Bratseth <bratseth@vespa.ai>
2023-10-04 10:22:22 +0200

    Cleanup

--- node-repository/src/test/java/com/yahoo/vespa/hosted/provision/provisioning/PriceCalculatorTest.java
+++ node-repository/src/test/java/com/yahoo/vespa/hosted/provision/provisioning/PriceCalculatorTest.java
@@ -71,7 +71,21 @@ public class PriceCalculatorTest {
     }
 
     @Test
-    public void testEnclavePrice2() {
+    public void testLargeVolumePrice() {
+        PriceCalculator calculator = new PriceCalculator();
+        var c1 = new ClusterResources(29, 1, new NodeResources(10, 100, 1000, 0.3));
+        var c2 = new ClusterResources(31, 1, new NodeResources(10, 100, 1000, 0.3,
+                                                               NodeResources.DiskSpeed.fast, NodeResources.StorageType.local,
+                                                               NodeResources.Architecture.x86_64,
+                                                               new NodeResources.GpuResources(2, 40)));
+        var pricingInfo = new PriceCalculator.PricingInfo(false, PricingInfo.SupportLevel.STANDARD, 40);
+        // System.out.println("Yearly cost:  " + 24*365*(c1.cost() + c2.cost()));
+        // System.out.println("Yearly price: " + 24*365*calculator.price(List.of(c1, c2), pricingInfo));
+        assertEquals(153.60, calculator.price(List.of(c1, c2), pricingInfo), 0.01);
+    }
+
+    @Test
+    public void testLargeVolumeEnclavePrice() {
         PriceCalculator calculator = new PriceCalculator();
         var c1 = new ClusterResources(29, 1, new NodeResources(10, 100, 1000, 0.3));
         var c2 = new ClusterResources(31, 1, new NodeResources(10, 100, 1000, 0.3,
@@ -79,8 +93,8 @@ public class PriceCalculatorTest {
                                                                NodeResources.Architecture.x86_64,
                                                                new NodeResources.GpuResources(2, 40)));
         var pricingInfo = new PriceCalculator.PricingInfo(true, PricingInfo.SupportLevel.STANDARD, 40);
-        System.out.println("Cost:  " + 24*365*(c1.cost() + c2.cost()));
-        System.out.println("Price: " + 24*365*calculator.price(List.of(c1, c2), pricingInfo));
+        // System.out.println("Yearly cost:  " + 24*365*(c1.cost() + c2.cost()));
+        // System.out.println("Yearly price: " + 24*365*calculator.price(List.of(c1, c2), pricingInfo));
         assertEquals(40.21, calculator.price(List.of(c1, c2), pricingInfo), 0.01);
     }
 
