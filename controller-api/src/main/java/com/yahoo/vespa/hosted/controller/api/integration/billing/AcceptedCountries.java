0f2ce7b6011610ba2af87a19e2a2eeb90bc0f94f
Bjørn Christian Seime <bjorncs@vespa.ai>
2023-10-26 13:46:01 +0200

    Introduce method to `/billing/v2` that returns accepted countries with tax types

---
+++ controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/integration/billing/AcceptedCountries.java
@@ -0,0 +1,23 @@
+// Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+package com.yahoo.vespa.hosted.controller.api.integration.billing;
+
+import java.util.List;
+
+/**
+ * @author bjorncs
+ */
+public record AcceptedCountries(List<Country> countries) {
+
+    public AcceptedCountries {
+        countries = List.copyOf(countries);
+    }
+
+    public record Country(String code, String displayName, List<TaxType> taxTypes) {
+        public Country {
+            taxTypes = List.copyOf(taxTypes);
+        }
+    }
+
+    public record TaxType(String id, String description, String pattern, String example) {}
+}
