596421557e3165ef25dd478edf64b2812d5b4777
gjoranv <gjoranv@gmail.com>
2023-11-03 18:08:32 +0100

    More controller code to internal repo.

--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/api/systemflags/v1/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/**
- * @author bjorncs
- */
-@ExportPackage
-package com.yahoo.vespa.hosted.controller.api.systemflags.v1;
-
-import com.yahoo.osgi.annotation.ExportPackage;
--- controller-api/src/main/java/com/yahoo/vespa/hosted/controller/tenant/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-/**
- * @author mpolden
- */
-@ExportPackage
-package com.yahoo.vespa.hosted.controller.tenant;
-
-import com.yahoo.osgi.annotation.ExportPackage;
