808831f0b51fd4f85239e5659cc1fb8200c70994
Valerij Fredriksen <valerijf@vespa.ai>
2023-11-04 09:44:19 +0100

    Reapply "Move node-admin"

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/configserver/orchestrator/OrchestratorNotFoundException.java
+++
@@ -1,9 +0,0 @@
-// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.hosted.node.admin.configserver.orchestrator;
-
-@SuppressWarnings("serial")
-public class OrchestratorNotFoundException extends OrchestratorException {
-    public OrchestratorNotFoundException(String message) {
-        super(message, true);
-    }
-}
--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/container/metrics/MetricValue.java
+++
@@ -1,9 +0,0 @@
-// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.hosted.node.admin.container.metrics;
-
-/**
- * @author freva
- */
-public interface MetricValue {
-    Number getValue();
-}
