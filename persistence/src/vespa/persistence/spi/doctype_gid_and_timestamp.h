64369b4a2e36c4d9b354fe3ae7fdbb35e17023f3
Tor Egge <Tor.Egge@online.no>
2023-11-06 22:17:15 +0100

    Compare all elements of DocTypeGidAndTimestamp.

--- persistence/src/vespa/persistence/spi/doctype_gid_and_timestamp.h
+++ persistence/src/vespa/persistence/spi/doctype_gid_and_timestamp.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 #include <vespa/document/base/globalid.h>
+#include <vespa/vespalib/stllike/hash_fun.h>
 #include <vespa/vespalib/stllike/string.h>
 #include <iosfwd>
 
@@ -30,7 +31,8 @@ struct DocTypeGidAndTimestamp {
     DocTypeGidAndTimestamp& operator=(DocTypeGidAndTimestamp&&) noexcept;
 
     bool operator==(const DocTypeGidAndTimestamp& rhs) const noexcept {
-        return ((doc_type == rhs.doc_type) && (gid == rhs.gid));
+        return ((doc_type == rhs.doc_type) && (gid == rhs.gid) &&
+                (timestamp == rhs.timestamp));
     }
 
     void print(vespalib::asciistream&) const;
@@ -38,7 +40,8 @@ struct DocTypeGidAndTimestamp {
 
     struct hash {
         size_t operator()(const DocTypeGidAndTimestamp& dt_gid_ts) const noexcept {
-            const size_t h = document::GlobalId::hash()(dt_gid_ts.gid);
+            size_t h = document::GlobalId::hash()(dt_gid_ts.gid);
+            h = h ^ (vespalib::hash<vespalib::string>()(dt_gid_ts.doc_type) + 0x9e3779b9U + (h << 6U) + (h >> 2U));
             return h ^ (dt_gid_ts.timestamp + 0x9e3779b9U + (h << 6U) + (h >> 2U)); // Basically boost::hash_combine
         }
     };
