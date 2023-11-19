f5d72158d305cf027f7ad34956303ac669802046
Henning Baldersheim <balder@yahoo-inc.com>
2023-11-19 23:36:09 +0100

    Control always-mark-phrase-expensive with feature flag.

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -320,6 +320,13 @@ public class Flags {
             "Takes effect at redeployment",
             INSTANCE_ID);
 
+    public static final UnboundBooleanFlag ALWAYS_MARK_PHRASE_EXPENSIVE = defineFeatureFlag(
+            "always-mark-phrase-expensive", false,
+            List.of("baldersheim"), "2023-11-20", "2023-12-31",
+            "If true all phrases will be marked expensive, independent of parents",
+            "Takes effect at redeployment",
+            INSTANCE_ID);
+
     public static final UnboundBooleanFlag WRITE_CONFIG_SERVER_SESSION_DATA_AS_ONE_BLOB = defineFeatureFlag(
             "write-config-server-session-data-as-blob", false,
             List.of("hmusum"), "2023-07-19", "2024-01-01",
