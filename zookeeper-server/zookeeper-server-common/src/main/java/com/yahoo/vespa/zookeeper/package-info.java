2a3b518b8fcc15d3a83d0dcb46e7d02643a9627a
Harald Musum <musum@vespa.ai>
2023-12-14 18:29:37 +0100

    Revert "Jonmv/zk 3.9.1 clients 2"

--- zookeeper-server/zookeeper-server-common/src/main/java/com/yahoo/vespa/zookeeper/package-info.java
+++ zookeeper-server/zookeeper-server-common/src/main/java/com/yahoo/vespa/zookeeper/package-info.java
@@ -1,4 +1,5 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
-package com.yahoo.vespa.zookeeper.client;
+package com.yahoo.vespa.zookeeper;
+
 import com.yahoo.osgi.annotation.ExportPackage;
