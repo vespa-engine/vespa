9cf9e62f0d97a0530936c829062320859d3db06c
jonmv <venstad@gmail.com>
2023-12-15 17:04:22 +0100

    Revert "Merge pull request #29674 from vespa-engine/revert-29671-jonmv/reapply-zk-3.9.1"
    This reverts commit 28f8cf3e298d51ca703ceee36a992297d38637cc, reversing
    changes made to 3a9f89fe60e3420eed435daee435a4f8534c9512.

--- zkfacade/src/main/java/com/yahoo/vespa/zookeeper/client/package-info.java
+++ zkfacade/src/main/java/com/yahoo/vespa/zookeeper/client/package-info.java
@@ -1,5 +1,4 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
-package com.yahoo.vespa.zookeeper;
-
+package com.yahoo.vespa.zookeeper.client;
 import com.yahoo.osgi.annotation.ExportPackage;
