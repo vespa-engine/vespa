f6e22f410e4790f87ce1b867376d012ac47c0d47
Harald Musum <musum@yahooinc.com>
2024-01-12 13:47:54 +0100

    Define new cloud

--- hosted-zone-api/src/main/java/ai/vespa/cloud/Cloud.java
+++ hosted-zone-api/src/main/java/ai/vespa/cloud/Cloud.java
@@ -4,7 +4,8 @@ package ai.vespa.cloud;
 /**
  * The cloud provider in which a cloud deployment may be running.
  *
- * This is "aws" when this runs in Amazon Web Services, and "gcp" when this runs in Google Cloud Platform.
+ * This is "aws" when this runs in Amazon Web Services, "gcp" when this runs in Google Cloud Platform and
+ * "azure" when this runs in Microsoft Azure.
  *
  * @author mpolden
  */
