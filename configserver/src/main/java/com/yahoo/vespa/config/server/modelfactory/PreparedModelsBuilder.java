1a23b2a258d50583e4923a33abfe449e09c5c00d
Jon Marius Venstad <jonmv@users.noreply.github.com>
2024-01-16 14:35:02 +0100

    Revert "Reapply defer container config changes when restarting"

--- configserver/src/main/java/com/yahoo/vespa/config/server/modelfactory/PreparedModelsBuilder.java
+++ configserver/src/main/java/com/yahoo/vespa/config/server/modelfactory/PreparedModelsBuilder.java
@@ -128,7 +128,6 @@ public class PreparedModelsBuilder extends ModelsBuilder<PreparedModelsBuilder.P
                 createModelContextProperties(modelFactory.version(), applicationPackage),
                 getAppDir(applicationPackage),
                 onnxModelCost,
-                Set.of(), // Known after preparation and validation, passed from activated models builder only.
                 wantedDockerImageRepository,
                 modelVersion,
                 wantedNodeVespaVersion);
