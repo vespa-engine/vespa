8571d2effcb0ba53604632592eebb97d47451af4
Tor Egge <tegge@vespa.ai>
2024-01-21 17:58:11 +0100

    Include ostream header to get definition of std::ostream

--- searchlib/src/vespa/searchlib/query/streaming/querynoderesultbase.cpp
+++ searchlib/src/vespa/searchlib/query/streaming/querynoderesultbase.cpp
@@ -1,5 +1,6 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #include "querynoderesultbase.h"
+#include <ostream>
 
 namespace search::streaming {
 
