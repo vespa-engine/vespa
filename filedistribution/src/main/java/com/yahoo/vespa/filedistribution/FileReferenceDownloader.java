8734b1c8a73fbe936c71ef46b94df8e320798ed8
jonmv <venstad@gmail.com>
2024-01-22 11:29:10 +0100

    Download packages in parallel in maintainer

--- filedistribution/src/main/java/com/yahoo/vespa/filedistribution/FileReferenceDownloader.java
+++ filedistribution/src/main/java/com/yahoo/vespa/filedistribution/FileReferenceDownloader.java
@@ -15,6 +15,7 @@ import java.time.Instant;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -98,7 +99,7 @@ public class FileReferenceDownloader {
         }
     }
 
-    Future<Optional<File>> startDownload(FileReferenceDownload fileReferenceDownload) {
+    CompletableFuture<Optional<File>> startDownload(FileReferenceDownload fileReferenceDownload) {
         FileReference fileReference = fileReferenceDownload.fileReference();
         Optional<FileReferenceDownload> inProgress = downloads.get(fileReference);
         if (inProgress.isPresent()) return inProgress.get().future();
