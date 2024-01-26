a33e3149df36528a48bb8a3da17a4fb95c6a8b23
Tor Brede Vekterli <vekterli@vespa.ai>
2024-01-26 17:47:11 +0100

    Revert "Revert "Jonmv/reapply leaner token buffer""

--- document/src/main/java/com/yahoo/document/json/document/DocumentParser.java
+++ document/src/main/java/com/yahoo/document/json/document/DocumentParser.java
@@ -20,7 +20,7 @@ public class DocumentParser {
     private static final String UPDATE = "update";
     private static final String PUT = "put";
     private static final String ID = "id";
-    private static final String CONDITION = "condition";
+    public static final String CONDITION = "condition";
     public static final String CREATE_IF_NON_EXISTENT = "create";
     public static final String FIELDS = "fields";
     public static final String REMOVE = "remove";
@@ -86,16 +86,6 @@ public class DocumentParser {
     private void handleIdentLevelOne(DocumentParseInfo documentParseInfo, boolean docIdAndOperationIsSetExternally)
             throws IOException {
         JsonToken currentToken = parser.getCurrentToken();
-        if (currentToken == JsonToken.VALUE_TRUE || currentToken == JsonToken.VALUE_FALSE) {
-            try {
-                if (CREATE_IF_NON_EXISTENT.equals(parser.getCurrentName())) {
-                    documentParseInfo.create = Optional.ofNullable(parser.getBooleanValue());
-                    return;
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Got IO exception while parsing document", e);
-            }
-        }
         if ((currentToken == JsonToken.VALUE_TRUE || currentToken == JsonToken.VALUE_FALSE) &&
                 CREATE_IF_NON_EXISTENT.equals(parser.getCurrentName())) {
             documentParseInfo.create = Optional.of(currentToken == JsonToken.VALUE_TRUE);
@@ -111,12 +101,11 @@ public class DocumentParser {
         }
     }
 
-    private  void handleIdentLevelTwo(DocumentParseInfo documentParseInfo) {
+    private void handleIdentLevelTwo(DocumentParseInfo documentParseInfo) {
         try {
-            JsonToken currentToken = parser.getCurrentToken();
             // "fields" opens a dictionary and is therefore on level two which might be surprising.
-            if (currentToken == JsonToken.START_OBJECT && FIELDS.equals(parser.getCurrentName())) {
-                documentParseInfo.fieldsBuffer.bufferObject(currentToken, parser);
+            if (parser.currentToken() == JsonToken.START_OBJECT && FIELDS.equals(parser.getCurrentName())) {
+                documentParseInfo.fieldsBuffer.bufferObject(parser);
                 processIndent();
             }
         } catch (IOException e) {
