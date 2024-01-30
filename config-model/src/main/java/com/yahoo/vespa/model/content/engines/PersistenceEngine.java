9980f0009e99858b2c04981a1c6cfdd7728f65dd
Henning Baldersheim <balder@yahoo-inc.com>
2024-01-30 17:06:13 +0100

    Revert has been long gone.

--- config-model/src/main/java/com/yahoo/vespa/model/content/engines/PersistenceEngine.java
+++ config-model/src/main/java/com/yahoo/vespa/model/content/engines/PersistenceEngine.java
@@ -24,14 +24,6 @@ public abstract class PersistenceEngine extends TreeConfigProducer<AnyConfigProd
         PersistenceEngine create(DeployState deployState, StorageNode storageNode, StorageGroup parentGroup, ModelElement storageNodeElement);
 
         /**
-         * If a write request succeeds on some nodes and fails on others, causing request to
-         * fail to client, the content layer will revert the operation where it succeeded if
-         * reverts are supported. (Typically require backend to keep multiple entries of the
-         * same document identifier persisted at the same time)
-         */
-        boolean supportRevert();
-
-        /**
          * Multi level splitting can increase split performance a lot where documents have been
          * co-localized, for backends where retrieving document identifiers contained in bucket
          * is cheap. Backends where split is cheaper than fetching document identifiers will
