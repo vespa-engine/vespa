f3806e470bb093c51f63653e7318ae2f4ba9e959
Harald Musum <musum@vespa.ai>
2024-02-26 13:50:59 +0100

    Revert "Revert "Use class names instead of strings""

---
+++ container-search/src/main/java/com/yahoo/vespa/streamingvisitors/package-info.java
@@ -0,0 +1,6 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+@ExportPackage
+package com.yahoo.vespa.streamingvisitors;
+
+import com.yahoo.osgi.annotation.ExportPackage;
