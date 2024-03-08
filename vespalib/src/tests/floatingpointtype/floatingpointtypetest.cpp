ad93b7abf48cfb90af9b034a4bca30d70b028303
Tor Egge <Tor.Egge@online.no>
2024-03-08 13:18:47 +0100

    Rewrite floating point type unit test to gtest.

--- vespalib/src/tests/floatingpointtype/floatingpointtypetest.cpp
+++ vespalib/src/tests/floatingpointtype/floatingpointtypetest.cpp
@@ -1,17 +1,9 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/vespalib/testkit/testapp.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/objects/floatingpointtype.h>
 
-class Test : public vespalib::TestApp
-{
-public:
-    void testFloatingPoint();
-    int Main() override;
-};
-
-void
-Test::testFloatingPoint()
+TEST(FloatingPointTypeTest, test_floating_point)
 {
     vespalib::Double d1(1.0);
     vespalib::Double d2(1.000000000000001);
@@ -20,11 +12,11 @@ Test::testFloatingPoint()
 
     EXPECT_TRUE(d1.getValue() != d2.getValue());
 
-    EXPECT_EQUAL(d1, d2);
-    EXPECT_EQUAL(d2, d1);
+    EXPECT_EQ(d1, d2);
+    EXPECT_EQ(d2, d1);
 
-    EXPECT_NOT_EQUAL(d1, d3);
-    EXPECT_NOT_EQUAL(d1, d4);
+    EXPECT_NE(d1, d3);
+    EXPECT_NE(d1, d4);
 
     EXPECT_TRUE(d1 - d2 == 0);
     EXPECT_TRUE(d2 - d1 == 0);
@@ -40,34 +32,26 @@ Test::testFloatingPoint()
     EXPECT_TRUE(!(d1 < 1));
     EXPECT_TRUE(!(d1 > 1));
 
-    EXPECT_EQUAL(d2 * 4, d4);
+    EXPECT_EQ(d2 * 4, d4);
 
-    EXPECT_EQUAL(++d4, 5.0);
-    EXPECT_EQUAL(d4++, 5.0);
-    EXPECT_EQUAL(d4, 6.0);
+    EXPECT_EQ(++d4, 5.0);
+    EXPECT_EQ(d4++, 5.0);
+    EXPECT_EQ(d4, 6.0);
 
     d4 /= 3;
-    EXPECT_EQUAL(d4, 2.00000000001);
+    EXPECT_EQ(d4, 2.00000000001);
     d4 *= 2;
-    EXPECT_EQUAL(d4, 4.000000000001);
+    EXPECT_EQ(d4, 4.000000000001);
 
-    EXPECT_EQUAL(--d4, 3.0);
-    EXPECT_EQUAL(d4--, 3.0);
-    EXPECT_EQUAL(d4, 2.0);
+    EXPECT_EQ(--d4, 3.0);
+    EXPECT_EQ(d4--, 3.0);
+    EXPECT_EQ(d4, 2.0);
     d4 /= 0.50000000001;
 
-    EXPECT_EQUAL(d4, 4.0);
+    EXPECT_EQ(d4, 4.0);
 
     EXPECT_TRUE(!(d3 + 1 > 0));
     EXPECT_TRUE(!(d3 + 1 < 0));
 }
 
-int
-Test::Main()
-{
-    TEST_INIT("floatingpointtype_test");
-    testFloatingPoint();
-    TEST_DONE();
-}
-
-TEST_APPHOOK(Test)
+GTEST_MAIN_RUN_ALL_TESTS()
