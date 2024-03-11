77285fac0a550736011fe18ca90668fef8f104b0
Tor Egge <Tor.Egge@online.no>
2024-03-11 21:09:08 +0100

    Rewrite searchcore FeedToken unit test to gtest.

--- searchcore/src/tests/proton/feedtoken/feedtoken_test.cpp
+++ searchcore/src/tests/proton/feedtoken/feedtoken_test.cpp
@@ -1,6 +1,6 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/searchcore/proton/common/feedtoken.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/util/exceptions.h>
 
 using namespace proton;
@@ -21,47 +21,24 @@ public:
     size_t getReceivedCount() const { return _receivedCount; }
 };
 
-class Test : public vespalib::TestApp {
-private:
-    void testAck();
-    void testFail();
-    void testHandover();
-
-public:
-    int Main() override {
-        TEST_INIT("feedtoken_test");
-
-        testAck();       TEST_FLUSH();
-        testFail();      TEST_FLUSH();
-        testHandover();  TEST_FLUSH();
-
-        TEST_DONE();
-    }
-};
-
-TEST_APPHOOK(Test);
-
-void
-Test::testAck()
+TEST(FeedTokenTest, test_ack)
 {
     LocalTransport transport;
     {
         FeedToken token = feedtoken::make(transport);
     }
-    EXPECT_EQUAL(1u, transport.getReceivedCount());
+    EXPECT_EQ(1u, transport.getReceivedCount());
 }
 
-void
-Test::testFail()
+TEST(FeedTokenTest, test_fail)
 {
     LocalTransport transport;
     FeedToken token = feedtoken::make(transport);
     token->fail();
-    EXPECT_EQUAL(1u, transport.getReceivedCount());
+    EXPECT_EQ(1u, transport.getReceivedCount());
 }
 
-void
-Test::testHandover()
+TEST(FeedTokenTest, test_handover)
 {
     struct MyHandover {
         static FeedToken handover(FeedToken token) {
@@ -75,7 +52,7 @@ Test::testHandover()
         FeedToken token = feedtoken::make(transport);
         token = MyHandover::handover(token);
     }
-    EXPECT_EQUAL(1u, transport.getReceivedCount());
+    EXPECT_EQ(1u, transport.getReceivedCount());
 }
 
-
+GTEST_MAIN_RUN_ALL_TESTS()
