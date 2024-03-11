294ab805c782752283e49b3a57ee8d0e7996deb7
Tor Egge <Tor.Egge@online.no>
2024-03-11 15:35:07 +0100

    Rewrite messagebus result unit test to gtest.

--- messagebus/src/tests/result/result.cpp
+++ messagebus/src/tests/result/result.cpp
@@ -1,12 +1,14 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/messagebus/result.h>
 #include <vespa/messagebus/error.h>
 #include <vespa/messagebus/errorcode.h>
 #include <vespa/messagebus/testlib/simplemessage.h>
+#include <vespa/vespalib/gtest/gtest.h>
 
 using namespace mbus;
+
+namespace {
 
 struct MyMessage : public SimpleMessage
 {
@@ -22,28 +24,23 @@ struct MyMessage : public SimpleMessage
 int MyMessage::ctorCnt = 0;
 int MyMessage::dtorCnt = 0;
 
-struct Test : public vespalib::TestApp
-{
-    Result sendOk(Message::UP msg);
-    Result sendFail(Message::UP msg);
-    int Main() override;
-};
-
 Result
-Test::sendOk(Message::UP msg) {
+sendOk(Message::UP msg)
+{
     (void) msg;
     return Result();
 }
 
 Result
-Test::sendFail(Message::UP msg) {
+sendFail(Message::UP msg)
+{
     return Result(Error(ErrorCode::FATAL_ERROR, "error"), std::move(msg));
 }
 
-int
-Test::Main()
+}
+
+TEST(ResultTest, test_result)
 {
-    TEST_INIT("result_test");
     { // test accepted
         Message::UP msg(new MyMessage("test"));
         Result res = sendOk(std::move(msg));
@@ -68,7 +65,6 @@ Test::Main()
     }
     EXPECT_TRUE(MyMessage::ctorCnt == 2);
     EXPECT_TRUE(MyMessage::dtorCnt == 2);
-    TEST_DONE();
 }
 
-TEST_APPHOOK(Test);
+GTEST_MAIN_RUN_ALL_TESTS()
