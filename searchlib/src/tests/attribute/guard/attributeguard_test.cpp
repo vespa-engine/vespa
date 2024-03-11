86668c0bb15727fb94a4ed5e769cad43112078ee
Tor Egge <Tor.Egge@online.no>
2024-03-11 22:56:59 +0100

    Rewrite searchlib AttributeGuard unit test to gtest.

--- searchlib/src/tests/attribute/guard/attributeguard_test.cpp
+++ searchlib/src/tests/attribute/guard/attributeguard_test.cpp
@@ -1,31 +1,17 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/log/log.h>
-LOG_SETUP("attributeguard_test");
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/searchlib/attribute/attributeguard.h>
 #include <vespa/searchlib/attribute/extendableattributes.h>
+#include <vespa/vespalib/gtest/gtest.h>
 
 namespace search {
 
-class AttributeGuardTest : public vespalib::TestApp
+TEST(AttributeGuardTest, test_attribute_guard)
 {
-public:
-    int Main() override;
-};
-
-int
-AttributeGuardTest::Main()
-{
-    TEST_INIT("attributeguard_test");
-
-
     AttributeVector::SP ssattr(new SingleStringExtAttribute("ss1"));
     AttributeGuard guard(ssattr);
     EXPECT_TRUE(guard.valid());
-
-    TEST_DONE();
 }
 
 }
 
-TEST_APPHOOK(search::AttributeGuardTest);
+GTEST_MAIN_RUN_ALL_TESTS()
