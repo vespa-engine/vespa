a5c6cbe2198bb6ad2a9c5c1b6c821d692ff0eac0
Tor Egge <Tor.Egge@online.no>
2024-04-09 11:52:26 +0200

    Turn off restrict warning when compiling with gcc 12 on ubuntu 22.04 or debian 12.
    Include cstddef to get definition of size_t.

--- searchlib/src/vespa/searchlib/expression/simple_interpolate.cpp
+++ searchlib/src/vespa/searchlib/expression/simple_interpolate.cpp
@@ -1,6 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "simple_interpolate.h"
+#include <cstddef>
 
 namespace search::expression {
 
