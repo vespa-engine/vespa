f53f584170288eb4c3698ee99a605c7255cc8d99
Arne Juul <arnej@vespa.ai>
2024-04-24 18:19:28 +0000

    add some resource attributes and more per-service labels

--- config-model/src/test/java/com/yahoo/vespa/model/admin/otel/OpenTelemetryConfigGeneratorTest.java
+++ config-model/src/test/java/com/yahoo/vespa/model/admin/otel/OpenTelemetryConfigGeneratorTest.java
@@ -2,6 +2,10 @@
 package com.yahoo.vespa.model.admin.otel;
 
 import com.yahoo.config.model.ApplicationConfigProducerRoot.StatePortInfo;
+import com.yahoo.config.model.producer.TreeConfigProducer;
+import com.yahoo.config.model.test.MockRoot;
+import com.yahoo.vespa.model.AbstractService;
+import com.yahoo.vespa.model.PortAllocBridge;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,10 +17,30 @@ public class OpenTelemetryConfigGeneratorTest {
 
     @Test
     void testBuildsYaml() {
-        var generator = new OpenTelemetryConfigGenerator(null);
-        generator.addStatePorts(List.of(new StatePortInfo("localhost", 19098, "config-sentinel", "sentinel")));
+        var generator = new OpenTelemetryConfigGenerator(null, null);
+        var root = new MockRoot();
+        var mockPort1 = new StatePortInfo("localhost", 19098,
+                                          new MockService(root, "sentinel"));
+        var mockSvc2 = new MockService(root, "searchnode");
+        mockSvc2.setProp("clustername", "mycluster");
+        mockSvc2.setProp("clustertype", "mockup");
+        var mockPort2 = new StatePortInfo("other.host.local", 19102, mockSvc2);
+        generator.addStatePorts(List.of(mockPort1, mockPort2));
         String yaml = generator.generate();
+        // System.err.println(">>>\n" + yaml + "\n<<<");
         assertTrue(yaml.contains("sentinel"));
+    }
+
+    static class MockService extends AbstractService {
+        private final String name;
+        public MockService(TreeConfigProducer<?> parent, String name) {
+            super(parent, name);
+            this.name = name;
+        }
+        public String getServiceName() { return name; }
+        public String getServiceType() { return "dummy"; }
+        @Override public int getPortCount() { return 0; }
+        @Override public void allocatePorts(int start, PortAllocBridge from) { }
     }
 
 }
