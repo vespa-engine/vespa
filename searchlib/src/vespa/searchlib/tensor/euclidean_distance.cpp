14601d4abe0fdf9f5deb9ffb05aa4da4b2a71b59
Henning Baldersheim <balder@yahoo-inc.com>
2024-05-13 13:25:44 +0000

    Use an assertless typify in innerloop

--- searchlib/src/vespa/searchlib/tensor/euclidean_distance.cpp
+++ searchlib/src/vespa/searchlib/tensor/euclidean_distance.cpp
@@ -45,7 +45,7 @@ public:
         return score;
     }
     double calc_with_limit(TypedCells rhs, double limit) const noexcept override {
-        vespalib::ConstArrayRef<AttributeCellType> rhs_vector = rhs.typify<AttributeCellType>();
+        vespalib::ConstArrayRef<AttributeCellType> rhs_vector = rhs.unsafe_typify<AttributeCellType>();
         double sum = 0.0;
         size_t sz = _lhs_vector.size();
         for (size_t i = 0; i < sz && sum <= limit; ++i) {
