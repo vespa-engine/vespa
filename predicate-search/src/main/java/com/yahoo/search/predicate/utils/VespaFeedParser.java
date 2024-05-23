207297c42d5eef339f0e875ee9cfd2248e8227c2
Harald Musum <musum@yahooinc.com>
2024-05-23 19:33:59 +0200

    Rewrite VespaFeedParser to handle Json feed files

--- predicate-search/src/main/java/com/yahoo/search/predicate/utils/VespaFeedParser.java
+++ predicate-search/src/main/java/com/yahoo/search/predicate/utils/VespaFeedParser.java
@@ -9,9 +9,10 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 /**
- * Parses a feed file containing documents in XML format. Its implementation is based on the following assumptions:
+ * Parses a feed file containing documents in JSON format. Its implementation is based on the following assumptions:
  *  1. Each document has single predicate field.
  *  2. The predicate is stored in a field named "boolean".
+ *  3. There is just one "boolean" field on each line
  *
  *  @author bjorncs
  */
@@ -20,26 +21,25 @@ public class VespaFeedParser {
     public static int parseDocuments(String feedFile, int maxDocuments, Consumer<Predicate> consumer) throws IOException {
         int documentCount = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(feedFile), 8 * 1024)) {
-            reader.readLine();
-            reader.readLine(); // Skip to start of first document
             String line = reader.readLine();
-            while (!line.startsWith("</vespafeed>") && documentCount < maxDocuments) {
-                while (!line.startsWith("<boolean>")) {
+            while (!line.startsWith("]") && documentCount < maxDocuments) {
+                while (!line.contains("\"boolean\":")) {
                     line = reader.readLine();
                 }
                 Predicate predicate = Predicate.fromString(extractBooleanExpression(line));
                 consumer.accept(predicate);
                 ++documentCount;
-                while (!line.startsWith("<document") && !line.startsWith("</vespafeed>")) {
-                    line = reader.readLine();
-                }
+                line = reader.readLine();
             }
         }
         return documentCount;
     }
 
     private static String extractBooleanExpression(String line) {
-        return line.substring(9, line.length() - 10);
+        String field = "\"boolean\":";
+        var start = line.indexOf(field);
+        var end = line.indexOf("\"", start + field.length() + 1);
+        return line.substring(start + field.length() +1 , end);
     }
 
 }
