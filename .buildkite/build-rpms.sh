9858bd36a59b10e6a326be2e8ccce564fe1c89d9
Arnstein Ressem <aressem@vespa.ai>
2024-05-31 11:54:57 +0200

    Default threads are -T0, use 4 threads instead.

--- .buildkite/build-rpms.sh
+++ .buildkite/build-rpms.sh
@@ -9,7 +9,7 @@ make  -f .copr/Makefile srpm outdir="$WORKDIR"
 rpmbuild --rebuild \
   --define="_topdir $WORKDIR/vespa-rpmbuild" \
   --define "_debugsource_template %{nil}" \
-  --define "_binary_payload w10T.zstdio" \
+  --define "_binary_payload w10T4.zstdio" \
   --define "installdir $WORKDIR/vespa-install" "$WORKDIR"/vespa-"$VESPA_VERSION"-*.src.rpm
 
 mv "$WORKDIR"/vespa-rpmbuild/RPMS/*/*.rpm "$WORKDIR/artifacts/$ARCH/rpms"
