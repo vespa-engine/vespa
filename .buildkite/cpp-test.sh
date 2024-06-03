dd9e4092f894c557299b62c873b31c30575f104b
Arnstein Ressem <aressem@vespa.ai>
2024-06-03 23:52:30 +0200

    Reduce threads.

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 set +e
-if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPP_THREADS" ; then
-  sleep 3600
+#if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPP_THREADS" ; then
+if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j 12 ; then
+  #sleep 3600
   exit 1
 fi
 
