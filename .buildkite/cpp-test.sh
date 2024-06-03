bf7ab54814a21d4abacf79e7b2a4c848a38fabf8
Arnstein Ressem <aressem@vespa.ai>
2024-06-03 23:03:56 +0200

    Sleep again.

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 set +e
 if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPP_THREADS" ; then
-  #sleep 3600
+  sleep 3600
   exit 1
 fi
 
