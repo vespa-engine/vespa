18200a82d71a281467536389cf0a0983a6ff33f5
Arnstein Ressem <aressem@vespa.ai>
2024-06-04 13:31:56 +0200

    Dont run install and cpp-test together.

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -3,9 +3,5 @@
 set -euo pipefail
 
 set +e
-#if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPP_THREADS" ; then
-if ! ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --verbose -j 12 ; then
-  #sleep 3600
-  exit 1
-fi
+ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT" 
 
