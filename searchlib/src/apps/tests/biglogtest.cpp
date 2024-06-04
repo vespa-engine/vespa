82c15109e9467980a5e367c5b23323f69c2112ab
Tor Egge <Tor.Egge@online.no>
2024-06-04 11:24:05 +0200

    Rewrite searchlib biglog unit test to gtest.

--- searchlib/src/apps/tests/biglogtest.cpp
+++ searchlib/src/apps/tests/biglogtest.cpp
@@ -1,62 +1,70 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/vespalib/util/random.h>
 #include <vespa/searchlib/docstore/logdatastore.h>
 #include <vespa/searchlib/index/dummyfileheadercontext.h>
 #include <vespa/searchlib/transactionlog/nosyncproxy.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/util/size_literals.h>
 #include <vespa/vespalib/util/threadstackexecutor.h>
 #include <vespa/vespalib/data/databuffer.h>
 #include <filesystem>
+#include <memory>
 
 using namespace search;
 using search::index::DummyFileHeaderContext;
 
-class Test : public vespalib::TestApp {
+class BigLogTest : public ::testing::Test {
 private:
     struct Blob {
         ssize_t sz;
-        char *buf;
-        Blob(size_t s) : sz(s), buf(s == 0 ? 0 : new char[s]) {}
+        std::unique_ptr<char[]> buf;
+        Blob(size_t s) : sz(s), buf(s == 0 ? nullptr : new char[s]) {}
     };
     using Map = std::map<uint32_t, uint32_t>;
 
-    void makeBlobs();
-    void cleanBlobs();
+    static void makeBlobs();
+    static void cleanBlobs();
     void checkBlobs(const IDataStore &datastore, const Map &lidToBlobMap);
 
+    std::string _dir;
+    static std::vector<Blob> _blobs;
+    static vespalib::RandomGen _randomgenerator;
+
+protected:
     template <typename DS>
     void testDIO();
 
-    std::string _dir;
-    std::vector<Blob> _blobs;
-    vespalib::RandomGen _randomgenerator;
-
 public:
-    int Main() override {
-        TEST_INIT("big_logdatastore_test");
-
-        if (_argc > 0) {
-            DummyFileHeaderContext::setCreator(_argv[0]);
-        }
-        makeBlobs();
-
-        _dir = "logged";
-        TEST_DO(testDIO<LogDataStore>());
-
-        cleanBlobs();
-
-        TEST_DONE();
-    }
-
-    Test() : _dir(""), _blobs(), _randomgenerator(42) {}
-    ~Test() {}
+    BigLogTest();
+    ~BigLogTest();
+    static void SetUpTestSuite();
+    static void TearDownTestSuite();
 };
 
-TEST_APPHOOK(Test);
+std::vector<BigLogTest::Blob> BigLogTest::_blobs;
+vespalib::RandomGen BigLogTest::_randomgenerator(42);
+
+BigLogTest::BigLogTest()
+    : _dir("logged")
+{
+}
+
+BigLogTest::~BigLogTest() = default;
 
 void
-Test::makeBlobs()
+BigLogTest::SetUpTestSuite()
+{
+    makeBlobs();
+}
+
+void
+BigLogTest::TearDownTestSuite()
+{
+    cleanBlobs();
+}
+
+void
+BigLogTest::makeBlobs()
 {
     _randomgenerator.setSeed(42);
     _blobs.push_back(Blob(0));
@@ -66,7 +74,7 @@ Test::makeBlobs()
         size_t blobsize = _randomgenerator.nextUint32() % (1<<sizeclass);
         if (blobsize > usemem) blobsize = usemem;
         _blobs.push_back(Blob(blobsize));
-        char *p = _blobs.back().buf;
+        char *p = _blobs.back().buf.get();
         for (size_t j=0; j < blobsize; ++j) {
             *p++ = _randomgenerator.nextUint32();
         }
@@ -75,22 +83,19 @@ Test::makeBlobs()
 }
 
 void
-Test::cleanBlobs()
+BigLogTest::cleanBlobs()
 {
     printf("count %lu blobs sizes:", _blobs.size());
     while (_blobs.size() > 0) {
-        char *p = _blobs.back().buf;
         printf(" %lu", _blobs.back().sz);
-        delete[] p;
         _blobs.pop_back();
     }
     printf("\n");
 }
 
-
 void
-Test::checkBlobs(const IDataStore &datastore,
-                 const Map &lidToBlobMap)
+BigLogTest::checkBlobs(const IDataStore &datastore,
+                       const Map &lidToBlobMap)
 {
     for (Map::const_iterator it = lidToBlobMap.begin();
          it != lidToBlobMap.end();
@@ -99,8 +104,8 @@ Test::checkBlobs(const IDataStore &datastore,
         uint32_t lid = it->first;
         uint32_t bno = it->second;
         vespalib::DataBuffer got;
-        EXPECT_EQUAL(datastore.read(lid, got), _blobs[bno].sz);
-        EXPECT_TRUE(memcmp(got.getData(), _blobs[bno].buf, _blobs[bno].sz) == 0);
+        EXPECT_EQ(_blobs[bno].sz, datastore.read(lid, got));
+        EXPECT_EQ(0, memcmp(got.getData(), _blobs[bno].buf.get(), _blobs[bno].sz));
     }
 }
 
@@ -144,7 +149,7 @@ factory<LogDataStore>::~factory() {}
 
 template <typename DS>
 void
-Test::testDIO()
+BigLogTest::testDIO()
 {
     uint64_t serial = 0;
 
@@ -158,14 +163,14 @@ Test::testDIO()
         for (uint32_t lid=0; lid<15; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         uint64_t flushToken = ds().initFlush(serial);
         ds().flush(flushToken);
         for (uint32_t lid=10; lid<30; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         checkBlobs(ds(), lidToBlobMap);
         flushToken = ds().initFlush(serial);
@@ -179,17 +184,17 @@ Test::testDIO()
         for (uint32_t lid=3; lid<8; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         for (uint32_t lid=23; lid<28; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         for (uint32_t lid=100033; lid<100088; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         checkBlobs(ds(), lidToBlobMap);
 
@@ -226,7 +231,7 @@ Test::testDIO()
         for (uint32_t lid=1234567; lid < 1234999; ++lid) {
             uint32_t blobno = _randomgenerator.nextUint32() % _blobs.size();
             lidToBlobMap[lid] = blobno;
-            ds().write(++serial, lid, _blobs[blobno].buf, _blobs[blobno].sz);
+            ds().write(++serial, lid, _blobs[blobno].buf.get(), _blobs[blobno].sz);
         }
         checkBlobs(ds(), lidToBlobMap);
         uint64_t flushToken = ds().initFlush(22);
@@ -238,5 +243,19 @@ Test::testDIO()
         checkBlobs(ds(), lidToBlobMap);
     }
     std::filesystem::remove_all(std::filesystem::path(_dir));
-    TEST_FLUSH();
+}
+
+TEST_F(BigLogTest, logdatastore_dio)
+{
+    testDIO<LogDataStore>();
+}
+
+int
+main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    if (argc > 0) {
+        DummyFileHeaderContext::setCreator(argv[0]);
+    }
+    return RUN_ALL_TESTS();
 }
