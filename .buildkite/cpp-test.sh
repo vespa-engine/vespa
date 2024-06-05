aaed1910925ad3d21fb398be95cd803f81b3b669
Arnstein Ressem <aressem@vespa.ai>
2024-06-05 10:50:33 +0200

    Try to run ctests with nice and use valgrind fair scheduling.

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
+nice ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
