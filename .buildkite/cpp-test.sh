25ced7681ea7f02a21d4e4dfae004ae133a657f8
Arnstein Ressem <aressem@vespa.ai>
2024-06-05 11:30:39 +0200

    C++ tests fails under nice.

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-nice ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
+ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
