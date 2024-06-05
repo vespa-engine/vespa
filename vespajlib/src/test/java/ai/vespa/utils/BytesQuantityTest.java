a24481e565841f852f742956baab5ce139c5f212
Bjørn Christian Seime <bjorncs@vespa.ai>
2024-06-05 17:05:37 +0200

    Add `BytesQuantity`

---
+++ vespajlib/src/test/java/ai/vespa/utils/BytesQuantityTest.java
@@ -0,0 +1,42 @@
+package ai.vespa.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author bjorncs
+ */
+class BytesQuantityTest {
+
+    @Test
+    void from_string() {
+        assertEquals(1L, BytesQuantity.fromString("1 bytes").toBytes());
+        assertEquals(1L, BytesQuantity.fromString("1 B").toBytes());
+        assertEquals(1L, BytesQuantity.fromString("1B").toBytes());
+        assertEquals(1L, BytesQuantity.fromString("1").toBytes());
+        assertEquals(1024L, BytesQuantity.fromString("1kB").toBytes());
+        assertEquals(1024L, BytesQuantity.fromString("1k").toBytes());
+        assertEquals(1024L, BytesQuantity.fromString("1K").toBytes());
+        assertEquals(1024L, BytesQuantity.fromString("1KB").toBytes());
+        assertEquals(1024L * 1024, BytesQuantity.fromString("1MB").toBytes());
+        assertEquals(1024L * 1024, BytesQuantity.fromString("1M").toBytes());
+        assertEquals(1024L * 1024 * 1024, BytesQuantity.fromString("1GB").toBytes());
+        assertEquals(1024L * 1024 * 1024, BytesQuantity.fromString("1G").toBytes());
+        assertEquals(1024L * 1024 * 1024 * 1024, BytesQuantity.fromString("1TB").toBytes());
+        assertEquals(1024L * 1024 * 1024 * 1024, BytesQuantity.fromString("1T").toBytes());
+    }
+
+    @Test
+    void as_pretty_string() {
+        assertEquals("1 bytes", BytesQuantity.ofBytes(1).asPrettyString());
+        assertEquals("2 kB", BytesQuantity.ofKB(2).asPrettyString());
+        assertEquals("3 MB", BytesQuantity.ofMB(3).asPrettyString());
+        assertEquals("4 GB", BytesQuantity.ofGB(4).asPrettyString());
+        assertEquals("5 TB", BytesQuantity.ofTB(5).asPrettyString());
+
+        assertEquals("2560 bytes", BytesQuantity.ofBytes(2048 + 512).asPrettyString());
+        assertEquals("3073 kB", BytesQuantity.ofBytes(3 * 1024 * 1024 + 1024).asPrettyString());
+        assertEquals("5120 TB", BytesQuantity.ofBytes(1024L * 1024 * 1024 * 1024 * 1024 * 5).asPrettyString());
+    }
+}
