79529f9ee5edebe1a532d041ec18a92e287d81b3
Tor Egge <Tor.Egge@online.no>
2024-06-10 12:04:32 +0200

    Rewrite leaf blueprints unit test to gtest.

--- searchlib/src/tests/queryeval/blueprint/leaf_blueprints_test.cpp
+++ searchlib/src/tests/queryeval/blueprint/leaf_blueprints_test.cpp
@@ -1,33 +1,20 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/vespalib/testkit/testapp.h>
 #include <vespa/searchlib/queryeval/blueprint.h>
 #include <vespa/searchlib/queryeval/leaf_blueprints.h>
 #include <vespa/searchlib/fef/matchdata.h>
-
-#include <vespa/log/log.h>
-LOG_SETUP("blueprint_test");
+#include <vespa/vespalib/gtest/gtest.h>
 
 using namespace search::queryeval;
 using namespace search::fef;
 
-class Test : public vespalib::TestApp
-{
-public:
-    void testEmptyBlueprint();
-    void testSimpleBlueprint();
-    void testFakeBlueprint();
-    int Main() override;
-};
-
-void
-Test::testEmptyBlueprint()
+TEST(LeafBlueprintsTest, empty_blueprint)
 {
     MatchData::UP md(MatchData::makeTestInstance(100, 10));
     EmptyBlueprint empty(FieldSpecBase(1, 11));
     ASSERT_TRUE(empty.getState().numFields() == 1u);
-    EXPECT_EQUAL(1u, empty.getState().field(0).getFieldId());
-    EXPECT_EQUAL(11u, empty.getState().field(0).getHandle());
+    EXPECT_EQ(1u, empty.getState().field(0).getFieldId());
+    EXPECT_EQ(11u, empty.getState().field(0).getHandle());
 
     empty.basic_plan(true, 100);
     empty.fetchPostings(ExecuteInfo::FULL);
@@ -36,18 +23,17 @@ Test::testEmptyBlueprint()
     SimpleResult res;
     res.search(*search);
     SimpleResult expect; // empty
-    EXPECT_EQUAL(res, expect);
+    EXPECT_EQ(res, expect);
 }
 
-void
-Test::testSimpleBlueprint()
+TEST(LeafBlueprintsTest, simple_blueprint)
 {
     MatchData::UP md(MatchData::makeTestInstance(100, 10));
     SimpleResult a;
     a.addHit(3).addHit(5).addHit(7);
     SimpleBlueprint simple(a);
     simple.tag("tag");
-    EXPECT_EQUAL("tag", simple.tag());
+    EXPECT_EQ("tag", simple.tag());
     simple.basic_plan(true, 100);
     simple.fetchPostings(ExecuteInfo::FULL);
     SearchIterator::UP search = simple.createSearch(*md);
@@ -56,11 +42,10 @@ Test::testSimpleBlueprint()
     res.search(*search);
     SimpleResult expect;
     expect.addHit(3).addHit(5).addHit(7);
-    EXPECT_EQUAL(res, expect);
+    EXPECT_EQ(res, expect);
 }
 
-void
-Test::testFakeBlueprint()
+TEST(LeafBlueprintsTest, fake_blueprint)
 {
     MatchData::UP md(MatchData::makeTestInstance(100, 10));
     FakeResult fake;
@@ -76,36 +61,36 @@ Test::testFakeBlueprint()
     SearchIterator::UP search = orig.createSearch(*md);
     search->initFullRange();
     EXPECT_TRUE(!search->seek(1u));
-    EXPECT_EQUAL(10u, search->getDocId());
+    EXPECT_EQ(10u, search->getDocId());
     {
         search->unpack(10u);
         TermFieldMatchData &data = *md->resolveTermField(handle);
-        EXPECT_EQUAL(fieldId, data.getFieldId());
-        EXPECT_EQUAL(10u, data.getDocId());
-        EXPECT_EQUAL(10u, data.getDocId());
+        EXPECT_EQ(fieldId, data.getFieldId());
+        EXPECT_EQ(10u, data.getDocId());
+        EXPECT_EQ(10u, data.getDocId());
         FieldPositionsIterator itr = data.getIterator();
-        EXPECT_EQUAL(50u, itr.getFieldLength());
-        EXPECT_EQUAL(2u, itr.size());
+        EXPECT_EQ(50u, itr.getFieldLength());
+        EXPECT_EQ(2u, itr.size());
         ASSERT_TRUE(itr.valid());
-        EXPECT_EQUAL(2u, itr.getPosition());
+        EXPECT_EQ(2u, itr.getPosition());
         itr.next();
         ASSERT_TRUE(itr.valid());
-        EXPECT_EQUAL(3u, itr.getPosition());
+        EXPECT_EQ(3u, itr.getPosition());
         itr.next();
         EXPECT_TRUE(!itr.valid());
     }
     EXPECT_TRUE(search->seek(25));
-    EXPECT_EQUAL(25u, search->getDocId());
+    EXPECT_EQ(25u, search->getDocId());
     {
         search->unpack(25u);
         TermFieldMatchData &data = *md->resolveTermField(handle);
-        EXPECT_EQUAL(fieldId, data.getFieldId());
-        EXPECT_EQUAL(25u, data.getDocId());
+        EXPECT_EQ(fieldId, data.getFieldId());
+        EXPECT_EQ(25u, data.getDocId());
         FieldPositionsIterator itr = data.getIterator();
-        EXPECT_EQUAL(10u, itr.getFieldLength());
-        EXPECT_EQUAL(1u, itr.size());
+        EXPECT_EQ(10u, itr.getFieldLength());
+        EXPECT_EQ(1u, itr.size());
         ASSERT_TRUE(itr.valid());
-        EXPECT_EQUAL(5u, itr.getPosition());
+        EXPECT_EQ(5u, itr.getPosition());
         itr.next();
         EXPECT_TRUE(!itr.valid());
     }
@@ -113,14 +98,4 @@ Test::testFakeBlueprint()
     EXPECT_TRUE(search->isAtEnd());
 }
 
-int
-Test::Main()
-{
-    TEST_INIT("leaf_blueprints_test");
-    testEmptyBlueprint();
-    testSimpleBlueprint();
-    testFakeBlueprint();
-    TEST_DONE();
-}
-
-TEST_APPHOOK(Test);
+GTEST_MAIN_RUN_ALL_TESTS()
