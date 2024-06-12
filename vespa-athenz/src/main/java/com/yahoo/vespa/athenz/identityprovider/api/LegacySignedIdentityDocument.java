29b8827c9979f498c10cc51e62919750498be068
Morten Tokle <mortent@vespa.ai>
2024-06-12 15:34:08 +0200

    Revert "Revert "Remove legacy document version support""

--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/api/LegacySignedIdentityDocument.java
+++
@@ -1,6 +0,0 @@
-// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.vespa.athenz.identityprovider.api;
-
-public record LegacySignedIdentityDocument(String signature, int signingKeyVersion, int documentVersion,
-                                           IdentityDocument identityDocument) implements SignedIdentityDocument {
-}
--- vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/client/IdentityDocumentSigner.java
+++ vespa-athenz/src/main/java/com/yahoo/vespa/athenz/identityprovider/client/IdentityDocumentSigner.java
@@ -3,11 +3,9 @@ package com.yahoo.vespa.athenz.identityprovider.client;
 
 import com.yahoo.security.SignatureUtils;
 import com.yahoo.vespa.athenz.api.AthenzIdentity;
-import com.yahoo.vespa.athenz.api.AthenzService;
 import com.yahoo.vespa.athenz.identityprovider.api.DefaultSignedIdentityDocument;
 import com.yahoo.vespa.athenz.identityprovider.api.IdentityDocument;
 import com.yahoo.vespa.athenz.identityprovider.api.IdentityType;
-import com.yahoo.vespa.athenz.identityprovider.api.LegacySignedIdentityDocument;
 import com.yahoo.vespa.athenz.identityprovider.api.SignedIdentityDocument;
 import com.yahoo.vespa.athenz.identityprovider.api.VespaUniqueInstanceId;
 
@@ -22,7 +20,6 @@ import java.util.Base64;
 import java.util.Set;
 import java.util.TreeSet;
 
-import static com.yahoo.vespa.athenz.identityprovider.api.SignedIdentityDocument.LEGACY_DEFAULT_DOCUMENT_VERSION;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
@@ -44,39 +41,8 @@ public class IdentityDocumentSigner {
         }
     }
 
-    public String generateLegacySignature(IdentityDocument doc, PrivateKey privateKey) {
-        return generateSignature(doc.providerUniqueId(), doc.providerService(), doc.configServerHostname(),
-                                 doc.instanceHostname(), doc.createdAt(), doc.ipAddresses(), doc.identityType(), privateKey, doc.serviceIdentity());
-    }
-
-    // Cluster type is ignored due to old Vespa versions not forwarding unknown fields in signed identity document
-    private String generateSignature(VespaUniqueInstanceId providerUniqueId,
-                                    AthenzIdentity providerService,
-                                    String configServerHostname,
-                                    String instanceHostname,
-                                    Instant createdAt,
-                                    Set<String> ipAddresses,
-                                    IdentityType identityType,
-                                    PrivateKey privateKey,
-                                    AthenzIdentity serviceIdentity) {
-        try {
-            Signature signer = SignatureUtils.createSigner(privateKey);
-            signer.initSign(privateKey);
-            writeToSigner(
-                    signer, providerUniqueId, providerService, configServerHostname, instanceHostname, createdAt,
-                    ipAddresses, identityType);
-            writeToSigner(signer, serviceIdentity);
-            byte[] signature = signer.sign();
-            return Base64.getEncoder().encodeToString(signature);
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public boolean hasValidSignature(SignedIdentityDocument doc, PublicKey publicKey) {
-        if (doc instanceof LegacySignedIdentityDocument signedDoc) {
-            return validateLegacySignature(signedDoc, publicKey);
-        } else if (doc instanceof DefaultSignedIdentityDocument signedDoc) {
+        if (doc instanceof DefaultSignedIdentityDocument signedDoc) {
             try {
                 Signature signer = SignatureUtils.createVerifier(publicKey);
                 signer.initVerify(publicKey);
@@ -88,47 +54,5 @@ public class IdentityDocumentSigner {
         } else {
             throw new IllegalArgumentException("Unknown identity document type: " + doc.getClass().getName());
         }
-    }
-
-    private boolean validateLegacySignature(SignedIdentityDocument doc, PublicKey publicKey) {
-        try {
-            IdentityDocument iddoc = doc.identityDocument();
-            Signature signer = SignatureUtils.createVerifier(publicKey);
-            signer.initVerify(publicKey);
-            writeToSigner(
-                    signer, iddoc.providerUniqueId(), iddoc.providerService(), iddoc.configServerHostname(),
-                    iddoc.instanceHostname(), iddoc.createdAt(), iddoc.ipAddresses(), iddoc.identityType());
-            if (doc.documentVersion() >= LEGACY_DEFAULT_DOCUMENT_VERSION) {
-                writeToSigner(signer, iddoc.serviceIdentity());
-            }
-            return signer.verify(Base64.getDecoder().decode(doc.signature()));
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void writeToSigner(Signature signer,
-                                      VespaUniqueInstanceId providerUniqueId,
-                                      AthenzIdentity providerService,
-                                      String configServerHostname,
-                                      String instanceHostname,
-                                      Instant createdAt,
-                                      Set<String> ipAddresses,
-                                      IdentityType identityType) throws SignatureException {
-        signer.update(providerUniqueId.asDottedString().getBytes(UTF_8));
-        signer.update(providerService.getFullName().getBytes(UTF_8));
-        signer.update(configServerHostname.getBytes(UTF_8));
-        signer.update(instanceHostname.getBytes(UTF_8));
-        ByteBuffer timestampAsBuffer = ByteBuffer.allocate(Long.BYTES);
-        timestampAsBuffer.putLong(createdAt.toEpochMilli());
-        signer.update(timestampAsBuffer.array());
-        for (String ipAddress : new TreeSet<>(ipAddresses)) {
-            signer.update(ipAddress.getBytes(UTF_8));
-        }
-        signer.update(identityType.id().getBytes(UTF_8));
-    }
-
-    private static void writeToSigner(Signature signer, AthenzIdentity serviceIdentity) throws SignatureException{
-        signer.update(serviceIdentity.getFullName().getBytes(UTF_8));
     }
 }
