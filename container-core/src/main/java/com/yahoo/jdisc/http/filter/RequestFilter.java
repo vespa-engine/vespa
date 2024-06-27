3af2492205e24c5917d41f9afa91e3ae28980239
jonmv <venstad@gmail.com>
2024-06-27 13:08:05 +0200

    Move dep-annotations reading to interface, which filters now inherit from

--- container-core/src/main/java/com/yahoo/jdisc/http/filter/RequestFilter.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/filter/RequestFilter.java
@@ -1,13 +1,14 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.filter;
 
+import com.yahoo.component.chain.model.Chainable;
 import com.yahoo.jdisc.handler.ResponseHandler;
 import com.yahoo.jdisc.http.HttpRequest;
 
 /**
  * @author Einar M R Rosenvinge
  */
-public interface RequestFilter extends com.yahoo.jdisc.SharedResource, RequestFilterBase {
+public interface RequestFilter extends com.yahoo.jdisc.SharedResource, RequestFilterBase, Chainable {
 
     void filter(HttpRequest request, ResponseHandler handler);
 
--- container-core/src/main/java/com/yahoo/jdisc/http/filter/ResponseFilter.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/filter/ResponseFilter.java
@@ -1,14 +1,15 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.filter;
 
+import com.yahoo.component.chain.model.Chainable;
 import com.yahoo.jdisc.Request;
 import com.yahoo.jdisc.Response;
 import com.yahoo.jdisc.SharedResource;
 
 /**
- * @author <a href="mailto:einarmr@yahoo-inc.com">Einar M R Rosenvinge</a>
+ * @author Einar M R Rosenvinge
  */
-public interface ResponseFilter extends SharedResource, ResponseFilterBase {
+public interface ResponseFilter extends SharedResource, ResponseFilterBase, Chainable {
 
     public void filter(Response response, Request request);
 }
