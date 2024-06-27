257c396d933113604b9111d1a04cbd2d7d17c01f
jonmv <venstad@gmail.com>
2024-06-27 16:17:16 +0200

    Move inheritance up

--- container-core/src/main/java/com/yahoo/jdisc/http/filter/RequestFilter.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/filter/RequestFilter.java
@@ -1,14 +1,13 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.filter;
 
-import com.yahoo.component.chain.model.Chainable;
 import com.yahoo.jdisc.handler.ResponseHandler;
 import com.yahoo.jdisc.http.HttpRequest;
 
 /**
  * @author Einar M R Rosenvinge
  */
-public interface RequestFilter extends com.yahoo.jdisc.SharedResource, RequestFilterBase, Chainable {
+public interface RequestFilter extends com.yahoo.jdisc.SharedResource, RequestFilterBase {
 
     void filter(HttpRequest request, ResponseHandler handler);
 
--- container-core/src/main/java/com/yahoo/jdisc/http/filter/ResponseFilter.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/filter/ResponseFilter.java
@@ -1,7 +1,6 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.filter;
 
-import com.yahoo.component.chain.model.Chainable;
 import com.yahoo.jdisc.Request;
 import com.yahoo.jdisc.Response;
 import com.yahoo.jdisc.SharedResource;
@@ -9,7 +8,7 @@ import com.yahoo.jdisc.SharedResource;
 /**
  * @author Einar M R Rosenvinge
  */
-public interface ResponseFilter extends SharedResource, ResponseFilterBase, Chainable {
+public interface ResponseFilter extends SharedResource, ResponseFilterBase {
 
     public void filter(Response response, Request request);
 }
