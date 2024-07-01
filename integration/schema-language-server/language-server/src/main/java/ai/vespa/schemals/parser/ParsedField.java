6fbf1731debd39a1e278d37dbf68d7eea16eadce
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-07-01 11:32:40 +0200

    feat: Add id as property to field in grammar

--- integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/parser/ParsedField.java
+++ integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/parser/ParsedField.java
@@ -4,10 +4,17 @@ package ai.vespa.schemals.parser;
 public class ParsedField extends ParsedBlock {
 
     private ParsedType type;
+    private int overrideId = 0;
 
     ParsedField(String name, ParsedType type) {
         super(name, "field");
         this.type = type;
     }
+
+    ParsedType getType() { return this.type; }
+    boolean hasIdOverride() { return overrideId != 0; }
+    int idOverride() { return overrideId; }
+
+    void setId(int id) { this.overrideId = id; }
 
 }
