152f7e29124203abae9fc0e4cd452911f03676ff
Magnus Eide-Fredriksen <magnuseifr@vespa.ai>
2024-07-01 16:22:46 +0200

    feat: completion snippets

--- integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/completion/SchemaCompletion.java
+++ integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/completion/SchemaCompletion.java
@@ -1,9 +1,13 @@
 package ai.vespa.schemals.completion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 
 import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionItemKind;
+import org.eclipse.lsp4j.InsertTextFormat;
 import org.eclipse.lsp4j.Position;
 
 import ai.vespa.schemals.context.EventPositionContext;
@@ -13,48 +17,70 @@ import ai.vespa.schemals.tree.CSTUtils;
 
 public class SchemaCompletion {
 
+    private static CompletionItem constructSnippet(String label, String snippet, String detail) {
+        CompletionItem item = new CompletionItem();
+        item.setLabel(label);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+        item.setInsertText(snippet);
+        item.setDetail(detail);
+        return item;
+    }
+
+    private static CompletionItem constructSnippet(String label, String snippet) {
+        return constructSnippet(label, snippet, "");
+    }
+
     // TODO: make compatible with parser
-    private static HashMap<EventPositionContext.EnclosingBody, String[]> validKeywordsInBody = new HashMap<>() {{
-        put(EventPositionContext.EnclosingBody.SCHEMA, new String[]{
-            "document",
-            "index",
-            "field",
-            "fieldset",
-            "rank-profile",
-            "constant",
-            "onnx-model",
-            "stemming",
-            "document-summary",
-            "annotation",
-            "import",
-            "raw-as-base64-in-summary"
+    private static HashMap<EventPositionContext.EnclosingBody, CompletionItem[]> bodyKeywordSnippets = new HashMap<>() {{
+        put(EventPositionContext.EnclosingBody.SCHEMA, new CompletionItem[]{
+            constructSnippet("document", "document ${1:name} {\n\t$0\n}"), // TODO: figure out client tab format
+            constructSnippet("index", "index ${1:index-name}: ${2:property}", "index:"),
+            constructSnippet("index", "index ${1:index-name} {\n\t$0\n}", "index {}"),
+            constructSnippet("field", "field ${1:name} type $2 {\n\t$0\n}"),
+            constructSnippet("fieldset", "fieldset ${1:default} {\n\tfields: $0\n}"),
+            constructSnippet("rank-profile", "rank-profile ${1:name} {\n\t$0\n}"),
+            constructSnippet("constant", "constant ${1:name} {\n\t$0\n}"),
+            constructSnippet("onnx-model", "onnx-model ${1:name} {\n\t$0\n}"),
+            constructSnippet("stemming", "stemming: $1"),
+            constructSnippet("document-summary", "document-summary ${1:name} {\n\t$0\n}"),
+            constructSnippet("annotation", "annotation ${1:name} {\n\t$0\n}"),
+            constructSnippet("import field", "import field ${1:name} as $2 {}"),
+            constructSnippet("raw-as-base64-in-summary", "raw-as-base64-in-summary")
         });
 
-        put(EventPositionContext.EnclosingBody.DOCUMENT, new String[]{
-            "struct",
-            "field",
-            "compression"
+        put(EventPositionContext.EnclosingBody.DOCUMENT, new CompletionItem[]{
+            constructSnippet("struct", "struct ${1:name} {\n\t$0\n}"),
+            constructSnippet("field", "field ${1:name} type $2 {\n\t$0\n}"),
+            constructSnippet("compression", "compression {\n\t$0\n}"),
+
         });
 
-        put(EventPositionContext.EnclosingBody.FIELD, new String[]{
-            "alias",
-            "attribute",
-            "bolding",
-            "dictionary",
-            "id",
-            "index",
-            "indexing",
-            "match",
-            "normalizing",
-            "query-command",
-            "rank",
-            "rank-type",
-            "sorting",
-            "stemming",
-            "struct-field",
-            "summary",
-            "summary-to",
-            "weight"
+        put(EventPositionContext.EnclosingBody.FIELD, new CompletionItem[]{
+            constructSnippet("alias", "alias: $1"),
+            constructSnippet("attribute", "attribute ${1:name}: ${2:property}", "attribute:"),
+            constructSnippet("attribute", "attribute ${1:name} {\n\t$0\n}", "attribute {}"),
+            constructSnippet("bolding", "bolding: on"),
+            constructSnippet("dictionary", "dictionary {\n\t$0\n}"),
+            constructSnippet("id", "id: "),
+            constructSnippet("index", "index ${1:index-name}: ${2:property}", "index:"),
+            constructSnippet("index", "index ${1:index-name} {\n\t$0\n}", "index {}"),
+            constructSnippet("indexing", "indexing: ", "indexing:"),
+            constructSnippet("indexing", "indexing {\n\t$0\n}", "indexing {}"),
+            constructSnippet("match", "match: ", "match:"),
+            constructSnippet("match", "match {\n\t$0\n}", "match {}"),
+            constructSnippet("normalizing", "normalizing: "),
+            constructSnippet("query-command", "query-command: "),
+            constructSnippet("rank", "rank {\n\t$0\n}"),
+            constructSnippet("rank-type", "rank-type: "),
+            constructSnippet("sorting", "sorting: ", "sorting:"),
+            constructSnippet("sorting", "sorting {\n\t$0\n}", "sorting {}"),
+            constructSnippet("stemming", "stemming: "),
+            constructSnippet("struct-field", "struct-field ${1:name} {\n\t$0\n}"),
+            constructSnippet("summary", "summary: ", "summary:"),
+            constructSnippet("summary", "summary {\n\t$0\n}", "summary {}"),
+            // summary-to is deprecated
+            constructSnippet("weight", "weight: ")
         });
     }};
 
@@ -63,12 +89,8 @@ public class SchemaCompletion {
         
         EventPositionContext.EnclosingBody enclosingBody = context.findEnclosingBody();
 
-        context.logger.println(enclosingBody.toString());
-
-        if (validKeywordsInBody.containsKey(enclosingBody)) {
-            for (String str : validKeywordsInBody.get(enclosingBody)) {
-                ret.add(new CompletionItem(str));
-            }
+        if (bodyKeywordSnippets.containsKey(enclosingBody)) {
+            ret.addAll(Arrays.asList(bodyKeywordSnippets.get(enclosingBody)));
         }
 
         return ret;
