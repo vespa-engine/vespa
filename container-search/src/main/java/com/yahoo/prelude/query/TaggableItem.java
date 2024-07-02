c700babbe23608e359e4ee660cf76e9282c7f775
Tor Egge <Tor.Egge@online.no>
2024-07-02 16:02:29 +0200

    Add Beta annotation for DocumentFrequency record.

--- container-search/src/main/java/com/yahoo/prelude/query/TaggableItem.java
+++ container-search/src/main/java/com/yahoo/prelude/query/TaggableItem.java
@@ -1,5 +1,6 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.prelude.query;
+import com.yahoo.api.annotations.Beta;
 
 import java.util.Optional;
 
@@ -46,6 +47,9 @@ public interface TaggableItem {
     void setExplicitSignificance(boolean significance);
     double getSignificance();
 
+    @Beta
     void setDocumentFrequency(DocumentFrequency documentFrequency);
+
+    @Beta
     Optional<DocumentFrequency> getDocumentFrequency();
 }
