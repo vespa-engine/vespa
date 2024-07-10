a73330a9d23b9ca87b0417453d629fa200af01ef
Lester Solbakken <lester.solbakken@gmail.com>
2024-07-10 11:47:14 +0200

    Return overflowing tokens to avoid memory leak in HF tokenizer

--- linguistics-components/src/main/java/com/yahoo/language/huggingface/HuggingFaceTokenizer.java
+++ linguistics-components/src/main/java/com/yahoo/language/huggingface/HuggingFaceTokenizer.java
@@ -65,6 +65,7 @@ public class HuggingFaceTokenizer extends AbstractComponent implements Embedder,
 
                                var hfb = ai.djl.huggingface.tokenizers.HuggingFaceTokenizer.builder()
                                        .optTokenizerPath(tokenizerDir)
+                                       .optWithOverflowingTokens(true)
                                        .optAddSpecialTokens(b.addSpecialTokens != null ? b.addSpecialTokens : true);
                                if (b.maxLength != null) {
                                    hfb.optMaxLength(b.maxLength);
