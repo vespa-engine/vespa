59b5472e3d42e3577bbcc4ed9b440bc9a561a125
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-07-15 09:34:47 +0200

    chore: Add general isASTInstance in the SchemaNode

--- integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/tree/SchemaNode.java
+++ integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/tree/SchemaNode.java
@@ -267,6 +267,14 @@ public class SchemaNode implements Iterable<SchemaNode> {
         simulatedSchimaClass = astClass;
     }
 
+    public boolean isASTInstance(Class<?> astClass) {
+        if (language == LanguageType.CUSTOM && astClass.isInstance(simulatedSchimaClass)) return true;
+        if (language == LanguageType.SCHEMA) return astClass.isInstance(originalSchemaNode);
+        if (language == LanguageType.RANK_EXPRESSION) return astClass.isInstance(originalRankExpressionNode);
+        if (language == LanguageType.INDEXING) return astClass.isInstance(originalIndexingNode);
+        return false;
+    }
+
     public boolean isSchemaASTInstance(Class<? extends Node> astClass) {
         if (language == LanguageType.CUSTOM) return astClass.equals(simulatedSchimaClass);
         
