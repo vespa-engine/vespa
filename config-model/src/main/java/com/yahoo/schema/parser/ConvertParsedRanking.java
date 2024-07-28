c65b6c7a8985fdf4807b0a1bed725b8418ff2d7a
Jon Bratseth <bratseth@vespa.ai>
2024-07-28 18:24:10 +0200

    Improve error messages

--- config-model/src/main/java/com/yahoo/schema/parser/ConvertParsedRanking.java
+++ config-model/src/main/java/com/yahoo/schema/parser/ConvertParsedRanking.java
@@ -34,66 +34,74 @@ public class ConvertParsedRanking {
     }
 
     void convertRankProfile(Schema schema, ParsedRankProfile parsed) {
-        RankProfile profile = makeRankProfile(schema, parsed.name());
-        parsed.getInherited().forEach(profile::inherit);
-        parsed.isStrict().ifPresent(profile::setStrict);
-        parsed.isUseSignificanceModel().ifPresent(profile::setUseSignificanceModel);
-        parsed.getConstants().values().forEach(profile::add); ;
-        parsed.getOnnxModels().forEach(profile::add);
-        parsed.getInputs().forEach(profile::addInput);
+        try {
+            RankProfile profile = makeRankProfile(schema, parsed.name());
+            parsed.getInherited().forEach(profile::inherit);
+            parsed.isStrict().ifPresent(profile::setStrict);
+            parsed.isUseSignificanceModel().ifPresent(profile::setUseSignificanceModel);
+            parsed.getConstants().values().forEach(profile::add);
+            ;
+            parsed.getOnnxModels().forEach(profile::add);
+            parsed.getInputs().forEach(profile::addInput);
 
-        for (var func : parsed.getFunctions()) {
-            String name = func.name();
-            List<String> parameters = func.getParameters();
-            String expression = func.getExpression();
-            boolean inline = func.getInline();
-            profile.addFunction(name, parameters, expression, inline);
+            for (var func : parsed.getFunctions()) {
+                String name = func.name();
+                List<String> parameters = func.getParameters();
+                String expression = func.getExpression();
+                boolean inline = func.getInline();
+                profile.addFunction(name, parameters, expression, inline);
+            }
+
+            parsed.getRankScoreDropLimit().ifPresent(profile::setRankScoreDropLimit);
+            parsed.getSecondPhaseRankScoreDropLimit().ifPresent(profile::setSecondPhaseRankScoreDropLimit);
+            parsed.getTermwiseLimit().ifPresent(profile::setTermwiseLimit);
+            parsed.getPostFilterThreshold().ifPresent(profile::setPostFilterThreshold);
+            parsed.getApproximateThreshold().ifPresent(profile::setApproximateThreshold);
+            parsed.getTargetHitsMaxAdjustmentFactor().ifPresent(profile::setTargetHitsMaxAdjustmentFactor);
+            parsed.getKeepRankCount().ifPresent(profile::setKeepRankCount);
+            parsed.getMinHitsPerThread().ifPresent(profile::setMinHitsPerThread);
+            parsed.getNumSearchPartitions().ifPresent(profile::setNumSearchPartitions);
+            parsed.getNumThreadsPerSearch().ifPresent(profile::setNumThreadsPerSearch);
+            parsed.getReRankCount().ifPresent(profile::setRerankCount);
+
+            parsed.getMatchPhase().ifPresent(profile::setMatchPhase);
+            parsed.getDiversity().ifPresent(profile::setDiversity);
+
+            parsed.getFirstPhaseExpression().ifPresent(profile::setFirstPhaseRanking);
+            parsed.getSecondPhaseExpression().ifPresent(profile::setSecondPhaseRanking);
+
+            parsed.getGlobalPhaseExpression().ifPresent(profile::setGlobalPhaseRanking);
+            parsed.getGlobalPhaseRerankCount().ifPresent(profile::setGlobalPhaseRerankCount);
+
+            parsed.getMatchFeatures().forEach(profile::addMatchFeatures);
+            parsed.getRankFeatures().forEach(profile::addRankFeatures);
+            parsed.getSummaryFeatures().forEach(profile::addSummaryFeatures);
+            parsed.getInheritedMatchFeatures().ifPresent(profile::setInheritedMatchFeatures);
+            parsed.getInheritedSummaryFeatures().ifPresent(profile::setInheritedSummaryFeatures);
+            if (parsed.getIgnoreDefaultRankFeatures())
+                profile.setIgnoreDefaultRankFeatures(true);
+
+            parsed.getMutateOperations().forEach(profile::addMutateOperation);
+            parsed.getFieldsWithRankFilter().forEach
+                                                    ((fieldName, isFilter) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.PREFERBITVECTOR, isFilter));
+
+            parsed.getFieldsWithRankWeight().forEach
+                                                    ((fieldName, weight) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.WEIGHT, weight));
+
+            parsed.getFieldsWithRankType().forEach
+                                                  ((fieldName, rankType) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.RANKTYPE, RankType.fromString(rankType)));
+
+            parsed.getRankProperties().forEach
+                                              ((key, values) -> {
+                                                  for (String value : values) profile.addRankProperty(key, value);
+                                              });
+
+            // always?
+            rankProfileRegistry.add(profile);
         }
-
-        parsed.getRankScoreDropLimit().ifPresent(profile::setRankScoreDropLimit);
-        parsed.getSecondPhaseRankScoreDropLimit().ifPresent(profile::setSecondPhaseRankScoreDropLimit);
-        parsed.getTermwiseLimit().ifPresent(profile::setTermwiseLimit);
-        parsed.getPostFilterThreshold().ifPresent(profile::setPostFilterThreshold);
-        parsed.getApproximateThreshold().ifPresent(profile::setApproximateThreshold);
-        parsed.getTargetHitsMaxAdjustmentFactor().ifPresent(profile::setTargetHitsMaxAdjustmentFactor);
-        parsed.getKeepRankCount().ifPresent(profile::setKeepRankCount);
-        parsed.getMinHitsPerThread().ifPresent(profile::setMinHitsPerThread);
-        parsed.getNumSearchPartitions().ifPresent(profile::setNumSearchPartitions);
-        parsed.getNumThreadsPerSearch().ifPresent(profile::setNumThreadsPerSearch);
-        parsed.getReRankCount().ifPresent(profile::setRerankCount);
-
-        parsed.getMatchPhase().ifPresent(profile::setMatchPhase);
-        parsed.getDiversity().ifPresent(profile::setDiversity);
-
-        parsed.getFirstPhaseExpression().ifPresent(profile::setFirstPhaseRanking);
-        parsed.getSecondPhaseExpression().ifPresent(profile::setSecondPhaseRanking);
-
-        parsed.getGlobalPhaseExpression().ifPresent(profile::setGlobalPhaseRanking);
-        parsed.getGlobalPhaseRerankCount().ifPresent(profile::setGlobalPhaseRerankCount);
-
-        parsed.getMatchFeatures().forEach(profile::addMatchFeatures);
-        parsed.getRankFeatures().forEach(profile::addRankFeatures);
-        parsed.getSummaryFeatures().forEach(profile::addSummaryFeatures);
-        parsed.getInheritedMatchFeatures().ifPresent(profile::setInheritedMatchFeatures);
-        parsed.getInheritedSummaryFeatures().ifPresent(profile::setInheritedSummaryFeatures);
-        if (parsed.getIgnoreDefaultRankFeatures())
-            profile.setIgnoreDefaultRankFeatures(true);
-
-        parsed.getMutateOperations().forEach(profile::addMutateOperation);
-        parsed.getFieldsWithRankFilter().forEach
-            ((fieldName, isFilter) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.PREFERBITVECTOR, isFilter));
-
-        parsed.getFieldsWithRankWeight().forEach
-            ((fieldName, weight) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.WEIGHT, weight));
-
-        parsed.getFieldsWithRankType().forEach
-            ((fieldName, rankType) -> profile.addRankSetting(fieldName, RankProfile.RankSetting.Type.RANKTYPE, RankType.fromString(rankType)));
-
-        parsed.getRankProperties().forEach
-            ((key, values) -> {for (String value : values) profile.addRankProperty(key, value);});
-
-        // always?
-        rankProfileRegistry.add(profile);
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("In " + parsed, e);
+        }
     }
 
 }
