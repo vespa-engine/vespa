9d6f983dad7ca2c56e86ce0cad8fef691eb021c6
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-08-07 11:19:21 +0200

    refactor: Moved semantic token configuration to a seperate file

--- integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/parser/SubLanguageData.java
+++ integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/parser/SubLanguageData.java
@@ -1,4 +1,8 @@
 package ai.vespa.schemals.parser;
 
+/**
+ * SubLanguageData represents another language embedded in schema language.
+ * It store the content to parse in another parser and the number of stripped chars in front.
+ */
 public record SubLanguageData(String content, int leadingStripped) {
 }
