feaabfdfd66ec3a699fa5c72c83d3e7c65101002
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-08-09 17:28:35 +0200

    chore: Update the vscode publish workflow

--- integration/schema-language-server/clients/vscode/src/publish.ts
+++ integration/schema-language-server/clients/vscode/src/publish.ts
@@ -1,22 +1,17 @@
 import * as fs from 'fs';
 import { publishVSIX, createVSIX } from '@vscode/vsce';
-import * as actions from '@actions/core';
 
 async function publish(): Promise<void> {
-    const version = actions.getInput("version", { required: true });
+    const version = process.env.VERSION_TYPE
 
     await createVSIX({
         version: version
     });
 
-    actions.info("Created VSIX");
-
     const rawJSON = await fs.promises.readFile("package.json", "utf8");
     const json = JSON.parse(rawJSON) as { name: string, version: string };
 
     const vsixFile = `${json.name}-${json.version}.vsix`;
-
-    actions.info(`Publishing VSIX file: ${vsixFile}`);
 
     await publishVSIX(vsixFile, {
         pat: process.env.VSCE_PAT
