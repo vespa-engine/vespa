b85dfd650247105c73acbc562d965f6321008458
Henning Baldersheim <balder@vespa.ai>
2024-08-19 09:24:12 +0000

    Use msbIdx for direct lookup, and then additional check if prev is large enough.

--- vespalib/src/vespa/vespalib/stllike/hashtable.cpp
+++ vespalib/src/vespa/vespalib/stllike/hashtable.cpp
@@ -4,14 +4,12 @@
 
 namespace {
 
-static const unsigned long __stl_prime_list[] =
+constexpr unsigned long STL_PRIME_LIST[] =
 {
-            7ul,        17ul,         53ul,         97ul,        193ul,
-          389ul,       769ul,       1543ul,       3079ul,       6151ul,
-        12289ul,     24593ul,      49157ul,      98317ul,     196613ul,
-       393241ul,    786433ul,    1572869ul,    3145739ul,    6291469ul,
-     12582917ul,  25165843ul,   50331653ul,  100663319ul,  201326611ul,
-    402653189ul, 805306457ul, 1610612741ul, 3221225473ul, 4294967291ul
+            7ul,         7ul,          7ul,         17ul,         53ul,         97ul,        193ul,         389ul,
+          769ul,      1543ul,       3079ul,       6151ul,      12289ul,      24593ul,      49157ul,       98317ul,
+       196613ul,     393241ul,     786433ul,    1572869ul,    3145739ul,    6291469ul,   12582917ul,   25165843ul,
+     50331653ul,  100663319ul,  201326611ul,  402653189ul,  805306457ul, 1610612741ul, 3221225473ul, 4294967291ul
 };
 
 }
@@ -19,18 +17,14 @@ static const unsigned long __stl_prime_list[] =
 namespace vespalib {
 
 size_t
-hashtable_base::getModulo(size_t newSize, const unsigned long * list, size_t sz) noexcept
-{
-    const unsigned long* first = list;
-    const unsigned long* last = list + sz;
-    const unsigned long* pos = std::lower_bound(first, last, newSize);
-    return (pos == last) ? *(last - 1) : *pos;
-}
-
-size_t
 hashtable_base::getModuloStl(size_t size) noexcept
 {
-    return getModulo(size, __stl_prime_list, sizeof(__stl_prime_list)/sizeof(__stl_prime_list[0]));
+    if (size > 0xfffffffful) return 0xfffffffful;
+    if (size < 8) return 7ul;
+    uint32_t index = Optimized::msbIdx(size);
+    return (size <= STL_PRIME_LIST[index - 1])
+           ? STL_PRIME_LIST[index - 1]
+           : STL_PRIME_LIST[index];
 }
 
 }
