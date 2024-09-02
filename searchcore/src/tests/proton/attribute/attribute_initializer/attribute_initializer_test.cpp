9579476b1a58a1f4d01d1e5f26bb60c9c7e73be0
Tor Egge <Tor.Egge@online.no>
2024-09-02 14:07:43 +0200

    Rewrite searchcore attribute initializer unit test to gtest.

--- searchcore/src/tests/proton/attribute/attribute_initializer/attribute_initializer_test.cpp
+++ searchcore/src/tests/proton/attribute/attribute_initializer/attribute_initializer_test.cpp
@@ -11,9 +11,9 @@
 #include <vespa/searchlib/test/directory_handler.h>
 #include <vespa/searchcommon/attribute/config.h>
 #include <vespa/searchcommon/attribute/i_multi_value_attribute.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/util/stash.h>
 #include <vespa/vespalib/util/threadstackexecutor.h>
-#include <vespa/vespalib/testkit/test_kit.h>
 
 #include <vespa/log/log.h>
 LOG_SETUP("attribute_initializer_test");
@@ -128,162 +128,179 @@ Fixture::createInitializer(AttributeSpec &&spec, std::optional<SerialNum> serial
     return std::make_unique<AttributeInitializer>(_diskLayout->createAttributeDir(spec.getName()), "test.subdb", std::move(spec), serialNum, _factory, _executor);
 }
 
-TEST("require that integer attribute can be initialized")
+class AttributeInitializerTest : public ::testing::Test
+{
+protected:
+    AttributeInitializerTest();
+    ~AttributeInitializerTest() override;
+    static void SetUpTestSuite();
+};
+
+AttributeInitializerTest::AttributeInitializerTest()
+    : ::testing::Test()
+{
+}
+
+AttributeInitializerTest::~AttributeInitializerTest() = default;
+
+void
+AttributeInitializerTest::SetUpTestSuite()
+{
+    std::filesystem::remove_all(std::filesystem::path(test_dir));
+}
+
+TEST_F(AttributeInitializerTest, require_that_integer_attribute_can_be_initialized)
 {
     saveAttr("a", int32_sv, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", int32_sv}, 5)->init().getAttribute();
-    EXPECT_EQUAL(2u, av->getCreateSerialNum());
-    EXPECT_EQUAL(2u, av->getNumDocs());
+    EXPECT_EQ(2u, av->getCreateSerialNum());
+    EXPECT_EQ(2u, av->getNumDocs());
 }
 
-TEST("require that mismatching base type is not loaded")
+TEST_F(AttributeInitializerTest, require_that_mismatching_base_type_is_not_loaded)
 {
     saveAttr("a", int32_sv, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", int16_sv}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that mismatching collection type is not loaded")
+TEST_F(AttributeInitializerTest, require_that_mismatching_collection_type_is_not_loaded)
 {
     saveAttr("a", int32_sv, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", int32_array}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that mismatching weighted set collection type params is not loaded")
+TEST_F(AttributeInitializerTest, require_that_mismatching_weighted_set_collection_type_params_is_not_loaded)
 {
     saveAttr("a", string_wset, 10, 2);
     saveAttr("b", string_wset2, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", string_wset2}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
     auto av2 = f.createInitializer({"b", string_wset}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av2->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av2->getNumDocs());
+    EXPECT_EQ(5u, av2->getCreateSerialNum());
+    EXPECT_EQ(1u, av2->getNumDocs());
 }
 
-TEST("require that predicate attributes can be initialized")
+TEST_F(AttributeInitializerTest, require_that_predicate_attributes_can_be_initialized)
 {
     saveAttr("a", predicate, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", predicate}, 5)->init().getAttribute();
-    EXPECT_EQUAL(2u, av->getCreateSerialNum());
-    EXPECT_EQUAL(2u, av->getNumDocs());
+    EXPECT_EQ(2u, av->getCreateSerialNum());
+    EXPECT_EQ(2u, av->getNumDocs());
 }
 
-TEST("require that predicate attributes will not be initialized with future-created attribute")
+TEST_F(AttributeInitializerTest, require_that_predicate_attributes_will_not_be_initialized_with_future_created_attribute)
 {
     saveAttr("a", predicate, 10, 8);
     Fixture f;
     auto av = f.createInitializer({"a", predicate}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that predicate attributes will not be initialized with mismatching type")
+TEST_F(AttributeInitializerTest, require_that_predicate_attributes_will_not_be_initialized_with_mismatching_type)
 {
     saveAttr("a", predicate, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", getPredicateWithArity(4)}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that tensor attribute can be initialized")
+TEST_F(AttributeInitializerTest, require_that_tensor_attribute_can_be_initialized)
 {
     saveAttr("a", getTensor("tensor(x[10])"), 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", getTensor("tensor(x[10])")}, 5)->init().getAttribute();
-    EXPECT_EQUAL(2u, av->getCreateSerialNum());
-    EXPECT_EQUAL(2u, av->getNumDocs());
+    EXPECT_EQ(2u, av->getCreateSerialNum());
+    EXPECT_EQ(2u, av->getNumDocs());
 }
 
-TEST("require that tensor attributes will not be initialized with future-created attribute")
+TEST_F(AttributeInitializerTest, require_that_tensor_attributes_will_not_be_initialized_with_future_created_attribute)
 {
     saveAttr("a", getTensor("tensor(x[10])"), 10, 8);
     Fixture f;
     auto av = f.createInitializer({"a", getTensor("tensor(x[10])")}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that tensor attributes will not be initialized with mismatching type")
+TEST_F(AttributeInitializerTest, require_that_tensor_attributes_will_not_be_initialized_with_mismatching_type)
 {
     saveAttr("a", getTensor("tensor(x[10])"), 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", getTensor("tensor(x[11])")}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that too old attribute is not loaded")
+TEST_F(AttributeInitializerTest, require_that_too_old_attribute_is_not_loaded)
 {
     saveAttr("a", int32_sv, 3, 2);
     Fixture f;
     auto av = f.createInitializer({"a", int32_sv}, 5)->init().getAttribute();
-    EXPECT_EQUAL(5u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(5u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that transient memory usage is reported for first time posting list attribute load after enabling posting lists")
+TEST_F(AttributeInitializerTest, require_that_transient_memory_usage_is_reported_for_first_time_posting_list_attribute_load_after_enabling_posting_lists)
 {
     saveAttr("a", int32_wset, 10, 2);
     Fixture f;
     auto avi = f.createInitializer({"a", get_int32_wset_fs()}, 5);
-    EXPECT_EQUAL(40u, avi->get_transient_memory_usage());
+    EXPECT_EQ(40u, avi->get_transient_memory_usage());
 }
 
-TEST("require that transient memory usage is reported for normal posting list attribute load")
+TEST_F(AttributeInitializerTest, require_that_transient_memory_usage_is_reported_for_normal_posting_list_attribute_load)
 {
     saveAttr("a", get_int32_wset_fs(), 10, 2);
     Fixture f;
     auto avi = f.createInitializer({"a", get_int32_wset_fs()}, 5);
-    EXPECT_EQUAL(24u, avi->get_transient_memory_usage());
+    EXPECT_EQ(24u, avi->get_transient_memory_usage());
 }
 
-TEST("require that transient memory usage is reported for attribute load without posting list")
+TEST_F(AttributeInitializerTest, require_that_transient_memory_usage_is_reported_for_attribute_load_without_posting_list)
 {
     saveAttr("a", int32_wset, 10, 2);
     Fixture f;
     auto avi = f.createInitializer({"a", int32_wset}, 5);
-    EXPECT_EQUAL(0u, avi->get_transient_memory_usage());
+    EXPECT_EQ(0u, avi->get_transient_memory_usage());
 }
 
-TEST("require that saved attribute is ignored when serial num is not set")
+TEST_F(AttributeInitializerTest, require_that_saved_attribute_is_ignored_when_serial_num_is_not_set)
 {
     saveAttr("a", int32_sv, 10, 2);
     Fixture f;
     auto av = f.createInitializer({"a", int32_sv}, std::nullopt)->init().getAttribute();
-    EXPECT_EQUAL(0u, av->getCreateSerialNum());
-    EXPECT_EQUAL(1u, av->getNumDocs());
+    EXPECT_EQ(0u, av->getCreateSerialNum());
+    EXPECT_EQ(1u, av->getNumDocs());
 }
 
-TEST("require that reserved document is reinitialized during load")
+TEST_F(AttributeInitializerTest, require_that_reserved_document_is_reinitialized_during_load)
 {
     saveAttr("a", string_wset, 10, 2, true);
     Fixture f;
     auto av = f.createInitializer({"a", string_wset}, 5)->init().getAttribute();
-    EXPECT_EQUAL(2u, av->getCreateSerialNum());
-    EXPECT_EQUAL(2u, av->getNumDocs());
+    EXPECT_EQ(2u, av->getCreateSerialNum());
+    EXPECT_EQ(2u, av->getNumDocs());
     auto mvav = av->as_multi_value_attribute();
     ASSERT_TRUE(mvav != nullptr);
     Stash stash;
     auto read_view = mvav->make_read_view(IMultiValueAttribute::WeightedSetTag<const char*>(), stash);
     ASSERT_TRUE(read_view != nullptr);
     auto reserved_values = read_view->get_values(0u);
-    EXPECT_EQUAL(0u, reserved_values.size());
+    EXPECT_EQ(0u, reserved_values.size());
 }
 
 }
 
-TEST_MAIN()
-{
-    std::filesystem::remove_all(std::filesystem::path(test_dir));
-    TEST_RUN_ALL();
-}
+GTEST_MAIN_RUN_ALL_TESTS()
