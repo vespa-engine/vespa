6da0ac569b03093d5c0702363c6f52812d6e75f7
Harald Musum <musum@yahooinc.com>
2024-09-02 10:01:11 +0200

    Extend expiry date for a few flags

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -355,13 +355,13 @@ public class Flags {
 
     public static final UnboundBooleanFlag WRITE_CONFIG_SERVER_SESSION_DATA_AS_ONE_BLOB = defineFeatureFlag(
             "write-config-server-session-data-as-blob", false,
-            List.of("hmusum"), "2023-07-19", "2024-09-01",
+            List.of("hmusum"), "2023-07-19", "2024-11-01",
             "Whether to write config server session data in one blob or as individual paths",
             "Takes effect immediately");
 
     public static final UnboundBooleanFlag READ_CONFIG_SERVER_SESSION_DATA_AS_ONE_BLOB = defineFeatureFlag(
             "read-config-server-session-data-as-blob", false,
-            List.of("hmusum"), "2023-07-19", "2024-09-01",
+            List.of("hmusum"), "2023-07-19", "2024-11-01",
             "Whether to read config server session data from session data blob or from individual paths",
             "Takes effect immediately");
 
@@ -396,7 +396,7 @@ public class Flags {
 
     public static final UnboundStringFlag UNKNOWN_CONFIG_DEFINITION = defineStringFlag(
             "unknown-config-definition", "warn",
-            List.of("hmusum"), "2023-09-25", "2024-09-01",
+            List.of("hmusum"), "2023-09-25", "2024-10-01",
             "How to handle user config referencing unknown config definitions. Valid values are 'warn' and 'fail'",
             "Takes effect at redeployment",
             INSTANCE_ID);
