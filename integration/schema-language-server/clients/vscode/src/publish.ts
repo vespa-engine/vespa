0540abbcd3a04fe29394be15b1c15f81b01e58f3
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-09-05 12:32:31 +0200

    refactor: LSP release action

--- integration/schema-language-server/clients/vscode/src/publish.ts
+++ integration/schema-language-server/clients/vscode/src/publish.ts
@@ -2,11 +2,8 @@ import * as fs from 'fs';
 import { publishVSIX, createVSIX } from '@vscode/vsce';
 
 async function publish(): Promise<void> {
-    const version = process.env.VERSION_TYPE
-
-    await createVSIX({
-        version: version
-    });
+    
+    await createVSIX();
 
     const rawJSON = await fs.promises.readFile("package.json", "utf8");
     const json = JSON.parse(rawJSON) as { name: string, version: string };
