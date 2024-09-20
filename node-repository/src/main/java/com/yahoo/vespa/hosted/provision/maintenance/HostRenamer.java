151be0597e4694911c7c255db330e241898d0b24
Andreas Eriksen <andreer@yahooinc.com>
2024-09-20 11:41:29 +0200

    rename by clusterid

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/HostRenamer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/maintenance/HostRenamer.java
@@ -1,7 +1,6 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.vespa.hosted.provision.maintenance;
 
-import com.yahoo.config.provision.ApplicationId;
 import com.yahoo.config.provision.ClusterSpec;
 import com.yahoo.config.provision.NodeType;
 import com.yahoo.jdisc.Metric;
@@ -12,6 +11,7 @@ import com.yahoo.vespa.hosted.provision.NodeList;
 import com.yahoo.vespa.hosted.provision.NodeRepository;
 import com.yahoo.vespa.hosted.provision.node.Agent;
 import com.yahoo.vespa.hosted.provision.node.Allocation;
+import com.yahoo.vespa.hosted.provision.node.ClusterId;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -35,57 +35,57 @@ public class HostRenamer extends NodeRepositoryMaintainer {
         this.hostnameSchemeFlag = Flags.HOSTNAME_SCHEME.bindTo(nodeRepository.flagSource());
     }
 
-    record ApplicationGroup(ApplicationId applicationId, Integer groupIndex){};
+    record ClusterGroup(ClusterId clusterId, Integer groupIndex){}
 
     @Override
     protected double maintain() {
         if (!nodeRepository().nodes().isWorking()) return 0.0;
         NodeList allNodes = nodeRepository().nodes().list();
         NodeList activeHosts = allNodes.nodeType(NodeType.host).state(Node.State.active);
-        Set<ApplicationGroup> retiringApplicationGroups = applicationsOnRetiringHosts(activeHosts, allNodes);
+        Set<ClusterGroup> retiringClusterGroups = applicationsOnRetiringHosts(activeHosts, allNodes);
         for (var host : activeHosts) {
-            Set<ApplicationGroup> applicationGroupsOnHost = applicationsGroupsOn(host, allNodes);
-            if (!changeHostname(host, applicationGroupsOnHost)) continue;
+            Set<ClusterGroup> clusterGroupsOnHost = applicationsGroupsOn(host, allNodes);
+            if (!changeHostname(host, clusterGroupsOnHost)) continue;
 
-            if (Collections.disjoint(retiringApplicationGroups, applicationGroupsOnHost)) {
+            if (Collections.disjoint(retiringClusterGroups, clusterGroupsOnHost)) {
                 LOG.info("Deprovisioning " + host + " to change its hostname");
                 nodeRepository().nodes().deprovision(host.hostname(), Agent.system, nodeRepository().clock().instant());
-                retiringApplicationGroups.addAll(applicationGroupsOnHost);
+                retiringClusterGroups.addAll(clusterGroupsOnHost);
             }
         }
         return 1.0;
     }
 
-    private Set<ApplicationGroup> applicationsGroupsOn(Node host, NodeList allNodes) {
-        Set<ApplicationGroup> applicationGroups = new HashSet<>();
+    private Set<ClusterGroup> applicationsGroupsOn(Node host, NodeList allNodes) {
+        Set<ClusterGroup> clusterGroups = new HashSet<>();
         for (var child : allNodes.childrenOf(host)) {
             Allocation allocation = child.allocation().orElseThrow();
-            applicationGroups.add(new ApplicationGroup(
-                    allocation.owner(),
+            clusterGroups.add(new ClusterGroup(
+                    new ClusterId(allocation.owner(), allocation.membership().cluster().id()),
                     allocation.membership().cluster().group().map(ClusterSpec.Group::index).orElse(0)));
         }
-        return applicationGroups;
+        return clusterGroups;
     }
 
-    private Set<ApplicationGroup> applicationsOnRetiringHosts(NodeList activeHosts, NodeList allNodes) {
-        Set<ApplicationGroup> applications = new HashSet<>();
+    private Set<ClusterGroup> applicationsOnRetiringHosts(NodeList activeHosts, NodeList allNodes) {
+        Set<ClusterGroup> applications = new HashSet<>();
         for (var host : activeHosts.retiring()) {
             applications.addAll(applicationsGroupsOn(host, allNodes));
         }
         return applications;
     }
 
-    private boolean changeHostname(Node node, Set<ApplicationGroup> applicationGroups) {
+    private boolean changeHostname(Node node, Set<ClusterGroup> clusterGroups) {
         if (node.hostname().endsWith(".vespa-cloud.net")) {
             return false;
         }
         Set<String> wantedSchemes;
-        if (applicationGroups.isEmpty()) {
+        if (clusterGroups.isEmpty()) {
             wantedSchemes = Set.of(hostnameSchemeFlag.value());
         } else {
-            wantedSchemes = applicationGroups.stream()
-                                     .map(applicationGroup -> hostnameSchemeFlag.withApplicationId(
-                                             Optional.of(applicationGroup.applicationId())).value())
+            wantedSchemes = clusterGroups.stream()
+                                     .map(clusterGroup -> hostnameSchemeFlag.withApplicationId(
+                                             Optional.of(clusterGroup.clusterId().application())).value())
                                      .collect(Collectors.toSet());
         }
         return wantedSchemes.size() == 1 && wantedSchemes.iterator().next().equals("standard");
