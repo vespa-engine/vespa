8fbe2b17d26e5318593dd0dbbb20cbba39c7095b
Harald Musum <musum@vespa.ai>
2024-10-01 23:16:29 +0200

    Extend expiry date for some flags

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -396,7 +396,7 @@ public class Flags {
 
     public static final UnboundStringFlag UNKNOWN_CONFIG_DEFINITION = defineStringFlag(
             "unknown-config-definition", "warn",
-            List.of("hmusum"), "2023-09-25", "2024-10-01",
+            List.of("hmusum"), "2023-09-25", "2024-11-01",
             "How to handle user config referencing unknown config definitions. Valid values are 'warn' and 'fail'",
             "Takes effect at redeployment",
             INSTANCE_ID);
@@ -500,7 +500,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag DELETE_EXPIRED_CONFIG_SESSIONS_NEW_PROCEDURE = defineFeatureFlag(
             "delete-expired-config-sessions-new-procedure", true,
-            List.of("hmusum"), "2024-06-10", "2024-10-01",
+            List.of("hmusum"), "2024-06-10", "2024-11-01",
             "Whether to delete remote and local config sessions at the same time",
             "Takes effect immediately");
 
@@ -549,13 +549,13 @@ public class Flags {
 
     public static final UnboundBooleanFlag TCP_SHRINK_WINDOW = defineFeatureFlag(
             "tcp-shrink-window", true,
-            List.of("hmusum"), "2024-08-14", "2024-10-01",
+            List.of("hmusum"), "2024-08-14", "2024-11-01",
             "Whether to enable sysctl setting net.ipv4.tcp_shrink_window, default true",
             "Takes effect on next host-admin run");
 
     public static final UnboundLongFlag FILE_DOWNLOAD_BACKOFF_INITIAL_TIME_MS = defineLongFlag(
             "file-download-backoff-initial-time-ms", 5000,
-            List.of("hmusum"), "2024-08-16", "2024-10-01",
+            List.of("hmusum"), "2024-08-16", "2024-11-01",
             "Initial backoff time in milliseconds when failing to download a file reference",
             "Takes effect on restart of Docker container");
 
