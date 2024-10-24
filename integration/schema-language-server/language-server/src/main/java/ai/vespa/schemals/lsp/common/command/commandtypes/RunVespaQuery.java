156836b4632b8e9c4ded8d28ac5c16b194c68b0d
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-10-24 14:11:06 +0200

    feat: Add simple way to run vespa queries from yql files

--- integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/lsp/common/command/commandtypes/RunVespaQuery.java
+++ integration/schema-language-server/language-server/src/main/java/ai/vespa/schemals/lsp/common/command/commandtypes/RunVespaQuery.java
@@ -1,9 +1,15 @@
 package ai.vespa.schemals.lsp.common.command.commandtypes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+
+import org.eclipse.lsp4j.MessageType;
 
 import com.google.gson.JsonPrimitive;
 
+import ai.vespa.schemals.common.ClientLogger;
 import ai.vespa.schemals.context.EventExecuteCommandContext;
 
 public class RunVespaQuery implements SchemaCommand {
@@ -28,6 +34,64 @@ public class RunVespaQuery implements SchemaCommand {
     public void execute(EventExecuteCommandContext context) {
         context.logger.info("Running Vespa query...");
         context.logger.info(queryCommand);
+
+        QueryResult result = runVespaQuery(queryCommand, context.logger);
+
+        if (!result.success()) {
+            context.messageHandler.sendMessage(MessageType.Error, "Failed to run query:\n" + result.result());
+            return;
+        }
+
+        context.logger.info(result.result());
+    }
+
+    private record QueryResult(boolean success, String result) {};
+
+    private QueryResult runVespaQuery(String query, ClientLogger logger) {
+
+        boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+        ProcessBuilder builder = new ProcessBuilder();
+
+        if (isWindows) {
+            builder.command(String.format("cmd.exe /c %s", query));
+        } else {
+            builder.command("/usr/local/bin/vespa", "query", query);
+        }
+
+        try {
+
+            Process process = builder.start();
+    
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+    
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                return new QueryResult(true, output.toString());
+            }
+
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            StringBuilder error = new StringBuilder();
+            while ((line = errorReader.readLine()) != null) {
+                error.append(line).append("\n");
+            }
+
+            return new QueryResult(false, error.toString());
+    
+        } catch (InterruptedException e) {
+            return new QueryResult(false, "Program interrupted");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            return new QueryResult(false, "IOException occurred.");
+        }
+
     }
     
 }
