6705098c831482c02f2eea89c5c64266b05937ed
Tor Egge <Tor.Egge@online.no>
2024-10-26 17:09:14 +0200

    Rewrite searchcore job load sampler unit test to gtest.

--- searchcore/src/tests/proton/common/job_load_sampler_test.cpp
+++ searchcore/src/tests/proton/common/job_load_sampler_test.cpp
@@ -1,13 +1,12 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/searchcore/proton/metrics/job_load_sampler.h>
-#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <chrono>
 
 using namespace proton;
 
-namespace job_load_sampler_test {
-
+inline namespace job_load_sampler_test {
 constexpr double EPS = 0.000001;
 
 using time_point = std::chrono::time_point<std::chrono::steady_clock>;
@@ -20,18 +19,20 @@ time_point fakeTime(double now)
     return time_point(duration_cast<steady_clock::duration>(duration<double>(now)));
 }
 
-struct Fixture
+}
+
+class JobLoadSamplerTest : public ::testing::Test
 {
     JobLoadSampler _sampler;
-    Fixture()
-        : _sampler(fakeTime(10))
-    {
-    }
-    Fixture &start(double now) {
+protected:
+    JobLoadSamplerTest();
+    ~JobLoadSamplerTest() override;
+public:
+    JobLoadSamplerTest& start(double now) {
         _sampler.startJob(fakeTime(now));
         return *this;
     }
-    Fixture &end(double now) {
+    JobLoadSamplerTest& end(double now) {
         _sampler.endJob(fakeTime(now));
         return *this;
     }
@@ -40,66 +41,72 @@ struct Fixture
     }
 };
 
-TEST_F("require that empty sampler gives 0 load", Fixture)
+JobLoadSamplerTest::JobLoadSamplerTest()
+    : ::testing::Test(),
+      _sampler(fakeTime(10))
 {
-    EXPECT_APPROX(0.0, f.sample(11), EPS);
 }
 
-TEST_F("require that empty time interval gives 0 load", Fixture)
+JobLoadSamplerTest::~JobLoadSamplerTest() = default;
+
+TEST_F(JobLoadSamplerTest, require_that_empty_sampler_gives_0_load)
 {
-    EXPECT_APPROX(0.0, f.sample(10), EPS);
+    EXPECT_NEAR(0.0, sample(11), EPS);
 }
 
-TEST_F("require that job that starts and ends in interval gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_empty_time_interval_gives_0_load)
 {
-    f.start(12).end(17);
-    EXPECT_APPROX(0.5, f.sample(20), EPS);
-    EXPECT_APPROX(0.0, f.sample(21), EPS);
+    EXPECT_NEAR(0.0, sample(10), EPS);
 }
 
-TEST_F("require that job that starts in interval gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_job_that_starts_and_ends_in_interval_gets_correct_load)
 {
-    f.start(12);
-    EXPECT_APPROX(0.8, f.sample(20), EPS);
-    EXPECT_APPROX(1.0, f.sample(21), EPS);
+    start(12).end(17);
+    EXPECT_NEAR(0.5, sample(20), EPS);
+    EXPECT_NEAR(0.0, sample(21), EPS);
 }
 
-TEST_F("require that job that ends in interval gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_job_that_starts_in_interval_gets_correct_load)
 {
-    f.start(12).sample(20);
-    f.end(27);
-    EXPECT_APPROX(0.7, f.sample(30), EPS);
-    EXPECT_APPROX(0.0, f.sample(31), EPS);
+    start(12);
+    EXPECT_NEAR(0.8, sample(20), EPS);
+    EXPECT_NEAR(1.0, sample(21), EPS);
 }
 
-TEST_F("require that job that runs in complete interval gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_job_that_ends_in_interval_gets_correct_load)
 {
-    f.start(12).sample(20);
-    EXPECT_APPROX(1.0, f.sample(30), EPS);
-    EXPECT_APPROX(1.0, f.sample(31), EPS);
+    start(12).sample(20);
+    end(27);
+    EXPECT_NEAR(0.7, sample(30), EPS);
+    EXPECT_NEAR(0.0, sample(31), EPS);
 }
 
-TEST_F("require that multiple jobs that starts and ends in interval gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_job_that_runs_in_complete_interval_gets_correct_load)
+{
+    start(12).sample(20);
+    EXPECT_NEAR(1.0, sample(30), EPS);
+    EXPECT_NEAR(1.0, sample(31), EPS);
+}
+
+TEST_F(JobLoadSamplerTest, require_that_multiple_jobs_that_starts_and_ends_in_interval_gets_correct_load)
 {
     // job1: 12->17: 0.5
     // job2: 14->16: 0.2
-    f.start(12).start(14).end(16).end(17);
-    EXPECT_APPROX(0.7, f.sample(20), EPS);
+    start(12).start(14).end(16).end(17);
+    EXPECT_NEAR(0.7, sample(20), EPS);
 }
 
-TEST_F("require that multiple jobs that starts and ends in several intervals gets correct load", Fixture)
+TEST_F(JobLoadSamplerTest, require_that_multiple_jobs_that_starts_and_ends_in_several_intervals_gets_correct_load)
 {
     // job1: 12->22
     // job2: 14->34
     // job3: 25->45
-    f.start(12).start(14);
-    EXPECT_APPROX(1.4, f.sample(20), EPS);
-    f.end(22).start(25);
-    EXPECT_APPROX(1.7, f.sample(30), EPS);
-    f.end(34);
-    EXPECT_APPROX(1.4, f.sample(40), EPS);
-    f.end(45);
-    EXPECT_APPROX(0.5, f.sample(50), EPS);
-}
-
+    start(12).start(14);
+    EXPECT_NEAR(1.4, sample(20), EPS);
+    end(22).start(25);
+    EXPECT_NEAR(1.7, sample(30), EPS);
+    end(34);
+    EXPECT_NEAR(1.4, sample(40), EPS);
+    end(45);
+    EXPECT_NEAR(0.5, sample(50), EPS);
 }
