f4d8706cd0b0be44c14f7ddd1eb171bcbdf3920c
Jon Bratseth <bratseth@vespa.ai>
2024-10-31 19:19:47 +0100

    NormalizeExpression 2-way type inference

--- indexinglanguage/src/main/java/com/yahoo/vespa/indexinglanguage/expressions/NormalizeExpression.java
+++ indexinglanguage/src/main/java/com/yahoo/vespa/indexinglanguage/expressions/NormalizeExpression.java
@@ -23,7 +23,18 @@ public final class NormalizeExpression extends Expression {
     }
 
     public Linguistics getLinguistics() { return linguistics; }
-    
+
+    @Override
+    public DataType setInputType(DataType inputType, VerificationContext context) {
+        return super.setInputType(inputType, context);
+    }
+
+    @Override
+    public DataType setOutputType(DataType outputType, VerificationContext context) {
+        super.setOutputType(DataType.STRING, outputType, null, context);
+        return AnyDataType.instance; // We take the string value of any input
+    }
+
     @Override
     protected void doVerify(VerificationContext context) {
         context.setCurrentType(createdOutputType());
