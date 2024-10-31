d616d0d4b8522bfea227643be0b29430a700c1ba
Magnus Eide-Fredriksen <magnuseifr@gmail.com>
2024-10-31 14:56:09 +0100

    chore: update testing to be compatible with new changes

--- integration/schema-language-server/language-server/src/test/java/ai/vespa/schemals/testutils/TestSchemaDiagnosticsHandler.java
+++ integration/schema-language-server/language-server/src/test/java/ai/vespa/schemals/testutils/TestSchemaDiagnosticsHandler.java
@@ -1,10 +1,12 @@
 package ai.vespa.schemals.testutils;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.eclipse.lsp4j.Diagnostic;
 
 import ai.vespa.schemals.SchemaDiagnosticsHandler;
+import ai.vespa.schemals.common.SchemaDiagnostic.DiagnosticCode;
 
 /*
  * Convenience class for testing without having to construct a language client
@@ -20,5 +22,13 @@ public class TestSchemaDiagnosticsHandler extends SchemaDiagnosticsHandler {
     @Override
     public void publishDiagnostics(String fileURI, List<Diagnostic> diagnostics) {
         diagnosticsStore.addAll(diagnostics);
+    }
+
+    @Override
+    public void resetUndefinedSymbolDiagnostics(String fileURI, List<Diagnostic> newUndefinedSymbolDiagnostics) {
+        Predicate<Diagnostic> undefinedSymbolPredicate = (diagnostic -> diagnostic.getCode().getRight().equals(DiagnosticCode.UNDEFINED_SYMBOL.ordinal()));
+
+        diagnosticsStore.removeIf(undefinedSymbolPredicate);
+        diagnosticsStore.addAll(newUndefinedSymbolDiagnostics.stream().filter(undefinedSymbolPredicate).toList());
     }
 }
