a5112ea73d6ee21acf98fdb43b5ee80e06a97181
Tor Egge <Tor.Egge@online.no>
2024-11-01 12:07:17 +0100

    Style fixes for searchlib posting list cache.

--- searchlib/src/vespa/searchlib/diskindex/i_posting_list_cache.h
+++ searchlib/src/vespa/searchlib/diskindex/i_posting_list_cache.h
@@ -19,13 +19,13 @@ public:
        uint64_t file_id;
        uint64_t bit_offset;
        uint64_t bit_length;
-       Key() : backing_store_file(nullptr), file_id(0), bit_offset(0), bit_length(0) { }
+       Key() noexcept : backing_store_file(nullptr), file_id(0), bit_offset(0), bit_length(0) { }
        size_t hash() const noexcept { return std::rotl(file_id, 40) + bit_offset; }
-       bool operator==(const Key& rhs) const {
+       bool operator==(const Key& rhs) const noexcept {
            // Don't check backing_store_file, it is just passed in key for convenience
            return file_id == rhs.file_id &&
-           bit_offset == rhs.bit_offset &&
-           bit_length == rhs.bit_length;
+                  bit_offset == rhs.bit_offset &&
+                  bit_length == rhs.bit_length;
        }
     };
     /*
