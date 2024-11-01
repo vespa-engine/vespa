62a2b72fa0c1a482ca45684f21b5016e078df906
Tor Egge <Tor.Egge@online.no>
2024-11-01 22:07:10 +0100

    Rewrite searchcore health adapter unit test to gtest.

--- searchcore/src/tests/proton/server/health_adapter_test.cpp
+++ searchcore/src/tests/proton/server/health_adapter_test.cpp
@@ -1,7 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/vespalib/testkit/test_kit.h>
 #include <vespa/searchcore/proton/server/health_adapter.h>
 #include <vespa/searchcore/proton/common/statusreport.h>
+#include <vespa/vespalib/gtest/gtest.h>
 
 using namespace proton;
 
@@ -21,39 +21,54 @@ struct MyStatusProducer : public StatusProducer {
 
 MyStatusProducer::~MyStatusProducer() = default;
 
-TEST_FF("require that empty status list passes health check", MyStatusProducer(), HealthAdapter(f1)) {
+TEST(HealthAdapterTest, require_that_empty_status_list_passes_health_check)
+{
+    MyStatusProducer f1;
+    HealthAdapter f2(f1);
     EXPECT_TRUE(f2.getHealth().ok);
-    EXPECT_EQUAL(std::string("All OK"), f2.getHealth().msg);
+    EXPECT_EQ(std::string("All OK"), f2.getHealth().msg);
 }
 
-TEST_FF("require that UP components passes health check", MyStatusProducer(), HealthAdapter(f1)) {
+TEST(HealthAdapterTest, require_that_UP_components_passes_health_check)
+{
+    MyStatusProducer f1;
+    HealthAdapter f2(f1);
     f1.add("c1", StatusReport::UPOK, "xxx");
     f1.add("c2", StatusReport::UPOK, "yyy");
     f1.add("c3", StatusReport::UPOK, "zzz");
     EXPECT_TRUE(f2.getHealth().ok);
-    EXPECT_EQUAL(std::string("All OK"), f2.getHealth().msg);
+    EXPECT_EQ(std::string("All OK"), f2.getHealth().msg);
 }
 
-TEST_FF("require that PARTIAL component fails health check", MyStatusProducer(), HealthAdapter(f1)) {
+TEST(HealthAdapterTest, require_that_PARTIAL_component_fails_health_check)
+{
+    MyStatusProducer f1;
+    HealthAdapter f2(f1);
     f1.add("c1", StatusReport::UPOK, "xxx");
     f1.add("c2", StatusReport::PARTIAL, "yyy");
     f1.add("c3", StatusReport::UPOK, "zzz");
     EXPECT_FALSE(f2.getHealth().ok);
-    EXPECT_EQUAL(std::string("c2: yyy"), f2.getHealth().msg);
+    EXPECT_EQ(std::string("c2: yyy"), f2.getHealth().msg);
 }
 
-TEST_FF("require that DOWN component fails health check", MyStatusProducer(), HealthAdapter(f1)) {
+TEST(HealthAdapterTest, require_that_DOWN_component_fails_health_check)
+{
+    MyStatusProducer f1;
+    HealthAdapter f2(f1);
     f1.add("c1", StatusReport::UPOK, "xxx");
     f1.add("c2", StatusReport::DOWN, "yyy");
     f1.add("c3", StatusReport::UPOK, "zzz");
     EXPECT_FALSE(f2.getHealth().ok);
-    EXPECT_EQUAL(std::string("c2: yyy"), f2.getHealth().msg);
+    EXPECT_EQ(std::string("c2: yyy"), f2.getHealth().msg);
 }
 
-TEST_FF("require that multiple failure messages are concatenated", MyStatusProducer(), HealthAdapter(f1)) {
+TEST(HealthAdapterTest, require_that_multiple_failure_messages_are_concatenated)
+{
+    MyStatusProducer f1;
+    HealthAdapter f2(f1);
     f1.add("c1", StatusReport::PARTIAL, "xxx");
     f1.add("c2", StatusReport::UPOK, "yyy");
     f1.add("c3", StatusReport::DOWN, "zzz");
     EXPECT_FALSE(f2.getHealth().ok);
-    EXPECT_EQUAL(std::string("c1: xxx, c3: zzz"), f2.getHealth().msg);
+    EXPECT_EQ(std::string("c1: xxx, c3: zzz"), f2.getHealth().msg);
 }
