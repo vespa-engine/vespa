5541b0fda370083f1876c9114b9309523e46f690
Tor Egge <Tor.Egge@online.no>
2024-11-04 13:40:50 +0100

    Include algorithm to get declaration of std::fill.

--- vespalib/src/vespa/vespalib/util/rendezvous.hpp
+++ vespalib/src/vespa/vespalib/util/rendezvous.hpp
@@ -1,6 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "exceptions.h"
+#include <algorithm>
 #include <cassert>
 
 namespace vespalib {
