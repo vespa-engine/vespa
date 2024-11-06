dd76fb427f00763e3292307ee3adcf083f73e0a4
Tor Egge <Tor.Egge@online.no>
2024-11-06 12:46:51 +0100

    Rewrite searchcore resolve view visitor unit test to gtest.

--- searchcore/src/tests/proton/matching/resolveviewvisitor_test.cpp
+++ searchcore/src/tests/proton/matching/resolveviewvisitor_test.cpp
@@ -10,7 +10,7 @@ LOG_SETUP("resolveviewvisitor_test");
 #include <vespa/searchcore/proton/matching/viewresolver.h>
 #include <vespa/searchlib/query/tree/node.h>
 #include <vespa/searchlib/query/tree/querybuilder.h>
-#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <string>
 
 namespace fef_test = search::fef::test;
@@ -39,65 +39,76 @@ ViewResolver getResolver(const string &test_view) {
     return resolver;
 }
 
-struct Fixture {
+class ResolveViewVisitorTest : public ::testing::Test {
+protected:
     IndexEnvironment index_environment;
-
-    Fixture() {
-        index_environment.getFields().emplace_back(FieldType::INDEX, CollectionType::SINGLE, field1, 0);
-        index_environment.getFields().emplace_back(FieldType::INDEX, CollectionType::SINGLE, field2, 1);
-    }
+    ResolveViewVisitorTest();
+    ~ResolveViewVisitorTest() override;
+    void checkResolveAlias(const string &view_name, const string &alias) const;
 };
 
-TEST_F("requireThatFieldsResolveToThemselves", Fixture) {
-    ViewResolver resolver = getResolver(view);
-
-    QueryBuilder<ProtonNodeTypes> builder;
-    ProtonTermData &base = builder.addStringTerm(term, field1, id, weight);
-    Node::UP node = builder.build();
-
-    ResolveViewVisitor visitor(resolver, f.index_environment);
-    node->accept(visitor);
-
-    EXPECT_EQUAL(1u, base.numFields());
-    EXPECT_EQUAL(field1, base.field(0).getName());
+ResolveViewVisitorTest::ResolveViewVisitorTest()
+    : ::testing::Test(),
+      index_environment()
+{
+    index_environment.getFields().emplace_back(FieldType::INDEX, CollectionType::SINGLE, field1, 0);
+    index_environment.getFields().emplace_back(FieldType::INDEX, CollectionType::SINGLE, field2, 1);
 }
 
-void checkResolveAlias(const string &view_name, const string &alias, const Fixture &f) {
+ResolveViewVisitorTest::~ResolveViewVisitorTest() = default;
+
+void
+ResolveViewVisitorTest::checkResolveAlias(const string &view_name, const string &alias) const
+{
     ViewResolver resolver = getResolver(view_name);
 
     QueryBuilder<ProtonNodeTypes> builder;
     ProtonTermData &base = builder.addStringTerm(term, alias, id, weight);
     Node::UP node = builder.build();
 
-    ResolveViewVisitor visitor(resolver, f.index_environment);
+    ResolveViewVisitor visitor(resolver, index_environment);
     node->accept(visitor);
 
-    ASSERT_EQUAL(2u, base.numFields());
-    EXPECT_EQUAL(field1, base.field(0).getName());
-    EXPECT_EQUAL(field2, base.field(1).getName());
+    ASSERT_EQ(2u, base.numFields());
+    EXPECT_EQ(field1, base.field(0).getName());
+    EXPECT_EQ(field2, base.field(1).getName());
 }
 
-TEST_F("requireThatViewsCanResolveToMultipleFields", Fixture) {
-    checkResolveAlias(view, view, f);
+TEST_F(ResolveViewVisitorTest, requireThatFieldsResolveToThemselves) {
+    ViewResolver resolver = getResolver(view);
+
+    QueryBuilder<ProtonNodeTypes> builder;
+    ProtonTermData &base = builder.addStringTerm(term, field1, id, weight);
+    Node::UP node = builder.build();
+
+    ResolveViewVisitor visitor(resolver, index_environment);
+    node->accept(visitor);
+
+    EXPECT_EQ(1u, base.numFields());
+    EXPECT_EQ(field1, base.field(0).getName());
 }
 
-TEST_F("requireThatEmptyViewResolvesAsDefault", Fixture) {
+TEST_F(ResolveViewVisitorTest, requireThatViewsCanResolveToMultipleFields) {
+    checkResolveAlias(view, view);
+}
+
+TEST_F(ResolveViewVisitorTest, requireThatEmptyViewResolvesAsDefault) {
     const string default_view = "default";
     const string empty_view = "";
-    checkResolveAlias(default_view, empty_view, f);
+    checkResolveAlias(default_view, empty_view);
 }
 
-TEST_F("requireThatWeCanForceFilterField", Fixture) {
+TEST_F(ResolveViewVisitorTest, requireThatWeCanForceFilterField) {
     ViewResolver resolver = getResolver(view);
-    f.index_environment.getFields().back().setFilter(true);
-    ResolveViewVisitor visitor(resolver, f.index_environment);
+    index_environment.getFields().back().setFilter(true);
+    ResolveViewVisitor visitor(resolver, index_environment);
 
     { // use filter field settings from index environment
         QueryBuilder<ProtonNodeTypes> builder;
         ProtonStringTerm &sterm = builder.addStringTerm(term, view, id, weight);
         Node::UP node = builder.build();
         node->accept(visitor);
-        ASSERT_EQUAL(2u, sterm.numFields());
+        ASSERT_EQ(2u, sterm.numFields());
         EXPECT_TRUE(!sterm.field(0).is_filter());
         EXPECT_TRUE(sterm.field(1).is_filter());
     }
@@ -107,13 +118,13 @@ TEST_F("requireThatWeCanForceFilterField", Fixture) {
         sterm.setPositionData(false); // force filter
         Node::UP node = builder.build();
         node->accept(visitor);
-        ASSERT_EQUAL(2u, sterm.numFields());
+        ASSERT_EQ(2u, sterm.numFields());
         EXPECT_TRUE(sterm.field(0).is_filter());
         EXPECT_TRUE(sterm.field(1).is_filter());
     }
 }
 
-TEST_F("require that equiv nodes resolve view from children", Fixture) {
+TEST_F(ResolveViewVisitorTest, require_that_equiv_nodes_resolve_view_from_children) {
     ViewResolver resolver;
     resolver.add(view, field1);
 
@@ -123,15 +134,15 @@ TEST_F("require that equiv nodes resolve view from children", Fixture) {
     builder.addStringTerm(term, field2, 43, weight);
     Node::UP node = builder.build();
 
-    ResolveViewVisitor visitor(resolver, f.index_environment);
+    ResolveViewVisitor visitor(resolver, index_environment);
     node->accept(visitor);
 
-    ASSERT_EQUAL(2u, base.numFields());
-    EXPECT_EQUAL(field1, base.field(0).getName());
-    EXPECT_EQUAL(field2, base.field(1).getName());
+    ASSERT_EQ(2u, base.numFields());
+    EXPECT_EQ(field1, base.field(0).getName());
+    EXPECT_EQ(field2, base.field(1).getName());
 }
 
-TEST_F("require that view is resolved for SameElement and its children", Fixture) {
+TEST_F(ResolveViewVisitorTest, require_that_view_is_resolved_for_SameElement_and_its_children) {
     ViewResolver resolver;
     resolver.add(view, field1);
     resolver.add("view2", field2);
@@ -142,16 +153,16 @@ TEST_F("require that view is resolved for SameElement and its children", Fixture
     builder.addStringTerm(term, field2, 43, weight);
     Node::UP node = builder.build();
 
-    ResolveViewVisitor visitor(resolver, f.index_environment);
+    ResolveViewVisitor visitor(resolver, index_environment);
     node->accept(visitor);
 
-    ASSERT_EQUAL(1u, same_elem.numFields());
-    EXPECT_EQUAL(field2, same_elem.field(0).getName());
+    ASSERT_EQ(1u, same_elem.numFields());
+    EXPECT_EQ(field2, same_elem.field(0).getName());
 
-    ASSERT_EQUAL(1u, my_term.numFields());
-    EXPECT_EQUAL(field1, my_term.field(0).getName());
+    ASSERT_EQ(1u, my_term.numFields());
+    EXPECT_EQ(field1, my_term.field(0).getName());
 }
 
 }  // namespace
 
-TEST_MAIN() { TEST_RUN_ALL(); }
+GTEST_MAIN_RUN_ALL_TESTS()
