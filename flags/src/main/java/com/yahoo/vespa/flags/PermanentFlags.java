d8f8eb0323e8795dd76e33100cb421b8c54d2c1f
Harald Musum <musum@vespa.ai>
2024-11-09 16:53:22 +0100

    Add flag that can set keepSessionsWithUnknownStatusHours in config server config

--- flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
@@ -510,6 +510,13 @@ public class PermanentFlags {
             "Allowed Athenz proxy identities",
             "takes effect at redeployment");
 
+    public static final UnboundLongFlag CONFIG_SERVER_UNKNOWN_STATUS_SESSION_EXPIRY_TIME = defineLongFlag(
+            "config-server-unknown-status-session-expiry-time", 6,
+            "Expiry time in hours for sessions with unknown status, can be lowered if there are incidents/bugs " +
+                    " or free disk space is too low and one needs to delete sessions",
+            "Takes effect after restart of config server"
+    );
+
     private PermanentFlags() {}
 
     private static UnboundBooleanFlag defineFeatureFlag(
