74ae9d0c6d554e6497a163ac76145173e2bc8f59
Bjørn Christian Seime <bjorncs@vespa.ai>
2024-11-18 14:49:39 +0100

    Initial migration to Jetty 12

--- container-core/src/main/java/com/yahoo/jdisc/http/server/jetty/RequestUtils.java
+++ container-core/src/main/java/com/yahoo/jdisc/http/server/jetty/RequestUtils.java
@@ -1,12 +1,10 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.jdisc.http.server.jetty;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.eclipse.jetty.http2.server.HTTP2ServerConnection;
+import org.eclipse.jetty.http2.server.internal.HTTP2ServerConnection;
 import org.eclipse.jetty.io.Connection;
-import org.eclipse.jetty.server.HttpConnection;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.internal.HttpConnection;
 
 /**
  * @author bjorncs
@@ -16,8 +14,6 @@ public class RequestUtils {
     public static final String JDISC_REQUEST_SSLSESSION = "jdisc.request.SSLSession";
     public static final String JDISC_REQUEST_CHAIN = "jdisc.request.chain";
     public static final String JDISC_RESPONSE_CHAIN = "jdisc.response.chain";
-    public static final String SERVLET_REQUEST_X509CERT = SecureRequestCustomizer.JAKARTA_SERVLET_REQUEST_X_509_CERTIFICATE;
-    public static final String JETTY_REQUEST_SSLSESSION = new SecureRequestCustomizer().getSslSessionAttribute();
 
     // The local port as reported by servlet spec. This will be influenced by Host header and similar mechanisms.
     // The request URI uses the local listen port as the URI is used for handler routing/bindings.
@@ -27,11 +23,11 @@ public class RequestUtils {
     private RequestUtils() {}
 
     public static Connection getConnection(Request request) {
-        return request.getHttpChannel().getConnection();
+        return request.getConnectionMetaData().getConnection();
     }
 
     public static JDiscServerConnector getConnector(Request request) {
-        return (JDiscServerConnector) request.getHttpChannel().getConnector();
+        return (JDiscServerConnector) request.getConnectionMetaData().getConnector();
     }
 
     static boolean isHttpServerConnection(Connection connection) {
@@ -39,7 +35,7 @@ public class RequestUtils {
     }
 
     /**
-     * Note: {@link HttpServletRequest#getLocalPort()} may return the local port of the load balancer / reverse proxy if proxy-protocol is enabled.
+     * Note: HttpServletRequest.getLocalPort() may return the local port of the load balancer / reverse proxy if proxy-protocol is enabled.
      * @return the actual local port of the underlying Jetty connector
      */
     public static int getConnectorLocalPort(Request request) {
