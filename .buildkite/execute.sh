f3d26e20ee665b18c785399ec6131f9d39d35cc0
Arnstein Ressem <aressem@vespa.ai>
2024-11-27 13:36:15 +0100

    Format annotaion in markdown.

--- .buildkite/execute.sh
+++ .buildkite/execute.sh
@@ -17,7 +17,7 @@ function report()
   echo "Reporting...."
   if [[ $BUILDKITE == true ]]; then
       if [[ -f $LOG_DIR/error-$STEP.log ]]; then
-          tail -100 "$LOG_DIR/error-$STEP.log" | head -80 \
+          (echo '```term'; tail -100 "$LOG_DIR/error-$STEP.log" | head -80; echo '```') \
           | buildkite-agent annotate --style 'error' --context 'ctx-error'
       fi
   fi
