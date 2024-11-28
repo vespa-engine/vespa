3b444419888d594cb5656b23afb6d5f326bdea23
Tor Egge <Tor.Egge@online.no>
2024-11-28 21:19:05 +0100

    Add comment describing why full reset might be needed for term field match data when unpacking match data for a bit vector iterator.

--- searchlib/src/vespa/searchlib/diskindex/disktermblueprint.cpp
+++ searchlib/src/vespa/searchlib/diskindex/disktermblueprint.cpp
@@ -146,6 +146,11 @@ DiskTermBlueprint::createLeafSearch(const TermFieldMatchDataArray & tfmda) const
         LOG(debug, "Return BitVectorIterator: %s, wordNum(%" PRIu64 "), docCount(%" PRIu64 ")",
             getName(_field_index.get_field_id()).c_str(), _lookupRes.wordNum, _lookupRes.counts._numDocs);
         auto bv = get_bitvector();
+        /*
+         * If bitvectors are used when _is_filter_field is false due to word being very common in this disk index
+         * then the term field match data needs a full reset during unpack to clear out values set during unpack
+         * from another iterator for the same term and another disk index or memory index.
+         */
         return BitVectorIterator::create(bv, bv->size(), *tfmda[0], strict(), false, !_is_filter_field);
     }
     auto search(_field_index.create_iterator(_lookupRes, _postingHandle, tfmda));
