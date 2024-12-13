0cb73d472d9b0f0477dc48f15843d21eec133f1a
Arnstein Ressem <aressem@gmail.com>
2024-12-13 08:35:43 +0100

    Revert "Revert "Publish the config-model-fat RPMs separately to allow download of thi…" MERGEOK"

--- .buildkite/publish-artifacts.sh
+++ .buildkite/publish-artifacts.sh
@@ -12,10 +12,11 @@ cd "$WORKDIR/artifacts/$ARCH"
 
 tar -cf rpm-repo.tar rpms &
 tar -cf maven-repo.tar maven-repo
+cp -a rpms/vespa-config-model-fat-*.rpm .
 wait
 
-for FILE in *.tar; do
+for FILE in *.tar *.rpm; do
     cosign sign-blob -y --oidc-provider=buildkite-agent --output-signature "$FILE.sig" --output-certificate "$FILE.pem" "$FILE"
 done
 
-buildkite-agent artifact upload "*.tar;*.tar.sig;*.tar.pem" "$BUILDKITE_ARTIFACT_DESTINATION/$VESPA_VERSION/artifacts/$ARCH"
+buildkite-agent artifact upload "*.tar;*.tar.sig;*.tar.pem;*.rpm;*.rpm.sig;*.rpm.pem" "$BUILDKITE_ARTIFACT_DESTINATION/$VESPA_VERSION/artifacts/$ARCH"
