b9b3f02149a84c6af1d603f6fcc612fab7497575
Harald Musum <musum@vespa.ai>
2024-12-13 09:03:20 +0100

    Extend expiry date for some flags

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -226,7 +226,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag USE_V8_GEO_POSITIONS = defineFeatureFlag(
             "use-v8-geo-positions", true,
-            List.of("arnej"), "2021-11-15", "2024-12-31",
+            List.of("arnej"), "2021-11-15", "2025-02-01",
             "Use Vespa 8 types and formats for geographical positions",
             "Takes effect at redeployment",
             INSTANCE_ID);
@@ -247,7 +247,7 @@ public class Flags {
 
     public static final UnboundStringFlag LOG_FILE_COMPRESSION_ALGORITHM = defineStringFlag(
             "log-file-compression-algorithm", "",
-            List.of("arnej"), "2022-06-14", "2024-12-31",
+            List.of("arnej"), "2022-06-14", "2025-02-01",
             "Which algorithm to use for compressing log files. Valid values: empty string (default), gzip, zstd",
             "Takes effect immediately",
             INSTANCE_ID);
@@ -364,7 +364,7 @@ public class Flags {
 
     public static final UnboundBooleanFlag CLOUD_TRIAL_NOTIFICATIONS = defineFeatureFlag(
             "cloud-trial-notifications", false,
-            List.of("bjorncs", "oyving"), "2023-10-13", "2024-12-31",
+            List.of("bjorncs", "oyving"), "2023-10-13", "2025-02-01",
             "Whether to send cloud trial email notifications",
             "Takes effect immediately");
 
@@ -379,7 +379,7 @@ public class Flags {
 
     public static UnboundBooleanFlag LOGSERVER_OTELCOL_AGENT = defineFeatureFlag(
             "logserver-otelcol-agent", false,
-            List.of("olaa"), "2024-04-03", "2024-12-31",
+            List.of("olaa"), "2024-04-03", "2025-02-01",
             "Whether logserver container should run otel agent",
             "Takes effect at redeployment", INSTANCE_ID);
 
