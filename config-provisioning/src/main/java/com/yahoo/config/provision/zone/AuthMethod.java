dfb09408a4739ca1ff6b1f7781fa9eca233bc35d
evgiz <sigve@vespa.ai>
2025-01-21 10:37:21 +0100

    Add serialization for zone endpoint auth methods in load balancer

--- config-provisioning/src/main/java/com/yahoo/config/provision/zone/AuthMethod.java
+++ config-provisioning/src/main/java/com/yahoo/config/provision/zone/AuthMethod.java
@@ -1,6 +1,8 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.config.provision.zone;
 
+import java.util.Optional;
+
 /**
  * An endpoint's authentication method.
  *
@@ -16,5 +18,31 @@ public enum AuthMethod {
 
     /** Clients cannot authenticate with the endpoint directly */
     none;
+
+    @Override
+    public String toString() {
+        return serialize();
+    }
+
+    public String serialize() {
+        return switch(this) {
+            case mtls -> "mtls";
+            case token -> "token";
+            case none -> "none";
+        };
+    }
+
+    public static Optional<AuthMethod> fromString(String value) {
+        if(value.equals(AuthMethod.mtls.serialize())) {
+            return Optional.of(AuthMethod.mtls);
+        }
+        if(value.equals(AuthMethod.token.serialize())) {
+            return Optional.of(AuthMethod.token);
+        }
+        if(value.equals(AuthMethod.none.serialize())) {
+            return Optional.of(AuthMethod.none);
+        }
+        return Optional.empty();
+    }
 
 }
--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/persistence/LoadBalancerSerializer.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/persistence/LoadBalancerSerializer.java
@@ -6,6 +6,7 @@ import com.yahoo.config.provision.CloudAccount;
 import com.yahoo.config.provision.ZoneEndpoint;
 import com.yahoo.config.provision.ZoneEndpoint.AllowedUrn;
 import com.yahoo.config.provision.ZoneEndpoint.AccessType;
+import com.yahoo.config.provision.zone.AuthMethod;
 import com.yahoo.slime.ArrayTraverser;
 import com.yahoo.slime.Cursor;
 import com.yahoo.slime.Inspector;
@@ -63,6 +64,7 @@ public class LoadBalancerSerializer {
     private static final String settingsField = "settings";
     private static final String publicField = "public";
     private static final String privateField = "private";
+    private static final String authMethodsField = "authMethods";
     private static final String allowedUrnsField = "allowedUrns";
     private static final String accessTypeField = "type";
     private static final String urnField = "urn";
@@ -153,6 +155,12 @@ public class LoadBalancerSerializer {
     private static void toSlime(Cursor settingsObject, ZoneEndpoint settings) {
         settingsObject.setBool(publicField, settings.isPublicEndpoint());
         settingsObject.setBool(privateField, settings.isPrivateEndpoint());
+
+        Cursor authMethods = settingsObject.setArray(authMethodsField);
+        for(AuthMethod method : settings.authMethods()) {
+            authMethods.addString(method.serialize());
+        }
+
         if (settings.isPrivateEndpoint()) {
             Cursor allowedUrnsArray = settingsObject.setArray(allowedUrnsField);
             for (AllowedUrn urn : settings.allowedUrns()) {
@@ -171,14 +179,20 @@ public class LoadBalancerSerializer {
         if ( ! settingsObject.valid()) return ZoneEndpoint.defaultEndpoint;
         return new ZoneEndpoint(settingsObject.field(publicField).asBool(),
                                 settingsObject.field(privateField).asBool(),
+                                SlimeUtils.entriesStream(settingsObject.field(authMethodsField))
+                                        .map(field -> AuthMethod.fromString(field.asString()))
+                                        .filter(Optional::isPresent)
+                                        .map(Optional::get)
+                                        .toList(),
                                 SlimeUtils.entriesStream(settingsObject.field(allowedUrnsField))
-                                          .map(urnObject -> new AllowedUrn(switch (urnObject.field(accessTypeField).asString()) {
-                                                                               case "awsPrivateLink" -> AccessType.awsPrivateLink;
-                                                                               case "gcpServiceConnect" -> AccessType.gcpServiceConnect;
-                                                                               default -> throw new IllegalArgumentException("unknown service access type in '" + urnObject + "'");
-                                                                           },
-                                                                           urnObject.field(urnField).asString()))
-                                          .toList());
+                                        .map(urnObject -> new AllowedUrn(
+                                                switch (urnObject.field(accessTypeField).asString()) {
+                                                    case "awsPrivateLink" -> AccessType.awsPrivateLink;
+                                                    case "gcpServiceConnect" -> AccessType.gcpServiceConnect;
+                                                    default -> throw new IllegalArgumentException("unknown service access type in '" + urnObject + "'");
+                                                },
+                                               urnObject.field(urnField).asString()))
+                                        .toList());
     }
 
     private static <T> Optional<T> optionalValue(Inspector field, Function<Inspector, T> fieldMapper) {
