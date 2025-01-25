495a7ef95396c1b8392f59bd66d94f7ff481575b
Jon Bratseth <bratseth@vespa.ai>
2025-01-25 13:41:20 +0100

    Add to_uri as prophesied in the documentation

---
+++ indexinglanguage/src/main/java/com/yahoo/vespa/indexinglanguage/expressions/ToUriExpression.java
@@ -0,0 +1,53 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.vespa.indexinglanguage.expressions;
+
+import com.yahoo.document.DataType;
+import com.yahoo.document.datatypes.StringFieldValue;
+import com.yahoo.document.datatypes.UriFieldValue;
+
+/**
+ * @author bratseth
+ */
+public final class ToUriExpression extends Expression {
+
+    @Override
+    public DataType setInputType(DataType input, VerificationContext context) {
+        super.setInputType(input, context);
+        return DataType.URI;
+    }
+
+    @Override
+    public DataType setOutputType(DataType output, VerificationContext context) {
+        super.setOutputType(DataType.URI, output, null, context);
+        return getInputType(context);
+    }
+
+    @Override
+    protected void doVerify(VerificationContext context) {
+        if (context.getCurrentType() == null)
+            throw new VerificationException(this, "Expected input, but no input is provided");
+        context.setCurrentType(createdOutputType());
+    }
+
+    @Override
+    protected void doExecute(ExecutionContext context) {
+        context.setCurrentValue(new UriFieldValue(String.valueOf(context.getCurrentValue())));
+    }
+
+    @Override
+    public DataType createdOutputType() { return DataType.URI; }
+
+    @Override
+    public String toString() { return "to_uri"; }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof ToUriExpression;
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
