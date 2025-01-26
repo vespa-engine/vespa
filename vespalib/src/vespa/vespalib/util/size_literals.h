b4ad7ee1bd92cb4edb13eea236c6332607f547c0
Tor Egge <Tor.Egge@online.no>
2025-01-26 17:29:28 +0100

    Remove deprecated space between literal operator quotes and suffix. See CWG2521 for rationale.

--- vespalib/src/vespa/vespalib/util/size_literals.h
+++ vespalib/src/vespa/vespalib/util/size_literals.h
@@ -4,18 +4,18 @@
 
 #include <cstddef>
 
-constexpr size_t operator "" _Ki(unsigned long long k_in) {
+constexpr size_t operator ""_Ki(unsigned long long k_in) {
 	return size_t(k_in << 10u);
 }
 
-constexpr size_t operator "" _Mi(unsigned long long m_in) {
+constexpr size_t operator ""_Mi(unsigned long long m_in) {
 	return size_t(m_in << 20u);
 }
 
-constexpr size_t operator "" _Gi(unsigned long long g_in) {
+constexpr size_t operator ""_Gi(unsigned long long g_in) {
 	return size_t(g_in << 30u);
 }
 
-constexpr size_t operator "" _Ti(unsigned long long t_in) {
+constexpr size_t operator ""_Ti(unsigned long long t_in) {
 	return size_t(t_in << 40u);
 }
