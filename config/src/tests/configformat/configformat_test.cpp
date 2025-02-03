fab5b7bcd923a15f4e35ace4397c5c36427a63ae
Tor Egge <Tor.Egge@online.no>
2025-02-03 21:36:59 +0100

    Rewrite config configformat unit test to gtest.

--- config/src/tests/configformat/configformat_test.cpp
+++ config/src/tests/configformat/configformat_test.cpp
@@ -1,12 +1,13 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <vespa/vespalib/testkit/test_kit.h>
 #include <vespa/config/print/fileconfigformatter.h>
 #include <vespa/vespalib/data/slime/slime.h>
+#include <vespa/vespalib/gtest/gtest.h>
 
 using namespace config;
 using namespace vespalib::slime::convenience;
 
-TEST("requireThatConfigIsFormatted") {
+TEST(ConfigFormatTest, requireThatConfigIsFormatted)
+{
     ConfigDataBuffer buffer;
     vespalib::Slime & slime(buffer.slimeObject());
     Cursor &c = slime.setObject().setObject("configPayload").setObject("myField");
@@ -15,7 +16,7 @@ TEST("requireThatConfigIsFormatted") {
 
     FileConfigFormatter formatter;
     formatter.encode(buffer);
-    EXPECT_EQUAL(std::string("myField \"foo\"\n"), buffer.getEncodedString());
+    EXPECT_EQ(std::string("myField \"foo\"\n"), buffer.getEncodedString());
 }
 
-TEST_MAIN() { TEST_RUN_ALL(); }
+GTEST_MAIN_RUN_ALL_TESTS()
