91bc7c1c57254dd5f714fe3c7ddedc1bf6b4c5e5
Valerij Fredriksen <valerijf@vespa.ai>
2025-02-17 11:56:33 +0100

    Stop provisioning exclusivity=provision

--- node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/HostProvisioner.java
+++ node-repository/src/main/java/com/yahoo/vespa/hosted/provision/provisioning/HostProvisioner.java
@@ -24,9 +24,6 @@ public interface HostProvisioner {
 
     enum HostSharing {
 
-        /** The host must be provisioned exclusively for the application ID. */
-        provision,
-
         /** The host must be exclusive to a single application ID */
         exclusive,
 
@@ -34,7 +31,7 @@ public interface HostProvisioner {
         shared;
 
         public boolean isExclusiveAllocation() {
-            return this == provision || this == exclusive;
+            return this == exclusive;
         }
 
     }
