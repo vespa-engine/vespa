197b0ce7314d596dd3bf42883f4d2b7a51ef96ab
gjoranv <gjoranv@gmail.com>
2025-02-20 10:44:23 +0100

    Double quote to prevent globbing and word splitting

--- .buildkite/factory-command.sh
+++ .buildkite/factory-command.sh
@@ -13,7 +13,7 @@ FACTORY_API="https://api.factory.vespa.ai/factory/v1"
 
 IDENTITY_PATH="${ATHENZ_KEY_AND_CERT_PATH:-/workspace/identity}"
 CURL="curl -sL --key $IDENTITY_PATH/key --cert $IDENTITY_PATH/cert"
-TOKEN=$(curl -sL --key $IDENTITY_PATH/key --cert $IDENTITY_PATH/cert -X POST -H "Content-Type: application/x-www-form-urlencoded" -d"grant_type=client_credentials&scope=vespa.factory%3Adomain" "https://zts.athenz.vespa-cloud.com:4443/zts/v1/oauth2/token" | jq -re '.access_token')
+TOKEN=$(curl -sL --key "$IDENTITY_PATH/key" --cert "$IDENTITY_PATH/cert" -X POST -H "Content-Type: application/x-www-form-urlencoded" -d"grant_type=client_credentials&scope=vespa.factory%3Adomain" "https://zts.athenz.vespa-cloud.com:4443/zts/v1/oauth2/token" | jq -re '.access_token')
 
 shift
 case $COMMAND in
