401ac38401bc1376c3007fb32732256ae5b6b45f
Arne Juul <arnej@vespa.ai>
2025-03-11 11:48:00 +0000

    de-inline some code

--- vespalib/src/vespa/fastlib/text/generate_hash_table.h
+++ vespalib/src/vespa/fastlib/text/generate_hash_table.h
@@ -25,7 +25,7 @@ class GenerateHashTable {
     struct Bucket {
         Idx slot;
         std::vector<KeyT> keys;
-        bool operator< (const Bucket& other) {
+        bool operator< (const Bucket& other) const {
             return keys.size() > other.keys.size();
         }
     };
@@ -45,44 +45,9 @@ class GenerateHashTable {
 
     struct FoundBias { KeyT bias; Taken taken; };
 
-    FoundBias findBias(const KeyV& keys) const {
-        for (KeyT bias = 1; bias != 0; bias++) {
-            Taken taken = _takenSlots;
-            bool allOk = true;
-            for (KeyT key : keys) {
-                Idx hash = mix_hash(key, bias, _size);
-                if (taken[hash]) {
-                    allOk = false;
-                    break;
-                }
-                taken[hash] = true;
-            }
-            if (allOk) {
-                return FoundBias{bias, taken};
-            }
-        }
-        // this won't happen for reasonable input:
-        throw "Could not find any possible bias";
-    }
+    FoundBias findBias(const KeyV& keys) const;
 
-    void findBiases() {
-        std::vector<Bucket> buckets;
-        for (Idx slot = 0; slot < _size; ++ slot) {
-            buckets.emplace_back(Bucket{slot, {}});
-            _bias[slot] = 0;
-        }
-        for (KeyT key : _keys) {
-            Idx slot = mix_hash(key, 0, _size);
-            buckets[slot].keys.push_back(key);
-        }
-        std::sort(buckets.begin(), buckets.end());
-        for (auto & bucket : buckets) {
-            if (bucket.keys.size() == 0) break;
-            auto [bias, taken] = findBias(bucket.keys);
-            _bias[bucket.slot] = bias;
-            _takenSlots = taken;
-        }
-    }
+    void findBiases();
 
 public:
 
@@ -104,6 +69,7 @@ public:
         return result;
     }
 
+    ~GenerateHashTable();
 };
 
 } // namespace vespalib
