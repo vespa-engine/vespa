63e767613085f4c4d29df7d0f0c061966660b5da
Arne Juul <arnej@vespa.ai>
2025-03-12 14:35:53 +0000

    allow forcing dummy keyring implementation

--- client/go/internal/cli/auth/secrets.go
+++ client/go/internal/cli/auth/secrets.go
@@ -3,22 +3,72 @@
 package auth
 
 import (
+	"os"
+
 	"github.com/zalando/go-keyring"
 )
 
-type Keyring struct{}
+type realKeyring struct{}
+type dummyKeyring struct{}
+
+func NewKeyring() SecretStore {
+	if env := os.Getenv("VESPA_CLI_DUMMY_KEYRING"); env != "" {
+		return &dummyKeyring{}
+	}
+	return &realKeyring{}
+}
 
 // Set sets the given key/value pair with the given namespace.
-func (k *Keyring) Set(namespace, key, value string) error {
+func (k *realKeyring) Set(namespace, key, value string) error {
 	return keyring.Set(namespace, key, value)
 }
 
 // Get gets a value for the given namespace and key.
-func (k *Keyring) Get(namespace, key string) (string, error) {
+func (k *realKeyring) Get(namespace, key string) (string, error) {
 	return keyring.Get(namespace, key)
 }
 
 // Delete deletes a value for the given namespace and key.
-func (k *Keyring) Delete(namespace, key string) error {
+func (k *realKeyring) Delete(namespace, key string) error {
 	return keyring.Delete(namespace, key)
+}
+
+func dummyRingFileName(namespace, key string) (string, error) {
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	home := userHome + "/.vespa"
+	if err := os.MkdirAll(home, 0700); err != nil {
+		return "", err
+	}
+	return home + "/keyring." + namespace + "." + key, nil
+}
+
+// Set sets the given key/value pair with the given namespace.
+func (k *dummyKeyring) Set(namespace, key, value string) error {
+	fn, err := dummyRingFileName(namespace, key)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fn, []byte(value), 0400)
+}
+
+// Get gets a value for the given namespace and key.
+func (k *dummyKeyring) Get(namespace, key string) (string, error) {
+	fn, err := dummyRingFileName(namespace, key)
+	if err != nil {
+		return "", err
+	}
+	a, b := os.ReadFile(fn)
+	return string(a), b
+}
+
+// Delete deletes a value for the given namespace and key.
+func (k *dummyKeyring) Delete(namespace, key string) error {
+	fn, err := dummyRingFileName(namespace, key)
+	if err != nil {
+		return err
+	}
+	return os.Remove(fn)
 }
