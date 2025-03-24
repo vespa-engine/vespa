656d1f69883fcafc4a7c9a3d23a128fe7ed976e6
thomasht86 <thomas@vespa.ai>
2025-03-24 14:49:53 +0100

    add more inferenceparameters

--- vespajlib/src/main/java/ai/vespa/llm/InferenceParameters.java
+++ vespajlib/src/main/java/ai/vespa/llm/InferenceParameters.java
@@ -27,6 +27,8 @@ public class InferenceParameters {
     public static final String OPTION_TOP_P = "topp";
     public static final String OPTION_N_PREDICT = "npredict";
     public static final String OPTION_REPEAT_PENALTY = "repeatpenalty";
+    public static final String OPTION_FREQUENCY_PENALTY = "frequencypenalty";
+    public static final String OPTION_PRESENCE_PENALTY = "precencepenalty";
     public static final String OPTION_SEED = "seed";
     public static final String OPTION_JSON_SCHEMA = "json_schema";
     
