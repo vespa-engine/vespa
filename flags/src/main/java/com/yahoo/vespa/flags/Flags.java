52c016ea50d4dc98237ae9a7fb13b20733848ce3
Harald Musum <musum@vespa.ai>
2025-03-25 11:43:00 +0100

    Stop using feature flag FILE_DISTRIBUTION_COMPRESSION_TYPES_TO_SERVE

--- flags/src/main/java/com/yahoo/vespa/flags/Flags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/Flags.java
@@ -345,14 +345,6 @@ public class Flags {
             "Takes effect at redeployment",
             INSTANCE_ID);
 
-    public static final UnboundListFlag<String> FILE_DISTRIBUTION_COMPRESSION_TYPES_TO_SERVE = defineListFlag(
-            "file-distribution-compression-types-to-serve", List.of("zstd", "lz4", "none", "gzip"), String.class,
-            List.of("hmusum"), "2025-02-24", "2025-04-01",
-            "List of compression type to use when distributing files, in preferred order, depends " +
-                    "on what the client accepts (sent in request from client)",
-            "Takes effect at restart of config server",
-            INSTANCE_ID);
-
     public static final UnboundBooleanFlag INCREMENTAL_USAGE_CALCULATION = defineFeatureFlag(
             "incremental-usage-calculation", false,
             List.of("evgiz"), "2025-02-27", "2025-05-01",
