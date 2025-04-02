221778ee4fc8780f4a8088e4e4e5ef6a90750b95
Tor Egge <Tor.Egge@online.no>
2025-04-02 16:22:29 +0200

    Handle missing policy for string sort blob writer.

--- searchlib/src/vespa/searchlib/attribute/string_sort_blob_writer.cpp
+++ searchlib/src/vespa/searchlib/attribute/string_sort_blob_writer.cpp
@@ -6,19 +6,68 @@
 #include <cstring>
 #include <span>
 
+using search::common::sortspec::MissingPolicy;
+
 namespace search::attribute {
 
+namespace {
+
 template <bool asc>
-StringSortBlobWriter<asc>::StringSortBlobWriter(const BlobConverter* bc) noexcept
+unsigned char remap(unsigned char val)
+{
+    return (asc ? val : (0xff - val));
+}
+
+}
+
+template <bool asc>
+StringSortBlobWriter<asc>::StringSortBlobWriter(const BlobConverter* bc, MissingPolicy policy,
+                                                std::string_view missing_value, bool multi_value) noexcept
     : _best_size(),
       _serialize_to(nullptr),
       _available(0),
-      _bc(bc)
+      _bc(bc),
+      _missing_blob(),
+      _value_prefix()
 {
+    switch (policy) {
+        case MissingPolicy::DEFAULT:
+            if (multi_value) {
+                _missing_blob.emplace_back(1);
+                _value_prefix.emplace(0);
+            } else {
+                set_missing_blob({}); // Serialize missing value as empty string
+            }
+            break;
+        case MissingPolicy::FIRST:
+            _missing_blob.emplace_back(0);
+            _value_prefix.emplace(1);
+            break;
+        case MissingPolicy::LAST:
+            _missing_blob.emplace_back(1);
+            _value_prefix.emplace(0);
+            break;
+        case MissingPolicy::AS:
+            set_missing_blob(missing_value);
+            break;
+        default:
+            break;
+    }
 }
 
 template <bool asc>
 StringSortBlobWriter<asc>::~StringSortBlobWriter() noexcept = default;
+
+template <bool asc>
+void
+StringSortBlobWriter<asc>::set_missing_blob(std::string_view value)
+{
+    _missing_blob.clear();
+    for (auto c: value) {
+        _missing_blob.emplace_back(remap<asc>(c));
+    }
+    _missing_blob.emplace_back(remap<asc>(0));
+}
 
 template <bool asc>
 void
@@ -40,7 +89,7 @@ StringSortBlobWriter<asc>::candidate(const char* val)
     }
     if (_best_size.has_value()) {
         auto common_size = std::min(_best_size.value(), buf.size());
-        auto cmpres = std::memcmp(_serialize_to + 1, buf.data(), common_size);
+        auto cmpres = std::memcmp(_serialize_to + value_prefix_len(), buf.data(), common_size);
         if constexpr (asc) {
             if (cmpres < 0 || (cmpres == 0 && _best_size.value() < buf.size())) {
                 return true;
@@ -51,11 +100,13 @@ StringSortBlobWriter<asc>::candidate(const char* val)
             }
         }
     }
-    if (_available < buf.size() + 1) {
+    if (_available < buf.size() + value_prefix_len()) {
         return false;
     }
-    _serialize_to[0] = has_value;
-    memcpy(_serialize_to + 1, buf.data(), buf.size());
+    if (_value_prefix.has_value()) {
+        _serialize_to[0] = _value_prefix.value();
+    }
+    memcpy(_serialize_to + value_prefix_len(), buf.data(), buf.size());
     _best_size = buf.size();
     return true;
 }
@@ -66,18 +117,18 @@ StringSortBlobWriter<asc>::write()
 {
     if (_best_size.has_value()) {
         if constexpr (!asc) {
-            std::span<unsigned char> buf(_serialize_to + 1, _best_size.value());
+            std::span<unsigned char> buf(_serialize_to + value_prefix_len(), _best_size.value());
             for (auto& c : buf) {
                 c = 0xff - c;
             }
         }
-        return 1 + _best_size.value();
+        return value_prefix_len() + _best_size.value();
     } else {
-        if (_available < 1) {
+        if (_available < _missing_blob.size()) {
             return -1;
         }
-        _serialize_to[0] = missing_value;
-        return 1;
+        memcpy(_serialize_to, _missing_blob.data(), _missing_blob.size());
+        return _missing_blob.size();
     }
 }
 
