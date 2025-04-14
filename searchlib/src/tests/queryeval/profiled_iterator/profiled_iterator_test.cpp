8646340f57fbaff4204c40749979be1571eb5ed6
Håvard Pettersen <havardpe@yahooinc.com>
2025-04-14 10:06:43 +0000

    return new profiling tags (/3/2/type/seek -> [4]type::doSeek)

--- searchlib/src/tests/queryeval/profiled_iterator/profiled_iterator_test.cpp
+++ searchlib/src/tests/queryeval/profiled_iterator/profiled_iterator_test.cpp
@@ -25,6 +25,29 @@ using vespalib::Trinary;
 
 size_t num_docs = 100;
 
+std::string extract_name(std::string_view tag) {
+    auto end = tag.find("<");
+    auto ns = tag.rfind("::", end);
+    size_t begin = (ns > tag.size()) ? 0 : ns + 2;
+    return std::string(tag.substr(begin, end - begin));
+}
+
+std::string strip_tag(std::string_view tag) {
+    std::string prefix;
+    std::string suffix;
+    size_t begin = 0;
+    if (auto pos = tag.find(']'); pos < tag.size()) {
+        prefix = tag.substr(0, pos + 1);
+        begin = pos + 1;
+    }
+    auto end = tag.size();
+    if (auto pos = tag.rfind("::"); pos < tag.size()) {
+        suffix = tag.substr(pos);
+        end = pos;
+    }
+    return prefix + extract_name(tag.substr(begin, end - begin)) + suffix;
+}
+
 bool is_true(Trinary a) {
     REQUIRE(a != Trinary::Undefined);
     return (a == Trinary::True);
@@ -47,26 +70,29 @@ struct MySources {
 MySources my_sources({{1,3},{3,3},{5,3},
                       {2,5},{4,5},{6,5}});
 
-SearchIterator::UP t(std::vector<uint32_t> hits) {
-    auto search = std::make_unique<SimpleSearch>(SimpleResult(hits), false);
-    search->tag("t");
+int id_seq = 0;
+void reset_id() { id_seq = 0; }
+SearchIterator::UP with_id(SearchIterator::UP search) {
+    search->set_id(++id_seq);
     return search;
 }
 
 SearchIterator::UP T(std::vector<uint32_t> hits) {
-    auto search = std::make_unique<SimpleSearch>(SimpleResult(hits), true);
-    search->tag("T");
-    return search;
+    return with_id(std::make_unique<SimpleSearch>(SimpleResult(hits), true));
+}
+
+SearchIterator::UP t(std::vector<uint32_t> hits) {
+    return with_id(std::make_unique<SimpleSearch>(SimpleResult(hits), false));
 }
 
 SearchIterator::UP OR(SearchIterator::UP s1, SearchIterator::UP s2) {
     bool strict = is_true(s1->is_strict(), s2->is_strict());
-    return OrSearch::create({std::move(s1), std::move(s2)}, strict);
+    return with_id(OrSearch::create({std::move(s1), std::move(s2)}, strict));
 }
 
 SearchIterator::UP AND(SearchIterator::UP s1, SearchIterator::UP s2) {
     bool strict = is_true(s1->is_strict());
-    return AndSearch::create({std::move(s1), std::move(s2)}, strict);
+    return with_id(AndSearch::create({std::move(s1), std::move(s2)}, strict));
 }
 
 SearchIterator::UP blend(SearchIterator::UP s1, uint32_t id1,
@@ -76,10 +102,16 @@ SearchIterator::UP blend(SearchIterator::UP s1, uint32_t id1,
     SourceBlenderSearch::Children list;
     list.emplace_back(s1.release(), id1);
     list.emplace_back(s2.release(), id2);
-    return SourceBlenderSearch::create(my_sources.selector.createIterator(), list, strict);
+    return with_id(SourceBlenderSearch::create(my_sources.selector.createIterator(), list, strict));
+}
+
+SearchIterator::UP create_term(std::vector<uint32_t> hits) {
+    reset_id();
+    return T(hits);
 }
 
 SearchIterator::UP create_iterator_tree() {
+    reset_id();
     return AND(OR(T({4,6,8}),
                   T({5,7,9})),
                blend(t({1,3,5,7,9}), 3,
@@ -87,6 +119,7 @@ SearchIterator::UP create_iterator_tree() {
 }
 
 SearchIterator::UP create_weak_and() {
+    reset_id();
     struct DummyHeap : WeakAndHeap {
         void adjust(score_t *, score_t *) override {}
         DummyHeap() : WeakAndHeap(100) {}
@@ -96,7 +129,7 @@ SearchIterator::UP create_weak_and() {
     terms.emplace_back(T({1,2,3}).release(), 100, 3);
     terms.emplace_back(T({5,6}).release(), 200, 2);
     terms.emplace_back(T({8}).release(), 300, 1);
-    return WeakAndSearch::create(terms, wand::MatchParams(dummy_heap), wand::Bm25TermFrequencyScorer(num_docs), 100, true, true);
+    return with_id(WeakAndSearch::create(terms, wand::MatchParams(dummy_heap), wand::Bm25TermFrequencyScorer(num_docs), 100, true, true));
 }
 
 void collect(std::map<std::string,size_t> &counts, const auto &node) {
@@ -110,7 +143,7 @@ void collect(std::map<std::string,size_t> &counts, const auto &node) {
     }
     const auto &name = node["name"];
     if (name.valid()) {
-        auto name_str = name.asString().make_string();
+        auto name_str = strip_tag(name.asString().make_stringview());
         counts[name_str] += node["count"].asLong();
     }
 };
@@ -163,25 +196,25 @@ void verify_operation(ExecutionProfiler &profiler, std::map<std::string,size_t> 
 TEST(ProfiledIteratorTest, init_seek_unpack_termwise_is_profiled) {
     ExecutionProfiler profiler(64);
     std::map<std::string,size_t> seen;
-    auto root = ProfiledIterator::profile(profiler, T({1,2,3}));
+    auto root = ProfiledIterator::profile(profiler, create_term({1,2,3}));
     root->initRange(1,4);
-    verify_operation(profiler, seen, "/SimpleSearch/init");
+    verify_operation(profiler, seen, "[1]SimpleSearch::initRange");
     root->seek(2);
-    verify_operation(profiler, seen, "/SimpleSearch/seek");
+    verify_operation(profiler, seen, "[1]SimpleSearch::doSeek");
     root->unpack(2);
-    verify_operation(profiler, seen, "/SimpleSearch/unpack");
+    verify_operation(profiler, seen, "[1]SimpleSearch::doUnpack");
     root->initRange(1,4);
-    verify_operation(profiler, seen, "/SimpleSearch/init");
+    verify_operation(profiler, seen, "[1]SimpleSearch::initRange");
     auto bits = root->get_hits(1);
-    verify_operation(profiler, seen, "/SimpleSearch/termwise");
+    verify_operation(profiler, seen, "[1]SimpleSearch::get_hits");
     root->initRange(1,4);
-    verify_operation(profiler, seen, "/SimpleSearch/init");
+    verify_operation(profiler, seen, "[1]SimpleSearch::initRange");
     root->or_hits_into(*bits, 1);
-    verify_operation(profiler, seen, "/SimpleSearch/termwise");
+    verify_operation(profiler, seen, "[1]SimpleSearch::or_hits_into");
     root->initRange(1,4);
-    verify_operation(profiler, seen, "/SimpleSearch/init");
+    verify_operation(profiler, seen, "[1]SimpleSearch::initRange");
     root->and_hits_into(*bits, 1);
-    verify_operation(profiler, seen, "/SimpleSearch/termwise");
+    verify_operation(profiler, seen, "[1]SimpleSearch::and_hits_into");
 }
 
 TEST(ProfiledIteratorTest, iterator_tree_can_be_profiled) {
@@ -196,13 +229,13 @@ TEST(ProfiledIteratorTest, iterator_tree_can_be_profiled) {
     fprintf(stderr, "%s", slime.toString().c_str());
     auto counts = collect_counts(slime.get());
     print_counts(counts);
-    EXPECT_EQ(counts["/AndSearchStrict/init"], 2);
-    EXPECT_EQ(counts["/0/StrictHeapOrSearch/init"], 2);
-    EXPECT_EQ(counts["/0/0/SimpleSearch/init"], 2);
-    EXPECT_EQ(counts["/0/1/SimpleSearch/init"], 2);
-    EXPECT_EQ(counts["/1/SourceBlenderSearchNonStrict/init"], 2);
-    EXPECT_EQ(counts["/1/0/SimpleSearch/init"], 2);
-    EXPECT_EQ(counts["/1/1/SimpleSearch/init"], 2);
+    EXPECT_EQ(counts["[7]AndSearchStrict::initRange"], 2);
+    EXPECT_EQ(counts["[3]StrictHeapOrSearch::initRange"], 2);
+    EXPECT_EQ(counts["[1]SimpleSearch::initRange"], 2);
+    EXPECT_EQ(counts["[2]SimpleSearch::initRange"], 2);
+    EXPECT_EQ(counts["[6]SourceBlenderSearchNonStrict::initRange"], 2);
+    EXPECT_EQ(counts["[4]SimpleSearch::initRange"], 2);
+    EXPECT_EQ(counts["[5]SimpleSearch::initRange"], 2);
 }
 
 TEST(ProfiledIteratorTest, weak_and_can_be_profiled) {
@@ -216,14 +249,14 @@ TEST(ProfiledIteratorTest, weak_and_can_be_profiled) {
     fprintf(stderr, "%s", slime.toString().c_str());
     auto counts = collect_counts(slime.get());
     print_counts(counts);
-    EXPECT_EQ(counts["/WeakAndSearchLR/init"], 1);
-    EXPECT_EQ(counts["/0/SimpleSearch/init"], 1);
-    EXPECT_EQ(counts["/1/SimpleSearch/init"], 1);
-    EXPECT_EQ(counts["/2/SimpleSearch/init"], 1);
-    EXPECT_EQ(counts["/WeakAndSearchLR/seek"], 7);
-    EXPECT_EQ(counts["/0/SimpleSearch/seek"], 4);
-    EXPECT_EQ(counts["/1/SimpleSearch/seek"], 3);
-    EXPECT_EQ(counts["/2/SimpleSearch/seek"], 2);
+    EXPECT_EQ(counts["[4]WeakAndSearchLR::initRange"], 1);
+    EXPECT_EQ(counts["[1]SimpleSearch::initRange"], 1);
+    EXPECT_EQ(counts["[2]SimpleSearch::initRange"], 1);
+    EXPECT_EQ(counts["[3]SimpleSearch::initRange"], 1);
+    EXPECT_EQ(counts["[4]WeakAndSearchLR::doSeek"], 7);
+    EXPECT_EQ(counts["[1]SimpleSearch::doSeek"], 4);
+    EXPECT_EQ(counts["[2]SimpleSearch::doSeek"], 3);
+    EXPECT_EQ(counts["[3]SimpleSearch::doSeek"], 2);
 }
 
 GTEST_MAIN_RUN_ALL_TESTS()
--- searchlib/src/vespa/searchlib/queryeval/profiled_iterator.h
+++ searchlib/src/vespa/searchlib/queryeval/profiled_iterator.h
@@ -9,15 +9,18 @@ namespace search::queryeval {
 
 /**
  * Wraps a search iterator to profile its operations. Each iterator
- * has 4 distinct operations that will be profiled separately:
+ * has 6 distinct operations that will be profiled separately.
  *
- * 'init' -> initRange
- * 'seek' -> doSeek
- * 'unpack' -> doUnpack
- * 'termwise' -> get_hits, or_hits_into, and_hits_into
+ * The full name of each profiled task will be the id of the iterator (enumeration from blueprint tree)
+ * followed by the type of the iterator.
+ * The suffix will be the actual function name associated with the operation.
  *
- * The full name of each profiled task will be the path down the
- * iterator tree combined with the class name and the operation name.
+ * [id]type::initRange
+ * [id]type::doSeek
+ * [id]type::doUnpack
+ * [id]type::get_hits
+ * [id]type::or_hits_into
+ * [id]type::and_hits_into
  **/
 class ProfiledIterator : public SearchIterator
 {
@@ -25,22 +28,27 @@ private:
     using Profiler = vespalib::ExecutionProfiler;
     Profiler &_profiler;
     std::unique_ptr<SearchIterator> _search;
-    Profiler::TaskId _init_tag;
-    Profiler::TaskId _seek_tag;
-    Profiler::TaskId _unpack_tag;
-    Profiler::TaskId _termwise_tag;
+    Profiler::TaskId _initRange_tag;
+    Profiler::TaskId _doSeek_tag;
+    Profiler::TaskId _doUnpack_tag;
+    Profiler::TaskId _get_hits_tag;
+    Profiler::TaskId _or_hits_into_tag;
+    Profiler::TaskId _and_hits_into_tag;
     struct ctor_tag{};
 public:
     ProfiledIterator(Profiler &profiler,
                      std::unique_ptr<SearchIterator> search,
-                     Profiler::TaskId init_tag,
-                     Profiler::TaskId seek_tag,
-                     Profiler::TaskId unpack_tag,
-                     Profiler::TaskId termwise_tag,
+                     Profiler::TaskId initRange_tag,
+                     Profiler::TaskId doSeek_tag,
+                     Profiler::TaskId doUnpack_tag,
+                     Profiler::TaskId get_hits_tag,
+                     Profiler::TaskId or_hits_into_tag,
+                     Profiler::TaskId and_hits_into_tag,
                      ctor_tag) noexcept
       : _profiler(profiler), _search(std::move(search)),
-        _init_tag(init_tag), _seek_tag(seek_tag),
-        _unpack_tag(unpack_tag), _termwise_tag(termwise_tag) {}
+        _initRange_tag(initRange_tag), _doSeek_tag(doSeek_tag),
+        _doUnpack_tag(doUnpack_tag), _get_hits_tag(get_hits_tag),
+        _or_hits_into_tag(or_hits_into_tag), _and_hits_into_tag(and_hits_into_tag) {}
     void initRange(uint32_t begin_id, uint32_t end_id) override;
     void doSeek(uint32_t docid) override;
     void doUnpack(uint32_t docid) override;
@@ -52,9 +60,7 @@ public:
     Trinary is_strict() const override { return _search->is_strict(); }
     Trinary matches_any() const override { return _search->matches_any(); }
     const PostingInfo *getPostingInfo() const override { return _search->getPostingInfo(); }
-    static std::unique_ptr<SearchIterator> profile(Profiler &profiler,
-                                                   std::unique_ptr<SearchIterator> node,
-                                                   const std::string &path = "/");
+    static std::unique_ptr<SearchIterator> profile(Profiler &profiler, std::unique_ptr<SearchIterator> node);
 };
 
 } // namespace
