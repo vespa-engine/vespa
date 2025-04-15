2244966c5f5760c263bdff14fb80a6175472c3f3
Geir Storli <geirst@vespa.ai>
2025-04-15 13:36:34 +0000

    Rewrite raw buf unit test to gtest.

--- searchlib/src/tests/util/rawbuf_test.cpp
+++ searchlib/src/tests/util/rawbuf_test.cpp
@@ -1,7 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/searchlib/util/rawbuf.h>
-#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <string>
 
 #include <vespa/log/log.h>
@@ -21,40 +21,40 @@ void checkAddNum(void (RawBuf::*addNum)(T, size_t, char), size_t num,
                  size_t fieldw, char fill, const string &expected) {
     RawBuf buf(10);
     (buf.*addNum)(num, fieldw, fill);
-    EXPECT_EQUAL(expected, getString(buf));
+    EXPECT_EQ(expected, getString(buf));
 }
 
-TEST("require that rawbuf can append data of known length") {
+TEST(RawBufTest, require_that_rawbuf_can_append_data_of_known_length) {
     RawBuf buf(10);
     const string data("foo bar baz qux quux");
     buf.append(data.data(), data.size());
-    EXPECT_EQUAL(data, getString(buf));
+    EXPECT_EQ(data, getString(buf));
 }
 
-TEST("require that prealloc makes enough room") {
+TEST(RawBufTest, require_that_prealloc_makes_enough_room) {
     RawBuf buf(10);
     buf.append("foo", 3);
-    EXPECT_EQUAL(7u, buf.GetFreeLen());
+    EXPECT_EQ(7u, buf.GetFreeLen());
     buf.preAlloc(100);
-    EXPECT_EQUAL("foo", getString(buf));
-    EXPECT_LESS_EQUAL(100u, buf.GetFreeLen());
+    EXPECT_EQ("foo", getString(buf));
+    EXPECT_LE(100u, buf.GetFreeLen());
 }
 
-TEST("require that rawbuf can 'putToInet' 64-bit numbers") {
+TEST(RawBufTest, require_that_rawbuf_can_putToInet_64_bit_numbers) {
     RawBuf buf(1);
     buf.Put64ToInet(0x123456789abcdef0ULL);
-    EXPECT_EQUAL(8ul, buf.GetUsedLen());
-    EXPECT_EQUAL(0x12, (int) buf.GetDrainPos()[0] & 0xff);
-    EXPECT_EQUAL(0x34, (int) buf.GetDrainPos()[1] & 0xff);
-    EXPECT_EQUAL(0x56, (int) buf.GetDrainPos()[2] & 0xff);
-    EXPECT_EQUAL(0x78, (int) buf.GetDrainPos()[3] & 0xff);
-    EXPECT_EQUAL(0x9a, (int) buf.GetDrainPos()[4] & 0xff);
-    EXPECT_EQUAL(0xbc, (int) buf.GetDrainPos()[5] & 0xff);
-    EXPECT_EQUAL(0xde, (int) buf.GetDrainPos()[6] & 0xff);
-    EXPECT_EQUAL(0xf0, (int) buf.GetDrainPos()[7] & 0xff);
+    EXPECT_EQ(8ul, buf.GetUsedLen());
+    EXPECT_EQ(0x12, (int) buf.GetDrainPos()[0] & 0xff);
+    EXPECT_EQ(0x34, (int) buf.GetDrainPos()[1] & 0xff);
+    EXPECT_EQ(0x56, (int) buf.GetDrainPos()[2] & 0xff);
+    EXPECT_EQ(0x78, (int) buf.GetDrainPos()[3] & 0xff);
+    EXPECT_EQ(0x9a, (int) buf.GetDrainPos()[4] & 0xff);
+    EXPECT_EQ(0xbc, (int) buf.GetDrainPos()[5] & 0xff);
+    EXPECT_EQ(0xde, (int) buf.GetDrainPos()[6] & 0xff);
+    EXPECT_EQ(0xf0, (int) buf.GetDrainPos()[7] & 0xff);
 }
 
 
 }  // namespace
 
-TEST_MAIN() { TEST_RUN_ALL(); }
+GTEST_MAIN_RUN_ALL_TESTS()
