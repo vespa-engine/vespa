877ef0e37d0b0cd6734f273b19b85d6cf95632a1
Jan Böker <boeker@vespa.ai>
2025-04-15 09:14:54 +0000

    Move test for ProcessMemoryStats over to gtest

--- vespalib/src/tests/util/process_memory_stats/process_memory_stats_test.cpp
+++ vespalib/src/tests/util/process_memory_stats/process_memory_stats_test.cpp
@@ -1,7 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/stllike/asciistream.h>
-#include <vespa/vespalib/testkit/test_kit.h>
 #include <vespa/vespalib/util/process_memory_stats.h>
 #include <vespa/vespalib/util/size_literals.h>
 #include <iostream>
@@ -31,14 +31,14 @@ std::string toString(const ProcessMemoryStats &stats)
 
 }
 
-TEST("Simple stats")
+TEST(ProcessMemoryStatsTest, simple_stats)
 {
     auto lambda = [](ProcessMemoryStats::SamplingStrategy strategy) {
         ProcessMemoryStats stats(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats) << std::endl;
-        EXPECT_LESS(0u, stats.getVirt());
-        EXPECT_LESS(0u, stats.getMappedRss());
-        EXPECT_LESS(0u, stats.getAnonymousRss());
+        EXPECT_LT(0u, stats.getVirt());
+        EXPECT_LT(0u, stats.getMappedRss());
+        EXPECT_LT(0u, stats.getAnonymousRss());
     };
 
     std::cout << "smaps" << std::endl;
@@ -48,7 +48,7 @@ TEST("Simple stats")
     lambda(ProcessMemoryStats::SamplingStrategy::STATM);
 }
 
-TEST("grow anonymous memory")
+TEST(ProcessMemoryStatsTest, grow_anonymous_memory)
 {
     auto lambda = [](ProcessMemoryStats::SamplingStrategy strategy) {
         ProcessMemoryStats stats1(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
@@ -56,11 +56,11 @@ TEST("grow anonymous memory")
         size_t mapLen = 64_Ki;
         void *mapAddr = mmap(nullptr, mapLen, PROT_READ | PROT_WRITE,
                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        EXPECT_NOT_EQUAL(reinterpret_cast<void *>(-1), mapAddr);
+        EXPECT_NE(reinterpret_cast<void *>(-1), mapAddr);
         ProcessMemoryStats stats2(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats2) << std::endl;
-        EXPECT_LESS_EQUAL(stats1.getVirt() + mapLen,
-                          stats2.getVirt());
+        EXPECT_LE(stats1.getVirt() + mapLen,
+                  stats2.getVirt());
         memset(mapAddr, 1, mapLen);
         ProcessMemoryStats stats3(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats3) << std::endl;
@@ -75,7 +75,7 @@ TEST("grow anonymous memory")
     lambda(ProcessMemoryStats::SamplingStrategy::STATM);
 }
 
-TEST("grow mapped memory")
+TEST(ProcessMemoryStatsTest, grow_mapped_memory)
 {
     auto lambda = [](ProcessMemoryStats::SamplingStrategy strategy) {
         std::ofstream of("mapfile");
@@ -84,16 +84,16 @@ TEST("grow mapped memory")
         of.write(&buf[0], buf.size());
         of.close();
         int mapfileFileDescriptor = open("mapfile", O_RDONLY, 0666);
-        EXPECT_LESS_EQUAL(0, mapfileFileDescriptor);
+        EXPECT_LE(0, mapfileFileDescriptor);
         ProcessMemoryStats stats1(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats1) << std::endl;
         void *mapAddr = mmap(nullptr, mapLen, PROT_READ, MAP_SHARED,
                              mapfileFileDescriptor, 0);
-        EXPECT_NOT_EQUAL(reinterpret_cast<void *>(-1), mapAddr);
+        EXPECT_NE(reinterpret_cast<void *>(-1), mapAddr);
         ProcessMemoryStats stats2(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats2) << std::endl;
-        EXPECT_LESS_EQUAL(stats1.getVirt() + mapLen, stats2.getVirt());
-        EXPECT_EQUAL(0, memcmp(mapAddr, &buf[0], mapLen));
+        EXPECT_LE(stats1.getVirt() + mapLen, stats2.getVirt());
+        EXPECT_EQ(0, memcmp(mapAddr, &buf[0], mapLen));
         ProcessMemoryStats stats3(ProcessMemoryStats::create(SIZE_EPSILON, strategy));
         std::cout << toString(stats3) << std::endl;
         // Cannot check that resident grows if swap is enabled and system loaded
@@ -107,7 +107,7 @@ TEST("grow mapped memory")
     lambda(ProcessMemoryStats::SamplingStrategy::STATM);
 }
 
-TEST("order samples")
+TEST(ProcessMemoryStatsTest, order_samples)
 {
     ProcessMemoryStats a(0,0,7);
     ProcessMemoryStats b(0,0,8);
@@ -115,7 +115,7 @@ TEST("order samples")
     EXPECT_FALSE(b < a);
 }
 
-TEST("parseStatm")
+TEST(ProcessMemoryStatsTest, parse_statm)
 {
     // size resident shared text lib data dt
     std::string statm = "3332000 1917762 8060 1 0 2960491 0";
@@ -123,9 +123,9 @@ TEST("parseStatm")
     asciistream stream(view);
 
     ProcessMemoryStats stats = ProcessMemoryStats::parseStatm(stream);
-    EXPECT_EQUAL(3332000 * ProcessMemoryStats::PAGE_SIZE, stats.getVirt());
-    EXPECT_EQUAL((1917762 - 8060) * ProcessMemoryStats::PAGE_SIZE, stats.getAnonymousRss());
-    EXPECT_EQUAL(8060 * ProcessMemoryStats::PAGE_SIZE, stats.getMappedRss());
+    EXPECT_EQ(3332000 * ProcessMemoryStats::PAGE_SIZE, stats.getVirt());
+    EXPECT_EQ((1917762 - 8060) * ProcessMemoryStats::PAGE_SIZE, stats.getAnonymousRss());
+    EXPECT_EQ(8060 * ProcessMemoryStats::PAGE_SIZE, stats.getMappedRss());
 }
 
-TEST_MAIN() { TEST_RUN_ALL(); }
+GTEST_MAIN_RUN_ALL_TESTS()
