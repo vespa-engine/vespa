ad9bd9e20e23e3c895d8f97e3954a68ec912c05d
Tor Egge <Tor.Egge@online.no>
2025-04-25 16:47:52 +0200

    Prepare for handling element gap in streaming search near query node.

--- searchlib/src/vespa/searchlib/query/streaming/near_query_node.h
+++ searchlib/src/vespa/searchlib/query/streaming/near_query_node.h
@@ -11,9 +11,19 @@ namespace search::streaming {
 */
 class NearQueryNode : public AndQueryNode
 {
+    uint32_t                _distance;
+    std::optional<uint32_t> _element_gap;
 protected:
-    template <bool ordered>
-    bool evaluate_helper() const;
+    void update_element_gap() const { } // Noop for now
+    HitKey calc_window_end_pos(const Hit &hit) const {
+        update_element_gap();
+        if (!_element_gap.has_value() || hit.element_length() + _element_gap.value() > hit.position() + _distance) {
+            return { hit.field_id(), hit.element_id(), hit.position() + _distance };
+        } else {
+            return { hit.field_id(), hit.element_id() + 1,
+                     hit.position() + _distance - hit.element_length() - _element_gap.value() };
+        }
+    }
 public:
     NearQueryNode() noexcept : AndQueryNode("NEAR"), _distance(0) { }
     explicit NearQueryNode(const char * opName) noexcept : AndQueryNode(opName), _distance(0) { }
@@ -22,8 +32,8 @@ public:
     size_t distance()          const { return _distance; }
     void visitMembers(vespalib::ObjectVisitor &visitor) const override;
     bool isFlattenable(ParseItem::ItemType) const override { return false; }
-private:
-    size_t _distance;
+    // Currently only for testing
+    void set_element_gap(std::optional<uint32_t> element_gap) noexcept { _element_gap = element_gap; }
 };
 
 }
