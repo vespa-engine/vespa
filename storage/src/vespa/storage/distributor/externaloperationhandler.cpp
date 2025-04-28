871dc810acbb9a02c2fee20aedb98e194533b34c
Harald Musum <musum@vespa.ai>
2025-04-28 09:04:59 +0200

    Point to doc for how to configure max document size

--- storage/src/vespa/storage/distributor/externaloperationhandler.cpp
+++ storage/src/vespa/storage/distributor/externaloperationhandler.cpp
@@ -318,7 +318,8 @@ bool ExternalOperationHandler::message_size_is_above_put_or_update_limit(uint32_
 
 void ExternalOperationHandler::reject_as_oversized_message(api::StorageCommand& cmd) {
     const uint32_t limit = _op_ctx.distributor_config().max_document_operation_message_size_bytes();
-    std::string msg = vespalib::make_string("Message size (%u bytes) exceeds maximum configured limit (%u bytes)",
+    std::string msg = vespalib::make_string("Message size (%u bytes) exceeds maximum configured limit (%u bytes), "
+                                            "see https://docs.vespa.ai/en/reference/services-content.html#max-document-size for how to configure",
                                             cmd.getApproxByteSize(), limit);
     // TODO increment a metric
     bounce_with_result(cmd, api::ReturnCode(api::ReturnCode::REJECTED, std::move(msg)));
