477c7f02ef64284ebad143302ab8b30174cda5ae
Arne Juul <arnej@vespa.ai>
2025-04-29 13:18:12 +0000

    expect new version-string behavior

--- messagebus/src/tests/routing/routing.cpp
+++ messagebus/src/tests/routing/routing.cpp
@@ -629,7 +629,7 @@ RoutingTest::testTrace(TestData &data, const std::vector<string> &expected)
 bool
 RoutingTest::testTrace(const std::vector<string> &expected, const Trace &trace)
 {
-    const string& version = vespalib::Vtag::currentVersion.toString();
+    const string& version = vespalib::Vtag::currentVersion.toAbbreviatedString();
     string actual = trace.toString();
     size_t pos = 0;
     for (uint32_t i = 0; i < expected.size(); ++i) {
