00fcc1a6dfb2e8812efce53de6ecde25282347d3
Tor Egge <Tor.Egge@online.no>
2025-05-06 14:17:01 +0200

    Rewrite eval tensor conformance unit test to gtest.

--- eval/src/tests/tensor/tensor_conformance/tensor_conformance_test.cpp
+++ eval/src/tests/tensor/tensor_conformance/tensor_conformance_test.cpp
@@ -1,6 +1,6 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include <vespa/vespalib/testkit/test_kit.h>
+#include <vespa/vespalib/gtest/gtest.h>
 #include <vespa/vespalib/util/stringfmt.h>
 #include <vespa/vespalib/io/mapped_file_input.h>
 #include <vespa/vespalib/data/slime/slime.h>
@@ -12,10 +12,10 @@ using vespalib::MappedFileInput;
 
 std::string module_build_path("../../../../");
 
-TEST("require that (some) cross-language tensor conformance tests pass with C++ expression evaluation") {
+TEST(TensorConformanceTest, require_that_cross_language_tensor_conformance_tests_pass_with_CXX_expression_evaluation) {
     std::string result_file = "conformance_result.json";
     std::string binary = module_build_path + "src/apps/tensor_conformance/vespa-tensor-conformance";
-    EXPECT_EQUAL(system(fmt("%s generate-some | %s evaluate | %s verify > %s", binary.c_str(), binary.c_str(), binary.c_str(), result_file.c_str()).c_str()), 0);
+    EXPECT_EQ(system(fmt("%s generate-some | %s evaluate | %s verify > %s", binary.c_str(), binary.c_str(), binary.c_str(), result_file.c_str()).c_str()), 0);
     Slime result;
     MappedFileInput input(result_file);
     JsonFormat::decode(input, result);
@@ -25,13 +25,13 @@ TEST("require that (some) cross-language tensor conformance tests pass with C++ 
     int simple_tests = result["stats"]["cpp_simple_value"].asLong();
     int streamed_tests = result["stats"]["cpp_streamed_value"].asLong();
     EXPECT_TRUE(result["fail_cnt"].valid());
-    EXPECT_EQUAL(result["fail_cnt"].asLong(), 0);
+    EXPECT_EQ(result["fail_cnt"].asLong(), 0);
     EXPECT_TRUE(result["ignore_cnt"].valid());
-    EXPECT_EQUAL(result["ignore_cnt"].asLong(), 0);
-    EXPECT_GREATER(num_tests, 1000);
-    EXPECT_EQUAL(prod_tests, num_tests);
-    EXPECT_EQUAL(simple_tests, num_tests);
-    EXPECT_EQUAL(streamed_tests, num_tests);
+    EXPECT_EQ(result["ignore_cnt"].asLong(), 0);
+    EXPECT_GT(num_tests, 1000);
+    EXPECT_EQ(prod_tests, num_tests);
+    EXPECT_EQ(simple_tests, num_tests);
+    EXPECT_EQ(streamed_tests, num_tests);
 }
 
-TEST_MAIN() { TEST_RUN_ALL(); }
+GTEST_MAIN_RUN_ALL_TESTS()
