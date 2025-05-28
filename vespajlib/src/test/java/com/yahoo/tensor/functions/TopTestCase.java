335f5ceeb0c77ecc543f86f7925f863f37e40efa
Arne Juul <arnej@vespa.ai>
2025-05-28 13:00:09 +0000

    add validation when resolving type of Top

--- vespajlib/src/test/java/com/yahoo/tensor/functions/TopTestCase.java
+++ vespajlib/src/test/java/com/yahoo/tensor/functions/TopTestCase.java
@@ -2,10 +2,13 @@
 package com.yahoo.tensor.functions;
 
 import com.yahoo.tensor.Tensor;
+import com.yahoo.tensor.evaluation.MapEvaluationContext;
 import com.yahoo.tensor.evaluation.VariableTensor;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author arnej
@@ -36,19 +39,52 @@ public class TopTestCase {
         var n = Tensor.from(3);
         var arg = Tensor.from("tensor(p{}):{a:7,b:-3,c:12,d:-11,e:-1}");
         var op = new Top<>(new ConstantTensor<>(n), new ConstantTensor<>(arg));
+        var tt = op.type(new MapEvaluationContext<>());
+        assertEquals("tensor(p{})", tt.toString());
         Tensor result = op.evaluate();
         var expect = Tensor.from("tensor(p{}):{a:7,c:12,e:-1}");
         assertEquals(expect, result);
     }
 
     @Test
+    public void testCompactMapped() {
+        var n = Tensor.from(3);
+        var arg = Tensor.from("tensor<bfloat16>(p{}):{a:7,b:-3,c:12,d:-11,e:-1}");
+        var op = new Top<>(new ConstantTensor<>(n), new ConstantTensor<>(arg));
+        var tt = op.type(new MapEvaluationContext<>());
+        assertEquals("tensor<float>(p{})", tt.toString());
+        Tensor result = op.evaluate();
+        var expect = Tensor.from("tensor<float>(p{}):{a:7,c:12,e:-1}");
+        assertEquals(expect, result);
+    }
+
+    @Test
     public void testMixed() {
+        // not really allowed, see check below
         var n = Tensor.from(3);
         var arg = Tensor.from("tensor(a{},b{},x[2]):{a:{b:[1,2],c:[2,4]},d:{e:[3,5],f:[2,1]}}");
         var op = new Top<>(new ConstantTensor<>(n), new ConstantTensor<>(arg));
         Tensor result = op.evaluate();
-        System.err.println("RESULT: " + result);
         var expect = Tensor.from("tensor(a{},b{},x[2]):{a:{c:[0,4]},d:{e:[3,5]}}");
         assertEquals(expect, result);
+    }
+
+    @Test
+    public void testMixedChecked() {
+        var n = Tensor.from(3);
+        var arg = Tensor.from("tensor(a{},b{},x[2]):{a:{b:[1,2],c:[2,4]},d:{e:[3,5],f:[2,1]}}");
+        var op = new Top<>(new ConstantTensor<>(n), new ConstantTensor<>(arg));
+        // confusing and therefore caught when type resolving:
+        var ex = assertThrows(IllegalArgumentException.class, () -> op.type(new MapEvaluationContext<>()));
+        assertTrue(ex.getMessage().contains("should be a sparse tensor"));
+    }
+
+    @Test
+    public void testSwappedArgsChecked() {
+        var n = Tensor.from(3);
+        var arg = Tensor.from("tensor(p{}):{a:7,b:-3,c:12,d:-11,e:-1}");
+        var op = new Top<>(new ConstantTensor<>(arg), new ConstantTensor<>(n));
+        var ex = assertThrows(IllegalArgumentException.class, () -> op.type(new MapEvaluationContext<>()));
+        assertTrue(ex.getMessage().contains("should be a number"));
     }
 }
