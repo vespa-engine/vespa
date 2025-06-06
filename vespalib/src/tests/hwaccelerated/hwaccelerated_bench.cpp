901f8d0fe201196b82f7a8cf56ddaac83f1bba1e
Tor Brede Vekterli <vekterli@vespa.ai>
2025-06-06 14:22:09 +0000

    Extend vectorization benchmarks with dotproduct and popcount
    Also add printing of actual target used for each benchmark.

--- vespalib/src/tests/hwaccelerated/hwaccelerated_bench.cpp
+++ vespalib/src/tests/hwaccelerated/hwaccelerated_bench.cpp
@@ -15,16 +15,16 @@ std::vector<T> createAndFill(size_t sz) {
     return v;
 }
 
-template<typename T>
+template <typename T, typename Fn>
 void
-benchmarkEuclideanDistance(const hwaccelerated::IAccelerated & accel, size_t sz, size_t count) {
+benchmark_fn(Fn f, size_t sz, size_t count) {
     srand(1);
     std::vector<T> a = createAndFill<T>(sz);
     std::vector<T> b = createAndFill<T>(sz);
     steady_time start = steady_clock::now();
     double sumOfSums(0);
     for (size_t j(0); j < count; j++) {
-        double sum = accel.squaredEuclideanDistance(&a[0], &b[0], sz);
+        double sum = f(&a[0], &b[0], sz);
         sumOfSums += sum;
     }
     duration elapsed = steady_clock::now() - start;
@@ -32,13 +32,39 @@ benchmarkEuclideanDistance(const hwaccelerated::IAccelerated & accel, size_t sz,
 }
 
 void
-benchMarkEuclidianDistance(const hwaccelerated::IAccelerated & accelerator, size_t sz, size_t count) {
+benchmark_squared_euclidean_distance(const hwaccelerated::IAccelerated& accelerator, size_t sz, size_t count) {
+    auto euclidean_dist_fn = [&accelerator](const auto* lhs, const auto* rhs, size_t my_sz) {
+        return accelerator.squaredEuclideanDistance(lhs, rhs, my_sz);
+    };
     printf("double : ");
-    benchmarkEuclideanDistance<double>(accelerator, sz, count);
+    benchmark_fn<double>(euclidean_dist_fn, sz, count);
     printf("float  : ");
-    benchmarkEuclideanDistance<float>(accelerator, sz, count);
+    benchmark_fn<float>(euclidean_dist_fn, sz, count);
     printf("int8_t : ");
-    benchmarkEuclideanDistance<int8_t>(accelerator, sz, count);
+    benchmark_fn<int8_t>(euclidean_dist_fn, sz, count);
+}
+
+void
+benchmark_dot_product(const hwaccelerated::IAccelerated& accelerator, size_t sz, size_t count) {
+    auto dot_product_fn = [&accelerator](const auto* lhs, const auto* rhs, size_t my_sz) {
+        return accelerator.dotProduct(lhs, rhs, my_sz);
+    };
+    printf("double : ");
+    benchmark_fn<double>(dot_product_fn, sz, count);
+    printf("float  : ");
+    benchmark_fn<float>(dot_product_fn, sz, count);
+    printf("int8_t : ");
+    benchmark_fn<int8_t>(dot_product_fn, sz, count);
+}
+
+void
+benchmark_popcount(const hwaccelerated::IAccelerated& accelerator, size_t sz, size_t count) {
+    auto popcount_fn = [&accelerator](const auto* lhs, const auto* rhs, size_t my_sz) {
+        (void)rhs; // ... a little bit sneaky
+        return accelerator.populationCount(lhs, my_sz);
+    };
+    printf("uint64_t : ");
+    benchmark_fn<uint64_t>(popcount_fn, sz, count);
 }
 
 int main(int argc, char *argv[]) {
@@ -50,10 +76,26 @@ int main(int argc, char *argv[]) {
     if (argc > 2) {
         count = atol(argv[2]);
     }
+    auto baseline_accel      = hwaccelerated::IAccelerated::create_platform_baseline_accelerator();
+    const auto& native_accel = hwaccelerated::IAccelerated::getAccelerator();
+
     printf("%s %d %d\n", argv[0], length, count);
-    printf("Squared Euclidian Distance - Platform baseline\n");
-    benchMarkEuclidianDistance(*hwaccelerated::IAccelerated::create_platform_baseline_accelerator(), length, count);
-    printf("Squared Euclidian Distance - Optimized for this CPU\n");
-    benchMarkEuclidianDistance(hwaccelerated::IAccelerated::getAccelerator(), length, count);
+    printf("Squared Euclidean Distance - Baseline (%s)\n", baseline_accel->target_name());
+    benchmark_squared_euclidean_distance(*baseline_accel, length, count);
+    printf("Squared Euclidean Distance - Optimized for this CPU (%s)\n", native_accel.target_name());
+    benchmark_squared_euclidean_distance(native_accel, length, count);
+
+    printf("\n");
+    printf("Dot Product - Baseline (%s)\n", baseline_accel->target_name());
+    benchmark_dot_product(*baseline_accel, length, count);
+    printf("Dot Product - Optimized for this CPU (%s)\n", native_accel.target_name());
+    benchmark_dot_product(native_accel, length, count);
+
+    printf("\n");
+    printf("Popcount - Baseline (%s)\n", baseline_accel->target_name());
+    benchmark_popcount(*baseline_accel, length, count);
+    printf("Popcount - Optimized for this CPU (%s)\n", native_accel.target_name());
+    benchmark_popcount(native_accel, length, count);
+
     return 0;
 }
