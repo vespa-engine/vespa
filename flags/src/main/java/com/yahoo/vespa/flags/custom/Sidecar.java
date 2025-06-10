68c99448875ce8cab2133ada3d0030e59a5cfa5b
Gleb Sizov <glebsv@gmail.com>
2025-06-10 10:38:02 +0200

    Updated sidecars flag - added volume mounts, environment variables, and command. Other minor tweaks.

--- flags/src/main/java/com/yahoo/vespa/flags/custom/Sidecar.java
+++ flags/src/main/java/com/yahoo/vespa/flags/custom/Sidecar.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-
+import java.util.stream.Collectors;
 
 /**
  * Defines properties for sidecar flag.
@@ -17,26 +19,35 @@ import java.util.Objects;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Sidecar {
     private final String name;
-    private final SidecarImage image;
+    private final String image;
     private final SidecarQuota quota;
+    private final List<String> volumeMounts;
+    private final Map<String, String> envs;
+    private final List<String> command;
 
     @JsonCreator
     public Sidecar(
             @JsonProperty("name") String name,
-            @JsonProperty("image") SidecarImage image,
-            @JsonProperty("quota") SidecarQuota quota) {
+            @JsonProperty("image") String image,
+            @JsonProperty("quota") SidecarQuota quota,
+            @JsonProperty("volumeMounts") List<String> volumeMounts,
+            @JsonProperty("envs") Map<String, String> envs,
+            @JsonProperty("command") List<String> command) {
         this.name = name;
         this.image = image;
         this.quota = quota;
+        this.volumeMounts = volumeMounts;
+        this.envs = envs;
+        this.command = command;
     }
 
     @JsonGetter("name")
     public String getName() {
         return name;
     }
-    
+
     @JsonGetter("image")
-    public SidecarImage getImage() {
+    public String getImage() {
         return image;
     }
 
@@ -45,20 +56,45 @@ public class Sidecar {
         return quota;
     }
 
+    @JsonGetter("volumeMounts")
+    public List<String> getVolumeMounts() {
+        return volumeMounts;
+    }
+
+    @JsonGetter("envs")
+    public Map<String, String> getEnvs() {
+        return envs;
+    }
+
+    @JsonGetter("command")
+    public List<String> getCommand() {
+        return command;
+    }
+
     @Override
     public String toString() {
-        return "Sidecar{" +
-                "image=" + image.toString() + 
-                ", quota=" + quota.toString() +
-                '}';
+        return "Sidecar{name='%s', image='%s', quota=%s, volumeMounts={%s}, envs=[%s], command=[%s]}"
+                .formatted(
+                        name,
+                        image,
+                        quota,
+                        volumeMounts.stream().map("'%s'"::formatted).collect(Collectors.joining(", ")),
+                        envs.entrySet().stream()
+                                .map(entry -> "%s='%s'".formatted(entry.getKey(), entry.getValue()))
+                                .collect(Collectors.joining(", ")),
+                        command.stream().map("'%s'"::formatted).collect(Collectors.joining(", ")));
     }
-    
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         var that = (Sidecar) o;
-        return Objects.equals(image, that.image) && Objects.equals(quota, that.quota);
+        return Objects.equals(image, that.image)
+                && Objects.equals(quota, that.quota)
+                && Objects.equals(volumeMounts, that.volumeMounts)
+                && Objects.equals(envs, that.envs)
+                && Objects.equals(command, that.command);
     }
 
     @Override
