d092fb776c7d0a922c28c98edb95222bdc8d4232
Håvard Pettersen <3535158+havardpe@users.noreply.github.com>
2025-06-11 11:10:16 +0000

    remove junit test framework

--- searchsummary/src/tests/juniper/latintokenizertest.cpp
+++ searchsummary/src/tests/juniper/latintokenizertest.cpp
@@ -1,9 +1,349 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include "latintokenizertest.h"
 
-int main(int, char**) {
-    LatinTokenizerTest lta;
-    lta.SetStream(&std::cout);
-    lta.Run();
-    return lta.Report();
+#include "latintokenizer.h"
+#include <vespa/vespalib/gtest/gtest.h>
+#include <vespa/vespalib/util/stringfmt.h>
+
+class Mapel_Pucntuation {
+private:
+    /** Member variables. */
+    static bool* _lookup;
+
+public:
+    /** Constructors */
+    Mapel_Pucntuation();
+
+    /** Punctuation predicate. */
+    bool operator()(char c) const { return _lookup[static_cast<unsigned char>(c)]; }
+};
+
+class Maple_Space {
+private:
+    /** Member variables. */
+    static bool* _lookup;
+
+public:
+    /** Constructors */
+    Maple_Space();
+
+    /** Space predicate. */
+    bool operator()(char c) const { return _lookup[static_cast<unsigned char>(c)]; }
+};
+
+bool* Maple_Space::_lookup = NULL;
+bool* Mapel_Pucntuation::_lookup = NULL;
+
+Mapel_Pucntuation::Mapel_Pucntuation() {
+
+    // Initialize lookup table.
+    if (_lookup == NULL) {
+
+        _lookup = new bool[256];
+
+        for (unsigned int i = 0; i < 256; ++i) { _lookup[i] = false; }
+
+        _lookup[static_cast<unsigned char>('.')] = true;
+        _lookup[static_cast<unsigned char>(',')] = true;
+        _lookup[static_cast<unsigned char>(':')] = true;
+        _lookup[static_cast<unsigned char>(';')] = true;
+        _lookup[static_cast<unsigned char>('|')] = true;
+        _lookup[static_cast<unsigned char>('!')] = true;
+        _lookup[static_cast<unsigned char>('?')] = true;
+        _lookup[static_cast<unsigned char>('@')] = true;
+        _lookup[static_cast<unsigned char>('/')] = true;
+        _lookup[static_cast<unsigned char>('(')] = true;
+        _lookup[static_cast<unsigned char>(')')] = true;
+        _lookup[static_cast<unsigned char>('[')] = true;
+        _lookup[static_cast<unsigned char>(']')] = true;
+        _lookup[static_cast<unsigned char>('{')] = true;
+        _lookup[static_cast<unsigned char>('}')] = true;
+        _lookup[static_cast<unsigned char>('<')] = true;
+        _lookup[static_cast<unsigned char>('>')] = true;
+        _lookup[static_cast<unsigned char>('*')] = true;
+        _lookup[static_cast<unsigned char>('=')] = true;
+        _lookup[static_cast<unsigned char>('%')] = true;
+        _lookup[static_cast<unsigned char>('\\')] = true;
+    }
 }
+
+Maple_Space::Maple_Space() {
+
+    // Initialize lookup table.
+    if (_lookup == NULL) {
+
+        _lookup = new bool[256];
+
+        for (unsigned int i = 0; i < 256; ++i) { _lookup[i] = false; }
+
+        _lookup[static_cast<unsigned char>(' ')] = true;
+        _lookup[static_cast<unsigned char>('\n')] = true;
+        _lookup[static_cast<unsigned char>('\t')] = true;
+        _lookup[static_cast<unsigned char>('\r')] = true;
+        _lookup[static_cast<unsigned char>('"')] = true;
+        _lookup[static_cast<unsigned char>('\'')] = true;
+        _lookup[static_cast<unsigned char>('`')] = true;
+        _lookup[static_cast<unsigned char>('_')] = true;
+    }
+}
+
+template <typename IsSeparator, typename IsPunctuation>
+void TestWord(Fast_LatinTokenizer<IsSeparator, IsPunctuation>* lt, const char* correct, bool punct = false) {
+    typename Fast_LatinTokenizer<IsSeparator, IsPunctuation>::Fast_Token token;
+    EXPECT_TRUE(lt->MoreTokens());
+
+    token = lt->GetNextToken();
+    char temp = *token.second;
+    *token.second = '\0';
+    std::string word = vespalib::make_string("%s", token.first);
+    *token.second = temp;
+
+    {
+        SCOPED_TRACE(vespalib::make_string("%s%s == %s", "word: ", word.c_str(), correct).c_str());
+
+        EXPECT_TRUE(word == correct);
+
+        EXPECT_TRUE(token._punctuation == punct);
+
+    }
+}
+
+TEST(LatinTokenizerTest, testSimple) {
+
+    Fast_SimpleLatinTokenizer lt;
+    std::string               s("This is. my . test String.");
+    lt.SetNewText(const_cast<char*>(s.c_str()));
+
+    {
+        SCOPED_TRACE("This");
+        TestWord(&lt, "This");
+    }
+    {
+        SCOPED_TRACE("is");
+        TestWord(&lt, "is");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+    {
+        SCOPED_TRACE("my");
+        TestWord(&lt, "my");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+    {
+        SCOPED_TRACE("test");
+        TestWord(&lt, "test");
+    }
+    {
+        SCOPED_TRACE("String");
+        TestWord(&lt, "String");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+
+    EXPECT_TRUE(!lt.MoreTokens());
+}
+
+TEST(LatinTokenizerTest, testSimpleLength) {
+
+    Fast_SimpleLatinTokenizer lt;
+    std::string               s("This is. my . test String.");
+    lt.SetNewText(const_cast<char*>(s.c_str()), s.length());
+
+    {
+        SCOPED_TRACE("This");
+        TestWord(&lt, "This");
+    }
+    {
+        SCOPED_TRACE("is");
+        TestWord(&lt, "is");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+    {
+        SCOPED_TRACE("my");
+        TestWord(&lt, "my");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+    {
+        SCOPED_TRACE("test");
+        TestWord(&lt, "test");
+    }
+    {
+        SCOPED_TRACE("String");
+        TestWord(&lt, "String");
+    }
+    {
+        SCOPED_TRACE(".");
+        TestWord(&lt, ".", true);
+    }
+
+    EXPECT_TRUE(!lt.MoreTokens());
+}
+
+TEST(LatinTokenizerTest, testEnding) {
+
+    std::string                text("This is   my test String ");
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(const_cast<char*>(text.c_str()));
+
+    TestWord(lt, "This");
+    TestWord(lt, "is");
+    TestWord(lt, "my");
+    TestWord(lt, "test");
+    TestWord(lt, "String");
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    EXPECT_TRUE(text == lt->GetOriginalText());
+
+    delete lt;
+}
+
+TEST(LatinTokenizerTest, testEndingLength) {
+
+    std::string                text("This is   my test String ");
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(const_cast<char*>(text.c_str()), text.length());
+
+    TestWord(lt, "This");
+    TestWord(lt, "is");
+    TestWord(lt, "my");
+    TestWord(lt, "test");
+    TestWord(lt, "String");
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    EXPECT_TRUE(text == std::string(lt->GetOriginalText()));
+
+    delete lt;
+}
+
+TEST(LatinTokenizerTest, testNull) {
+
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(NULL);
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    EXPECT_TRUE(lt->GetOriginalText() == NULL);
+
+    delete lt;
+}
+
+TEST(LatinTokenizerTest, testNullLength) {
+
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(NULL, 0);
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    EXPECT_TRUE(lt->GetOriginalText() == NULL);
+
+    delete lt;
+}
+
+TEST(LatinTokenizerTest, testEmpty) {
+
+    std::string                text(" ");
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(const_cast<char*>(text.c_str()));
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    delete lt;
+}
+
+TEST(LatinTokenizerTest, testEmptyLength) {
+
+    std::string                text(" ");
+    Fast_SimpleLatinTokenizer* lt = new Fast_SimpleLatinTokenizer(const_cast<char*>(text.c_str()), text.length());
+
+    EXPECT_TRUE(!lt->MoreTokens());
+
+    delete lt;
+}
+
+class TPS {
+private:
+    TPS(const TPS&);
+    TPS& operator=(const TPS&);
+
+public:
+    TPS() : _myfunc(NULL) {}
+    void Init(int (*myfunc)(int c)) { _myfunc = myfunc; }
+
+    bool operator()(char c) {
+        //      LatinTokenizerTest::EXPECT_TRUE(_myfunc);
+        return (_myfunc(static_cast<unsigned char>(c)) != 0);
+    }
+
+private:
+    int (*_myfunc)(int c);
+};
+
+TEST(LatinTokenizerTest, testTypeparamObservers) {
+
+    using MyTokenizer = Fast_LatinTokenizer<TPS, TPS>;
+
+    std::string  text("4Some6text");
+    MyTokenizer* tok = new MyTokenizer(const_cast<char*>(text.c_str()));
+    tok->GetIsPunctuation().Init(std::ispunct);
+    tok->GetIsSeparator().Init(std::isdigit);
+
+    TestWord(tok, "Some");
+    TestWord(tok, "text");
+    EXPECT_TRUE(!tok->MoreTokens());
+
+    delete tok;
+}
+
+TEST(LatinTokenizerTest, testMapelURL) {
+
+    using MyTokenizer = Fast_LatinTokenizer<Maple_Space, Mapel_Pucntuation>;
+
+    std::string  text("http://search.msn.co.uk/results.asp?q= cfg=SMCBROWSE rn=1825822 dp=1873075 v=166:");
+    MyTokenizer* tok = new MyTokenizer(const_cast<char*>(text.c_str()));
+
+    TestWord(tok, "http", false);
+    TestWord(tok, ":", true);
+    TestWord(tok, "/", true);
+    TestWord(tok, "/", true);
+    TestWord(tok, "search", false);
+    TestWord(tok, ".", true);
+    TestWord(tok, "msn", false);
+    TestWord(tok, ".", true);
+    TestWord(tok, "co", false);
+    TestWord(tok, ".", true);
+    TestWord(tok, "uk", false);
+    TestWord(tok, "/", true);
+    TestWord(tok, "results", false);
+    TestWord(tok, ".", true);
+    TestWord(tok, "asp", false);
+    TestWord(tok, "?", true);
+    TestWord(tok, "q", false);
+    TestWord(tok, "=", true);
+    TestWord(tok, "cfg", false);
+    TestWord(tok, "=", true);
+    TestWord(tok, "SMCBROWSE", false);
+    TestWord(tok, "rn", false);
+    TestWord(tok, "=", true);
+    TestWord(tok, "1825822", false);
+    TestWord(tok, "dp", false);
+    TestWord(tok, "=", true);
+    TestWord(tok, "1873075", false);
+    TestWord(tok, "v", false);
+    TestWord(tok, "=", true);
+    TestWord(tok, "166", false);
+    TestWord(tok, ":", true);
+    EXPECT_TRUE(!tok->MoreTokens());
+
+    delete tok;
+}
+
+GTEST_MAIN_RUN_ALL_TESTS()
--- searchsummary/src/tests/juniper/matchobjectTest.cpp
+++ searchsummary/src/tests/juniper/matchobjectTest.cpp
@@ -2,26 +2,17 @@
 /*
  * Author: Knut Omang
  */
-#include "matchobjectTest.h"
+
+#include "testenv.h"
 #include "fakerewriter.h"
-
-// Comment out cerr below to ignore unimplemented tests
-#define NOTEST(name)                                                                                              \
-    std::cerr << std::endl                                                                                        \
-              << __FILE__ << ':' << __LINE__ << ": "                                                              \
-              << "No test for method '" << (name) << "'" << std::endl;
-
-/*************************************************************************
- *                      Test methods
- *
- * This section contains boolean methods for testing each public method
- * in the class being tested
- *************************************************************************/
+#include <vespa/vespalib/gtest/gtest.h>
+#include <vespa/vespalib/testkit/test_path.h>
+#include <map>
 
 /**
  * Test of the Term method.
  */
-void MatchObjectTest::testTerm() {
+TEST(MatchObjectTest, testTerm) {
     // Test that two equal keywords are matched properly:
     TestQuery q("NEAR/2(word,PHRASE(near,word))");
 
@@ -30,16 +21,16 @@ void MatchObjectTest::testTerm() {
 
     // Fetch a result descriptor:
     auto res = juniper::Analyse(*juniper::TestConfig, q._qhandle, content, content_len, 0);
-    _test(static_cast<bool>(res));
+    EXPECT_TRUE(static_cast<bool>(res));
 
     // Do the scanning manually. This calls accept several times
     res->Scan();
     Matcher& m = *res->_matcher;
 
-    _test(m.TotalHits() == 3); // 3 occurrences
+    EXPECT_TRUE(m.TotalHits() == 3); // 3 occurrences
     match_candidate_set& ms = m.OrderedMatchSet();
 
-    _test(ms.size() == 2);
+    EXPECT_TRUE(ms.size() == 2);
 
     // printf("%d %d\n", m.TotalHits(),ms.size());
     TestQuery q1("t*t");
@@ -50,36 +41,36 @@ void MatchObjectTest::testTerm() {
     auto      r2 = juniper::Analyse(*juniper::TestConfig, q2._qhandle, content, content_len, 0);
     auto      r3 = juniper::Analyse(*juniper::TestConfig, q3._qhandle, content, content_len, 0);
     auto      r4 = juniper::Analyse(*juniper::TestConfig, q4._qhandle, content, content_len, 0);
-    _test(static_cast<bool>(r1));
+    EXPECT_TRUE(static_cast<bool>(r1));
     if (r1) {
         r1->Scan();
-        _test(r1->_matcher->TotalHits() == 1);
+        EXPECT_TRUE(r1->_matcher->TotalHits() == 1);
     }
-    _test(static_cast<bool>(r2));
+    EXPECT_TRUE(static_cast<bool>(r2));
     if (r2) {
         r2->Scan();
-        _test(r2->_matcher->TotalHits() == 2);
+        EXPECT_TRUE(r2->_matcher->TotalHits() == 2);
     }
 
     if (r3) {
         r3->Scan();
-        _test(r3->_matcher->TotalHits() == 2);
+        EXPECT_TRUE(r3->_matcher->TotalHits() == 2);
     } else {
-        _test(static_cast<bool>(r3));
+        EXPECT_TRUE(static_cast<bool>(r3));
     }
 
     if (r4) {
         r4->Scan();
-        _test_equal(r4->_matcher->TotalHits(), 2);
+        EXPECT_EQ(r4->_matcher->TotalHits(), 2);
     } else {
-        _test(static_cast<bool>(r4));
+        EXPECT_TRUE(static_cast<bool>(r4));
     }
 }
 
 /**
  * Test of the Match method.
  */
-void MatchObjectTest::testMatch() {
+TEST(MatchObjectTest, testMatch) {
     // Check that we hit on the longest match first
     juniper::QueryParser p("AND(junipe,juniper)");
     juniper::QueryHandle qh(p, NULL);
@@ -94,12 +85,12 @@ void MatchObjectTest::testMatch() {
     token.token = ucs4_str;
     token.curlen = 8;
     int idx = mo->Match(mi, token, opts);
-    _test(strcmp(mo->Term(idx)->term(), "juniper") == 0);
+    EXPECT_TRUE(strcmp(mo->Term(idx)->term(), "juniper") == 0);
 
     {
         // This test would loop in v.2.2.2
         TestQuery q("(word,");
-        _test(q._qparser.ParseError());
+        EXPECT_TRUE(q._qparser.ParseError());
     }
 
     {
@@ -129,7 +120,7 @@ void MatchObjectTest::testMatch() {
         juniper::Result   res1(*juniper::TestConfig, qh1, doc.c_str(), doc.size());
         juniper::Summary* sum = res1.GetTeaser(NULL);
         std::string       s(sum->Text());
-        _test_equal(s, "A simple document with an <b>extremelylongwordhit</b> in the middle"
+        EXPECT_EQ(s, "A simple document with an <b>extremelylongwordhit</b> in the middle"
                        " of it that islong enough to allow...triggered "
                        "<b>extremelylongwordhit</b>.A simple document with an "
                        "<b>extremelylongwordhit</b> in the middle of it that islong enough to allow...");
@@ -139,7 +130,7 @@ void MatchObjectTest::testMatch() {
 /**
  * Test matching in annotated buffers
  */
-void MatchObjectTest::testMatchAnnotated() {
+TEST(MatchObjectTest, testMatchAnnotated) {
     const char*       doc = "A big and ugly teaser about "
                             "\xEF\xBF\xB9"
                             "buying"
@@ -153,7 +144,7 @@ void MatchObjectTest::testMatchAnnotated() {
     juniper::Summary* sum = res1.GetTeaser(NULL);
     std::string       s(sum->Text());
 
-    _test_equal(s, "A <b>big</b> and ugly teaser about <b>"
+    EXPECT_EQ(s, "A <b>big</b> and ugly teaser about <b>"
                    "\xEF\xBF\xB9"
                    "buying"
                    "\xEF\xBF\xBA"
@@ -165,14 +156,14 @@ void MatchObjectTest::testMatchAnnotated() {
 /** Test parameter input via options
  */
 
-void MatchObjectTest::testParams() {
+TEST(MatchObjectTest, testParams) {
     {
         TestQuery    q("AND(a,b)", "near.1");
         QueryHandle& qh = q._qhandle;
         std::string  stk;
         qh.MatchObj()->Query()->Dump(stk);
         // Expect l:1 == limit:1 v: Validity check of keywords needed, c: Completeness req'ed
-        _test_equal(stk, "Node<a:2,l:1,v,c>[a:100,b:100]");
+        EXPECT_EQ(stk, "Node<a:2,l:1,v,c>[a:100,b:100]");
     }
 
     {
@@ -182,7 +173,7 @@ void MatchObjectTest::testParams() {
         qh.MatchObj()->Query()->Dump(stk);
         // Expect l:1 == limit:1 o: ordered, v: Validity check of keywords needed,
         //        c: Completeness req'ed
-        _test_equal(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
+        EXPECT_EQ(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
     }
 
     {
@@ -192,7 +183,7 @@ void MatchObjectTest::testParams() {
         qh.MatchObj()->Query()->Dump(stk);
         // Expect l:1 == limit:1 o: ordered, v: Validity check of keywords needed,
         //        c: Completeness req'ed
-        _test_equal(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
+        EXPECT_EQ(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
     }
 
     {
@@ -203,7 +194,7 @@ void MatchObjectTest::testParams() {
         qh.MatchObj()->Query()->Dump(stk);
         // Expect l:1 == limit:1 o: ordered, v: Validity check of keywords needed,
         //        c: Completeness req'ed
-        _test_equal(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
+        EXPECT_EQ(stk, "Node<a:2,o,l:1,v,c>[a:100,b:100]");
     }
 
     {
@@ -214,75 +205,12 @@ void MatchObjectTest::testParams() {
         qh.MatchObj()->Query()->Dump(stk);
         // Expect l:1 == limit:1 o: ordered, v: Validity check of keywords needed,
         //        c: Completeness req'ed
-        _test_equal(stk, "Node<a:2>[a:100,b:100]");
+        EXPECT_EQ(stk, "Node<a:2>[a:100,b:100]");
     }
 }
 
-/*************************************************************************
- *                      Test administration methods
- *************************************************************************/
-
-/**
- * Set up common stuff for all test methods.
- * This method is called immediately before each test method is called
- */
-bool MatchObjectTest::setUp() {
-    return true;
-}
-
-/**
- * Tear down common stuff for all test methods.
- * This method is called immediately after each test method is called
- */
-void MatchObjectTest::tearDown() {}
-
-/**
- * Build up a map with all test methods
- */
-void MatchObjectTest::init() {
-    test_methods_["testTerm"] = &MatchObjectTest::testTerm;
-    test_methods_["testMatch"] = &MatchObjectTest::testMatch;
-    test_methods_["testMatchAnnotated"] = &MatchObjectTest::testMatchAnnotated;
-    test_methods_["testParams"] = &MatchObjectTest::testParams;
-}
-
-/*************************************************************************
- *                         main entry points
- *************************************************************************/
-
-void MatchObjectTest::Run(MethodContainer::iterator& itr) {
-    try {
-        if (setUp()) {
-            (this->*itr->second)();
-            tearDown();
-        }
-    } catch (...) { _fail("Got unknown exception in test method " + itr->first); }
-}
-
-void MatchObjectTest::Run(const char* method) {
-    MethodContainer::iterator pos(test_methods_.find(method));
-    if (pos != test_methods_.end()) {
-        Run(pos);
-    } else {
-        std::cerr << "ERROR: No test method named \"" << method << "\"" << std::endl;
-        _fail("No such method");
-    }
-}
-
-void MatchObjectTest::Run() {
-    for (MethodContainer::iterator itr(test_methods_.begin()); itr != test_methods_.end(); ++itr) Run(itr);
-}
-
-/*
- * Parse runtime arguments before running.
- * If the -m METHOD parameter is given, run only that method
- */
-void MatchObjectTest::Run(int argc, char* argv[]) {
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-m") == 0 && argc > i + 1) {
-            Run(argv[++i]);
-            return;
-        }
-    }
-    Run();
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    juniper::TestEnv te(argc, argv, TEST_PATH("testclient.rc").c_str());
+    return RUN_ALL_TESTS();
 }
