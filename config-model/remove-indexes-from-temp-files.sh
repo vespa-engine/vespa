698fdbfc4ba7e3c54ff827c05de42d11bf400afc
Arne Juul <arnej@vespa.ai>
2025-06-12 11:34:11 +0000

    too many scripts rely on sh==bash, just use that

--- config-model/remove-indexes-from-temp-files.sh
+++ config-model/remove-indexes-from-temp-files.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 perl -pi -e 's{[[][0-9]*[]][.]}{[].}g;s{[[][0-9]*[]] }{[] };chomp;s/$/\n/' temp/*/*.cfg
--- messagebus/src/vespa/messagebus/create-class-h.sh
+++ messagebus/src/vespa/messagebus/create-class-h.sh
@@ -1,6 +1,6 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
-#!/bin/sh
+#!/bin/bash
 
 class=$1
 guard=`echo $class | tr 'a-z' 'A-Z'`
