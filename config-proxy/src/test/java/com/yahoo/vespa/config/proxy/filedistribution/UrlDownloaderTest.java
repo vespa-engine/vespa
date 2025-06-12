fe35cbfed5fb146f30089b840c4889f0e3760b8c
Harald Musum <musum@vespa.ai>
2025-06-12 09:10:38 +0200

    Use apache http client in config proxy
    To be able to use DefaultHttpRequestRetryStrategy, which will retry
    responses with status codes 429 and 503 and retries requests when getting these

--- config-proxy/src/test/java/com/yahoo/vespa/config/proxy/filedistribution/UrlDownloaderTest.java
+++ config-proxy/src/test/java/com/yahoo/vespa/config/proxy/filedistribution/UrlDownloaderTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+import java.net.URI;
 import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -18,8 +18,8 @@ public class UrlDownloaderTest {
 
     @Test
     public void testFileDownload() throws IOException {
-        URL url = new URL("https://docs.vespa.ai/foo");
-        var downloader = new UrlDownloader(url, new DownloadOptions(null));
+        URI uri = URI.create("https://docs.vespa.ai/foo");
+        var downloader = new UrlDownloader(uri, new DownloadOptions(null));
 
         assertFalse(downloader.alreadyDownloaded(temporaryFolder));
 
