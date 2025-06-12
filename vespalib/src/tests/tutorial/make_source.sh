56843213c2a3962777ad0b13208a7133bbc4fdc0
Håvard Pettersen <3535158+havardpe@users.noreply.github.com>
2025-06-12 17:05:23 +0000

    remove tests related to deprecated testing framework

--- vespalib/src/tests/tutorial/make_source.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-filename=$1
-echo "<h2>$filename</h2>"
-echo "<pre class=\"prettyprint linenums\">"
-(cat $filename | ./xml_escape)
-echo "</pre>"
