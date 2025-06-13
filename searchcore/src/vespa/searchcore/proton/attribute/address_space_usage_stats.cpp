016ec1804af7364686e3f8ce02064fa8a39530da
Tor Egge <Tor.Egge@online.no>
2025-06-13 17:05:14 +0200

    Avoid timing dependency when two attribute vectors have same relative address space usage.

--- searchcore/src/vespa/searchcore/proton/attribute/address_space_usage_stats.cpp
+++ searchcore/src/vespa/searchcore/proton/attribute/address_space_usage_stats.cpp
@@ -15,13 +15,37 @@ AddressSpaceUsageStats::AddressSpaceUsageStats(const vespalib::AddressSpace & us
 
 AddressSpaceUsageStats::~AddressSpaceUsageStats() = default;
 
+bool
+AddressSpaceUsageStats::keep(const vespalib::AddressSpace& usage,
+                             const std::string& attributeName,
+                             const std::string& component_name,
+                             const std::string& subDbName)
+{
+    if (_attributeName.empty()) {
+        return true;
+    }
+    // Prefer the highest usage, then lowest subdb name, lowest attribute name, lowest component name
+    auto old_usage = _usage.usage();
+    auto new_usage = usage.usage();
+    if (new_usage != old_usage) {
+        return new_usage > old_usage;
+    }
+    if (subDbName != _subDbName) {
+        return subDbName < _subDbName;
+    }
+    if (attributeName != _attributeName) {
+        return attributeName < _attributeName;
+    }
+    return component_name < _component_name;
+}
+
 void
 AddressSpaceUsageStats::merge(const vespalib::AddressSpace &usage,
                               const std::string &attributeName,
                               const std::string &component_name,
                               const std::string &subDbName)
 {
-    if (attributeName.empty() || usage.usage() > _usage.usage()) {
+    if (keep(usage, attributeName, component_name, subDbName)) {
         _usage = usage;
         _attributeName = attributeName;
         _component_name = component_name;
