ecd8d6a0fb1f31414586915cea8643517ab0bf5b
Arne Juul <arnej@vespa.ai>
2025-06-19 13:41:08 +0000

    use "ctest" not "ctest3"

--- .buildkite/cpp-test.sh
+++ .buildkite/cpp-test.sh
@@ -3,4 +3,4 @@
 
 set -euo pipefail
 
-ctest3 --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
+ctest --output-junit "$LOG_DIR/vespa-cpptest-results.xml" --output-on-failure -j "$NUM_CPU_LIMIT"
