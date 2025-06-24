4512f8202abf8c9417dab3dac5fcd25f9506f6f4
Bjørn Christian Seime <bjorn.christian@seime.no>
2025-06-24 15:46:41 +0200

    feat(gguf-embedder): conditionally enable debug logging from llama.cpp

--- model-integration/src/main/java/ai/vespa/embedding/GgufEmbedder.java
+++ model-integration/src/main/java/ai/vespa/embedding/GgufEmbedder.java
@@ -14,6 +14,7 @@ import de.kherud.llama.args.PoolingType;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -38,7 +39,6 @@ public class GgufEmbedder extends AbstractComponent implements Embedder {
         var modelParams = new ModelParameters()
                 .enableEmbedding()
                 .setParallel(config.parallel())
-                .disableLog()
                 .setModel(modelPath)
                 .setGpuLayers(config.gpuLayers());
         if (config.continuousBatching()) modelParams.enableContBatching();
@@ -50,6 +50,7 @@ public class GgufEmbedder extends AbstractComponent implements Embedder {
         if (config.seed() > -1) modelParams.setSeed(config.seed());
         if (config.threads() != 0) modelParams.setThreads(calculateThreadCount(config.threads()));
         if (config.batchThreads() != 0) modelParams.setThreadsBatch(calculateThreadCount(config.batchThreads()));
+        if (!log.isLoggable(Level.FINE)) modelParams.disableLog();
         model = new LlamaModel(modelParams);
         maxPromptTokens = config.maxPromptTokens();
     }
