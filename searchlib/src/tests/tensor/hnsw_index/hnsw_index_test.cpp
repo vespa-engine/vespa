b8827d3c40909c093aaf1b68329b1065c52dcaf7
Jan Böker <boeker@vespa.ai>
2025-06-27 13:50:10 +0000

    Adapt tests to changes related to adaptive beam search

--- searchlib/src/tests/tensor/hnsw_index/hnsw_index_test.cpp
+++ searchlib/src/tests/tensor/hnsw_index/hnsw_index_test.cpp
@@ -269,12 +269,13 @@ public:
         SCOPED_TRACE(label);
         uint32_t k = 3;
         uint32_t explore_k = 100;
+        double adaptive_beam_search_slack = 0.0;
         std::span<float> qv_ref(qv);
         vespalib::eval::TypedCells qv_cells(qv_ref);
         auto df = index->distance_function_factory().for_query_vector(qv_cells);
         auto got_by_docid = (global_filter->is_active()) ?
-                            index->find_top_k_with_filter(k, *df, *global_filter, false, 0.01, explore_k, _doom->get_doom(), 10000.0) :
-                            index->find_top_k(k, *df, explore_k, _doom->get_doom(), 10000.0);
+                            index->find_top_k_with_filter(k, *df, *global_filter, false, 0.01, explore_k, adaptive_beam_search_slack, _doom->get_doom(), 10000.0) :
+                            index->find_top_k(k, *df, explore_k, adaptive_beam_search_slack, _doom->get_doom(), 10000.0);
         std::vector<uint32_t> act;
         act.reserve(got_by_docid.size());
         for (auto& hit : got_by_docid) {
@@ -286,7 +287,7 @@ public:
         uint32_t k = 3;
         auto qv = vectors.get_vector(docid, 0);
         auto df = index->distance_function_factory().for_query_vector(qv);
-        auto rv = index->top_k_candidates(*df, k, global_filter->ptr_if_active(), filter_first, 0.01, _doom->get_doom()).peek();
+        auto rv = index->top_k_candidates(*df, k, 0.0, global_filter->ptr_if_active(), filter_first, 0.01, _doom->get_doom()).peek();
         std::sort(rv.begin(), rv.end(), LesserDistance());
         size_t idx = 0;
         for (const auto & hit : rv) {
@@ -297,7 +298,7 @@ public:
         if (exp_hits.size() == k) {
             std::vector<uint32_t> expected_by_docid = exp_hits;
             std::sort(expected_by_docid.begin(), expected_by_docid.end());
-            auto got_by_docid = index->find_top_k(k, *df, k, _doom->get_doom(), 100100.25);
+            auto got_by_docid = index->find_top_k(k, *df, k, 0.0, _doom->get_doom(), 100100.25);
             for (idx = 0; idx < k; ++idx) {
                 EXPECT_EQ(expected_by_docid[idx], got_by_docid[idx].docid);
             }
@@ -310,14 +311,14 @@ public:
         auto qv = vectors.get_vector(docid, 0);
         auto df = index->distance_function_factory().for_query_vector(qv);
         uint32_t k = 3;
-        auto rv = index->top_k_candidates(*df, k, global_filter->ptr_if_active(), false, 0.01, _doom->get_doom()).peek();
+        auto rv = index->top_k_candidates(*df, k, 0.0, global_filter->ptr_if_active(), false, 0.01, _doom->get_doom()).peek();
         std::sort(rv.begin(), rv.end(), LesserDistance());
         EXPECT_EQ(rv.size(), 3);
         EXPECT_LE(rv[0].distance, rv[1].distance);
         double thr = (rv[0].distance + rv[1].distance) * 0.5;
         auto got_by_docid = (global_filter->is_active())
-            ? index->find_top_k_with_filter(k, *df, *global_filter, false, 0.01, k, _doom->get_doom(), thr)
-            : index->find_top_k(k, *df, k, _doom->get_doom(), thr);
+            ? index->find_top_k_with_filter(k, *df, *global_filter, false, 0.01, k, 0.0, _doom->get_doom(), thr)
+            : index->find_top_k(k, *df, k, 0.0, _doom->get_doom(), thr);
         EXPECT_EQ(got_by_docid.size(), 1);
         EXPECT_EQ(got_by_docid[0].docid, index->get_docid(rv[0].nodeid));
         for (const auto & hit : got_by_docid) {
