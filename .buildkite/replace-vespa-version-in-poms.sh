0eb8ba3cf50d5f4a5c1af030e9bec1f07848c648
gjoranv <gjoranv@gmail.com>
2025-07-11 15:29:50 +0200

    Misc improvements done while writing tests:
    - Use portable sed syntax to allow testing on mac.
    - Fail with error message if dir does not exist
    - Print a message if dir contains no pom.xml files
    - Use basename in usage message

--- .buildkite/replace-vespa-version-in-poms.sh
+++ .buildkite/replace-vespa-version-in-poms.sh
@@ -4,14 +4,25 @@
 set -euo pipefail
 
 if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <Vespa version> <path>"
+    echo "Usage: $(basename "$0") <Vespa version> <path>"
     exit 1
 fi
 
 readonly VESPA_VERSION=$1
 readonly DIR=$2
 
-find -L "$DIR" -name "pom.xml" -exec sed -i \
+# Fail if DIR does not exist or is not a directory
+if [[ ! -d "$DIR" ]]; then
+    echo "Directory $DIR does not exist or is not a directory."
+    exit 1
+fi
+
+if [[ -z $(find -L "$DIR" -name "pom.xml") ]]; then
+    echo "No pom.xml files found in $DIR"
+    exit 0
+fi
+
+find -L "$DIR" -name "pom.xml" -exec sed -i '' \
      -e "s,<version>.*SNAPSHOT.*</version>,<version>$VESPA_VERSION</version>," \
      -e "s,<vespaversion>.*project.version.*</vespaversion>,<vespaversion>$VESPA_VERSION</vespaversion>," \
      -e "s,<test-framework.version>.*project.version.*</test-framework.version>,<test-framework.version>$VESPA_VERSION</test-framework.version>," \
