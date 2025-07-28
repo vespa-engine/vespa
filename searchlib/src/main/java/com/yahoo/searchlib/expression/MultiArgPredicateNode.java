33e29ac0ac82d26f1f37ed7d3697d4ce8bb1ae34
Johan Solbakken <johsol@vespa.ai>
2025-07-28 06:31:55 +0000

    Add support for `and`, `or` and `not` predicates to filter in grouping
    This commit adds three rules in `filterExp` to the grouping parser.
    
    In Java public API it extends the public interface with `NotPredicate`,
    `AndPredicate` and `OrPredicate`.
    
    And, it extends identifiable with 4 new node types:
    `MultiArgPredicateNode`, `OrPredicateNode`, `AndPredicateNode` and
    `NotPredicateNode` in Java and C++.

---
+++ searchlib/src/main/java/com/yahoo/searchlib/expression/MultiArgPredicateNode.java
@@ -0,0 +1,88 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.searchlib.expression;
+
+import com.yahoo.vespa.objects.Deserializer;
+import com.yahoo.vespa.objects.ObjectVisitor;
+import com.yahoo.vespa.objects.Serializer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Protocol class for descendants of {@code FilterExpressionNode} that has several {@code FilterExpressionNode} as children.
+ *
+ * @author johsol
+ */
+public abstract class MultiArgPredicateNode extends FilterExpressionNode {
+
+    public static final int classId = registerClass(0x4000 + 175, MultiArgPredicateNode.class);
+    private List<FilterExpressionNode> args = new ArrayList<>();
+
+    public MultiArgPredicateNode() {}
+
+    public MultiArgPredicateNode(List<FilterExpressionNode> args) {
+        this.args = args;
+    }
+
+    public final MultiArgPredicateNode addArg(FilterExpressionNode arg) {
+        Objects.requireNonNull(arg, "Can not add null arg");
+        args.add(arg);
+        return this;
+    }
+
+    public FilterExpressionNode getArg(int i) {
+        return args.get(i);
+    }
+
+    public int getNumArgs() {
+        return args.size();
+    }
+
+    public List<FilterExpressionNode> getArgs() {
+        return args;
+    }
+
+    @Override
+    protected int onGetClassId() {
+        return classId;
+    }
+
+    @Override
+    protected void onSerialize(Serializer buf) {
+        int numArgs = args.size();
+        buf.putInt(null, numArgs);
+        for (FilterExpressionNode node : args) {
+            serializeOptional(buf, node);
+        }
+    }
+
+    @Override
+    protected void onDeserialize(Deserializer buf) {
+        args.clear();
+        int numArgs = buf.getInt(null);
+        for (int i = 0; i < numArgs; i++) {
+            FilterExpressionNode node = (FilterExpressionNode)deserializeOptional(buf);
+            args.add(node);
+        }
+    }
+
+    @Override
+    public void visitMembers(ObjectVisitor visitor) {
+        super.visitMembers(visitor);
+        visitor.visit("args", args);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MultiArgPredicateNode rhs = (MultiArgPredicateNode)o;
+        return args.equals(rhs.args);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(args);
+    }
+}
---
+++ searchlib/src/main/java/com/yahoo/searchlib/expression/OrPredicateNode.java
@@ -0,0 +1,60 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.searchlib.expression;
+
+import com.yahoo.vespa.objects.Deserializer;
+import com.yahoo.vespa.objects.ObjectVisitor;
+import com.yahoo.vespa.objects.Serializer;
+
+import java.util.List;
+
+/**
+ * Protocol class for {@code OrPredicate}.
+ *
+ * @author johsol
+ */
+public class OrPredicateNode extends MultiArgPredicateNode {
+
+    public static final int classId = registerClass(0x4000 + 176, OrPredicateNode.class, OrPredicateNode::new);
+
+    public OrPredicateNode() {
+    }
+
+    public OrPredicateNode(List<FilterExpressionNode> args) {
+        super(args);
+    }
+
+    @Override
+    public FilterExpressionNode clone() {
+        return new OrPredicateNode(getArgs().stream().map(FilterExpressionNode::clone).toList());
+    }
+
+    @Override
+    protected int onGetClassId() {
+        return classId;
+    }
+
+    @Override
+    protected void onSerialize(Serializer buf) {
+        super.onSerialize(buf);
+    }
+
+    @Override
+    protected void onDeserialize(Deserializer buf) {
+        super.onDeserialize(buf);
+    }
+
+    @Override
+    public void visitMembers(ObjectVisitor visitor) {
+        super.visitMembers(visitor);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
