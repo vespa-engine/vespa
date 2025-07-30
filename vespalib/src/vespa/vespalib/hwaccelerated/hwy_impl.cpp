023337514ac01e9d55a140076a5af95d96156023
Tor Brede Vekterli <vekterli@vespa.ai>
2025-07-30 14:04:00 +0000

    Use custom kernel for NEON i8 dot product
    Handily beats the "emulated" SDOT operation and slightly
    outperforms the auto-vectorizer since we use fused
    multiply-adds.

--- vespalib/src/vespa/vespalib/hwaccelerated/hwy_impl.cpp
+++ vespalib/src/vespa/vespalib/hwaccelerated/hwy_impl.cpp
@@ -32,18 +32,18 @@ namespace hn = hwy::HWY_NAMESPACE;
 
 template <typename T> //requires (hwy::IsFloat<T>() || hwy::IsSame<T, hwy::bfloat16_t>>())
 HWY_INLINE T
-my_hwy_dot_impl(const T* HWY_RESTRICT a, const T* HWY_RESTRICT b, size_t sz) noexcept {
+my_hwy_dot_impl(const T* HWY_RESTRICT a, const T* HWY_RESTRICT b, const size_t sz) noexcept {
     const hn::ScalableTag<T> d;
     return hwy::ConvertScalarTo<T>(hn::Dot::Compute<0>(d, a, b, sz));
 }
 
 HWY_NOINLINE float
-my_hwy_dot_float(const float* HWY_RESTRICT a, const float* HWY_RESTRICT b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_dot_float(const float* HWY_RESTRICT a, const float* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     return my_hwy_dot_impl(a, b, sz);
 }
 
 HWY_NOINLINE float
-my_hwy_dot_bf16(const BFloat16* HWY_RESTRICT a, const BFloat16* HWY_RESTRICT b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_dot_bf16(const BFloat16* HWY_RESTRICT a, const BFloat16* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     // Highway already comes with dot product kernels supporting BF16, so use these.
     static_assert(sizeof(BFloat16)  == sizeof(hwy::bfloat16_t));
     static_assert(alignof(BFloat16) == alignof(hwy::bfloat16_t));
@@ -54,13 +54,13 @@ my_hwy_dot_bf16(const BFloat16* HWY_RESTRICT a, const BFloat16* HWY_RESTRICT b, 
 }
 
 HWY_NOINLINE double
-my_hwy_dot_double(const double* HWY_RESTRICT a, const double* HWY_RESTRICT b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_dot_double(const double* HWY_RESTRICT a, const double* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     return my_hwy_dot_impl(a, b, sz);
 }
 
 template <typename T> requires (hwy::IsFloat<T>())
 HWY_NOINLINE double
-my_hwy_square_euclidean_distance_unrolled_impl(const T* a, const T* b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_square_euclidean_distance_unrolled_impl(const T* HWY_RESTRICT a, const T* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     const hn::ScalableTag<T> d;
     const auto kernel_fn = [](auto lhs, auto rhs, auto& accu) noexcept {
         const auto sub = hn::Sub(lhs, rhs);
@@ -71,7 +71,10 @@ my_hwy_square_euclidean_distance_unrolled_impl(const T* a, const T* b, size_t sz
 }
 
 HWY_NOINLINE double
-my_hwy_bf16_square_euclidean_distance_unrolled(const BFloat16* a, const BFloat16* b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_bf16_square_euclidean_distance_unrolled(const BFloat16* HWY_RESTRICT a,
+                                               const BFloat16* HWY_RESTRICT b,
+                                               const size_t sz) VESPA_HWY_NOEXCEPT
+{
     static_assert(sizeof(BFloat16)  == sizeof(hwy::bfloat16_t));
     static_assert(alignof(BFloat16) == alignof(hwy::bfloat16_t));
     // We make the assumption that both vespalib::BFloat16 and hwy::bfloat16_t are POD-like
@@ -86,68 +89,54 @@ my_hwy_bf16_square_euclidean_distance_unrolled(const BFloat16* a, const BFloat16
     const hn::Repartition<float, decltype(dbf16)> df;
     // Since we're widening the element type, loading e.g. 8 lanes of BF16 in a single
     // 128-bit vector requires us to process 2 vectors of 4 lanes of float32.
-    auto accu = [df](auto lhs, auto rhs, auto& acc0, auto& acc1) noexcept {
+    auto kernel_fn = [df](auto lhs, auto rhs, auto& acc0, auto& acc1) noexcept {
         const auto sub_lo = hn::Sub(hn::PromoteLowerTo(df, lhs), hn::PromoteLowerTo(df, rhs));
         acc0 = hn::MulAdd(sub_lo, sub_lo, acc0);
         const auto sub_hi = hn::Sub(hn::PromoteUpperTo(df, lhs), hn::PromoteUpperTo(df, rhs));
         acc1 = hn::MulAdd(sub_hi, sub_hi, acc1);
     };
     using MyKernel = HwyReduceKernel<UsesNAccumulators<4>, UnrolledBy<2>, HasAccumulatorArity<2>>;
-    return MyKernel::pairwise(dbf16, df, a_bf16, b_bf16, sz, hn::Zero(df), accu, VecAdd(), LaneReduceSum());
+    return MyKernel::pairwise(dbf16, df, a_bf16, b_bf16, sz, hn::Zero(df), kernel_fn, VecAdd(), LaneReduceSum());
 }
 
 // Important: `sz` should be low enough that the intermediate i32 sum does not overflow!
 HWY_NOINLINE int32_t
-sub_mul_add_i8s_via_i16_to_i32(const int8_t* a, const int8_t* b, size_t sz) {
+sub_mul_add_i8s_via_i16_to_i32(const int8_t* HWY_RESTRICT a, const int8_t* HWY_RESTRICT b, const size_t sz) {
     const hn::ScalableTag<int8_t>                  d8;
     const hn::Repartition<int16_t, decltype(d8)>  d16;
     const hn::Repartition<int32_t, decltype(d16)> d32;
 
-    auto accu = [d16, d32](auto lhs, auto rhs, auto& acc0, auto& acc1, auto& acc2, auto& acc3) noexcept {
+    auto kernel_fn = [d16, d32](auto lhs, auto rhs, auto& acc0, auto& acc1, auto& acc2, auto& acc3) noexcept {
         const auto sub_l_i16 = hn::Sub(hn::PromoteLowerTo(d16, lhs), hn::PromoteLowerTo(d16, rhs));
         const auto sub_u_i16 = hn::Sub(hn::PromoteUpperTo(d16, lhs), hn::PromoteUpperTo(d16, rhs));
         acc0 = hn::ReorderWidenMulAccumulate(d32, sub_l_i16, sub_l_i16, acc0, acc1);
         acc2 = hn::ReorderWidenMulAccumulate(d32, sub_u_i16, sub_u_i16, acc2, acc3);
     };
     using MyKernel = HwyReduceKernel<UsesNAccumulators<8>, UnrolledBy<4>, HasAccumulatorArity<4>>;
-    return MyKernel::pairwise(d8, d32, a, b, sz, hn::Zero(d32), accu, VecAdd(), LaneReduceSum());
+    return MyKernel::pairwise(d8, d32, a, b, sz, hn::Zero(d32), kernel_fn, VecAdd(), LaneReduceSum());
 }
 
 HWY_NOINLINE double
-my_hwy_square_euclidean_distance_i8(const int8_t* a, const int8_t* b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_square_euclidean_distance_i8(const int8_t* HWY_RESTRICT a, const int8_t* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     // If we cannot possibly overflow intermediate i32 accumulators we can directly
     // compute the distance without requiring any chunking. Max chunk size is defined
     // by the number of worst-case sums of -128**2 that can fit into an i32.
     constexpr size_t max_n_per_chunk = INT32_MAX / (INT8_MIN*INT8_MIN);
-    if (sz <= max_n_per_chunk) [[likely]] {
-        return sub_mul_add_i8s_via_i16_to_i32(a, b, sz);
-    }
-    // Process input in chunks that are small enough that the intermediate i32 accumulators
-    // won't overflow, but large enough that we can spin up the vector steam engines fully.
-    // TODO explicitly test this fallback path
-    double sum = 0;
-    size_t i = 0;
-    for (; i + max_n_per_chunk <= sz; i += max_n_per_chunk) {
-        sum += sub_mul_add_i8s_via_i16_to_i32(a + i, b + i, max_n_per_chunk);
-    }
-    if (sz > i) {
-        sum += sub_mul_add_i8s_via_i16_to_i32(a + i, b + i, sz - i);
-    }
-    return sum;
+    return compute_chunked_sum<max_n_per_chunk, double>(sub_mul_add_i8s_via_i16_to_i32, a, b, sz);
 }
 
 HWY_NOINLINE double
-my_hwy_square_euclidean_distance_float_unrolled(const float* a, const float* b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_square_euclidean_distance_float_unrolled(const float* HWY_RESTRICT a, const float* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     return my_hwy_square_euclidean_distance_unrolled_impl(a, b, sz);
 }
 
 HWY_NOINLINE double
-my_hwy_square_euclidean_distance_double_unrolled(const double* a, const double* b, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_square_euclidean_distance_double_unrolled(const double* HWY_RESTRICT a, const double* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     return my_hwy_square_euclidean_distance_unrolled_impl(a, b, sz);
 }
 
 HWY_NOINLINE size_t
-my_hwy_popcount(const uint64_t* a, size_t sz) VESPA_HWY_NOEXCEPT {
+my_hwy_popcount(const uint64_t* a, const size_t sz) VESPA_HWY_NOEXCEPT {
     // TODO have a way to explicitly disable fallbacks for benchmarking purposes
 #if HWY_TARGET != HWY_AVX2 && HWY_TARGET != HWY_AVX3
     const hn::ScalableTag<uint64_t> d;
@@ -163,10 +152,12 @@ my_hwy_popcount(const uint64_t* a, size_t sz) VESPA_HWY_NOEXCEPT {
 #endif
 }
 
+#if HWY_TARGET != HWY_NEON
+
 HWY_NOINLINE int32_t
-mul_add_i8_as_i32(const int8_t* a, const int8_t* b, size_t sz) noexcept {
-    const hn::ScalableTag<int8_t>  d8;
-    const hn::ScalableTag<int32_t> d32;
+mul_add_i8_as_i32(const int8_t* HWY_RESTRICT a, const int8_t* HWY_RESTRICT b, const size_t sz) noexcept {
+    const hn::ScalableTag<int8_t>                 d8;
+    const hn::Repartition<int32_t, decltype(d8)> d32;
     const auto kernel_fn = [d32](auto lhs_i8, auto rhs_i8, auto& accu) noexcept {
         accu = hn::SumOfMulQuadAccumulate(d32, lhs_i8, rhs_i8, accu);
     };
@@ -174,33 +165,46 @@ mul_add_i8_as_i32(const int8_t* a, const int8_t* b, size_t sz) noexcept {
     return MyKernel::pairwise(d8, d32, a, b, sz, hn::Zero(d32), kernel_fn, VecAdd(), LaneReduceSum());
 }
 
+#else
+
+// The `SumOfMulQuadAccumulate` op has suboptimal codegen for the baseline NEON target since
+// it has to "emulate" the SDOT i8*i8 -> i16+i16 -> i32 instruction semantics without having
+// access to any more explicit input/output accumulators, i.e. it can't readily use the
+// `ReorderWidenMulAccumulate` operation since that takes in an output accumulator vector.
+// We work around this by running 4x per-kernel accumulators in parallel and using high/low
+// i8->i16 promotion before doing a fused mul+add of pairwise i16->i32 (this is similar to
+// how we do i8 Euclidean distance computations). This brings the performance slightly beyond
+// what the compiler auto-vectorizer is capable of conjuring up, since it does not seem to
+// generate _fused_ mul+adds.
+HWY_NOINLINE int32_t
+mul_add_i8_as_i32(const int8_t* HWY_RESTRICT a, const int8_t* HWY_RESTRICT b, const size_t sz) noexcept {
+    const hn::ScalableTag<int8_t>                  d8;
+    const hn::Repartition<int16_t, decltype(d8)>  d16;
+    const hn::Repartition<int32_t, decltype(d16)> d32;
+
+    const auto kernel_fn = [d16, d32](auto lhs_i8, auto rhs_i8, auto& acc0, auto& acc1, auto& acc2, auto& acc3) noexcept {
+        const auto lhs_i16_lo = hn::PromoteLowerTo(d16, lhs_i8);
+        const auto lhs_i16_hi = hn::PromoteUpperTo(d16, lhs_i8);
+        const auto rhs_i16_lo = hn::PromoteLowerTo(d16, rhs_i8);
+        const auto rhs_i16_hi = hn::PromoteUpperTo(d16, rhs_i8);
+        acc0 = hn::ReorderWidenMulAccumulate(d32, lhs_i16_lo, rhs_i16_lo, acc0, acc1);
+        acc2 = hn::ReorderWidenMulAccumulate(d32, lhs_i16_hi, rhs_i16_hi, acc2, acc3);
+    };
+
+    using MyKernel = HwyReduceKernel<UsesNAccumulators<8>, UnrolledBy<2>, HasAccumulatorArity<4>>;
+    return MyKernel::pairwise(d8, d32, a, b, sz, hn::Zero(d32), kernel_fn, VecAdd(), LaneReduceSum());
+}
+
+#endif // HWY_NEON
+
 HWY_NOINLINE int64_t
-my_hwy_i8_dot_product(const int8_t* a, const int8_t* b, size_t sz) VESPA_HWY_NOEXCEPT {
-    // TODO have a way to explicitly disable fallbacks for benchmarking purposes
-#if HWY_TARGET != HWY_NEON
+my_hwy_i8_dot_product(const int8_t* HWY_RESTRICT a, const int8_t* HWY_RESTRICT b, const size_t sz) VESPA_HWY_NOEXCEPT {
     // If we cannot possibly overflow intermediate i32 accumulators we can directly
     // compute the dot product without requiring any chunking. Max chunk size is defined
     // by the number of worst-case sums of i8 multiplications (-128**2) that can fit
     // into a single i32 accumulator.
     constexpr size_t max_n_per_chunk = INT32_MAX / (INT8_MIN*INT8_MIN);
-    if (sz <= max_n_per_chunk) [[likely]] {
-        return mul_add_i8_as_i32(a, b, sz);
-    }
-    // TODO explicitly test overflow fallback path
-    int64_t sum = 0;
-    size_t i = 0;
-    for (; i + max_n_per_chunk <= sz; i += max_n_per_chunk) {
-        sum += mul_add_i8_as_i32(a + i, b + i, max_n_per_chunk);
-    }
-    if (sz > i) {
-        sum += mul_add_i8_as_i32(a + i, b + i, sz - i);
-    }
-    return sum;
-#else
-    // The `SumOfMulQuadAccumulate` op seems to have suboptimal codegen for the NEON target
-    // on Highway 1.2.0, so here the auto-vectorizer actually wins out by some margin.
-    return PlatformGenericAccelerator().dotProduct(a, b, sz);
-#endif
+    return compute_chunked_sum<max_n_per_chunk, int64_t>(mul_add_i8_as_i32, a, b, sz);
 }
 
 const char* my_hwy_target_name() VESPA_HWY_NOEXCEPT {
