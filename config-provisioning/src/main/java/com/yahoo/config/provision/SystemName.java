7a39b7476872278b08808b140398e2a5766edf2e
Bjørn Christian Seime <bjorn.christian@seime.no>
2025-08-26 15:17:49 +0200

    fix(config-provisioning): add back `isPublic()` used by older config models

--- config-provisioning/src/main/java/com/yahoo/config/provision/SystemName.java
+++ config-provisioning/src/main/java/com/yahoo/config/provision/SystemName.java
@@ -63,6 +63,9 @@ public enum SystemName {
     /** Whether the system is similar to Public, e.g. PublicCd. */
     public boolean isPublicLike() { return this == Public || this == PublicCd; }
 
+    // TODO Vespa 9: remove. Use isPublicLike() instead.
+    @Deprecated(forRemoval = true, since = "2025-08-26") public boolean isPublic() { return isPublicLike(); }
+
     public boolean isMainLike() { return this == main || this == cd; }
 
     public boolean isKubernetes() { return this == kubernetes; }
