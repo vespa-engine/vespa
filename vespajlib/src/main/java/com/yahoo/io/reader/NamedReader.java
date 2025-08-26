4f683358efb78a19bc35295ff5a4039dcbed4ebc
Jon Bratseth <bratseth@vespa.ai>
2025-08-26 14:40:57 +0200

    Remove duplicate method

--- vespajlib/src/main/java/com/yahoo/io/reader/NamedReader.java
+++ vespajlib/src/main/java/com/yahoo/io/reader/NamedReader.java
@@ -41,7 +41,6 @@ public class NamedReader extends Reader {
     public static Reader of(java.lang.CharSequence charSequence) {
         return new NamedReader("of", new java.io.StringReader(charSequence.toString()));
     }
-    public static Reader nullReader() { return new NamedReader("nullReader", Reader.nullReader()); }
 
     // The rest is reader method implementations which delegates to the wrapped reader
     @Override
