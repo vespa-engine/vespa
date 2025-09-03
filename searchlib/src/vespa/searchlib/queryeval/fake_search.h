2ceab13c7ccde3c6070f0f34e7a5b9dcdc5acf0b
Tor Egge <Tor.Egge@online.no>
2025-09-03 10:57:40 +0200

    Forward get_element_ids and and_element_ids_into from fake search iterator to search context or element id extractor.

--- searchlib/src/vespa/searchlib/queryeval/fake_search.h
+++ searchlib/src/vespa/searchlib/queryeval/fake_search.h
@@ -41,6 +41,8 @@ public:
     }
     const PostingInfo *getPostingInfo() const override { return _result.postingInfo(); }
     void visitMembers(vespalib::ObjectVisitor &visitor) const override;
+    void get_element_ids(uint32_t docid, std::vector<uint32_t>& element_ids) override;
+    void and_element_ids_into(uint32_t docid, std::vector<uint32_t>& element_ids) override;
 };
 
 }
