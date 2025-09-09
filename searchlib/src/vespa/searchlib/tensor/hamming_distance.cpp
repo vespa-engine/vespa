987d499d7794f3540ce804b79ef6c0a3739d280f
Tor Brede Vekterli <vekterli@vespa.ai>
2025-09-09 12:31:31 +0000

    Wire BFloat16 acceleration for distance functions and dot/l2 distance operations
    Take 3, testing effects of increased unrolling of auto-vectorized kernels.

--- searchlib/src/vespa/searchlib/tensor/hamming_distance.cpp
+++ searchlib/src/vespa/searchlib/tensor/hamming_distance.cpp
@@ -49,9 +49,11 @@ public:
 };
 
 template class BoundHammingDistance<TemporaryVectorStore<Int8Float>>;
+template class BoundHammingDistance<TemporaryVectorStore<vespalib::BFloat16>>;
 template class BoundHammingDistance<TemporaryVectorStore<float>>;
 template class BoundHammingDistance<TemporaryVectorStore<double>>;
 template class BoundHammingDistance<ReferenceVectorStore<Int8Float>>;
+template class BoundHammingDistance<ReferenceVectorStore<vespalib::BFloat16>>;
 template class BoundHammingDistance<ReferenceVectorStore<float>>;
 template class BoundHammingDistance<ReferenceVectorStore<double>>;
 
@@ -75,6 +77,7 @@ HammingDistanceFunctionFactory<FloatType>::for_insertion_vector(TypedCells lhs) 
 }
 
 template class HammingDistanceFunctionFactory<Int8Float>;
+template class HammingDistanceFunctionFactory<vespalib::BFloat16>;
 template class HammingDistanceFunctionFactory<float>;
 template class HammingDistanceFunctionFactory<double>;
 
