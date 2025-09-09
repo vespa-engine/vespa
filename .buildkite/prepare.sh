bdf43f93c5a749ad2e80b0d6a754910a493beed9
Arne Juul <arnej@vespa.ai>
2025-09-09 13:54:22 +0000

    avoid relying on obscure bash features; assume python3 is OK already.

--- .buildkite/prepare.sh
+++ .buildkite/prepare.sh
@@ -6,10 +6,14 @@ set -x
 
 "$SOURCE_DIR/.buildkite/replace-vespa-version-in-poms.sh" "$VESPA_VERSION" "$SOURCE_DIR"
 
+cd $SOURCE_DIR
+find . -name "*.java" |
+    awk -F/ '{print $2}' | sort -u > /tmp/list.java-modules.txt
+find . -name "package-info.java" -print0 | xargs -0 grep -HnE "@(com.yahoo.api.annotations.)?PublicApi.*"  |
+    awk -F/ '{print $2}' | sort -u > /tmp/list.public-api-modules.txt
+
 # We disable javadoc for all modules not marked as public API
-for MODULE in $(comm -2 -3 \
-                <(find . -name "*.java" | awk -F/ '{print $2}' | sort -u)
-                <(find . -name "package-info.java" -exec grep -HnE "@(com.yahoo.api.annotations.)?PublicApi.*" {} \; | awk -F/ '{print $2}' | sort -u)); do
+for MODULE in $(comm -2 -3 /tmp/list.java-modules.txt /tmp/list.public-api-modules.txt); do
     mkdir -p "$MODULE/src/main/javadoc"
     echo "No javadoc available for module" > "$MODULE/src/main/javadoc/README"
 done
@@ -17,9 +21,5 @@ done
 mkdir -p "$WORKDIR/artifacts/$ARCH/rpms"
 mkdir -p "$WORKDIR/artifacts/$ARCH/maven-repo"
 
-# TODO(aressem): This should move into the vespaengine/docker-image-build-alma* image
-# Make sure we use the latest python3 version installed and ensure that pip is installed
-# shellcheck shell=bash disable=SC2010
-PYBIN="$(ls /usr/bin/python3* | grep -E "/usr/bin/python3.[0-9]+$" |sort -n -k2 -t.|tail -1)"
-alternatives --set python3 "$PYBIN"
-dnf install -y "$(basename "$PYBIN")"-pip
+# Assume that the latest python3 version installed and pip is installed.
+# Done already in vespaengine/docker-image-build-alma* images.
