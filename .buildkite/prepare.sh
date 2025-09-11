bc1ae8cc6bcda17296171be7450e98cb16469ba9
Arne Juul <arnej@vespa.ai>
2025-09-11 09:58:30 +0000

    remove javadoc-disabling
    * after discussion with various people, we do not
      want to write to the source tree here; we will
      check in the README files instead and add some
      consistency checking later if needed.

--- .buildkite/prepare.sh
+++ .buildkite/prepare.sh
@@ -6,18 +6,6 @@ set -x
 
 "$SOURCE_DIR/.buildkite/replace-vespa-version-in-poms.sh" "$VESPA_VERSION" "$SOURCE_DIR"
 
-cd "${SOURCE_DIR}" || exit 1
-find . -name "*.java" |
-    awk -F/ '{print $2}' | sort -u > /tmp/list.java-modules.txt
-find . -name "package-info.java" -print0 | xargs -0 grep -HnE "@(com.yahoo.api.annotations.)?PublicApi.*"  |
-    awk -F/ '{print $2}' | sort -u > /tmp/list.public-api-modules.txt
-
-# We disable javadoc for all modules not marked as public API
-for MODULE in $(comm -2 -3 /tmp/list.java-modules.txt /tmp/list.public-api-modules.txt); do
-    mkdir -p "$MODULE/src/main/javadoc"
-    echo "No javadoc available for module" > "$MODULE/src/main/javadoc/README"
-done
-
 mkdir -p "$WORKDIR/artifacts/$ARCH/rpms"
 mkdir -p "$WORKDIR/artifacts/$ARCH/maven-repo"
 
