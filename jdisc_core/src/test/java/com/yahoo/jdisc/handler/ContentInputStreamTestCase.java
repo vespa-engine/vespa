32d54a88fd2ae5e8ea216e20da82db4e389b5ab7
Arne Juul <arnej@vespa.ai>
2025-09-26 11:22:06 +0000

    avoid calling finalize() directly

--- jdisc_core/src/test/java/com/yahoo/jdisc/handler/ContentInputStreamTestCase.java
+++ jdisc_core/src/test/java/com/yahoo/jdisc/handler/ContentInputStreamTestCase.java
@@ -4,6 +4,7 @@ package com.yahoo.jdisc.handler;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Future;
+import java.nio.ByteBuffer;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,15 +19,37 @@ public class ContentInputStreamTestCase {
         assertTrue(UnsafeContentInputStream.class.isAssignableFrom(ContentInputStream.class));
     }
 
+    static class Completer implements CompletionHandler {
+        public boolean complete = false;
+        public void completed() { this.complete = true; }
+        public void failed(Throwable t) { throw new IllegalStateException("completer failed: " + t); }
+    }
+
     @Test
-    @SuppressWarnings("FinalizeCalledExplicitly")
     void requireThatFinalizerClosesStream() throws Throwable {
         BufferedContentChannel channel = new BufferedContentChannel();
-        FastContentWriter writer = new FastContentWriter(channel);
-        writer.write("foo");
-        writer.close();
+        byte[] buf = { 102, 111, 111 };
+        channel.write(ByteBuffer.wrap(buf), new Completer());
+        var detector = new Completer();
+        channel.close(detector);
+        makeContentInputStream(channel.toReadable());
+        for (int retry = 0; retry < 1000; retry++) {
+            if (detector.complete) {
+                break;
+            }
+            System.err.println("Waiting for close() completion to be called... retries: " + retry);
+            try {
+                Thread.sleep(10);
+                System.gc();
+            } catch (InterruptedException e) {}
+        }
+        assertTrue(detector.complete);
+        System.err.println("close() completion called");
+    }
 
-        new ContentInputStream(channel.toReadable()).finalize();
+    void makeContentInputStream(ReadableContentChannel channel) {
+        // throw it away to let it be GC'ed
+        new ContentInputStream(channel);
     }
 
 }
