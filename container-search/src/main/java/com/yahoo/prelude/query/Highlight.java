ac03dcba35e62cca9c33e1b3abeb03769dee29b8
Arne Juul <arnej@vespa.ai>
2025-10-07 12:33:01 +0000

    remove some unused imports

--- container-search/src/main/java/com/yahoo/prelude/query/Highlight.java
+++ container-search/src/main/java/com/yahoo/prelude/query/Highlight.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static com.yahoo.language.LinguisticsCase.toLowerCase;
 
--- container-search/src/main/java/com/yahoo/prelude/query/UriItem.java
+++ container-search/src/main/java/com/yahoo/prelude/query/UriItem.java
@@ -3,7 +3,6 @@ package com.yahoo.prelude.query;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
