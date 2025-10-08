9d7e261b29d0cb070a518b262eb85bf66409e2f0
Harald Musum <musum@vespa.ai>
2025-10-08 13:26:32 +0200

    Handle file disappearing when trying to check file size

--- container-core/src/main/java/com/yahoo/container/logging/LogFileHandler.java
+++ container-core/src/main/java/com/yahoo/container/logging/LogFileHandler.java
@@ -365,8 +365,14 @@ class LogFileHandler <LOGTYPE> {
         private void getFileSize(Instant now) {
             if (fileOutput != null) {
                 try {
-                    fileSize = Files.size(Paths.get(fileName));
+                    var path = Paths.get(fileName);
                     lastFileSizeCheck = now;
+                    // Don't try to get size if file does not exist (file was compressed or deleted manually)
+                    if (path.toFile().exists()) {
+                        fileSize = Files.size(path);
+                    } else {
+                        fileSize = 0;
+                    }
                 } catch (IOException e) {
                     logger.log(Level.WARNING, "Failed to get log file size: " + Exceptions.toMessageString(e), e);
                 }
