a3837538ef6008d4961f6b22508d5ac05c0388ba
Tor Egge <Tor.Egge@online.no>
2025-10-09 16:38:35 +0200

    Verify that scope of hidden terms is limited.

--- searchlib/src/tests/query/streaming/query_builder_test.cpp
+++ searchlib/src/tests/query/streaming/query_builder_test.cpp
@@ -11,7 +11,6 @@ using search::query::QueryBuilder;
 using search::query::SimpleQueryNodeTypes;
 using search::query::StackDumpCreator;
 using search::query::Weight;
-using search::streaming::AndNotQueryNode;
 using search::streaming::Query;
 using search::streaming::QueryNodeResultFactory;
 using search::streaming::QueryTermList;
@@ -19,22 +18,21 @@ using search::streaming::QueryTermList;
 TEST(StreamingQueryBuilderTest, hidden_terms_are_not_ranked)
 {
     QueryBuilder<SimpleQueryNodeTypes> builder;
+    builder.addAnd(2);
     builder.addAndNot(2);
-    {
-        builder.addStringTerm("a", "", 0, Weight(0));
-        builder.addStringTerm("b", "", 0, Weight(0));
-    }
+    builder.addStringTerm("a", "", 0, Weight(0));
+    builder.addStringTerm("b", "", 0, Weight(0));
+    builder.addStringTerm("c", "", 0, Weight(0));
     auto node =  builder.build();
     std::string stackDump = StackDumpCreator::create(*node);
     QueryNodeResultFactory empty;
     Query q(empty, stackDump);
-    auto* p = dynamic_cast<AndNotQueryNode*>(&q.getRoot());
-    ASSERT_NE(nullptr, p);
     QueryTermList terms;
-    p->getLeaves(terms);
-    EXPECT_EQ(2, terms.size());
+    q.getRoot().getLeaves(terms);
+    EXPECT_EQ(3, terms.size());
     EXPECT_TRUE(terms[0]->isRanked());
     EXPECT_FALSE(terms[1]->isRanked());
+    EXPECT_TRUE(terms[2]->isRanked());
 }
 
 GTEST_MAIN_RUN_ALL_TESTS()
