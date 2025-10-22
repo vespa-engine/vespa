fec6eab00b092284d0cc1bfd18ca88e87e9d3419
Jon Bratseth <bratseth@gmail.com>
2025-10-22 11:38:50 +0200

    Quote correctly

--- config-model/src/main/java/com/yahoo/schema/expressiontransforms/OnnxModelTransformer.java
+++ config-model/src/main/java/com/yahoo/schema/expressiontransforms/OnnxModelTransformer.java
@@ -131,7 +131,7 @@ public class OnnxModelTransformer extends ExpressionTransformer<RankProfileTrans
 
     public static String asString(ExpressionNode node) {
         if ( ! (node instanceof ConstantNode))
-            throw new IllegalArgumentException("Expected a constant string as argument, but got '" + node);
+            throw new IllegalArgumentException("Expected a constant string as argument, but got '" + node + "'");
         return stripQuotes(node.toString());
     }
 
