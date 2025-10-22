fbd7cfeef0f6fa7d4084bb5e4824f57e5d32e70e
Harald Musum <musum@vespa.ai>
2025-10-22 12:27:59 +0200

    Use zookeeper 3.9.4 by default

--- flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
+++ flags/src/main/java/com/yahoo/vespa/flags/PermanentFlags.java
@@ -194,7 +194,7 @@ public class PermanentFlags {
             HOSTNAME, NODE_TYPE, TENANT_ID, APPLICATION, INSTANCE_ID, CLUSTER_TYPE, CLUSTER_ID, VESPA_VERSION);
 
     public static final UnboundStringFlag ZOOKEEPER_SERVER_VERSION = defineStringFlag(
-            "zookeeper-server-version", "3.9.3",
+            "zookeeper-server-version", "3.9.4",
             "ZooKeeper server version, a jar file zookeeper-server-<ZOOKEEPER_SERVER_VERSION>-jar-with-dependencies.jar must exist",
             "Takes effect on restart of Docker container",
             NODE_TYPE, INSTANCE_ID, HOSTNAME);
