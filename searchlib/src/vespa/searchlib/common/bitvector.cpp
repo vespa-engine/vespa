9bd29fd62990f1ba8853e2bec3476bdb7b95274a
Tor Egge <Tor.Egge@online.no>
2025-11-08 16:41:14 +0100

    Remove unneeded clear of old guard bit when growing bitvector inplace.

--- searchlib/src/vespa/searchlib/common/bitvector.cpp
+++ searchlib/src/vespa/searchlib/common/bitvector.cpp
@@ -179,10 +179,6 @@ BitVector::setSize(Index sz)
 {
     set_bit_no_range_check(sz);  // Need to place the new stop sign first
     std::atomic_thread_fence(std::memory_order_release);
-    if (sz > _sz) {
-        // Can only remove the old stopsign if it is ahead of the new.
-        clear_bit_no_range_check(_sz);
-    }
     vespalib::atomic::store_ref_release(_sz, sz);
 }
 
