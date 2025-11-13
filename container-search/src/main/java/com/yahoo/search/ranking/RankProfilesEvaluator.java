dd77bdec878c4a794c4ac9ecc67c1a9a8c7eaa9b
Bjørn Christian Seime <bjorn.christian@seime.no>
2025-11-13 16:21:59 +0100

    fix(container-search): use configured ONNX runtime in global phase
    The global phase ranker previously used a test instance of the ONNX runtime.
    Use instance from DI to get correct configuration and Triton suppport.

--- container-search/src/main/java/com/yahoo/search/ranking/RankProfilesEvaluator.java
+++ container-search/src/main/java/com/yahoo/search/ranking/RankProfilesEvaluator.java
@@ -2,10 +2,9 @@
 
 package com.yahoo.search.ranking;
 
-import ai.vespa.models.evaluation.FunctionEvaluator;
+import ai.vespa.modelintegration.evaluator.OnnxRuntime;
 import ai.vespa.models.evaluation.Model;
 import ai.vespa.models.evaluation.ModelsEvaluator;
-import com.yahoo.api.annotations.Beta;
 import com.yahoo.component.AbstractComponent;
 import com.yahoo.component.annotation.Inject;
 import com.yahoo.filedistribution.fileacquirer.FileAcquirer;
@@ -14,25 +13,17 @@ import com.yahoo.vespa.config.search.core.OnnxModelsConfig;
 import com.yahoo.vespa.config.search.core.RankingConstantsConfig;
 import com.yahoo.vespa.config.search.core.RankingExpressionsConfig;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.function.Supplier;
-import java.util.logging.Logger;
 
 /**
  * proxy for model-evaluation components
  * @author arnej
  */
-@Beta
 public class RankProfilesEvaluator extends AbstractComponent {
 
     private final ModelsEvaluator evaluator;
-    private static final Logger logger = Logger.getLogger(RankProfilesEvaluator.class.getName());
 
     @Inject
     public RankProfilesEvaluator(
@@ -40,14 +31,16 @@ public class RankProfilesEvaluator extends AbstractComponent {
             RankingConstantsConfig constantsConfig,
             RankingExpressionsConfig expressionsConfig,
             OnnxModelsConfig onnxModelsConfig,
-            FileAcquirer fileAcquirer)
+            FileAcquirer fileAcquirer,
+            OnnxRuntime onnxRuntime)
     {
         this.evaluator = new ModelsEvaluator(
                 rankProfilesConfig,
                 constantsConfig,
                 expressionsConfig,
                 onnxModelsConfig,
-                fileAcquirer);
+                fileAcquirer,
+                onnxRuntime);
         extractGlobalPhaseData(rankProfilesConfig);
     }
 
@@ -59,11 +52,7 @@ public class RankProfilesEvaluator extends AbstractComponent {
         return m;
     }
 
-    public FunctionEvaluator evaluatorForFunction(String rankProfile, String functionName) {
-        return modelForRankProfile(rankProfile).evaluatorOf(functionName);
-    }
-
-    private Map<String, GlobalPhaseSetup> profilesWithGlobalPhase = new HashMap<>();
+    private final Map<String, GlobalPhaseSetup> profilesWithGlobalPhase = new HashMap<>();
 
     Optional<GlobalPhaseSetup> getGlobalPhaseSetup(String rankProfile) {
         return Optional.ofNullable(profilesWithGlobalPhase.get(rankProfile));
