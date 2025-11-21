6f5d8ccb43e8b073692361aaeb2e80f6394c4f30
Harald Musum <musum@vespa.ai>
2025-11-21 15:19:07 +0100

    Show output from command if unable to find Java version
    Also split out a method and add some unit testing

--- client/go/internal/admin/jvm/application_container_test.go
+++ client/go/internal/admin/jvm/application_container_test.go
@@ -13,3 +13,15 @@ func TestMD5SimpleInputs(t *testing.T) {
 	assert.Equal(t, "d41d8cd98f00b204e9800998ecf8427e", md5Hex(""))
 	assert.Equal(t, "4044e8209f286312a68bbb54f8714922", md5Hex("admin/cluster-controllers/0\n"))
 }
+
+func TestParseJavaVersion(t *testing.T) {
+	majorVersion := parseJavaVersion(`openjdk version "17.0.16" 2025-10-21
+                        OpenJDK Runtime Environment Homebrew (build 17.0.16+0)
+                        OpenJDK 64-Bit Server VM Homebrew (build 17.0.16+0, mixed mode, sharing)`)
+	assert.Equal(t, 17, majorVersion)
+
+	majorVersion_for_1_8 := parseJavaVersion(`openjdk version "1.8.16" 2015-10-21
+                            OpenJDK Runtime Environment Homebrew (build 1.8.16+0)
+                            OpenJDK 64-Bit Server VM Homebrew (build 1.8.16+0, mixed mode, sharing)`)
+	assert.Equal(t, 8, majorVersion_for_1_8)
+}
