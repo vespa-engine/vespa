6fdb80aa8606c94e2e3ed83cbd123fcf1881787c
Johan Solbakken <johsol@vespa.ai>
2025-12-02 15:27:43 +0000

    Add InspectorDataSource

---
+++ vespajlib/src/main/java/com/yahoo/data/access/InspectorDataSource.java
@@ -0,0 +1,62 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.data.access;
+
+import com.yahoo.data.disclosure.DataSink;
+import com.yahoo.data.disclosure.DataSource;
+
+/**
+ * Walks a tree of objects implementing {@link Inspector} and emits to a {@link DataSink}.
+ *
+ * @author johsol
+ */
+public class InspectorDataSource implements DataSource {
+
+    private final Inspector inspector;
+
+    public InspectorDataSource(Inspector inspector) {
+        this.inspector = inspector;
+    }
+
+    @Override
+    public void emit(DataSink sink) {
+        walk(sink, inspector);
+    }
+
+    private void walk(DataSink sink, Inspector inspector) {
+        switch (inspector.type()) {
+            case EMPTY -> {
+                sink.emptyValue();
+            }
+            case BOOL -> {
+                sink.booleanValue(inspector.asBool());
+            }
+            case LONG -> {
+                sink.longValue(inspector.asLong());
+            }
+            case DOUBLE -> {
+                sink.doubleValue(inspector.asDouble());
+            }
+            case STRING -> {
+                sink.stringValue(inspector.asString());
+            }
+            case DATA -> {
+                sink.dataValue(inspector.asData());
+            }
+            case ARRAY -> {
+                sink.startArray();
+                for (int i = 0; i < inspector.entryCount(); i++) {
+                    walk(sink, inspector.entry(i));
+                }
+                sink.endArray();
+            }
+            case OBJECT -> {
+                sink.startObject();
+                for (var field : inspector.fields()) {
+                    sink.fieldName(field.getKey());
+                    walk(sink, field.getValue());
+                }
+                sink.endObject();
+            }
+        }
+    }
+}
