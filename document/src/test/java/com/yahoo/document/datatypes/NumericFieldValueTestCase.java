f1d8fad9f37d96302e3590d358237f4aebdb9fbb
Johan Solbakken <johsol@vespa.ai>
2025-12-02 13:40:59 +0000

    Test numeric field value derivatives emit correctly

--- document/src/test/java/com/yahoo/document/datatypes/NumericFieldValueTestCase.java
+++ document/src/test/java/com/yahoo/document/datatypes/NumericFieldValueTestCase.java
@@ -1,13 +1,24 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.yahoo.document.datatypes;
 
+import com.yahoo.data.disclosure.slime.SlimeDataSink;
+import com.yahoo.slime.Slime;
+import com.yahoo.slime.SlimeUtils;
 import org.junit.Test;
+
+import static com.yahoo.test.JunitCompat.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 /**
  * @author baldersheim
  */
 public class NumericFieldValueTestCase {
+
+    private void assertSlime(Slime expected, Slime actual) {
+        assertTrue("Expected " + SlimeUtils.toJson(expected) + " but got " + SlimeUtils.toJson(actual),
+                expected.get().equalTo(actual.get()));
+    }
+
     @Test
     public void requireThatConstructionFromStringWorks() {
         assertEquals(67, new IntegerFieldValue("67").getInteger());
@@ -15,5 +26,15 @@ public class NumericFieldValueTestCase {
         assertEquals(67, new ByteFieldValue("67").getByte());
         assertEquals(67.45, new FloatFieldValue("67.45").getFloat(),    0.00001);
         assertEquals(67.45, new DoubleFieldValue("67.45").getDouble(), 0.000001);
+    }
+
+    @Test
+    public void testNumericFieldValueEmitsCorrectly() {
+        assertSlime(SlimeUtils.jsonToSlime("67"), SlimeDataSink.buildSlime(new IntegerFieldValue("67")));
+        assertSlime(SlimeUtils.jsonToSlime("67"), SlimeDataSink.buildSlime(new LongFieldValue("67")));
+        assertSlime(SlimeUtils.jsonToSlime("67"), SlimeDataSink.buildSlime(new ByteFieldValue("67")));
+        assertSlime(SlimeUtils.jsonToSlime("8.5"), SlimeDataSink.buildSlime(new Float16FieldValue("8.5")));
+        assertSlime(SlimeUtils.jsonToSlime("8.5"), SlimeDataSink.buildSlime(new FloatFieldValue("8.5")));
+        assertSlime(SlimeUtils.jsonToSlime("8.5"), SlimeDataSink.buildSlime(new DoubleFieldValue("8.5")));
     }
 }
