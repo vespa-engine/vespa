db4e765322f299b50ed1d0a9e60b30244a6bf7b9
Arne Juul <arnej@vespa.ai>
2026-02-04 12:54:12 +0000

    add missing includes

--- config/src/vespa/config/subscription/confighandle.hpp
+++ config/src/vespa/config/subscription/confighandle.hpp
@@ -1,6 +1,7 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include "confighandle.h"
+#include "configsubscription.h"
 #include <vespa/config/common/configvalue.hpp>
 
 namespace config {
