0f46da2c5e2ec2c04576e252585a07cdc9455d5a
Jan Böker <boeker@vespa.ai>
2026-02-10 12:58:53 +0000

    Wire in element_filter from protobuf into sameElement node

--- searchcore/src/vespa/searchcore/proton/matching/blueprintbuilder.cpp
+++ searchcore/src/vespa/searchcore/proton/matching/blueprintbuilder.cpp
@@ -129,6 +129,9 @@ private:
         if (n.numFields() == 1) {
             auto se = std::make_unique<SameElementBlueprint>(n.field(0).fieldSpec(),
                                                              n.descendants_index_handles, n.is_expensive());
+            if (n.get_element_filter().size() > 0) {
+                se->addChild(std::make_unique<IndexingBlueprint>(n.get_element_filter()[0]));
+            }
             for (auto* node : n.getChildren()) {
                 se->addChild(build(_requestContext, *node, _context, _global_layout));
             }
