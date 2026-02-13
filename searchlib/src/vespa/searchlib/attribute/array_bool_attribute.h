7a8dab4fbf8051602283e2b1bc12c6fd98a53038
Håvard Pettersen <3535158+havardpe@users.noreply.github.com>
2026-02-13 11:48:42 +0000

    inherit directly from attribute vector
    also extend and clean up unit tests somewhat

--- searchlib/src/vespa/searchlib/attribute/array_bool_attribute.h
+++ searchlib/src/vespa/searchlib/attribute/array_bool_attribute.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "not_implemented_attribute.h"
+#include "attributevector.h"
 #include "raw_buffer_store.h"
 #include <vespa/searchcommon/attribute/i_multi_value_attribute.h>
 #include <vespa/vespalib/util/rcuvector.h>
@@ -20,7 +20,7 @@ namespace search::attribute {
  * Values are set directly per document (no change vector), similar to
  * SingleRawAttribute and tensor attributes.
  */
-class ArrayBoolAttribute : public NotImplementedAttribute,
+class ArrayBoolAttribute : public AttributeVector,
                            public IMultiValueAttribute
 {
     using AtomicEntryRef = vespalib::datastore::AtomicEntryRef;
@@ -58,12 +58,22 @@ public:
     uint32_t getValueCount(DocId doc) const override;
     largeint_t getInt(DocId doc) const override;
     double getFloat(DocId doc) const override;
+    std::span<const char> get_raw(DocId doc) const override;
     uint32_t get(DocId doc, largeint_t* v, uint32_t sz) const override;
     uint32_t get(DocId doc, double* v, uint32_t sz) const override;
+    uint32_t get(DocId doc, std::string* v, uint32_t sz) const override;
+    uint32_t get(DocId doc, const char** v, uint32_t sz) const override;
     uint32_t get(DocId doc, EnumHandle* e, uint32_t sz) const override;
     uint32_t get(DocId doc, WeightedInt* v, uint32_t sz) const override;
     uint32_t get(DocId doc, WeightedFloat* v, uint32_t sz) const override;
+    uint32_t get(DocId doc, WeightedString* v, uint32_t sz) const override;
+    uint32_t get(DocId doc, WeightedConstChar* v, uint32_t sz) const override;
     uint32_t get(DocId doc, WeightedEnum* v, uint32_t sz) const override;
+    uint32_t getEnum(DocId doc) const override;
+    bool is_sortable() const noexcept override;
+    std::unique_ptr<attribute::ISortBlobWriter> make_sort_blob_writer(bool ascending, const common::BlobConverter* converter,
+                                                                      common::sortspec::MissingPolicy policy,
+                                                                      std::string_view missing_value) const override;
 
     // Search
     std::unique_ptr<SearchContext>
