170f7d6bb33a87339e92cc20ace3376086ef094c
Johan Solbakken <johan.hvaler@gmail.com>
2026-02-20 10:18:42 +0000

    Use ELEMENT_FILTER from parser

--- container-search/src/main/java/com/yahoo/search/yql/VespaSerializer.java
+++ container-search/src/main/java/com/yahoo/search/yql/VespaSerializer.java
@@ -13,6 +13,7 @@ import static com.yahoo.search.yql.YqlParser.CONNECTION_WEIGHT;
 import static com.yahoo.search.yql.YqlParser.CONNECTIVITY;
 import static com.yahoo.search.yql.YqlParser.DISTANCE;
 import static com.yahoo.search.yql.YqlParser.DOT_PRODUCT;
+import static com.yahoo.search.yql.YqlParser.ELEMENT_FILTER;
 import static com.yahoo.search.yql.YqlParser.END_ANCHOR;
 import static com.yahoo.search.yql.YqlParser.EQUIV;
 import static com.yahoo.search.yql.YqlParser.FILTER;
@@ -684,7 +685,9 @@ public class VespaSerializer {
             boolean hasFilter = !item.getElementFilter().isEmpty();
             if (hasFilter) {
                 // NOTE(johsol): sameElement with annotation must be wrapped in parens
-                destination.append("({elementFilter:[");
+                destination.append("({");
+                destination.append(ELEMENT_FILTER);
+                destination.append(":[");
                 List<Integer> filter = item.getElementFilter();
                 for (int i = 0; i < filter.size(); i++) {
                     if (i > 0) destination.append(", ");
