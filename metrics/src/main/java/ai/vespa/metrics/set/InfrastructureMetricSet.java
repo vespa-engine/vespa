1bc5751028206a0233fddb4713d182a06caf0eb1
Sebastian August Berg <sebastian-august@hotmail.com>
2026-03-05 15:21:27 +0100

    max sum and count for s3 metrics

--- metrics/src/main/java/ai/vespa/metrics/set/InfrastructureMetricSet.java
+++ metrics/src/main/java/ai/vespa/metrics/set/InfrastructureMetricSet.java
@@ -222,12 +222,12 @@ public class InfrastructureMetricSet {
         addMetric(metrics, ControllerMetrics.ATLASSIAN_ASSETS.max());
         addMetric(metrics, ControllerMetrics.ATLASSIAN_SERVICEDESK_CUSTOMERS.max());
 
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_LEASED_CONCURRENCY.last());
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_AVAILABLE_CONCURRENCY.last());
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_MAX_CONCURRENCY.last());
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_PENDING_CONCURRENCY_ACQUIRES.last());
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_CONCURRENCY_ACQUIRE_DURATION.max());
-        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_OPEN_STREAMS.last());
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_LEASED_CONCURRENCY, EnumSet.of(max, sum, count));
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_AVAILABLE_CONCURRENCY, EnumSet.of(max, sum, count));
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_MAX_CONCURRENCY, EnumSet.of(max, sum, count));
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_PENDING_CONCURRENCY_ACQUIRES, EnumSet.of(max, sum, count));
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_CONCURRENCY_ACQUIRE_DURATION, EnumSet.of(max, sum, count));
+        addMetric(metrics, ControllerMetrics.AWS_S3_POOL_OPEN_STREAMS, EnumSet.of(max, sum, count));
 
         return metrics;
     }
