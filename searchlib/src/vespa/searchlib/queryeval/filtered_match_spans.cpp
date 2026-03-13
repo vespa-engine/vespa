63e7517365714a21c3f45ca16d20fcf3620d1712
Tor Egge <Tor.Egge@online.no>
2026-03-13 13:35:42 +0100

    Add unit test for filtered match spans.

--- searchlib/src/vespa/searchlib/queryeval/filtered_match_spans.cpp
+++ searchlib/src/vespa/searchlib/queryeval/filtered_match_spans.cpp
@@ -20,11 +20,14 @@ class ElementIdMatchSpanFilter {
 public:
     ElementIdMatchSpanFilter(std::span<const uint32_t> element_ids) noexcept
         : _element_ids(element_ids),
-          _element_ids_itr(_element_ids.begin()),
+          _element_ids_itr(),
           _first(MatchSpanPos(0, 0)),
           _last(MatchSpanPos(0, 0)),
           _valid(false)
     {
+    }
+    void new_field() noexcept {
+        _element_ids_itr = _element_ids.begin();
         step();
     }
     void step() noexcept
@@ -41,11 +44,14 @@ public:
             }
         }
     }
-    [[nodiscard]] bool seek(const MatchSpanPos& seek_to) noexcept {
+    [[nodiscard]] bool seek(uint32_t, const MatchSpanPos& seek_to) noexcept {
         while (_valid && _last < seek_to) {
             step();
         }
         return _valid;
+    }
+    [[nodiscard]] bool is_after(uint32_t, const MatchSpanPos& last_pos) noexcept {
+        return _first > last_pos;
     }
     [[nodiscard]] bool valid() const noexcept { return _valid; }
     [[nodiscard]] const MatchSpanPos& first() const noexcept { return _first; }
@@ -66,14 +72,22 @@ std::span<const MatchSpan>
 FilteredMatchSpans::intersection_helper(std::span<const MatchSpan> spans, MatchSpanFilter filter)
 {
     _filtered_spans.clear();
+    std::optional<uint32_t> field_id;
     for (auto& match_span : spans) {
-        if (!filter.seek(match_span.first())) {
-            break;
+        if (!field_id.has_value() || field_id.value() != match_span.field_id()) {
+            field_id = match_span.field_id();
+            filter.new_field();
         }
-        while (filter.first() <= match_span.last()) {
+        if (!filter.seek(match_span.field_id(), match_span.first())) {
+            continue;
+        }
+        while (!filter.is_after(match_span.field_id(), match_span.last())) {
             auto first = std::max(match_span.first(), filter.first());
             auto last = std::min(match_span.last(), filter.last());
             _filtered_spans.emplace_back(match_span.field_id(), first, last);
+            if (filter.last() >= match_span.last()) {
+                break;
+            }
             filter.step();
             if (!filter.valid()) {
                 break;
