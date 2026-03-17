f655b023500211bdc2686f84221a0586f31f1084
Brage <bragehk@vespa.ai>
2026-03-17 11:36:22 +0100

    fix: better error message

--- client/go/internal/vespa/vault.go
+++ client/go/internal/vespa/vault.go
@@ -102,7 +102,7 @@ func (t *cloudTarget) ensureVaultAccessRule(vaultName string) error {
 	}
 
 	if len(t.apiOptions.APIKey) > 0 {
-		return fmt.Errorf("editing vault access rules is not allowed with an API key: please switch to control plane auth with 'vespa auth login'")
+		return fmt.Errorf("Editing vault access rules is not allowed with an API key: please switch to interactive auth with 'vespa auth login'")
 	}
 
 	// Build new rule and merge into the full GET response body (preserving extra fields)
