234597172e3d1343af98d66f4368f45a264f5a76
Sebastian August Berg <sebastian-august@hotmail.com>
2026-03-18 12:30:24 +0100

    Scope equiv production to matched term in adding rules

--- container-search/src/test/java/com/yahoo/prelude/semantics/test/ExpansionTestCase.java
+++ container-search/src/test/java/com/yahoo/prelude/semantics/test/ExpansionTestCase.java
@@ -31,4 +31,22 @@ public class ExpansionTestCase extends RuleBaseAbstractTestCase {
                         "testfield:foo testfield:bar");
     }
 
+    /** EQUIV production should preserve query structure, not collapse into a root-level EQUIV. */
+    @Test
+    void testEquivPreservesQueryStructure() {
+        assertSemantics("AND red running (EQUIV equiv1 equiv2 equiv3)", "red running equiv1");
+    }
+
+    /** EQUIV at the start of a multi-term query should also preserve structure. */
+    @Test
+    void testEquivPreservesQueryStructureMatchFirst() {
+        assertSemantics("AND (EQUIV equiv1 equiv2 equiv3) is great", "equiv1 is great");
+    }
+
+    /** EQUIV in the middle of a multi-term query should also preserve structure. */
+    @Test
+    void testEquivPreservesQueryStructureMatchMiddle() {
+        assertSemantics("AND foo (EQUIV equiv1 equiv2 equiv3) bar", "foo equiv1 bar");
+    }
+
 }
