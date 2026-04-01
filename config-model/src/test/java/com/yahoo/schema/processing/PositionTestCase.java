f0e3ed96ee3989135664275fc115e7391c502a99
Radu Gheorghe <radu@vespa.ai>
2026-04-01 13:17:54 +0300

    add missing imports

--- config-model/src/test/java/com/yahoo/schema/processing/PositionTestCase.java
+++ config-model/src/test/java/com/yahoo/schema/processing/PositionTestCase.java
@@ -20,8 +20,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
