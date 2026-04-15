0df08772032d15433bd4e63b9b925b4c10bad7bd
Harald Musum <musum@vespa.ai>
2026-04-15 09:25:53 +0200

    Change default value of MAX_DOCUMENT_OPERATION_REQUEST_SIZE_MIB to 100

--- config-model-api/src/main/java/com/yahoo/config/model/api/ModelContext.java
+++ config-model-api/src/main/java/com/yahoo/config/model/api/ModelContext.java
@@ -112,7 +112,7 @@ public interface ModelContext {
         @ModelFeatureFlag(owners = {"hmusum"}) default boolean forwardAllLogLevels() { return true; }
         @ModelFeatureFlag(owners = {"hmusum"}) default long zookeeperPreAllocSize() { return 65536L; }
         @ModelFeatureFlag(owners = {"vekterli"}) default int maxContentNodeMaintenanceOpConcurrency() { return -1; }
-        @ModelFeatureFlag(owners = {"glebashnik"}) default int maxDocumentOperationRequestSizeMib() { return 128; }
+        @ModelFeatureFlag(owners = {"glebashnik"}) default int maxDocumentOperationRequestSizeMib() { return 100; }
         @ModelFeatureFlag(owners = {"glebashnik"}) default Object sidecarsForTest() { return null; }
         @ModelFeatureFlag(owners = {"bjorncs"}) default boolean useTriton() { return false; }
         @ModelFeatureFlag(owners = {"arnej"}) default boolean ignoreConnectivityChecksAtStartup() { return false; }
