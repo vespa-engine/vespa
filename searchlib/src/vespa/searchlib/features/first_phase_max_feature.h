5875d1a749d742e2d896d0e1c8ec4a54c0b27d26
Johan Solbakken <johsol@vespa.ai>
2026-04-17 11:44:03 +0000

    Add new files

---
+++ searchlib/src/vespa/searchlib/features/first_phase_max_feature.h
@@ -0,0 +1,29 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+#include <vespa/searchlib/fef/blueprint.h>
+#include <vespa/searchlib/fef/featureexecutor.h>
+
+namespace search::features {
+
+/**
+ * Executor for first phase max feature which outputs the max score from first
+ * phase.
+ */
+class FirstPhaseMaxExecutor : public fef::FeatureExecutor {
+public:
+
+};
+
+/**
+ * Blueprint for first phase max feature.
+ */
+class FirstPhaseMaxBlueprint : public fef::Blueprint {
+public:
+
+};
+
+
+} // namespace search::features
+
