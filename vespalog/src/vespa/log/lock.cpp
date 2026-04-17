fd258deee96d6b24c86a1c30385d61653e76d1ac
Arne Juul <arnej@vespa.ai>
2026-04-17 13:35:36 +0000

    run clang-format in vespalog module

--- vespalog/src/vespa/log/lock.cpp
+++ vespalog/src/vespa/log/lock.cpp
@@ -1,57 +1,48 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/file.h>
-#include <cstring>
-#include <unistd.h>
-#include <limits.h>
-#include <cstdio>
-
 #include "log.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstring>
 LOG_SETUP_INDIRECT(".log.lock", "$Id$");
-#include "lock.h"
 #include "internal.h"
+#include "lock.h"
 
 #undef LOG
 #define LOG LOG_INDIRECT
 
 namespace ns_log {
 
-Lock::Lock(int arg_fd)
-    : _fd(dup(arg_fd)),
-      _isLocked(false)
-{
+Lock::Lock(int arg_fd) : _fd(dup(arg_fd)), _isLocked(false) {
     if (_fd == -1) {
-        throwInvalid("Cannot duplicate fd %d for lock: %s", arg_fd,
-                     strerror(errno));
+        throwInvalid("Cannot duplicate fd %d for lock: %s", arg_fd, strerror(errno));
     }
 }
 
-
-Lock::Lock(const char *filename, int mode)
-    : _fd(open(filename, mode, 0666)),
-      _isLocked(false)
-{
+Lock::Lock(const char* filename, int mode) : _fd(open(filename, mode, 0666)), _isLocked(false) {
     if (_fd == -1) {
         if (mode & O_CREAT) {
             unlink(filename);
             _fd = open(filename, mode, 0666);
         }
         if (_fd == -1) {
-            throwInvalid("Cannot open lockfile '%s': %s", filename,
-                         strerror(errno));
+            throwInvalid("Cannot open lockfile '%s': %s", filename, strerror(errno));
         }
     }
     fcntl(_fd, F_SETFD, FD_CLOEXEC);
 }
 
-void
-Lock::lock(bool exclusive)
-{
+void Lock::lock(bool exclusive) {
     // Early escape to avoid double locking which would of course block forever
-    if (_isLocked) return;
+    if (_isLocked)
+        return;
 
     if (flock(_fd, exclusive ? LOCK_EX : LOCK_SH) == -1) {
         int err = errno;
@@ -62,26 +53,23 @@ Lock::lock(bool exclusive)
     _isLocked = true;
 }
 
-void
-Lock::unlock() {
+void Lock::unlock() {
     if (_isLocked) {
         flock(_fd, LOCK_UN);
         _isLocked = false;
     }
 }
 
-int Lock::size()
-{
+int Lock::size() {
     struct stat st;
-    int err = fstat(_fd, &st);
+    int         err = fstat(_fd, &st);
     if (err == -1) {
         return -1;
     }
     return st.st_size;
 }
 
-Lock::~Lock()
-{
+Lock::~Lock() {
     unlock();
     close(_fd);
 }
