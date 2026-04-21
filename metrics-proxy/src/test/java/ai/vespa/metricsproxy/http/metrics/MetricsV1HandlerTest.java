b5bb9f01dcc2178a90404fcbbe0bb5991c83bbe1
Ola Aunrønning <olaa@vespa.ai>
2026-04-21 16:01:48 +0200

    Expose host_life metric in metrics/v1/values

--- metrics-proxy/src/test/java/ai/vespa/metricsproxy/http/metrics/MetricsV1HandlerTest.java
+++ metrics-proxy/src/test/java/ai/vespa/metricsproxy/http/metrics/MetricsV1HandlerTest.java
@@ -2,14 +2,20 @@
 package ai.vespa.metricsproxy.http.metrics;
 
 import ai.vespa.metricsproxy.metric.model.json.GenericJsonModel;
+import ai.vespa.metricsproxy.metric.model.json.GenericService;
 import com.yahoo.container.jdisc.RequestHandlerTestDriver;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import java.io.IOException;
 import java.util.concurrent.Executors;
 
 import static ai.vespa.metricsproxy.http.metrics.MetricsV1Handler.V1_PATH;
 import static ai.vespa.metricsproxy.http.metrics.MetricsV1Handler.VALUES_PATH;
+import static ai.vespa.metricsproxy.metric.model.json.JacksonUtil.objectMapper;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author gjoranv
@@ -28,7 +34,9 @@ public class MetricsV1HandlerTest extends MetricsHandlerTestBase<GenericJsonMode
         var handler = new MetricsV1Handler(Executors.newSingleThreadExecutor(),
                                                         getMetricsManager(),
                                                         vespaServices,
-                                                        getMetricsConsumers());
+                                                        getMetricsConsumers(),
+                                                        getApplicationDimensions(),
+                                                        getNodeDimensions());
         testDriver = new RequestHandlerTestDriver(handler);
     }
 
@@ -40,6 +48,17 @@ public class MetricsV1HandlerTest extends MetricsHandlerTestBase<GenericJsonMode
     @Override
     GenericJsonModel getGenericJsonModel(GenericJsonModel genericJsonModel) {
         return genericJsonModel;
+    }
+
+    @Test
+    public void response_contains_host_life_alive_metric() throws IOException {
+        String response = testDriver.sendRequest(VALUES_URI).readAll();
+        GenericJsonModel jsonModel = objectMapper().readValue(response, GenericJsonModel.class);
+
+        GenericService hostLife = getService(jsonModel, "host_life");
+        assertNotNull(hostLife);
+        assertEquals(1, hostLife.metrics.size());
+        assertEquals(1L, hostLife.metrics.get(0).values.get("alive").longValue());
     }
 
 }
