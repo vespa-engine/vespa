6261c9058e6c095c84cacc2813da8209aa446897
Jan Böker <boeker@vespa.ai>
2026-04-22 06:23:50 +0000

    Add ANN time budget and ANN timeout functionality to Doom class

--- vespalib/src/vespa/vespalib/util/doom.cpp
+++ vespalib/src/vespa/vespalib/util/doom.cpp
@@ -3,12 +3,45 @@
 #include "doom.h"
 
 #include "fake_doom.h"
+#include <cassert>
 
 namespace vespalib {
 
-Doom::Doom(const std::atomic<steady_time>& now_ref, steady_time softDoom, steady_time hardDoom,
-           bool explicitSoftDoom) noexcept
-    : _now(now_ref), _softDoom(softDoom), _hardDoom(hardDoom), _isExplicitSoftDoom(explicitSoftDoom) {
+Doom::Doom(const std::atomic<steady_time> & now_ref, steady_time softDoom,
+           steady_time hardDoom, bool explicitSoftDoom) noexcept
+    : Doom(now_ref,
+           vespalib::duration(std::numeric_limits<uint32_t>::max()), false, softDoom,
+           softDoom, hardDoom, explicitSoftDoom)
+{ }
+
+Doom::Doom(const std::atomic<steady_time> & now_ref,
+           vespalib::duration ann_timebudget, bool ann_timeout_enabled, steady_time ann_timeout,
+           steady_time softDoom, steady_time hardDoom, bool explicitSoftDoom) noexcept
+    : _now(now_ref),
+      _ann_timebudget(ann_timebudget),
+      _ann_timeout_enabled(ann_timeout_enabled),
+      _ann_timeout(ann_timeout),
+      _softDoom(softDoom),
+      _hardDoom(hardDoom),
+      _isExplicitSoftDoom(explicitSoftDoom)
+{ }
+
+const AnnDoom Doom::make_ann_doom(uint32_t remaining_searches) const noexcept {
+    assert(remaining_searches > 0);
+    vespalib::steady_time now(_now.load(std::memory_order_relaxed));
+    // ANN might doom due to a depleted time budget or a timeout,
+    // whatever happens first. The timeout might be the ANN timeout
+    // or the soft-timeout. The soft-timeout is used when ANN timeouts
+    // are not enabled, and in this case, reaching the soft-timeout
+    // is not reported as an ANN timeout.
+    vespalib::duration timeout_left = _ann_timeout_enabled ? ((_ann_timeout - now) / remaining_searches)
+                                                           : _softDoom - now;
+
+    if (_ann_timebudget < timeout_left) {
+        return AnnDoom(_now, now + _ann_timebudget, false);
+    } else {
+        return AnnDoom(_now, now + timeout_left, _ann_timeout_enabled);
+    }
 }
 
 const Doom& Doom::never() noexcept {
--- vespalib/src/vespa/vespalib/util/doom.h
+++ vespalib/src/vespa/vespalib/util/doom.h
@@ -2,17 +2,22 @@
 
 #pragma once
 
+#include "ann_doom.h"
 #include "time.h"
-
 #include <atomic>
 
 namespace vespalib {
 
 class Doom {
 public:
-    Doom(const std::atomic<steady_time>& now_ref, steady_time doom) noexcept : Doom(now_ref, doom, doom, false) {}
-    Doom(const std::atomic<steady_time>& now_ref, steady_time softDoom, steady_time hardDoom,
-         bool explicitSoftDoom) noexcept;
+    Doom(const std::atomic<steady_time> & now_ref, steady_time doom) noexcept
+        : Doom(now_ref, doom, doom, false)
+    {}
+    Doom(const std::atomic<steady_time> & now_ref, steady_time softDoom,
+         steady_time hardDoom, bool explicitSoftDoom) noexcept;
+    Doom(const std::atomic<steady_time> & now_ref,
+         vespalib::duration ann_timebudget, bool ann_timeout_enabled, steady_time ann_timeout,
+         steady_time softDoom, steady_time hardDoom, bool explicitSoftDoom) noexcept;
 
     bool soft_doom() const noexcept { return (getTimeNS() > _softDoom); }
     bool hard_doom() const noexcept { return (getTimeNS() > _hardDoom); }
@@ -21,14 +26,19 @@ public:
     bool isExplicitSoftDoom() const noexcept { return _isExplicitSoftDoom; }
     static const Doom& never() noexcept;
 
+    const AnnDoom make_ann_doom(uint32_t remaining_searches) const noexcept;
+
 private:
     vespalib::steady_time getTimeNS() const noexcept {
         return vespalib::steady_time(_now.load(std::memory_order_relaxed));
     }
-    const std::atomic<steady_time>& _now;
-    steady_time                     _softDoom;
-    steady_time                     _hardDoom;
-    bool                            _isExplicitSoftDoom;
+    const std::atomic<steady_time> & _now;
+    vespalib::duration               _ann_timebudget;
+    bool                             _ann_timeout_enabled;
+    steady_time                      _ann_timeout;
+    steady_time                      _softDoom;
+    steady_time                      _hardDoom;
+    bool                             _isExplicitSoftDoom;
 };
 
 } // namespace vespalib
