a5c760affe68d728eebeb0d1d194ce3534878d6f
Jan Böker <boeker@vespa.ai>
2026-04-23 13:12:58 +0000

    Add comments

--- searchcore/src/vespa/searchcore/proton/matching/ann_deadline_configuration.h
+++ searchcore/src/vespa/searchcore/proton/matching/ann_deadline_configuration.h
@@ -8,11 +8,21 @@
 
 namespace proton::matching {
 
+/**
+ * Class that stores the ANN time budget and ANN timeout.
+ * Used to create Deadline objects for the individual ANN searches.
+ */
 class AnnDeadlineConfiguration {
 public:
     AnnDeadlineConfiguration(const vespalib::Doom& now, vespalib::steady_time soft_doom);
     AnnDeadlineConfiguration(const vespalib::Doom& now, vespalib::duration timebudget, bool timeout_enabled, vespalib::steady_time timeout) noexcept;
 
+    /**
+     * Returns a Deadline for the next ANN search.
+     * This deadline is the minimum of the time budget and the time until the timeout,
+     * where the time until the timeout is distributed equally between all remaining searches if
+     * the timeout is enabled.
+     */
     const vespalib::Deadline make_ann_deadline(uint32_t remaining_searches) const noexcept;
 
 private:
