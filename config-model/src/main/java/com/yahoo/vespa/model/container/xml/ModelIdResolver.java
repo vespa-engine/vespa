131eeac7da27095dd3465973da88668e299345a0
thomasht86 <thomas@vespa.ai>
2026-04-24 08:23:14 +0200

    remove newline

--- config-model/src/main/java/com/yahoo/vespa/model/container/xml/ModelIdResolver.java
+++ config-model/src/main/java/com/yahoo/vespa/model/container/xml/ModelIdResolver.java
@@ -85,7 +85,6 @@ public class ModelIdResolver {
         register(m, "lightonai-denseon-int8", "https://data.vespa-cloud.com/onnx_models/lightonai-denseon-int8/model.onnx", Set.of(ONNX_MODEL));
         register(m, "lightonai-denseon-int8-vocab", "https://data.vespa-cloud.com/onnx_models/lightonai-denseon-int8/tokenizer.json", Set.of(HF_TOKENIZER));
 
-
         register(m, "alibaba-gte-modernbert", "https://data.vespa-cloud.com/onnx_models/alibaba-gte-modernbert-base/model.onnx", Set.of(ONNX_MODEL));
         register(m, "alibaba-gte-modernbert-vocab", "https://data.vespa-cloud.com/onnx_models/alibaba-gte-modernbert-base/tokenizer.json", Set.of(HF_TOKENIZER));
         register(m, "alibaba-gte-modernbert-int8", "https://data.vespa-cloud.com/onnx_models/alibaba-gte-modernbert-int8/model.onnx", Set.of(ONNX_MODEL));
