14b73c579e2954be917ef2466bbac5a364a0599f
Johan Solbakken <johsol@vespa.ai>
2026-04-24 09:39:51 +0000

    Add node spec variant

--- searchlib/src/tests/queryeval/iterator_benchmark/benchmark_spec.cpp
+++ searchlib/src/tests/queryeval/iterator_benchmark/benchmark_spec.cpp
@@ -1,1 +1,7 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
 #include "benchmark_spec.h"
+
+namespace search::queryeval::test {
+
+}
--- searchlib/src/tests/queryeval/iterator_benchmark/benchmark_spec.h
+++ searchlib/src/tests/queryeval/iterator_benchmark/benchmark_spec.h
@@ -6,3 +6,30 @@
  * Building trees.
  */
 
+#include "common.h"
+
+#include <variant>
+#include <vector>
+
+namespace search::queryeval::test {
+
+struct Spec;
+
+struct TermSpec {
+    FieldConfig field;
+    double hit_ratio;
+};
+
+struct AndSpec {
+    std::vector<Spec> children;
+};
+
+struct OrSpec {
+    std::vector<Spec> children;
+};
+
+struct Spec {
+    std::variant<TermSpec, AndSpec, OrSpec> node;
+};
+
+}
