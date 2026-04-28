6a4423db5a2e48e4d1b94f676fd8cb496fa9e0fd
Tor Egge <Tor.Egge@online.no>
2026-04-28 15:04:18 +0200

    Add likely attribute.

--- searchlib/src/vespa/searchlib/attribute/single_raw_ext_attribute.cpp
+++ searchlib/src/vespa/searchlib/attribute/single_raw_ext_attribute.cpp
@@ -46,7 +46,7 @@ SingleRawExtAttribute::add(std::span<const char> v, int32_t)
     const size_t start(_offsets.back());
     const size_t sz(v.size());
     _buffer.resize(start + sz);
-    if (sz > 0) {
+    if (sz > 0) [[likely]] {
         memcpy(_buffer.data() + start, v.data(), sz);
     }
     return true;
