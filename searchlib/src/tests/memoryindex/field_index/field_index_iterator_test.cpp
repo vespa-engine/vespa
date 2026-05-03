24d07f4c82b124516f19a194a80ddc588576547c
Arne Juul <arnej@vespa.ai>
2026-05-03 19:49:21 +0000

    run clang-format in directory searchlib/src/tests/memoryindex/field_index

--- searchlib/src/tests/memoryindex/field_index/field_index_iterator_test.cpp
+++ searchlib/src/tests/memoryindex/field_index/field_index_iterator_test.cpp
@@ -1,10 +1,10 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 #include <vespa/searchcommon/common/schema.h>
+#include <vespa/searchlib/fef/termfieldmatchdataarray.h>
 #include <vespa/searchlib/memoryindex/field_index.h>
 #include <vespa/searchlib/test/memoryindex/wrap_inserter.h>
 #include <vespa/searchlib/test/searchiteratorverifier.h>
-#include <vespa/searchlib/fef/termfieldmatchdataarray.h>
 #include <vespa/vespalib/gtest/gtest.h>
 
 using namespace search::fef;
@@ -15,19 +15,14 @@ using namespace search::memoryindex;
 using search::index::schema::DataType;
 using search::test::SearchIteratorVerifier;
 
-template <typename FieldIndexType>
-class Verifier : public SearchIteratorVerifier {
+template <typename FieldIndexType> class Verifier : public SearchIteratorVerifier {
 private:
     mutable TermFieldMatchData _tfmd;
     TermFieldMatchDataArray    _tfmda;
-    FieldIndexType _field_index;
+    FieldIndexType             _field_index;
 
 public:
-    Verifier(const Schema& schema)
-        : _tfmd(),
-          _tfmda(),
-          _field_index(schema, 0)
-    {
+    Verifier(const Schema& schema) : _tfmd(), _tfmda(), _field_index(schema, 0) {
         _tfmda.add(&_tfmd);
         WrapInserter inserter(_field_index);
         inserter.word("a");
@@ -39,7 +34,7 @@ public:
     ~Verifier() override;
 
     SearchIterator::UP create(bool strict) const override {
-        (void) strict;
+        (void)strict;
         return _field_index.make_search_iterator("a", 0, _tfmda);
     }
     std::unique_ptr<SearchIterator> create(bool, const TermFieldMatchDataArray& tfmda) const override {
@@ -48,37 +43,27 @@ public:
     void verify_hidden_from_ranking() { SearchIteratorVerifier::verify_hidden_from_ranking(_tfmda); }
 };
 
-template <typename FieldIndexType>
-Verifier<FieldIndexType>::~Verifier() = default;
+template <typename FieldIndexType> Verifier<FieldIndexType>::~Verifier() = default;
 
-Schema
-get_schema()
-{
+Schema get_schema() {
     Schema result;
     result.addIndexField(Schema::IndexField("f0", DataType::STRING));
     return result;
 }
 
-template <typename FieldIndexType>
-struct Fixture {
-    Schema schema;
+template <typename FieldIndexType> struct Fixture {
+    Schema                   schema;
     Verifier<FieldIndexType> verifier;
-    Fixture()
-        : schema(get_schema()),
-          verifier(schema)
-    {
-    }
+    Fixture() : schema(get_schema()), verifier(schema) {}
 };
 
-TEST(FieldIndexIteratorTest, require_that_normal_posting_iterator_conforms)
-{
+TEST(FieldIndexIteratorTest, require_that_normal_posting_iterator_conforms) {
     Fixture<FieldIndex<false>> f;
     f.verifier.verify();
     f.verifier.verify_hidden_from_ranking();
 }
 
-TEST(FieldIndexIteratorTest, require_that_interleaved_posting_iterator_conforms)
-{
+TEST(FieldIndexIteratorTest, require_that_interleaved_posting_iterator_conforms) {
     Fixture<FieldIndex<true>> f;
     f.verifier.verify();
     f.verifier.verify_hidden_from_ranking();
