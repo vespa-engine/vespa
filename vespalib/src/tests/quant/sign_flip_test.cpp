16598c40a353544c35ff5963fa76d481769c7783
Tor Brede Vekterli <vekterli@vespa.ai>
2026-05-13 11:54:50 +0000

    Explicitly test full block and block remainder cases

--- vespalib/src/tests/quant/sign_flip_test.cpp
+++ vespalib/src/tests/quant/sign_flip_test.cpp
@@ -6,6 +6,7 @@
 
 #include <gmock/gmock.h>
 
+#include <algorithm>
 #include <random>
 #include <span>
 #include <vector>
@@ -21,7 +22,25 @@ template <std::floating_point T> void flip(std::span<T> f, const uint64_t seed) 
     flip_sign_bits(f, prng);
 }
 
-template <std::floating_point T> void do_test_sign_flips_are_deterministic_and_invertible() {
+template <std::floating_point T> void do_test_full_block_sign_flips_are_deterministic_and_invertible() {
+    SCOPED_TRACE("full block");
+    std::vector<T> v(64); // Leaky abstraction alert: fixed block size of 64 due to 64 PRNG output bits.
+    std::iota(v.begin(), v.end(), 1);
+    const std::vector v_orig = v;
+
+    flip<T>(v, 0xf00d);
+    std::vector<T> expected = {1,   2,   -3,  4,   -5,  -6,  7,   -8,  9,   10, 11,  -12, -13, -14, -15, -16,
+                               17,  -18, 19,  20,  -21, 22,  23,  24,  -25, 26, 27,  -28, 29,  30,  31,  32,
+                               -33, 34,  -35, -36, -37, -38, -39, 40,  41,  42, -43, 44,  -45, -46, 47,  -48,
+                               -49, -50, 51,  52,  53,  54,  -55, -56, 57,  58, -59, 60,  -61, 62,  -63, -64};
+    EXPECT_THAT(v, ElementsAreArray(expected));
+
+    flip<T>(v, 0xf00d);
+    EXPECT_THAT(v, ElementsAreArray(v_orig));
+}
+
+template <std::floating_point T> void do_test_remainder_sign_flips_are_deterministic_and_invertible() {
+    SCOPED_TRACE("block remainder");
     std::vector<T> v = {1, 2, 3, 4, 5, 6, 7, 8};
     std::vector<T> v2 = {9, 10, 11, 12, 13, 14, 15, 16};
 
@@ -35,25 +54,18 @@ template <std::floating_point T> void do_test_sign_flips_are_deterministic_and_i
     EXPECT_THAT(v, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
     flip<T>(v2, 0xc0ffee);
     EXPECT_THAT(v2, ElementsAre(9, 10, 11, 12, 13, 14, 15, 16));
-
-    // Abstraction leak alert: test that chunk remainders are handled
-    std::vector<T> v3(64);
-    // TODO append_range() when we know all our compiler versions have it
-    v3.insert(v3.end(), v.begin(), v.end()); // 64 + 8 elements
-    flip<T>(v3, 0x1337);
-    EXPECT_THAT(std::span(v3).subspan(64), ElementsAre(-1, -2, -3, 4, -5, 6, -7, -8));
-    flip<T>(v3, 0x1337);
-    EXPECT_THAT(std::span(v3).subspan(64), ElementsAreArray(v));
 }
 
 } // namespace
 
 TEST(SignFlipTest, float_sign_flips_are_deterministic_and_invertible) {
-    do_test_sign_flips_are_deterministic_and_invertible<float>();
+    do_test_full_block_sign_flips_are_deterministic_and_invertible<float>();
+    do_test_remainder_sign_flips_are_deterministic_and_invertible<float>();
 }
 
 TEST(SignFlipTest, double_sign_flips_are_deterministic_and_invertible) {
-    do_test_sign_flips_are_deterministic_and_invertible<double>();
+    do_test_full_block_sign_flips_are_deterministic_and_invertible<double>();
+    do_test_remainder_sign_flips_are_deterministic_and_invertible<double>();
 }
 
 TEST(SignFlipTest, sign_flipping_buffer_boundary_cases) {
