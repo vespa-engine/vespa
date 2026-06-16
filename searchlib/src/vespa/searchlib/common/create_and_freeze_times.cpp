75e79ffb4d5a78f3cc77d3ec8f8260ee48c9b977
Tor Egge <Tor.Egge@online.no>
2026-06-16 16:11:10 +0200

    Add CreateAndFreezeTimes, used to keep track of when flushes started and completed.

---
+++ searchlib/src/vespa/searchlib/common/create_and_freeze_times.cpp
@@ -0,0 +1,74 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include "create_and_freeze_times.h"
+
+#include "fileheadertags.h"
+
+#include <vespa/vespalib/data/fileheader.h>
+
+#include <algorithm>
+
+using search::tags::CREATE_TIME;
+using search::tags::FREEZE_TIME;
+using std::chrono::microseconds;
+using std::chrono::steady_clock;
+using std::chrono::system_clock;
+using std::chrono::utc_clock;
+
+namespace search::common {
+
+namespace {
+
+// Handle low resolution steady clock
+steady_clock::duration min_flush_duration =
+    std::max(duration_cast<steady_clock::duration>(microseconds(1u)), steady_clock::duration(1u));
+
+} // namespace
+
+CreateAndFreezeTimes::CreateAndFreezeTimes(const vespalib::GenericHeader& header) : _create_time(0), _freeze_time(0) {
+    if (header.hasTag(CREATE_TIME) && header.hasTag(FREEZE_TIME)) {
+        auto create_time = header.getTag(CREATE_TIME).asInteger();
+        auto freeze_time = header.getTag(FREEZE_TIME).asInteger();
+        if (freeze_time >= create_time) {
+            _create_time = create_time;
+            _freeze_time = freeze_time;
+        }
+    }
+}
+
+int64_t CreateAndFreezeTimes::to_utc_us(std::chrono::system_clock::time_point system_time) {
+    auto utc_time = utc_clock::from_sys(system_time);
+    return duration_cast<microseconds>(utc_time.time_since_epoch()).count();
+}
+
+system_clock::time_point CreateAndFreezeTimes::from_utc_us(uint64_t us) {
+    auto utc_time = utc_clock::time_point(microseconds(us));
+    return utc_clock::to_sys(utc_time);
+}
+
+void CreateAndFreezeTimes::merge(const CreateAndFreezeTimes& rhs) noexcept {
+    if (rhs._create_time > 0 && rhs._freeze_time > 0) {
+        if (_create_time > 0 && _freeze_time > 0) {
+            _create_time = std::min(_create_time, rhs._create_time);
+            _freeze_time = std::max(_freeze_time, rhs._freeze_time);
+        } else {
+            _create_time = rhs._create_time;
+            _freeze_time = rhs._freeze_time;
+        }
+    }
+}
+
+steady_clock::duration CreateAndFreezeTimes::get_flush_duration() const {
+    if (_freeze_time >= _create_time) {
+        return std::max(duration_cast<steady_clock::duration>(microseconds(_freeze_time - _create_time)),
+                        min_flush_duration);
+    }
+    return steady_clock::duration::zero();
+}
+
+steady_clock::duration CreateAndFreezeTimes::make_flush_duration(const steady_clock::time_point& create_time) {
+    steady_clock::duration flush_duration = steady_clock::now() - create_time;
+    return std::max(flush_duration, min_flush_duration);
+}
+
+} // namespace search::common
--- searchlib/src/vespa/searchlib/common/fileheadercontext.h
+++ searchlib/src/vespa/searchlib/common/fileheadercontext.h
@@ -1,7 +1,8 @@
 // Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 #pragma once
 
-#include <chrono>
+#include "create_and_freeze_times.h"
+
 #include <string>
 
 namespace vespalib {
