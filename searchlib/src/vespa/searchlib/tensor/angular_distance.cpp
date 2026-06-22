0136f58e6c3f3b812060476782f0161162829902
Tor Brede Vekterli <vekterli@vespa.ai>
2026-06-22 14:19:41 +0000

    Add quantized bound angular distance function
    This is an adaptation of the existing bound angular distance
    function, but which supports transparently operating on pre-
    quantized vectors (using the EDEN algorithm).
    
    Angular was chosen as the first quantized distance function since
    we have thus far focused on dot product support for quantized
    vectors.
    
    The _right_ hand side argument to the distance function is
    expected to always be a pre-quantized vector encapsulated in an
    opaque int8 array.
    
    The type of the _left_ hand side argument depends on the factory
    function invoked; _query_ vectors will use `float` precision, HNSW
    _insertion_ vectors must be pre-quantized as `int8`.
    
    This means we evaluate query-to-candidate distances with asymmetric
    types, significantly increasing the evaluation precision over doing
    this with both sides quantized.
    
    For the `float` query vector case, we always store a copy of the
    input query vector, as we have to explicitly rotate it into the
    same frame of reference as that used by the quantized vectors.
    Query inputs that are not `float` will be implicitly converted to
    this type. An `int8` input in this case will be elementwise
    _widened_ to `float` and will _not_ be treated as pre-quantized
    input.

--- searchlib/src/vespa/searchlib/tensor/angular_distance.cpp
+++ searchlib/src/vespa/searchlib/tensor/angular_distance.cpp
@@ -5,11 +5,13 @@
 #include "temporary_vector_store.h"
 
 #include <vespa/vespalib/hwaccelerated/functions.h>
+#include <vespa/vespalib/quant/eden.h>
 
 #include <cmath>
 #include <numbers>
 
 using vespalib::typify_invoke;
+using vespalib::eval::CellType;
 using vespalib::eval::Int8Float;
 using vespalib::eval::TypedCells;
 using vespalib::eval::TypifyCellType;
@@ -17,7 +19,32 @@ namespace hwaccelerated = vespalib::hwaccelerated;
 
 namespace search::tensor {
 
-template <typename VectorStoreType> class BoundAngularDistance final : public BoundDistanceFunction {
+class BoundAngularDistanceBase : public BoundDistanceFunction {
+public:
+    double convert_threshold(double threshold) const noexcept override {
+        if (threshold < 0.0) {
+            return 0.0;
+        }
+        if (threshold > std::numbers::pi) {
+            return 2.0;
+        }
+        double cosine_similarity = cos(threshold);
+        return 1.0 - cosine_similarity;
+    }
+    double to_rawscore(double distance) const noexcept override {
+        double cosine_similarity = 1.0 - distance;
+        // should be in the range [-1,1] but roundoff may cause problems:
+        cosine_similarity = std::min(1.0, cosine_similarity);
+        cosine_similarity = std::max(-1.0, cosine_similarity);
+        double angle_distance = acos(cosine_similarity); // in range [0,pi]
+        double score = 1.0 / (1.0 + angle_distance);
+        return score;
+    }
+    double calc_with_limit(TypedCells rhs, double) const noexcept override { return calc(rhs); }
+};
+
+template <typename VectorStoreType>
+class BoundAngularDistance final : public BoundAngularDistanceBase {
     using FloatType = VectorStoreType::FloatType;
     mutable VectorStoreType          _tmpSpace;
     const std::span<const FloatType> _lhs;
@@ -41,26 +68,6 @@ public:
         double                     distance = 1.0 - cosine_similarity; // in range [0,2]
         return distance;
     }
-    double convert_threshold(double threshold) const noexcept override {
-        if (threshold < 0.0) {
-            return 0.0;
-        }
-        if (threshold > std::numbers::pi) {
-            return 2.0;
-        }
-        double cosine_similarity = cos(threshold);
-        return 1.0 - cosine_similarity;
-    }
-    double to_rawscore(double distance) const noexcept override {
-        double cosine_similarity = 1.0 - distance;
-        // should be in the range [-1,1] but roundoff may cause problems:
-        cosine_similarity = std::min(1.0, cosine_similarity);
-        cosine_similarity = std::max(-1.0, cosine_similarity);
-        double angle_distance = acos(cosine_similarity); // in range [0,pi]
-        double score = 1.0 / (1.0 + angle_distance);
-        return score;
-    }
-    double calc_with_limit(TypedCells rhs, double) const noexcept override { return calc(rhs); }
 };
 
 template class BoundAngularDistance<TemporaryVectorStore<float>>;
@@ -89,9 +96,110 @@ BoundDistanceFunction::UP AngularDistanceFunctionFactory<FloatType>::for_inserti
     }
 }
 
+namespace {
+
+struct Float32LhsQuantizedRhs {
+    using VectorStoreType = MutableSingleTemporaryVectorStore<float>;
+    using LhsFloatType = float;
+    constexpr static bool pre_quantized_lhs = false;
+};
+
+struct QuantizedLhsAndRhs {
+    using VectorStoreType = ReferenceVectorStore<Int8Float>;
+    using LhsFloatType = Int8Float;
+    constexpr static bool pre_quantized_lhs = true;
+};
+
+} // namespace
+
+// TODO generalize for other quantized bound distance functions?
+template <typename QuantTraits>
+class BoundQuantizedAngularDistance final : public BoundAngularDistanceBase {
+    using LhsVectorStoreType = typename QuantTraits::VectorStoreType;
+    using LhsFloatType = typename QuantTraits::LhsFloatType;
+    // A distance function instance shall only be used by one thread at a time,
+    // so this can be mutable without violating constness assumptions.
+    // FIXME this one is relatively expensive to create and keep around...!
+    //  Make as much as possible into free-standing functions and only create quantizer
+    //  when pre-rotating.
+    mutable vespalib::quant::EdenQuantizer _quantizer;
+    LhsVectorStoreType                     _tmp_space;
+    const std::span<const LhsFloatType>    _lhs;
+    double                                 _lhs_norm_sq;
+
+public:
+    BoundQuantizedAngularDistance(TypedCells lhs, size_t dimensions, uint8_t bits, uint64_t seed)
+        : _quantizer(dimensions, bits, seed), _tmp_space(lhs.size), _lhs(_tmp_space.storeLhs(lhs)) {
+        if constexpr (!QuantTraits::pre_quantized_lhs) { // full precision format
+            assert(lhs.size == _quantizer.dimensions());
+            // Quantized vectors are all in rotated space, so pre-rotate the query vector once
+            // so that it is in the same frame of reference. This avoids having to rotate the
+            // _quantized_ vectors when performing dot products. Note: this aliases `_lhs`.
+            _quantizer.rotate_vector_inplace(_tmp_space.mutable_lhs_buf());
+        } else {
+            static_assert(std::is_same_v<LhsFloatType, Int8Float>);
+            assert(lhs.size == _quantizer.quantized_size());
+        }
+        const auto* a = _lhs.data();
+        _lhs_norm_sq = dot(cast(a), cast(a)); // TODO dedicated L2 norm function
+    }
+    ~BoundQuantizedAngularDistance() override;
+
+    // rhs is always a quantized vector representation
+    double calc(TypedCells rhs) const noexcept override {
+        assert(rhs.type == CellType::INT8);
+        assert(rhs.size == _quantizer.quantized_size());
+        auto   rhs_vector = rhs.unsafe_typify<Int8Float>();
+        auto*  a = _lhs.data(); // f32 (raw query vector) or i8 (quantized insertion vector)
+        auto*  b = rhs_vector.data();
+        double b_norm_sq = dot(cast(b), cast(b)); // TODO dedicated L2 norm function (avoids 2x codebook lookups)
+        double squared_norms = _lhs_norm_sq * b_norm_sq;
+        double dot_product = dot(cast(a), cast(b));
+        double div = (squared_norms > 0) ? sqrt(squared_norms) : 1.0;
+        double cosine_similarity = dot_product / div;
+        double distance = 1.0 - cosine_similarity; // in range [0,2]
+        return distance;
+    }
+
+private:
+    [[nodiscard]] float dot(const int8_t* lhs, const int8_t* rhs) const noexcept {
+        auto lhs_u8 = std::span<const uint8_t>(reinterpret_cast<const uint8_t*>(lhs), _quantizer.quantized_size());
+        auto rhs_u8 = std::span<const uint8_t>(reinterpret_cast<const uint8_t*>(rhs), _quantizer.quantized_size());
+        return _quantizer.quantized_lhs_rhs_dot_product(lhs_u8, rhs_u8);
+    }
+
+    [[nodiscard]] float dot(const float* lhs, const int8_t* rhs) const noexcept {
+        auto lhs_f32 = std::span<const float>(lhs, _quantizer.dimensions());
+        auto rhs_u8 = std::span<const uint8_t>(reinterpret_cast<const uint8_t*>(rhs), _quantizer.quantized_size());
+        return _quantizer.pre_rotated_query_dot_product(lhs_f32, rhs_u8);
+    }
+
+    [[nodiscard]] float dot(const float* lhs, const float* rhs) const noexcept {
+        return hwaccelerated::dot_product(lhs, rhs, _quantizer.dimensions());
+    }
+};
+
+template <typename LhsVectorStoreType>
+BoundQuantizedAngularDistance<LhsVectorStoreType>::~BoundQuantizedAngularDistance() = default;
+
+BoundDistanceFunction::UP QuantizedAngularDistanceFunctionFactory::for_query_vector(TypedCells lhs) const {
+    using DFT = BoundQuantizedAngularDistance<Float32LhsQuantizedRhs>;
+    return std::make_unique<DFT>(lhs, _dimensions, _bits, _seed);
+}
+
+BoundDistanceFunction::UP QuantizedAngularDistanceFunctionFactory::for_insertion_vector(TypedCells lhs) const {
+    // Insertion vectors are always in quantized int8 form
+    using DFT = BoundQuantizedAngularDistance<QuantizedLhsAndRhs>;
+    assert(lhs.type == CellType::INT8);
+    return std::make_unique<DFT>(lhs, _dimensions, _bits, _seed);
+}
+
 template class AngularDistanceFunctionFactory<float>;
 template class AngularDistanceFunctionFactory<double>;
 template class AngularDistanceFunctionFactory<Int8Float>;
 template class AngularDistanceFunctionFactory<vespalib::BFloat16>;
+
+template class BoundQuantizedAngularDistance<Float32LhsQuantizedRhs>;
+template class BoundQuantizedAngularDistance<QuantizedLhsAndRhs>;
 
 } // namespace search::tensor
