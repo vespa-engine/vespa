57ab0a1f4774a873d3d428b0dfc7f58a10ca38d0
Tor Brede Vekterli <vekterli@vespa.ai>
2026-07-08 15:23:33 +0000

    Implicitly convert cell types during tensor (de-)quantization
    This generalizes the tensor type support for `quantize_tensor`
    and `dequantize_tensor`, which previously explicitly only supported
    input/output tensors with `float` cell types. This was because
    the quantizer itself operates natively only on `float` vectors
    as the quantization input and dequantization output.
    
    With this commit, both `quantize_tensor` and `dequantize_tensor`
    take in an auxiliary `scratch_space` vector that is used for
    intermediate float conversions iff needed.
    
    Quantization _from_ non-`float` cells will convert distinct dense
    subspaces to `float` via the scratch space prior to quantization.
    
    Dequantization _to_ non-`float` cells will dequantize to the `float`
    scratch space, then convert to the target cell type. This also
    happens per distinct dense subspace. Conversion to `int8` is
    done with round-to-nearest semantics (with saturation), as that
    empirically reduces vector reconstruction error (compared to
    rounding towards zero).
    
    The `scratch_space` vector can be reused across calls and need not
    be sized prior to use.

--- searchlib/src/tests/attribute/tensorattribute/tensor_quantization_test.cpp
+++ searchlib/src/tests/attribute/tensorattribute/tensor_quantization_test.cpp
@@ -5,17 +5,21 @@
 #include <vespa/eval/eval/tensor_spec.h>
 #include <vespa/eval/eval/value.h>
 #include <vespa/eval/eval/value_codec.h>
+#include <vespa/eval/eval/value_type_spec.h>
 #include <vespa/searchlib/tensor/tensor_quantization.h>
 #include <vespa/vespalib/quant/eden.h>
 
 #include <gmock/gmock.h>
 
+#include <format>
 #include <iostream>
 
 using vespalib::eval::CellType;
 using vespalib::eval::FastValueBuilderFactory;
 using vespalib::eval::TensorSpec;
-using vespalib::eval::Value;
+using TensorValue = vespalib::eval::Value; // ambiguity with GTest testing::Value
+using vespalib::BFloat16;
+using vespalib::eval::Int8Float;
 using vespalib::eval::ValueType;
 using vespalib::quant::EdenQuantizer;
 using vespalib::quant::QuantMode;
@@ -26,7 +30,7 @@ namespace search::tensor {
 
 namespace {
 
-Value::UP create_tensor(const TensorSpec& spec) {
+TensorValue::UP create_tensor(const TensorSpec& spec) {
     return value_from_spec(spec, FastValueBuilderFactory::get());
 }
 
@@ -67,61 +71,146 @@ TEST(TensorQuantizationTest, quantized_type_conversion_flattens_indexed_dimensio
     EXPECT_EQ(to_quant_spec("tensor<float>(x{},y{},a[2],b[3],c[4])", 4), "tensor<int8>(c[16],x{},y{})");
 }
 
-TEST(TensorQuantizationTest, can_quantize_and_dequantize_indexed_tensors) {
-    constexpr uint8_t q_bits = 4;
+TEST(TensorQuantizationTest, quantized_tensor_shape_is_invariant_of_input_cell_type) {
+    EXPECT_EQ(to_quant_spec("tensor(x[16])", 1), "tensor<int8>(x[6])");
+    EXPECT_EQ(to_quant_spec("tensor<double>(x[16])", 1), "tensor<int8>(x[6])"); // alias of the above
+    EXPECT_EQ(to_quant_spec("tensor<float>(x[16])", 1), "tensor<int8>(x[6])");
+    EXPECT_EQ(to_quant_spec("tensor<bfloat16>(x[16])", 1), "tensor<int8>(x[6])");
+    EXPECT_EQ(to_quant_spec("tensor<int8>(x[16])", 1), "tensor<int8>(x[6])");
 
-    auto t = create_tensor(
-        TensorSpec("tensor<float>(x[2],y[3])").add({{"x", 0}, {"y", 1}}, 12).add({{"x", 1}, {"y", 2}}, 9));
-    auto q = make_quantizer_from_type(t->type(), q_bits);
-    auto q_type = to_quantized_tensor_type(t->type(), *q);
+    EXPECT_EQ(to_quant_spec("tensor(a{},b{},x[2],y[3],z[4])", 4), "tensor<int8>(a{},b{},z[16])");
+    EXPECT_EQ(to_quant_spec("tensor<double>(a{},b{},x[2],y[3],z[4])", 4), "tensor<int8>(a{},b{},z[16])");
+    EXPECT_EQ(to_quant_spec("tensor<float>(a{},b{},x[2],y[3],z[4])", 4), "tensor<int8>(a{},b{},z[16])");
+    EXPECT_EQ(to_quant_spec("tensor<bfloat16>(a{},b{},x[2],y[3],z[4])", 4), "tensor<int8>(a{},b{},z[16])");
+    EXPECT_EQ(to_quant_spec("tensor<int8>(a{},b{},x[2],y[3],z[4])", 4), "tensor<int8>(a{},b{},z[16])");
+}
 
-    auto qt_mse = search::tensor::quantize_f32_tensor(*t, q_type, *q, QuantMode::MSE);
-    auto dqt_mse = search::tensor::dequantize_tensor(*qt_mse, t->type(), *q);
-    auto qt_ip = search::tensor::quantize_f32_tensor(*t, q_type, *q, QuantMode::InnerProduct);
-    auto dqt_ip = search::tensor::dequantize_tensor(*qt_ip, t->type(), *q);
+namespace {
+
+struct DequantizedTensors {
+    std::unique_ptr<TensorValue> mse_dequant; // MSE-optimized quantization mode
+    std::unique_ptr<TensorValue> ip_dequant;  // Inner-product-optimized quantization mode
+};
+
+DequantizedTensors quantize_tensor_roundtrip(const TensorValue& t, const uint8_t q_bits) {
+    auto q = make_quantizer_from_type(t.type(), q_bits);
+    auto q_type = to_quantized_tensor_type(t.type(), *q);
+
+    std::vector<float> scratch_space;
+
+    auto qt_mse = search::tensor::quantize_tensor(t, q_type, *q, QuantMode::MSE, scratch_space);
+    auto dqt_mse = search::tensor::dequantize_tensor(*qt_mse, t.type(), *q, scratch_space);
+
+    auto qt_ip = search::tensor::quantize_tensor(t, q_type, *q, QuantMode::InnerProduct, scratch_space);
+    auto dqt_ip = search::tensor::dequantize_tensor(*qt_ip, t.type(), *q, scratch_space);
 
     EXPECT_EQ(qt_mse->type(), q_type);
     EXPECT_EQ(qt_ip->type(), q_type);
 
-    // OG vector
-    EXPECT_THAT(t->cells().typify<float>(), ElementsAre(/*x=0*/ 0, 12, 0, /*x=1*/ 0, 0, 9));
-    // we have vector at home, MSE edition:
-    std::vector<float> mse_expected = {-0.364243, 11.9888, 1.24358, -0.301369, -0.404984, 8.79533};
-    EXPECT_THAT(dqt_mse->cells().typify<float>(), Pointwise(FloatNear(0.0001), mse_expected));
-    // Inner-product edition:
-    std::vector<float> ip_expected = {-0.36747, 12.0951, 1.2546, -0.304039, -0.408572, 8.87325};
-    EXPECT_THAT(dqt_ip->cells().typify<float>(), Pointwise(FloatNear(0.0001), ip_expected));
+    return {std::move(dqt_mse), std::move(dqt_ip)};
 }
 
-TEST(TensorQuantizationTest, can_quantize_and_dequantize_mixed_tensors) {
-    constexpr uint8_t q_bits = 4;
+DequantizedTensors quantize_indexed_tensor_roundtrip(const CellType cell_type, const uint8_t q_bits = 4) {
+    std::string cell_type_name = vespalib::eval::value_type::cell_type_to_name(cell_type);
+    std::string spec_str = std::format("tensor<{}>(x[2],y[3])", cell_type_name);
+    // Values are losslessly representable across all cell types, and will therefore
+    // result in the same float values used as input to the quantization algorithm.
+    auto t = create_tensor(TensorSpec(spec_str).add({{"x", 0}, {"y", 1}}, 12).add({{"x", 1}, {"y", 2}}, 9));
 
-    auto t = create_tensor(TensorSpec("tensor<float>(a{},x[2],y[3])")
+    return quantize_tensor_roundtrip(*t, q_bits);
+}
+
+template <typename CT>
+void do_test_4_bit_quantized_roundtrip_of_dense_indexed_floating_point_tensor() {
+    auto dq = quantize_indexed_tensor_roundtrip(vespalib::eval::get_cell_type<CT>(), 4);
+
+    // We can use the same expected floating point values for double/float/BFloat16, since doubles
+    // are implicitly converted to floats (and thus have no more actual precision) and BFloat16 is
+    // implicitly mantissa-truncated before comparison.
+    std::vector<CT> mse_expected = {-0.36424255, 11.988836, 1.243578, -0.30136871, -0.40498352, 8.7953281};
+    EXPECT_THAT(dq.mse_dequant->cells().template typify<CT>(), Pointwise(FloatNear(0.00001), mse_expected));
+
+    std::vector<CT> ip_expected = {-0.36747026, 12.095058, 1.2545962, -0.30403852, -0.40857196, 8.8732548};
+    EXPECT_THAT(dq.ip_dequant->cells().template typify<CT>(), Pointwise(FloatNear(0.00001), ip_expected));
+}
+
+DequantizedTensors quantize_mixed_tensor_roundtrip(const CellType cell_type, const uint8_t q_bits = 4) {
+    std::string cell_type_name = vespalib::eval::value_type::cell_type_to_name(cell_type);
+    std::string spec_str = std::format("tensor<{}>(a{{}},x[2],y[3])", cell_type_name);
+
+    // Will be normalized and arranged as bar=[0, 5, 0, 0, -3, 0], foo=[0, 12, 0, 0, 0, 9] in memory.
+    auto t = create_tensor(TensorSpec(spec_str)
                                .add({{"a", "foo"}, {"x", 0}, {"y", 1}}, 12)
                                .add({{"a", "foo"}, {"x", 1}, {"y", 2}}, 9)
                                .add({{"a", "bar"}, {"x", 0}, {"y", 1}}, 5)
-                               .add({{"a", "bar"}, {"x", 1}, {"y", 1}}, 3));
-    auto q = make_quantizer_from_type(t->type(), q_bits);
-    auto q_type = to_quantized_tensor_type(t->type(), *q);
+                               .add({{"a", "bar"}, {"x", 1}, {"y", 1}}, -3));
 
-    auto qt_mse = search::tensor::quantize_f32_tensor(*t, q_type, *q, QuantMode::MSE);
-    auto dqt_mse = search::tensor::dequantize_tensor(*qt_mse, t->type(), *q);
-    auto qt_ip = search::tensor::quantize_f32_tensor(*t, q_type, *q, QuantMode::InnerProduct);
-    auto dqt_ip = search::tensor::dequantize_tensor(*qt_ip, t->type(), *q);
+    return quantize_tensor_roundtrip(*t, q_bits);
+}
 
-    EXPECT_EQ(qt_mse->type(), q_type);
-    EXPECT_EQ(qt_ip->type(), q_type);
+template <typename CT>
+void do_test_4_bit_quantized_roundtrip_of_mixed_floating_point_tensor() {
+    auto dq = quantize_mixed_tensor_roundtrip(vespalib::eval::get_cell_type<CT>(), 4);
 
-    // OG vector. Arranged as bar=[0, 5, 0, 0, 3, 0], foo=[0, 12, 0, 0, 0, 9]
-    EXPECT_THAT(t->cells().typify<float>(), ElementsAre(0, 5, 0, 0, 3, 0, 0, 12, 0, 0, 0, 9));
+    // As with indexed tensors, we can reuse the expected values across FP cell types.
+    std::vector<CT> mse_expected = {-0.4283701, 4.92065,  0.228362, -0.08518672, -3.048389,  -0.005629063,
+                                    -0.3642426, 11.98884, 1.243578, -0.3013687,  -0.4049835, 8.795328};
+    EXPECT_THAT(dq.mse_dequant->cells().template typify<CT>(), Pointwise(DoubleNear(0.00001), mse_expected));
 
-    std::vector<float> mse_expected = {-0.00648499, 4.85099, 0.384633, -0.0835068, 3.1499,    -0.309387,
-                                       -0.364243,   11.9888, 1.24358,  -0.301369,  -0.404984, 8.79533};
-    EXPECT_THAT(dqt_mse->cells().typify<float>(), Pointwise(FloatNear(0.0001), mse_expected));
+    std::vector<CT> ip_expected = {-0.4315633, 4.957332, 0.2300643, -0.08582163, -3.071113, -0.005670905,
+                                   -0.3674703, 12.09506, 1.254596,  -0.3040385,  -0.408572, 8.873255};
+    EXPECT_THAT(dq.ip_dequant->cells().template typify<CT>(), Pointwise(DoubleNear(0.00001), ip_expected));
+}
 
-    std::vector<float> ip_expected = {-0.00654173, 4.8935,  0.388003, -0.0842385, 3.1775,    -0.312098,
-                                      -0.36747,    12.0951, 1.2546,   -0.304039,  -0.408572, 8.87325};
-    EXPECT_THAT(dqt_ip->cells().typify<float>(), Pointwise(FloatNear(0.0001), ip_expected));
+} // namespace
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_double_dense_indexed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_dense_indexed_floating_point_tensor<double>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_float_dense_indexed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_dense_indexed_floating_point_tensor<float>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_bfloat16_dense_indexed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_dense_indexed_floating_point_tensor<BFloat16>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_int8_dense_indexed_tensors) {
+    auto dq = quantize_indexed_tensor_roundtrip(CellType::INT8);
+    // Original vector is {0, 12, 0, 0, 0, 9}
+    // As an interesting point of comparison, conversion using static_cast (flooring) rather
+    // than std::round (nearest) would yield {0, 11, 1, 0, 0, 8} instead, which has an absolute
+    // error of 3 rather than 1.
+    std::vector<Int8Float> mse_expected = {0, 12, 1, 0, 0, 9};
+    EXPECT_THAT(dq.mse_dequant->cells().typify<Int8Float>(), Pointwise(Eq(), mse_expected));
+
+    std::vector<Int8Float> ip_expected = {0, 12, 1, 0, 0, 9};
+    EXPECT_THAT(dq.ip_dequant->cells().typify<Int8Float>(), Pointwise(Eq(), ip_expected));
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_double_mixed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_mixed_floating_point_tensor<double>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_float_mixed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_mixed_floating_point_tensor<float>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_bfloat16_mixed_tensors) {
+    do_test_4_bit_quantized_roundtrip_of_mixed_floating_point_tensor<BFloat16>();
+}
+
+TEST(TensorQuantizationTest, can_quantize_and_dequantize_int8_mixed_tensors) {
+    auto dq = quantize_mixed_tensor_roundtrip(CellType::INT8, 4);
+    // Original vector is {0, 5, 0, 0, -3, 0, 0, 12, 0, 0, 0, 9}
+    // In this case, using static_cast (flooring) rather than std::round (nearest) would yield
+    // {0, 4, 0, 0, -3, 0, 0, 11, 1, 0, 0, 8} instead, with an absolute error of 4 rather than 1.
+    std::vector<Int8Float> mse_expected = {0, 5, 0, 0, -3, 0, 0, 12, 1, 0, 0, 9};
+    EXPECT_THAT(dq.mse_dequant->cells().typify<Int8Float>(), Pointwise(Eq(), mse_expected));
+
+    std::vector<Int8Float> ip_expected = {0, 5, 0, 0, -3, 0, 0, 12, 1, 0, 0, 9};
+    EXPECT_THAT(dq.ip_dequant->cells().typify<Int8Float>(), Pointwise(Eq(), ip_expected));
 }
 
 } // namespace search::tensor
--- searchlib/src/vespa/searchlib/tensor/tensor_quantization.h
+++ searchlib/src/vespa/searchlib/tensor/tensor_quantization.h
@@ -6,9 +6,10 @@
 #include <vespa/vespalib/quant/eden.h>
 
 #include <memory>
+#include <vector>
 
 /*
- * Generalized support for quantizing and dequantizing indexed and mixed float32 tensors.
+ * Generalized support for quantizing and dequantizing indexed and mixed tensors.
  *
  * We quantize each dense subspace individually, treating each such subspace as if it's logically
  * a single vector whose size is the product of all indexed dimension sizes. This allows for
@@ -16,7 +17,7 @@
  * and also allows for selectively dequantizing individual _mapped_ subspaces.
  *
  * This packing means that a quantized tensor has a differently shaped type than the original
- * float tensor. All mapped dimensions are preserved as-is, but all indexed dimensions are
+ * input tensor. All mapped dimensions are preserved as-is, but all indexed dimensions are
  * replaced with a single "aggregate" int8 indexed dimension whose size is equal to the number
  * of bytes required to store a single full, dense subspace. The number of bytes required
  * is proportional to the number of quantization bits used. To ensure dimension name uniqueness
@@ -38,32 +39,52 @@ namespace search::tensor {
  *
  * The tensor type must contain at least 1 indexed dimension.
  */
-[[nodiscard]] vespalib::eval::ValueType to_quantized_tensor_type(const vespalib::eval::ValueType& f32_tensor_type,
+[[nodiscard]] vespalib::eval::ValueType to_quantized_tensor_type(const vespalib::eval::ValueType&      in_tensor_type,
                                                                  const vespalib::quant::EdenQuantizer& quantizer);
 
 /*
- * Returns a quantized representation of the input `f32_in_tensor`, using the specified quantization mode.
+ * Returns a quantized representation of the input `in_tensor`, using the specified quantization mode.
+ *
+ * Quantization internally operates on float vectors, so if `in_tensor` has a cell type other than
+ * FLOAT the `scratch_space` vector will be resized to the combined dense input subspace size and
+ * used as a temporary conversion buffer. Cells of type DOUBLE will be narrowed (possible precision
+ * loss), all other types (BFLOAT16, INT8) will be widened (lossless).
+ *
+ * `scratch_space` is _not_ touched if the input cell type is FLOAT.
  *
  * Preconditions:
  *   - `quantizer` must be logically the same as that provided as input to `to_quantized_tensor_type()`
  *      (i.e. it must have the same construction parameters, but need not be the same actual instance).
- *   - `f32_in_tensor.type()` must be the same as that provided as input to `to_quantized_tensor_type()`.
+ *   - `in_tensor.type()` must be the same as that provided as input to `to_quantized_tensor_type()`.
  *   - `quantized_type` must be the same as the output of `to_quantized_tensor_type()`.
  */
-[[nodiscard]] std::unique_ptr<vespalib::eval::Value>
-quantize_f32_tensor(const vespalib::eval::Value& f32_in_tensor, const vespalib::eval::ValueType& quantized_type,
-                    vespalib::quant::EdenQuantizer& quantizer, vespalib::quant::QuantMode quantization_mode);
+[[nodiscard]] std::unique_ptr<vespalib::eval::Value> quantize_tensor(const vespalib::eval::Value&     in_tensor,
+                                                                     const vespalib::eval::ValueType& quantized_type,
+                                                                     vespalib::quant::EdenQuantizer&  quantizer,
+                                                                     vespalib::quant::QuantMode quantization_mode,
+                                                                     std::vector<float>&        scratch_space);
 
 /*
+ * Returns a dequantized representation of the input `quantized_in_tensor`, with an output tensor
+ * shape matching that of `out_tensor_type`.
+ *
+ * Dequantization internally operates on float vectors, so if `out_tensor_type` has a cell type
+ * other than FLOAT the `scratch_space` vector will be resized to the combined dense output
+ * subspace size and used as a temporary conversion buffer. Note that int8 output tensors are
+ * converted by floating point _rounding_ (not truncation) followed by saturation to [-128, 127].
+ *
+ * `scratch_space` is _not_ touched if the output cell type is FLOAT.
+ *
  * Preconditions:
  *   - `quantized_in_tensor` must contain tensor data that was produced by a prior call to
- *     `quantize_f32_tensor()`.
- *   - `out_f32_tensor_type` must match the type of the tensor that was originally provided
- *      as `f32_in_tensor` to the `quantize_f32_tensor` function.
- *   - `quantizer` must be logically the same as the one used in the prior `quantize_f32_tensor` call.
+ *     `quantize_tensor()`.
+ *   - `out_tensor_type` must match the type of the tensor that was originally provided
+ *      as `in_tensor` to the `quantize_tensor` function.
+ *   - `quantizer` must be logically the same as the one used in the prior `quantize_tensor` call.
  */
 [[nodiscard]] std::unique_ptr<vespalib::eval::Value>
-dequantize_tensor(const vespalib::eval::Value&     quantized_in_tensor,
-                  const vespalib::eval::ValueType& out_f32_tensor_type, vespalib::quant::EdenQuantizer& quantizer);
+dequantize_tensor(const vespalib::eval::Value& quantized_in_tensor, const vespalib::eval::ValueType& out_tensor_type,
+                  vespalib::quant::EdenQuantizer& quantizer, std::vector<float>& scratch_space);
+
 
 } // namespace search::tensor
