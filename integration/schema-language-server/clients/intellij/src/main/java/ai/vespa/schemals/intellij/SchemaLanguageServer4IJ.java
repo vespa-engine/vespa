f0c2a37cf560dda727b05e76e9e07e7f3979081b
Brage <bragehk@vespa.ai>
2026-08-03 10:18:27 +0200

    fix: removed internal intellij API

--- integration/schema-language-server/clients/intellij/src/main/java/ai/vespa/schemals/intellij/SchemaLanguageServer4IJ.java
+++ integration/schema-language-server/clients/intellij/src/main/java/ai/vespa/schemals/intellij/SchemaLanguageServer4IJ.java
@@ -4,22 +4,12 @@ import com.intellij.openapi.project.Project;
 import com.redhat.devtools.lsp4ij.server.JavaProcessCommandBuilder;
 import com.redhat.devtools.lsp4ij.server.ProcessStreamConnectionProvider;
 
-import java.util.Arrays;
 import java.util.List;
-import com.intellij.openapi.extensions.PluginId;
-import com.intellij.ide.plugins.PluginManagerCore;
 
 public class SchemaLanguageServer4IJ extends ProcessStreamConnectionProvider {
-    
-    public SchemaLanguageServer4IJ(Project project) {
-        PluginId id = PluginId.getId("ai.vespa");
-        var plugin = PluginManagerCore.getPlugin(id);
-        if (plugin == null) {
-            throw new IllegalStateException("Plugin " + id + " not found. Cannot start the Vespa Schema Language Support plugin.");
-        }
-        var pluginPath = plugin.getPluginPath();
 
-        var serverPath = pluginPath
+    public SchemaLanguageServer4IJ(Project project) {
+        var serverPath = PluginPaths.pluginDirectoryOf(SchemaLanguageServer4IJ.class)
                 .resolve("schema-language-server-jar-with-dependencies.jar")
                 .toAbsolutePath()
                 .toString();
