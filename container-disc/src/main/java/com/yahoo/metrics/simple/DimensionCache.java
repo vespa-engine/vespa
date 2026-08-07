a10ddb2a4438149b5ea7b1574808f3cfe21a81aa
Wangshu Pang <pangwangshu@gmail.com>
2026-08-07 00:41:48 -0700

    Skip padding counter metrics in DimensionCache
    Counters are aggregate metrics and should not be carried over when
    padding, per MetricSet.partialClone()'s contract. Fixes counters
    reappearing in state/v1/metrics with count=0 after their time window
    has passed.

--- container-disc/src/main/java/com/yahoo/metrics/simple/DimensionCache.java
+++ container-disc/src/main/java/com/yahoo/metrics/simple/DimensionCache.java
@@ -86,7 +86,7 @@ class DimensionCache {
                 continue;
             }
             Identifier id = new Identifier(metric, leastOld.getKey());
-            if ( ! toPresent.hasIdentifier(id)) {
+            if ( ! toPresent.hasIdentifier(id) && ! leastOld.getValue().metric.isCounter()) {
                 toPresent.put(id, leastOld.getValue().metric.pruneData());
                 --toAdd;
             }
