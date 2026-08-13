90690937b7a473f2907acace14e9c7fd596f2fce
Gleb Sizov <glebsv@gmail.com>
2026-08-13 14:12:06 +0200

    Removed the rest of the dead code

--- configserver/src/test/java/com/yahoo/vespa/config/server/MockConfigConvergenceChecker.java
+++ configserver/src/test/java/com/yahoo/vespa/config/server/MockConfigConvergenceChecker.java
@@ -7,17 +7,12 @@ import com.yahoo.vespa.config.server.application.ConfigConvergenceChecker;
 import com.yahoo.vespa.flags.InMemoryFlagSource;
 
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class MockConfigConvergenceChecker extends ConfigConvergenceChecker {
 
     private final long wantedGeneration;
-    private final List<ServiceInfo> servicesThatFailFirstIteration;
-
-    private int iteration = 0;
 
     public MockConfigConvergenceChecker(long wantedGeneration) {
         this(wantedGeneration, List.of());
@@ -26,7 +21,6 @@ public class MockConfigConvergenceChecker extends ConfigConvergenceChecker {
     public MockConfigConvergenceChecker(long wantedGeneration, List<ServiceInfo> servicesThatFailFirstIteration) {
         super(new InMemoryFlagSource());
         this.wantedGeneration = wantedGeneration;
-        this.servicesThatFailFirstIteration = servicesThatFailFirstIteration;
     }
 
     @Override
@@ -43,18 +37,4 @@ public class MockConfigConvergenceChecker extends ConfigConvergenceChecker {
     public ServiceResponse getServiceConfigGeneration(Application application, String hostAndPortToCheck, Duration timeout) {
         return new ServiceResponse(ServiceResponse.Status.ok, wantedGeneration);
     }
-
-    @Override
-    public ServiceListResponse checkConvergenceUnlessDeferringChangesUntilRestart(Application application, Set<String> hostnames) {
-        iteration++;
-        if (servicesThatFailFirstIteration.isEmpty() || iteration > 1)
-            return new ServiceListResponse(Map.of(), wantedGeneration, wantedGeneration);
-
-        Map<ServiceInfo, Long> services = new HashMap<>();
-        for (var service : servicesThatFailFirstIteration) {
-            services.put(service, wantedGeneration - 1);
-        }
-        return new ServiceListResponse(services, wantedGeneration, wantedGeneration - 1);
-    }
-
 }
