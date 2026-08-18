f441c9c40a151f8b453e1f7ef66e7cf2bce9d87f
Jan Böker <boeker@vespa.ai>
2026-08-18 12:50:22 +0000

    Add StringRangePostingSearchContext

---
+++ searchlib/src/vespa/searchlib/attribute/string_range_posting_search_context.hpp
@@ -0,0 +1,41 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#pragma once
+
+#include "posting_list_folded_search_context.hpp"
+#include "string_range_posting_search_context.h"
+
+namespace search::attribute {
+
+template <typename BaseSC, typename AttrT, typename DataT>
+StringRangePostingSearchContext<BaseSC, AttrT, DataT>::StringRangePostingSearchContext(BaseSC&&     base_sc,
+                                                                                       bool         useBitVector,
+                                                                                       const AttrT& toBeSearched)
+    : Parent(std::move(base_sc), useBitVector, toBeSearched), _range_spec(this->get_string_range_spec()) {
+    if (this->valid() && _range_spec) {
+        const std::string& left = _range_spec->left;
+        const std::string& right = _range_spec->right;
+        auto               comp_left = _enumStore.make_folded_comparator(left.c_str());
+        auto               comp_right = _enumStore.make_folded_comparator(right.c_str());
+        this->lookupRange(comp_left, comp_right);
+        if (this->_uniqueValues == 1u) {
+            this->lookupSingle();
+        }
+    }
+}
+
+template <typename BaseSC, typename AttrT, typename DataT>
+bool StringRangePostingSearchContext<BaseSC, AttrT, DataT>::use_dictionary_entry(
+    PostingListSearchContext::DictionaryConstIterator& /*it*/) const {
+    return true;
+}
+
+template <typename BaseSC, typename AttrT, typename DataT>
+bool StringRangePostingSearchContext<BaseSC, AttrT, DataT>::use_posting_lists_when_non_strict(
+    const ExecuteInfo& /*info*/) const {
+    // Testing
+    return true;
+    // return false;
+}
+
+} // namespace search::attribute
