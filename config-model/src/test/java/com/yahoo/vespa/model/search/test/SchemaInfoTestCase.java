6fc9a9fbd9c6b1596e81bb273f99940394a29ccf
Johan Solbakken <johsol@vespa.ai>
2026-08-25 12:18:54 +0000

    set default to true and updated expected in tests

--- config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
+++ config-model/src/test/java/com/yahoo/vespa/model/search/test/SchemaInfoTestCase.java
@@ -33,6 +33,7 @@ public class SchemaInfoTestCase {
     schema[0].field[0].attribute false
     schema[0].field[0].index false
     schema[0].field[0].bitPacked false
+    schema[0].field[0].fastMapSearch false
     schema[0].summaryclass[0].name "default"
     schema[0].summaryclass[0].fields[0].name "documentid"
     schema[0].summaryclass[0].fields[0].type "longstring"
@@ -56,11 +57,13 @@ public class SchemaInfoTestCase {
     schema[0].field[0].attribute false
     schema[0].field[0].index false
     schema[0].field[0].bitPacked false
+    schema[0].field[0].fastMapSearch false
     schema[0].field[1].name "f2"
     schema[0].field[1].type "string"
     schema[0].field[1].attribute false
     schema[0].field[1].index false
     schema[0].field[1].bitPacked false
+    schema[0].field[1].fastMapSearch false
     schema[0].fieldset[0].name "fs1"
     schema[0].fieldset[0].field[0] "f1"
     schema[0].fieldset[0].field[1] "f2"
@@ -88,6 +91,7 @@ public class SchemaInfoTestCase {
     schema[0].field[0].attribute false
     schema[0].field[0].index false
     schema[0].field[0].bitPacked false
+    schema[0].field[0].fastMapSearch false
     schema[0].field[1].name "f1Index"
     schema[0].field[1].type "string"
     schema[0].field[1].alias[0] "a1"
@@ -95,6 +99,7 @@ public class SchemaInfoTestCase {
     schema[0].field[1].attribute false
     schema[0].field[1].index true
     schema[0].field[1].bitPacked false
+    schema[0].field[1].fastMapSearch false
     schema[0].summaryclass[0].name "default"
     schema[0].summaryclass[0].fields[0].name "documentid"
     schema[0].summaryclass[0].fields[0].type "longstring"
@@ -117,6 +122,7 @@ public class SchemaInfoTestCase {
     schema[0].field[0].attribute false
     schema[0].field[0].index false
     schema[0].field[0].bitPacked false
+    schema[0].field[0].fastMapSearch false
     schema[0].field[1].name "f1Attribute"
     schema[0].field[1].type "int"
     schema[0].field[1].alias[0] "a1"
@@ -124,6 +130,7 @@ public class SchemaInfoTestCase {
     schema[0].field[1].attribute true
     schema[0].field[1].index false
     schema[0].field[1].bitPacked false
+    schema[0].field[1].fastMapSearch false
     schema[0].summaryclass[0].name "default"
     schema[0].summaryclass[0].fields[0].name "documentid"
     schema[0].summaryclass[0].fields[0].type "longstring"
