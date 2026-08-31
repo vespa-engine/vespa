52877124906c5aeae0350b0f4b531b7a97ec95bd
Johan Solbakken <johsol@vespa.ai>
2026-08-31 14:18:40 +0000

    add test case for fast map search class

---
+++ searchlib/src/test/java/com/yahoo/searchlib/document/FastMapSearchTest.java
@@ -0,0 +1,29 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.searchlib.document;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author johsol
+ */
+public class FastMapSearchTest {
+
+    @Test
+    public void testKeyValueSeparatorIsDel() {
+        assertEquals(String.valueOf((char) 0x7F), FastMapSearch.keyValueSeparator());
+    }
+
+    @Test
+    public void testToKeyValueFieldName() {
+        assertEquals("myMap$keyvalue", FastMapSearch.toKeyValueFieldName("myMap"));
+    }
+
+    @Test
+    public void testToKeyValueTerm() {
+        assertEquals("key" + FastMapSearch.keyValueSeparator() + "value",
+                     FastMapSearch.toKeyValueTerm("key", "value"));
+    }
+
+}
