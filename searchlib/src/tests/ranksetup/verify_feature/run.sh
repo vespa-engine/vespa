16b3c35e8bef03bdbb709b43cf9bd445f7cb7fa3
Tor Egge <Tor.Egge@online.no>
2026-09-01 11:06:32 +0200

    Enable valgrind for verify feature unit test.

--- searchlib/src/tests/ranksetup/verify_feature/run.sh
+++ searchlib/src/tests/ranksetup/verify_feature/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-setarch $(arch) -R ./searchlib_verify_feature_test_app
+if test -x /usr/bin/setarch
+then
+    setarch $(arch) -R ${VALGRIND} ./searchlib_verify_feature_test_app
+else
+    ${VALGRIND} ./searchlib_verify_feature_test_app
+fi
