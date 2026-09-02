a3964be42110446bfb08f948bad4cb1e3e3967e5
Jon Bratseth <bratseth@gmail.com>
2026-09-02 11:54:27 +0200

    Remove ClusterSpec from ClusterMembership

--- config-provisioning/src/main/java/com/yahoo/config/provision/ClusterMembership.java
+++ config-provisioning/src/main/java/com/yahoo/config/provision/ClusterMembership.java
@@ -15,8 +15,6 @@ import java.util.Optional;
  */
 public class ClusterMembership {
 
-    private final ClusterSpec cluster;
-
     private final ClusterSpec.Type type;
     private final ClusterSpec.Id id;
     private final int group;
@@ -25,9 +23,16 @@ public class ClusterMembership {
 
     private final String stringValue;
 
-    private ClusterMembership(String stringValue, Version vespaVersion, Optional<DockerImage> dockerImageRepo,
-                              ZoneEndpoint zoneEndpoint, List<SidecarSpec> sidecars, List<AzName> availabilityZones,
-                              Optional<String> profile) {
+    private ClusterMembership(ClusterSpec.Type type, ClusterSpec.Id id, int group, int index, boolean retired) {
+        this.type = type;
+        this.id = id;
+        this.group = group;
+        this.index = index;
+        this.retired = retired;
+        this.stringValue = toStringValue(type, id, group, index, retired);
+    }
+
+    private ClusterMembership(String stringValue) {
         String[] components = stringValue.split("/");
         if (components.length < 3)
             throw new RuntimeException("Could not parse '" + stringValue + "' to a cluster membership. " +
@@ -45,62 +50,36 @@ public class ClusterMembership {
             missingElements = 1;
         }
 
-        boolean exclusive = false;
-        boolean stateful = false;
         boolean retired = false;
         if (components.length > (4 - missingElements)) {
             for (int i = (4 - missingElements); i < components.length; i++) {
                 String component = components[i];
-                switch (component) {
-                    case "exclusive" -> exclusive = true;
-                    case "retired" -> retired = true;
-                    case "stateful" -> stateful = true;
+                if (component.equals("retired")) {
+                    retired = true;
+                    break;
                 }
             }
         }
 
-        this.cluster = ClusterSpec.specification(ClusterSpec.Type.valueOf(components[0]),
-                                                 ClusterSpec.Id.from(components[1]))
-                                  .group(groupIndex == null ? null : ClusterSpec.Group.from(groupIndex))
-                                  .vespaVersion(vespaVersion)
-                                  .exclusive(exclusive)
-                                  .dockerImageRepository(dockerImageRepo)
-                                  .loadBalancerSettings(zoneEndpoint)
-                                  .stateful(stateful)
-                                  .sidecars(sidecars)
-                                  .availabilityZones(availabilityZones)
-                                  .profile(profile.orElse(null))
-                                  .build();
-        this.type = cluster.type();
-        this.id = cluster.id();
+        this.type = ClusterSpec.Type.valueOf(components[0]);
+        this.id = ClusterSpec.Id.from(components[1]);
         this.group = groupIndex == null ? 0 : groupIndex;
         this.index = nodeIndex;
         this.retired = retired;
         this.stringValue = toStringValue();
     }
 
-    private ClusterMembership(ClusterSpec cluster, int index, boolean retired) {
-        this.cluster = cluster;
-        this.type = cluster.type();
-        this.id = cluster.id();
-        this.group = cluster.group().map(ClusterSpec.Group::index).orElse(0);
-        this.index = index;
-        this.retired = retired;
-        this.stringValue = toStringValue();
-    }
-
     protected String toStringValue() {
-        return cluster.type().name() +
-               "/" + cluster.id().value() +
-               (cluster.group().isPresent() ? "/" + cluster.group().get().index() : "/") +
-               "/" + index +
-               ( cluster.isExclusive() ? "/exclusive" : "") +
-               ( retired ? "/retired" : "") +
-               ( cluster.isStateful() ? "/stateful" : "");
+        return toStringValue(type, id, group, index, retired);
     }
 
-    /** Returns the cluster this node is a member of */
-    public ClusterSpec cluster() { return cluster; }
+    protected static String toStringValue(ClusterSpec.Type type,
+                                          ClusterSpec.Id id,
+                                          int group,
+                                          int index,
+                                          boolean retired) {
+        return type.name() + "/" + id.value() + "/" + group + "/" + index + ( retired ? "/retired" : "");
+    }
 
     /** Returns the type of the cluster this belongs to. */
     public ClusterSpec.Type type() { return type; }
@@ -119,20 +98,24 @@ public class ClusterMembership {
 
     /** Returns a copy of this which is retired */
     public ClusterMembership retire() {
-        return new ClusterMembership(cluster, index, true);
+        return new ClusterMembership(type, id, group, index, true);
     }
 
     /** Returns a copy of this node which is not retired */
     public ClusterMembership unretire() {
-        return new ClusterMembership(cluster, index, false);
+        return new ClusterMembership(type, id, group, index, false);
     }
 
     public ClusterMembership with(ClusterSpec newCluster) {
-        return new ClusterMembership(newCluster, index, retired);
+        return new ClusterMembership(newCluster.type(),
+                                     newCluster.id(),
+                                     newCluster.group().map(ClusterSpec.Group::index).orElse(0),
+                                     index,
+                                     retired);
     }
 
     public ClusterMembership withGroup(int group) {
-        return with(cluster.with(Optional.of(ClusterSpec.Group.from(group))));
+        return new ClusterMembership(type, id, group, index, retired);
     }
 
     /**
@@ -145,16 +128,18 @@ public class ClusterMembership {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        ClusterMembership that = (ClusterMembership) o;
-        return index == that.index &&
-               retired == that.retired &&
-               cluster.equals(that.cluster) &&
-               stringValue.equals(that.stringValue);
+        ClusterMembership other = (ClusterMembership)o;
+        if ( ! this.type.equals(other.type)) return false;
+        if ( ! this.id.equals(other.id)) return false;
+        if ( this.group != other.group) return false;
+        if ( this.index != other.index) return false;
+        if ( this.retired != other.retired) return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cluster, index, retired, stringValue);
+        return Objects.hash(type, id, group, index, retired);
     }
 
     @Override
@@ -186,15 +171,19 @@ public class ClusterMembership {
     public static ClusterMembership from(String stringValue, Version vespaVersion, Optional<DockerImage> dockerImageRepo,
                                          ZoneEndpoint zoneEndpoint, List<SidecarSpec> sidecars, List<AzName> availabilityZones,
                                          Optional<String> profile) {
-        return new ClusterMembership(stringValue, vespaVersion, dockerImageRepo, zoneEndpoint, sidecars, availabilityZones, profile);
+        return new ClusterMembership(stringValue);
     }
 
     public static ClusterMembership from(ClusterSpec cluster, int index) {
-        return new ClusterMembership(cluster, index, false);
+        return new ClusterMembership(cluster.type(), cluster.id(), cluster.group().map(ClusterSpec.Group::index).orElse(0), index, false);
     }
 
     public static ClusterMembership retiredFrom(ClusterSpec cluster, int index) {
-        return new ClusterMembership(cluster, index, true);
+        return new ClusterMembership(cluster.type(), cluster.id(), cluster.group().map(ClusterSpec.Group::index).orElse(0), index, true);
+    }
+
+    public static ClusterMembership from(String stringValue) {
+        return new ClusterMembership(stringValue);
     }
 
 }
