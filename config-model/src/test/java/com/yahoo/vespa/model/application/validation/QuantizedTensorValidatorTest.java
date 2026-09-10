75d1f0684b9e547ac6460f6789e8e621842179cd
Tor Brede Vekterli <vekterli@vespa.ai>
2026-09-10 08:59:19 +0000

    Add deployment warning when using quantized tensors
    Currently in an experimental state where things may still change
    under the hood without prior notice (or attempts to gracefully
    handle backwards compatibility), so add a deployment warning if
    quantized tensors are used.

--- config-model/src/test/java/com/yahoo/vespa/model/application/validation/QuantizedTensorValidatorTest.java
+++ config-model/src/test/java/com/yahoo/vespa/model/application/validation/QuantizedTensorValidatorTest.java
@@ -2,6 +2,7 @@
 package com.yahoo.vespa.model.application.validation;
 
 import com.yahoo.config.application.api.ApplicationPackage;
+import com.yahoo.config.application.api.DeployLogger;
 import com.yahoo.config.model.NullConfigModelRegistry;
 import com.yahoo.config.model.deploy.DeployState;
 import com.yahoo.config.model.deploy.TestProperties;
@@ -12,6 +13,7 @@ import com.yahoo.vespa.model.content.utils.ContentClusterBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.logging.Level;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -67,6 +69,30 @@ public class QuantizedTensorValidatorTest {
         }
     }
 
+    // TODO remove this test once no longer experimental
+    @Test
+    void quantized_field_logs_experimental_feature_deployment_warning() {
+        var fieldSpec = quantizedField("tensor<float>(x[1024])");
+        var logger = new TestLogger();
+        assertDoesNotThrow(() -> createModelAndValidate(schema(fieldSpec), logger));
+        assertEquals("WARNING: For schema 'test', field 'f': quantization support is currently EXPERIMENTAL "+
+                "and should only be used for testing. It is not guaranteed that persisted, experimental " +
+                "quantized tensors will be compatible across Vespa versions.\n", logger.toString());
+    }
+
+    // TODO remove this test once no longer experimental
+    @Test
+    void non_quantized_fields_do_not_log_deployment_warnings() {
+        String fieldSpec = """
+               field f type tensor<float>(x[1024]) {
+                 indexing: attribute
+               }
+               """;
+        var logger = new TestLogger();
+        assertDoesNotThrow(() -> createModelAndValidate(schema(fieldSpec), logger));
+        assertEquals("", logger.toString());
+    }
+
     private static String quantizedField(String typeSpec, String extra) {
         return Text.format("""
                field f type %s {
@@ -95,8 +121,17 @@ public class QuantizedTensorValidatorTest {
                 """, fieldSpec);
     }
 
-    private static void createModelAndValidate(String schema) {
-        DeployState deployState = createDeployState(schema);
+    private static class TestLogger implements DeployLogger {
+        private final StringBuilder message = new StringBuilder();
+        @Override
+        public void log(Level level, String message) {
+            this.message.append(Text.format("%s: %s\n", level.getName(), message));
+        }
+        public String toString() { return message.toString(); }
+    }
+
+    private static void createModelAndValidate(String schema, DeployLogger logger) {
+        DeployState deployState = createDeployState(schema, logger);
         VespaModel model;
         try {
             model = new VespaModel(new NullConfigModelRegistry(), deployState);
@@ -106,7 +141,11 @@ public class QuantizedTensorValidatorTest {
         ValidationTester.validate(new QuantizedTensorValidator(), model, deployState);
     }
 
-    private static DeployState createDeployState(String schema) {
+    private static void createModelAndValidate(String schema) {
+        createModelAndValidate(schema, null);
+    }
+
+    private static DeployState createDeployState(String schema, DeployLogger logger) {
         String servicesXml = Text.format("<services version='1.0'>\n%s\n</services>",
                 new ContentClusterBuilder().getXml());
         ApplicationPackage app = new MockApplicationPackage.Builder()
@@ -116,6 +155,9 @@ public class QuantizedTensorValidatorTest {
         var builder = new DeployState.Builder()
                 .applicationPackage(app)
                 .properties(new TestProperties().setHostedVespa(false));
+        if (logger != null) {
+            builder.deployLogger(logger);
+        }
         return builder.build();
     }
 
