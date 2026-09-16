19755d39b41f7237add22912495d15e098cb9203
Arne Juul <arnej@vespa.ai>
2026-09-16 09:23:22 +0000

    Revert "Fall back to AZ-unaware dispatch when zone info is unreliable"
    This reverts commit f1d0f834f9e35cfec4277c86ca5b9c2e574f25e0.

--- container-search/src/main/java/com/yahoo/search/dispatch/lb/LoadBalancer.java
+++ container-search/src/main/java/com/yahoo/search/dispatch/lb/LoadBalancer.java
@@ -3,7 +3,6 @@ package com.yahoo.search.dispatch.lb;
 
 import com.yahoo.search.dispatch.RequestDuration;
 import com.yahoo.search.dispatch.searchcluster.Group;
-import com.yahoo.search.dispatch.searchcluster.Node;
 
 import java.time.Duration;
 import java.util.Collection;
@@ -50,17 +49,11 @@ public class LoadBalancer {
             case LATENCY_AMORTIZED_OVER_TIME -> new AdaptiveScheduler(AdaptiveScheduler.Type.TIME, new Random(seed), scoreboard);
         };
 
-        // Unless this container and every group has a proper AZ assigned, fall back to non-AZ-aware routing.
-        if (localAvailabilityZone.equals(Node.UNKNOWN_AVAILABILITY_ZONE) ||
-            groups.stream().anyMatch(group -> group.availabilityZone().equals(Node.UNKNOWN_AVAILABILITY_ZONE))) {
-            this.remoteGroups = Set.of();
-        }
-        else {
-            this.remoteGroups = groups.stream()
-                                      .filter(group -> !group.availabilityZone().equals(localAvailabilityZone))
-                                      .map(Group::id)
-                                      .collect(Collectors.toUnmodifiableSet());
-        }
+
+        this.remoteGroups = groups.stream()
+                                  .filter(group -> !group.availabilityZone().equals(localAvailabilityZone))
+                                  .map(Group::id)
+                                  .collect(Collectors.toUnmodifiableSet());
     }
 
     /**
