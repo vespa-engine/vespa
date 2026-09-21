acef2354d347b013deccf20a6b00c1fe70d60af7
Jan Böker <boeker@vespa.ai>
2026-09-21 11:42:05 +0000

    Avoid warnings by explicitly defining constructors in .cpp file

--- searchlib/src/vespa/searchlib/query/string_range_spec.cpp
+++ searchlib/src/vespa/searchlib/query/string_range_spec.cpp
@@ -10,6 +10,27 @@
 
 namespace search {
 
+StringRangeSpec::StringRangeSpec() = default;
+
+StringRangeSpec::StringRangeSpec(std::string left_in, bool left_closed_in, bool left_unbounded_in,
+                                 std::string right_in, bool right_closed_in, bool right_unbounded_in)
+    : StringRangeSpec(std::move(left_in), left_closed_in, left_unbounded_in, std::move(right_in), right_closed_in,
+                      right_unbounded_in, 0) {
+}
+
+StringRangeSpec::StringRangeSpec(std::string left_in, bool left_closed_in, bool left_unbounded_in,
+                                 std::string right_in, bool right_closed_in, bool right_unbounded_in,
+                                 int32_t range_limit_in)
+    : left(std::move(left_in)),
+      left_closed(left_closed_in),
+      left_unbounded(left_unbounded_in),
+      right(std::move(right_in)),
+      right_closed(right_closed_in),
+      right_unbounded(right_unbounded_in),
+      range_limit(range_limit_in) {
+}
+
+StringRangeSpec::StringRangeSpec(const StringRangeSpec&) = default;
 StringRangeSpec::~StringRangeSpec() = default;
 
 } // namespace search
